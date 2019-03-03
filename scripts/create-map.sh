7f4d0ad178aab32a4da4be574d1066fdb70163d7
Alban Crequy <alban@kinvolk.io>
2019-03-03 18:37:21 +0100

    script feedback

--- scripts/create-map.sh
+++ scripts/create-map.sh
@@ -13,3 +13,5 @@ rm -f $bpfdir/containermap
 
 $BPFTOOL map create $innermap type hash key 64 value 64 entries 64 name containermapinner flags 1
 $BPFTOOL map create $bpfdir/containermap type hash_of_maps innermap pinned $innermap key 64 value 4 entries 64 name containermap flags 1
+
+echo done
