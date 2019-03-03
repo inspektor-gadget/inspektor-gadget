c19fa8192a1a54c90ec3ce45214d14fddf1e3c73
Alban Crequy <alban@kinvolk.io>
2019-03-03 18:07:53 +0100

    fix inner map

--- scripts/create-map.sh
+++ scripts/create-map.sh
@@ -6,7 +6,7 @@ set -x
 BPFTOOL=${BPFTOOL:-bpftool}
 
 bpfdir=/sys/fs/bpf
+innermap=$bpfdir/containermapinner
 
-innermap=/sys/fs/bpf/containerids/065001d1cd080f5559f455779fdb85e52d6f5c197fe83bac763c7bd1f08f4f6b
-
+$BPFTOOL map create $innermap type hash key 64 value 64 entries 64 name containermapinner flags 1
 $BPFTOOL map create $bpfdir/containermap type hash_of_maps innermap pinned $innermap key 64 value 4 entries 64 name containermap flags 1
