178a67aedaed0d36b0c4664cc4832dda702f8360
Alban Crequy <alban@kinvolk.io>
2019-03-03 19:58:32 +0100

    debugging script

--- scripts/update-labels.sh
+++ scripts/update-labels.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -e
-set -x
 
 bpfdir=/sys/fs/bpf/containerids
 mkdir -p $bpfdir
@@ -22,11 +21,12 @@ kubectl get pod --all-namespaces -o json | \
 	[.namespace, .podName, .nodename, .labels, .statuses.containerId, .statuses.containerName] |
 	@tsv' | \
   while IFS=$'\t' read -r namespace podname nodename labels containerid containername; do
+    set -e
     echo "Processing container: $namespace $podname $nodename $containerid $containername"
     rm -f $bpfdir/$containerid
     bpftool map create $bpfdir/$containerid type hash key 64 value 64 entries 64 name $containerid flags 1
     echo "Labels"
-    echo $labels | base64 -d
+    echo $labels | base64 -d | jq '.'
     echo $labels | base64 -d | \
 	jq -r 'to_entries[] |
 		{
@@ -36,6 +36,7 @@ kubectl get pod --all-namespaces -o json | \
 		[.key, .value] |
 		@tsv' | \
 	while IFS=$'\t' read -r key value; do
+                set -e
 		echo "Processing label $key=$value in $containerid"
 		key_hex=$(printf "%-64s" "$key" | od -t x1 -w64 -v | head -1 | cut -d" " -f2-)
 		value_hex=$(printf "%-64s" "$value" | od -t x1 -w64 -v | head -1 | cut -d" " -f2-)
