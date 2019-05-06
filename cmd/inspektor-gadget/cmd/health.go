0d7295ca2dc43d35370c7dd43a9ea183f5dddae0
Alban Crequy <alban@kinvolk.io>
2019-05-06 13:11:50 +0200

    Remove execPod() code for now
    It does not work and it is not used. We can always re-add it later when
    we get it to work.

--- cmd/inspektor-gadget/cmd/health.go
+++ cmd/inspektor-gadget/cmd/health.go
@@ -10,20 +10,12 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	corev1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
-	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
-	restclient "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 
-	"github.com/kinvolk/inspektor-gadget/pkg/factory"
 	"github.com/kinvolk/inspektor-gadget/pkg/k8sutil"
-	"github.com/kinvolk/inspektor-gadget/pkg/podexecuter"
 )
 
 var healthCmd = &cobra.Command{
@@ -66,67 +58,6 @@ func runHealth(cmd *cobra.Command, args []string) {
 		fmt.Fprintln(w, line)
 	}
 	w.Flush()
-}
-
-func execPod(client *kubernetes.Clientset, node string) string {
-	var listOptions = metaV1.ListOptions{
-		LabelSelector: "k8s-app=gadget",
-		FieldSelector: "spec.nodeName=" + node + ",status.phase=Running",
-	}
-	pods, err := client.CoreV1().Pods("kube-system").List(listOptions)
-	if err != nil {
-		return fmt.Sprintf("%s", err)
-	}
-	if len(pods.Items) == 0 {
-		return "not-found"
-	}
-	if len(pods.Items) != 1 {
-		return "too-many"
-	}
-	podName := pods.Items[0].Name
-
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	loadingRules.DefaultClientConfig = &clientcmd.DefaultClientConfig
-	if viper.GetString("kubeconfig") != "" {
-		loadingRules.ExplicitPath = viper.GetString("kubeconfig")
-	}
-	overrides := &clientcmd.ConfigOverrides{ClusterDefaults: clientcmd.ClusterDefaults}
-	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, overrides)
-
-	restConfig, err := clientConfig.ClientConfig()
-	if err != nil {
-		return fmt.Sprintf("%s", err)
-	}
-	factory.SetKubernetesDefaults(restConfig)
-	restClient, err := restclient.RESTClientFor(restConfig)
-	if err != nil {
-		return fmt.Sprintf("RESTClientFor: %s", err)
-	}
-
-	// TODO: consider abstracting into a client invocation or client helper
-	req := restClient.Post().
-		Resource("pods").
-		Name(podName).
-		Namespace("kube-system").
-		SubResource("exec")
-	req.VersionedParams(&corev1.PodExecOptions{
-		Container: "gadget",
-		Command:   []string{"/bin/sh", "-c", "echo BOO ; sleep 300"},
-	}, scheme.ParameterCodec)
-
-	streams := genericclioptions.IOStreams{
-		In:     nil,
-		Out:    os.Stdout,
-		ErrOut: os.Stderr,
-	}
-	coreClient, err := corev1client.NewForConfig(restConfig)
-	if err != nil {
-		return fmt.Sprintf("%s", err)
-	}
-	e := podexecuter.NewPodExecuter(coreClient, restConfig, streams)
-	e.Exec(fmt.Sprintf("k8s-app=gadget"), "kube-system")
-
-	return fmt.Sprintf("%s", pods.Items[0].Status.Phase)
 }
 
 func execPodQuick(client *kubernetes.Clientset, node string, podCmd string) string {
