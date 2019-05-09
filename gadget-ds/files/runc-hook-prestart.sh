f157f62ddec57fe999fea5a09a9d32bae0389f6f
Kai Lüke <kai@kinvolk.io>
2019-05-09 19:10:19 +0200

    Create metadata map and add filters for namespace and pod name

--- gadget-ds/files/runc-hook-prestart.sh
+++ gadget-ds/files/runc-hook-prestart.sh
@@ -35,12 +35,20 @@ if [ ! -f $BPFDIR/cgroupmap ] ; then
 fi
 if [ ! -f $BPFDIR/cgrouplabelsmap ] ; then
   INNERMAP=$BPFDIR/containermapinner
+  INNERMAPMETA=$BPFDIR/containermapinnermeta
   rm -f $INNERMAP
+  rm -f $INNERMAPMETA
   rm -f $BPFDIR/containermap
   rm -f $BPFDIR/cgrouplabelsmap
+  # templates for inner maps
   $BPFTOOL map create $INNERMAP type hash key 64 value 64 entries 64 name containermapinner flags 1
+  $BPFTOOL map create $INNERMAPMETA type array key 4 value 64 entries 64 name containermapinnermeta
+  # containermap is only needed in Flatcar alpha/beta/stable, not Edge where an OCI hook exists. It is written by pidmap and read by *snoop (not *snoop-edge)
   $BPFTOOL map create $BPFDIR/containermap type hash_of_maps innermap pinned $INNERMAP key 64 value 4 entries 8000 name containermap flags 1
-  $BPFTOOL map create $BPFDIR/cgrouplabelsmap type hash_of_maps innermap pinned $INNERMAP key 8 value 4 entries 8000 name containermap flags 1
+  # create map from cgroup ID to label map, filled with maps for all pods here later, read by *snoop-edge
+  $BPFTOOL map create $BPFDIR/cgrouplabelsmap type hash_of_maps innermap pinned $INNERMAP key 8 value 4 entries 8000 name cgrouplabelsmap flags 1
+  # create map from cgroup ID to metadata map, filled with maps for all pods here later, read by *snoop-edge
+  $BPFTOOL map create $BPFDIR/cgroupmetadatamap type hash_of_maps innermap pinned $INNERMAPMETA key 8 value 4 entries 8000 name cgroupmetadatamap flags 1
 fi
 
 $BPFTOOL map update pinned $BPFDIR/cgroupmap key hex $CGROUP_ID_HEX value hex $CONTAINERID_HEX
@@ -67,8 +75,11 @@ $KUBECTL --kubeconfig=/etc/kubernetes/kubeconfig get pod --all-namespaces -o jso
       continue
     fi
     echo "Processing container: $namespace $podname $nodename $containername"
+    # create label and metadata map for this pod
     rm -f $BPFDIR/labels$CGROUP_ID
     $BPFTOOL map create $BPFDIR/labels$CGROUP_ID type hash key 64 value 64 entries 64 name labels$CGROUP_ID flags 1
+    rm -f $BPFDIR/metadata$CGROUP_ID
+    $BPFTOOL map create $BPFDIR/metadata$CGROUP_ID type array key 4 value 64 entries 2 name metadata$CGROUP_ID
 
     if [ "$PAUSE_CONTAINER" = "no" ] ; then
       echo "Registering to straceback"
@@ -76,6 +87,20 @@ $KUBECTL --kubeconfig=/etc/kubernetes/kubeconfig get pod --all-namespaces -o jso
     else
       echo "Found pause container. Don't register to straceback"
     fi
+
+    echo "Metadata"
+    # 0: namespace, 1: podname
+    namespace_hex=$(printf "%-64s" "$namespace" | od -t x1 -w64 -v | head -1 | cut -d" " -f2-)
+    echo "  $namespace_hex"
+    podname_hex=$(printf "%-64s" "$podname" | od -t x1 -w64 -v | head -1 | cut -d" " -f2-)
+    echo "  $podname_hex"
+    $BPFTOOL map update pinned $BPFDIR/metadata$CGROUP_ID key 0 value hex $namespace_hex
+    echo "bpftool map updated returned: $?"
+    $BPFTOOL map update pinned $BPFDIR/metadata$CGROUP_ID key 1 value hex $podname_hex
+    echo "bpftool map updated returned: $?"
+    # update cgroup ID map to point to this new pod map
+    $BPFTOOL map update pinned $BPFDIR/cgroupmetadatamap key hex $CGROUP_ID_HEX value pinned $BPFDIR/metadata$CGROUP_ID
+    echo "bpftool map updated returned: $?"
 
     echo "Labels"
     echo $labels | base64 -d | jq '.'
