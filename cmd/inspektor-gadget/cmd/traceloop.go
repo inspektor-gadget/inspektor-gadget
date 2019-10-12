af1aa900b6f8b1a9eeeb2c9b89c1a3fa93b0b62a
Alban Crequy <alban@kinvolk.io>
2019-10-12 18:59:18 +0200

    traceloop: add subcmd pod to get a pod automatically

--- cmd/inspektor-gadget/cmd/traceloop.go
+++ cmd/inspektor-gadget/cmd/traceloop.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -35,6 +36,18 @@ var traceloopShowCmd = &cobra.Command{
 	Run:   runTraceloopShow,
 }
 
+var traceloopPodCmd = &cobra.Command{
+	Use:   "pod",
+	Short: "show the traces in one pod",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 3 {
+			return errors.New("requires 3 arguments: namespace, pod name and idx")
+		}
+		return nil
+	},
+	Run: runTraceloopPod,
+}
+
 var traceloopCloseCmd = &cobra.Command{
 	Use:   "close",
 	Short: "close one trace",
@@ -45,6 +58,7 @@ func init() {
 	rootCmd.AddCommand(traceloopCmd)
 	traceloopCmd.AddCommand(traceloopListCmd)
 	traceloopCmd.AddCommand(traceloopShowCmd)
+	traceloopCmd.AddCommand(traceloopPodCmd)
 	traceloopCmd.AddCommand(traceloopCloseCmd)
 }
 
@@ -111,6 +125,40 @@ func runTraceloopShow(cmd *cobra.Command, args []string) {
 		fmt.Printf("%s", execPodSimple(client, node.Name,
 			fmt.Sprintf(`curl --silent --unix-socket /run/traceloop.socket 'http://localhost/dump-by-name?name=%s' ; echo`, args[0])))
 	}
+}
+
+func runTraceloopPod(cmd *cobra.Command, args []string) {
+	contextLogger := log.WithFields(log.Fields{
+		"command": "inspektor-gadget traceloop pod namespace podname idx",
+		"args":    args,
+	})
+
+	if len(args) < 3 {
+		contextLogger.Fatalf("Missing parameter: namespace or podname or idx")
+	} else if len(args) > 3 {
+		contextLogger.Fatalf("Too many parameters")
+	}
+	namespace := args[0]
+	podname := args[1]
+	idx := args[2]
+
+	client, err := k8sutil.NewClientset(viper.GetString("kubeconfig"))
+	if err != nil {
+		contextLogger.Fatalf("Error in creating setting up Kubernetes client: %q", err)
+	}
+
+	pod, err := client.CoreV1().Pods(namespace).Get(podname, metaV1.GetOptions{})
+	if err != nil {
+		contextLogger.Fatalf("Cannot get pod %s: %q", podname, err)
+	}
+
+	if pod.Spec.NodeName == "" {
+		contextLogger.Fatalf("Pod %s not scheduled yet", podname)
+	}
+
+	fmt.Printf("%s", execPodSimple(client, pod.Spec.NodeName,
+		fmt.Sprintf(`curl --silent --unix-socket /run/traceloop.socket 'http://localhost/dump-pod?namespace=%s&podname=%s&idx=%s' ; echo`,
+			namespace, podname, idx)))
 }
 
 func runTraceloopClose(cmd *cobra.Command, args []string) {
