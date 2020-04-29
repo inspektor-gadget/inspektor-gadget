c400762c7e9cc4b23a860d70f81961294ebe21f2
Alban Crequy <alban@kinvolk.io>
2020-04-29 22:28:11 +0200

    traceloop: handle container ids with cri-o://

--- cmd/kubectl-gadget/traceloop.go
+++ cmd/kubectl-gadget/traceloop.go
@@ -180,6 +180,7 @@ func runTraceloopList(cmd *cobra.Command, args []string) {
 				}
 				containerID := trace.ContainerID
 				containerID = strings.TrimPrefix(containerID, "docker://")
+				containerID = strings.TrimPrefix(containerID, "cri-o://")
 				if len(containerID) > 8 {
 					containerID = containerID[:8]
 				}
