0a658a382f5981ebc740a430808b553f4d682690
Mauricio Vásquez <mauricio@kinvolk.io>
2020-05-13 18:22:31 -0500

    pkg/containerutils: Fix for GetCgroupPaths
    If the path is "/", make it "".

--- pkg/gadgettracermanager/containerutils/containerutils.go
+++ pkg/gadgettracermanager/containerutils/containerutils.go
@@ -105,6 +105,14 @@ func GetCgroupPaths(pid int) (string, string, error) {
 		return "", "", fmt.Errorf("cannot parse cgroup: %v", err)
 	}
 
+	if cgroupPathV1 == "/" {
+		cgroupPathV1 = ""
+	}
+
+	if cgroupPathV2 == "/" {
+		cgroupPathV2 = ""
+	}
+
 	if cgroupPathV2 == "" && cgroupPathV1 == "" {
 		return "", "", fmt.Errorf("cannot find cgroup path in /proc/PID/cgroup")
 	}
