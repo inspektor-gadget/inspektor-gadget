4e3a29ee91e73924f6b62ba8a0f41ff561ac2733
Alban Crequy <alban@kinvolk.io>
2020-05-22 13:45:12 +0200

    Add integration tests with minikube

--- Makefile
+++ Makefile
@@ -48,4 +48,7 @@ test:
 
 .PHONY: integration-tests
 integration-tests:
-	go test -test.v ./integration/... -integration -image docker.io/kinvolk/gadget:latest # FIXME
+	KUBECTL_GADGET="$(shell pwd)/kubectl-gadget-linux-amd64" \
+		go test -test.v ./integration/... \
+			-integration \
+			-image docker.io/kinvolk/gadget:$(shell ./tools/image-tag branch)
--- integration/cli_test.go
+++ integration/cli_test.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"os"
 	"os/exec"
+	"regexp"
 	"testing"
 	"text/template"
 
@@ -22,11 +23,13 @@ func TestDeploy(t *testing.T) {
 	}
 
 	commands := []struct {
-		name         string
-		cmd          string
-		outputName   string
-		expected     string
-		ignoreOutput bool
+		name           string
+		cmd            string
+		outputName     string
+		expected       string
+		expectedRegexp string
+		ignoreOutput   bool
+		debugFailed    bool
 	}{
 		{
 			name:         "Cleanup test namespace from previous tests",
@@ -35,7 +38,7 @@ func TestDeploy(t *testing.T) {
 		},
 		{
 			name:         "Cleanup gadget deployment from previous tests",
-			cmd:          "kubectl gadget deploy $GADGET_IMAGE_FLAG | kubectl delete --force --grace-period=0 -f - || true",
+			cmd:          "$KUBECTL_GADGET deploy $GADGET_IMAGE_FLAG | kubectl delete --force --grace-period=0 -f - || true",
 			ignoreOutput: true,
 		},
 		{
@@ -45,7 +48,12 @@ func TestDeploy(t *testing.T) {
 		},
 		{
 			name:         "Wait until Inspektor Gadget is ready",
-			cmd:          "for POD in $(kubectl get pod -n kube-system -l k8s-app=gadget -o name) ; do kubectl wait -n kube-system --for=condition=ready $POD ; done ; sleep 2",
+			cmd:          "for POD in $(sleep 5 ; kubectl get pod -n kube-system -l k8s-app=gadget -o name) ; do kubectl wait -n kube-system --for=condition=ready $POD ; done ; kubectl get pod -n kube-system ; sleep 15",
+			ignoreOutput: true,
+		},
+		{
+			name:         "Show logs in the gadget pods",
+			cmd:          "for POD in $(kubectl get pod -n kube-system -l k8s-app=gadget -o name) ; do echo Logs for $POD ; kubectl logs -n kube-system $POD ; echo ; done",
 			ignoreOutput: true,
 		},
 		{
@@ -54,25 +62,66 @@ func TestDeploy(t *testing.T) {
 			expected: "namespace/test-ig created\n",
 		},
 		{
-			name:     "Run multiplication pod",
-			cmd:      "kubectl run --restart=Never -ti -n test-ig --image=busybox multiplication -- sh -c 'RANDOM=output ; echo \"3*7*2\" | bc > /tmp/file-$RANDOM'",
-			expected: "",
+			name:         "Run multiplication pod",
+			cmd:          "kubectl run --restart=Never -n test-ig --image=busybox multiplication -- sh -c 'RANDOM=output ; echo \"3*7*2\" | bc > /tmp/file-$RANDOM ; sleep infinity'",
+			ignoreOutput: true,
+		},
+		{
+			name:         "Wait until multiplication pod is ready",
+			cmd:          "sleep 5 ; kubectl wait -n test-ig --for=condition=ready pod/multiplication ; kubectl get pod -n test-ig ; sleep 2",
+			ignoreOutput: true,
+		},
+		{
+			name:         "Run debug pod",
+			cmd:          "kubectl delete pod shell1 ; kubectl delete pod shell2 ; kubectl get pod -A -o yaml ; mount ; kubectl run --restart=Never -i -n default --image=busybox shell1 -- sh -c 'echo SHELL1;ls -l /proc/self/ns/ ; ls -l /sys/fs/cgroup ; mount' ; kubectl run --restart=Never -n default --image=busybox shell2 -- sh -c 'echo SHELL2;ls -l /proc/self/ns/'",
+			ignoreOutput: true,
+			debugFailed:  true,
 		},
 		{
 			name:     "Check traceloop list",
-			cmd:      "sleep 2 ; $KUBECTL_GADGET traceloop list -n test-ig --no-headers | awk '{print $1\" \"$6}'",
-			expected: "multiplication deleted\n",
+			cmd:      "sleep 5 ; $KUBECTL_GADGET traceloop list -n test-ig --no-headers | grep multiplication | awk '{print $1\" \"$6}'",
+			expected: "multiplication started\n",
 		},
 		{
 			name:         "Get trace ID for the multiplication pod",
-			cmd:          `$KUBECTL_GADGET traceloop list -n test-ig --no-headers | awk '{printf "%s", $4}'`,
+			cmd:          `$KUBECTL_GADGET traceloop list -n test-ig --no-headers | grep multiplication | awk '{printf "%s", $4}'`,
 			outputName:   "multiplication_trace_id",
 			ignoreOutput: true,
 		},
 		{
-			name:     "Check traceloop show",
-			cmd:      `$KUBECTL_GADGET traceloop show {{index .Value "multiplication_trace_id"}} | grep '\[bc\] write(1, .*, 3) = 3' | sed 's/^.*\[bc\]/[bc]/'`,
-			expected: "[bc] write(1, \"42\\n\", 3) = 3\n",
+			name:           "Check traceloop show",
+			cmd:            `$KUBECTL_GADGET traceloop show {{index .Value "multiplication_trace_id"}}`,
+			expectedRegexp: "\\[bc\\] write\\(1, \"42\\\\n\", 3\\) = 3\n",
+		},
+		{
+			name:         "Debug: gadget logs",
+			cmd:          "for POD in $(kubectl get pod -n kube-system -l k8s-app=gadget -o name) ; do echo Logs for $POD ; kubectl logs -n kube-system $POD ; echo ; done",
+			ignoreOutput: true,
+			debugFailed:  true,
+		},
+		{
+			name:         "Debug: traceloop list",
+			cmd:          "$KUBECTL_GADGET traceloop list -A",
+			ignoreOutput: true,
+			debugFailed:  true,
+		},
+		{
+			name:         "Debug",
+			cmd:          "id ; ls -l /proc/self/ns ; docker ps ; sudo lsns -t uts -J",
+			ignoreOutput: true,
+			debugFailed:  true,
+		},
+		{
+			name:         "Debug BPF printks",
+			cmd:          "sudo cat /sys/kernel/debug/tracing/trace",
+			ignoreOutput: true,
+			debugFailed:  true,
+		},
+		{
+			name:         "Debug2",
+			cmd:          "for i in $(docker ps |grep -v CONTAINER|awk '{print $1}') ; do echo $i ; PID=$(docker inspect $i |jq '.[0].State.Pid') ; docker inspect $i|jq -r '.[0].Config.Image'; echo pid=$PID; sudo ls -l /proc/$PID/ns/uts; done",
+			ignoreOutput: true,
+			debugFailed:  true,
 		},
 		{
 			name:     "Cleanup test namespace",
@@ -81,12 +130,15 @@ func TestDeploy(t *testing.T) {
 		},
 		{
 			name:     "Cleanup gadget deployment",
-			cmd:      "kubectl gadget deploy $GADGET_IMAGE_FLAG | kubectl delete -f -",
+			cmd:      "$KUBECTL_GADGET deploy $GADGET_IMAGE_FLAG | kubectl delete -f -",
 			expected: "serviceaccount \"gadget\" deleted\nclusterrolebinding.rbac.authorization.k8s.io \"gadget\" deleted\ndaemonset.apps \"gadget\" deleted\n",
 		},
 	}
 
-	os.Setenv("KUBECTL_GADGET", "../kubectl-gadget")
+	if os.Getenv("KUBECTL_GADGET") == "" {
+		t.Fatalf("please set $KUBECTL_GADGET.")
+	}
+
 	if *image != "" {
 		os.Setenv("GADGET_IMAGE_FLAG", "--image "+*image)
 	}
@@ -99,7 +151,7 @@ func TestDeploy(t *testing.T) {
 	failed := false
 	for _, tt := range commands {
 		t.Run(tt.name, func(t *testing.T) {
-			if failed {
+			if failed && !tt.debugFailed {
 				t.Skip("Previous command failed.")
 			}
 			tmpl, err := template.New("cmd").Parse(tt.cmd)
@@ -127,9 +179,17 @@ func TestDeploy(t *testing.T) {
 				outputs.Value[tt.outputName] = actual
 			}
 
-			if !tt.ignoreOutput && actual != tt.expected {
-				failed = true
-				t.Fatalf("diff: %v", pretty.Diff(tt.expected, actual))
+			if !tt.ignoreOutput {
+				if tt.expectedRegexp != "" {
+					r := regexp.MustCompile(tt.expectedRegexp)
+					if !r.MatchString(actual) {
+						failed = true
+						t.Fatalf("regexp didn't match: %s\n%s\n", tt.expectedRegexp, actual)
+					}
+				} else if actual != tt.expected {
+					failed = true
+					t.Fatalf("diff: %v", pretty.Diff(tt.expected, actual))
+				}
 			}
 		})
 	}
