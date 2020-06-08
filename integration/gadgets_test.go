9f5ebe126e200cd90e46a97dcec579ea09c54aa1
Alban Crequy <alban@kinvolk.io>
2020-06-08 16:07:21 +0200

    Integration tests: add tests for other gadgets

--- integration/gadgets_test.go
+++ integration/gadgets_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"os"
 	"os/exec"
@@ -15,18 +16,29 @@ var integration = flag.Bool("integration", false, "run integration tests")
 // image such as docker.io/kinvolk/gadget:latest
 var image = flag.String("image", "", "gadget container image")
 
-func TestTraceloop(t *testing.T) {
+type command struct {
+	id             string
+	name           string
+	cmd            string
+	background     bool
+	expectedString string
+	expectedRegexp string
+	cleanup        bool
+}
+
+type backgroundCommand struct {
+	id     string
+	cmd    *exec.Cmd
+	stdout bytes.Buffer
+	stderr bytes.Buffer
+}
+
+func TestGadgets(t *testing.T) {
 	if !*integration {
 		t.Skip("skipping integration test.")
 	}
 
-	commands := []struct {
-		name           string
-		cmd            string
-		expectedString string
-		expectedRegexp string
-		cleanup        bool
-	}{
+	commands := []command{
 		{
 			name:           "Deploy Inspektor Gadget",
 			cmd:            "$KUBECTL_GADGET deploy $GADGET_IMAGE_FLAG | kubectl apply -f -",
@@ -39,6 +51,48 @@ func TestTraceloop(t *testing.T) {
 		{
 			name: "Wait until Inspektor Gadget is initialised",
 			cmd:  "sleep 15",
+		},
+		{
+			id:         "execsnoop",
+			name:       "Start execsnoop",
+			cmd:        "$KUBECTL_GADGET execsnoop --namespace test-snoop",
+			background: true,
+		},
+		{
+			id:         "tcptracer",
+			name:       "Start tcptracer",
+			cmd:        "$KUBECTL_GADGET tcptracer --namespace test-snoop",
+			background: true,
+		},
+		{
+			name: "Wait until gadgets are ready",
+			cmd:  "sleep 10",
+		},
+		{
+			name:           "Create test namespace",
+			cmd:            "kubectl create ns test-snoop",
+			expectedString: "namespace/test-snoop created\n",
+		},
+		{
+			name: "Run test pod",
+			cmd:  "kubectl run -i --restart=Never -n test-snoop --image=busybox snooptest -- sh -c 'sleep 2 ; cat /non-existent ; nc -w 1 -l -p 8080 & (echo ok | nc -w 1 127.0.0.1 8080) ; sleep 10 ; exit 0'",
+		},
+		{
+			id:   "execsnoop",
+			name: "Stop execsnoop",
+			expectedRegexp: "(?s)NODE PCOMM            PID    PPID   RET ARGS\n" +
+				".*/bin/cat /non-existent\n",
+		},
+		{
+			id:             "tcptracer",
+			name:           "Stop tcptracer",
+			expectedRegexp: "C .*nc .*4 .*127.0.0.1 .*127.0.0.1 .*8080",
+		},
+		{
+			name:           "Cleanup test namespace",
+			cmd:            "kubectl delete ns test-snoop",
+			expectedString: "namespace \"test-snoop\" deleted\n",
+			cleanup:        true,
 		},
 		{
 			name:           "Create test namespace",
@@ -91,6 +145,8 @@ func TestTraceloop(t *testing.T) {
 		os.Setenv("GADGET_IMAGE_FLAG", "--image "+*image)
 	}
 
+	backgroundCommands := map[string]*backgroundCommand{}
+
 	failed := false
 	for _, tt := range commands {
 		t.Run(tt.name, func(t *testing.T) {
@@ -98,14 +154,38 @@ func TestTraceloop(t *testing.T) {
 				t.Skip("Previous command failed.")
 			}
 
-			t.Logf("Command: %s\n", tt.cmd)
-			cmd := exec.Command("/bin/sh", "-c", tt.cmd)
-			output, err := cmd.CombinedOutput()
-			actual := string(output)
-			t.Logf("Command returned:\n%s\n", actual)
-			if err != nil {
-				failed = true
-				t.Fatal(err)
+			t.Logf("Command %s: %s\n", tt.id, tt.cmd)
+			var cmd *exec.Cmd
+			var actual string
+			if tt.cmd != "" {
+				cmd = exec.Command("/bin/sh", "-c", tt.cmd)
+			} else {
+				bg, ok := backgroundCommands[tt.id]
+				if !ok {
+					failed = true
+					t.Fatalf("cannot find command: %s\n", tt.id)
+				}
+				cmd = bg.cmd
+				actual = bg.stderr.String() + bg.stdout.String()
+				t.Logf("Background command returned:\n%s\n", actual)
+			}
+			if tt.background {
+				bg := backgroundCommand{
+					cmd: cmd,
+				}
+				backgroundCommands[tt.id] = &bg
+				cmd.Stdout = &bg.stdout
+				cmd.Stderr = &bg.stderr
+				t.Logf("Start command\n")
+				cmd.Start()
+			} else if tt.cmd != "" {
+				output, err := cmd.CombinedOutput()
+				if err != nil {
+					failed = true
+					t.Fatal(err)
+				}
+				actual = string(output)
+				t.Logf("Command returned:\n%s\n", actual)
 			}
 
 			if tt.expectedRegexp != "" {
@@ -120,5 +200,18 @@ func TestTraceloop(t *testing.T) {
 				t.Fatalf("diff: %v", pretty.Diff(tt.expectedString, actual))
 			}
 		})
+	}
+	for id, bg := range backgroundCommands {
+		err := bg.cmd.Process.Kill()
+		if err != nil {
+			t.Fatalf("cannot kill %s: %s", id, err)
+		}
+		err = bg.cmd.Wait()
+		if err != nil {
+			_, ok := err.(*exec.ExitError)
+			if !ok {
+				t.Fatalf("cannot terminate %s: %s", id, err)
+			}
+		}
 	}
 }
