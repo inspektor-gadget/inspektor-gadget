3149914b63d9370effbd35f7e935e14ecec5657c
Alban Crequy <albancrequy@microsoft.com>
2021-11-03 19:29:23 +0100

    New gadget: audit-seccomp

---
+++ pkg/gadgets/audit-seccomp/tracer/bpf/audit-seccomp.h
@@ -0,0 +1,20 @@
+#ifndef GADGET_AUDIT_SECCOMP_H
+#define GADGET_AUDIT_SECCOMP_H
+
+#include "../../../../vmlinux/vmlinux-cgo.h"
+
+#define TASK_COMM_LEN 16
+
+#include "../../../../gadgettracermanager/common.h"
+
+struct event {
+	u64 pid;
+	u64 mntns_id;
+	u64 syscall;
+	u64 code;
+	char comm[TASK_COMM_LEN];
+
+	struct container container;
+};
+
+#endif
---
+++ pkg/gadgets/audit-seccomp/types/types.go
@@ -0,0 +1,35 @@
+// Copyright 2019-2021 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	eventtypes "github.com/kinvolk/inspektor-gadget/pkg/types"
+)
+
+type Event struct {
+	eventtypes.Event
+
+	Syscall   string `json:"syscall,omitempty"`
+	Code      string `json:"code,omitempty"`
+	Pid       uint32 `json:"pid,omitempty"`
+	MountNsID uint64 `json:"mountnsid,omitempty"`
+	Comm      string `json:"pcomm,omitempty"`
+}
+
+func Base(ev eventtypes.Event) Event {
+	return Event{
+		Event: ev,
+	}
+}
