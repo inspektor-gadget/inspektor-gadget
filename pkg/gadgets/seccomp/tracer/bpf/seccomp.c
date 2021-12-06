600c26aeb7f5136c716b916d81e9d72e65268598
Jose Blanquicet <josebl@microsoft.com>
2021-12-06 10:49:04 -0500

    seccomp: Record runc syscalls only after the profile is installed
    The policies generated by the seccomp-advisor gadget are not enough
    to get the pod to run successfully and, as a workaround, it is necessary
    to set `allowPrivilegeEscalation` to `false` to make them work. However,
    it does not always work.
    
    This PR fixes a bug on the recording of the syscalls performed by
    `runc`. It was previously relying on the fact that the `seccomp()` will
    be called but it is not always true. Actually, it is false in most
    cases.
    
    Consider we want to record only the syscalls executed by `runc` after
    the seccomp profile is actually installed. However, if we are running
    the seccomp-advisor gadget, it's very probably that the pod does not
    have a seccomp profile yet, so `seccomp()` will not be called.
    Therefore, we decide to start recording from the
    `prctl(PR_GET_PDEATHSIG)` call on. It is a safe place right before all
    the `seccomp()` calls that will be always executed during the `runc`
    initialisation.

--- pkg/gadgets/seccomp/tracer/bpf/seccomp.c
+++ pkg/gadgets/seccomp/tracer/bpf/seccomp.c
@@ -9,21 +9,46 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_tracing.h>
 
 #include "seccomp-common.h"
 
 #define TASK_COMM_LEN 16
 #define TS_COMPAT 0x0002
 
+// prctl syscall number from
+// https://github.com/seccomp/libseccomp/blob/abad8a8f41fc13efbb95fc1ccaa3e181342bade7/src/syscalls.csv#L265
+#ifndef __NR_prctl
+# if defined(bpf_target_x86)
+#  define __NR_prctl 157
+# elif defined(bpf_target_arm64)
+#  define __NR_prctl 167
+# else
+#  error "Unsupported architecture"
+# endif
+#endif
+
+// prclt syscall parameters from
+// https://github.com/torvalds/linux/blob/5147da902e0dd162c6254a61e4c57f21b60a9b1c/include/uapi/linux/prctl.h#L10
+// https://github.com/torvalds/linux/blob/5147da902e0dd162c6254a61e4c57f21b60a9b1c/include/uapi/linux/prctl.h#L175
+#ifndef PR_GET_PDEATHSIG
+# define PR_GET_PDEATHSIG 2
+#endif
+#ifndef PR_SET_NO_NEW_PRIVS
+# define PR_SET_NO_NEW_PRIVS 38
+#endif
+
 // Seccomp syscall number from
 // https://github.com/torvalds/linux/blob/v5.12/tools/testing/selftests/seccomp/seccomp_bpf.c#L115
 // Only x86_64 is supported for now.
 #ifndef __NR_seccomp
-#define __NR_seccomp 317
-#endif
-
-#ifndef SECCOMP_SET_MODE_FILTER
-#define SECCOMP_SET_MODE_FILTER         1
+# if defined(bpf_target_x86)
+#  define __NR_seccomp 317
+# elif defined(bpf_target_arm64)
+#  define __NR_seccomp 277
+# else
+#  error "Unsupported architecture"
+# endif
 #endif
 
 struct {
@@ -77,21 +102,34 @@ int tracepoint__raw_syscalls__sys_enter(struct bpf_raw_tracepoint_args *ctx)
 			return 0;
 	}
 
+	// If it is runc, we want to record only the syscalls executed after the
+	// seccomp profile is actually installed. However, if we are running the
+	// seccomp-advisor gadget, it is very probably that the pod does not have
+	// a seccomp profile yet, so seccomp() will not be called. Therefore, we
+	// decide to start recording from the prctl(PR_GET_PDEATHSIG) call on. It
+	// is a safe place right before all the seccomp() calls that will be always
+	// executed during the runc initialisation:
+	// https://github.com/opencontainers/runc/blob/8b4a8f093d0dbdf45100597f710d16777845ee83/libcontainer/standard_init_linux.go#L148
 	if (is_runc) {
-		/* libseccomp makes invalid calls to seccomp() to determine the api
-		 * level. Ignore those. */
-		if (id == __NR_seccomp &&
-		PT_REGS_PARM1(&regs) == SECCOMP_SET_MODE_FILTER &&
-		PT_REGS_PARM3(&regs) != 0) {
-			/* Mark this container: seccomp has been called. */
-			syscall_bitmap[SYSCALLS_COUNT] = 1;
+		if (syscall_bitmap[SYSCALLS_COUNT] == 0) {
+			if (id == __NR_prctl && PT_REGS_PARM1(&regs) == PR_GET_PDEATHSIG) {
+				// Start recording the runc syscalls from now on.
+				syscall_bitmap[SYSCALLS_COUNT] = 1;
+			}
+
 			return 0;
 		}
-		/* Don't register syscalls performed by runc before the seccomp policy is actually installed */
-		if (syscall_bitmap[SYSCALLS_COUNT] == 0)
+
+		// Record all the runc syscalls after prctl(PR_GET_PDEATHSIG) except
+		// for seccomp() and prctl(PR_SET_NO_NEW_PRIVS) because we know they
+		// are executed before the seccomp profile is installed.
+		if ((id == __NR_prctl && PT_REGS_PARM1(&regs) == PR_SET_NO_NEW_PRIVS) ||
+				(id == __NR_seccomp)) {
 			return 0;
+		}
 	}
 
+	// Record the syscall
 	syscall_bitmap[id] = 0x01;
 
 	return 0;
