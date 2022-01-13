inspektor-gadget/inspektor-gadget

commit 9943c314cae4734369b4e090253eda45a9064173

kubectl-gadget: Use a RBAC policy to limit gadget service rights on cluster.
Before this commit, gadget service was cluster admin.
This commit adds several rules to limit the rights of gadget service on the
cluster to only what it needs.
With this commit, it can now:
* get, watch and list namespaces, nodes and pods (needed by several gadgets),
* update pods in gadget namespace (needed by traceloop).
* list services (needed by network-policy gadget)
* do everything on traces as we define this resource.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

cmd/kubectl-gadget/deploy.go
@@ -88,17 +88,58 @@ metadata:
   name: gadget
   namespace: gadget
 ---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  namespace: gadget
+  name: gadget-role
+rules:
+- apiGroups: [""]
+  resources: ["pods"]
+  # update is needed by traceloop gadget.
+  verbs: ["update"]
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: gadget-role-binding
+  namespace: gadget
+subjects:
+- kind: ServiceAccount
+  name: gadget
+roleRef:
+  kind: Role
+  name: gadget-role
+  apiGroup: rbac.authorization.k8s.io
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: gadget-cluster-role
+rules:
+- apiGroups: [""]
+  resources: ["namespaces", "nodes", "pods"]
+  verbs: ["get", "watch", "list"]
+- apiGroups: [""]
+  resources: ["services"]
+  # list services is needed by network-policy gadget.
+  verbs: ["list"]
+- apiGroups: ["gadget.kinvolk.io"]
+  resources: ["traces", "traces/status"]
+  # For traces, we need all rights on them as we define this resource.
+  verbs: ["delete", "deletecollection", "get", "list", "patch", "create", "update", "watch"]
+---
 kind: ClusterRoleBinding
 apiVersion: rbac.authorization.k8s.io/v1
 metadata:
-  name: gadget
+  name: gadget-cluster-role-binding
 subjects:
 - kind: ServiceAccount
   name: gadget
   namespace: gadget
 roleRef:
   kind: ClusterRole
-  name: cluster-admin
+  name: gadget-cluster-role
   apiGroup: rbac.authorization.k8s.io
 ---
 apiVersion: apps/v1

cmd/kubectl-gadget/undeploy.go
@@ -123,7 +123,7 @@ again:
 
 	// 3. gadget cluster role binding
 	err = k8sClient.RbacV1().ClusterRoleBindings().Delete(
-		context.TODO(), "gadget", metav1.DeleteOptions{},
+		context.TODO(), "gadget-cluster-role-binding", metav1.DeleteOptions{},
 	)
 	if err != nil {
 		errs = append(
@@ -131,8 +131,18 @@ again:
 		)
 	}
 
-	// 4. gadget namespace (it also removes daemonset as well as serviceaccount
-	// since they live in this namespace).
+	// 4. gadget cluster role
+	err = k8sClient.RbacV1().ClusterRoles().Delete(
+		context.TODO(), "gadget-cluster-role", metav1.DeleteOptions{},
+	)
+	if err != nil {
+		errs = append(
+			errs, fmt.Sprintf("failed to remove \"gadget\" cluster role: %s", err),
+		)
+	}
+
+	// 5. gadget namespace (it also removes daemonset, serviceaccount, rolebinding
+	// and role since they live in this namespace).
 	err = k8sClient.CoreV1().Namespaces().Delete(
 		context.TODO(), "gadget", metav1.DeleteOptions{},
 	)

commit 9cc746b1c417f2e3ef529adeeadc1b84a4951778

kubectl-gadget/deploy: Add cluster role rule needed by seccomp gadget
Fixes: 9943c314cae4 ("kubectl-gadget: Use a RBAC policy to limit gadget service rights on cluster.")

cmd/kubectl-gadget/deploy.go
@@ -128,6 +128,10 @@ rules:
   resources: ["traces", "traces/status"]
   # For traces, we need all rights on them as we define this resource.
   verbs: ["delete", "deletecollection", "get", "list", "patch", "create", "update", "watch"]
+- apiGroups: ["*"]
+  resources: ["deployments", "replicasets", "statefulsets", "daemonsets", "jobs", "cronjobs", "replicationcontrollers"]
+  # Required to retrieve the owner references used by the seccomp gadget.
+  verbs: ["get"]
 ---
 kind: ClusterRoleBinding
 apiVersion: rbac.authorization.k8s.io/v1

pkg/container-collection/options.go
@@ -262,6 +262,8 @@ func WithPubSub(funcs ...pubsub.FuncNotify) ContainerCollectionOption {
 // resource kinds. Otherwise, it returns nil.
 func getExpectedOwnerReference(ownerReferences []metav1.OwnerReference) *metav1.OwnerReference {
 	// From: https://kubernetes.io/docs/concepts/workloads/controllers/
+	// Notice that any change on this map needs to be aligned with the gadget
+	// cluster role.
 	expectedResKinds := map[string]struct{}{
 		"Deployment":            {},
 		"ReplicaSet":            {},
@@ -353,7 +355,9 @@ func ownerReferenceEnrichment(
 
 	var highestOwnerRef *metav1.OwnerReference
 
-	// Iterate until we reach the highest level of reference
+	// Iterate until we reach the highest level of reference with one of the
+	// expected resource kind. Take into account that if this logic is changed,
+	// the gadget cluster role needs to be updated accordingly.
 	for {
 		if len(ownerReferences) == 0 {
 			ownerReferences, err = getOwnerReferences(dynamicClient,