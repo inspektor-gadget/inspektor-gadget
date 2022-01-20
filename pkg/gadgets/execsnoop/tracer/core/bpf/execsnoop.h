58e1ed95f92c5b21022c1c2ae20e4de37d46b5c0
Mauricio Vásquez <mauricio@kinvolk.io>
2022-01-20 16:33:47 -0500

    pkg/gadget: Add bpf program for execsnoop

---
+++ pkg/gadgets/execsnoop/tracer/core/bpf/execsnoop.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#ifndef __EXECSNOOP_H
+#define __EXECSNOOP_H
+
+#define ARGSIZE  128
+#define TASK_COMM_LEN 16
+#define TOTAL_MAX_ARGS 60
+#define DEFAULT_MAXARGS 20
+#define FULL_MAX_ARGS_ARR (TOTAL_MAX_ARGS * ARGSIZE)
+#define INVALID_UID ((uid_t)-1)
+#define BASE_EVENT_SIZE (size_t)(&((struct event*)0)->args)
+#define EVENT_SIZE(e) (BASE_EVENT_SIZE + e->args_size)
+#define LAST_ARG (FULL_MAX_ARGS_ARR - ARGSIZE)
+
+struct event {
+	__u32 pid;
+	__u32 ppid;
+	__u32 uid;
+	__u64 mntns_id;
+	int retval;
+	int args_count;
+	unsigned int args_size;
+	char comm[TASK_COMM_LEN];
+	char args[FULL_MAX_ARGS_ARR];
+};
+
+#endif /* __EXECSNOOP_H */
