inspektor-gadget/inspektor-gadget

commit 24fcb0326536338924098a547fc4fe4977e6762a

utils: trace: Use new API to Watch trace in waitForTraceState().
Instead of sleeping on traces, we now wait for Added or Modified event while
checking if the traces are in the expected state.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

cmd/kubectl-gadget/utils/trace.go
@@ -35,6 +35,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
+	watchtools "k8s.io/client-go/tools/watch"
 
 	gadgetv1alpha1 "github.com/kinvolk/inspektor-gadget/pkg/apis/gadget/v1alpha1"
 	clientset "github.com/kinvolk/inspektor-gadget/pkg/client/clientset/versioned"
@@ -423,59 +424,119 @@ Loop:
 	return retEvent, nil
 }
 
-// waitForTraceState loops over all trace whom ID is given as parameter
-// waiting until they are in the expected state.
-func waitForTraceState(traceID string, expectedState string) (*gadgetv1alpha1.TraceList, error) {
-	start := time.Now()
-
-RetryLoop:
-	for {
-		successNodeCount := 0
-		timeout := time.Since(start) > traceTimeout
-		nodeErrors := make(map[string]string)
-		nodeWarnings := make(map[string]string)
-
-		traces, err := getTraceListFromID(traceID)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, i := range traces.Items {
-			if i.Status.OperationError != "" {
-				nodeErrors[i.Spec.Node] = i.Status.OperationError
-			} else if i.Status.OperationWarning != "" {
-				nodeWarnings[i.Spec.Node] = i.Status.OperationWarning
-				// TODO(francis) This code will not work if the trace is already in the
-				// expected state, for example if we decide to generate it twice.
-				// We need to add a cookie (and check it) to be sure the trace is ready.
-			} else if i.Status.State == expectedState {
-				successNodeCount++
-			} else {
-				// Consider Trace as timed out if it neither moved the state forward
-				// nor notified of an error or warning within the time window.
-				if timeout {
-					nodeErrors[i.Spec.Node] = fmt.Sprintf("No results received from trace within %v", traceTimeout)
-					continue
-				}
-
-				time.Sleep(100 * time.Millisecond)
-				continue RetryLoop
-			}
-		}
-
-		// Print errors even if some nodes succeeded.
-		defer printTraceFeedback(nodeErrors, len(traces.Items))
-
-		// Don't print warnings if at least one node succeeded. This avoids showing
-		// warnings together with the actual output generated by other nodes.
-		if successNodeCount == 0 {
-			printTraceFeedback(nodeWarnings, len(traces.Items))
-
-			return nil, errors.New("Failed to run the gadget on all nodes: None of them succeeded")
-		}
-
-		return traces, nil
+// getTraceWatcher returns a watcher on trace(s) whom ID was given as parameter.
+// This watcher can then be used to wait on State.Output modification.
+func getTraceWatcher(traceID string) (watch.Interface, error) {
+	traceClient, err := getTraceClient()
+	if err != nil {
+		return nil, err
 	}
+
+	var watchOptions = metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=%s", GLOBAL_TRACE_ID, traceID),
+	}
+
+	watcher, err := traceClient.GadgetV1alpha1().Traces("gadget").Watch(context.TODO(), watchOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return watcher, nil
+}
+
+// waitForTraceState wait for traces whom ID is given as parameter to be in the
+// expected state.
+func waitForTraceState(traceID string, expectedState string) (*gadgetv1alpha1.TraceList, error) {
+	var returnedTraces gadgetv1alpha1.TraceList
+
+	tracesNumber := 0
+	watchedTracesNumber := 0
+
+	watcher, err := getTraceWatcher(traceID)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeErrors := make(map[string]string)
+	nodeWarnings := make(map[string]string)
+
+	ctx, cancel := watchtools.ContextWithOptionalTimeout(context.Background(), traceTimeout)
+	_, err = untilWithoutRetry(ctx, watcher, func(event watch.Event) (bool, error) {
+		// This function will be executed until:
+		// 1. The number of watched traces equals the number of traces, i.e. we
+		// dealt with the traces which interest us.
+		// 2. Or it returns an error.
+		// 3. Or time out is fired.
+		// NOTE In case 2 and 3, it exists, at least, one trace we did not deal
+		// with.
+
+		// Deal particularly with error.
+		if event.Type == watch.Error {
+			return false, err
+		}
+
+		// We are only interested in Added and Modified event, as we want
+		// Status.State value to change.
+		// More particularly, we monitor Added event for gadget like dns and
+		// Modified for gadget like seccompadvisor.
+		if event.Type != watch.Added && event.Type != watch.Modified {
+			return false, nil
+		}
+
+		if event.Type == watch.Added {
+			// The API adds fake watch.Added events, so we use them to count the
+			// number of trace:
+			// To establish initial state, the watch begins with synthetic "Added"
+			// events of all resources instances that exist at the starting resource
+			// version
+			tracesNumber++
+		}
+
+		trace := event.Object.(*gadgetv1alpha1.Trace)
+
+		if trace.Status.OperationWarning != "" {
+			watchedTracesNumber++
+
+			nodeWarnings[trace.Spec.Node] = trace.Status.OperationWarning
+
+			return false, nil
+		}
+
+		if trace.Status.OperationError != "" {
+			watchedTracesNumber++
+
+			nodeErrors[trace.Spec.Node] = trace.Status.OperationError
+
+			return false, nil
+		}
+
+		// If the trace is not in the state we expect, we are not interested.
+		if trace.Status.State != expectedState {
+			return false, nil
+		}
+
+		watchedTracesNumber++
+		// If the current trace matches our filter we add it to the list of trace
+		// we will return.
+		returnedTraces.Items = append(returnedTraces.Items, *trace)
+
+		return watchedTracesNumber == tracesNumber, nil
+	})
+	cancel()
+
+	// We print errors whatever happened.
+	defer printTraceFeedback(nodeErrors, tracesNumber)
+
+	// We print warnings only if all trace failed.
+	if len(returnedTraces.Items) == 0 {
+		printTraceFeedback(nodeWarnings, tracesNumber)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &returnedTraces, nil
 }
 
 var sigIntReceivedNumber = 0

commit 45728e843292d5f9f4a2bd00668afbe3e6abc4ee

cmd/kubectl-gadget: Fix handling of trace operation warning
An operation warning doesn't mean an error, hence the logic should
just store the warning and continue ahead instead of returning.

Fixes: 24fcb0326536 ("utils: trace: Use new API to Watch trace in waitForTraceState().")

cmd/kubectl-gadget/utils/trace.go
@@ -503,11 +503,7 @@ func waitForTraceState(traceID string, expectedState string) (*gadgetv1alpha1.Tr
 		trace := event.Object.(*gadgetv1alpha1.Trace)
 
 		if trace.Status.OperationWarning != "" {
-			watchedTracesNumber++
-
 			nodeWarnings[trace.Spec.Node] = trace.Status.OperationWarning
-
-			return false, nil
 		}
 
 		if trace.Status.OperationError != "" {