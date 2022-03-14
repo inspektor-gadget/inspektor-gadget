inspektor-gadget/inspektor-gadget

commit f3f588f6859e105cdcafae67696c5930291ea082

pkg/gadgets: Increase traceloop opened file limit.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/traceloop/gadget.go
@@ -121,6 +121,13 @@ func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
 		echo "-:pfree_uts_ns" >> /sys/kernel/debug/tracing/kprobe_events 2>/dev/null || true
 		echo "-:pcap_capable" >> /sys/kernel/debug/tracing/kprobe_events 2>/dev/null || true
 
+		# Remove the opened files limit to avoid getting this error:
+		# error while loading "tracepoint/raw_syscalls/sys_enter" (too many open files):
+		# Indeed, traceloop creates a lof of tracers which each has maps and events:
+		# https://github.com/kinvolk/traceloop/blob/6f4efc6fca46d92c75f4ec4e6c6e1d829bdeaddf/bpf/straceback-guess-bpf.h#L27-L28
+		# So, this can generate a lof ot opened files.
+		ulimit -n unlimited
+
 		rm -f /run/traceloop.socket
 		exec /bin/traceloop k8s
 	`)

cmd/kubectl-gadget/deploy.go
@@ -291,7 +291,8 @@ spec:
               # https://elixir.bootlin.com/linux/v5.14.14/source/kernel/ptrace.c#L284
               - SYS_PTRACE
 
-              # Needed by setrlimit in gadgettracermanager.
+              # Needed by setrlimit in gadgettracermanager and by the traceloop
+              # gadget.
               - SYS_RESOURCE
 
               # Needed for gadgets that don't dumb the memory rlimit.

commit f77b6b08f9858b63ecddc7dd6ee666423cfa9f7d

pkg/gadgets: Correct traceloop opened file limit increase.
We cannot use unlimited, so we stick to hard which should offer sufficient
opened file limit for traceloop to work.

Fixes: f3f588f6859 ("pkg/gadgets: Increase traceloop opened file limit.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/traceloop/gadget.go
@@ -110,7 +110,7 @@ func (f *TraceFactory) Operations() map[string]gadgets.TraceOperation {
 }
 
 func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
-	t.cmd = exec.Command("/bin/sh", "-c", `
+	t.cmd = exec.Command("/bin/bash", "-c", `
 		# gobpf currently uses global kprobes via debugfs/tracefs and not the Perf
 		# Event file descriptor based kprobe (Linux >=4.17). So unfortunately, kprobes
 		# can remain from previous executions. Ideally, gobpf should implement Perf
@@ -126,7 +126,7 @@ func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
 		# Indeed, traceloop creates a lof of tracers which each has maps and events:
 		# https://github.com/kinvolk/traceloop/blob/6f4efc6fca46d92c75f4ec4e6c6e1d829bdeaddf/bpf/straceback-guess-bpf.h#L27-L28
 		# So, this can generate a lof ot opened files.
-		ulimit -n unlimited
+		ulimit -n hard
 
 		rm -f /run/traceloop.socket
 		exec /bin/traceloop k8s