inspektor-gadget/inspektor-gadget

commit d05d1d78597aee1d84186f096f25db3929382ea5

pkg/gadgets/filetop: Add tracer

pkg/gadgets/filetop/gadget.go
@@ -1,0 +1,220 @@
+// Copyright 2019-2021 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filetop
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets"
+	filetoptracer "github.com/kinvolk/inspektor-gadget/pkg/gadgets/filetop/tracer"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/filetop/types"
+
+	gadgetv1alpha1 "github.com/kinvolk/inspektor-gadget/pkg/apis/gadget/v1alpha1"
+)
+
+const (
+	MAX_ROWS_DEFAULT  = 20
+	INTERVAL_DEFAULT  = 1
+	SORT_BY_DEFAULT   = types.RBYTES
+	ALL_FILES_DEFAULT = false
+)
+
+type Trace struct {
+	resolver gadgets.Resolver
+
+	started bool
+	tracer  *filetoptracer.Tracer
+}
+
+type TraceFactory struct {
+	gadgets.BaseFactory
+}
+
+func NewFactory() gadgets.TraceFactory {
+	return &TraceFactory{
+		BaseFactory: gadgets.BaseFactory{DeleteTrace: deleteTrace},
+	}
+}
+
+func (f *TraceFactory) Description() string {
+	t := `filetop shows reads and writes by file, with container details.
+
+The following parameters are supported:
+ - interval: Output interval, in seconds. (default %d)
+ - max_rows: Maximum rows to print. (default %d)
+ - sort: The field to sort the results by (%s). (default %s)`
+	return fmt.Sprintf(t, INTERVAL_DEFAULT, MAX_ROWS_DEFAULT,
+		strings.Join(types.SortBySlice, ","), SORT_BY_DEFAULT)
+}
+
+func (f *TraceFactory) OutputModesSupported() map[string]struct{} {
+	return map[string]struct{}{
+		"Stream": {},
+	}
+}
+
+func deleteTrace(name string, t interface{}) {
+	trace := t.(*Trace)
+	if trace.tracer != nil {
+		trace.tracer.Stop()
+	}
+}
+
+func (f *TraceFactory) Operations() map[string]gadgets.TraceOperation {
+	n := func() interface{} {
+		return &Trace{
+			resolver: f.Resolver,
+		}
+	}
+
+	return map[string]gadgets.TraceOperation{
+		"start": {
+			Doc: "Start filetop gadget",
+			Operation: func(name string, trace *gadgetv1alpha1.Trace) {
+				f.LookupOrCreate(name, n).(*Trace).Start(trace)
+			},
+		},
+		"stop": {
+			Doc: "Stop filetop gadget",
+			Operation: func(name string, trace *gadgetv1alpha1.Trace) {
+				f.LookupOrCreate(name, n).(*Trace).Stop(trace)
+			},
+		},
+	}
+}
+
+func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
+	if t.started {
+		gadgets.CleanupTraceStatus(trace)
+		trace.Status.State = "Started"
+		return
+	}
+
+	traceName := gadgets.TraceName(trace.ObjectMeta.Namespace, trace.ObjectMeta.Name)
+
+	maxRows := MAX_ROWS_DEFAULT
+	intervalSeconds := INTERVAL_DEFAULT
+	sortBy := SORT_BY_DEFAULT
+	allFiles := ALL_FILES_DEFAULT
+
+	if trace.Spec.Parameters != nil {
+		params := trace.Spec.Parameters
+		var err error
+
+		if val, ok := params["max_rows"]; ok {
+			maxRows, err = strconv.Atoi(val)
+			if err != nil {
+				gadgets.CleanupTraceStatus(trace)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for ouput_rows", val)
+				return
+			}
+		}
+
+		if val, ok := params["interval"]; ok {
+			intervalSeconds, err = strconv.Atoi(val)
+			if err != nil {
+				gadgets.CleanupTraceStatus(trace)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for interval", val)
+				return
+			}
+		}
+
+		if val, ok := params["sortby"]; ok {
+			sortBy, err = types.ParseSortBy(val)
+			if err != nil {
+				gadgets.CleanupTraceStatus(trace)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for sortby", val)
+				return
+			}
+		}
+
+		if val, ok := params["all_files"]; ok {
+			allFiles, err = strconv.ParseBool(val)
+			if err != nil {
+				gadgets.CleanupTraceStatus(trace)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for all_files", val)
+				return
+			}
+		}
+	}
+
+	config := &filetoptracer.Config{
+		AllFiles:   allFiles,
+		MaxRows:    maxRows,
+		Interval:   time.Second * time.Duration(intervalSeconds),
+		SortBy:     sortBy,
+		MountnsMap: gadgets.TracePinPath(trace.ObjectMeta.Namespace, trace.ObjectMeta.Name),
+		Node:       trace.Spec.Node,
+	}
+
+	statsCallback := func(stats []types.Stats) {
+		ev := types.Event{
+			Node:  trace.Spec.Node,
+			Stats: stats,
+		}
+
+		r, err := json.Marshal(ev)
+		if err != nil {
+			log.Warnf("Gadget %s: Failed to marshall event: %s", trace.Spec.Gadget, err)
+			return
+		}
+		t.resolver.PublishEvent(traceName, string(r))
+	}
+
+	errorCallback := func(err error) {
+		ev := types.Event{
+			Error: fmt.Sprintf("Gadget failed with: %v", err),
+			Node:  trace.Spec.Node,
+		}
+		r, err := json.Marshal(&ev)
+		if err != nil {
+			log.Warnf("Gadget %s: Failed to marshall event: %s", trace.Spec.Gadget, err)
+			return
+		}
+		t.resolver.PublishEvent(traceName, string(r))
+	}
+
+	tracer, err := filetoptracer.NewTracer(config, t.resolver, statsCallback, errorCallback)
+	if err != nil {
+		trace.Status.OperationError = fmt.Sprintf("failed to create tracer: %s", err)
+		return
+	}
+
+	t.tracer = tracer
+	t.started = true
+
+	gadgets.CleanupTraceStatus(trace)
+	trace.Status.State = "Started"
+}
+
+func (t *Trace) Stop(trace *gadgetv1alpha1.Trace) {
+	if !t.started {
+		trace.Status.OperationError = "Not started"
+		return
+	}
+
+	t.tracer.Stop()
+	t.tracer = nil
+	t.started = false
+
+	gadgets.CleanupTraceStatus(trace)
+	trace.Status.State = "Stopped"
+}

pkg/gadget-collection/gadget-collection.go
@@ -18,6 +18,7 @@ import (
 	"github.com/kinvolk/inspektor-gadget/pkg/gadgets"
 	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/biolatency"
 	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/dns"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/filetop"
 	networkpolicyadvisor "github.com/kinvolk/inspektor-gadget/pkg/gadgets/networkpolicy"
 	processcollector "github.com/kinvolk/inspektor-gadget/pkg/gadgets/process-collector"
 	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/seccomp"
@@ -29,6 +30,7 @@ func TraceFactories() map[string]gadgets.TraceFactory {
 	return map[string]gadgets.TraceFactory{
 		"biolatency":             biolatency.NewFactory(),
 		"dns":                    dns.NewFactory(),
+		"filetop":                filetop.NewFactory(),
 		"network-policy-advisor": networkpolicyadvisor.NewFactory(),
 		"process-collector":      processcollector.NewFactory(),
 		"seccomp":                seccomp.NewFactory(),

pkg/gadgets/filetop/tracer/filetop_bpfel.go
@@ -1,0 +1,124 @@
+// Code generated by bpf2go; DO NOT EDIT.
+// +build 386 amd64 amd64p32 arm arm64 mips64le mips64p32le mipsle ppc64le riscv64
+
+package tracer
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+
+	"github.com/cilium/ebpf"
+)
+
+// loadFiletop returns the embedded CollectionSpec for filetop.
+func loadFiletop() (*ebpf.CollectionSpec, error) {
+	reader := bytes.NewReader(_FiletopBytes)
+	spec, err := ebpf.LoadCollectionSpecFromReader(reader)
+	if err != nil {
+		return nil, fmt.Errorf("can't load filetop: %w", err)
+	}
+
+	return spec, err
+}
+
+// loadFiletopObjects loads filetop and converts it into a struct.
+//
+// The following types are suitable as obj argument:
+//
+//     *filetopObjects
+//     *filetopPrograms
+//     *filetopMaps
+//
+// See ebpf.CollectionSpec.LoadAndAssign documentation for details.
+func loadFiletopObjects(obj interface{}, opts *ebpf.CollectionOptions) error {
+	spec, err := loadFiletop()
+	if err != nil {
+		return err
+	}
+
+	return spec.LoadAndAssign(obj, opts)
+}
+
+// filetopSpecs contains maps and programs before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type filetopSpecs struct {
+	filetopProgramSpecs
+	filetopMapSpecs
+}
+
+// filetopSpecs contains programs before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type filetopProgramSpecs struct {
+	VfsReadEntry  *ebpf.ProgramSpec `ebpf:"vfs_read_entry"`
+	VfsWriteEntry *ebpf.ProgramSpec `ebpf:"vfs_write_entry"`
+}
+
+// filetopMapSpecs contains maps before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type filetopMapSpecs struct {
+	Entries    *ebpf.MapSpec `ebpf:"entries"`
+	MountNsSet *ebpf.MapSpec `ebpf:"mount_ns_set"`
+}
+
+// filetopObjects contains all objects after they have been loaded into the kernel.
+//
+// It can be passed to loadFiletopObjects or ebpf.CollectionSpec.LoadAndAssign.
+type filetopObjects struct {
+	filetopPrograms
+	filetopMaps
+}
+
+func (o *filetopObjects) Close() error {
+	return _FiletopClose(
+		&o.filetopPrograms,
+		&o.filetopMaps,
+	)
+}
+
+// filetopMaps contains all maps after they have been loaded into the kernel.
+//
+// It can be passed to loadFiletopObjects or ebpf.CollectionSpec.LoadAndAssign.
+type filetopMaps struct {
+	Entries    *ebpf.Map `ebpf:"entries"`
+	MountNsSet *ebpf.Map `ebpf:"mount_ns_set"`
+}
+
+func (m *filetopMaps) Close() error {
+	return _FiletopClose(
+		m.Entries,
+		m.MountNsSet,
+	)
+}
+
+// filetopPrograms contains all programs after they have been loaded into the kernel.
+//
+// It can be passed to loadFiletopObjects or ebpf.CollectionSpec.LoadAndAssign.
+type filetopPrograms struct {
+	VfsReadEntry  *ebpf.Program `ebpf:"vfs_read_entry"`
+	VfsWriteEntry *ebpf.Program `ebpf:"vfs_write_entry"`
+}
+
+func (p *filetopPrograms) Close() error {
+	return _FiletopClose(
+		p.VfsReadEntry,
+		p.VfsWriteEntry,
+	)
+}
+
+func _FiletopClose(closers ...io.Closer) error {
+	for _, closer := range closers {
+		if err := closer.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Do not access this directly.
+//go:embed filetop_bpfel.o
+var _FiletopBytes []byte

pkg/gadgets/filetop/tracer/tracer.go
@@ -1,0 +1,250 @@
+//go:build linux
+// +build linux
+
+// Copyright 2019-2021 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tracer
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+	"time"
+	"unsafe"
+
+	containercollection "github.com/kinvolk/inspektor-gadget/pkg/container-collection"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/filetop/types"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+)
+
+// #include <linux/types.h>
+// #include "./bpf/filetop.h"
+import "C"
+
+//go:generate sh -c "GOOS=$(go env GOHOSTOS) GOARCH=$(go env GOHOSTARCH) go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc clang filetop ./bpf/filetop.bpf.c -- -I./bpf/ -I../../.. -target bpf -D__TARGET_ARCH_x86"
+
+type Config struct {
+	TargetPid int
+	AllFiles  bool
+	MaxRows   int
+	Interval  time.Duration
+	SortBy    types.SortBy
+	// TODO: Make it a *ebpf.Map once
+	// https://github.com/cilium/ebpf/issues/515 and
+	// https://github.com/cilium/ebpf/issues/517 are fixed
+	MountnsMap string
+	Node       string
+}
+
+type Tracer struct {
+	config        *Config
+	objs          filetopObjects
+	readLink      link.Link
+	writeLink     link.Link
+	resolver      containercollection.ContainerResolver
+	statsCallback func([]types.Stats)
+	errorCallback func(error)
+	done          chan bool
+}
+
+func NewTracer(config *Config, resolver containercollection.ContainerResolver,
+	statsCallback func([]types.Stats), errorCallback func(error)) (*Tracer, error) {
+	t := &Tracer{
+		config:        config,
+		resolver:      resolver,
+		statsCallback: statsCallback,
+		errorCallback: errorCallback,
+		done:          make(chan bool),
+	}
+
+	if err := t.start(); err != nil {
+		t.Stop()
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (t *Tracer) Stop() {
+	close(t.done)
+
+	if t.readLink != nil {
+		t.readLink.Close()
+		t.readLink = nil
+	}
+	if t.writeLink != nil {
+		t.writeLink.Close()
+		t.writeLink = nil
+	}
+
+	t.objs.Close()
+}
+
+func (t *Tracer) start() error {
+	spec, err := loadFiletop()
+	if err != nil {
+		return fmt.Errorf("Failed to load ebpf program: %w", err)
+	}
+
+	filter_by_mnt_ns := false
+
+	if t.config.MountnsMap != "" {
+		filter_by_mnt_ns = true
+		m := spec.Maps["mount_ns_set"]
+		m.Pinning = ebpf.PinByName
+		m.Name = filepath.Base(t.config.MountnsMap)
+	}
+
+	consts := map[string]interface{}{
+		"target_pid":        uint32(t.config.TargetPid),
+		"regular_file_only": !t.config.AllFiles,
+		"filter_by_mnt_ns":  filter_by_mnt_ns,
+	}
+
+	if err := spec.RewriteConstants(consts); err != nil {
+		return fmt.Errorf("error RewriteConstants: %w", err)
+	}
+
+	opts := ebpf.CollectionOptions{
+		Maps: ebpf.MapOptions{
+			PinPath: filepath.Dir(t.config.MountnsMap),
+		},
+	}
+
+	if err := spec.LoadAndAssign(&t.objs, &opts); err != nil {
+		return fmt.Errorf("Failed to load ebpf program: %w", err)
+	}
+
+	kpread, err := link.Kprobe("vfs_read", t.objs.VfsReadEntry)
+	if err != nil {
+		return fmt.Errorf("Error opening kprobe: %w", err)
+	}
+	t.readLink = kpread
+
+	kpwrite, err := link.Kprobe("vfs_write", t.objs.VfsWriteEntry)
+	if err != nil {
+		return fmt.Errorf("Error opening kprobe: %w", err)
+	}
+	t.writeLink = kpwrite
+
+	t.run()
+
+	return nil
+}
+
+func (t *Tracer) next_stats() ([]types.Stats, error) {
+	stats := []types.Stats{}
+
+	var prev *C.struct_file_id = nil
+	key := C.struct_file_id{}
+	entries := t.objs.Entries
+
+	defer func() {
+		// delete elements
+		err := entries.NextKey(nil, unsafe.Pointer(&key))
+		if err != nil {
+			return
+		}
+
+		for {
+			if err := entries.Delete(key); err != nil {
+				return
+			}
+
+			prev = &key
+			if err := entries.NextKey(unsafe.Pointer(prev), unsafe.Pointer(&key)); err != nil {
+				return
+			}
+		}
+	}()
+
+	// gather elements
+	err := entries.NextKey(nil, unsafe.Pointer(&key))
+	if err != nil {
+		if errors.Is(err, ebpf.ErrKeyNotExist) {
+			return stats, nil
+		}
+		return nil, fmt.Errorf("Error getting next key: %w", err)
+	}
+
+	for {
+		file_stat := C.struct_file_stat{}
+		if err := entries.Lookup(key, unsafe.Pointer(&file_stat)); err != nil {
+			return nil, err
+		}
+
+		stat := types.Stats{
+			Reads:      uint64(file_stat.reads),
+			Writes:     uint64(file_stat.writes),
+			ReadBytes:  uint64(file_stat.read_bytes),
+			WriteBytes: uint64(file_stat.write_bytes),
+			Pid:        uint32(file_stat.pid),
+			Tid:        uint32(file_stat.tid),
+			Filename:   C.GoString(&file_stat.filename[0]),
+			Comm:       C.GoString(&file_stat.comm[0]),
+			FileType:   byte(file_stat.type_),
+			MountNsId:  uint64(file_stat.mntns_id),
+		}
+
+		container := t.resolver.LookupContainerByMntns(stat.MountNsId)
+		if container != nil {
+			stat.Container = container.Name
+			stat.Pod = container.Podname
+			stat.Namespace = container.Namespace
+			stat.Node = t.config.Node
+		}
+
+		stats = append(stats, stat)
+
+		prev = &key
+		if err := entries.NextKey(unsafe.Pointer(prev), unsafe.Pointer(&key)); err != nil {
+			if errors.Is(err, ebpf.ErrKeyNotExist) {
+				break
+			}
+			return nil, fmt.Errorf("error getting next key: %w\n", err)
+		}
+	}
+
+	types.SortStats(stats, t.config.SortBy)
+
+	return stats, nil
+}
+
+func (t *Tracer) run() {
+	ticker := time.NewTicker(t.config.Interval)
+
+	go func() {
+		for {
+			select {
+			case <-t.done:
+				break
+			case <-ticker.C:
+				stats, err := t.next_stats()
+				if err != nil {
+					t.errorCallback(err)
+					return
+				}
+
+				n := len(stats)
+				if n > t.config.MaxRows {
+					n = t.config.MaxRows
+				}
+				t.statsCallback(stats[:n])
+			}
+		}
+	}()
+}

pkg/gadgets/filetop/types/types.go
@@ -1,0 +1,106 @@
+// Copyright 2019-2021 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"fmt"
+	"sort"
+)
+
+type SortBy int
+
+const (
+	ALL SortBy = iota
+	READS
+	WRITES
+	RBYTES
+	WBYTES
+)
+
+var SortBySlice = []string{
+	"all",
+	"reads",
+	"writes",
+	"rbytes",
+	"wbytes",
+}
+
+func (s SortBy) String() string {
+	if int(s) < 0 || int(s) >= len(SortBySlice) {
+		return "INVALID"
+	}
+
+	return SortBySlice[int(s)]
+}
+
+func ParseSortBy(sortby string) (SortBy, error) {
+	for i, v := range SortBySlice {
+		if v == sortby {
+			return SortBy(i), nil
+		}
+	}
+	return ALL, fmt.Errorf("%q is not a valid sort by value", sortby)
+}
+
+// Event is the information the gadget sends to the client each capture
+// interval
+type Event struct {
+	Error string `json:"error,omitempty"`
+
+	// Node where the event comes from.
+	Node string `json:"node,omitempty"`
+
+	Stats []Stats `json:"stats,omitempty"`
+}
+
+// Stats represents the operations performed on a single file
+type Stats struct {
+	Node      string `json:"node,omitempty"`
+	Namespace string `json:"namespace,omitempty"`
+	Pod       string `json:"pod,omitempty"`
+	Container string `json:"container,omitempty"`
+
+	Reads      uint64 `json:"reads,omitempty"`
+	Writes     uint64 `json:"writes,omitempty"`
+	ReadBytes  uint64 `json:"rbytes,omitempty"`
+	WriteBytes uint64 `json:"wbytes,omitempty"`
+	Pid        uint32 `json:"pid,omitempty"`
+	Tid        uint32 `json:"tid,omitempty"`
+	MountNsId  uint64 `json:"mountnsid,omitempty"`
+	Filename   string `json:"filename,omitempty"`
+	Comm       string `json:"comm,omitempty"`
+	FileType   byte   `json:"file_type,omitempty"`
+}
+
+func SortStats(stats []Stats, sort_by SortBy) {
+	sort.Slice(stats, func(i, j int) bool {
+		a := stats[i]
+		b := stats[j]
+
+		switch sort_by {
+		case READS:
+			return a.Reads > b.Reads
+		case WRITES:
+			return a.Writes > b.Writes
+		case RBYTES:
+			return a.ReadBytes > b.ReadBytes
+		case WBYTES:
+			return a.WriteBytes > b.WriteBytes
+		default:
+			return a.Reads > b.Reads && a.Writes > b.Writes &&
+				a.ReadBytes > b.ReadBytes && a.WriteBytes > b.WriteBytes
+		}
+	})
+}

pkg/resources/samples/trace-filetop.yaml
@@ -1,0 +1,12 @@
+apiVersion: gadget.kinvolk.io/v1alpha1
+kind: Trace
+metadata:
+  name: filetop
+  namespace: gadget
+spec:
+  node: ubuntu-hirsute
+  gadget: filetop
+  runMode: Manual
+  outputMode: Stream
+  filter:
+    namespace: default

commit 8ae5c1c10fe4229a5f6db69934e3aa42ef804e32

pkg/gadgets: Use params key for filetop gadget.
Fixes: d05d1d78597 ("pkg/gadgets/filetop: Add tracer")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/filetop/gadget.go
@@ -58,11 +58,14 @@ func (f *TraceFactory) Description() string {
 	t := `filetop shows reads and writes by file, with container details.
 
 The following parameters are supported:
- - interval: Output interval, in seconds. (default %d)
- - max_rows: Maximum rows to print. (default %d)
- - sort: The field to sort the results by (%s). (default %s)`
-	return fmt.Sprintf(t, IntervalDefault, MaxRowsDefault,
-		strings.Join(types.SortBySlice, ","), SortByDefault)
+ - %s: Output interval, in seconds. (default %d)
+ - %s: Maximum rows to print. (default %d)
+ - %s: The field to sort the results by (%s). (default %s)
+ - %s: Show all files. (default %v, i.e. show regular files only)`
+	return fmt.Sprintf(t, types.IntervalParam, IntervalDefault,
+		types.MaxRowsParam, MaxRowsDefault,
+		types.SortByParam, strings.Join(types.SortBySlice, ","), SortByDefault,
+		types.AllFilesParam, AllFilesDefault)
 }
 
 func (f *TraceFactory) OutputModesSupported() map[string]struct{} {
@@ -118,34 +121,34 @@ func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
 		params := trace.Spec.Parameters
 		var err error
 
-		if val, ok := params["max_rows"]; ok {
+		if val, ok := params[types.MaxRowsParam]; ok {
 			maxRows, err = strconv.Atoi(val)
 			if err != nil {
-				trace.Status.OperationError = fmt.Sprintf("%q is not valid for ouput_rows", val)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for %s: %v", val, types.MaxRowsParam, err)
 				return
 			}
 		}
 
-		if val, ok := params["interval"]; ok {
+		if val, ok := params[types.IntervalParam]; ok {
 			intervalSeconds, err = strconv.Atoi(val)
 			if err != nil {
-				trace.Status.OperationError = fmt.Sprintf("%q is not valid for interval", val)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for %s: %v", val, types.IntervalParam, err)
 				return
 			}
 		}
 
-		if val, ok := params["sortby"]; ok {
+		if val, ok := params[types.SortByParam]; ok {
 			sortBy, err = types.ParseSortBy(val)
 			if err != nil {
-				trace.Status.OperationError = fmt.Sprintf("%q is not valid for sortby", val)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for %s: %v", val, types.SortByParam, err)
 				return
 			}
 		}
 
-		if val, ok := params["all_files"]; ok {
+		if val, ok := params[types.AllFilesParam]; ok {
 			allFiles, err = strconv.ParseBool(val)
 			if err != nil {
-				trace.Status.OperationError = fmt.Sprintf("%q is not valid for all_files", val)
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for %s: %v", val, types.AllFilesParam, err)
 				return
 			}
 		}