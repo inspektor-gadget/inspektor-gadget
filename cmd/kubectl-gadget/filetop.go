inspektor-gadget/inspektor-gadget

commit aa9c4b44326e69340d44854d1fc7fa9796434ef3

cmd/kubectl-gadget: Add filetop gadget

cmd/kubectl-gadget/filetop.go
@@ -1,0 +1,269 @@
+// Copyright 2019-2021 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
+
+	"github.com/kinvolk/inspektor-gadget/cmd/kubectl-gadget/utils"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/filetop/types"
+)
+
+var (
+	nodeStats map[string][]types.Stats
+	mu        sync.Mutex
+	done      bool = false
+)
+
+var (
+	// arguments
+	interval int = 1
+
+	// flags
+	maxrows    int
+	sortby_str string
+	sortby     types.SortBy
+	allFiles   bool
+)
+
+var filetopCmd = &cobra.Command{
+	Use:   "filetop [interval]",
+	Short: "Trace reads and writes by file, with container details",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+
+		nodeStats = make(map[string][]types.Stats)
+
+		if len(args) == 1 {
+			interval, err = strconv.Atoi(args[0])
+			if err != nil {
+				return fmt.Errorf("%q is not a valid value for interval", args[0])
+			}
+		}
+
+		config := &utils.TraceConfig{
+			GadgetName:       "filetop",
+			Operation:        "start",
+			TraceOutputMode:  "Stream",
+			TraceOutputState: "Started",
+			CommonFlags:      &params,
+			Parameters: map[string]string{
+				"max_rows":  strconv.Itoa(maxrows),
+				"interval":  strconv.Itoa(interval),
+				"sort_by":   sortby_str,
+				"all_files": strconv.FormatBool(allFiles),
+			},
+		}
+
+		startprint()
+
+		if err := utils.RunTraceStreamCallback(config, callback); err != nil {
+			return fmt.Errorf("error running trace: %w", err)
+		}
+
+		return nil
+	},
+	SilenceUsage: true,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		sortby, err = types.ParseSortBy(sortby_str)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+	Args: cobra.MaximumNArgs(1),
+}
+
+func init() {
+	filetopCmd.Flags().IntVarP(&maxrows, "maxrows", "r", 20, "Maximum rows to print")
+	filetopCmd.Flags().StringVarP(&sortby_str, "sort", "", "rbytes", "Sort column")
+	filetopCmd.Flags().BoolVarP(&allFiles, "all-files", "a", false, "Include non-regular file types (sockets, FIFOs, etc)")
+
+	rootCmd.AddCommand(filetopCmd)
+	utils.AddCommonFlags(filetopCmd, &params)
+}
+
+func callback(line string, node string) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	var event types.Event
+
+	if err := json.Unmarshal([]byte(line), &event); err != nil {
+		fmt.Fprintf(os.Stderr, "error unmarshalling json: %s", err)
+		return
+	}
+
+	if event.Error != "" {
+		fmt.Fprintf(os.Stderr, "error in node %s:%s", event.Node, event.Error)
+		return
+	}
+
+	nodeStats[node] = event.Stats
+}
+
+func startprint() {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+
+	go func() {
+		for {
+			print()
+			_ = <-ticker.C
+		}
+	}()
+}
+
+func print() {
+	// sort and print events
+	mu.Lock()
+
+	stats := []types.Stats{}
+	for _, stat := range nodeStats {
+		stats = append(stats, stat...)
+	}
+	nodeStats = make(map[string][]types.Stats)
+
+	mu.Unlock()
+
+	types.SortStats(stats, sortby)
+
+	switch params.OutputMode {
+	case utils.OutputModeColumns:
+		if term.IsTerminal(int(os.Stdout.Fd())) {
+			utils.ClearScreen()
+		} else {
+			fmt.Println("")
+		}
+		fmt.Printf("%-16s %-16s %-16s %-16s %-7s %-16s %-6s %-6s %-7s %-7s %1s %s\n",
+			"NODE", "NAMESPACE", "POD", "CONTAINER",
+			"PID", "COMM", "READS", "WRITES", "R_Kb", "W_Kb", "T", "FILE")
+		for idx, event := range stats {
+			if idx == maxrows {
+				break
+			}
+			fmt.Printf("%-16s %-16s %-16s %-16s %-7d %-16s %-6d %-6d %-7d %-7d %c %s\n",
+				event.Node, event.Namespace, event.Pod, event.Container,
+				event.Pid, event.Comm, event.Reads, event.Writes, event.ReadBytes/1024,
+				event.WriteBytes/1024, event.FileType, event.Filename)
+		}
+	case utils.OutputModeJson:
+		b, err := json.Marshal(stats)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error marshalling json: %v", err)
+			return
+		}
+		fmt.Println(string(b))
+	case utils.OutputModeCustomColumns:
+		if term.IsTerminal(int(os.Stdout.Fd())) {
+			utils.ClearScreen()
+		} else {
+			fmt.Println("")
+		}
+		fmt.Println(getCustomColsHeader(params.CustomColumns))
+		for idx, stat := range stats {
+			if idx == maxrows {
+				break
+			}
+			fmt.Println(formatEventCostumCols(&stat, params.CustomColumns))
+		}
+	}
+}
+
+func getCustomColsHeader(cols []string) string {
+	var sb strings.Builder
+
+	for _, col := range cols {
+		switch col {
+		case "node":
+			sb.WriteString(fmt.Sprintf("%-16s", "NODE"))
+		case "namespace":
+			sb.WriteString(fmt.Sprintf("%-16s", "NAMESPACE"))
+		case "pod":
+			sb.WriteString(fmt.Sprintf("%-16s", "POD"))
+		case "container":
+			sb.WriteString(fmt.Sprintf("%-16s", "CONTAINER"))
+		case "pid":
+			sb.WriteString(fmt.Sprintf("%-7s", "PID"))
+		case "tid":
+			sb.WriteString(fmt.Sprintf("%-7s", "TID"))
+		case "comm":
+			sb.WriteString(fmt.Sprintf("%-16s", "COMM"))
+		case "reads":
+			sb.WriteString(fmt.Sprintf("%-6s", "READS"))
+		case "writes":
+			sb.WriteString(fmt.Sprintf("%-6s", "WRITES"))
+		case "r_kb":
+			sb.WriteString(fmt.Sprintf("%-7s", "R_kb"))
+		case "w_kb":
+			sb.WriteString(fmt.Sprintf("%-7s", "W_kb"))
+		case "t":
+			sb.WriteString(fmt.Sprintf("%s", "T"))
+		case "file":
+			sb.WriteString(fmt.Sprintf("%s", "FILE"))
+		}
+		sb.WriteRune(' ')
+	}
+
+	return sb.String()
+}
+
+func formatEventCostumCols(stats *types.Stats, cols []string) string {
+	var sb strings.Builder
+
+	for _, col := range cols {
+		switch col {
+		case "node":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Node))
+		case "namespace":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Namespace))
+		case "pod":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Pod))
+		case "container":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Container))
+		case "pid":
+			sb.WriteString(fmt.Sprintf("%-7d", stats.Pid))
+		case "tid":
+			sb.WriteString(fmt.Sprintf("%-7d", stats.Tid))
+		case "comm":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Comm))
+		case "reads":
+			sb.WriteString(fmt.Sprintf("%-6d", stats.Reads))
+		case "writes":
+			sb.WriteString(fmt.Sprintf("%-6d", stats.Writes))
+		case "r_kb":
+			sb.WriteString(fmt.Sprintf("%-7d", stats.ReadBytes))
+		case "w_kb":
+			sb.WriteString(fmt.Sprintf("%-7d", stats.WriteBytes))
+		case "t":
+			sb.WriteString(fmt.Sprintf("%c", stats.FileType))
+		case "file":
+			sb.WriteString(fmt.Sprintf("%s", stats.Filename))
+		}
+		sb.WriteRune(' ')
+	}
+
+	return sb.String()
+}

go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/vishvananda/netns v0.0.0-20210104183010-2eb08e3e575f
 	github.com/weaveworks/tcptracer-bpf v0.0.0-20200114145059-84a08fc667c0
 	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 	k8s.io/api v0.21.2

go.sum
@@ -1498,13 +1498,15 @@ golang.org/x/sys v0.0.0-20210423082822-04245dca01da/go.mod h1:h1NjWce9XRLGQEsW7w
 golang.org/x/sys v0.0.0-20210426230700-d19ff857e887/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/sys v0.0.0-20210510120138-977fb7262007/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.0.0-20210616094352-59db8d763f22/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 h1:GkvMjFtXUmahfDtashnc1mnrCtuBVcwse5QV2lUk/tI=
 golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/term v0.0.0-20201117132131-f5c789dd3221/go.mod h1:Nr5EML6q2oocZ2LXRh80K7BxOlk5/8JxuGnuhpl+muw=
 golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1/go.mod h1:bj7SfCRtBDWHUb9snDiAeCFNEtKQo2Wmx5Cou7ajbmo=
-golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d h1:SZxvLBoTP5yHO3Frd4z4vrF+DBX9vMVanchswa69toE=
 golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d/go.mod h1:bj7SfCRtBDWHUb9snDiAeCFNEtKQo2Wmx5Cou7ajbmo=
+golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 h1:JGgROgKl9N8DuW20oFS5gxc+lE67/N3FcwmBPMe7ArY=
+golang.org/x/term v0.0.0-20210927222741-03fcf44c2211/go.mod h1:jbD1KX2456YbFQfuXm/mYQcufACuNUgVhRMnK/tPxf8=
 golang.org/x/text v0.0.0-20160726164857-2910a502d2bf/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
 golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
 golang.org/x/text v0.3.0/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=

commit 715f629888d970c89e64f60beb945f32d5cd1763

cmd/kubectl: Use params key for filetop CLI.
Fixes: aa9c4b44326 ("cmd/kubectl-gadget: Add filetop gadget")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

cmd/kubectl-gadget/filetop.go
@@ -70,10 +70,10 @@ var filetopCmd = &cobra.Command{
 			TraceOutputState: "Started",
 			CommonFlags:      &params,
 			Parameters: map[string]string{
-				"max_rows":  strconv.Itoa(maxRows),
-				"interval":  strconv.Itoa(interval),
-				"sort_by":   sortByStr,
-				"all_files": strconv.FormatBool(allFiles),
+				types.MaxRowsParam:  strconv.Itoa(maxRows),
+				types.IntervalParam: strconv.Itoa(interval),
+				types.SortByParam:   sortByStr,
+				types.AllFilesParam: strconv.FormatBool(allFiles),
 			},
 		}
 