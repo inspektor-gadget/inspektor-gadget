e4a785fccb24416a822f28d327a3ceb6a41a337e
Francis Laniel <flaniel@linux.microsoft.com>
2022-04-28 16:36:18 +0100

    pkg/gadgets/process-collector: Use bpf2go to generate golang eBPF structures.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- tools/btfgen.sh
+++ tools/btfgen.sh
@@ -18,20 +18,22 @@ if [ ! -d "${INSPEKTOR_GADGET}" ]; then
     exit 1
 fi
 
-${BTFHUB}/tools/btfgen.sh -a ${ARCH}                                                    \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/audit-seccomp/tracer/bpf/audit-seccomp.o         \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/bindsnoop/tracer/core/bindsnoop_bpfel.o          \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/biotop/tracer/biotop_bpfel.o                     \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/execsnoop/tracer/core/execsnoop_bpfel.o          \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/filetop/tracer/filetop_bpfel.o                   \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/fsslower/tracer/core/fsslower_bpfel.o            \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/mountsnoop/tracer/core/mountsnoop_bpfel.o        \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/oomkill/tracer/oomkill_bpfel.o                   \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/opensnoop/tracer/core/opensnoop_bpfel.o          \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/seccomp/tracer/bpf/seccomp.o                     \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/sigsnoop/tracer/core/sigsnoop_bpfel.o            \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/tcpconnect/tracer/core/tcpconnect_bpfel.o        \
-    -o ${INSPEKTOR_GADGET}/pkg/gadgets/tcptop/tracer/tcptop_bpfel.o                     \
+${BTFHUB}/tools/btfgen.sh -a ${ARCH}                                                                \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/audit-seccomp/tracer/bpf/audit-seccomp.o                     \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/bindsnoop/tracer/core/bindsnoop_bpfel.o                      \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/biotop/tracer/biotop_bpfel.o                                 \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/execsnoop/tracer/core/execsnoop_bpfel.o                      \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/filetop/tracer/filetop_bpfel.o                               \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/fsslower/tracer/core/fsslower_bpfel.o                        \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/mountsnoop/tracer/core/mountsnoop_bpfel.o                    \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/oomkill/tracer/oomkill_bpfel.o                               \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/opensnoop/tracer/core/opensnoop_bpfel.o                      \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/process-collector/tracer/processcollector_bpfel.o            \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/process-collector/tracer/processcollectorwithfilters_bpfel.o \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/seccomp/tracer/bpf/seccomp.o                                 \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/sigsnoop/tracer/core/sigsnoop_bpfel.o                        \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/tcpconnect/tracer/core/tcpconnect_bpfel.o                    \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/tcptop/tracer/tcptop_bpfel.o                                 \
     #
 
 mkdir -p ${OUTPUT}
