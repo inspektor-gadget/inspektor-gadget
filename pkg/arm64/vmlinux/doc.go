a2c43714923342982ecfb12db2630e4eb2a8e720
Francis Laniel <flaniel@linux.microsoft.com>
2022-04-29 12:06:25 +0100

    pkg/arm64: Add vmlinux.h for arm64.
    It was taken from iovisor/bcc as time of commit:
    0a5fbf66ac71 ("libbpf-tools: add arm64 and powerpc vmlinux.h headers")
    
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

---
+++ pkg/arm64/vmlinux/doc.go
@@ -0,0 +1,18 @@
+// Copyright 2019-2021 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package vmlinux does not provide any Go code but vmlinux.h to compile your
+// BPF code. vmlinux was taken from iovisor/bcc as time of commit:
+// 0a5fbf66ac71 ("libbpf-tools: add arm64 and powerpc vmlinux.h headers")
+package vmlinux
