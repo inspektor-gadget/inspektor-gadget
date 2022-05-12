c88bf19260640a101b770e46a4fc809f9a20951d
Francis Laniel <flaniel@linux.microsoft.com>
2022-05-12 11:17:47 +0100

    pkg/gadgets: Use kprobe for execsnoop on arm64.
    There is a bug in kernel for arm64 where sys_exit_execve tracepoint will never
    be triggered [1].
    By using kprobe, we can have the events generated but arguments will not be
    supported due to loops.
    
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
    [1] https://marc.info/?l=linux-arm-kernel&m=165218044913020

---
+++ pkg/gadgets/execsnoop/tracer/core/arm64_loadlinks.go
@@ -0,0 +1,38 @@
+//go:build (linux && arm64)
+// +build linux,arm64
+
+// Copyright 2019-2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tracer
+
+import (
+	"fmt"
+
+	"github.com/cilium/ebpf/link"
+)
+
+func loadExecsnoopLinks(objs execsnoopObjects) (link.Link, link.Link, error) {
+	enter, err := link.Kprobe("do_execveat_common.isra.0", objs.ExecveatEntry, nil)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error opening tracepoint: %w", err)
+	}
+
+	exit, err := link.Kretprobe("do_execveat_common.isra.0", objs.ExecveatExit, nil)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error opening tracepoint: %w", err)
+	}
+
+	return enter, exit, nil
+}
