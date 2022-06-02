39aefb92dd1df2ff73647b17707984662f8718c0
Francis Laniel <flaniel@linux.microsoft.com>
2022-06-02 17:13:45 +0100

    pkg/gadgets: Add capabilities CO-RE tracer.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- tools/btfgen.sh
+++ tools/btfgen.sh
@@ -25,6 +25,7 @@ ${BTFHUB}/tools/btfgen.sh -a ${ARCH}                                            
     -o ${INSPEKTOR_GADGET}/pkg/gadgets/audit-seccomp/tracer/auditseccompwithfilters_bpfel.o         \
     -o ${INSPEKTOR_GADGET}/pkg/gadgets/bindsnoop/tracer/core/bindsnoop_bpfel.o                      \
     -o ${INSPEKTOR_GADGET}/pkg/gadgets/biotop/tracer/biotop_bpfel.o                                 \
+    -o ${INSPEKTOR_GADGET}/pkg/gadgets/capabilities/tracer/core/capabilities_bpfel.o                \
     -o ${INSPEKTOR_GADGET}/pkg/gadgets/execsnoop/tracer/core/execsnoop_bpfel.o                      \
     -o ${INSPEKTOR_GADGET}/pkg/gadgets/filetop/tracer/filetop_bpfel.o                               \
     -o ${INSPEKTOR_GADGET}/pkg/gadgets/fsslower/tracer/core/fsslower_bpfel.o                        \
