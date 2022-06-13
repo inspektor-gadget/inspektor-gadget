484f136fdf838f0b092d92ae3d00ae531881e61d
Francis Laniel <flaniel@linux.microsoft.com>
2022-06-13 16:13:36 +0100

    pkg/gadgets: Create interface for profile.
    This will ease implementation of CO-RE for this gadget.
    
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- pkg/gadgets/profile/gadget.go
+++ pkg/gadgets/profile/gadget.go
@@ -15,27 +15,20 @@
 package profile
 
 import (
-	"bytes"
 	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"syscall"
 
-	"github.com/google/uuid"
 	gadgetv1alpha1 "github.com/kinvolk/inspektor-gadget/pkg/apis/gadget/v1alpha1"
 	"github.com/kinvolk/inspektor-gadget/pkg/gadgets"
 	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/profile/types"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/profile/tracer"
+	standardtracer "github.com/kinvolk/inspektor-gadget/pkg/gadgets/profile/tracer/standard"
 )
 
 type Trace struct {
 	resolver gadgets.Resolver
 
-	started           bool
-	cmd               *exec.Cmd
-	stdout            bytes.Buffer
-	stderr            bytes.Buffer
-	mountNsMapPinPath string
+	started bool
+	tracer  tracer.Tracer
 }
 
 type TraceFactory struct {
@@ -60,9 +53,8 @@ func (f *TraceFactory) OutputModesSupported() map[string]struct{} {
 
 func deleteTrace(name string, t interface{}) {
 	trace := t.(*Trace)
-	if trace.started {
-		trace.cmd.Process.Kill()
-		trace.cmd.Wait()
+	if trace.tracer != nil && trace.started {
+		trace.tracer.Stop()
 	}
 }
 
@@ -103,29 +95,17 @@ func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
 		return
 	}
 
-	t.mountNsMapPinPath = filepath.Join(gadgets.PinPath, uuid.New().String())
-	if err := mountNsMap.Pin(t.mountNsMapPinPath); err != nil {
-		trace.Status.OperationError = fmt.Sprintf("failed to pin tracer's mount ns map: %s", err)
-		return
+	_, userStackOnly := trace.Spec.Parameters[types.ProfileUserParam]
+	_, kernelStackOnly := trace.Spec.Parameters[types.ProfileKernelParam]
+	config := &tracer.Config{
+		MountnsMap:      mountNsMap,
+		UserStackOnly:   userStackOnly,
+		KernelStackOnly: kernelStackOnly,
 	}
 
-	t.cmd = exec.Command("/usr/share/bcc/tools/profile", "-f", "-d",
-		"--mntnsmap", t.mountNsMapPinPath)
-
-	if _, ok := trace.Spec.Parameters[types.ProfileUserParam]; ok {
-		t.cmd.Args = append(t.cmd.Args, "-U")
-	}
-
-	if _, ok := trace.Spec.Parameters[types.ProfileKernelParam]; ok {
-		t.cmd.Args = append(t.cmd.Args, "-K")
-	}
-
-	t.stdout.Reset()
-	t.stderr.Reset()
-	t.cmd.Stdout = &t.stdout
-	t.cmd.Stderr = &t.stderr
-	if err := t.cmd.Start(); err != nil {
-		trace.Status.OperationError = fmt.Sprintf("Failed to start: %s", err)
+	t.tracer, err = standardtracer.NewTracer(config, trace.Spec.Node)
+	if err != nil {
+		trace.Status.OperationError = fmt.Sprintf("failed to create tracer: %s", err)
 		return
 	}
 	t.started = true
@@ -139,33 +119,15 @@ func (t *Trace) Stop(trace *gadgetv1alpha1.Trace) {
 		trace.Status.OperationError = "Not started"
 		return
 	}
-	err := t.cmd.Process.Signal(syscall.SIGINT)
+
+	output, err := t.tracer.Stop()
 	if err != nil {
-		trace.Status.OperationError = fmt.Sprintf(
-			"Failed to send SIGINT to process: %s (stdout: %q stderr: %q)",
-			err,
-			t.stdout.String(),
-			t.stderr.String(),
-		)
+		trace.Status.OperationError = err.Error()
 		return
 	}
 
-	err = t.cmd.Wait()
-	if err != nil {
-		trace.Status.OperationError = fmt.Sprintf(
-			"Failed to wait for process: %s (stdout: %q stderr: %q)",
-			err,
-			t.stdout.String(),
-			t.stderr.String(),
-		)
-		return
-	}
-	t.cmd = nil
+	t.tracer = nil
 	t.started = false
-
-	os.Remove(t.mountNsMapPinPath)
-
-	output := t.stdout.String()
 
 	trace.Status.Output = output
 	trace.Status.State = "Completed"
