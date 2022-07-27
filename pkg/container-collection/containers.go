11fac6cf2c448d916e9338f9b26b1b2cb074a818
Jose Blanquicet <josebl@microsoft.com>
2022-07-27 16:58:43 +0200

    pkg/container-collection: Add JSON tag to Container struct
    It is useful for the local-gadget list-containers command.

--- pkg/container-collection/containers.go
+++ pkg/container-collection/containers.go
@@ -22,42 +22,42 @@ import (
 // Container represents a container with its metadata.
 type Container struct {
 	// ID is the container id, typically a 64 hexadecimal string
-	ID string
+	ID string `json:"id,omitempty"`
 
 	// Pid is the process id of the container
-	Pid uint32
+	Pid uint32 `json:"pid,omitempty"`
 
 	// Container's configuration is the config.json from the OCI runtime
 	// spec
-	OciConfig *ocispec.Spec
+	OciConfig *ocispec.Spec `json:"ociConfig,omitempty"`
 
 	// Bundle is the directory containing the config.json from the OCI
 	// runtime spec
 	// See https://github.com/opencontainers/runtime-spec/blob/main/bundle.md
-	Bundle string
+	Bundle string `json:"bundle,omitempty"`
 
 	// Linux metadata can be derived from the pid via /proc/$pid/...
-	Mntns      uint64
-	Netns      uint64
-	CgroupPath string
-	CgroupID   uint64
+	Mntns      uint64 `json:"mntns,omitempty"`
+	Netns      uint64 `json:"netns,omitempty"`
+	CgroupPath string `json:"cgroupPath,omitempty"`
+	CgroupID   uint64 `json:"cgroupID,omitempty"`
 	// Data required to find the container to Pod association in the
 	// gadgettracermanager.
-	CgroupV1 string
-	CgroupV2 string
+	CgroupV1 string `json:"cgroupV1,omitempty"`
+	CgroupV2 string `json:"cgroupV2,omitempty"`
 
 	// Kubernetes metadata
-	Namespace string
-	Podname   string
-	Name      string
-	Labels    map[string]string
+	Namespace string            `json:"namespace,omitempty"`
+	Podname   string            `json:"podname,omitempty"`
+	Name      string            `json:"name,omitempty"`
+	Labels    map[string]string `json:"labels,omitempty"`
 	// The owner reference information is added to the seccomp profile as
 	// annotations to help users to identify the workflow of the profile.
-	OwnerReference *metav1.OwnerReference
-	PodUID         string
+	OwnerReference *metav1.OwnerReference `json:"ownerReference,omitempty"`
+	PodUID         string                 `json:"podUID,omitempty"`
 
 	// Container Runtime metadata
-	Runtime string
+	Runtime string `json:"runtime,omitempty"`
 }
 
 type ContainerSelector struct {
