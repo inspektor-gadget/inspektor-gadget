inspektor-gadget/inspektor-gadget

commit b9db9360d8b8ef4bb21c2c9a07561e49f2c8914a

integration: Disable liveness probes when running on ARO

integration/command.go
@@ -69,10 +69,18 @@ type command struct {
 	started bool
 }
 
-var deployInspektorGadget *command = &command{
-	name:           "DeployInspektorGadget",
-	cmd:            "$KUBECTL_GADGET deploy $GADGET_IMAGE_FLAG",
-	expectedRegexp: `\d\/\d gadget pod\(s\) ready`,
+func deployInspektorGadget(image string, livenessProbe bool) *command {
+	cmd := fmt.Sprintf("$KUBECTL_GADGET deploy --liveness-probe=%t", livenessProbe)
+
+	if image != "" {
+		cmd = cmd + "--image=" + image
+	}
+
+	return &command{
+		name:           "DeployInspektorGadget",
+		cmd:            cmd,
+		expectedRegexp: `\d\/\d gadget pod\(s\) ready`,
+	}
 }
 
 func deploySPO(limitReplicas, bestEffortResourceMgmt bool) *command {

integration/integration_test.go
@@ -121,10 +121,6 @@ func testMain(m *testing.M) int {
 		return -1
 	}
 
-	if *image != "" {
-		os.Setenv("GADGET_IMAGE_FLAG", "--image "+*image)
-	}
-
 	if *k8sDistro != "" {
 		found := false
 		for _, val := range supportedK8sDistros {
@@ -149,7 +145,14 @@ func testMain(m *testing.M) int {
 	cleanupCommands := []*command{deleteRemainingNamespacesCommand()}
 
 	if !*doNotDeployIG {
-		initCommands = append(initCommands, deployInspektorGadget)
+		livenessProbe := true
+		// livenessProbe are causing some issues in the ARO integration cluster,
+		// see: https://github.com/kinvolk/inspektor-gadget/issues/799
+		if *k8sDistro == K8sDistroARO {
+			livenessProbe = false
+		}
+		deployCmd := deployInspektorGadget(*image, livenessProbe)
+		initCommands = append(initCommands, deployCmd)
 
 		cleanupCommands = append(cleanupCommands, cleanupInspektorGadget)
 	}

commit 9ffef909c8ebca00339e19f55c2d6ac4c81dcaa4

integration: Fix logic to generate command
A space before "--image" is needed, otherwise it fails with
"Error: invalid argument "true--image=XXX" for "--liveness-probe""

Fixes: b9db9360d8b8 ("integration: Disable liveness probes when running on ARO")

integration/command.go
@@ -73,7 +73,7 @@ func deployInspektorGadget(image string, livenessProbe bool) *command {
 	cmd := fmt.Sprintf("$KUBECTL_GADGET deploy --liveness-probe=%t", livenessProbe)
 
 	if image != "" {
-		cmd = cmd + "--image=" + image
+		cmd = cmd + " --image=" + image
 	}
 
 	return &command{