7d8cb529852e8c3fee2eae75647572b79d1a8a85
Mauricio Vásquez <mauriciov@microsoft.com>
2022-08-17 09:14:24 -0500

    pkg/gadgets: Add utils package to run tests
    This commit creates some helper types to run tests:
    
    - Runner: A type to execute events in a different mount namespace
    simulating they're running in a container.
    - testRunner: Some common logic to run tests.

---
+++ pkg/gadgets/internal/test/helpers.go
@@ -0,0 +1,131 @@
+//go:build linux
+// +build linux
+
+// Copyright 2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/cilium/ebpf"
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/sys/unix"
+)
+
+// CreateMntNsFilterMap creates and fills an eBPF map that can be used
+// to filter by mount namespace in the different tracers.
+func CreateMntNsFilterMap(t *testing.T, mountNsIDs ...uint64) *ebpf.Map {
+	t.Helper()
+
+	const one = uint32(1)
+
+	mntnsSpec := &ebpf.MapSpec{
+		Name:       "filter_map_" + t.Name(),
+		Type:       ebpf.Hash,
+		KeySize:    8,
+		ValueSize:  4,
+		MaxEntries: 1024,
+	}
+	m, err := ebpf.NewMap(mntnsSpec)
+	if err != nil {
+		t.Fatalf("Failed to create eBPF map: %s", err)
+	}
+
+	for _, mountnsid := range mountNsIDs {
+		if err := m.Put(mountnsid, one); err != nil {
+			m.Close()
+			t.Fatalf("Failed to update eBPF map: %s", err)
+		}
+	}
+
+	return m
+}
+
+// RequireRoot skips the test if the not running as root
+func RequireRoot(t *testing.T) {
+	t.Helper()
+
+	if unix.Getuid() != 0 {
+		t.Skip("Test requires root")
+	}
+}
+
+func NewRunnerWithTest(t *testing.T, config *RunnerConfig) *Runner {
+	t.Helper()
+
+	runner, err := NewRunner(config)
+	if err != nil {
+		t.Fatalf("Creating runner: %s", err)
+	}
+
+	t.Cleanup(runner.Close)
+
+	return runner
+}
+
+func RunWithRunner(t *testing.T, runner *Runner, f func() error) {
+	t.Helper()
+
+	if err := runner.Run(f); err != nil {
+		t.Fatalf("Error generating event: %s", err)
+	}
+}
+
+type ValidateEventType[Event any, Extra any] func(*testing.T, *RunnerInfo, Extra, []Event)
+
+// ExpectNoEvent doesn't expect any event to be captured by the tracer.
+func ExpectNoEvent[Event any, Extra any](t *testing.T, _ *RunnerInfo, _ Extra, events []Event) {
+	if len(events) != 0 {
+		t.Fatalf("No events are expected")
+	}
+}
+
+// ExpectAtLeastOneEvent expects that at least one of the captures events matches.
+func ExpectAtLeastOneEvent[Event any, Extra any](getEvent func(info *RunnerInfo, extra Extra) *Event) ValidateEventType[Event, Extra] {
+	return func(t *testing.T, info *RunnerInfo, extra Extra, events []Event) {
+		expectedEvent := getEvent(info, extra)
+
+		for _, event := range events {
+			if reflect.DeepEqual(expectedEvent, &event) {
+				return
+			}
+		}
+
+		// Provide extra info when only a single event was captured
+		if len(events) == 1 {
+			t.Fatalf("Event doesn't match:\n%s",
+				cmp.Diff(expectedEvent, &events[0]))
+		}
+		t.Fatalf("Event wasn't captured")
+	}
+}
+
+// ExpectOneEvent expects only matching event to be captured.
+func ExpectOneEvent[Event any, Extra any](getEvent func(info *RunnerInfo, extra Extra) *Event) ValidateEventType[Event, Extra] {
+	return func(t *testing.T, info *RunnerInfo, extra Extra, events []Event) {
+		expectedEvent := getEvent(info, extra)
+
+		if len(events) != 1 {
+			t.Fatalf("More than one event found")
+		}
+
+		if !reflect.DeepEqual(expectedEvent, &events[0]) {
+			t.Fatalf("Event doesn't match:\n%s",
+				cmp.Diff(expectedEvent, &events[0]))
+		}
+	}
+}
---
+++ pkg/gadgets/internal/test/runner.go
@@ -0,0 +1,183 @@
+//go:build linux
+// +build linux
+
+// Copyright 2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+// RunnerConfig defines how the runner should behave.
+// TODO: We could implement more options like unsharing the network
+// namespace, running on a different group ID, etc.
+type RunnerConfig struct {
+	// User ID to run under
+	UID int
+}
+
+// RunnerInfo contains information about the runner and it's used by the
+// tests to verify that the generated events have the correct value for
+// fields like PID, UID and MountNsID.
+type RunnerInfo struct {
+	Pid       int
+	Comm      string
+	UID       int
+	MountNsID uint64
+}
+
+// Runner is a helper type to execute tests in different conditions. It
+// creates a go routine that is executed in a different mount namespace,
+// user ID, etc. to simulate events hapenning inside containers.
+type Runner struct {
+	config *RunnerConfig
+
+	tasks   chan func() error
+	replies chan error
+	Info    *RunnerInfo
+}
+
+func NewRunner(config *RunnerConfig) (*Runner, error) {
+	if config == nil {
+		config = &RunnerConfig{}
+	}
+	r := &Runner{
+		config:  config,
+		tasks:   make(chan func() error),
+		replies: make(chan error),
+	}
+
+	go r.runLoop()
+
+	if err := <-r.replies; err != nil {
+		r.Close()
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func (r *Runner) Run(f func() error) error {
+	r.tasks <- f
+	return <-r.replies
+}
+
+func (r *Runner) Close() {
+	if r.tasks != nil {
+		close(r.tasks)
+		r.tasks = nil
+	}
+	if r.replies != nil {
+		close(r.replies)
+		r.replies = nil
+	}
+}
+
+func (r *Runner) runLoop() {
+	// Don't switch thread in the following code
+	runtime.LockOSThread()
+	// We aren't calling runtime.UnlockOSThread() to let this thread
+	// die because some changes are not recoverable, like switching
+	// to a non-root UID.
+
+	mntns, err := createMntNamespace()
+	if err != nil {
+		r.replies <- fmt.Errorf("creating mount namespace: %w", err)
+		return
+	}
+	defer unix.Close(mntns)
+
+	if r.config.UID != 0 {
+		// syscall.Setuid() can't be used here because it'll
+		// change the UID of all threads and we only need to
+		// change the one of this thread.
+		// https://github.com/golang/go/commit/d1b1145cace8b968307f9311ff611e4bb810710c
+		_, _, errno := syscall.Syscall(syscall.SYS_SETUID, uintptr(r.config.UID), 0, 0)
+		if errno != 0 {
+			r.replies <- fmt.Errorf("setting uid: %w", err)
+			return
+		}
+	}
+
+	pid := os.Getpid()
+	tid := unix.Gettid()
+
+	mountnsid, err := getMntNamespaceInode(pid, tid)
+	if err != nil {
+		r.replies <- fmt.Errorf("getting namespace inode: %w", err)
+		return
+	}
+
+	comm, err := os.Executable()
+	if err != nil {
+		r.replies <- fmt.Errorf("getting current executable: %w", err)
+		return
+	}
+
+	r.Info = &RunnerInfo{
+		Pid:       pid,
+		Comm:      filepath.Base(comm),
+		UID:       r.config.UID,
+		MountNsID: mountnsid,
+	}
+
+	// Indicate it's ready to process tasks
+	r.replies <- nil
+
+	for task := range r.tasks {
+		r.replies <- task()
+	}
+}
+
+func createMntNamespace() (int, error) {
+	if err := unix.Unshare(syscall.CLONE_NEWNS); err != nil {
+		return -1, err
+	}
+	return getMntNsHandle()
+}
+
+func getMntNamespaceInode(pid, tid int) (uint64, error) {
+	fileinfo, err := os.Stat(fmt.Sprintf("/proc/%d/task/%d/ns/mnt", pid, tid))
+	if err != nil {
+		return 0, err
+	}
+	stat, ok := fileinfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return 0, errors.New("not a syscall.Stat_t")
+	}
+	return stat.Ino, nil
+}
+
+// getMntNsHandle gets a handle to the current thread mount namespace.
+func getMntNsHandle() (int, error) {
+	pid := os.Getpid()
+	tid := unix.Gettid()
+
+	path := fmt.Sprintf("/proc/%d/task/%d/ns/mnt", pid, tid)
+
+	fd, err := unix.Open(path, unix.O_RDONLY|unix.O_CLOEXEC, 0)
+	if err != nil {
+		return -1, err
+	}
+	return fd, nil
+}
