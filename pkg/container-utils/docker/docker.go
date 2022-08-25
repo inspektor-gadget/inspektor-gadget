inspektor-gadget/inspektor-gadget

commit 6444282dc5d17bf9e44252f803df31fca05b1d60

Replace `PidFromContainerID` for `GetContainerDetails`, providing more details

pkg/container-utils/docker/docker.go
@@ -21,12 +21,12 @@ import (
 	"strings"
 	"time"
 
-	log "github.com/sirupsen/logrus"
-
 	dockertypes "github.com/docker/docker/api/types"
 	dockerfilters "github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
+	"github.com/kinvolk/inspektor-gadget/pkg/container-utils/cgroups"
 	runtimeclient "github.com/kinvolk/inspektor-gadget/pkg/container-utils/runtime-client"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -63,28 +63,6 @@ func NewDockerClient(socketPath string) (runtimeclient.ContainerRuntimeClient, e
 		client:     cli,
 		socketPath: socketPath,
 	}, nil
-}
-
-func (c *DockerClient) PidFromContainerID(containerID string) (int, error) {
-	containerID, err := runtimeclient.ParseContainerID(Name, containerID)
-	if err != nil {
-		return -1, err
-	}
-
-	containerJSON, err := c.client.ContainerInspect(context.Background(), containerID)
-	if err != nil {
-		return -1, err
-	}
-
-	if containerJSON.State == nil {
-		return -1, errors.New("container state is nil")
-	}
-
-	if containerJSON.State.Pid == 0 {
-		return -1, errors.New("got zero pid")
-	}
-
-	return containerJSON.State.Pid, nil
 }
 
 func listContainers(c *DockerClient, filter *dockerfilters.Args) ([]dockertypes.Container, error) {
@@ -142,13 +120,69 @@ func (c *DockerClient) GetContainer(containerID string) (*runtimeclient.Containe
 	return DockerContainerToContainerData(&containers[0]), nil
 }
 
-func DockerContainerToContainerData(container *dockertypes.Container) *runtimeclient.ContainerData {
-	return &runtimeclient.ContainerData{
-		ID:      container.ID,
-		Name:    strings.TrimPrefix(container.Names[0], "/"),
-		Running: container.State == "running",
-		Runtime: Name,
+func (c *DockerClient) GetContainerDetails(containerID string) (*runtimeclient.ContainerDetailsData, error) {
+	containerID, err := runtimeclient.ParseContainerID(Name, containerID)
+	if err != nil {
+		return nil, err
 	}
+
+	containerJSON, err := c.client.ContainerInspect(context.Background(), containerID)
+	if err != nil {
+		return nil, err
+	}
+
+	if containerJSON.State == nil {
+		return nil, errors.New("container state is nil")
+	}
+	if containerJSON.State.Pid == 0 {
+		return nil, errors.New("got zero pid")
+	}
+	if containerJSON.HostConfig == nil {
+		return nil, errors.New("container host config is nil")
+	}
+
+	containerDetailsData := runtimeclient.ContainerDetailsData{
+		ContainerData: runtimeclient.ContainerData{
+			ID:      containerJSON.ID,
+			Name:    strings.TrimPrefix(containerJSON.Name, "/"),
+			State:   containerStatusStateToRuntimeClientState(containerJSON.State.Status),
+			Runtime: Name,
+		},
+		Pid:         containerJSON.State.Pid,
+		CgroupsPath: string(containerJSON.HostConfig.Cgroup),
+	}
+	if len(containerJSON.Mounts) > 0 {
+		containerDetailsData.Mounts = make([]runtimeclient.ContainerMountData, len(containerJSON.Mounts))
+		for i, containerMount := range containerJSON.Mounts {
+			containerDetailsData.Mounts[i] = runtimeclient.ContainerMountData{
+				Destination: containerMount.Destination,
+				Source:      containerMount.Source,
+			}
+		}
+	}
+
+	// Try to get cgroups information from /proc/<pid>/cgroup as a fallback.
+	// However, don't fail if such a file is not available, as it would prevent the
+	// whole feature to work on systems without this file.
+	if containerDetailsData.CgroupsPath == "" {
+		log.Debugf("cgroups info not available on Docker for container %s. Trying /proc/%d/cgroup as a fallback",
+			containerID, containerDetailsData.Pid)
+
+		// Get cgroup paths for V1 and V2.
+		cgroupPathV1, cgroupPathV2, err := cgroups.GetCgroupPaths(containerDetailsData.Pid)
+		if err == nil {
+			cgroupsPath := cgroupPathV1
+			if cgroupsPath == "" {
+				cgroupsPath = cgroupPathV2
+			}
+			containerDetailsData.CgroupsPath = cgroupsPath
+		} else {
+			log.Warnf("failed to get cgroups info of container %s from /proc/%d/cgroup: %s",
+				containerID, containerDetailsData.Pid, err)
+		}
+	}
+
+	return &containerDetailsData, nil
 }
 
 func (c *DockerClient) Close() error {
@@ -157,4 +191,30 @@ func (c *DockerClient) Close() error {
 	}
 
 	return nil
+}
+
+// Convert the state from container status to state of runtime client.
+func containerStatusStateToRuntimeClientState(containerState string) (runtimeClientState string) {
+	switch containerState {
+	case "created":
+		runtimeClientState = runtimeclient.StateCreated
+	case "running":
+		runtimeClientState = runtimeclient.StateRunning
+	case "exited":
+		runtimeClientState = runtimeclient.StateExited
+	case "dead":
+		runtimeClientState = runtimeclient.StateExited
+	default:
+		runtimeClientState = runtimeclient.StateUnknown
+	}
+	return
+}
+
+func DockerContainerToContainerData(container *dockertypes.Container) *runtimeclient.ContainerData {
+	return &runtimeclient.ContainerData{
+		ID:      container.ID,
+		Name:    strings.TrimPrefix(container.Names[0], "/"),
+		State:   containerStatusStateToRuntimeClientState(container.Status),
+		Runtime: Name,
+	}
 }

pkg/container-collection/k8s.go
@@ -112,9 +112,10 @@ func (k *K8sClient) PodToContainers(pod *v1.Pod) []Container {
 			continue
 		}
 
-		pid, err := k.runtimeClient.PidFromContainerID(s.ContainerID)
+		containerData, err := k.runtimeClient.GetContainerDetails(s.ContainerID)
 		if err != nil {
-			log.Warnf("Skip pod %s/%s: cannot find pid: %v", pod.GetNamespace(), pod.GetName(), err)
+			log.Warnf("Skip pod %s/%s: cannot find container (ID: %s): %v",
+				pod.GetNamespace(), pod.GetName(), s.ContainerID, err)
 			continue
 		}
 
@@ -129,7 +130,7 @@ func (k *K8sClient) PodToContainers(pod *v1.Pod) []Container {
 			Podname:   pod.GetName(),
 			Name:      s.Name,
 			Labels:    labels,
-			Pid:       uint32(pid),
+			Pid:       uint32(containerData.Pid),
 		}
 		containers = append(containers, containerDef)
 	}

pkg/container-collection/options.go
@@ -31,6 +31,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 
 	containerutils "github.com/kinvolk/inspektor-gadget/pkg/container-utils"
+	"github.com/kinvolk/inspektor-gadget/pkg/container-utils/cgroups"
 	runtimeclient "github.com/kinvolk/inspektor-gadget/pkg/container-utils/runtime-client"
 	"github.com/kinvolk/inspektor-gadget/pkg/runcfanotify"
 )
@@ -126,15 +127,15 @@ func WithContainerRuntimeEnrichment(runtime *containerutils.RuntimeConfig) Conta
 			return nil
 		}
 		for _, container := range containers {
-			if !container.Running {
+			if container.State != runtimeclient.StateRunning {
 				log.Debugf("Runtime enricher(%s): Skip container %q (ID: %s): not running",
 					runtime.Name, container.Name, container.ID)
 				continue
 			}
 
-			pid, err := runtimeClient.PidFromContainerID(container.ID)
+			containerData, err := runtimeClient.GetContainerDetails(container.ID)
 			if err != nil {
-				log.Debugf("Runtime enricher (%s): Skip container %q (ID: %s): couldn't find pid: %s",
+				log.Debugf("Runtime enricher (%s): Skip container %q (ID: %s): couldn't find container: %s",
 					runtime.Name, container.Name, container.ID, err)
 				continue
 			}
@@ -142,7 +143,7 @@ func WithContainerRuntimeEnrichment(runtime *containerutils.RuntimeConfig) Conta
 			cc.initialContainers = append(cc.initialContainers,
 				&Container{
 					ID:      container.ID,
-					Pid:     uint32(pid),
+					Pid:     uint32(containerData.Pid),
 					Name:    container.Name,
 					Runtime: container.Runtime,
 
@@ -605,13 +606,13 @@ func WithCgroupEnrichment() ContainerCollectionOption {
 				return true
 			}
 
-			cgroupPathV1, cgroupPathV2, err := containerutils.GetCgroupPaths(pid)
+			cgroupPathV1, cgroupPathV2, err := cgroups.GetCgroupPaths(pid)
 			if err != nil {
 				log.Errorf("cgroup enricher: failed to get cgroup paths on container %s: %s", container.ID, err)
 				return true
 			}
-			cgroupPathV2WithMountpoint, _ := containerutils.CgroupPathV2AddMountpoint(cgroupPathV2)
-			cgroupID, _ := containerutils.GetCgroupID(cgroupPathV2WithMountpoint)
+			cgroupPathV2WithMountpoint, _ := cgroups.CgroupPathV2AddMountpoint(cgroupPathV2)
+			cgroupID, _ := cgroups.GetCgroupID(cgroupPathV2WithMountpoint)
 
 			container.CgroupPath = cgroupPathV2WithMountpoint
 			container.CgroupID = cgroupID

pkg/container-utils/cgroups/cgroups.go
@@ -1,0 +1,138 @@
+// Copyright 2019-2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cgroups
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"unsafe"
+)
+
+/*
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+
+struct cgid_file_handle
+{
+  //struct file_handle handle;
+  unsigned int handle_bytes;
+  int handle_type;
+  uint64_t cgid;
+};
+
+uint64_t get_cgroupid(char *path) {
+  struct cgid_file_handle *h;
+  int mount_id;
+  int err;
+  uint64_t ret;
+
+  h = malloc(sizeof(struct cgid_file_handle));
+  if (!h)
+    return 0;
+
+  h->handle_bytes = 8;
+  err = name_to_handle_at(AT_FDCWD, path, (struct file_handle *)h, &mount_id, 0);
+  if (err != 0) {
+    ret = 0;
+    goto out;
+  }
+
+  if (h->handle_bytes != 8) {
+    ret = 0;
+    goto out;
+  }
+
+  ret = h->cgid;
+
+out:
+  free(h);
+
+  return ret;
+}
+*/
+import "C"
+
+func CgroupPathV2AddMountpoint(path string) (string, error) {
+	pathWithMountpoint := filepath.Join("/sys/fs/cgroup/unified", path)
+	if _, err := os.Stat(pathWithMountpoint); os.IsNotExist(err) {
+		pathWithMountpoint = filepath.Join("/sys/fs/cgroup", path)
+		if _, err := os.Stat(pathWithMountpoint); os.IsNotExist(err) {
+			return "", fmt.Errorf("cannot access cgroup %q: %w", path, err)
+		}
+	}
+	return pathWithMountpoint, nil
+}
+
+// GetCgroupID returns the cgroup2 ID of a path.
+func GetCgroupID(pathWithMountpoint string) (uint64, error) {
+	cPathWithMountpoint := C.CString(pathWithMountpoint)
+	ret := uint64(C.get_cgroupid(cPathWithMountpoint))
+	C.free(unsafe.Pointer(cPathWithMountpoint))
+	if ret == 0 {
+		return 0, fmt.Errorf("GetCgroupID on %q failed", pathWithMountpoint)
+	}
+	return ret, nil
+}
+
+// GetCgroupPaths returns the cgroup1 and cgroup2 paths of a process.
+// It does not include the "/sys/fs/cgroup/{unified,systemd,}" prefix.
+func GetCgroupPaths(pid int) (string, string, error) {
+	cgroupPathV1 := ""
+	cgroupPathV2 := ""
+	if cgroupFile, err := os.Open(filepath.Join("/proc", fmt.Sprintf("%d", pid), "cgroup")); err == nil {
+		defer cgroupFile.Close()
+		reader := bufio.NewReader(cgroupFile)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				break
+			}
+			if strings.HasPrefix(line, "1:name=systemd:") {
+				cgroupPathV1 = strings.TrimPrefix(line, "1:name=systemd:")
+				cgroupPathV1 = strings.TrimSuffix(cgroupPathV1, "\n")
+				continue
+			}
+			if strings.HasPrefix(line, "0::") {
+				cgroupPathV2 = strings.TrimPrefix(line, "0::")
+				cgroupPathV2 = strings.TrimSuffix(cgroupPathV2, "\n")
+				continue
+			}
+		}
+	} else {
+		return "", "", fmt.Errorf("cannot parse cgroup: %w", err)
+	}
+
+	if cgroupPathV1 == "/" {
+		cgroupPathV1 = ""
+	}
+
+	if cgroupPathV2 == "/" {
+		cgroupPathV2 = ""
+	}
+
+	if cgroupPathV2 == "" && cgroupPathV1 == "" {
+		return "", "", fmt.Errorf("cannot find cgroup path in /proc/PID/cgroup")
+	}
+
+	return cgroupPathV1, cgroupPathV2, nil
+}

pkg/container-utils/containerd/containerd.go
@@ -17,6 +17,7 @@ package containerd
 import (
 	"time"
 
+	criclient "github.com/kinvolk/inspektor-gadget/pkg/container-utils/cri"
 	runtimeclient "github.com/kinvolk/inspektor-gadget/pkg/container-utils/runtime-client"
 )
 
@@ -27,7 +28,7 @@ const (
 )
 
 type ContainerdClient struct {
-	runtimeclient.CRIClient
+	criclient.CRIClient
 }
 
 func NewContainerdClient(socketPath string) (runtimeclient.ContainerRuntimeClient, error) {
@@ -35,7 +36,7 @@ func NewContainerdClient(socketPath string) (runtimeclient.ContainerRuntimeClien
 		socketPath = DefaultSocketPath
 	}
 
-	criClient, err := runtimeclient.NewCRIClient(Name, socketPath, DefaultTimeout)
+	criClient, err := criclient.NewCRIClient(Name, socketPath, DefaultTimeout)
 	if err != nil {
 		return nil, err
 	}

pkg/container-utils/containerutils.go
@@ -15,7 +15,6 @@
 package containerutils
 
 import (
-	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -24,7 +23,6 @@ import (
 	"regexp"
 	"strings"
 	"syscall"
-	"unsafe"
 
 	"github.com/kinvolk/inspektor-gadget/pkg/container-utils/containerd"
 	"github.com/kinvolk/inspektor-gadget/pkg/container-utils/crio"
@@ -33,55 +31,6 @@ import (
 
 	ocispec "github.com/opencontainers/runtime-spec/specs-go"
 )
-
-/*
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdint.h>
-
-struct cgid_file_handle
-{
-  //struct file_handle handle;
-  unsigned int handle_bytes;
-  int handle_type;
-  uint64_t cgid;
-};
-
-uint64_t get_cgroupid(char *path) {
-  struct cgid_file_handle *h;
-  int mount_id;
-  int err;
-  uint64_t ret;
-
-  h = malloc(sizeof(struct cgid_file_handle));
-  if (!h)
-    return 0;
-
-  h->handle_bytes = 8;
-  err = name_to_handle_at(AT_FDCWD, path, (struct file_handle *)h, &mount_id, 0);
-  if (err != 0) {
-    ret = 0;
-    goto out;
-  }
-
-  if (h->handle_bytes != 8) {
-    ret = 0;
-    goto out;
-  }
-
-  ret = h->cgid;
-
-out:
-  free(h);
-
-  return ret;
-}
-*/
-import "C"
 
 var AvailableRuntimes = []string{
 	docker.Name,
@@ -106,71 +55,6 @@ func NewContainerRuntimeClient(runtime *RuntimeConfig) (runtimeclient.ContainerR
 		return nil, fmt.Errorf("unknown container runtime: %s (available %s)",
 			runtime, strings.Join(AvailableRuntimes, ", "))
 	}
-}
-
-func CgroupPathV2AddMountpoint(path string) (string, error) {
-	pathWithMountpoint := filepath.Join("/sys/fs/cgroup/unified", path)
-	if _, err := os.Stat(pathWithMountpoint); os.IsNotExist(err) {
-		pathWithMountpoint = filepath.Join("/sys/fs/cgroup", path)
-		if _, err := os.Stat(pathWithMountpoint); os.IsNotExist(err) {
-			return "", fmt.Errorf("cannot access cgroup %q: %w", path, err)
-		}
-	}
-	return pathWithMountpoint, nil
-}
-
-// GetCgroupID returns the cgroup2 ID of a path.
-func GetCgroupID(pathWithMountpoint string) (uint64, error) {
-	cPathWithMountpoint := C.CString(pathWithMountpoint)
-	ret := uint64(C.get_cgroupid(cPathWithMountpoint))
-	C.free(unsafe.Pointer(cPathWithMountpoint))
-	if ret == 0 {
-		return 0, fmt.Errorf("GetCgroupID on %q failed", pathWithMountpoint)
-	}
-	return ret, nil
-}
-
-// GetCgroupPaths returns the cgroup1 and cgroup2 paths of a process.
-// It does not include the "/sys/fs/cgroup/{unified,systemd,}" prefix.
-func GetCgroupPaths(pid int) (string, string, error) {
-	cgroupPathV1 := ""
-	cgroupPathV2 := ""
-	if cgroupFile, err := os.Open(filepath.Join("/proc", fmt.Sprintf("%d", pid), "cgroup")); err == nil {
-		defer cgroupFile.Close()
-		reader := bufio.NewReader(cgroupFile)
-		for {
-			line, err := reader.ReadString('\n')
-			if err != nil {
-				break
-			}
-			if strings.HasPrefix(line, "1:name=systemd:") {
-				cgroupPathV1 = strings.TrimPrefix(line, "1:name=systemd:")
-				cgroupPathV1 = strings.TrimSuffix(cgroupPathV1, "\n")
-				continue
-			}
-			if strings.HasPrefix(line, "0::") {
-				cgroupPathV2 = strings.TrimPrefix(line, "0::")
-				cgroupPathV2 = strings.TrimSuffix(cgroupPathV2, "\n")
-				continue
-			}
-		}
-	} else {
-		return "", "", fmt.Errorf("cannot parse cgroup: %w", err)
-	}
-
-	if cgroupPathV1 == "/" {
-		cgroupPathV1 = ""
-	}
-
-	if cgroupPathV2 == "/" {
-		cgroupPathV2 = ""
-	}
-
-	if cgroupPathV2 == "" && cgroupPathV1 == "" {
-		return "", "", fmt.Errorf("cannot find cgroup path in /proc/PID/cgroup")
-	}
-
-	return cgroupPathV1, cgroupPathV2, nil
 }
 
 func getNamespaceInode(pid int, nsType string) (uint64, error) {

pkg/container-utils/cri/cri.go
@@ -1,0 +1,286 @@
+// Copyright 2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cri
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	runtimeclient "github.com/kinvolk/inspektor-gadget/pkg/container-utils/runtime-client"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+)
+
+// CRIClient implements the ContainerRuntimeClient interface using the CRI
+// plugin interface to communicate with the different container runtimes.
+type CRIClient struct {
+	Name        string
+	SocketPath  string
+	ConnTimeout time.Duration
+
+	conn   *grpc.ClientConn
+	client pb.RuntimeServiceClient
+}
+
+func NewCRIClient(name, socketPath string, timeout time.Duration) (CRIClient, error) {
+	conn, err := grpc.Dial(
+		socketPath,
+		grpc.WithInsecure(),
+		grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
+			d := net.Dialer{Timeout: timeout}
+			return d.DialContext(ctx, "unix", socketPath)
+		}),
+	)
+	if err != nil {
+		return CRIClient{}, err
+	}
+
+	return CRIClient{
+		Name:        name,
+		SocketPath:  socketPath,
+		ConnTimeout: timeout,
+		conn:        conn,
+		client:      pb.NewRuntimeServiceClient(conn),
+	}, nil
+}
+
+func listContainers(c *CRIClient, filter *pb.ContainerFilter) ([]*pb.Container, error) {
+	request := &pb.ListContainersRequest{}
+	if filter != nil {
+		request.Filter = filter
+	}
+
+	res, err := c.client.ListContainers(context.Background(), request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list containers with request %+v: %w",
+			request, err)
+	}
+
+	return res.GetContainers(), nil
+}
+
+func (c *CRIClient) GetContainers() ([]*runtimeclient.ContainerData, error) {
+	containers, err := listContainers(c, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := make([]*runtimeclient.ContainerData, len(containers))
+
+	for i, container := range containers {
+		ret[i] = CRIContainerToContainerData(c.Name, container)
+	}
+
+	return ret, nil
+}
+
+func (c *CRIClient) GetContainer(containerID string) (*runtimeclient.ContainerData, error) {
+	containers, err := listContainers(c, &pb.ContainerFilter{
+		Id: containerID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(containers) == 0 {
+		return nil, fmt.Errorf("container %q not found", containerID)
+	}
+	if len(containers) > 1 {
+		log.Warnf("CRIClient: multiple containers (%d) with ID %q. Taking the first one: %+v",
+			len(containers), containerID, containers)
+	}
+
+	return CRIContainerToContainerData(c.Name, containers[0]), nil
+}
+
+func (c *CRIClient) GetContainerDetails(containerID string) (*runtimeclient.ContainerDetailsData, error) {
+	containerID, err := runtimeclient.ParseContainerID(c.Name, containerID)
+	if err != nil {
+		return nil, err
+	}
+
+	request := &pb.ContainerStatusRequest{
+		ContainerId: containerID,
+		Verbose:     true,
+	}
+
+	res, err := c.client.ContainerStatus(context.Background(), request)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseContainerDetailsData(c.Name, res.Status, res.Info)
+}
+
+func (c *CRIClient) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+
+	return nil
+}
+
+// parseContainerDetailsData parses the container status and extra information
+// returned by ContainerStatus() into a ContainerDetailsData structure.
+func parseContainerDetailsData(runtimeName string, containerStatus *pb.ContainerStatus,
+	extraInfo map[string]string,
+) (*runtimeclient.ContainerDetailsData, error) {
+	// Create container details structure to be filled.
+	containerDetailsData := &runtimeclient.ContainerDetailsData{
+		ContainerData: runtimeclient.ContainerData{
+			ID:      containerStatus.Id,
+			Name:    strings.TrimPrefix(containerStatus.GetMetadata().Name, "/"),
+			State:   containerStatusStateToRuntimeClientState(containerStatus.GetState()),
+			Runtime: runtimeName,
+		},
+	}
+
+	// Parse the extra info and fill the data.
+	err := parseExtraInfo(extraInfo, containerDetailsData)
+	if err != nil {
+		return nil, err
+	}
+
+	return containerDetailsData, nil
+}
+
+// parseExtraInfo parses the extra information returned by ContainerStatus()
+// into a ContainerDetailsData structure. It keeps backward compatibility after
+// the ContainerInfo format was modified in:
+// cri-o v1.18.0: https://github.com/cri-o/cri-o/commit/be8e876cdabec4e055820502fed227aa44971ddc
+// containerd v1.6.0-beta.1: https://github.com/containerd/containerd/commit/85b943eb47bc7abe53b9f9e3d953566ed0f65e6c
+// NOTE: CRI-O does not have runtime spec prior to 1.18.0
+func parseExtraInfo(extraInfo map[string]string,
+	containerDetailsData *runtimeclient.ContainerDetailsData,
+) error {
+	// Define the info content (only required fields).
+	type RuntimeSpecContent struct {
+		Mounts []struct {
+			Destination string `json:"destination"`
+			Source      string `json:"source,omitempty"`
+		} `json:"mounts,omitempty"`
+		Linux *struct {
+			CgroupsPath string `json:"cgroupsPath,omitempty"`
+		} `json:"linux,omitempty" platform:"linux"`
+	}
+	type InfoContent struct {
+		Pid         int                `json:"pid"`
+		RuntimeSpec RuntimeSpecContent `json:"runtimeSpec"`
+	}
+
+	// Set invalid value to PID.
+	pid := -1
+	containerDetailsData.Pid = pid
+
+	// Get the extra info from the map.
+	var runtimeSpec *RuntimeSpecContent
+	info, ok := extraInfo["info"]
+	if ok {
+		// Unmarshal the JSON to fields.
+		var infoContent InfoContent
+		err := json.Unmarshal([]byte(info), &infoContent)
+		if err != nil {
+			return fmt.Errorf("failed extracting pid from container status reply: %w", err)
+		}
+
+		// Set the PID value.
+		pid = infoContent.Pid
+
+		// Set the runtime spec pointer, to be copied below.
+		runtimeSpec = &infoContent.RuntimeSpec
+
+		// Legacy parsing.
+	} else {
+		// Extract the PID.
+		pidStr, ok := extraInfo["pid"]
+		if !ok {
+			return fmt.Errorf("container status reply from runtime doesn't contain pid")
+		}
+		var err error
+		pid, err = strconv.Atoi(pidStr)
+		if err != nil {
+			return fmt.Errorf("failed to parse pid %q: %w", pidStr, err)
+		}
+
+		// Extract the runtime spec (may not exist).
+		runtimeSpecStr, ok := extraInfo["runtimeSpec"]
+		if ok {
+			// Unmarshal the JSON to fields.
+			runtimeSpec = &RuntimeSpecContent{}
+			err := json.Unmarshal([]byte(runtimeSpecStr), runtimeSpec)
+			if err != nil {
+				return fmt.Errorf("failed extracting runtime spec from container status reply: %w", err)
+			}
+		}
+	}
+
+	// Validate extracted fields.
+	if pid == 0 {
+		return fmt.Errorf("got zero pid")
+	}
+
+	// Set the PID value.
+	containerDetailsData.Pid = pid
+
+	// Copy the runtime spec fields.
+	if runtimeSpec != nil {
+		if runtimeSpec.Linux != nil {
+			containerDetailsData.CgroupsPath = runtimeSpec.Linux.CgroupsPath
+		}
+		if len(runtimeSpec.Mounts) > 0 {
+			containerDetailsData.Mounts = make([]runtimeclient.ContainerMountData, len(runtimeSpec.Mounts))
+			for i, specMount := range runtimeSpec.Mounts {
+				containerDetailsData.Mounts[i] = runtimeclient.ContainerMountData{
+					Destination: specMount.Destination,
+					Source:      specMount.Source,
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// Convert the state from container status to state of runtime client.
+func containerStatusStateToRuntimeClientState(containerStatusState pb.ContainerState) (runtimeClientState string) {
+	switch containerStatusState {
+	case pb.ContainerState_CONTAINER_CREATED:
+		runtimeClientState = runtimeclient.StateCreated
+	case pb.ContainerState_CONTAINER_RUNNING:
+		runtimeClientState = runtimeclient.StateRunning
+	case pb.ContainerState_CONTAINER_EXITED:
+		runtimeClientState = runtimeclient.StateExited
+	case pb.ContainerState_CONTAINER_UNKNOWN:
+		runtimeClientState = runtimeclient.StateUnknown
+	default:
+		runtimeClientState = runtimeclient.StateUnknown
+	}
+	return
+}
+
+func CRIContainerToContainerData(runtimeName string, container *pb.Container) *runtimeclient.ContainerData {
+	return &runtimeclient.ContainerData{
+		ID:      container.Id,
+		Name:    strings.TrimPrefix(container.GetMetadata().Name, "/"),
+		State:   containerStatusStateToRuntimeClientState(container.GetState()),
+		Runtime: runtimeName,
+	}
+}

commit 9861983327e32c71927d75f79efff3ec49a2c4d5

pkg/container-utils/docker: Fix bad state
The correct variable to use is State instead of Status. It was causing
to always have the State as "unknown", causing
"./local-gadget list-containers" not to show any docker containers.

Fixes: 6444282dc5d1 ("Replace `PidFromContainerID` for `GetContainerDetails`, providing more details")

pkg/container-utils/docker/docker.go
@@ -214,7 +214,7 @@ func DockerContainerToContainerData(container *dockertypes.Container) *runtimecl
 	return &runtimeclient.ContainerData{
 		ID:      container.ID,
 		Name:    strings.TrimPrefix(container.Names[0], "/"),
-		State:   containerStatusStateToRuntimeClientState(container.Status),
+		State:   containerStatusStateToRuntimeClientState(container.State),
 		Runtime: Name,
 	}
 }