inspektor-gadget/inspektor-gadget

commit 4130c004fead8f29937f74c7fd04de8ac3340348

Rename ebpf programs
Before:

PROGID   TYPE             NAME             PID     COMM                 CUMULRUNCOUNT CUMULRUNTIME
514      RawTracepoint    tracepoint__raw  2951    gadgettracerman            1640398 2.212490654s
517      TracePoint       tracepoint__sys  2951    gadgettracerman               5513 474.843483ms
516      TracePoint       tracepoint__sys  2951    gadgettracerman              65935  38.822213ms
515      TracePoint       tracepoint__sys  2951    gadgettracerman               5513  36.453878ms
518      TracePoint       tracepoint__sys  2951    gadgettracerman              65934  32.103604ms

After:

PROGID   TYPE             NAME             PID     COMM                 CUMULRUNCOUNT CUMULRUNTIME
531      RawTracepoint    ig_seccomp_e     233130  gadgettracerman             404341 236.197313ms
532      TracePoint       ig_open_e        233130  gadgettracerman              10314  15.861754ms
533      TracePoint       ig_open_x        233130  gadgettracerman              10315  10.500331ms
534      TracePoint       ig_openat_e      233130  gadgettracerman              11338   3.835068ms
535      TracePoint       ig_openat_x      233130  gadgettracerman              11338   3.210878ms

pkg/gadgets/trace/exec/tracer/arm64_loadlinks.go
@@ -25,12 +25,12 @@ import (
 )
 
 func loadExecsnoopLinks(objs execsnoopObjects) (link.Link, link.Link, error) {
-	enter, err := link.Kprobe("do_execveat_common.isra.0", objs.ExecveatEntry, nil)
+	enter, err := link.Kprobe("do_execveat_common.isra.0", objs.IgExecveatE, nil)
 	if err != nil {
 		return nil, nil, fmt.Errorf("error opening tracepoint: %w", err)
 	}
 
-	exit, err := link.Kretprobe("do_execveat_common.isra.0", objs.ExecveatExit, nil)
+	exit, err := link.Kretprobe("do_execveat_common.isra.0", objs.IgExecveatE, nil)
 	if err != nil {
 		gadgets.CloseLink(enter)
 		return nil, nil, fmt.Errorf("error opening tracepoint: %w", err)

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -40,10 +40,10 @@ static __always_inline bool valid_uid(uid_t uid) {
 
 #ifdef __TARGET_ARCH_arm64
 SEC("kprobe/do_execveat_common.isra.0")
-int BPF_KPROBE(execveat_entry)
+int BPF_KPROBE(ig_execveat_e)
 #else /* !__TARGET_ARCH_arm64 */
 SEC("tracepoint/syscalls/sys_enter_execve")
-int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter* ctx)
+int ig_execve_e(struct trace_event_raw_sys_enter* ctx)
 #endif /* !__TARGET_ARCH_arm64 */
 {
 	u64 id;
@@ -128,10 +128,10 @@ int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter* ctx
 
 #ifdef __TARGET_ARCH_arm64
 SEC("kretprobe/do_execveat_common.isra.0")
-int BPF_KRETPROBE(execveat_exit)
+int BPF_KRETPROBE(ig_execveat_)
 #else /* !__TARGET_ARCH_arm64 */
 SEC("tracepoint/syscalls/sys_exit_execve")
-int tracepoint__syscalls__sys_exit_execve(struct trace_event_raw_sys_exit* ctx)
+int ig_execve_x(struct trace_event_raw_sys_exit* ctx)
 #endif /* !__TARGET_ARCH_arm64 */
 {
 	u64 id;

pkg/gadgets/trace/exec/tracer/execsnoop_bpfel_arm64.go
@@ -68,8 +68,8 @@ type execsnoopSpecs struct {
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type execsnoopProgramSpecs struct {
-	ExecveatEntry *ebpf.ProgramSpec `ebpf:"execveat_entry"`
-	ExecveatExit  *ebpf.ProgramSpec `ebpf:"execveat_exit"`
+	IgExecveat  *ebpf.ProgramSpec `ebpf:"ig_execveat_"`
+	IgExecveatE *ebpf.ProgramSpec `ebpf:"ig_execveat_e"`
 }
 
 // execsnoopMapSpecs contains maps before they are loaded into the kernel.
@@ -117,14 +117,14 @@ func (m *execsnoopMaps) Close() error {
 //
 // It can be passed to loadExecsnoopObjects or ebpf.CollectionSpec.LoadAndAssign.
 type execsnoopPrograms struct {
-	ExecveatEntry *ebpf.Program `ebpf:"execveat_entry"`
-	ExecveatExit  *ebpf.Program `ebpf:"execveat_exit"`
+	IgExecveat  *ebpf.Program `ebpf:"ig_execveat_"`
+	IgExecveatE *ebpf.Program `ebpf:"ig_execveat_e"`
 }
 
 func (p *execsnoopPrograms) Close() error {
 	return _ExecsnoopClose(
-		p.ExecveatEntry,
-		p.ExecveatExit,
+		p.IgExecveat,
+		p.IgExecveatE,
 	)
 }
 

commit 7eef3b0d216f39dadc99b6938c26741347350037

pkg/gadgets: Fix Kprobe name for execsnoop on arm64.
Fixes: 4130c004fead ("Rename ebpf programs")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/trace/exec/tracer/arm64_loadlinks.go
@@ -30,7 +30,7 @@ func loadExecsnoopLinks(objs execsnoopObjects) (link.Link, link.Link, error) {
 		return nil, nil, fmt.Errorf("error opening tracepoint: %w", err)
 	}
 
-	exit, err := link.Kretprobe("do_execveat_common.isra.0", objs.IgExecveatE, nil)
+	exit, err := link.Kretprobe("do_execveat_common.isra.0", objs.IgExecveatX, nil)
 	if err != nil {
 		gadgets.CloseLink(enter)
 		return nil, nil, fmt.Errorf("error opening tracepoint: %w", err)

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -128,7 +128,7 @@ int ig_execve_e(struct trace_event_raw_sys_enter* ctx)
 
 #ifdef __TARGET_ARCH_arm64
 SEC("kretprobe/do_execveat_common.isra.0")
-int BPF_KRETPROBE(ig_execveat_)
+int BPF_KRETPROBE(ig_execveat_x)
 #else /* !__TARGET_ARCH_arm64 */
 SEC("tracepoint/syscalls/sys_exit_execve")
 int ig_execve_x(struct trace_event_raw_sys_exit* ctx)

pkg/gadgets/trace/exec/tracer/execsnoop_bpfel_arm64.go
@@ -68,8 +68,8 @@ type execsnoopSpecs struct {
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type execsnoopProgramSpecs struct {
-	IgExecveat  *ebpf.ProgramSpec `ebpf:"ig_execveat_"`
 	IgExecveatE *ebpf.ProgramSpec `ebpf:"ig_execveat_e"`
+	IgExecveatX *ebpf.ProgramSpec `ebpf:"ig_execveat_x"`
 }
 
 // execsnoopMapSpecs contains maps before they are loaded into the kernel.
@@ -117,14 +117,14 @@ func (m *execsnoopMaps) Close() error {
 //
 // It can be passed to loadExecsnoopObjects or ebpf.CollectionSpec.LoadAndAssign.
 type execsnoopPrograms struct {
-	IgExecveat  *ebpf.Program `ebpf:"ig_execveat_"`
 	IgExecveatE *ebpf.Program `ebpf:"ig_execveat_e"`
+	IgExecveatX *ebpf.Program `ebpf:"ig_execveat_x"`
 }
 
 func (p *execsnoopPrograms) Close() error {
 	return _ExecsnoopClose(
-		p.IgExecveat,
 		p.IgExecveatE,
+		p.IgExecveatX,
 	)
 }
 