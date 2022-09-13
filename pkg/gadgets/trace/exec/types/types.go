d3fbe6629887ff4243460ab6296291b047307a0d
Jose Blanquicet <josebl@microsoft.com>
2022-09-13 10:27:05 +0200

    cmd/common: Use GadgetParser to print trace/exec columns

--- pkg/gadgets/trace/exec/types/types.go
+++ pkg/gadgets/trace/exec/types/types.go
@@ -15,19 +15,40 @@
 package types
 
 import (
+	"fmt"
+
+	"github.com/kinvolk/inspektor-gadget/pkg/columns"
 	eventtypes "github.com/kinvolk/inspektor-gadget/pkg/types"
 )
 
 type Event struct {
 	eventtypes.Event
 
-	Pid       uint32   `json:"pid,omitempty"`
-	Ppid      uint32   `json:"ppid,omitempty"`
-	UID       uint32   `json:"uid,omitempty"`
-	MountNsID uint64   `json:"mountnsid,omitempty"`
-	Retval    int      `json:"ret,omitempty"`
-	Comm      string   `json:"pcomm,omitempty"`
-	Args      []string `json:"args,omitempty"`
+	Pid       uint32   `json:"pid,omitempty" column:"pid,width:7"`
+	Ppid      uint32   `json:"ppid,omitempty" column:"ppid,width:7"`
+	UID       uint32   `json:"uid,omitempty" column:"uid,width:7"`
+	MountNsID uint64   `json:"mountnsid,omitempty" column:"mntns,width:12,hide"`
+	Retval    int      `json:"ret,omitempty" column:"ret,width:4"`
+	Comm      string   `json:"pcomm,omitempty" column:"comm,width:16"`
+	Args      []string `json:"args,omitempty" column:"args,width:24"`
+}
+
+// execColumns is defined as global variable so that the callers panic if they
+// are importing this package with invalid column tags. However, force callers
+// to use MustGetColumns to include additional configuration to the columns.
+var execColumns = columns.MustCreateColumns[Event]()
+
+func MustGetColumns() *columns.Columns[Event] {
+	err := execColumns.SetExtractor("args", func(event *Event) (ret string) {
+		for _, arg := range event.Args {
+			ret += arg + " "
+		}
+		return ret
+	})
+	if err != nil {
+		panic(fmt.Errorf(`setting extractor for "args" column of exec event: %w`, err))
+	}
+	return execColumns
 }
 
 func Base(ev eventtypes.Event) Event {
