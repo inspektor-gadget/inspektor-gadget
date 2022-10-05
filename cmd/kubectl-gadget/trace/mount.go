e8e26066b6f5d2c8e7dad64d239aad767eaa366c
Burak Ok <burakok@microsoft.com>
2022-10-05 10:31:26 +0200

    trace/mount: Use columns library

--- cmd/kubectl-gadget/trace/mount.go
+++ cmd/kubectl-gadget/trace/mount.go
@@ -15,145 +15,34 @@
 package trace
 
 import (
-	"fmt"
-	"strings"
+	"github.com/spf13/cobra"
 
 	commontrace "github.com/kinvolk/inspektor-gadget/cmd/common/trace"
 	commonutils "github.com/kinvolk/inspektor-gadget/cmd/common/utils"
 	"github.com/kinvolk/inspektor-gadget/cmd/kubectl-gadget/utils"
-	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/trace/mount/types"
-
-	"github.com/spf13/cobra"
+	mountTypes "github.com/kinvolk/inspektor-gadget/pkg/gadgets/trace/mount/types"
 )
 
-type MountParser struct {
-	commonutils.BaseParser[types.Event]
-}
-
 func newMountCmd() *cobra.Command {
-	commonFlags := &utils.CommonFlags{
-		OutputConfig: commonutils.OutputConfig{
-			// The columns that will be used in case the user does not specify
-			// which specific columns they want to print.
-			CustomColumns: []string{
-				"node",
-				"namespace",
-				"pod",
-				"container",
-				"comm",
-				"pid",
-				"tid",
-				"mnt_ns",
-				"call",
-			},
-		},
-	}
+	var commonFlags utils.CommonFlags
 
-	cmd := &cobra.Command{
-		Use:   "mount",
-		Short: "Trace mount and umount system calls",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			mountGadget := &TraceGadget[types.Event]{
-				name:        "mountsnoop",
-				commonFlags: commonFlags,
-				parser:      NewMountParser(&commonFlags.OutputConfig),
-			}
-
-			return mountGadget.Run()
-		},
-	}
-
-	utils.AddCommonFlags(cmd, commonFlags)
-
-	return cmd
-}
-
-func NewMountParser(outputConfig *commonutils.OutputConfig) commontrace.TraceParser[types.Event] {
-	columnsWidth := map[string]int{
-		"node":      -16,
-		"namespace": -16,
-		"pod":       -30,
-		"container": -16,
-		"pid":       -7,
-		"tid":       -7,
-		"mnt_ns":    -11,
-		"comm":      -16,
-		"op":        -6,
-		"ret":       -4,
-		"lat":       -8,
-		"fs":        -16,
-		"src":       -16,
-		"target":    -16,
-		"data":      -16,
-		"call":      -16,
-		"flags":     -24,
-	}
-
-	return &MountParser{
-		BaseParser: commonutils.NewBaseWidthParser[types.Event](columnsWidth, outputConfig),
-	}
-}
-
-func getCall(e *types.Event) string {
-	switch e.Operation {
-	case "mount":
-		format := `mount("%s", "%s", "%s", %s, "%s") = %d`
-		return fmt.Sprintf(format, e.Source, e.Target, e.Fs, strings.Join(e.Flags, " | "),
-			e.Data, e.Retval)
-	case "umount":
-		format := `umount("%s", %s) = %d`
-		return fmt.Sprintf(format, e.Target, strings.Join(e.Flags, " | "), e.Retval)
-	}
-
-	return ""
-}
-
-func (p *MountParser) TransformIntoColumns(event *types.Event) string {
-	var sb strings.Builder
-
-	for _, col := range p.OutputConfig.CustomColumns {
-		switch col {
-		case "node":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Node))
-		case "namespace":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Namespace))
-		case "pod":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Pod))
-		case "container":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Container))
-		case "pid":
-			sb.WriteString(fmt.Sprintf("%*d", p.ColumnsWidth[col], event.Pid))
-		case "tid":
-			sb.WriteString(fmt.Sprintf("%*d", p.ColumnsWidth[col], event.Tid))
-		case "mnt_ns":
-			sb.WriteString(fmt.Sprintf("%*d", p.ColumnsWidth[col], event.MountNsID))
-		case "comm":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Comm))
-		case "op":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Operation))
-		case "ret":
-			sb.WriteString(fmt.Sprintf("%*d", p.ColumnsWidth[col], event.Retval))
-		case "lat":
-			sb.WriteString(fmt.Sprintf("%*d", p.ColumnsWidth[col], event.Latency/1000))
-		case "fs":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Fs))
-		case "src":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Source))
-		case "target":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Target))
-		case "data":
-			sb.WriteString(fmt.Sprintf("%*s", p.ColumnsWidth[col], event.Data))
-		case "call":
-			sb.WriteString(fmt.Sprintf("%-*s", p.ColumnsWidth[col], getCall(event)))
-		case "flags":
-			sb.WriteString(fmt.Sprintf("%s", strings.Join(event.Flags, " | ")))
-		default:
-			continue
+	runCmd := func(cmd *cobra.Command, args []string) error {
+		parser, err := commonutils.NewGadgetParserWithK8sInfo(&commonFlags.OutputConfig, mountTypes.GetColumns())
+		if err != nil {
+			return commonutils.WrapInErrParserCreate(err)
 		}
 
-		// Needed when field is larger than the predefined columnsWidth.
-		sb.WriteRune(' ')
+		mountGadget := &TraceGadget[mountTypes.Event]{
+			name:        "mountsnoop",
+			commonFlags: &commonFlags,
+			parser:      parser,
+		}
+
+		return mountGadget.Run()
 	}
 
-	return sb.String()
+	cmd := commontrace.NewMountCmd(runCmd)
+	utils.AddCommonFlags(cmd, &commonFlags)
+
+	return cmd
 }
