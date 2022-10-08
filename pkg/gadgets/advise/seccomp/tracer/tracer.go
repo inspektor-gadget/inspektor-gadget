792b5499c845839f110fbedcc06188828d0b4354
Michael Friese <mfriese@microsoft.com>
2022-10-08 20:13:11 +0200

    pkg/gadgets/advise/seccomp: remove "C" dependency

--- pkg/gadgets/advise/seccomp/tracer/tracer.go
+++ pkg/gadgets/advise/seccomp/tracer/tracer.go
@@ -19,18 +19,23 @@ import (
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET -cc clang seccomp ./bpf/seccomp.c -- -I./bpf/ -I../../../../${TARGET}
 
-// #include "bpf/seccomp-common.h"
-import "C"
-
 const (
 	BPFProgName = "ig_seccomp_e"
 	BPFMapName  = "syscalls_per_mntns"
+)
+
+const (
+	// Please update these values also in bpf/seccomp-common.h
+	syscallsCount              = 500
+	syscallsMapValueFooterSize = 1
+	syscallsMapValueSize       = syscallsCount + syscallsMapValueFooterSize
 )
 
 type Tracer struct {
@@ -60,7 +65,7 @@ func NewTracer() (*Tracer, error) {
 		seccompMap: coll.Maps[BPFMapName],
 	}
 
-	t.seccompMap.Update(uint64(0), [C.SYSCALLS_MAP_VALUE_SIZE]byte{}, ebpf.UpdateAny)
+	t.seccompMap.Update(uint64(0), [syscallsMapValueSize]byte{}, ebpf.UpdateAny)
 
 	tracepointProg, ok := coll.Programs[BPFProgName]
 	if !ok {
@@ -82,20 +87,20 @@ func (t *Tracer) Peek(mntns uint64) []byte {
 	b, err := t.seccompMap.LookupBytes(mntns)
 	if err != nil {
 		log.Errorf("Error while looking up the seccomp map: %s", err)
-		return make([]byte, C.SYSCALLS_COUNT)
+		return make([]byte, syscallsCount)
 	}
 	// LookupBytes does not return an error when the entry is not found, so
 	// we need to test b==nil too
 	if b == nil {
 		// The container just hasn't done any syscall
 		log.Errorf("No syscall found with mntns %d", mntns)
-		return make([]byte, C.SYSCALLS_COUNT)
+		return make([]byte, syscallsCount)
 	}
-	if len(b) < C.SYSCALLS_COUNT {
+	if len(b) < syscallsCount {
 		log.Errorf("Error while looking up the seccomp map: wrong length: %d", len(b))
-		return make([]byte, C.SYSCALLS_COUNT)
+		return make([]byte, syscallsCount)
 	}
-	return b[:C.SYSCALLS_COUNT]
+	return b[:syscallsCount]
 }
 
 func (t *Tracer) Delete(mntns uint64) {
