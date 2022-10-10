2a41a2feed9b5bac121fd3d3a4bf40d129feda98
Francis Laniel <flaniel@linux.microsoft.com>
2022-10-10 10:11:47 +0200

    pkg/gadgets: Add types for traceloop.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

---
+++ pkg/gadgets/traceloop/types/types.go
@@ -0,0 +1,47 @@
+// Copyright 2019-2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+)
+
+type SyscallParam struct {
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type Event struct {
+	eventtypes.Event
+
+	Timestamp  uint64         `json:"timestamp,omitempty"`
+	CPU        uint16         `json:"cpu,omitempty"`
+	Pid        uint64         `json:"pid,omitempty"`
+	Comm       string         `json:"pcomm,omitempty"`
+	Name       string         `json:"name,omitempty"`
+	Parameters []SyscallParam `json:"parameters,omitempty"`
+	Retval     int            `json:"ret,omitempty"`
+	MountNsID  uint64         `json:"mountnsid,omitempty"`
+}
+
+func Base(ev eventtypes.Event) Event {
+	return Event{
+		Event: ev,
+	}
+}
+
+func (e Event) GetBaseEvent() eventtypes.Event {
+	return e.Event
+}
