631d7673fc5d5b8701a8c140a44ac8d851c42056
Jose Blanquicet <josebl@microsoft.com>
2022-11-21 08:27:31 +0100

    cmd/local-gadget: Add seccomp audit gadget

--- pkg/local-gadget-manager/local-gadget-manager_test.go
+++ pkg/local-gadget-manager/local-gadget-manager_test.go
@@ -292,58 +292,6 @@ func TestSeccomp(t *testing.T) {
 	checkFdList(t, initialFdList, checkFdListAttempts, checkFdListInterval)
 }
 
-func TestAuditSeccomp(t *testing.T) {
-	if !*rootTest {
-		t.Skip("skipping test requiring root.")
-	}
-	localGadgetManager, err := NewManager([]*containerutils.RuntimeConfig{{Name: "docker"}})
-	if err != nil {
-		t.Fatalf("Failed to start local gadget manager: %s", err)
-	}
-	defer localGadgetManager.Close()
-
-	initialFdList := currentFdList(t)
-
-	containerName := "test-local-gadget-auditseccomp001"
-	err = localGadgetManager.AddTraceResource("audit-seccomp", "my-tracer", containerName, gadgetv1alpha1.TraceOutputModeStream)
-	if err != nil {
-		t.Fatalf("Failed to create tracer: %s", err)
-	}
-	err = localGadgetManager.ExecTraceResourceOperation("my-tracer", gadgetv1alpha1.OperationStart)
-	if err != nil {
-		t.Fatalf("Failed to start the tracer: %s", err)
-	}
-
-	seccompProfile := `{"defaultAction":"SCMP_ACT_ALLOW","syscalls":[{"action":"SCMP_ACT_LOG","names":["unshare"]}]}`
-	runTestContainer(t, containerName, "docker.io/library/alpine", "unshare -i ; echo OK", seccompProfile)
-
-	stop := make(chan struct{})
-
-	ch, err := localGadgetManager.StreamTraceResourceOutput("my-tracer", stop)
-	if err != nil {
-		t.Fatalf("Failed to get stream: %s", err)
-	}
-	results := <-ch
-	if !strings.Contains(results, `"container":"test-local-gadget-auditseccomp001","type":"normal","syscall":"unshare","code":"log"`) {
-		t.Fatalf("Failed to get correct Seccomp Audit: %s", results)
-	}
-
-	close(stop)
-
-	err = localGadgetManager.DeleteTraceResource("my-tracer")
-	if err != nil {
-		t.Fatalf("Failed to delete tracer: %s", err)
-	}
-
-	s := stacks()
-	keyword := "audit-seccomp"
-	if strings.Contains(s, keyword) {
-		t.Fatalf("Error: stack contains %q:\n%s", keyword, s)
-	}
-
-	checkFdList(t, initialFdList, checkFdListAttempts, checkFdListInterval)
-}
-
 func TestEbpftop(t *testing.T) {
 	if !*rootTest {
 		t.Skip("skipping test requiring root.")
