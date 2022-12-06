93970ff0a4346742d6bf51b08f5ead50b279dbfb
Michael Friese <mfriese@microsoft.com>
2022-12-06 11:59:56 +0100

    pkg/gadgets/top/tcp: remove "C" dependency

--- pkg/gadgets/top/tcp/tracer/bpf/tcptop.h
+++ pkg/gadgets/top/tcp/tracer/bpf/tcptop.h
@@ -3,13 +3,14 @@
 #define __TCPTOP_H
 
 #define TASK_COMM_LEN 16
+#define IPV6_LEN 16
 
 struct ip_key_t {
-	unsigned __int128 saddr;
-	unsigned __int128 daddr;
+	__u8 saddr[IPV6_LEN];
+	__u8 daddr[IPV6_LEN];
 	__u64 mntnsid;
 	__u32 pid;
-	char name[TASK_COMM_LEN];
+	__u8 name[TASK_COMM_LEN];
 	__u16 lport;
 	__u16 dport;
 	__u16 family;
