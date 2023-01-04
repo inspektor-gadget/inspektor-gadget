dfd8051b6ac0049e3f97868f1e142347b1cb6560
Qasim Sarfraz <qasimsarfraz@microsoft.com>
2023-01-04 12:15:50 +0100

    Makefile: Move minikube targets to main makefile

---
+++ minikube.mk
@@ -0,0 +1,52 @@
+MINIKUBE_VERSION ?= v1.27.0
+KUBERNETES_VERSION ?= v1.24.6
+MINIKUBE_DRIVER ?= docker
+
+PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+MINIKUBE_DIR ?= $(PROJECT_DIR)/bin/minikube
+
+CONTAINER_RUNTIME ?= docker
+
+# make does not allow implicit rules (with '%') to be phony so let's use
+# the 'phony_explicit' dependency to make implicit rules inherit the phony
+# attribute
+.PHONY: phony_explicit
+phony_explicit:
+
+# minikube
+
+MINIKUBE = $(MINIKUBE_DIR)/minikube-$(MINIKUBE_VERSION)
+.PHONY: minikube-install
+minikube-install:
+	mkdir -p $(MINIKUBE_DIR)
+	test -e $(MINIKUBE_DIR)/minikube-$(MINIKUBE_VERSION) || \
+	(cd $(MINIKUBE_DIR) && curl -Lo ./minikube-$(MINIKUBE_VERSION) https://github.com/kubernetes/minikube/releases/download/$(MINIKUBE_VERSION)/minikube-linux-$(shell go env GOHOSTARCH))
+	chmod +x $(MINIKUBE_DIR)/minikube-$(MINIKUBE_VERSION)
+
+# clean
+
+.PHONY: minikube-clean
+minikube-clean:
+	$(MINIKUBE) delete -p minikube-docker
+	$(MINIKUBE) delete -p minikube-containerd
+	$(MINIKUBE) delete -p minikube-cri-o
+	rm -rf $(MINIKUBE_DIR)
+
+# start
+
+MINIKUBE_START_TARGETS = \
+	minikube-start-docker \
+	minikube-start-containerd \
+	minikube-start-cri-o
+
+.PHONY: minikube-start-all
+minikube-start-all: $(MINIKUBE_START_TARGETS)
+
+minikube-start: minikube-start-$(CONTAINER_RUNTIME)
+
+.PHONY: phony_explicit
+minikube-start-%: minikube-install
+	$(MINIKUBE) status -p minikube-$* -f {{.APIServer}} >/dev/null || \
+	$(MINIKUBE) start -p minikube-$* --driver=$(MINIKUBE_DRIVER) --kubernetes-version=$(KUBERNETES_VERSION) --container-runtime=$* --wait=all
+	$(MINIKUBE) profile minikube-$*
+
