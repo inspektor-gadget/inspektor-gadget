43a6633f1d067af84ac89b6dbfa3eb1ebe9bc235
Francis Laniel <flaniel@linux.microsoft.com>
2023-01-09 13:12:08 +0100

    integration: Move audit seccomp test to a dedicated file.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- integration/inspektor-gadget/integration_test.go
+++ integration/inspektor-gadget/integration_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 The Inspektor Gadget authors
+// Copyright 2019-2022 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ import (
 	"github.com/cilium/ebpf"
 
 	. "github.com/inspektor-gadget/inspektor-gadget/integration"
-	seccompauditTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/audit/seccomp/types"
 	bioprofileTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/profile/block-io/types"
 	cpuprofileTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/profile/cpu/types"
 	processCollectorTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/snapshot/process/types"
@@ -261,94 +260,6 @@ func testMain(m *testing.M) int {
 
 func TestMain(m *testing.M) {
 	os.Exit(testMain(m))
-}
-
-func TestAuditSeccomp(t *testing.T) {
-	ns := GenerateTestNamespaceName("test-audit-seccomp")
-	spName := "log"
-
-	t.Parallel()
-
-	commands := []*Command{
-		CreateTestNamespaceCommand(ns),
-		{
-			Name: "CreateSeccompProfile",
-			Cmd: fmt.Sprintf(`
-				kubectl apply -f - <<EOF
-apiVersion: security-profiles-operator.x-k8s.io/v1beta1
-kind: SeccompProfile
-metadata:
-  name: %s
-  namespace: %s
-  annotations:
-    description: "Log some syscalls"
-spec:
-  defaultAction: SCMP_ACT_ALLOW
-  syscalls:
-  - action: SCMP_ACT_KILL
-    names:
-    - unshare
-  - action: SCMP_ACT_LOG
-    names:
-    - mkdir
-EOF
-			`, spName, ns),
-			ExpectedRegexp: fmt.Sprintf("seccompprofile.security-profiles-operator.x-k8s.io/%s created", spName),
-		},
-		{
-			Name: "WaitForSeccompProfile",
-			Cmd:  fmt.Sprintf("kubectl wait sp --for condition=ready -n %s %s", ns, spName),
-		},
-		{
-			Name: "RunSeccompAuditTestPod",
-			Cmd: fmt.Sprintf(`
-				kubectl apply -f - <<EOF
-apiVersion: v1
-kind: Pod
-metadata:
-  name: test-pod
-  namespace: %s
-spec:
-  securityContext:
-    seccompProfile:
-      type: Localhost
-      localhostProfile: operator/%s/log.json
-  restartPolicy: Never
-  terminationGracePeriodSeconds: 0
-  containers:
-  - name: test-pod
-    image: busybox
-    command: ["sh"]
-    args: ["-c", "while true; do unshare -i; sleep 1; done"]
-EOF
-			`, ns, ns),
-			ExpectedRegexp: "pod/test-pod created",
-		},
-		WaitUntilTestPodReadyCommand(ns),
-		{
-			Name: "RunAuditSeccompGadget",
-			Cmd:  fmt.Sprintf("$KUBECTL_GADGET audit seccomp -n %s --timeout 15 -o json", ns),
-			ExpectedOutputFn: func(output string) error {
-				expectedEntry := &seccompauditTypes.Event{
-					Event:   BuildBaseEvent(ns),
-					Syscall: "unshare",
-					Code:    "kill_thread",
-					Comm:    "unshare",
-				}
-
-				normalize := func(e *seccompauditTypes.Event) {
-					e.Node = ""
-					e.Pid = 0
-					e.MountNsID = 0
-				}
-
-				return ExpectEntriesToMatch(output, normalize, expectedEntry)
-			},
-		},
-		DeleteTestNamespaceCommand(ns),
-	}
-
-	RunTestSteps(commands, t)
 }
 
 func TestBindsnoop(t *testing.T) {
