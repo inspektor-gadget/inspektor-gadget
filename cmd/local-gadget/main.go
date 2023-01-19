2153fd75295c4d73667fff05d26467b9c97f401b
Michael Friese <mfriese@microsoft.com>
2023-01-19 15:02:32 +0100

    fully migrate to local-gadget

--- cmd/local-gadget/main.go
+++ cmd/local-gadget/main.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 The Inspektor Gadget authors
+// Copyright 2019-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,13 +19,16 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/inspektor-gadget/inspektor-gadget/cmd/local-gadget/advise"
-	"github.com/inspektor-gadget/inspektor-gadget/cmd/local-gadget/audit"
+	"github.com/inspektor-gadget/inspektor-gadget/cmd/common"
 	"github.com/inspektor-gadget/inspektor-gadget/cmd/local-gadget/containers"
-	"github.com/inspektor-gadget/inspektor-gadget/cmd/local-gadget/profile"
-	"github.com/inspektor-gadget/inspektor-gadget/cmd/local-gadget/snapshot"
-	"github.com/inspektor-gadget/inspektor-gadget/cmd/local-gadget/top"
-	"github.com/inspektor-gadget/inspektor-gadget/cmd/local-gadget/trace"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/local"
+
+	// This is a blank include that actually imports all gadgets - needs to be moved into another package; TODO
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-collection"
+
+	// Another blank import for the used operator
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/localmanager"
 )
 
 func main() {
@@ -35,16 +38,14 @@ func main() {
 	}
 
 	rootCmd.AddCommand(
-		advise.NewAdviseCmd(),
-		audit.NewAuditCmd(),
 		containers.NewListContainersCmd(),
-		profile.NewProfileCmd(),
-		snapshot.NewSnapshotCmd(),
-		top.NewTopCmd(),
-		trace.NewTraceCmd(),
-		newTraceloopCmd(),
 		newVersionCmd(),
 	)
+
+	runtime := &local.Runtime{}
+	// columnFilters for local-gadget
+	columnFilters := []columns.ColumnFilter{columns.Or(columns.WithTag("runtime"), columns.WithNoTags())}
+	common.AddCommandsFromRegistry(rootCmd, runtime, columnFilters)
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
