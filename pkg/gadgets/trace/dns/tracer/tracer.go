inspektor-gadget/inspektor-gadget

commit c68ba6a441cfe0e6ad9e81a8e59d71594adf2a1a

Add latency field to DNS trace gadget
Derive DNS latency from the request and response timestamps.
To avoid unbounded memory usage, set a limit on the number of
outstanding requests, dropping old requests to make space if necessary.

DNS requests are tracked in userspace. I tried using BPF_MAP_TYPE_LRU_HASH
in the eBPF program, but discovered that the LRU cache would evict
entries when the program was executed on different CPUs, even when
there were only a few entries in the map. For example, if nslookup
queried for A and AAAA records, the second request executing
on a different CPU could cause the first request to be evicted
from the map. The userspace implementation avoids this issue.

Signed-off-by: Will Daly <will.e.daly@gmail.com>

pkg/gadgets/trace/dns/tracer/tracer.go
@@ -24,6 +24,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/internal/networktracer"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/dns/types"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	"golang.org/x/sys/unix"
 )
 
 //go:generate bash -c "source ./clangosflags.sh; go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cc clang -type event_t dns ./bpf/dns.c -- $CLANG_OS_FLAGS -I./bpf/"
@@ -44,6 +45,34 @@ func NewTracer() (*Tracer, error) {
 		return nil, fmt.Errorf("failed to load asset: %w", err)
 	}
 
+	latencyCalc, err := newDNSLatencyCalculator()
+	if err != nil {
+		return nil, err
+	}
+
+	parseAndEnrichDNSEvent := func(rawSample []byte, netns uint64) (*types.Event, error) {
+		bpfEvent := (*dnsEventT)(unsafe.Pointer(&rawSample[0]))
+		if len(rawSample) < int(unsafe.Sizeof(*bpfEvent)) {
+			return nil, errors.New("invalid sample size")
+		}
+
+		event, err := bpfEventToDNSEvent(bpfEvent)
+		if err != nil {
+			return nil, err
+		}
+
+		// Derive latency from the query/response timestamps.
+		// Filter by packet type (OUTGOING for queries and HOST for responses) to exclude cases where
+		// the packet is forwarded between containers in the host netns.
+		if bpfEvent.Qr == 0 && bpfEvent.PktType == unix.PACKET_OUTGOING {
+			latencyCalc.storeDNSQueryTimestamp(netns, bpfEvent.Id, bpfEvent.Timestamp)
+		} else if bpfEvent.Qr == 1 && bpfEvent.PktType == unix.PACKET_HOST {
+			event.Latency = latencyCalc.calculateDNSResponseLatency(netns, bpfEvent.Id, bpfEvent.Timestamp)
+		}
+
+		return event, nil
+	}
+
 	return &Tracer{
 		Tracer: networktracer.NewTracer(
 			spec,
@@ -51,7 +80,7 @@ func NewTracer() (*Tracer, error) {
 			BPFPerfMapName,
 			BPFSocketAttach,
 			types.Base,
-			parseDNSEvent,
+			parseAndEnrichDNSEvent,
 		),
 	}, nil
 }
@@ -195,17 +224,13 @@ func parseLabelSequence(sample []byte) (ret string) {
 	return ret
 }
 
-func parseDNSEvent(rawSample []byte) (*types.Event, error) {
+func bpfEventToDNSEvent(bpfEvent *dnsEventT) (*types.Event, error) {
 	event := types.Event{
 		Event: eventtypes.Event{
 			Type: eventtypes.NORMAL,
 		},
 	}
 
-	bpfEvent := (*dnsEventT)(unsafe.Pointer(&rawSample[0]))
-	if len(rawSample) < int(unsafe.Sizeof(*bpfEvent)) {
-		return nil, errors.New("invalid sample size")
-	}
 	event.Event.Timestamp = gadgets.WallTimeFromBootTime(bpfEvent.Timestamp)
 
 	event.ID = fmt.Sprintf("%.4x", bpfEvent.Id)
@@ -227,7 +252,7 @@ func parseDNSEvent(rawSample []byte) (*types.Event, error) {
 	}
 
 	// Convert name into a string with dots
-	event.DNSName = parseLabelSequence(rawSample[unsafe.Offsetof(bpfEvent.Name):])
+	event.DNSName = parseLabelSequence(bpfEvent.Name[:])
 
 	// Parse the packet type
 	event.PktType = "UNKNOWN"

pkg/gadgets/internal/networktracer/tracer.go
@@ -107,8 +107,8 @@ type Tracer[Event any] struct {
 	bpfPerfMapName  string
 	bpfSocketAttach int
 
-	baseEvent  func(ev types.Event) *Event
-	parseEvent func([]byte) (*Event, error)
+	baseEvent    func(ev types.Event) *Event
+	processEvent func(rawSample []byte, netns uint64) (*Event, error)
 }
 
 func NewTracer[Event any](
@@ -117,7 +117,7 @@ func NewTracer[Event any](
 	bpfPerfMapName string,
 	bpfSocketAttach int,
 	baseEvent func(ev types.Event) *Event,
-	parseEvent func([]byte) (*Event, error),
+	processEvent func(rawSample []byte, netns uint64) (*Event, error),
 ) *Tracer[Event] {
 	gadgets.FixBpfKtimeGetBootNs(spec.Programs)
 
@@ -128,7 +128,7 @@ func NewTracer[Event any](
 		bpfPerfMapName:  bpfPerfMapName,
 		bpfSocketAttach: bpfSocketAttach,
 		baseEvent:       baseEvent,
-		parseEvent:      parseEvent,
+		processEvent:    processEvent,
 	}
 }
 
@@ -148,7 +148,7 @@ func (t *Tracer[Event]) Attach(pid uint32, eventCallback func(*Event)) error {
 	}
 	t.attachments[netns] = a
 
-	go t.listen(netns, a.perfRd, t.baseEvent, t.parseEvent, eventCallback)
+	go t.listen(netns, a.perfRd, t.baseEvent, t.processEvent, eventCallback)
 
 	return nil
 }
@@ -157,7 +157,7 @@ func (t *Tracer[Event]) listen(
 	netns uint64,
 	rd *perf.Reader,
 	baseEvent func(ev types.Event) *Event,
-	parseEvent func([]byte) (*Event, error),
+	processEvent func(rawSample []byte, netns uint64) (*Event, error),
 	eventCallback func(*Event),
 ) {
 	for {
@@ -178,7 +178,7 @@ func (t *Tracer[Event]) listen(
 			continue
 		}
 
-		event, err := parseEvent(record.RawSample)
+		event, err := processEvent(record.RawSample, netns)
 		if err != nil {
 			eventCallback(baseEvent(types.Err(err.Error())))
 			continue

pkg/gadgets/trace/sni/tracer/tracer.go
@@ -56,7 +56,7 @@ func NewTracer() (*Tracer, error) {
 	}, nil
 }
 
-func parseSNIEvent(sample []byte) (*types.Event, error) {
+func parseSNIEvent(sample []byte, netns uint64) (*types.Event, error) {
 	bpfEvent := (*snisnoopEventT)(unsafe.Pointer(&sample[0]))
 	if len(sample) < int(unsafe.Sizeof(*bpfEvent)) {
 		return nil, errors.New("invalid sample size")

go.mod
@@ -89,6 +89,7 @@ require (
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect

go.sum
@@ -662,6 +662,8 @@ github.com/hashicorp/go.net v0.0.1/go.mod h1:hjKkEWcCURg++eb33jQU7oqQcI9XDCnUzHA
 github.com/hashicorp/golang-lru v0.5.0/go.mod h1:/m3WP610KZHVQ1SGc6re/UDhFvYD7pJ4Ao+sR/qLZy8=
 github.com/hashicorp/golang-lru v0.5.1/go.mod h1:/m3WP610KZHVQ1SGc6re/UDhFvYD7pJ4Ao+sR/qLZy8=
 github.com/hashicorp/golang-lru v0.5.4/go.mod h1:iADmTwqILo4mZ8BN3D2Q6+9jd8WM5uGBxy+E8yxSoD4=
+github.com/hashicorp/golang-lru/v2 v2.0.1 h1:5pv5N1lT1fjLg2VQ5KWc7kmucp2x/kvFOnxuVTqZ6x4=
+github.com/hashicorp/golang-lru/v2 v2.0.1/go.mod h1:QeFd9opnmA6QUJc5vARoKUSoFhyfM2/ZepoAG6RGpeM=
 github.com/hashicorp/hcl v1.0.0 h1:0Anlzjpi4vEasTeNFn2mLJgTSwt0+6sfsiTG8qcWGx4=
 github.com/hashicorp/hcl v1.0.0/go.mod h1:E5yfLk+7swimpb2L/Alb/PJmXilQ/rhwaUYs4T20WEQ=
 github.com/hashicorp/logutils v1.0.0/go.mod h1:QIAnNjmIWmVIIkWDTG1z5v++HQmx9WQRO+LraFDTW64=

integration/inspektor-gadget/trace_dns_test.go
@@ -52,6 +52,7 @@ func TestTraceDns(t *testing.T) {
 					DNSName:    "inspektor-gadget.io.",
 					QType:      "A",
 					Rcode:      "NoError",
+					Latency:    1,
 				},
 				{
 					Event:      BuildBaseEvent(ns),
@@ -71,6 +72,7 @@ func TestTraceDns(t *testing.T) {
 					DNSName:    "inspektor-gadget.io.",
 					QType:      "AAAA",
 					Rcode:      "NoError",
+					Latency:    1,
 				},
 			}
 
@@ -83,6 +85,11 @@ func TestTraceDns(t *testing.T) {
 				e.Timestamp = 0
 				e.Node = ""
 				e.ID = "0000"
+
+				// Latency should be > 0 only for DNS responses.
+				if e.Latency > 0 {
+					e.Latency = 1
+				}
 			}
 
 			return ExpectEntriesToMatch(output, normalize, expectedEntries...)

integration/local-gadget/k8s/trace_dns_test.go
@@ -49,6 +49,7 @@ func TestTraceDns(t *testing.T) {
 					DNSName:    "inspektor-gadget.io.",
 					QType:      "A",
 					Rcode:      "NoError",
+					Latency:    1,
 				},
 				{
 					Event:      BuildBaseEvent(ns),
@@ -66,6 +67,7 @@ func TestTraceDns(t *testing.T) {
 					DNSName:    "inspektor-gadget.io.",
 					QType:      "AAAA",
 					Rcode:      "NoError",
+					Latency:    1,
 				},
 				{
 					Event:      BuildBaseEvent(ns),
@@ -83,6 +85,7 @@ func TestTraceDns(t *testing.T) {
 					DNSName:    "nodomain.inspektor-gadget.io.",
 					QType:      "A",
 					Rcode:      "NXDomain",
+					Latency:    1,
 				},
 			}
 
@@ -94,6 +97,11 @@ func TestTraceDns(t *testing.T) {
 				}
 				e.Timestamp = 0
 				e.ID = ""
+
+				// Latency should be > 0 only for DNS responses.
+				if e.Latency > 0 {
+					e.Latency = 1
+				}
 			}
 
 			return ExpectEntriesToMatch(output, normalize, expectedEntries...)

integration/local-gadget/non-k8s/trace_dns_test.go
@@ -63,6 +63,7 @@ func TestTraceDns(t *testing.T) {
 					DNSName:    "inspektor-gadget.io.",
 					QType:      "A",
 					Rcode:      "NoError",
+					Latency:    1,
 				},
 				{
 					Event: eventtypes.Event{
@@ -94,12 +95,18 @@ func TestTraceDns(t *testing.T) {
 					DNSName:    "inspektor-gadget.io.",
 					QType:      "AAAA",
 					Rcode:      "NoError",
+					Latency:    1,
 				},
 			}
 
 			normalize := func(e *dnsTypes.Event) {
 				e.ID = ""
 				e.Timestamp = 0
+
+				// Latency should be > 0 only for DNS responses.
+				if e.Latency > 0 {
+					e.Latency = 1
+				}
 			}
 
 			return ExpectEntriesToMatch(output, normalize, expectedEntries...)

commit 98bda782603a2e17d0b6cef97a7d641351a6ea43

pkg/gadgets: Use Event timestamp to compute latency in DNS gadget.
The bpf event timestamp can be 0 on older kernels.
As a consequence, the latency computation would not occur and it would always be
0.

So, we rather use the Event timestamp since we have a workaround for it on older
kernel.
It then enables latency computation on older kernels.

Fixes: c68ba6a441cf ("Add latency field to DNS trace gadget").
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/trace/dns/tracer/tracer.go
@@ -65,9 +65,9 @@ func NewTracer() (*Tracer, error) {
 		// Filter by packet type (OUTGOING for queries and HOST for responses) to exclude cases where
 		// the packet is forwarded between containers in the host netns.
 		if bpfEvent.Qr == 0 && bpfEvent.PktType == unix.PACKET_OUTGOING {
-			latencyCalc.storeDNSQueryTimestamp(netns, bpfEvent.Id, bpfEvent.Timestamp)
+			latencyCalc.storeDNSQueryTimestamp(netns, bpfEvent.Id, uint64(event.Event.Timestamp))
 		} else if bpfEvent.Qr == 1 && bpfEvent.PktType == unix.PACKET_HOST {
-			event.Latency = latencyCalc.calculateDNSResponseLatency(netns, bpfEvent.Id, bpfEvent.Timestamp)
+			event.Latency = latencyCalc.calculateDNSResponseLatency(netns, bpfEvent.Id, uint64(event.Event.Timestamp))
 		}
 
 		return event, nil