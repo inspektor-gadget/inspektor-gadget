62f3c0c6b7b9eb009e9b30733fc0e9dd6cf9472e
Michael Friese <mfriese@microsoft.com>
2023-03-03 15:38:59 +0100

    pkg/logger: improve logger interface to allow combination of dedicated and generic loggers
    This saves code when using different logging mechanisms in different places. So if your logger
    for example only has a .Log(severity, format, message) function, you can combine it with a
    dedicated logger (meaning a logger that has dedicated functions for all severities) or the
    other way around.

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -33,7 +33,7 @@ const (
 	TraceLevel = log.TraceLevel
 )
 
-type Logger interface {
+type DedicatedLogger interface {
 	Panic(params ...any)
 	Panicf(fmt string, params ...any)
 	Fatal(params ...any)
@@ -48,7 +48,142 @@ type Logger interface {
 	Debugf(fmt string, params ...any)
 	Trace(params ...any)
 	Tracef(fmt string, params ...any)
+}
+
+type GenericLogger interface {
+	Log(severity Level, params ...any)
+	Logf(severity Level, format string, params ...any)
+}
+
+type GenericLoggerWithLevelSetter interface {
+	GenericLogger
+	LevelGetterSetter
+}
+
+type DedicatedLoggerWithLevelSetter interface {
+	DedicatedLogger
+	LevelGetterSetter
+}
+
+type LevelGetterSetter interface {
 	SetLevel(Level)
+	GetLevel() Level
+}
+
+type Logger interface {
+	DedicatedLogger
+	GenericLogger
+	LevelGetterSetter
+}
+
+func NewFromGenericLogger(logger GenericLoggerWithLevelSetter) Logger {
+	return &StandardDedicatedLogger{GenericLoggerWithLevelSetter: logger}
+}
+
+func NewFromDedicatedLogger(logger DedicatedLoggerWithLevelSetter) Logger {
+	return &StandardGenericLogger{DedicatedLoggerWithLevelSetter: logger}
+}
+
+type StandardDedicatedLogger struct {
+	GenericLoggerWithLevelSetter
+}
+
+func (s *StandardDedicatedLogger) Panic(params ...any) {
+	s.Log(PanicLevel, params...)
+}
+
+func (s *StandardDedicatedLogger) Panicf(fmt string, params ...any) {
+	s.Logf(PanicLevel, fmt, params...)
+}
+
+func (s *StandardDedicatedLogger) Fatal(params ...any) {
+	s.Log(FatalLevel, params...)
+}
+
+func (s *StandardDedicatedLogger) Fatalf(fmt string, params ...any) {
+	s.Logf(FatalLevel, fmt, params...)
+}
+
+func (s *StandardDedicatedLogger) Error(params ...any) {
+	s.Log(ErrorLevel, params...)
+}
+
+func (s *StandardDedicatedLogger) Errorf(fmt string, params ...any) {
+	s.Logf(ErrorLevel, fmt, params...)
+}
+
+func (s *StandardDedicatedLogger) Warn(params ...any) {
+	s.Log(WarnLevel, params...)
+}
+
+func (s *StandardDedicatedLogger) Warnf(fmt string, params ...any) {
+	s.Logf(WarnLevel, fmt, params...)
+}
+
+func (s *StandardDedicatedLogger) Info(params ...any) {
+	s.Log(InfoLevel, params...)
+}
+
+func (s *StandardDedicatedLogger) Infof(fmt string, params ...any) {
+	s.Logf(InfoLevel, fmt, params...)
+}
+
+func (s *StandardDedicatedLogger) Debug(params ...any) {
+	s.Log(DebugLevel, params...)
+}
+
+func (s *StandardDedicatedLogger) Debugf(fmt string, params ...any) {
+	s.Logf(DebugLevel, fmt, params...)
+}
+
+func (s *StandardDedicatedLogger) Trace(params ...any) {
+	s.Log(TraceLevel, params...)
+}
+
+func (s *StandardDedicatedLogger) Tracef(fmt string, params ...any) {
+	s.Logf(TraceLevel, fmt, params...)
+}
+
+type StandardGenericLogger struct {
+	DedicatedLoggerWithLevelSetter
+}
+
+func (s *StandardGenericLogger) Log(severity Level, params ...any) {
+	switch severity {
+	case PanicLevel:
+		s.Panic(params...)
+	case FatalLevel:
+		s.Fatal(params...)
+	case ErrorLevel:
+		s.Error(params...)
+	case WarnLevel:
+		s.Warn(params...)
+	case InfoLevel:
+		s.Info(params...)
+	case DebugLevel:
+		s.Debug(params...)
+	case TraceLevel:
+		s.Trace(params...)
+	}
+}
+
+func (s *StandardGenericLogger) Logf(severity Level, format string, params ...any) {
+	switch severity {
+	case PanicLevel:
+		s.Panicf(format, params...)
+	case FatalLevel:
+		s.Fatalf(format, params...)
+	case ErrorLevel:
+		s.Errorf(format, params...)
+	case WarnLevel:
+		s.Warnf(format, params...)
+	case InfoLevel:
+		s.Infof(format, params...)
+	case DebugLevel:
+		s.Debugf(format, params...)
+	case TraceLevel:
+		s.Tracef(format, params...)
+	}
 }
 
 func DefaultLogger() Logger {
