inspektor-gadget/inspektor-gadget

commit 13cf80d62765870ed868010f3ed5cb79f87f7c0d

cmd: Check ig is run as root.
Otherwise an error message is printed explaining why the tool should be run as
root.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

cmd/ig/main.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -36,6 +37,13 @@ func main() {
 	rootCmd := &cobra.Command{
 		Use:   "ig",
 		Short: "Collection of gadgets for containers",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if os.Geteuid() != 0 {
+				return fmt.Errorf("%s must be run as root to be able to run eBPF programs", os.Args[0])
+			}
+
+			return nil
+		},
 	}
 
 	rootCmd.AddCommand(

commit 201138b0aed87b50ffc2f115fa05dabeb7a8a58c

pkg/runtile: Check local runtime is run as root.
We moved the check regarding ig being run as root from cmd to local runtime.
Indeed, some commands, like version or help, does not required to be run as root
to be executed while the local runtime requires it.

Fixes: 13cf80d62765 ("cmd: Check ig is run as root.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

cmd/ig/main.go
@@ -15,7 +15,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -37,13 +36,6 @@ func main() {
 	rootCmd := &cobra.Command{
 		Use:   "ig",
 		Short: "Collection of gadgets for containers",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if os.Geteuid() != 0 {
-				return fmt.Errorf("%s must be run as root to be able to run eBPF programs", os.Args[0])
-			}
-
-			return nil
-		},
 	}
 
 	rootCmd.AddCommand(

pkg/runtime/local/local.go
@@ -17,6 +17,8 @@ package local
 import (
 	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	gadgetregistry "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-registry"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
@@ -51,6 +53,10 @@ func prepareCatalog() *runtime.Catalog {
 }
 
 func (r *Runtime) Init(globalRuntimeParams *params.Params) error {
+	if os.Geteuid() != 0 {
+		return fmt.Errorf("%s must be run as root to be able to run eBPF programs", filepath.Base(os.Args[0]))
+	}
+
 	return nil
 }
 