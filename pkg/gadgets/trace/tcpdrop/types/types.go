0d2b0e1c74d2a38fc3d5e0dea4c996a1dfecc62b
Alban Crequy <albancrequy@linux.microsoft.com>
2023-04-17 16:31:37 +0200

    tcpdrop: add virtual columns

--- pkg/gadgets/trace/tcpdrop/types/types.go
+++ pkg/gadgets/trace/tcpdrop/types/types.go
@@ -15,6 +15,8 @@
 package types
 
 import (
+	"fmt"
+
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 )
@@ -24,28 +26,30 @@ type Event struct {
 	eventtypes.WithMountNsID
 	eventtypes.WithNetNsID
 
-	Pid       uint32 `json:"pid,omitempty" column:"pid,template:pid"`
-	Comm      string `json:"comm,omitempty" column:"comm,template:comm"`
-	IPVersion int    `json:"ipversion,omitempty" column:"ip,width:2,fixed"`
+	Pid  uint32 `json:"pid,omitempty" column:"pid,template:pid,order:1000"`
+	Comm string `json:"comm,omitempty" column:"comm,template:comm,order:1001"`
 
-	Saddr string `json:"saddr,omitempty" column:"saddr,template:ipaddr"`
-	Daddr string `json:"daddr,omitempty" column:"daddr,template:ipaddr"`
+	IPVersion int `json:"ipversion,omitempty" column:"ip,width:2,fixed,order:1005"`
 
-	Sport    uint16 `json:"sport,omitempty" column:"sport,template:ipport"`
-	Dport    uint16 `json:"dport,omitempty" column:"dport,template:ipport"`
-	State    string `json:"state,omitempty" column:"state,minWidth:9,maxWidth:12"`
-	Tcpflags string `json:"tcpflags,omitempty" column:"tcpflags,minWidth:7,maxWidth:31"`
-	Reason   string `json:"reason,omitempty" column:"reason,minWidth:14,maxWidth:23"`
+	Saddr string `json:"saddr,omitempty" column:"saddr,template:ipaddr,hide,order:2001"`
+	Sport uint16 `json:"sport,omitempty" column:"sport,template:ipport,hide,order:2002"`
+
+	Daddr string `json:"daddr,omitempty" column:"daddr,template:ipaddr,hide,order:3001"`
+	Dport uint16 `json:"dport,omitempty" column:"dport,template:ipport,hide,order:3002"`
+
+	State    string `json:"state,omitempty" column:"state,minWidth:9,maxWidth:12,order:5000"`
+	Tcpflags string `json:"tcpflags,omitempty" column:"tcpflags,minWidth:7,maxWidth:31,order:5001"`
+	Reason   string `json:"reason,omitempty" column:"reason,minWidth:14,maxWidth:23,order:5002"`
 
 	/* Source IP resolved by kubeipresolver  */
-	SrcKind      eventtypes.RemoteKind `json:"srcKind,omitempty" column:"srcKind,maxWidth:5"`
-	SrcNamespace string                `json:"srcNamespace,omitempty" column:"srcns"`
-	SrcName      string                `json:"srcName,omitempty" column:"srcname"`
+	SrcKind      eventtypes.RemoteKind `json:"srcKind,omitempty" column:"srcKind,maxWidth:5,hide,order:2100"`
+	SrcNamespace string                `json:"srcNamespace,omitempty" column:"srcns,hide,order:2101"`
+	SrcName      string                `json:"srcName,omitempty" column:"srcname,hide,order:2102"`
 
 	/* Destination IP resolved by kubeipresolver  */
-	DstKind      eventtypes.RemoteKind `json:"dstKind,omitempty" column:"dstKind,maxWidth:5"`
-	DstNamespace string                `json:"dstNamespace,omitempty" column:"dstns"`
-	DstName      string                `json:"dstName,omitempty" column:"dstname"`
+	DstKind      eventtypes.RemoteKind `json:"dstKind,omitempty" column:"dstKind,maxWidth:5,hide,order:3100"`
+	DstNamespace string                `json:"dstNamespace,omitempty" column:"dstns,hide,order:3101"`
+	DstName      string                `json:"dstName,omitempty" column:"dstname,hide,order:3102"`
 }
 
 func (e *Event) SetLocalPodDetails(owner, hostIP, podIP string, labels map[string]string) {
@@ -70,7 +74,51 @@ func (e *Event) SetEndpointsDetails(endpoints []eventtypes.EndpointDetails) {
 }
 
 func GetColumns() *columns.Columns[Event] {
-	return columns.MustCreateColumns[Event]()
+	cols := columns.MustCreateColumns[Event]()
+
+	// Virtual column for the source and destination endpoints
+	err := cols.AddColumn(columns.Column[Event]{
+		Name: "src",
+		Extractor: func(e *Event) string {
+			switch e.SrcKind {
+			case eventtypes.RemoteKindPod:
+				return "p/" + e.SrcNamespace + "/" + e.SrcName + ":" + fmt.Sprint(e.Sport)
+			case eventtypes.RemoteKindService:
+				return "s/" + e.SrcNamespace + "/" + e.SrcName + ":" + fmt.Sprint(e.Sport)
+			case eventtypes.RemoteKindOther:
+				return "o/" + e.Saddr + ":" + fmt.Sprint(e.Sport)
+			}
+			return e.Saddr + ":" + fmt.Sprint(e.Sport)
+		},
+		Visible: true,
+		Width:   30,
+		Order:   2000,
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = cols.AddColumn(columns.Column[Event]{
+		Name: "dst",
+		Extractor: func(e *Event) string {
+			switch e.DstKind {
+			case eventtypes.RemoteKindPod:
+				return "p/" + e.DstNamespace + "/" + e.DstName + ":" + fmt.Sprint(e.Dport)
+			case eventtypes.RemoteKindService:
+				return "s/" + e.DstNamespace + "/" + e.DstName + ":" + fmt.Sprint(e.Dport)
+			case eventtypes.RemoteKindOther:
+				return "o/" + e.Daddr + ":" + fmt.Sprint(e.Dport)
+			}
+			return e.Daddr + ":" + fmt.Sprint(e.Dport)
+		},
+		Visible: true,
+		Width:   30,
+		Order:   3000,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return cols
 }
 
 func Base(ev eventtypes.Event) *Event {
