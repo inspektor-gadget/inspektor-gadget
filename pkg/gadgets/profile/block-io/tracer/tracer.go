a5f4226100118e2a0b0c6a505f6d2ebef8b34af3
Jose Blanquicet <josebl@microsoft.com>
2023-04-21 14:51:08 +0200

    profile/block-io/tracer: Fix logic to compute interval
    We are missing the logic to make the intervals start from 0, see the
    original code in BCC:
    
    https://github.com/iovisor/bcc/blob/70e879960428f5726067db93f7c742c8b39d4886/libbpf-tools/trace_helpers.c#L943-L944

--- pkg/gadgets/profile/block-io/tracer/tracer.go
+++ pkg/gadgets/profile/block-io/tracer/tracer.go
@@ -75,10 +75,16 @@ func getReport(histMap *ebpf.Map) (types.Report, error) {
 			indexMax = i
 		}
 
+		start := uint64(1) << i
+		end := 2*start - 1
+		if start == 1 {
+			start = 0
+		}
+
 		data = append(data, types.Data{
 			Count:         uint64(val),
-			IntervalStart: (uint64(1) << (i + 1)) >> 1,
-			IntervalEnd:   (uint64(1) << (i + 1)) - 1,
+			IntervalStart: start,
+			IntervalEnd:   end,
 		})
 	}
 
