1070335da3f9661cc8e018eca5580a7862153f5d
Burak Ok <burakok@microsoft.com>
2023-04-24 17:47:10 +0200

    testutils/options: Add ImageTag
    Signed-off-by: Burak Ok <burakok@microsoft.com>

--- pkg/container-utils/testutils/options_test.go
+++ pkg/container-utils/testutils/options_test.go
@@ -13,6 +13,9 @@ func TestContainerOptions(t *testing.T) {
 	if opts.image != DefaultContainerImage {
 		t.Errorf("Expected default container image to be %q", DefaultContainerImage)
 	}
+	if opts.imageTag != DefaultContainerImageTag {
+		t.Errorf("Expected default container image tag to be %q", DefaultContainerImageTag)
+	}
 	if opts.seccompProfile != "" {
 		t.Errorf("Expected default seccompProfile to be empty")
 	}
