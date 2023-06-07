inspektor-gadget/inspektor-gadget

commit e91a93bb2c0a7d05ed6ac2689b89ec7465923dcf

Read bpf iterators from host pid ns
Commands to test:

docker run --privileged -ti --rm \
	-v /:/host \
	-v /sys/kernel/debug:/sys/kernel/debug \
	-v /run:/run \
	-v /sys/fs/bpf:/sys/fs/bpf \
	-v $PWD:/src \
	-e HOST_ROOT=/host \
	ubuntu /src/ig snapshot process -r docker

go run -exec 'sudo unshare -p -f' ./cmd/ig/... snapshot process -r docker
go run -exec 'sudo unshare -p -f' ./cmd/ig/... top ebpf -r docker
go run -exec 'sudo unshare -p -f' ./cmd/ig/... trace dns -r docker

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/utils/bpf-iter-ns/bpf-iter-ns.go
@@ -1,0 +1,186 @@
+// Copyright 2023 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bpfiterns reads a ebpf iterator in a different namespace.
+package bpfiterns
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/cilium/ebpf/link"
+	systemdDbus "github.com/coreos/go-systemd/v22/dbus"
+	_ "github.com/godbus/dbus/v5"
+	"github.com/google/uuid"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
+)
+
+var isHostPidNs bool
+
+func init() {
+	selfFileInfo, err := os.Stat("/proc/self/ns/pid")
+	if err != nil {
+		return
+	}
+	selfStat, ok := selfFileInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return
+	}
+
+	systemdFileInfo, err := os.Stat(fmt.Sprintf("%s/1/ns/pid", host.HostProcFs))
+	if err != nil {
+		return
+	}
+	systemdStat, ok := systemdFileInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return
+	}
+
+	isHostPidNs = selfStat.Ino == systemdStat.Ino
+}
+
+// Read reads the iterator in the host pid namespace.
+// It will test if the current pid namespace is the host pid namespace.
+func Read(iter *link.Iter) ([]byte, error) {
+	if isHostPidNs {
+		return ReadOnCurrentPidNs(iter)
+	} else {
+		return ReadOnHostPidNs(iter)
+	}
+}
+
+// ReadOnCurrentPidNs reads the iterator in the current pid namespace.
+func ReadOnCurrentPidNs(iter *link.Iter) ([]byte, error) {
+	file, err := iter.Open()
+	if err != nil {
+		return nil, fmt.Errorf("open BPF iterator: %w", err)
+	}
+	defer file.Close()
+	buf, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("read BPF iterator: %w", err)
+	}
+	return buf, err
+}
+
+// ReadOnHostPidNs reads the iterator in the host pid namespace.
+// It does so by pinning the iterator in a temporary directory in the host bpffs,
+// and then creating a systemd service that will read the iterator and write it
+// to a temporary pipe. The pipe is then read and returned.
+func ReadOnHostPidNs(iter *link.Iter) ([]byte, error) {
+	selfPidOnHost, err := os.Readlink(filepath.Join(host.HostProcFs, "self"))
+	if err != nil {
+		return nil, fmt.Errorf("readlink /proc/self: %w", err)
+	}
+	if selfPidOnHost == "" {
+		return nil, fmt.Errorf("empty /proc/self symlink")
+	}
+
+	tmpPinDir, err := os.MkdirTemp(host.HostRoot+"/sys/fs/bpf", "ig-iter-")
+	if err != nil {
+		return nil, fmt.Errorf("creating temporary directory in bpffs: %w", err)
+	}
+	pinPath := filepath.Join(tmpPinDir, "iter")
+	err = iter.Pin(pinPath)
+	if err != nil {
+		return nil, fmt.Errorf("pinning iterator: %w", err)
+	}
+	defer os.RemoveAll(tmpPinDir)
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, fmt.Errorf("creating pipe: %w", err)
+	}
+	writerPath := fmt.Sprintf("/proc/%s/fd/%d", selfPidOnHost, w.Fd())
+
+	var buf []byte
+	var errReader error
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		stdoutReader := bufio.NewReader(r)
+		// ReadAll will block until the write-side of the pipe is closed in both processes
+		// (the systemd service and this process)
+		buf, errReader = io.ReadAll(stdoutReader)
+		r.Close()
+		wg.Done()
+	}()
+
+	conn, err := systemdDbus.NewSystemdConnectionContext(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("connecting to systemd: %w", err)
+	}
+	defer conn.Close()
+
+	runID := uuid.New().String()[:8]
+	unitName := fmt.Sprintf("ig-%s.service", runID)
+
+	statusChan := make(chan string, 1)
+	properties := []systemdDbus.Property{
+		systemdDbus.PropDescription("Inspektor Gadget job on host pidns"),
+		// Type=oneshot ensures that StartTransientUnitContext will only return "done" when the job is done
+		systemdDbus.PropType("oneshot"),
+		systemdDbus.PropExecStart([]string{
+			"/bin/sh",
+			"-c",
+			fmt.Sprintf("cat %s > %s", pinPath, writerPath),
+		}, true),
+	}
+
+	_, err = conn.StartTransientUnitContext(context.TODO(),
+		unitName, "fail", properties, statusChan)
+	if err != nil {
+		return nil, fmt.Errorf("starting transient unit: %w", err)
+	}
+	timeout := time.NewTimer(10 * time.Second)
+	defer timeout.Stop()
+
+	select {
+	case s := <-statusChan:
+		close(statusChan)
+
+		// Close writer first: this will unblock the go routine reading from the pipe
+		w.Close()
+		wg.Wait()
+
+		if errReader != nil {
+			return nil, fmt.Errorf("reading from pipe: %w", errReader)
+		}
+
+		// "done" indicates successful execution of a job
+		// See https://pkg.go.dev/github.com/coreos/go-systemd/v22/dbus#Conn.StartUnit
+		if s != "done" {
+			conn.ResetFailedUnitContext(context.TODO(), unitName)
+
+			return nil, fmt.Errorf("creating systemd unit `%s`: got `%s`", unitName, s)
+		}
+	case <-timeout.C:
+		w.Close()
+		wg.Wait()
+
+		conn.ResetFailedUnitContext(context.TODO(), unitName)
+		return nil, errors.New("timeout waiting for systemd to create " + unitName)
+	}
+
+	return buf, nil
+}

go.mod
@@ -37,6 +37,8 @@ require (
 
 require (
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/coreos/go-systemd/v22 v22.5.0
+	github.com/godbus/dbus/v5 v5.1.0
 	github.com/gogo/protobuf v1.3.2
 	github.com/google/go-cmp v0.5.9
 	github.com/hashicorp/golang-lru/v2 v2.0.3

go.sum
@@ -37,6 +37,8 @@ github.com/containerd/nri v0.3.0 h1:2ZM4WImye1ypSnE7COjOvPAiLv84kaPILBDvb1tbDK8=
 github.com/containerd/nri v0.3.0/go.mod h1:Zw9q2lP16sdg0zYybemZ9yTDy8g7fPCIB3KXOGlggXI=
 github.com/containers/common v0.53.0 h1:Ax814cLeX5VXSnkKUdxz762g+27fJj1st4UvKoXmkKs=
 github.com/containers/common v0.53.0/go.mod h1:pABPxJwlTE8oYk9/2BW0e0mumkuhJHIPsABHTGRXN3w=
+github.com/coreos/go-systemd/v22 v22.5.0 h1:RrqgGjYQKalulkV8NGVIfkXQf6YYmOyiJKk8iXXhfZs=
+github.com/coreos/go-systemd/v22 v22.5.0/go.mod h1:Y58oyj3AT4RCenI/lSvhwexgC+NSVTIJ3seZv2GcEnc=
 github.com/cpuguy83/go-md2man/v2 v2.0.2/go.mod h1:tgQtvFlXSQOSOSIRvRPT7W67SCa46tRHOmNcaadrF8o=
 github.com/creack/pty v1.1.9/go.mod h1:oKZEueFk5CKHvIhNR5MUki03XCEU+Q6VDXinZuGJ33E=
 github.com/davecgh/go-spew v1.1.0/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
@@ -95,6 +97,9 @@ github.com/go-openapi/swag v0.22.3 h1:yMBqmnQ0gyZvEb/+KzuWZOXgllrXT4SADYbvDaXHv/
 github.com/go-openapi/swag v0.22.3/go.mod h1:UzaqsxGiab7freDnrUUra0MwWfN/q7tE4j+VcZ0yl14=
 github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0/go.mod h1:fyg7847qk6SyHyPtNmDHnmrv/HOrqktSC+C9fM+CJOE=
 github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 h1:tfuBGBXKqDEevZMzYi5KSi8KkcZtzBcTgAUUtapy0OI=
+github.com/godbus/dbus/v5 v5.0.4/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
+github.com/godbus/dbus/v5 v5.1.0 h1:4KLkAxT3aOY8Li4FRJe/KvhoNFFxo0m6fNuFUO8QJUk=
+github.com/godbus/dbus/v5 v5.1.0/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
 github.com/gogo/protobuf v1.3.2 h1:Ov1cvc58UF3b5XjBnZv7+opcTcQFZebYjWzi34vdm4Q=
 github.com/gogo/protobuf v1.3.2/go.mod h1:P1XiOD3dCwIKUDQYPy72D8LYyHL2YPYrpS2s69NZV8Q=
 github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b/go.mod h1:SBH7ygxi8pfUlaOkMMuAQtPIUF8ecWP5IEl/CR7VP2Q=

pkg/gadgets/internal/socketenricher/tracer.go
@@ -16,7 +16,6 @@ package socketenricher
 
 import (
 	"fmt"
-	"io"
 	"sync"
 	"time"
 
@@ -25,6 +24,7 @@ import (
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/kallsyms"
+	bpfiterns "github.com/inspektor-gadget/inspektor-gadget/pkg/utils/bpf-iter-ns"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET -cc clang socketenricher ./bpf/sockets-map.bpf.c -- -I./bpf/ -I../../../ -I../../../${TARGET}
@@ -144,12 +144,7 @@ func (se *SocketEnricher) start() error {
 	}
 	defer socketsIter.Close()
 
-	file, err := socketsIter.Open()
-	if err != nil {
-		return fmt.Errorf("open BPF iterator: %w", err)
-	}
-	defer file.Close()
-	_, err = io.ReadAll(file)
+	_, err = bpfiterns.Read(socketsIter)
 	if err != nil {
 		return fmt.Errorf("read BPF iterator: %w", err)
 	}
@@ -186,16 +181,10 @@ func (se *SocketEnricher) cleanupDeletedSockets(cleanupIter *link.Iter) {
 }
 
 func (se *SocketEnricher) cleanupDeletedSocketsNow(cleanupIter *link.Iter) error {
-	file, err := cleanupIter.Open()
-	if err != nil {
-		return fmt.Errorf("open BPF iterator: %w", err)
-	}
-	defer file.Close()
-	_, err = io.ReadAll(file)
-	if err != nil {
-		return fmt.Errorf("read BPF iterator: %w", err)
-	}
-	return nil
+	// No need to change pidns for this iterator because cleanupIter is an
+	// iterator on a map, not on tasks.
+	_, err := bpfiterns.ReadOnCurrentPidNs(cleanupIter)
+	return err
 }
 
 func (se *SocketEnricher) Close() {

pkg/gadgets/snapshot/process/tracer/tracer.go
@@ -33,6 +33,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	processcollectortypes "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/snapshot/process/types"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	bpfiterns "github.com/inspektor-gadget/inspektor-gadget/pkg/utils/bpf-iter-ns"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
 )
 
@@ -86,15 +87,14 @@ func runeBPFCollector(config *Config, enricher gadgets.DataEnricherByMntNs) ([]*
 	}
 	defer dumpTaskIter.Close()
 
-	file, err := dumpTaskIter.Open()
+	buf, err := bpfiterns.Read(dumpTaskIter)
 	if err != nil {
-		return nil, fmt.Errorf("opening BPF iterator: %w", err)
+		return nil, fmt.Errorf("reading iterator: %w", err)
 	}
-	defer file.Close()
 
 	events := []*processcollectortypes.Event{}
 
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(strings.NewReader(string(buf)))
 	for scanner.Scan() {
 		var command string
 		var tgid, pid, parentPid int

pkg/gadgets/top/ebpf/piditer/iter.go
@@ -15,17 +15,15 @@
 package piditer
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"unsafe"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
-	"golang.org/x/sys/unix"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/kallsyms"
+	bpfiterns "github.com/inspektor-gadget/inspektor-gadget/pkg/utils/bpf-iter-ns"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET -cc clang -type pid_iter_entry piditer ./bpf/pid_iter.bpf.c -- -I./bpf/ -I../../../../${TARGET}
@@ -82,36 +80,24 @@ func NewTracer() (iter *PidIter, err error) {
 // DumpPids returns an array of PidIterEntry containing information
 // on which pid (and comm) has an open fd to which eBPF Program ID.
 func (p *PidIter) DumpPids() ([]*PidIterEntry, error) {
-	rc, err := p.iter.Open()
+	buf, err := bpfiterns.Read(p.iter)
 	if err != nil {
-		return nil, fmt.Errorf("opening iter: %w", err)
+		return nil, fmt.Errorf("reading iter: %w", err)
 	}
-	defer rc.Close()
+
+	n := len(buf)
+	if n%iterEntrySize != 0 {
+		return nil, fmt.Errorf("invalid format: %d", n)
+	}
 
 	res := make([]*PidIterEntry, 0)
-
-	buf := make([]byte, 4096/iterEntrySize*iterEntrySize)
-	for {
-		n, err := io.ReadFull(rc, buf)
-		if err != nil {
-			if errors.Is(err, unix.EAGAIN) {
-				continue
-			}
-		}
-		if n == 0 {
-			break
-		}
-		if n%iterEntrySize != 0 {
-			return nil, fmt.Errorf("invalid format: %d", n)
-		}
-		for i := 0; i < n/iterEntrySize; i++ {
-			entry := (*piditerPidIterEntry)(unsafe.Pointer(&buf[i*iterEntrySize]))
-			res = append(res, &PidIterEntry{
-				ProgID: entry.Id,
-				Pid:    entry.Pid,
-				Comm:   gadgets.FromCString(entry.Comm[:]),
-			})
-		}
+	for i := 0; i < n/iterEntrySize; i++ {
+		entry := (*piditerPidIterEntry)(unsafe.Pointer(&buf[i*iterEntrySize]))
+		res = append(res, &PidIterEntry{
+			ProgID: entry.Id,
+			Pid:    entry.Pid,
+			Comm:   gadgets.FromCString(entry.Comm[:]),
+		})
 	}
 
 	return res, nil

commit 55f58bbe8dcfcaba6c82ad803bfbeb0bfa00c25f

bpf-iter-ns: fix bpf pin path
Fixes: e91a93bb2c0a ("Read bpf iterators from host pid ns")

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/utils/bpf-iter-ns/bpf-iter-ns.go
@@ -96,16 +96,22 @@ func ReadOnHostPidNs(iter *link.Iter) ([]byte, error) {
 		return nil, fmt.Errorf("empty /proc/self symlink")
 	}
 
-	tmpPinDir, err := os.MkdirTemp(host.HostRoot+"/sys/fs/bpf", "ig-iter-")
+	// Create a temporary directory in the host bpffs
+	bpfFS := "/sys/fs/bpf"
+	tmpPinDir, err := os.MkdirTemp(host.HostRoot+bpfFS, "ig-iter-")
 	if err != nil {
 		return nil, fmt.Errorf("creating temporary directory in bpffs: %w", err)
 	}
+	defer os.RemoveAll(tmpPinDir)
+
+	// Prepare the pin path from the container and host point of view
 	pinPath := filepath.Join(tmpPinDir, "iter")
+	pinPathHost := filepath.Join(bpfFS, filepath.Base(tmpPinDir), "iter")
+
 	err = iter.Pin(pinPath)
 	if err != nil {
 		return nil, fmt.Errorf("pinning iterator: %w", err)
 	}
-	defer os.RemoveAll(tmpPinDir)
 
 	r, w, err := os.Pipe()
 	if err != nil {
@@ -143,7 +149,7 @@ func ReadOnHostPidNs(iter *link.Iter) ([]byte, error) {
 		systemdDbus.PropExecStart([]string{
 			"/bin/sh",
 			"-c",
-			fmt.Sprintf("cat %s > %s", pinPath, writerPath),
+			fmt.Sprintf("cat %s > %s", pinPathHost, writerPath),
 		}, true),
 	}
 