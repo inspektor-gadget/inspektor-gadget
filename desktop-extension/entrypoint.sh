5ba159eeb6493e0ae492ec641ce7117691456a91
Alban Crequy <albancrequy@linux.microsoft.com>
2023-06-20 17:20:00 +0200

    desktop-extension/entrypoint.sh: fix mounts
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

--- desktop-extension/entrypoint.sh
+++ desktop-extension/entrypoint.sh
@@ -21,20 +21,26 @@ if ! readlink /host/proc/self > /dev/null ; then
   mount --bind /proc /host/proc
 fi
 
-BPF_MOUNTPOINT_TYPE="`stat -f -c %T /sys/fs/bpf`"
-if [ "$BPF_MOUNTPOINT_TYPE" != "bpf_fs" ] ; then
+# busybox's "stat -f -c %T" does not know all those filesystems
+# So we have to use %t (hexadecimal codes)
+FS_TYPE_BPF=cafe4a11
+FS_TYPE_DEBUGFS=64626720
+FS_TYPE_TRACEFS=74726163
+
+BPF_MOUNTPOINT_TYPE="`stat -f -c %t /sys/fs/bpf`"
+if [ "$BPF_MOUNTPOINT_TYPE" != "$FS_TYPE_BPF" ] ; then
   echo "/sys/fs/bpf is of type $BPF_MOUNTPOINT_TYPE. Remounting."
   mount -t bpf bpf /sys/fs/bpf/
 fi
 
-DEBUG_MOUNTPOINT_TYPE="`stat -f -c %T /sys/kernel/debug`"
-if [ "$DEBUG_MOUNTPOINT_TYPE" != "debugfs" ] ; then
+DEBUG_MOUNTPOINT_TYPE="`stat -f -c %t /sys/kernel/debug`"
+if [ "$DEBUG_MOUNTPOINT_TYPE" != "$FS_TYPE_DEBUGFS" ] ; then
   echo "/sys/kernel/debug is of type $DEBUG_MOUNTPOINT_TYPE. Remounting."
   mount -t debugfs debugfs /sys/kernel/debug
 fi
 
-TRACE_MOUNTPOINT_TYPE="`stat -f -c %T /sys/kernel/tracing`"
-if [ "$TRACE_MOUNTPOINT_TYPE" != "tracefs" ] ; then
+TRACE_MOUNTPOINT_TYPE="`stat -f -c %t /sys/kernel/tracing`"
+if [ "$TRACE_MOUNTPOINT_TYPE" != "$FS_TYPE_TRACEFS" ] ; then
   echo "/sys/kernel/tracing is of type $TRACE_MOUNTPOINT_TYPE. Remounting."
   mount -t tracefs tracefs /sys/kernel/tracing
 fi
@@ -42,6 +48,13 @@ fi
 echo "Creating directory for $IG_DESKTOP_EXTENSION_SOCKET..."
 IG_DESKTOP_EXTENSION_SOCKET_DIR=$(dirname $IG_DESKTOP_EXTENSION_SOCKET)
 mkdir -p $IG_DESKTOP_EXTENSION_SOCKET_DIR
+
+export LIFECYCLE_SERVER_ROOT="/proc/$(pidof -s lifecycle-server)/root/"
+if [ -f "$LIFECYCLE_SERVER_ROOT/usr/bin/runc" ] ; then
+  echo "runc found in lifecycle-server's root. Switching HOST_ROOT."
+  echo "export HOST_ROOT=\"$LIFECYCLE_SERVER_ROOT\""
+  export HOST_ROOT="$LIFECYCLE_SERVER_ROOT"
+fi
 
 echo "Starting ig-service..."
 exec /bin/ig-service -socket $IG_DESKTOP_EXTENSION_SOCKET
