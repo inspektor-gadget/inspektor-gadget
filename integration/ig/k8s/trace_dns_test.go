1105db565c9b01005103847b8631517947d3ed45
Francis Laniel <flaniel@linux.microsoft.com>
2023-06-26 18:59:45 +0200

    integration/ig: Add TestTraceDnsHost.
    This tests the --host flag for trace dns.
    
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- integration/ig/k8s/trace_dns_test.go
+++ integration/ig/k8s/trace_dns_test.go
@@ -21,6 +21,7 @@ import (
 
 	. "github.com/inspektor-gadget/inspektor-gadget/integration"
 	dnsTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/dns/types"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 )
 
 func TestTraceDns(t *testing.T) {
@@ -159,6 +160,100 @@ func TestTraceDns(t *testing.T) {
 		SleepForSecondsCommand(2), // wait to ensure ig has started
 		BusyboxPodRepeatCommand(ns, strings.Join(nslookupCmds, " ; ")),
 		WaitUntilTestPodReadyCommand(ns),
+		DeleteTestNamespaceCommand(ns),
+	}
+
+	RunTestSteps(commands, t, WithCbBeforeCleanup(PrintLogsFn(ns)))
+}
+
+func TestTraceDnsHost(t *testing.T) {
+	t.Parallel()
+	ns := GenerateTestNamespaceName("test-trace-dns")
+
+	commandsPreTest := []*Command{
+		CreateTestNamespaceCommand(ns),
+		PodCommand("dnstester", *dnsTesterImage, ns, "", ""),
+		WaitUntilPodReadyCommand(ns, "dnstester"),
+	}
+
+	RunTestSteps(commandsPreTest, t)
+	dnsServer, err := GetTestPodIP(ns, "dnstester")
+	if err != nil {
+		t.Fatalf("failed to get pod ip: %v", err)
+	}
+
+	traceDNSCmd := &Command{
+		Name:         "TraceDnsHost",
+		Cmd:          "ig trace dns -o json --host",
+		StartAndStop: true,
+		ExpectedOutputFn: func(output string) error {
+			expectedEntries := []*dnsTypes.Event{
+				{
+					Event: eventtypes.Event{
+						Type: eventtypes.NORMAL,
+					},
+					// nslookup has several threads and isc-worker0000 will do the DNS
+					// request.
+					Comm:       "isc-worker0000",
+					Qr:         dnsTypes.DNSPktTypeQuery,
+					Nameserver: dnsServer,
+					PktType:    "OUTGOING",
+					DNSName:    "fake.test.com.",
+					QType:      "A",
+				},
+				{
+					Event: eventtypes.Event{
+						Type: eventtypes.NORMAL,
+					},
+					Comm:       "isc-worker0000",
+					Qr:         dnsTypes.DNSPktTypeQuery,
+					Nameserver: dnsServer,
+					PktType:    "OUTGOING",
+					DNSName:    "fake.test.com.",
+					QType:      "AAAA",
+				},
+				{
+					Event: eventtypes.Event{
+						Type: eventtypes.NORMAL,
+					},
+					Comm:       "isc-worker0000",
+					Qr:         dnsTypes.DNSPktTypeQuery,
+					Nameserver: dnsServer,
+					PktType:    "OUTGOING",
+					DNSName:    "fake.test.com.",
+					QType:      "A",
+				},
+			}
+
+			normalize := func(e *dnsTypes.Event) {
+				e.Timestamp = 0
+				e.ID = ""
+				e.MountNsID = 0
+				e.NetNsID = 0
+				e.Pid = 0
+				e.Tid = 0
+
+				// Latency should be > 0 only for DNS responses.
+				if e.Latency > 0 {
+					e.Latency = 1
+				}
+			}
+
+			return ExpectEntriesToMatch(output, normalize, expectedEntries...)
+		},
+	}
+
+	cmd := fmt.Sprintf(`sh -c 'for i in $(seq 1 30); do nslookup -type=a fake.test.com. %s; nslookup -type=aaaa fake.test.com. %s; done'`, dnsServer, dnsServer)
+
+	commands := []*Command{
+		CreateTestNamespaceCommand(ns),
+		traceDNSCmd,
+		SleepForSecondsCommand(2), // wait to ensure ig has started
+		{
+			Name:           cmd,
+			Cmd:            cmd,
+			ExpectedRegexp: dnsServer,
+		},
 		DeleteTestNamespaceCommand(ns),
 	}
 
