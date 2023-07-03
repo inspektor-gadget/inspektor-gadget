inspektor-gadget/inspektor-gadget

commit ab918bf49c60d2e1ac3625053a742fa92c53c4f8

pkg/params: Add Validator for IP param type.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/params/validators.go
@@ -16,6 +16,7 @@ package params
 
 import (
 	"fmt"
+	"net"
 	"strconv"
 	"strings"
 	"time"
@@ -38,6 +39,7 @@ const (
 	TypeUint32   TypeHint = "uint32"
 	TypeUint64   TypeHint = "uint64"
 	TypeDuration TypeHint = "duration"
+	TypeIP       TypeHint = "ip"
 )
 
 var typeHintValidators = map[TypeHint]ParamValidator{
@@ -53,6 +55,7 @@ var typeHintValidators = map[TypeHint]ParamValidator{
 	TypeUint32:   ValidateUint(32),
 	TypeUint64:   ValidateUint(64),
 	TypeDuration: ValidateDuration,
+	TypeIP:       ValidateIP,
 }
 
 type ValueHint string
@@ -131,4 +134,11 @@ func ValidateSlice(validator ParamValidator) func(value string) error {
 func ValidateDuration(value string) error {
 	_, err := time.ParseDuration(value)
 	return err
+}
+
+func ValidateIP(value string) error {
+	if net.ParseIP(value) == nil {
+		return fmt.Errorf("%q is not a valid IP address", value)
+	}
+	return nil
 }

pkg/params/validator_test.go
@@ -452,3 +452,36 @@ func TestValidateDuration(t *testing.T) {
 		ValidateDuration,
 	)
 }
+
+func TestValidateIP(t *testing.T) {
+	testValidate(t,
+		[]validateTest{
+			{
+				name:          "IPv4_no_error",
+				value:         "127.0.0.1",
+				expectedError: false,
+			},
+			{
+				name:          "IPv6_no_error",
+				value:         "::1",
+				expectedError: false,
+			},
+			{
+				name:          "empty_error",
+				value:         "",
+				expectedError: true,
+			},
+			{
+				name:          "bad_input_0",
+				value:         "-",
+				expectedError: true,
+			},
+			{
+				name:          "bad_input_1",
+				value:         "foo",
+				expectedError: true,
+			},
+		},
+		ValidateIP,
+	)
+}

commit e059ff56c43340cdb1b5f6577f1a74aeff0d408c

pkg/params: Permits having empty string as IP.
Fixes: ab918bf49c60 ("pkg/params: Add Validator for IP param type.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/params/validators.go
@@ -137,7 +137,7 @@ func ValidateDuration(value string) error {
 }
 
 func ValidateIP(value string) error {
-	if net.ParseIP(value) == nil {
+	if value != "" && net.ParseIP(value) == nil {
 		return fmt.Errorf("%q is not a valid IP address", value)
 	}
 	return nil

pkg/params/validator_test.go
@@ -467,9 +467,9 @@ func TestValidateIP(t *testing.T) {
 				expectedError: false,
 			},
 			{
-				name:          "empty_error",
+				name:          "empty_no_error",
 				value:         "",
-				expectedError: true,
+				expectedError: false,
 			},
 			{
 				name:          "bad_input_0",