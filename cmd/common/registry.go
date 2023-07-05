inspektor-gadget/inspektor-gadget

commit 8511d3b25dd724086358e0683eb44e9fe020a81a

gadgets: Add custom formatting options
Currently we convert and show events to the users based on the parser
and type the gadget defines. This approach doesn't work well for
containerized gadgets as in those cases we don't know the event
beforehand. This commit extends the gadget interface to allow a gadget
to define a custom parser as well custom converting options for json,
jsonpretty and yaml.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

cmd/common/registry.go
@@ -193,9 +193,6 @@ func buildCommandFromGadget(
 
 	// Instantiate parser - this is important to do, because we might apply filters and such to this instance
 	parser := gadgetDesc.Parser()
-	if parser != nil && columnFilters != nil {
-		parser.SetColumnFilters(columnFilters...)
-	}
 
 	// Instantiate runtime params
 	runtimeParams := runtime.ParamDescs().ToParams()
@@ -210,10 +207,37 @@ func buildCommandFromGadget(
 	//  Example use case: setting default namespace for kubernetes
 
 	cmd := &cobra.Command{
-		Use:          gadgetDesc.Name(),
-		Short:        gadgetDesc.Description(),
-		SilenceUsage: true, // do not print usage when there is an error
+		Use:                gadgetDesc.Name(),
+		Short:              gadgetDesc.Description(),
+		SilenceUsage:       true, // do not print usage when there is an error
+		DisableFlagParsing: true,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			cmd.DisableFlagParsing = false
+			return cmd.ParseFlags(args)
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if c, ok := gadgetDesc.(gadgets.GadgetDescCustomParser); ok {
+				var err error
+				parser, err = c.CustomParser(gadgetParams, cmd.Flags().Args())
+				if err != nil {
+					return fmt.Errorf("calling custom parser: %w", err)
+				}
+			}
+
+			if parser != nil {
+				if columnFilters != nil {
+					parser.SetColumnFilters(columnFilters...)
+				}
+
+				outputFormats.Append(buildColumnsOutputFormat(gadgetParams, parser))
+				outputFormatsHelp := buildOutputFormatsHelp(outputFormats)
+				cmd.Flags().Lookup("output").Usage = strings.Join(outputFormatsHelp, "\n") + "\n\n"
+				cmd.Flags().Lookup("output").DefValue = "columns"
+				if showHelp, _ := cmd.Flags().GetBool("help"); showHelp {
+					return cmd.Help()
+				}
+			}
+
 			err := runtime.Init(runtimeGlobalParams)
 			if err != nil {
 				return fmt.Errorf("initializing runtime: %w", err)
@@ -444,11 +468,23 @@ func buildCommandFromGadget(
 				fe.Output(formatter.FormatHeader())
 				parser.SetEventCallback(formatter.EventHandlerFuncArray())
 			case OutputModeJSON:
-				parser.SetEventCallback(printEventAsJSONFn(fe))
+				jsonCallback := printEventAsJSONFn(fe)
+				if cjson, ok := gadgetDesc.(gadgets.GadgetJSONConverter); ok {
+					jsonCallback = cjson.JSONConverter(gadgetParams, fe)
+				}
+				parser.SetEventCallback(jsonCallback)
 			case OutputModeJSONPretty:
-				parser.SetEventCallback(printEventAsJSONPrettyFn(fe))
+				jsonPrettyCallback := printEventAsJSONFn(fe)
+				if cjson, ok := gadgetDesc.(gadgets.GadgetJSONPrettyConverter); ok {
+					jsonPrettyCallback = cjson.JSONPrettyConverter(gadgetParams, fe)
+				}
+				parser.SetEventCallback(jsonPrettyCallback)
 			case OutputModeYAML:
-				parser.SetEventCallback(printEventAsYAMLFn(fe))
+				yamlCallback := printEventAsYAMLFn(fe)
+				if cyaml, ok := gadgetDesc.(gadgets.GadgetYAMLConverter); ok {
+					yamlCallback = cyaml.YAMLConverter(gadgetParams, fe)
+				}
+				parser.SetEventCallback(yamlCallback)
 			}
 
 			// Gadgets with parser don't return anything, they provide the
@@ -494,6 +530,13 @@ func buildCommandFromGadget(
 
 	// Add parser output flags
 	if parser != nil {
+		outputFormats.Append(buildColumnsOutputFormat(gadgetParams, parser))
+	}
+	_, hasCustomParser := gadgetDesc.(gadgets.GadgetDescCustomParser)
+
+	if parser != nil || hasCustomParser {
+		defaultOutputFormat = "columns"
+
 		cmd.PersistentFlags().StringSliceVarP(
 			&filters,
 			"filter", "F",
@@ -510,9 +553,6 @@ func buildCommandFromGadget(
                              see [https://github.com/google/re2/wiki/Syntax] for more information on the syntax
 `,
 		)
-
-		defaultOutputFormat = "columns"
-		outputFormats.Append(buildColumnsOutputFormat(gadgetParams, parser))
 	}
 
 	// Add alternative output formats available in the gadgets

pkg/columns/columns.go
@@ -614,8 +614,7 @@ func GetFieldAsStringExt[T any](column ColumnInternals, floatFormat byte, floatP
 				fieldStart := unsafe.Add(entryStart, column.getOffset())
 
 				for i := 0; i < int(l); i++ {
-					b := (*(*byte)(unsafe.Pointer(fieldStart)))
-					fieldStart = unsafe.Add(fieldStart, 1)
+					b := *(*byte)(unsafe.Add(fieldStart, i))
 					if b == 0 {
 						break
 					}

pkg/gadget-service/service.go
@@ -126,6 +126,14 @@ func (s *Service) RunGadget(runGadget pb.GadgetManager_RunGadgetServer) error {
 		return fmt.Errorf("setting parameters: %w", err)
 	}
 
+	if c, ok := gadgetDesc.(gadgets.GadgetDescCustomParser); ok {
+		var err error
+		parser, err = c.CustomParser(gadgetParams, request.Args)
+		if err != nil {
+			return fmt.Errorf("calling custom parser: %w", err)
+		}
+	}
+
 	// Create payload buffer
 	outputBuffer := make(chan *pb.GadgetEvent, 1024) // TODO: Discuss 1024
 

pkg/gadgets/interface.go
@@ -15,6 +15,7 @@
 package gadgets
 
 import (
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/parser"
 )
@@ -102,6 +103,30 @@ func (of OutputFormats) Append(other OutputFormats) {
 // an entry in the supportedFormats map
 type GadgetOutputFormats interface {
 	OutputFormats() (supportedFormats OutputFormats, defaultFormatKey string)
+}
+
+// GadgetDescCustomParser can be implemented by gadgets that want to provide a custom parser
+// dependent on the parameters and arguments.
+type GadgetDescCustomParser interface {
+	CustomParser(*params.Params, []string) (parser.Parser, error)
+}
+
+// Printer is implemented by objects that can print information, like frontends.
+type Printer interface {
+	Output(payload string)
+	Logf(severity logger.Level, fmt string, params ...any)
+}
+
+type GadgetJSONConverter interface {
+	JSONConverter(params *params.Params, p Printer) func(ev any)
+}
+
+type GadgetJSONPrettyConverter interface {
+	JSONPrettyConverter(params *params.Params, p Printer) func(ev any)
+}
+
+type GadgetYAMLConverter interface {
+	YAMLConverter(params *params.Params, p Printer) func(ev any)
 }
 
 type EventHandlerSetter interface {

commit 8d52fd68fc9b0bf2fbbe6ce055bb7de251c58767

cmd: Check --help was given in RunE.
Setting DisableFlagParsing caused issues where --help would not be taken into
account:
$ sudo ./ig profile block-io --help
INFO[0000] Running. Press Ctrl + C to finish

To fix this, we need to check that --help was given in RunE and call the
corresponding Help().

Fixes: 8511d3b25dd7 ("gadgets: Add custom formatting options")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
Suggested-by: Michael Friese <mfriese@microsoft.com>

cmd/common/registry.go
@@ -216,6 +216,10 @@ func buildCommandFromGadget(
 			return cmd.ParseFlags(args)
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if showHelp, _ := cmd.Flags().GetBool("help"); showHelp {
+				return cmd.Help()
+			}
+
 			if c, ok := gadgetDesc.(gadgets.GadgetDescCustomParser); ok {
 				var err error
 				parser, err = c.CustomParser(gadgetParams, cmd.Flags().Args())