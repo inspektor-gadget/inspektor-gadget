inspektor-gadget/inspektor-gadget

commit 2ecc9fa183aa8d0c4e30409ec47866b4c66eae2d

Removing libseccomp installations and CGO usage
Signed-off-by: Amit Schendel <amits@armosec.io>

Makefile
@@ -132,7 +132,7 @@ kubectl-gadget: kubectl-gadget-$(GOHOSTOS)-$(GOHOSTARCH)
 	cp kubectl-gadget-$(GOHOSTOS)-$(GOHOSTARCH) kubectl-gadget
 
 kubectl-gadget-%: phony_explicit
-	export GO111MODULE=on CGO_ENABLED=0 && \
+	export GO111MODULE=on && \
 	export GOOS=$(shell echo $* |cut -f1 -d-) GOARCH=$(shell echo $* |cut -f2 -d-) && \
 	go build -ldflags $(LDFLAGS) \
 		-tags withoutebpf \

gadget-container/Makefile
@@ -9,7 +9,7 @@ TARGET_ARCH ?= $(shell go env GOHOSTARCH)
 .PHONY: gadgettracermanager
 gadgettracermanager:
 	mkdir -p bin
-	GO111MODULE=on CGO_ENABLED=1 GOOS=linux GOARCH=$(TARGET_ARCH) go build \
+	GO111MODULE=on GOOS=linux GOARCH=$(TARGET_ARCH) go build \
 		-o bin/gadgettracermanager \
 		./gadgettracermanager/
 
@@ -18,13 +18,13 @@ gadgettracermanager:
 .PHONY: ocihookgadget
 ocihookgadget:
 	mkdir -p bin
-	GO111MODULE=on CGO_ENABLED=1 GOOS=linux GOARCH=$(TARGET_ARCH) go build \
+	GO111MODULE=on GOOS=linux GOARCH=$(TARGET_ARCH) go build \
 		-o bin/ocihookgadget \
 		./hooks/oci/main.go
 
 .PHONY: nrigadget
 nrigadget:
 	mkdir -p bin
-	GO111MODULE=on CGO_ENABLED=1 GOOS=linux GOARCH=$(TARGET_ARCH) go build \
+	GO111MODULE=on GOOS=linux GOARCH=$(TARGET_ARCH) go build \
 		-o bin/nrigadget \
 		./hooks/nri/main.go

.github/actions/install-debian-packages/action.yml
@@ -1,33 +1,0 @@
-name: "Install debian packages"
-description: "Install debian packages needed by inspektor-gadget"
-
-inputs:
-  libseccomp-version:
-    description: "Version of the libseccomp to install."
-    default: "2.5.3-2ubuntu2"
-
-runs:
-  using: "composite"
-  steps:
-    - name: Cache debian packages
-      uses: actions/cache@v3.0.11
-      id: cache-debs
-      with:
-        path: "~/cache-debs"
-        # Any changes to this file or main workflow file will invalidate the
-        # cache.
-        key: ${{ hashFiles('.github/actions/install-debian-packages/action.yml', '.github/workflows/inspektor-gadget.yml') }}
-    - name: Install debian packages
-      shell: bash
-      run: |
-        if [[ "${{steps.cache-debs.outputs.cache-hit}}" == 'true' ]]; then
-          sudo cp --verbose --force --recursive ~/cache-debs/* /
-        else
-          sudo apt-get update
-          sudo apt-get install -y libseccomp-dev="${{inputs.libseccomp-version}}"
-          mkdir -p ~/cache-debs
-          sudo dpkg -L libseccomp-dev | \
-              while IFS= read -r f; do \
-                  if test -f $f; then echo $f; fi; \
-              done | xargs cp --parents --target-directory ~/cache-debs/
-        fi

.github/workflows/inspektor-gadget.yml
@@ -38,8 +38,6 @@ jobs:
         go-version: ${{ env.GO_VERSION }}
         cache: true
       id: go
-    - name: Install debian packages
-      uses: ./.github/actions/install-debian-packages
     - name: Check if generated files are updated
       run: |
         make manifests generate generate-documentation
@@ -97,8 +95,6 @@ jobs:
       with:
         go-version: ${{ env.GO_VERSION }}
       id: go
-    - name: Install debian packages
-      uses: ./.github/actions/install-debian-packages
     - name: Lint
       uses: golangci/golangci-lint-action@v3.7.0
       with:
@@ -184,8 +180,6 @@ jobs:
         go-version: ${{ env.GO_VERSION }}
         cache: true
       id: go
-    - name: Install debian packages
-      uses: ./.github/actions/install-debian-packages
     - name: Set container repository and determine image tag
       id: set-repo-determine-image-tag
       uses: ./.github/actions/set-container-repo-and-determine-image-tag
@@ -242,10 +236,6 @@ jobs:
         go-version: ${{ env.GO_VERSION }}
         cache: true
       id: go
-    - name: Install debian packages
-      # ALERT This action must be run after code was checkout otherwise it will
-      # not find this file.
-      uses: ./.github/actions/install-debian-packages
     - name: Build ${{ matrix.ig-target }}
       run: |
         if [ "${{ matrix.ig-target}}" = 'ig-linux-arm64' ]; then
@@ -742,8 +732,6 @@ jobs:
         go-version: ${{ env.GO_VERSION }}
         cache: true
       id: go
-    - name: Install debian packages
-      uses: ./.github/actions/install-debian-packages
     - name: Basic unit tests
       run: |
         make test
@@ -766,8 +754,6 @@ jobs:
         go-version: ${{ env.GO_VERSION }}
         cache: true
       id: go
-    - name: Install debian packages
-      uses: ./.github/actions/install-debian-packages
     - name: Show repository setup
       run: |
         echo "github.event_name: ${{ github.event_name }}"
@@ -870,8 +856,6 @@ jobs:
         go-version: ${{ env.GO_VERSION }}
         cache: true
       id: go
-    - name: Install debian packages
-      uses: ./.github/actions/install-debian-packages
     - name: Unit tests for ig (as root)
       run: |
         KERNEL=$(uname -r)

Dockerfiles/gadget-bcc.Dockerfile
@@ -24,7 +24,7 @@ RUN set -ex; \
 	dpkg --add-architecture ${TARGETARCH} && \
 	apt-get update && \
 	apt-get install -y gcc make ca-certificates git libelf-dev:${TARGETARCH} \
-		pkg-config:${TARGETARCH} libseccomp-dev:${TARGETARCH} && \
+		pkg-config:${TARGETARCH} && \
 	if [ "${TARGETARCH}" != "${BUILDARCH}" ]; then \
 		if [ ${TARGETARCH} = 'arm64' ]; then \
 				apt-get install -y gcc-aarch64-linux-gnu; \

Dockerfiles/gadget-default.Dockerfile
@@ -25,7 +25,7 @@ RUN set -ex; \
 	dpkg --add-architecture ${TARGETARCH} && \
 	apt-get update && \
 	apt-get install -y gcc make ca-certificates git libelf-dev:${TARGETARCH} \
-		pkg-config:${TARGETARCH} libseccomp-dev:${TARGETARCH} && \
+		pkg-config:${TARGETARCH} && \
 	if [ "${TARGETARCH}" != "${BUILDARCH}" ]; then \
 		if [ ${TARGETARCH} = 'arm64' ]; then \
 			apt-get install -y gcc-aarch64-linux-gnu; \
@@ -71,16 +71,16 @@ LABEL org.opencontainers.image.licenses=Apache-2.0
 # available on the base image
 RUN set -ex; \
 	if command -v tdnf; then \
-		tdnf install -y libseccomp wget util-linux socat; \
+		tdnf install -y wget util-linux socat; \
 	elif command -v yum; then \
-		yum install -y libseccomp wget util-linux socat; \
+		yum install -y wget util-linux socat; \
 	elif command -v apt-get; then \
 		apt-get update && \
-		apt-get install -y seccomp wget util-linux socat && \
+		apt-get install -y wget util-linux socat && \
 		apt-get clean && \
 		rm -rf /var/lib/apt/lists/*; \
 	elif command -v apk; then \
-		apk add gcompat libseccomp wget util-linux socat; \
+		apk add gcompat wget util-linux socat; \
 	fi && \
 	(rmdir /usr/src || true) && ln -sf /host/usr/src /usr/src && \
 	rm -f /etc/localtime && ln -sf /host/etc/localtime /etc/localtime

Dockerfiles/ig-tests.Dockerfile
@@ -10,7 +10,7 @@ RUN set -ex; \
 	export DEBIAN_FRONTEND=noninteractive; \
 	dpkg --add-architecture ${TARGETARCH} && \
 	apt-get update && \
-	apt-get install -y gcc build-essential libseccomp-dev:${TARGETARCH} && \
+	apt-get install -y gcc build-essential && \
 	if [ "${TARGETARCH}" != "${BUILDARCH}" ]; then \
 		if [ ${TARGETARCH} = 'arm64' ]; then \
 			apt-get install -y gcc-aarch64-linux-gnu crossbuild-essential-arm64; \

Dockerfiles/ig.Dockerfile
@@ -12,7 +12,7 @@ ENV VERSION=${VERSION}
 RUN \
 	dpkg --add-architecture ${TARGETARCH} && \
 	apt-get update && \
-	apt-get install -y build-essential libseccomp2:${TARGETARCH} libseccomp-dev:${TARGETARCH} && \
+	apt-get install -y build-essential && \
 	if [ "${TARGETARCH}" != "${BUILDARCH}" ]; then \
 		if [ ${TARGETARCH} = 'arm64' ]; then \
 			apt-get install -y gcc-aarch64-linux-gnu crossbuild-essential-arm64; \
@@ -31,7 +31,6 @@ ADD . /go/src/github.com/inspektor-gadget/inspektor-gadget
 WORKDIR /go/src/github.com/inspektor-gadget/inspektor-gadget
 
 RUN \
-	export CGO_ENABLED=1 ; \
 	if [ "${TARGETARCH}" != "${BUILDARCH}" ]; then \
 		if [ ${TARGETARCH} = 'arm64' ]; then \
 			export CC=aarch64-linux-gnu-gcc; \

commit 93af58d4d951d4f5b6ba2ed793057fc08a3d3257

Disable cgo explicitly
According to the documentation [0]: "The cgo tool is enabled by default
for native builds on systems where it is expected to work", hence we
need to set CGO_ENABLED=0 to explicitly disable it.

It was causing a lot of warnings in make kubectl-gadget on my system:

```
$ make kubectl-gadget
export GO111MODULE=on && \
export GOOS=linux GOARCH=amd64 && \
go build -ldflags "-X main.version=`git describe --tags --always`-dirty -X main.gadgetimage=ghcr.io/inspektor-gadget/inspektor-gadget:latest -extldflags '-static'" \
	-tags withoutebpf \
	-o kubectl-gadget-${GOOS}-${GOARCH} \
	github.com/inspektor-gadget/inspektor-gadget/cmd/kubectl-gadget
/usr/bin/ld: /tmp/go-link-2694774814/000027.o: in function `pluginOpen':
/_/plugin/plugin_dlopen.go:19: warning: Using 'dlopen' in statically linked applications requires at runtime the shared libraries from the glibc version used for linking
/usr/bin/ld: /tmp/go-link-2694774814/000008.o: in function `mygetgrouplist':
/_/os/user/getgrouplist_unix.go:15: warning: Using 'getgrouplist' in statically linked applications requires at runtime the shared libraries from the glibc version used for linking
/usr/bin/ld: /tmp/go-link-2694774814/000007.o: in function `mygetgrgid_r':
/_/os/user/cgo_lookup_cgo.go:45: warning: Using 'getgrgid_r' in statically linked applications requires at runtime the shared libraries from the glibc version used for linking
/usr/bin/ld: /tmp/go-link-2694774814/000007.o: in function `mygetgrnam_r':
/_/os/user/cgo_lookup_cgo.go:54: warning: Using 'getgrnam_r' in statically linked applications requires at runtime the shared libraries from the glibc version used for linking
/usr/bin/ld: /tmp/go-link-2694774814/000007.o: in function `mygetpwnam_r':
/_/os/user/cgo_lookup_cgo.go:36: warning: Using 'getpwnam_r' in statically linked applications requires at runtime the shared libraries from the glibc version used for linking
/usr/bin/ld: /tmp/go-link-2694774814/000007.o: in function `mygetpwuid_r':
/_/os/user/cgo_lookup_cgo.go:27: warning: Using 'getpwuid_r' in statically linked applications requires at runtime the shared libraries from the glibc version used for linking
/usr/bin/ld: /tmp/go-link-2694774814/000004.o: in function `_cgo_cbcce81e6342_C2func_getaddrinfo':
/tmp/go-build/cgo-gcc-prolog:58: warning: Using 'getaddrinfo' in statically linked applications requires at runtime the shared libraries from the glibc version used for linking
cp kubectl-gadget-linux-amd64 kubectl-gadget
```

[0]: https://pkg.go.dev/cmd/cgo

Fixes: 2ecc9fa183aa ("Removing libseccomp installations and CGO usage")

Makefile
@@ -137,7 +137,7 @@ kubectl-gadget: kubectl-gadget-$(GOHOSTOS)-$(GOHOSTARCH)
 	cp kubectl-gadget-$(GOHOSTOS)-$(GOHOSTARCH) kubectl-gadget
 
 kubectl-gadget-%: phony_explicit
-	export GO111MODULE=on && \
+	export GO111MODULE=on CGO_ENABLED=0 && \
 	export GOOS=$(shell echo $* |cut -f1 -d-) GOARCH=$(shell echo $* |cut -f2 -d-) && \
 	go build -ldflags $(LDFLAGS) \
 		-tags withoutebpf \

gadget-container/Makefile
@@ -9,7 +9,7 @@ TARGET_ARCH ?= $(shell go env GOHOSTARCH)
 .PHONY: gadgettracermanager
 gadgettracermanager:
 	mkdir -p bin
-	GO111MODULE=on GOOS=linux GOARCH=$(TARGET_ARCH) go build \
+	GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=$(TARGET_ARCH) go build \
 		-o bin/gadgettracermanager \
 		./gadgettracermanager/
 
@@ -18,13 +18,13 @@ gadgettracermanager:
 .PHONY: ocihookgadget
 ocihookgadget:
 	mkdir -p bin
-	GO111MODULE=on GOOS=linux GOARCH=$(TARGET_ARCH) go build \
+	GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=$(TARGET_ARCH) go build \
 		-o bin/ocihookgadget \
 		./hooks/oci/main.go
 
 .PHONY: nrigadget
 nrigadget:
 	mkdir -p bin
-	GO111MODULE=on GOOS=linux GOARCH=$(TARGET_ARCH) go build \
+	GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=$(TARGET_ARCH) go build \
 		-o bin/nrigadget \
 		./hooks/nri/main.go