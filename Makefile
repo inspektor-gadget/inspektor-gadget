inspektor-gadget/inspektor-gadget

commit 145f7570b1f907033f78b792dd7226f2dcf4e379

gadgets: Build OCI images instead of objects
This commit removes the compiled objects from the tree and updates the
makefile rules to build the gadgets as OCI images.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

Makefile
@@ -6,6 +6,8 @@ IMAGE_TAG ?= $(shell ./tools/image-tag branch)
 
 MINIKUBE ?= minikube
 KUBERNETES_DISTRIBUTION ?= ""
+GADGET_TAG ?= $(shell ../tools/image-tag branch)
+GADGET_REPOSITORY ?= ghcr.io/inspektor-gadget/gadget
 
 GOHOSTOS ?= $(shell go env GOHOSTOS)
 GOHOSTARCH ?= $(shell go env GOHOSTARCH)
@@ -104,6 +106,10 @@ ig-all: $(IG_TARGETS) ig
 
 ig: ig-$(GOHOSTOS)-$(GOHOSTARCH)
 	cp ig-$(GOHOSTOS)-$(GOHOSTARCH) ig
+
+.PHONY: install/ig
+install/ig: ig
+	sudo cp ig /usr/local/bin/ig
 
 ig-%: phony_explicit
 	echo Building $@
@@ -267,6 +273,8 @@ integration-tests: kubectl-gadget
 			-image $(CONTAINER_REPO):$(IMAGE_TAG) \
 			-dnstester-image $(DNSTESTER_IMAGE) \
 			-image-flavour $(IMAGE_FLAVOUR) \
+			-gadgets-repository $(GADGETS_REPOSITORY) \
+			-gadgets-tag $(GADGETS_TAG) \
 			$$INTEGRATION_TESTS_PARAMS
 
 .PHONY: generate-documentation
@@ -337,6 +345,14 @@ install-headers:
 remove-headers:
 	rm -rf /usr/include/gadget
 
+.PHOHY: build-gadgets
+build-gadgets: install/ig
+	$(MAKE) -C gadgets/ build
+
+.PHOHY: push-gadgets
+push-gadgets: install/ig
+	$(MAKE) -C gadgets/ push
+
 .PHONY: help
 help:
 	@echo  'Building targets:'
@@ -358,6 +374,8 @@ help:
 	@echo  '  btfgen			- Build BTF files'
 	@echo  '  list-ig-targets		- List ig available architectures'
 	@echo  '  list-kubectl-gadget-targets	- List kubectl plugin available architectures'
+	@echo  '  build-gadgets			- Build all gadgets'
+	@echo  '  push-gadgets			- Push all gadgets'
 	@echo  ''
 	@echo  'Testing targets:'
 	@echo  '  test				- Run unit tests'
@@ -368,6 +386,7 @@ help:
 	@echo  ''
 	@echo  'Installing targets:'
 	@echo  '  install/kubectl-gadget	- Build kubectl plugin and install it in ~/.local/bin'
+	@echo  '  install/ig	  		- Build and install ig cli tool in /usr/local/bin'
 	@echo  ''
 	@echo  'Development targets:'
 	@echo  '  clang-format			- Format ebpf source files'

gadgets/Makefile
@@ -1,24 +1,31 @@
+SHELL := /bin/bash
+
+GADGET_TAG ?= $(shell ../tools/image-tag branch)
+GADGET_REPOSITORY ?= ghcr.io/inspektor-gadget/gadget
+BUILDER_IMAGE ?= ghcr.io/inspektor-gadget/inspektor-gadget-ebpf-builder:latest
+IG ?= ig
 GADGETS = \
-	trace_exec_x86.bpf.o trace_exec_arm64.bpf.o \
-	trace_open_x86.bpf.o trace_open_arm64.bpf.o \
-	trace_tcpconnect_x86.bpf.o trace_tcpconnect_arm64.bpf.o \
-	trace_tcpretrans_x86.bpf.o trace_tcpretrans_arm64.bpf.o \
-	trace_dns_x86.bpf.o trace_dns_arm64.bpf.o \
+	trace_dns \
+	trace_exec \
+	trace_open \
+	trace_tcpconnect \
+	trace_tcpretrans \
 	#
 
 .PHONY: all
-all: $(GADGETS)
+all: build
 
-%_x86.bpf.o : %.bpf.c
-	clang -target bpf -Wall -g -O2 -D __TARGET_ARCH_x86 -c $(filter %.c,$^) \
-		-I ../include/ \
-		-I ../include/gadget/amd64/ \
-		-o $@
-%_arm64.bpf.o : %.bpf.c
-	clang -target bpf -Wall -g -O2 -D __TARGET_ARCH_arm64 -c $(filter %.c,$^) \
-		-I ../include/ \
-		-I ../include/gadget/arm64/ \
-		-o $@
+build: $(GADGETS)
 
-clean:
-	rm *.o
+.PHONY: $(GADGETS)
+$(GADGETS):
+	@echo "Building $@"
+	@sudo -E IG_EXPERIMENTAL=true $(IG) image build --builder-image $(BUILDER_IMAGE) \
+		-t $(GADGET_REPOSITORY)/$@:$(GADGET_TAG) $@
+
+.PHONY:
+push: build
+	@echo "Pushing all gadgets"
+	for GADGET in $(GADGETS); do \
+		sudo -E IG_EXPERIMENTAL=true $(IG) image push $(GADGET_REPOSITORY)/$$GADGET:$(GADGET_TAG); \
+	done

cmd/common/image/Makefile.build
@@ -1,6 +1,8 @@
 # This makefile is used by the build command, don't execute it manually
 
-BASECLAGS = -target bpf -Wall -g -O2
+CLANG ?= clang
+LLVM-STRIP ?= llvm-strip
+BASECFLAGS = -target bpf -Wall -g -O2
 CFLAGS ?=
 OUTPUTDIR ?= /tmp
 EBPFSOURCE ?= program.bpf.c
@@ -14,6 +16,6 @@ TARGETS = \
 all: $(TARGETS)
 
 $(OUTPUTDIR)/%.bpf.o: $(EBPFSOURCE)
-	clang $(BASECLAGS) $(CFLAGS) -D __TARGET_ARCH_$(subst amd64,x86,$*) \
+	$(CLANG) $(BASECFLAGS) $(CFLAGS) -D __TARGET_ARCH_$(subst amd64,x86,$*) \
 		-c $< -I /usr/include/gadget/$*/ -o $@
-	llvm-strip -g $@
+	$(LLVM-STRIP) -g $@

cmd/common/image/build.go
@@ -25,6 +25,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -59,8 +60,6 @@ type buildFile struct {
 	EBPFSource string `yaml:"ebpfsource"`
 	Metadata   string `yaml:"metadata"`
 	CFlags     string `yaml:"cflags"`
-	// TODO: custom build script
-	// TODO: author, etc.
 }
 
 type cmdOpts struct {
@@ -208,7 +207,8 @@ func buildLocal(opts *cmdOpts, conf *buildFile, output string) error {
 	}
 
 	buildCmd := exec.Command(
-		"make", "-f", makefilePath, "-j", "2",
+		"make", "-f", makefilePath,
+		"-j", fmt.Sprintf("%d", runtime.NumCPU()),
 		"EBPFSOURCE="+conf.EBPFSource,
 		"OUTPUTDIR="+output,
 		"CFLAGS="+conf.CFlags,
@@ -257,7 +257,7 @@ func buildInContainer(opts *cmdOpts, conf *buildFile, output string) error {
 	}
 
 	if !found {
-		fmt.Printf("Pulling builder image %s\n", opts.builderImage)
+		fmt.Printf("Pulling builder image %s. It could take few minutes.\n", opts.builderImage)
 		reader, err := cli.ImagePull(ctx, opts.builderImage, types.ImagePullOptions{})
 		if err != nil {
 			return fmt.Errorf("pulling builder image: %w", err)
@@ -271,14 +271,14 @@ func buildInContainer(opts *cmdOpts, conf *buildFile, output string) error {
 		&container.Config{
 			Image: opts.builderImage,
 			Cmd: []string{
-				"make", "-f", "/Makefile", "-j", "2",
+				"make", "-f", "/Makefile", "-j", fmt.Sprintf("%d", runtime.NumCPU()),
 				"EBPFSOURCE=" + filepath.Join("/work", conf.EBPFSource),
 				"OUTPUTDIR=/out",
 				"CFLAGS=" + conf.CFlags,
 			},
+			User: fmt.Sprintf("%d:%d", os.Getuid(), os.Getgid()),
 		},
 		&container.HostConfig{
-			AutoRemove: true,
 			Mounts: []mount.Mount{
 				{
 					Type:     mount.TypeBind,
@@ -298,6 +298,11 @@ func buildInContainer(opts *cmdOpts, conf *buildFile, output string) error {
 	if err != nil {
 		return fmt.Errorf("creating builder container: %w", err)
 	}
+	defer func() {
+		if err := cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{}); err != nil {
+			fmt.Printf("Failed to remove builder container: %s\n", err)
+		}
+	}()
 
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		return fmt.Errorf("starting builder container: %w", err)

cmd/common/image/list.go
@@ -36,7 +36,7 @@ func NewListCmd() *cobra.Command {
 	o := listOptions{}
 	cmd := &cobra.Command{
 		Use:          "list",
-		Short:        "List gadget images in the host",
+		Short:        "List gadget images on the host",
 		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runList(o)

cmd/common/utils/flags.go
@@ -140,7 +140,7 @@ func AddRegistryAuthVariablesAndFlags(cmd *cobra.Command, authOptions *oci.AuthO
 		&authOptions.AuthFile,
 		"authfile",
 		oci.DefaultAuthFile,
-		"path of the authentication file. This overrides the REGISTRY_AUTH_FILE environment variable",
+		"Path of the authentication file. This overrides the REGISTRY_AUTH_FILE environment variable",
 	)
 	viper.BindPFlag("registry.auth_file", cmd.Flags().Lookup("authfile"))
 	viper.BindEnv("registry.auth_file", "REGISTRY_AUTH_FILE")
@@ -149,6 +149,6 @@ func AddRegistryAuthVariablesAndFlags(cmd *cobra.Command, authOptions *oci.AuthO
 		&authOptions.Insecure,
 		"insecure",
 		false,
-		"allow connections to HTTP only registries",
+		"Allow connections to HTTP only registries",
 	)
 }

commit 8a27babad51c97b30a7be419704ffd6a1e6c0e5c

Makefile: Fix gadget tag calculation
Before this commit:

```
make: ../tools/image-tag: Command not found
KUBECTL_GADGET="/home/mvb/kinvolk/ebpf/inspektor-gadget/kubectl-gadget" \
	go test ./integration/inspektor-gadget/... \
		-v \
		-integration \
		-timeout 30m \
		-k8s-distro "" \
		-k8s-arch amd64 \
		-image ghcr.io/inspektor-gadget/inspektor-gadget:latest \
		-dnstester-image "ghcr.io/inspektor-gadget/dnstester:latest" \
		-gadget-repository ghcr.io/inspektor-gadget/gadget \
		-gadget-tag  \
		$INTEGRATION_TESTS_PARAMS
flag needs an argument: -gadget-tag
```

Fixes: 145f7570b1f9 ("gadgets: Build OCI images instead of objects")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

Makefile
@@ -6,7 +6,7 @@ IMAGE_TAG ?= $(shell ./tools/image-tag branch)
 
 MINIKUBE ?= minikube
 KUBERNETES_DISTRIBUTION ?= ""
-GADGET_TAG ?= $(shell ../tools/image-tag branch)
+GADGET_TAG ?= $(shell ./tools/image-tag branch)
 GADGET_REPOSITORY ?= ghcr.io/inspektor-gadget/gadget
 
 GOHOSTOS ?= $(shell go env GOHOSTOS)