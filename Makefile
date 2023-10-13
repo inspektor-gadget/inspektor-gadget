inspektor-gadget/inspektor-gadget

commit 13cb07051c4617853983f2b1d3dc9c8296b8b626

Create include folder with all header files exposed by IG
As we're moving more towards containerized gadgets, we need to prepare
everything needed to build gadgets outside the tree. The first step
is to consolidate all headers files used by gadgets in the same place.

This commit has a lot of changed files, but there is not any behaviour
change.

The only need thing, is the "make {install,remove}-headers" targets.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

Makefile
@@ -71,11 +71,23 @@ phony_explicit:
 
 ebpf-objects:
 	docker run --rm --name ebpf-object-builder --user $(shell id -u):$(shell id -g) \
-		-v $(shell pwd):/work $(EBPF_BUILDER)
+		-v $(shell pwd):/work $(EBPF_BUILDER) \
+		make ebpf-objects-outside-docker
 
 ebpf-objects-outside-docker:
-	TARGET=arm64 go generate ./...
-	TARGET=amd64 go generate ./...
+# We need <asm/types.h> and depending on Linux distributions, it is installed
+# at different paths:
+#
+# * Ubuntu, package linux-libc-dev:
+#   /usr/include/x86_64-linux-gnu/asm/types.h
+#
+# * Fedora, package kernel-headers
+#   /usr/include/asm/types.h
+#
+# Since Ubuntu does not install it in a standard path, add a compiler flag for
+# it.
+	TARGET=arm64 CFLAGS="-I/usr/include/$(uname -m)-linux-gnu -I$(shell pwd)/include/gadget/arm64/ -I$(shell pwd)/include/" go generate ./...
+	TARGET=amd64 CFLAGS="-I/usr/include/$(uname -m)-linux-gnu -I$(shell pwd)/include/gadget/amd64/ -I$(shell pwd)/include/" go generate ./...
 
 # ig
 
@@ -280,6 +292,14 @@ generate-manifests:
 	make -C charts APP_VERSION=latest template
 	cat charts/bin/deploy.yaml >> pkg/resources/manifests/deploy.yaml
 
+.PHONY: install-headers
+install-headers:
+	cp -r ./include/gadget/ /usr/include/
+
+.PHONY: remove-headers
+remove-headers:
+	rm -rf /usr/include/gadget
+
 .PHONY: help
 help:
 	@echo  'Building targets:'
@@ -319,3 +339,5 @@ help:
 	@echo  '  generate-manifests		- Generate manifests for the gadget deployment'
 	@echo  '  minikube-start		- Start a kubernetes cluster using minikube with the docker driver'
 	@echo  '  minikube-deploy		- Build and deploy the gadget container on minikube with docker driver, the cluster is started if it does not exist'
+	@echo  '  install-headers		- Install headers used to build gadgets in /usr/include/gadget'
+	@echo  '  remove-headers		- Remove headers installed in /usr/include/gadget'

gadgets/Makefile
@@ -11,15 +11,13 @@ all: $(GADGETS)
 
 %_x86.bpf.o : %.bpf.c
 	clang -target bpf -Wall -g -O2 -D __TARGET_ARCH_x86 -c $(filter %.c,$^) \
-		-I ../pkg/amd64/ \
-		-I ../pkg/gadgets/common/ \
-		-I ../pkg/gadgets/internal/socketenricher/bpf/ \
+		-I ../include/ \
+		-I ../include/gadget/amd64/ \
 		-o $@
 %_arm64.bpf.o : %.bpf.c
 	clang -target bpf -Wall -g -O2 -D __TARGET_ARCH_arm64 -c $(filter %.c,$^) \
-		-I ../pkg/arm64/ \
-		-I ../pkg/gadgets/common/ \
-		-I ../pkg/gadgets/internal/socketenricher/bpf/ \
+		-I ../include/ \
+		-I ../include/gadget/arm64/ \
 		-o $@
 
 clean:

commit ced11f72bcc063fe817299c3d7b2e5fedc30ab4e

Makefile: Fix uname invocation to compile ebpf files.
Fixes: 13cb07051c46 ("Create include folder with all header files exposed by IG")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

Makefile
@@ -86,8 +86,8 @@ ebpf-objects-outside-docker:
 #
 # Since Ubuntu does not install it in a standard path, add a compiler flag for
 # it.
-	TARGET=arm64 CFLAGS="-I/usr/include/$(uname -m)-linux-gnu -I$(shell pwd)/include/gadget/arm64/ -I$(shell pwd)/include/" go generate ./...
-	TARGET=amd64 CFLAGS="-I/usr/include/$(uname -m)-linux-gnu -I$(shell pwd)/include/gadget/amd64/ -I$(shell pwd)/include/" go generate ./...
+	TARGET=arm64 CFLAGS="-I/usr/include/$(shell uname -m)-linux-gnu -I$(shell pwd)/include/gadget/arm64/ -I$(shell pwd)/include/" go generate ./...
+	TARGET=amd64 CFLAGS="-I/usr/include/$(shell uname -m)-linux-gnu -I$(shell pwd)/include/gadget/amd64/ -I$(shell pwd)/include/" go generate ./...
 
 # ig
 