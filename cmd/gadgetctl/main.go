inspektor-gadget/inspektor-gadget

commit e89e6828c334e2414036ea5a18a0e573f011b094

add gadgetctl as client for ig
This change introduces a new binary (for now called `gadgetctl`), to serve as the client
for ig that is run in daemon mode. The connection is handled by the grpc-runtime, that
has been extended to allow direct connections to gRPC endpoints in addition to proxied
connections through the kubernetes API server.

Signed-off-by: Michael Friese <mfriese@microsoft.com>

cmd/gadgetctl/main.go
@@ -1,0 +1,85 @@
+// Copyright 2023 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/inspektor-gadget/inspektor-gadget/cmd/common"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/all-gadgets"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/environment"
+	grpcruntime "github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/grpc"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/experimental"
+)
+
+var infoSkipCommands = []string{"version"}
+
+func main() {
+	if experimental.Enabled() {
+		log.Info("Experimental features enabled")
+	}
+
+	rootCmd := &cobra.Command{
+		Use:   filepath.Base(os.Args[0]),
+		Short: "Collection of gadgets for containers",
+	}
+	common.AddVerboseFlag(rootCmd)
+
+	skipInfo := false
+	for _, arg := range os.Args[1:] {
+		for _, skipCmd := range infoSkipCommands {
+			if strings.ToLower(arg) == skipCmd {
+				skipInfo = true
+			}
+		}
+	}
+
+	rootCmd.AddCommand(common.NewVersionCmd())
+
+	runtime := grpcruntime.New()
+
+	runtimeGlobalParams := runtime.GlobalParamDescs().ToParams()
+	common.AddFlags(rootCmd, runtimeGlobalParams, nil, runtime)
+	err := runtime.Init(runtimeGlobalParams)
+	if err != nil {
+		log.Fatalf("initializing runtime: %v", err)
+	}
+
+	if !skipInfo {
+		// evaluate flags early for runtimeGlobalFlags; this will make
+		// sure that --remote-address has already been parsed when calling
+		// InitDeployInfo(), so it can target the specified address
+		rootCmd.ParseFlags(os.Args[1:])
+		runtime.InitDeployInfo()
+	}
+
+	hiddenColumnTags := []string{"kubernetes"}
+	common.AddCommandsFromRegistry(rootCmd, runtime, hiddenColumnTags)
+
+	rootCmd.AddCommand(common.NewSyncCommand(runtime))
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	environment.Environment = environment.Local
+}

commit 83fb693f1f1729d6f11ce888fd6baf15c8dfb90a

cmd: Ignore unknown flags when parsing params early
When connecting to a server we need to have information about the
connection itself. That is why we parse the flags as early as possible.
Even before we have all of them available, because the flags are derived
from the server's information.

This commit enables the option to ignore unknown flags on this early
flag parsing, so we can get information on the connection flags without
erroring out.

Fixes: e89e6828c334 ("add gadgetctl as client for ig")
Fixes: cd5768eb8078 ("pkg/runtime/grpc: add option to connect to the gadget
                      service using either tcp or unix socket")

Co-authored-by: Michael Friese <mfriese@microsoft.com>
Signed-off-by: Burak Ok <burakok@microsoft.com>

cmd/gadgetctl/main.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -66,7 +67,19 @@ func main() {
 		// evaluate flags early for runtimeGlobalFlags; this will make
 		// sure that --remote-address has already been parsed when calling
 		// InitDeployInfo(), so it can target the specified address
-		rootCmd.ParseFlags(os.Args[1:])
+
+		// Do not error out on unknown flags, but still validate currently
+		// known ones.
+		// Other flags will be validated in the `Execute()` call and unknown
+		// ones will be rejected
+		rootCmd.FParseErrWhitelist.UnknownFlags = true
+		err := rootCmd.ParseFlags(os.Args[1:])
+		if err != nil {
+			// Analogous to cobra error message
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+		rootCmd.FParseErrWhitelist.UnknownFlags = false
 		runtime.InitDeployInfo()
 	}
 

cmd/kubectl-gadget/main.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -78,7 +79,19 @@ func main() {
 		// evaluate flags early for runtimeGlobalFlags; this will make
 		// sure that --connection-method has already been parsed when calling
 		// InitDeployInfo()
-		rootCmd.ParseFlags(os.Args[1:])
+
+		// Do not error out on unknown flags, but still validate currently
+		// known ones.
+		// Other flags will be validated in the `Execute()` call and unknown
+		// ones will be rejected
+		rootCmd.FParseErrWhitelist.UnknownFlags = true
+		err := rootCmd.ParseFlags(os.Args[1:])
+		if err != nil {
+			// Analogous to cobra error message
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+		rootCmd.FParseErrWhitelist.UnknownFlags = false
 		runtime.InitDeployInfo()
 	}
 