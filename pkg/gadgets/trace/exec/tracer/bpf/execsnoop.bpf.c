inspektor-gadget/inspektor-gadget

commit 65143e8ce269824a5aeca76946f73557c70ce75d

Decreasing the size of trace/open and trace/exec hash maps
Signed-off-by: Ben <ben@armosec.io>

Adding object files

Signed-off-by: Ben <ben@armosec.io>

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -20,7 +20,7 @@ static const struct event empty_event = {};
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 10240);
+	__uint(max_entries, 1024);
 	__type(key, pid_t);
 	__type(value, struct event);
 } execs SEC(".maps");

pkg/gadgets/trace/open/tracer/bpf/opensnoop.bpf.c
@@ -22,7 +22,7 @@ const struct event *unusedevent __attribute__((unused));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 10240);
+	__uint(max_entries, 1024);
 	__type(key, u32);
 	__type(value, struct event);
 } start SEC(".maps");

commit 8a4461a5738a656e157fd015177380663daba54d

trace exec: Revert map size to its previous value.
Having only 1024 entries impedes vertical scaling.
Thus, --cwd is not not used we use 10240 entries.
When this option is used, we stick with 1024 entries.

Fixes: 65143e8ce269 ("Decreasing the size of trace/open and trace/exec hash maps")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -20,7 +20,11 @@ static const struct event empty_event = {};
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
+#ifdef WITH_CWD
 	__uint(max_entries, 1024);
+#else /* !WITH_CWD */
+	__uint(max_entries, 10240);
+#endif /* !WITH_CWD */
 	__type(key, pid_t);
 	__type(value, struct event);
 } execs SEC(".maps");