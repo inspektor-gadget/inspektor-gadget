inspektor-gadget/inspektor-gadget

commit d1bccf6c124fc41dcda6cc8d63ae7b98732db736

gadgets/run: Support eBPF parameters
Gadgets usually define different constants in the eBPF code to change
its behaviour. This commit implements the logic allow users setting
these flags from the CLI.

The parameters are gathered from a list provided in the gadget metadata
definition. Then the variable names are compared with the BTF
information and the corresponding variables will be replaced with the
user provided values.

Co-authored-by: Burak Ok <burakok@mircosoft.com>
Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>
Signed-off-by: Burak Ok <burakok@microsoft.com>

cmd/common/registry.go
@@ -16,6 +16,7 @@ package common
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"sort"
@@ -186,6 +187,7 @@ func buildCommandFromGadget(
 	hiddenColumnTags []string,
 ) *cobra.Command {
 	runGadgetDesc, isRunGadget := gadgetDesc.(runTypes.RunGadgetDesc)
+	var runGadgetInfo *runTypes.GadgetInfo
 	var outputMode string
 	var filters []string
 	var timeout int
@@ -229,6 +231,64 @@ func buildCommandFromGadget(
 		DisableFlagParsing: true,
 
 		PreRunE: func(cmd *cobra.Command, args []string) error {
+			err := runtime.Init(runtimeGlobalParams)
+			if err != nil {
+				return fmt.Errorf("initializing runtime: %w", err)
+			}
+			defer runtime.Close()
+
+			// TODO: This is a best effort approach to remove all the things that are
+			// related to args. Please notice that flags from the gadget haven't been
+			// added at this point, so it's not possible to use any logic to check more
+			// details about them.
+			onlyArgs := []string{}
+			for i := 0; i < len(args); i++ {
+				arg := args[i]
+				if arg[0] == '-' {
+					// --bar=foo: skip them
+					if strings.Contains(arg, "=") {
+						continue
+					}
+
+					// --bar with default value, skip it without skipping next arg
+					name := strings.TrimLeft(arg, "-")
+
+					f := cmd.Flags().Lookup(name)
+					// If NoOptDefVal was set we just continue to the next args value in the slice
+					// Example: `--help` would be such a value. No need to specify `true` after it
+					// See https://pkg.go.dev/github.com/spf13/pflag#readme-setting-no-option-default-values-for-flags
+					if f != nil && f.NoOptDefVal != "" {
+						continue
+					}
+
+					// In other cases skip next argument e.g: `--bar foo` would now skip `foo`
+					i++
+					continue
+				}
+
+				onlyArgs = append(onlyArgs, arg)
+			}
+
+			// Before running the gadget, we need to get the gadget info to perform
+			// different tasks like creating the parser and setting flags for the
+			// gadget's parameters.
+			if isRunGadget && len(onlyArgs) > 0 {
+				var err error
+				runGadgetInfo, err = runtime.GetGadgetInfo(context.TODO(), gadgetDesc, gadgetParams, onlyArgs)
+				if err != nil {
+					return fmt.Errorf("getting gadget info: %w", err)
+				}
+
+				ebpfParams := params.Params{}
+				for _, desc := range runGadgetInfo.GadgetMetadata.EBPFParams {
+					desc := desc
+					ebpfParams.Add(desc.ToParam())
+				}
+
+				AddFlags(cmd, &ebpfParams, nil, runtime)
+				gadgetParams.Add(ebpfParams...)
+			}
+
 			// we need to re-enable flag parsing, as cmd.ParseFlags() would not
 			// do anything otherwise
 			cmd.DisableFlagParsing = false
@@ -255,7 +315,6 @@ func buildCommandFromGadget(
 
 			ctx := fe.GetContext()
 
-			var gadgetInfo *runTypes.GadgetInfo
 			// Before running the gadget, we need to get the gadget info to create the
 			// parser based on it
 			if isRunGadget {
@@ -267,13 +326,7 @@ func buildCommandFromGadget(
 					return cmd.Help()
 				}
 
-				var err error
-				gadgetInfo, err = runtime.GetGadgetInfo(ctx, gadgetDesc, gadgetParams, args)
-				if err != nil {
-					return fmt.Errorf("getting gadget info: %w", err)
-				}
-
-				parser, err = runGadgetDesc.CustomParser(gadgetInfo)
+				parser, err = runGadgetDesc.CustomParser(runGadgetInfo)
 				if err != nil {
 					return fmt.Errorf("calling custom parser: %w", err)
 				}
@@ -572,19 +625,19 @@ func buildCommandFromGadget(
 			case OutputModeJSON:
 				jsonCallback := printEventAsJSONFn(fe)
 				if isRunGadget {
-					jsonCallback = runGadgetDesc.JSONConverter(gadgetInfo, fe)
+					jsonCallback = runGadgetDesc.JSONConverter(runGadgetInfo, fe)
 				}
 				parser.SetEventCallback(jsonCallback)
 			case OutputModeJSONPretty:
 				jsonPrettyCallback := printEventAsJSONPrettyFn(fe)
 				if isRunGadget {
-					jsonPrettyCallback = runGadgetDesc.JSONPrettyConverter(gadgetInfo, fe)
+					jsonPrettyCallback = runGadgetDesc.JSONPrettyConverter(runGadgetInfo, fe)
 				}
 				parser.SetEventCallback(jsonPrettyCallback)
 			case OutputModeYAML:
 				yamlCallback := printEventAsYAMLFn(fe)
 				if isRunGadget {
-					yamlCallback = runGadgetDesc.YAMLConverter(gadgetInfo, fe)
+					yamlCallback = runGadgetDesc.YAMLConverter(runGadgetInfo, fe)
 				}
 				parser.SetEventCallback(yamlCallback)
 			}

include/gadget/macros.h
@@ -11,4 +11,9 @@
 #define GADGET_TRACE_MAP(name) \
 	const void * gadget_trace_map_##name __attribute__((unused));
 
+// GADGET_PARAM is used to indicate that a given variable is used as a parameter.
+// Users of Inspektor Gadget can set these values from userspace
+#define GADGET_PARAM(name) \
+	const void * gadget_param_##name __attribute__((unused));
+
 #endif /* __MACROS_H */

pkg/gadget-service/service.go
@@ -166,6 +166,18 @@ func (s *Service) RunGadget(runGadget api.GadgetManager_RunGadgetServer) error {
 		if err != nil {
 			return fmt.Errorf("calling custom parser: %w", err)
 		}
+
+		// Update gadget parameters to take ebpf params into consideration
+		for _, p := range gadgetInfo.GadgetMetadata.EBPFParams {
+			p := p
+			gadgetParamDescs.Add(&p.ParamDesc)
+		}
+		gadgetParams = gadgetParamDescs.ToParams()
+		err = gadgetParams.CopyFromMap(request.Params, "")
+		if err != nil {
+			return fmt.Errorf("setting parameters: %w", err)
+		}
+
 	}
 
 	// Create payload buffer

pkg/gadgets/run/tracer/run.go
@@ -132,7 +132,84 @@ func getGadgetInfo(params *params.Params, args []string, logger logger.Logger) (
 		}
 	}
 
+	if err := fillTypeHints(spec, ret.GadgetMetadata.EBPFParams); err != nil {
+		return nil, fmt.Errorf("fill parameters type hints: %w", err)
+	}
+
 	return ret, nil
+}
+
+func getTypeHint(typ btf.Type) params.TypeHint {
+	switch typedMember := typ.(type) {
+	case *btf.Int:
+		switch typedMember.Encoding {
+		case btf.Signed:
+			switch typedMember.Size {
+			case 1:
+				return params.TypeInt8
+			case 2:
+				return params.TypeInt16
+			case 4:
+				return params.TypeInt32
+			case 8:
+				return params.TypeInt64
+			}
+		case btf.Unsigned:
+			switch typedMember.Size {
+			case 1:
+				return params.TypeUint8
+			case 2:
+				return params.TypeUint16
+			case 4:
+				return params.TypeUint32
+			case 8:
+				return params.TypeUint64
+			}
+		case btf.Bool:
+			return params.TypeBool
+		case btf.Char:
+			return params.TypeUint8
+		}
+	case *btf.Float:
+		switch typedMember.Size {
+		case 4:
+			return params.TypeFloat32
+		case 8:
+			return params.TypeFloat64
+		}
+	case *btf.Typedef:
+		typ, err := getUnderlyingType(typedMember)
+		if err != nil {
+			return params.TypeUnknown
+		}
+		return getTypeHint(typ)
+	case *btf.Volatile:
+		return getTypeHint(typedMember.Type)
+	}
+
+	return params.TypeUnknown
+}
+
+// fillTypeHints fills the TypeHint field in the ebpf parameters according to the BTF information
+// about those constants.
+func fillTypeHints(spec *ebpf.CollectionSpec, params map[string]types.EBPFParam) error {
+	for varName, p := range params {
+		var btfVar *btf.Var
+		err := spec.Types.TypeByName(varName, &btfVar)
+		if err != nil {
+			return fmt.Errorf("no BTF type found for: %s: %w", p.Key, err)
+		}
+
+		btfConst, ok := btfVar.Type.(*btf.Const)
+		if !ok {
+			return fmt.Errorf("type for %s is not a constant, got %s", p.Key, btfVar.Type)
+		}
+
+		p.TypeHint = getTypeHint(btfConst.Type)
+		params[varName] = p
+	}
+
+	return nil
 }
 
 func (g *GadgetDesc) GetGadgetInfo(params *params.Params, args []string) (*types.GadgetInfo, error) {

pkg/gadgets/run/tracer/tracer.go
@@ -36,6 +36,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/internal/networktracer"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/internal/socketenricher"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/run/types"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 )
 
@@ -56,6 +57,9 @@ type Config struct {
 	ProgContent []byte
 	Metadata    *types.GadgetMetadata
 	MountnsMap  *ebpf.Map
+
+	// constants to replace in the ebpf program
+	Consts map[string]interface{}
 }
 
 type Tracer struct {
@@ -150,12 +154,11 @@ func (t *Tracer) handleTracers() (string, error) {
 	return tracer.MapName, nil
 }
 
-func (t *Tracer) installTracer() error {
+func (t *Tracer) installTracer(params *params.Params) error {
 	var err error
 	var tracerMapName string
 
 	mapReplacements := map[string]*ebpf.Map{}
-	consts := map[string]interface{}{}
 
 	t.eventType, err = getEventTypeBTF(t.config.ProgContent, t.config.Metadata)
 	if err != nil {
@@ -169,6 +172,9 @@ func (t *Tracer) installTracer() error {
 			return fmt.Errorf("handling trace programs: %w", err)
 		}
 	}
+
+	t.setEBPFParameters(t.config.Metadata.EBPFParams, params)
+	consts := t.config.Consts
 
 	// Handle special maps like mount ns filter, socket enricher, etc.
 	for _, m := range t.spec.Maps {
@@ -437,6 +443,17 @@ func (t *Tracer) runTracers(gadgetCtx gadgets.GadgetContext) {
 	}
 }
 
+func (t *Tracer) setEBPFParameters(ebpfParams map[string]types.EBPFParam, gadgetParams *params.Params) {
+	t.config.Consts = make(map[string]interface{})
+	for varName, paramDef := range ebpfParams {
+		p := gadgetParams.Get(paramDef.Key)
+		if !p.IsSet() {
+			continue
+		}
+		t.config.Consts[varName] = p.AsAny()
+	}
+}
+
 func (t *Tracer) Run(gadgetCtx gadgets.GadgetContext) error {
 	params := gadgetCtx.GadgetParams()
 	args := gadgetCtx.Args()
@@ -454,7 +471,7 @@ func (t *Tracer) Run(gadgetCtx gadgets.GadgetContext) error {
 
 	t.config.Metadata = info.GadgetMetadata
 
-	if err := t.installTracer(); err != nil {
+	if err := t.installTracer(params); err != nil {
 		t.Stop()
 		return fmt.Errorf("install tracer: %w", err)
 	}

pkg/gadgets/run/types/metadata.go
@@ -26,13 +26,20 @@ import (
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 )
 
 // Keep this aligned with include/gadget/macros.h
 const (
 	// Prefix used to mark trace maps
 	traceMapPrefix = "gadget_trace_map_"
+	// Prefix used to mark eBPF params
+	paramPrefix = "gadget_param_"
 )
+
+type EBPFParam struct {
+	params.ParamDesc `yaml:",inline"`
+}
 
 const (
 	DefaultColumnWidth = 16
@@ -113,6 +120,8 @@ type GadgetMetadata struct {
 	Tracers map[string]Tracer `yaml:"tracers,omitempty"`
 	// Types generated by the gadget
 	Structs map[string]Struct `yaml:"structs,omitempty"`
+	// Params exposed by the gadget
+	EBPFParams map[string]EBPFParam `yaml:"ebpfParams,omitempty"`
 }
 
 func (m *GadgetMetadata) Validate(spec *ebpf.CollectionSpec) error {
@@ -120,6 +129,10 @@ func (m *GadgetMetadata) Validate(spec *ebpf.CollectionSpec) error {
 
 	if m.Name == "" {
 		result = multierror.Append(result, errors.New("gadget name is required"))
+	}
+
+	if err := m.validateParams(spec); err != nil {
+		result = multierror.Append(result, err)
 	}
 
 	if err := m.validateTracers(spec); err != nil {
@@ -216,6 +229,19 @@ func (m *GadgetMetadata) validateStructs(spec *ebpf.CollectionSpec) error {
 	return result
 }
 
+func (m *GadgetMetadata) validateParams(spec *ebpf.CollectionSpec) error {
+	var result error
+	for varName := range m.EBPFParams {
+		if err := checkParamVar(spec, varName); err != nil {
+			result = multierror.Append(result, err)
+		}
+		if len(m.EBPFParams[varName].Key) == 0 {
+			result = multierror.Append(result, fmt.Errorf("param %q has an empty key", varName))
+		}
+	}
+	return result
+}
+
 // Populate fills the metadata from its ebpf spec
 func (m *GadgetMetadata) Populate(spec *ebpf.CollectionSpec) error {
 	if m.Name == "" {
@@ -236,6 +262,10 @@ func (m *GadgetMetadata) Populate(spec *ebpf.CollectionSpec) error {
 
 	if err := m.populateTracers(spec); err != nil {
 		return fmt.Errorf("handling trace maps: %w", err)
+	}
+
+	if err := m.populateParams(spec); err != nil {
+		return fmt.Errorf("handling params: %w", err)
 	}
 
 	return nil
@@ -331,28 +361,56 @@ func (m *GadgetMetadata) populateTracers(spec *ebpf.CollectionSpec) error {
 	return nil
 }
 
-// getGadgetIdentByPrefix returns the string generated by GADGET_ macros.
-func getGadgetIdentByPrefix(spec *ebpf.CollectionSpec, prefix string) string {
+// getGadgetIdentByPrefix returns the strings generated by GADGET_ macros.
+func getGadgetIdentByPrefix(spec *ebpf.CollectionSpec, prefix string) ([]string, error) {
+	var resultNames []string
+	var resultError error
+
 	it := spec.Types.Iterate()
 	for it.Next() {
-		v, ok := it.Type.(*btf.Var)
+		btfVar, ok := it.Type.(*btf.Var)
 		if !ok {
 			continue
 		}
-
-		if strings.HasPrefix(v.Name, prefix) {
-			return strings.TrimPrefix(v.Name, prefix)
+		if !strings.HasPrefix(btfVar.Name, prefix) {
+			continue
 		}
+		if btfVar.Linkage != btf.GlobalVar {
+			resultError = multierror.Append(resultError, fmt.Errorf("%q is not a global variable", btfVar.Name))
+		}
+		btfPtr, ok := btfVar.Type.(*btf.Pointer)
+		if !ok {
+			resultError = multierror.Append(resultError, fmt.Errorf("%q is not a pointer", btfVar.Name))
+			continue
+		}
+		btfConst, ok := btfPtr.Target.(*btf.Const)
+		if !ok {
+			resultError = multierror.Append(resultError, fmt.Errorf("%q is not const", btfVar.Name))
+			continue
+		}
+		_, ok = btfConst.Type.(*btf.Void)
+		if !ok {
+			resultError = multierror.Append(resultError, fmt.Errorf("%q is not a const void pointer", btfVar.Name))
+			continue
+		}
+
+		resultNames = append(resultNames, strings.TrimPrefix(btfVar.Name, prefix))
 	}
 
-	return ""
+	return resultNames, resultError
 }
 
 // getTracerMapFromeBPF returns the tracer map from the eBPF object.
 // It looks for maps marked with GADGET_TRACE_MAP() and returns the first one.
 func getTracerMapFromeBPF(spec *ebpf.CollectionSpec) *ebpf.MapSpec {
-	mapName := getGadgetIdentByPrefix(spec, traceMapPrefix)
-	return spec.Maps[mapName]
+	mapNames, _ := getGadgetIdentByPrefix(spec, traceMapPrefix)
+	if len(mapNames) == 0 {
+		return nil
+	}
+	if len(mapNames) > 1 {
+		log.Warnf("multiple tracer maps found, using %q", mapNames[0])
+	}
+	return spec.Maps[mapNames[0]]
 }
 
 func (m *GadgetMetadata) populateStruct(btfStruct *btf.Struct) error {
@@ -400,4 +458,73 @@ func (m *GadgetMetadata) populateStruct(btfStruct *btf.Struct) error {
 	m.Structs[btfStruct.Name] = gadgetStruct
 
 	return nil
+}
+
+func (m *GadgetMetadata) populateParams(spec *ebpf.CollectionSpec) error {
+	var result error
+
+	paramNames, err := getGadgetIdentByPrefix(spec, paramPrefix)
+	if err != nil {
+		result = multierror.Append(result, err)
+	}
+
+	for _, name := range paramNames {
+		var btfVar *btf.Var
+		err := spec.Types.TypeByName(name, &btfVar)
+		if err != nil {
+			result = multierror.Append(result, fmt.Errorf("looking variable %q up: %w", name, err))
+			continue
+		}
+
+		err = checkParamVar(spec, name)
+		if err != nil {
+			result = multierror.Append(result, err)
+			continue
+		}
+
+		if m.EBPFParams == nil {
+			m.EBPFParams = make(map[string]EBPFParam)
+		}
+
+		if _, found := m.EBPFParams[name]; found {
+			log.Debugf("Param %q already defined, skipping", name)
+			continue
+		}
+
+		log.Debugf("Adding param %q", name)
+		m.EBPFParams[name] = EBPFParam{
+			ParamDesc: params.ParamDesc{
+				Key:         name,
+				Description: "TODO: Fill parameter description",
+			},
+		}
+	}
+
+	return result
+}
+
+func checkParamVar(spec *ebpf.CollectionSpec, name string) error {
+	var result error
+
+	var btfVar *btf.Var
+	err := spec.Types.TypeByName(name, &btfVar)
+	if err != nil {
+		result = multierror.Append(result, fmt.Errorf("variable %q not found in eBPF object: %w", name, err))
+		return result
+	}
+	if btfVar.Linkage != btf.GlobalVar {
+		result = multierror.Append(result, fmt.Errorf("%q is not a global variable", name))
+	}
+	btfConst, ok := btfVar.Type.(*btf.Const)
+	if !ok {
+		result = multierror.Append(result, fmt.Errorf("%q is not const", name))
+		return result
+	}
+	_, ok = btfConst.Type.(*btf.Volatile)
+	if !ok {
+		result = multierror.Append(result, fmt.Errorf("%q is not volatile", name))
+		return result
+	}
+
+	return result
 }

pkg/params/params.go
@@ -48,47 +48,47 @@ var ErrNotFound = errors.New("not found")
 type ParamDesc struct {
 	// Key is the name under which this param is registered; this will also be the key when
 	// getting a key/value map
-	Key string `json:"key"`
+	Key string `json:"key" yaml:"key"`
 
 	// Alias is a shortcut for this parameter, usually a single character used for command line
 	// interfaces
-	Alias string `json:"alias"`
+	Alias string `json:"alias" yaml:"alias,omitempty"`
 
 	// Title is an optional (pretty) alternative to key and used in user interfaces
-	Title string `json:"title"`
+	Title string `json:"title" yaml:"title,omitempty"`
 
 	// DefaultValue is the value that will be used if no other value has been assigned
-	DefaultValue string `json:"defaultValue"`
+	DefaultValue string `json:"defaultValue" yaml:"defaultValue"`
 
 	// Description holds an optional explanation for this parameter; shown in user interfaces
-	Description string `json:"description"`
+	Description string `json:"description" yaml:"description"`
 
 	// IsMandatory will be considered when validating; if the param has no value assigned and
 	// also no DefaultValue is set, validation will fail
-	IsMandatory bool `json:"isMandatory"`
+	IsMandatory bool `json:"isMandatory" yaml:"isMandatory,omitempty"`
 
 	// Tags can be used to skip parameters not needed for a specific environment
-	Tags []string `json:"tags"`
+	Tags []string `json:"tags" yaml:"tags,omitempty"`
 
 	// Validator is an optional function that will be called upon validation; may or may
 	// not be called in user interfaces. Setting TypeHint is preferred, but can also be used
 	// in combination with the Validator. Example: additionally to setting the TypeHint to
 	// TypeInt, the validator could be used to make sure the given int is in a specific range.
-	Validator ParamValidator `json:"-"`
+	Validator ParamValidator `json:"-" yaml:"-"`
 
 	// TypeHint is the preferred way to set the type of this parameter as it will invoke a
 	// matching validator automatically; if unset, a value of "string" is assumed
-	TypeHint TypeHint `json:"type"`
+	TypeHint TypeHint `json:"type" yaml:"type,omitempty"`
 
 	// ValueHint can give a hint on what content is expected here - for example, it can be
 	// used to hint that the param expects a kubernetes namespace, a list of nodes and so on.
 	// This is helpful for frontends to provide autocompletion/selections or set defaults
 	// accordingly.
-	ValueHint ValueHint `json:"valueHint"`
+	ValueHint ValueHint `json:"valueHint" yaml:"valueHint,omitempty"`
 
 	// PossibleValues holds all possible values for this parameter and will be considered
 	// when validating
-	PossibleValues []string `json:"possibleValues"`
+	PossibleValues []string `json:"possibleValues" yaml:"possibleValues,omitempty"`
 }
 
 // Param holds a ParamDesc but can additionally store a value
@@ -291,7 +291,7 @@ func (p *Params) CopyFromMap(source map[string]string, prefix string) error {
 	for k, v := range source {
 		if strings.HasPrefix(k, prefix) {
 			param := p.Get(strings.TrimPrefix(k, prefix))
-			if param == nil {
+			if param == nil || param.value == v {
 				continue
 			}
 			if param.TypeHint == TypeBytes {

commit c49480a4b7361e93f76a310b8c7ea1e95176eba2

cmd: Fix --authfile/--insecure flags
d1bccf6c124f ("gadgets/run: Support eBPF parameters") introduced a logic
to remove the flags before calling GetGadgetInfo(), unfortunately
it also broke the --authfile/--insecure flags as they were removed.

This commit changes the approach to use ParseEarlyFlags() to parse
flags that are already known before contacting the server, like
--insecure & --authfile, so they can be used by the server while it
executes GetGadgetInfo().

Fixes: d1bccf6c124f ("gadgets/run: Support eBPF parameters")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

cmd/common/registry.go
@@ -238,41 +238,21 @@ func buildCommandFromGadget(
 			}
 			defer runtime.Close()
 
-			// TODO: This is a best effort approach to remove all the things that are
-			// related to args. Please notice that flags from the gadget haven't been
-			// added at this point, so it's not possible to use any logic to check more
-			// details about them.
-			onlyArgs := []string{}
-			for i := 0; i < len(args); i++ {
-				arg := args[i]
-				if arg[0] == '-' {
-					// --bar=foo: skip them
-					if strings.Contains(arg, "=") {
-						continue
-					}
-
-					// --bar with default value, skip it without skipping next arg
-					name := strings.TrimLeft(arg, "-")
-
-					f := cmd.Flags().Lookup(name)
-					// If NoOptDefVal was set we just continue to the next args value in the slice
-					// Example: `--help` would be such a value. No need to specify `true` after it
-					// See https://pkg.go.dev/github.com/spf13/pflag#readme-setting-no-option-default-values-for-flags
-					if f != nil && f.NoOptDefVal != "" {
-						continue
-					}
-
-					// In other cases skip next argument e.g: `--bar foo` would now skip `foo`
-					i++
-					continue
-				}
-
-				onlyArgs = append(onlyArgs, arg)
+			// we need to re-enable flag parsing, as utils.ParseEarlyFlags() would
+			// not do anything otherwise
+			cmd.DisableFlagParsing = false
+			// Parse flags that are known at this time, like the ones we get from the gadget descriptor
+			if err := utils.ParseEarlyFlags(cmd, args); err != nil {
+				return err
 			}
+
+			// gadget parameters that are only available after contacting the server
+			extraGadgetParams := make(params.Params, 0)
 
 			// Before running the gadget, we need to get the gadget info to perform
 			// different tasks like creating the parser and setting flags for the
 			// gadget's parameters.
+			onlyArgs := cmd.Flags().Args()
 			if isRunGadget && len(onlyArgs) > 0 {
 				var err error
 				runGadgetInfo, err = runtime.GetGadgetInfo(context.TODO(), gadgetDesc, gadgetParams, onlyArgs)
@@ -293,7 +273,7 @@ func buildCommandFromGadget(
 					ebpfParams.Add(desc.ToParam())
 				}
 
-				gadgetParams.Add(ebpfParams...)
+				extraGadgetParams.Add(ebpfParams...)
 			}
 			// add flags
 			if gType != gadgets.TypeOneShot {
@@ -308,13 +288,10 @@ func buildCommandFromGadget(
 			}
 
 			// Add params matching the gadget type
-			gadgetParams.Add(*gadgets.GadgetParams(gadgetDesc, gType, parser).ToParams()...)
+			extraGadgetParams.Add(*gadgets.GadgetParams(gadgetDesc, gType, parser).ToParams()...)
 
-			// Add runtime flags
-			AddFlags(cmd, runtimeParams, skipParams, runtime)
-
-			// Add gadget flags
-			AddFlags(cmd, gadgetParams, skipParams, runtime)
+			// Add extra gadget flags
+			AddFlags(cmd, &extraGadgetParams, skipParams, runtime)
 
 			outputFormats.Append(gadgets.OutputFormats{
 				OutputModeJSON: {
@@ -375,9 +352,8 @@ func buildCommandFromGadget(
 				strings.Join(outputFormatsHelp, "\n")+"\n\n",
 			)
 
-			// we need to re-enable flag parsing, as cmd.ParseFlags() would not
-			// do anything otherwise
-			cmd.DisableFlagParsing = false
+			gadgetParams.Add(extraGadgetParams...)
+
 			return cmd.ParseFlags(args)
 		},
 		RunE: func(cmd *cobra.Command, _ []string) error {
@@ -725,8 +701,14 @@ func buildCommandFromGadget(
 		},
 	}
 
-	// TODO: why moving this inside PreRunE breaks the namespace parameter?
-	// It's always "default".
+	// Add flags known at this time, others will be added in PreRunE
+
+	// Add runtime flags
+	AddFlags(cmd, runtimeParams, skipParams, runtime)
+
+	// Add gadget flags
+	AddFlags(cmd, gadgetParams, skipParams, runtime)
+
 	// Add operator flags
 	for _, operatorParams := range operatorsParamsCollection {
 		AddFlags(cmd, operatorParams, skipParams, runtime)

cmd/common/utils/flags.go
@@ -17,7 +17,6 @@ package utils
 import (
 	"errors"
 	"fmt"
-	"os"
 	"slices"
 	"strings"
 
@@ -177,7 +176,7 @@ func removeHelpArg(args []string) []string {
 	return args
 }
 
-func ParseEarlyFlags(cmd *cobra.Command) error {
+func ParseEarlyFlags(cmd *cobra.Command, rawArgs []string) error {
 	// Do not error out on unknown flags, but still validate currently
 	// known ones.
 	// Other flags will be validated in the `Execute()` call and unknown
@@ -188,7 +187,7 @@ func ParseEarlyFlags(cmd *cobra.Command) error {
 		cmd.FParseErrWhitelist.UnknownFlags = false
 	}()
 	// temporary workaround for https://github.com/inspektor-gadget/inspektor-gadget/pull/2174#issuecomment-1780923952
-	args := slices.Clone(os.Args[1:]) // clone to avoid modifying the original os.Args
+	args := slices.Clone(rawArgs) // clone to avoid modifying the original os.Args
 	args = removeSplitSortArgs(args)
 	// --help should be handled after we registered all commands
 	args = removeHelpArg(args)

cmd/gadgetctl/main.go
@@ -69,7 +69,7 @@ func main() {
 		// sure that --remote-address has already been parsed when calling
 		// InitDeployInfo(), so it can target the specified address
 
-		err := commonutils.ParseEarlyFlags(rootCmd)
+		err := commonutils.ParseEarlyFlags(rootCmd, os.Args[1:])
 		if err != nil {
 			// Analogous to cobra error message
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)

cmd/ig/main.go
@@ -62,7 +62,7 @@ func main() {
 
 	// evaluate flags early; this will make sure that flags for host are evaluated before
 	// calling host.Init()
-	err := commonutils.ParseEarlyFlags(rootCmd)
+	err := commonutils.ParseEarlyFlags(rootCmd, os.Args[1:])
 	if err != nil {
 		// Analogous to cobra error message
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)

cmd/kubectl-gadget/main.go
@@ -81,7 +81,7 @@ func main() {
 		// sure that --connection-method has already been parsed when calling
 		// InitDeployInfo()
 
-		err := commonutils.ParseEarlyFlags(rootCmd)
+		err := commonutils.ParseEarlyFlags(rootCmd, os.Args[1:])
 		if err != nil {
 			// Analogous to cobra error message
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)