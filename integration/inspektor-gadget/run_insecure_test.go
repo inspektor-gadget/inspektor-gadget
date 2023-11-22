f7f13f0b313731ac376b17d3ae255aa38c94dd69
Mauricio Vásquez <mauriciov@microsoft.com>
2023-11-22 16:40:35 -0500

    integration: Add integration test for --insecure flag
    Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

---
+++ integration/inspektor-gadget/run_insecure_test.go
@@ -0,0 +1,85 @@
+// Copyright 2023 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/inspektor-gadget/inspektor-gadget/integration"
+)
+
+func TestRunInsecure(t *testing.T) {
+	ns := GenerateTestNamespaceName("test-run-insecure")
+
+	t.Parallel()
+
+	commandsPreTest := []*Command{
+		CreateTestNamespaceCommand(ns),
+		PodCommand("registry", "docker.io/library/registry:2", ns, "", ""),
+		WaitUntilPodReadyCommand(ns, "registry"),
+	}
+
+	RunTestSteps(commandsPreTest, t)
+
+	t.Cleanup(func() {
+		commands := []*Command{
+			DeleteTestNamespaceCommand(ns),
+		}
+		RunTestSteps(commands, t, WithCbBeforeCleanup(PrintLogsFn(ns)))
+	})
+
+	registryIP := GetTestPodIP(t, ns, "registry")
+
+	// copy gadget image to insecure registry
+	orasCpCmds := []*Command{
+		{
+			Name: "Copy gadget image",
+			Cmd: fmt.Sprintf(`
+				kubectl apply -f - <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: copier
+  namespace: %s
+spec:
+  template:
+    spec:
+      containers:
+      - name: copier
+        image: ghcr.io/oras-project/oras:v1.1.0
+        command:
+         - oras
+         - copy
+         - --to-plain-http
+         - %s/trace_open:%s
+         - %s:5000/trace_open:%s
+      restartPolicy: Never
+  backoffLimit: 4
+EOF`, ns, *gadgetRepository, *gadgetTag, registryIP, *gadgetTag),
+			ExpectedRegexp: "job.batch/copier created",
+		},
+		{
+			Name:           "WaitForCopierJob",
+			Cmd:            fmt.Sprintf("kubectl wait job --for condition=complete -n %s copier", ns),
+			ExpectedString: "job.batch/copier condition met\n",
+		},
+	}
+	RunTestSteps(orasCpCmds, t)
+
+	// TODO: Ideally it should not depend on a real gadget, but we don't have a "test gadget" available yet.
+	cmd := fmt.Sprintf("$KUBECTL_GADGET run %s:5000/trace_open:%s -n %s -o json --insecure", registryIP, *gadgetTag, ns)
+	runTraceOpen(t, ns, cmd)
+}
