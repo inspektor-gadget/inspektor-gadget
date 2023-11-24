inspektor-gadget/inspektor-gadget

commit 3c9916449a050d98bbbc4e94c3d3705b59ccc488

pkg: Add support for flags for trace open.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/trace/open/types/types.go
@@ -23,14 +23,16 @@ type Event struct {
 	eventtypes.Event
 	eventtypes.WithMountNsID
 
-	Pid  uint32 `json:"pid,omitempty" column:"pid,minWidth:7"`
-	Uid  uint32 `json:"uid" column:"uid,template:uid,hide"`
-	Gid  uint32 `json:"gid" column:"gid,template:gid,hide"`
-	Comm string `json:"comm,omitempty" column:"comm,maxWidth:16"`
-	Fd   int    `json:"fd,omitempty" column:"fd,minWidth:2,width:3"`
-	Ret  int    `json:"ret,omitempty" column:"ret,width:3,fixed,hide"`
-	Err  int    `json:"err,omitempty" column:"err,width:3,fixed"`
-	Path string `json:"path,omitempty" column:"path,minWidth:24,width:32"`
+	Pid      uint32   `json:"pid,omitempty" column:"pid,minWidth:7"`
+	Uid      uint32   `json:"uid,omitempty" column:"uid,minWidth:10,hide"`
+	Gid      uint32   `json:"gid" column:"gid,template:gid,hide"`
+	Comm     string   `json:"comm,omitempty" column:"comm,maxWidth:16"`
+	Fd       int      `json:"fd,omitempty" column:"fd,minWidth:2,width:3"`
+	Ret      int      `json:"ret,omitempty" column:"ret,width:3,fixed,hide"`
+	Err      int      `json:"err,omitempty" column:"err,width:3,fixed"`
+	Flags    []string `json:"flags,omitempty" column:"flags,width:24,hide"`
+	FlagsRaw int32    `json:"flagsRaw,omitempty"`
+	Path     string   `json:"path,omitempty" column:"path,minWidth:24,width:32"`
 }
 
 func GetColumns() *columns.Columns[Event] {

pkg/gadgets/trace/open/tracer/tracer.go
@@ -183,6 +183,8 @@ func (t *Tracer) run() {
 			Ret:           ret,
 			Fd:            fd,
 			Err:           errval,
+			FlagsRaw:      bpfEvent.Flags,
+			Flags:         DecodeFlags(bpfEvent.Flags),
 			Path:          gadgets.FromCString(bpfEvent.Fname[:]),
 		}
 

pkg/gadgets/trace/open/tracer/tracer_test.go
@@ -102,6 +102,7 @@ func TestOpenTracer(t *testing.T) {
 					Ret:           fd,
 					Err:           0,
 					Path:          "/dev/null",
+					Flags:         []string{"O_RDONLY"},
 				}
 			}),
 		},
@@ -134,6 +135,7 @@ func TestOpenTracer(t *testing.T) {
 					Ret:           fd,
 					Err:           0,
 					Path:          "/dev/null",
+					Flags:         []string{"O_RDONLY"},
 				}
 			}),
 		},

pkg/gadgets/trace/open/tracer/utils.go
@@ -1,0 +1,60 @@
+// Copyright 2023 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tracer
+
+var flagNames = []string{
+	"O_CREAT",
+	"O_EXCL",
+	"O_NOCTTY",
+	"O_TRUNC",
+	"O_APPEND",
+	"O_NONBLOCK",
+	"O_DSYNC",
+	"O_FASYNC",
+	"O_DIRECT",
+	"O_LARGEFILE",
+	"O_DIRECTORY",
+	"O_NOFOLLOW",
+	"O_NOATIME",
+	"O_CLOEXEC",
+}
+
+func DecodeFlags(flags int32) []string {
+	flagsStr := []string{}
+
+	// We first need to deal with the first 3 bits which indicates access mode.
+	switch flags & 0b11 {
+	case 0:
+		flagsStr = append(flagsStr, "O_RDONLY")
+	case 1:
+		flagsStr = append(flagsStr, "O_WRONLY")
+	case 2:
+		flagsStr = append(flagsStr, "O_RDWR")
+	}
+
+	// Then, we need to remove the last 6 bits and we can deal with the other
+	// flags.
+	// Indeed, O_CREAT is defined as 00000100, see:
+	// https://github.com/torvalds/linux/blob/9d646009f65d/include/uapi/asm-generic/fcntl.h#L24
+	flags >>= 6
+	for i, val := range flagNames {
+		if (1<<i)&flags == 0 {
+			continue
+		}
+		flagsStr = append(flagsStr, val)
+	}
+
+	return flagsStr
+}

commit 8f468fa6115ef289e23526c7b98bbe245977026a

trace/open: Add missing extractor for flags
TL;DR: We're missing the extractor for the flags and they were always
empty in the columns representation.

Before this commit:

```bash
$ sudo ig trace open -o columns=flags
FLAGS

// ^^^^ empty entries
```

After this commit:

```bash
$ sudo ig trace open -o columns=flags
FLAGS
O_RDONLY|O_CLOEXEC
O_RDONLY|O_CLOEXEC
O_RDONLY|O_CLOEXEC
O_RDONLY|O_CLOEXEC
O_RDONLY|O_CLOEXEC
```

Long story:

3c9916449a05 ("pkg: Add support for flags for trace open.") introduced
support for the flag column but it didn't define a custom extractor, at
that time the column was formatted as:

```bash
sudo ig trace open -o columns=flags
FLAGS
[O_RDONLY O_CLOEXEC]
[O_RDONLY O_CLOEXEC]
[O_RDONLY O_CLOEXEC]
[O_RDONLY O_CLOEXEC]
[O_RDONLY O_CLOEXEC]
[O_RDONLY O_CLOEXEC]
```

Later on, 5ad73284af88 ("pkg/columns: add support for dynamic fields")
modified the columns library **not** to format slices to strings, hence
after this commit the column was printed as empty.

It's also possible to fix this on the columns library, but having a
custom extractor allows us to provide a better formatting to the user.

Fixes: 3c9916449a05 ("pkg: Add support for flags for trace open.")
Fixes: 5ad73284af88 ("pkg/columns: add support for dynamic fields")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/gadgets/trace/open/types/types.go
@@ -16,6 +16,7 @@ package types
 
 import (
 	"io/fs"
+	"strings"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
@@ -41,7 +42,13 @@ type Event struct {
 }
 
 func GetColumns() *columns.Columns[Event] {
-	return columns.MustCreateColumns[Event]()
+	openColumns := columns.MustCreateColumns[Event]()
+
+	openColumns.MustSetExtractor("flags", func(event *Event) any {
+		return strings.Join(event.Flags, "|")
+	})
+
+	return openColumns
 }
 
 func Base(ev eventtypes.Event) *Event {