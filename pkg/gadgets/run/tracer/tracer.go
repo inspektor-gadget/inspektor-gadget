inspektor-gadget/inspektor-gadget

commit 07ca0fb4e97ac8d0688fbd056baa2ac1ac147689

Initial support for containerized gadgets
Containerized gadgets is a mechanism to run gadgets that are not builtin
within Inspektor Gadget. This support will allow in the future to
run gadgets developed by different parties. A good analogy is to that
Inspektor Gadget is for eBPF what docker is for containers.

This commit introduces some initial support for this. The supported
features are:
- Loading and attaching the different eBPF programs
- Polling the perf ring buffer and formatting the events based on their
  BTF information
- Enriching and filtering events by mount namespaces inode id

This support is experimental and will be extended / changed in follow
up PRs.

Some limitations are:
- Only tracer-like gadgets are supported

Signed-off-by: Burak Ok <burakok@microsoft.com>
Co-Authored-By: Mauricio Vásquez <mauriciov@microsoft.com>
Co-Authored-By: Alban Crequy <albancrequy@microsoft.com>

pkg/gadgets/run/tracer/tracer.go
@@ -1,0 +1,357 @@
+// Copyright 2023 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build !withoutebpf
+
+package tracer
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/btf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/perf"
+	"github.com/cilium/ebpf/ringbuf"
+	"github.com/solo-io/bumblebee/pkg/decoder"
+	beespec "github.com/solo-io/bumblebee/pkg/spec"
+	orascontent "oras.land/oras-go/pkg/content"
+	"oras.land/oras-go/pkg/oras"
+
+	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/run/types"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+)
+
+const (
+	mntNsIdType = "mnt_ns_id_t"
+)
+
+type Config struct {
+	RegistryAuth orascontent.RegistryOptions
+	ProgLocation string
+	ProgContent  []byte
+	MountnsMap   *ebpf.Map
+}
+
+type Tracer struct {
+	config         *Config
+	eventCallback  func(*types.Event)
+	decoderFactory decoder.DecoderFactory
+
+	spec       *ebpf.CollectionSpec
+	collection *ebpf.Collection
+
+	valueStruct       *btf.Struct
+	ringbufReader     *ringbuf.Reader
+	perfReader        *perf.Reader
+	printMapValueSize uint32
+
+	links []link.Link
+}
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	tracer := &Tracer{
+		config:         &Config{},
+		decoderFactory: decoder.NewDecoderFactory(),
+	}
+	return tracer, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	return nil
+}
+
+// Close is needed because of the StartStopGadget interface
+func (t *Tracer) Close() {
+}
+
+func (t *Tracer) getByobEbpfPackage() (*beespec.EbpfPackage, error) {
+	localRegistry := orascontent.NewMemory()
+
+	remoteRegistry, err := orascontent.NewRegistry(t.config.RegistryAuth)
+	if err != nil {
+		return nil, fmt.Errorf("create new oras registry: %w", err)
+	}
+
+	_, err = oras.Copy(
+		context.Background(),
+		remoteRegistry,
+		t.config.ProgLocation,
+		localRegistry,
+		t.config.ProgLocation,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("copy oras: %w", err)
+	}
+	byobClient := beespec.NewEbpfOCICLient()
+	return byobClient.Pull(context.Background(), t.config.ProgLocation, localRegistry)
+}
+
+func (t *Tracer) Stop() {
+	if t.collection != nil {
+		t.collection.Close()
+		t.collection = nil
+	}
+	for _, l := range t.links {
+		gadgets.CloseLink(l)
+	}
+	t.links = nil
+
+	if t.ringbufReader != nil {
+		t.ringbufReader.Close()
+	}
+	if t.perfReader != nil {
+		t.perfReader.Close()
+	}
+}
+
+func (t *Tracer) installTracer() error {
+	// Load the spec
+	var err error
+	t.spec, err = loadSpec(t.config.ProgContent)
+	if err != nil {
+		return err
+	}
+
+	mapReplacements := map[string]*ebpf.Map{}
+	consts := map[string]interface{}{}
+
+	printMap, err := getPrintMap(t.spec)
+	if err != nil {
+		return fmt.Errorf("get print map: %w", err)
+	}
+
+	var ok bool
+	t.valueStruct, ok = printMap.Value.(*btf.Struct)
+	if !ok {
+		return fmt.Errorf("BPF map %q does not have BTF info for values", printMap.Name)
+	}
+
+	// Almost same hack as in bumblebee/pkg/loader/loader.go
+	t.printMapValueSize = printMap.ValueSize
+	switch printMap.Type {
+	case ebpf.RingBuf:
+		printMap.ValueSize = 0
+	case ebpf.PerfEventArray:
+		printMap.KeySize = 4
+		printMap.ValueSize = 4
+	}
+
+	for _, m := range t.spec.Maps {
+		// Replace filter mount ns map
+		if m.Name == gadgets.MntNsFilterMapName {
+			mapReplacements[gadgets.MntNsFilterMapName] = t.config.MountnsMap
+			consts[gadgets.FilterByMntNsName] = true
+		}
+	}
+
+	if err := t.spec.RewriteConstants(consts); err != nil {
+		return fmt.Errorf("rewriting constants: %w", err)
+	}
+
+	// Load the ebpf objects
+	opts := ebpf.CollectionOptions{
+		MapReplacements: mapReplacements,
+	}
+	t.collection, err = ebpf.NewCollectionWithOptions(t.spec, opts)
+	if err != nil {
+		return fmt.Errorf("create BPF collection: %w", err)
+	}
+
+	m := t.collection.Maps[printMap.Name]
+	switch m.Type() {
+	case ebpf.RingBuf:
+		t.ringbufReader, err = ringbuf.NewReader(t.collection.Maps[printMap.Name])
+	case ebpf.PerfEventArray:
+		t.perfReader, err = perf.NewReader(t.collection.Maps[printMap.Name], gadgets.PerfBufferPages*os.Getpagesize())
+	}
+	if err != nil {
+		return fmt.Errorf("create BPF map reader: %w", err)
+	}
+
+	// Attach programs
+	for progName, p := range t.spec.Programs {
+		if p.Type == ebpf.Kprobe && strings.HasPrefix(p.SectionName, "kprobe/") {
+			l, err := link.Kprobe(p.AttachTo, t.collection.Programs[progName], nil)
+			if err != nil {
+				return fmt.Errorf("attach BPF program %q: %w", progName, err)
+			}
+			t.links = append(t.links, l)
+		} else if p.Type == ebpf.Kprobe && strings.HasPrefix(p.SectionName, "kretprobe/") {
+			l, err := link.Kretprobe(p.AttachTo, t.collection.Programs[progName], nil)
+			if err != nil {
+				return fmt.Errorf("attach BPF program %q: %w", progName, err)
+			}
+			t.links = append(t.links, l)
+		} else if p.Type == ebpf.TracePoint && strings.HasPrefix(p.SectionName, "tracepoint/") {
+			parts := strings.Split(p.AttachTo, "/")
+			l, err := link.Tracepoint(parts[0], parts[1], t.collection.Programs[progName], nil)
+			if err != nil {
+				return fmt.Errorf("attach BPF program %q: %w", progName, err)
+			}
+			t.links = append(t.links, l)
+		}
+	}
+
+	return nil
+}
+
+func (t *Tracer) run(gadgetCtx gadgets.GadgetContext) {
+	typ := t.valueStruct
+
+	var mntNsIdstart, mntNsIdend uint32
+
+	// we suppose the same data structure is always used, so we can precalculate the offsets for
+	// the mount ns id
+	for _, member := range typ.Members {
+		if member.Type.TypeName() != mntNsIdType {
+			continue
+		}
+
+		typDef, ok := member.Type.(*btf.Typedef)
+		if !ok {
+			continue
+		}
+
+		underlying, err := getUnderlyingType(typDef)
+		if err != nil {
+			continue
+		}
+
+		intM, ok := underlying.(*btf.Int)
+		if !ok {
+			continue
+		}
+
+		if intM.Size != 8 {
+			continue
+		}
+
+		mntNsIdstart = member.Offset.Bytes()
+		mntNsIdend = mntNsIdstart + intM.Size
+	}
+
+	for {
+		var rawSample []byte
+
+		if t.ringbufReader != nil {
+			record, err := t.ringbufReader.Read()
+			if err != nil {
+				if errors.Is(err, ringbuf.ErrClosed) {
+					// nothing to do, we're done
+					return
+				}
+				gadgetCtx.Logger().Errorf("read ring buffer: %w", err)
+				return
+			}
+			rawSample = record.RawSample
+		} else if t.perfReader != nil {
+			record, err := t.perfReader.Read()
+			if err != nil {
+				if errors.Is(err, perf.ErrClosed) {
+					return
+				}
+				gadgetCtx.Logger().Errorf("read perf ring buffer: %w", err)
+				return
+			}
+
+			if record.LostSamples != 0 {
+				gadgetCtx.Logger().Warnf("lost %d samples", record.LostSamples)
+				continue
+			}
+			rawSample = record.RawSample
+		}
+
+		// TODO: this check is not valid for all cases. For instance trace exec sends a variable length
+		if uint32(len(rawSample)) < t.printMapValueSize {
+			gadgetCtx.Logger().Errorf("read ring buffer: len(RawSample)=%d!=%d",
+				len(rawSample), t.printMapValueSize)
+			return
+		}
+
+		// data will be decoded in the client
+		data := rawSample[:t.printMapValueSize]
+
+		// get mnt_ns_id for enriching the event
+		mtn_ns_id := uint64(0)
+		if mntNsIdend != 0 {
+			buf := bytes.NewBuffer(data[mntNsIdstart:mntNsIdend])
+			// TODO: is binary.LittleEndian correct?
+			if err := binary.Read(buf, binary.LittleEndian, &mtn_ns_id); err != nil {
+				continue
+			}
+		}
+
+		event := types.Event{
+			Event: eventtypes.Event{
+				Type: eventtypes.NORMAL,
+			},
+			WithMountNsID: eventtypes.WithMountNsID{MountNsID: mtn_ns_id},
+			RawData:       data,
+		}
+
+		t.eventCallback(&event)
+	}
+}
+
+func (t *Tracer) Run(gadgetCtx gadgets.GadgetContext) error {
+	params := gadgetCtx.GadgetParams()
+	if len(params.Get(ProgramContent).AsBytes()) != 0 {
+		t.config.ProgContent = params.Get(ProgramContent).AsBytes()
+	} else {
+		args := gadgetCtx.Args()
+		if len(args) != 1 {
+			return fmt.Errorf("expected exactly one argument, got %d", len(args))
+		}
+
+		param := args[0]
+		t.config.ProgLocation = param
+		// Download the BPF module
+		byobEbpfPackage, err := t.getByobEbpfPackage()
+		if err != nil {
+			return fmt.Errorf("download byob ebpf package: %w", err)
+		}
+		t.config.ProgContent = byobEbpfPackage.ProgramFileBytes
+	}
+
+	if err := t.installTracer(); err != nil {
+		t.Stop()
+		return fmt.Errorf("install tracer: %w", err)
+	}
+
+	go t.run(gadgetCtx)
+	gadgetcontext.WaitForTimeoutOrDone(gadgetCtx)
+
+	return nil
+}
+
+func (t *Tracer) SetMountNsMap(mountnsMap *ebpf.Map) {
+	t.config.MountnsMap = mountnsMap
+}
+
+func (t *Tracer) SetEventHandler(handler any) {
+	nh, ok := handler.(func(ev *types.Event))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventCallback = nh
+}

go.mod
@@ -3,7 +3,7 @@ module github.com/inspektor-gadget/inspektor-gadget
 go 1.19
 
 require (
-	github.com/cilium/ebpf v0.9.1
+	github.com/cilium/ebpf v0.10.0
 	github.com/containerd/nri v0.3.0
 	github.com/containers/common v0.54.0
 	github.com/docker/docker v24.0.2+incompatible
@@ -46,6 +46,7 @@ require (
 	github.com/kr/pretty v0.3.1
 	github.com/moby/moby v24.0.2+incompatible
 	github.com/prometheus/client_golang v1.16.0
+	github.com/solo-io/bumblebee v0.0.14
 	github.com/stretchr/testify v1.8.4
 	github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635
 	github.com/tklauser/numcpus v0.6.1
@@ -57,11 +58,13 @@ require (
 	golang.org/x/text v0.10.0
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/cri-api v0.27.3
+	oras.land/oras-go v1.2.3
 )
 
 require (
 	github.com/AdaLogics/go-fuzz-headers v0.0.0-20230106234847-43070de90fa1 // indirect
 	github.com/AdamKorcz/go-118-fuzz-build v0.0.0-20221215162035-5330a85ea652 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.1.1 // indirect
 	github.com/Masterminds/sprig/v3 v3.2.2 // indirect
@@ -76,9 +79,12 @@ require (
 	github.com/containerd/typeurl/v2 v2.1.1 // indirect
 	github.com/cyphar/filepath-securejoin v0.2.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/cli v23.0.3+incompatible // indirect
 	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.7.0 // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
+	github.com/docker/go-metrics v0.0.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.10.1 // indirect
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
@@ -98,6 +104,7 @@ require (
 	github.com/google/gnostic v0.6.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/imdario/mergo v0.3.15 // indirect
@@ -116,6 +123,7 @@ require (
 	github.com/moby/sys/mountinfo v0.6.2 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
 	github.com/moby/sys/signal v0.7.0 // indirect
+	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
@@ -157,7 +165,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gotest.tools/v3 v3.4.0 // indirect
 	k8s.io/component-base v0.27.2 // indirect
 	k8s.io/gengo v0.0.0-20220902162205-c0856e24416d // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
@@ -169,6 +176,12 @@ require (
 	sigs.k8s.io/release-utils v0.7.3 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 )
+
+// Code available in https://github.com/inspektor-gadget/bumblebee/tree/gadget
+replace github.com/solo-io/bumblebee => github.com/inspektor-gadget/bumblebee v0.0.0-20230627162415-fa5fa890590d
+
+// ../../go/pkg/mod/oras.land/oras-go@v1.2.3/pkg/auth/docker/login.go:86:39: cannot use remote (variable of type *"github.com/docker/docker/registry".Service) as "github.com/docker/docker/registry".Service value in argument to c.loginWithTLS
+replace github.com/docker/docker => github.com/docker/docker v23.0.6+incompatible
 
 replace sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.13.1-0.20230315234915-a26de2d610c3
 

pkg/all-gadgets/allgadgets.go
@@ -56,4 +56,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/tcpconnect/tracer"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/tcpdrop/tracer"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/tcpretrans/tracer"
+
+	// Run gadget
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/run/tracer"
 )

pkg/gadgets/run/tracer/run.go
@@ -1,0 +1,337 @@
+// Copyright 2023 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tracer
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net"
+	"reflect"
+	"strings"
+	"unsafe"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/btf"
+	"github.com/solo-io/bumblebee/pkg/decoder"
+	"gopkg.in/yaml.v3"
+	k8syaml "sigs.k8s.io/yaml"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
+	gadgetregistry "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-registry"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/run/types"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/parser"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/experimental"
+)
+
+const (
+	ProgramContent  = "prog"
+	ParamDefinition = "definition"
+	printMapPrefix  = "print_"
+)
+
+type GadgetDesc struct{}
+
+func (g *GadgetDesc) Name() string {
+	return "run"
+}
+
+func (g *GadgetDesc) Category() string {
+	return gadgets.CategoryNone
+}
+
+func (g *GadgetDesc) Type() gadgets.GadgetType {
+	// Currently trace only
+	return gadgets.TypeTrace
+}
+
+func (g *GadgetDesc) Description() string {
+	return "Run a containerized gadget"
+}
+
+func (g *GadgetDesc) ParamDescs() params.ParamDescs {
+	return params.ParamDescs{
+		{
+			Key:         ProgramContent,
+			Title:       "eBPF program",
+			Description: "Compiled eBPF program",
+			TypeHint:    params.TypeBytes,
+		},
+		{
+			Key:         ParamDefinition,
+			Title:       "Gadget definition",
+			Description: "Gadget definition in yaml format",
+			TypeHint:    params.TypeBytes,
+		},
+	}
+}
+
+func (g *GadgetDesc) Parser() parser.Parser {
+	return nil
+}
+
+func getUnderlyingType(tf *btf.Typedef) (btf.Type, error) {
+	switch typedMember := tf.Type.(type) {
+	case *btf.Typedef:
+		return getUnderlyingType(typedMember)
+	default:
+		return typedMember, nil
+	}
+}
+
+func loadSpec(progContent []byte) (*ebpf.CollectionSpec, error) {
+	progReader := bytes.NewReader(progContent)
+	spec, err := ebpf.LoadCollectionSpecFromReader(progReader)
+	if err != nil {
+		return nil, fmt.Errorf("loading spec: %w", err)
+	}
+	return spec, err
+}
+
+func getPrintMap(spec *ebpf.CollectionSpec) (*ebpf.MapSpec, error) {
+	for _, m := range spec.Maps {
+		if m.Type != ebpf.RingBuf && m.Type != ebpf.PerfEventArray {
+			continue
+		}
+
+		if !strings.HasPrefix(m.Name, printMapPrefix) {
+			continue
+		}
+
+		return m, nil
+	}
+
+	return nil, fmt.Errorf("no BPF map with %q prefix found", printMapPrefix)
+}
+
+func getValueStructBTF(progContent []byte) (*btf.Struct, error) {
+	spec, err := loadSpec(progContent)
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := getPrintMap(spec)
+	if err != nil {
+		return nil, err
+	}
+
+	var valueStruct *btf.Struct
+	var ok bool
+	valueStruct, ok = m.Value.(*btf.Struct)
+	if !ok {
+		return nil, fmt.Errorf("BPF map %q does not have BTF info for values", m.Name)
+	}
+
+	return valueStruct, nil
+}
+
+func getType(typ btf.Type) reflect.Type {
+	switch typedMember := typ.(type) {
+	case *btf.Array:
+		arrType := getSimpleType(typedMember.Type)
+		return reflect.ArrayOf(int(typedMember.Nelems), arrType)
+	default:
+		return getSimpleType(typ)
+	}
+}
+
+func getSimpleType(typ btf.Type) reflect.Type {
+	switch typedMember := typ.(type) {
+	case *btf.Int:
+		switch typedMember.Encoding {
+		case btf.Signed:
+			switch typedMember.Size {
+			case 1:
+				return reflect.TypeOf(int8(0))
+			case 2:
+				return reflect.TypeOf(int16(0))
+			case 4:
+				return reflect.TypeOf(int32(0))
+			case 8:
+				return reflect.TypeOf(int64(0))
+			}
+		case btf.Unsigned:
+			switch typedMember.Size {
+			case 1:
+				return reflect.TypeOf(uint8(0))
+			case 2:
+				return reflect.TypeOf(uint16(0))
+			case 4:
+				return reflect.TypeOf(uint32(0))
+			case 8:
+				return reflect.TypeOf(uint64(0))
+			}
+		case btf.Bool:
+			return reflect.TypeOf(bool(false))
+		case btf.Char:
+			return reflect.TypeOf(uint8(0))
+		}
+	case *btf.Float:
+		switch typedMember.Size {
+		case 4:
+			return reflect.TypeOf(float32(0))
+		case 8:
+			return reflect.TypeOf(float64(0))
+		}
+	case *btf.Typedef:
+		typ, _ := getUnderlyingType(typedMember)
+		return getSimpleType(typ)
+	}
+
+	return nil
+}
+
+func (g *GadgetDesc) CustomParser(params *params.Params, args []string) (parser.Parser, error) {
+	if len(args) != 0 {
+		return nil, fmt.Errorf("no arguments expected: received %d", len(args))
+	}
+	progContent := params.Get(ProgramContent).AsBytes()
+	definitionBytes := params.Get(ParamDefinition).AsBytes()
+	if len(definitionBytes) == 0 {
+		return nil, fmt.Errorf("no definition provided")
+	}
+
+	valueStruct, err := getValueStructBTF(progContent)
+	if err != nil {
+		return nil, fmt.Errorf("getting value struct: %w", err)
+	}
+
+	cols := types.GetColumns()
+
+	var gadgetDefinition types.GadgetDefinition
+
+	if err := yaml.Unmarshal(definitionBytes, &gadgetDefinition); err != nil {
+		return nil, fmt.Errorf("unmarshaling definition: %w", err)
+	}
+
+	colAttrs := map[string]columns.Attributes{}
+	for _, col := range gadgetDefinition.ColumnsAttrs {
+		colAttrs[col.Name] = col
+	}
+
+	fields := []columns.DynamicField{}
+
+	for _, member := range valueStruct.Members {
+		member := member
+
+		attrs, ok := colAttrs[member.Name]
+		if !ok {
+			continue
+		}
+
+		switch typedMember := member.Type.(type) {
+		case *btf.Union:
+			if typedMember.Name == "ip_addr" {
+				cols.AddColumn(attrs, func(ev *types.Event) string {
+					// TODO: Handle IPv6
+					offset := uintptr(member.Offset.Bytes())
+					ipSlice := unsafe.Slice(&ev.RawData[offset], 4)
+					ipBytes := make(net.IP, 4)
+					copy(ipBytes, ipSlice)
+					return ipBytes.String()
+				})
+				continue
+			}
+		}
+
+		rType := getType(member.Type)
+		if rType == nil {
+			continue
+		}
+
+		field := columns.DynamicField{
+			Attributes: &attrs,
+			// TODO: remove once this is part of attributes
+			Template: attrs.Template,
+			Type:     rType,
+			Offset:   uintptr(member.Offset.Bytes()),
+		}
+
+		fields = append(fields, field)
+	}
+
+	base := func(ev *types.Event) unsafe.Pointer {
+		return unsafe.Pointer(&ev.RawData[0])
+	}
+	if err := cols.AddFields(fields, base); err != nil {
+		return nil, fmt.Errorf("adding fields: %w", err)
+	}
+
+	return parser.NewParser[types.Event](cols), nil
+}
+
+func genericConverter(params *params.Params, printer gadgets.Printer, convert func(any) ([]byte, error)) func(ev any) {
+	decoderFactory := decoder.NewDecoderFactory()()
+
+	// TODO: add support for OCI programs
+	progContent := params.Get(ProgramContent).AsBytes()
+
+	valueStruct, err := getValueStructBTF(progContent)
+	if err != nil {
+		printer.Logf(logger.WarnLevel, "coult not get value struct BTF: %s", err)
+		return nil
+	}
+
+	ctx := context.TODO()
+
+	return func(ev any) {
+		event := ev.(*types.Event)
+
+		result, err := decoderFactory.DecodeBtfBinary(ctx, valueStruct, event.RawData)
+		if err != nil {
+			printer.Logf(logger.WarnLevel, "decoding %+v: %s", ev, err)
+			return
+		}
+
+		// TODO: flatten the results?
+		event.Data = result
+
+		d, err := convert(event)
+		if err != nil {
+			printer.Logf(logger.WarnLevel, "marshalling %+v: %s", ev, err)
+			return
+		}
+		printer.Output(string(d))
+	}
+}
+
+func (g *GadgetDesc) JSONConverter(params *params.Params, printer gadgets.Printer) func(ev any) {
+	return genericConverter(params, printer, json.Marshal)
+}
+
+func (g *GadgetDesc) JSONPrettyConverter(params *params.Params, printer gadgets.Printer) func(ev any) {
+	convert := func(ev any) ([]byte, error) {
+		return json.MarshalIndent(ev, "", "  ")
+	}
+	return genericConverter(params, printer, convert)
+}
+
+func (g *GadgetDesc) YAMLConverter(params *params.Params, printer gadgets.Printer) func(ev any) {
+	return genericConverter(params, printer, k8syaml.Marshal)
+}
+
+func (g *GadgetDesc) EventPrototype() any {
+	return &types.Event{}
+}
+
+func init() {
+	if experimental.Enabled() {
+		gadgetregistry.Register(&GadgetDesc{})
+	}
+}

pkg/gadgets/run/types/types.go
@@ -1,0 +1,45 @@
+// Copyright 2023 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+)
+
+type Event struct {
+	eventtypes.Event
+	eventtypes.WithMountNsID
+	// Raw event sent by the ebpf program
+	RawData []byte `json:"raw_data"`
+	// How to flatten this?
+	Data interface{} `json:"data"`
+}
+
+func GetColumns() *columns.Columns[Event] {
+	return columns.MustCreateColumns[Event]()
+}
+
+func Base(ev eventtypes.Event) *Event {
+	return &Event{
+		Event: ev,
+	}
+}
+
+type GadgetDefinition struct {
+	Name         string               `yaml:"name"`
+	Description  string               `yaml:"description"`
+	ColumnsAttrs []columns.Attributes `yaml:"columns"`
+}

commit 456cc6ae968af3746bfa72832f1e834dfdc92870

run: Fix lot of leakages
The GadgetStartStop interface was removed a long time ago in a3e5871a338a ("pkg/runtime/local: Move context and timeout management into tracers"),
so the Stop() method was never being called. It was causing to leak
all BPF objects of the gadgets being run.

This commit also fixes a leakage on the network tracer as it was never
being closed.

Fixes: 07ca0fb4e97a ("Initial support for containerized gadgets")
Fixes: c1cc966f4f16 ("implement networking gadgets for containerized gadgets")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/gadgets/run/tracer/tracer.go
@@ -124,11 +124,7 @@ func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
 	return nil
 }
 
-// Close is needed because of the StartStopGadget interface
 func (t *Tracer) Close() {
-}
-
-func (t *Tracer) Stop() {
 	if t.collection != nil {
 		t.collection.Close()
 		t.collection = nil
@@ -146,6 +142,9 @@ func (t *Tracer) Stop() {
 	}
 	if t.socketEnricher != nil {
 		t.socketEnricher.Close()
+	}
+	if t.networkTracer != nil {
+		t.networkTracer.Close()
 	}
 }
 
@@ -599,7 +598,7 @@ func (t *Tracer) Run(gadgetCtx gadgets.GadgetContext) error {
 	t.config.Metadata = info.GadgetMetadata
 
 	if err := t.installTracer(params); err != nil {
-		t.Stop()
+		t.Close()
 		return fmt.Errorf("install tracer: %w", err)
 	}
 