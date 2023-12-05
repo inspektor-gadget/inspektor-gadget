inspektor-gadget/inspektor-gadget

commit f0884013d6d4ea75dc384ac285c06b195c19ba4a

socketenricher: Catch SpecUpdateAddresses() error.
This way, we can know if there was a problem with kallsyms which would lead to
later enrichment problems.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/internal/socketenricher/tracer.go
@@ -72,7 +72,10 @@ func (se *SocketEnricher) start() error {
 		return fmt.Errorf("loading socketsiter asset: %w", err)
 	}
 
-	kallsyms.SpecUpdateAddresses(specIter, []string{"socket_file_ops"})
+	err = kallsyms.SpecUpdateAddresses(specIter, []string{"socket_file_ops"})
+	if err != nil {
+		return fmt.Errorf("updating socket_file_ops address with ksyms: %w", err)
+	}
 
 	opts := ebpf.CollectionOptions{
 		Programs: ebpf.ProgramOptions{

pkg/gadgets/top/ebpf/piditer/iter.go
@@ -58,7 +58,7 @@ func NewTracer() (iter *PidIter, err error) {
 	}
 
 	if err := kallsyms.SpecUpdateAddresses(spec, []string{"bpf_prog_fops"}); err != nil {
-		return nil, fmt.Errorf("updating ksyms: %w", err)
+		return nil, fmt.Errorf("updating bpf_prog_fops address with ksyms: %w", err)
 	}
 
 	opts := ebpf.CollectionOptions{}

commit 681c3701c31e0f760a81d603f5b2249c29a5b2be

socketenricher: Print warning rather than error when problem with kallsyms.
Fixes: f0884013d6d4 ("socketenricher: Catch SpecUpdateAddresses() error.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/internal/socketenricher/tracer.go
@@ -74,7 +74,9 @@ func (se *SocketEnricher) start() error {
 
 	err = kallsyms.SpecUpdateAddresses(specIter, []string{"socket_file_ops"})
 	if err != nil {
-		return fmt.Errorf("updating socket_file_ops address with ksyms: %w", err)
+		// Being unable to access to /proc/kallsyms can be caused by not having
+		// CAP_SYSLOG.
+		log.Warnf("updating socket_file_ops address with ksyms: %v\nEither you cannot access /proc/kallsyms or this file does not contain socket_file_ops", err)
 	}
 
 	opts := ebpf.CollectionOptions{