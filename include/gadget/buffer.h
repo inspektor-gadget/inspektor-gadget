inspektor-gadget/inspektor-gadget

commit ba2bfca2aa77cb80e1b64242783df974263c7d22

run: Add functions to use perf or ring buffer.
eBPF functions are inspired by iovisor/bcc code [1].

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
[1]: https://github.com/iovisor/bcc/pull/4262

include/gadget/buffer.h
@@ -1,0 +1,48 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (c) 2022 Hengqi Chen */
+
+#ifndef __BUFFER_BPF_H
+#define __BUFFER_BPF_H
+
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+
+#ifndef MAX_EVENT_SIZE
+#define MAX_EVENT_SIZE		10240
+#endif
+
+#define GADGET_TRACER_MAP(name, size)			\
+	struct {					\
+		__uint(type, BPF_MAP_TYPE_RINGBUF);	\
+		__uint(max_entries, size);		\
+	} name SEC(".maps");				\
+	const void *gadget_map_tracer_##name __attribute__((unused));
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__uint(key_size, sizeof(__u32));
+	__uint(value_size, MAX_EVENT_SIZE);
+} gadget_heap SEC(".maps");
+
+static __always_inline void *gadget_reserve_buf(void *map, __u64 size)
+{
+	static const int zero = 0;
+
+	if (bpf_core_type_exists(struct bpf_ringbuf))
+		return bpf_ringbuf_reserve(map, size, 0);
+
+	return bpf_map_lookup_elem(&gadget_heap, &zero);
+}
+
+static __always_inline long gadget_submit_buf(void *ctx, void *map, void *buf, __u64 size)
+{
+	if (bpf_core_type_exists(struct bpf_ringbuf)) {
+		bpf_ringbuf_submit(buf, 0);
+		return 0;
+	}
+
+	return bpf_perf_event_output(ctx, map, BPF_F_CURRENT_CPU, buf, size);
+}
+
+#endif /* __BUFFER_BPF_H */

pkg/gadgets/run/tracer/tracer.go
@@ -22,6 +22,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"slices"
 	"strings"
 	"sync"
 	"unsafe"
@@ -31,6 +32,8 @@ import (
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/perf"
 	"github.com/cilium/ebpf/ringbuf"
+
+	log "github.com/sirupsen/logrus"
 
 	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
 	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
@@ -175,6 +178,73 @@ func (t *Tracer) Close() {
 	for _, networkTracer := range t.networkTracers {
 		networkTracer.Close()
 	}
+}
+
+var (
+	onceRingbuf      sync.Once
+	ringbufAvailable bool
+)
+
+func isRingbufAvailable() bool {
+	onceRingbuf.Do(func() {
+		ringbuf, err := ebpf.NewMap(&ebpf.MapSpec{
+			Type:       ebpf.RingBuf,
+			MaxEntries: uint32(os.Getpagesize()),
+		})
+
+		ringbuf.Close()
+
+		ringbufAvailable = err == nil
+	})
+
+	return ringbufAvailable
+}
+
+// createdByTracerMapMacro returns whether the tracer map was created using
+// GADGET_TRACER_MAP().
+func (t *Tracer) createdByTracerMapMacro(tracerMapName string) bool {
+	results, err := types.GetGadgetIdentByPrefix(t.spec, types.TracerMapPrefix)
+	if err != nil {
+		return false
+	}
+
+	return slices.Contains(results, tracerMapName)
+}
+
+func (t *Tracer) handleTracerMapDefinition(tracerMapName string) error {
+	if !isRingbufAvailable() {
+		if tracerMapName != "" {
+			log.Debugf("Ring buffers are not available, defaulting to perf ones")
+
+			bufMap, ok := t.spec.Maps[tracerMapName]
+			if !ok {
+				return fmt.Errorf("no buffer map named %s", tracerMapName)
+			}
+
+			bufMap.Type = ebpf.PerfEventArray
+			bufMap.KeySize = 4
+			bufMap.ValueSize = 4
+
+			t.spec.Maps[tracerMapName] = bufMap
+		}
+	} else {
+		heapMapName := "gadget_heap"
+		// If we delete the gadget_heap map, the verifier will not load the code
+		// because it cannot find it despite the code using it being dead one:
+		// ...: create BPF collection: program ig_mount_x: missing map gadget_heap
+		// Rather than deleting it, we just set its size to 0 and use a hash to
+		// avoid having one struct per CPU, i.e. reducing as much as possible the
+		// memory footprint it uses.
+		heapMap, ok := t.spec.Maps[heapMapName]
+		if ok {
+			heapMap.Type = ebpf.Hash
+			heapMap.ValueSize = 4
+
+			t.spec.Maps[heapMapName] = heapMap
+		}
+	}
+
+	return nil
 }
 
 type loadingOptions struct {

pkg/gadgets/run/types/metadata.go
@@ -38,6 +38,10 @@ const (
 
 	// Prefix used to mark snapshotters structs
 	snapshottersPrefix = "gadget_snapshotter_"
+
+	// Prefix used to mark tracer map created with GADGET_TRACER_MAP() defined in
+	// include/gadget/buffer.h.
+	TracerMapPrefix = "gadget_map_tracer_"
 )
 
 // Keep this aligned with include/gadget/types.h
@@ -422,7 +426,6 @@ func GetGadgetIdentByPrefix(spec *ebpf.CollectionSpec, prefix string) ([]string,
 		if !strings.HasPrefix(btfVar.Name, prefix) {
 			continue
 		}
-
 		if btfVar.Linkage != btf.GlobalVar {
 			resultError = multierror.Append(resultError, fmt.Errorf("%q is not a global variable", btfVar.Name))
 		}

commit 0d8b2276c1548c43ecc93de99d0e2fa6f8a6728a

include/gadget: Rework buffer.h to check enum bpf_func_id.
If we check that struct bpf_ringbuf exists, it can cause troubles while
including other headers.
So, we removed <vmlinux.h> and check that the corresponding functions ID exist
in enum bpf_func_id.

Fixes: ba2bfca2aa77 ("run: Add functions to use perf or ring buffer.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

include/gadget/buffer.h
@@ -4,8 +4,8 @@
 #ifndef __BUFFER_BPF_H
 #define __BUFFER_BPF_H
 
-#include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
 
 #ifndef MAX_EVENT_SIZE
 #define MAX_EVENT_SIZE		10240
@@ -29,7 +29,7 @@ static __always_inline void *gadget_reserve_buf(void *map, __u64 size)
 {
 	static const int zero = 0;
 
-	if (bpf_core_type_exists(struct bpf_ringbuf))
+	if (bpf_core_enum_value_exists(enum bpf_func_id, BPF_FUNC_ringbuf_reserve))
 		return bpf_ringbuf_reserve(map, size, 0);
 
 	return bpf_map_lookup_elem(&gadget_heap, &zero);
@@ -37,7 +37,7 @@ static __always_inline void *gadget_reserve_buf(void *map, __u64 size)
 
 static __always_inline long gadget_submit_buf(void *ctx, void *map, void *buf, __u64 size)
 {
-	if (bpf_core_type_exists(struct bpf_ringbuf)) {
+	if (bpf_core_enum_value_exists(enum bpf_func_id, BPF_FUNC_ringbuf_submit)) {
 		bpf_ringbuf_submit(buf, 0);
 		return 0;
 	}