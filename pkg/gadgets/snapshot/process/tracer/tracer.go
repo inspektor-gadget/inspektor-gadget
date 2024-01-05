inspektor-gadget/inspektor-gadget

commit 2ad8b61b0539cbf3ab1e9cab5fc9c341aa08f429

pkg: Add bound checks for integer converted by strconv functions.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/snapshot/process/tracer/tracer.go
@@ -19,6 +19,7 @@ package tracer
 import (
 	"errors"
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -27,6 +28,8 @@ import (
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
+
+	log "github.com/sirupsen/logrus"
 
 	containerutils "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
@@ -184,6 +187,12 @@ func getPidEvents(config *Config, enricher gadgets.DataEnricherByMntNs, pid int)
 		if err != nil {
 			continue
 		}
+
+		if tid64 > math.MaxUint32 {
+			log.Errorf("TID (%d) exceeds math.MaxUint32 (%d)", tid64, math.MaxUint32)
+			continue
+		}
+
 		tid := int(tid64)
 		event, err := getTidEvent(config, enricher, pid, tid)
 		if err != nil {
@@ -213,6 +222,12 @@ func runProcfsCollector(config *Config, enricher gadgets.DataEnricherByMntNs) ([
 		if err != nil {
 			continue
 		}
+
+		if pid64 > math.MaxUint32 {
+			log.Errorf("PID (%d) exceeds math.MaxUint32 (%d)", pid64, math.MaxUint32)
+			continue
+		}
+
 		pid := int(pid64)
 
 		if config.ShowThreads {

pkg/runtime/grpc/grpc-runtime.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"net"
 	"net/url"
 	"strings"
@@ -277,7 +278,11 @@ func (r *Runtime) getTargets(ctx context.Context, params *params.Params) ([]targ
 }
 
 func (r *Runtime) GetGadgetInfo(ctx context.Context, desc gadgets.GadgetDesc, gadgetParams *params.Params, args []string) (*runTypes.GadgetInfo, error) {
-	ctx, cancelDial := context.WithTimeout(ctx, time.Second*time.Duration(r.globalParams.Get(ParamConnectionTimeout).AsUint()))
+	duration := r.globalParams.Get(ParamConnectionTimeout).AsUint()
+	if duration > math.MaxInt64 {
+		return nil, fmt.Errorf("duration (%d) exceeds math.MaxInt64 (%d)", duration, math.MaxInt64)
+	}
+	ctx, cancelDial := context.WithTimeout(ctx, time.Second*time.Duration(duration))
 	defer cancelDial()
 
 	// use default params for now
@@ -341,7 +346,12 @@ func (r *Runtime) getConnToRandomTarget(ctx context.Context, runtimeParams *para
 	target := targets[0]
 	log.Debugf("using target %q (%q)", target.addressOrPod, target.node)
 
-	timeout := time.Second * time.Duration(r.globalParams.Get(ParamConnectionTimeout).AsUint())
+	duration := r.globalParams.Get(ParamConnectionTimeout).AsUint()
+	if duration > math.MaxInt64 {
+		return nil, fmt.Errorf("duration (%d) exceeds math.MaxInt64 (%d)", duration, math.MaxInt64)
+	}
+
+	timeout := time.Second * time.Duration(duration)
 	conn, err := r.dialContext(ctx, target, timeout)
 	if err != nil {
 		return nil, fmt.Errorf("dialing %q (%q): %w", target.addressOrPod, target.node, err)

pkg/gadgets/top/ebpf/tracer/tracer.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -182,7 +183,13 @@ func getPidMapFromProcFs() (map[uint32][]*types.Process, error) {
 		}
 		for _, fd := range fdescs {
 			if progID, err := getProgIDFromFile(filepath.Join(host.HostProcFs, p.Name(), "fdinfo", fd.Name())); err == nil {
-				pid, _ := strconv.ParseUint(p.Name(), 10, 32)
+				pid, err := strconv.ParseUint(p.Name(), 10, 32)
+				if err != nil {
+					return nil, err
+				}
+				if pid > math.MaxInt32 {
+					return nil, fmt.Errorf("PID (%d) exceeds math.MaxInt32 (%d)", pid, math.MaxInt32)
+				}
 				if _, ok := pidmap[progID]; !ok {
 					pidmap[progID] = make([]*types.Process, 0, 1)
 				}

pkg/runtime/grpc/info.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -60,7 +61,11 @@ func (r *Runtime) UpdateDeployInfo() error {
 }
 
 func (r *Runtime) loadRemoteDeployInfo() (*deployinfo.DeployInfo, error) {
-	timeout := time.Second * time.Duration(r.globalParams.Get(ParamConnectionTimeout).AsUint())
+	duration := r.globalParams.Get(ParamConnectionTimeout).AsUint()
+	if duration > math.MaxInt64 {
+		return nil, fmt.Errorf("duration (%d) exceeds math.MaxInt64 (%d)", duration, math.MaxInt64)
+	}
+	timeout := time.Second * time.Duration(duration)
 	ctx, cancelDial := context.WithTimeout(context.Background(), timeout)
 	defer cancelDial()
 

commit e33ef85661869bafb2d4d11e769d31ab85a3965e

Fix boundary checks
- Update timeout parameter to be Uint16 on pkg/runtime/grpc/grpc-runtime.go
- Use ParseInt() instead of ParseUint() in pkg/gadgets/snapshot/process/tracer/tracer.go

https://github.com/inspektor-gadget/inspektor-gadget/security/code-scanning/12
https://github.com/inspektor-gadget/inspektor-gadget/security/code-scanning/13
https://github.com/inspektor-gadget/inspektor-gadget/security/code-scanning/14

Fixes: 2ad8b61b0539 ("pkg: Add bound checks for integer converted by strconv functions.")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/gadgets/snapshot/process/tracer/tracer.go
@@ -19,7 +19,6 @@ package tracer
 import (
 	"errors"
 	"fmt"
-	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -28,8 +27,6 @@ import (
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
-
-	log "github.com/sirupsen/logrus"
 
 	containerutils "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
@@ -183,18 +180,12 @@ func getPidEvents(config *Config, enricher gadgets.DataEnricherByMntNs, pid int)
 			continue
 		}
 
-		tid64, err := strconv.ParseUint(item.Name(), 10, 32)
+		tid, err := strconv.ParseInt(item.Name(), 10, strconv.IntSize)
 		if err != nil {
 			continue
 		}
 
-		if tid64 > math.MaxUint32 {
-			log.Errorf("TID (%d) exceeds math.MaxUint32 (%d)", tid64, math.MaxUint32)
-			continue
-		}
-
-		tid := int(tid64)
-		event, err := getTidEvent(config, enricher, pid, tid)
+		event, err := getTidEvent(config, enricher, pid, int(tid))
 		if err != nil {
 			continue
 		}
@@ -218,26 +209,19 @@ func runProcfsCollector(config *Config, enricher gadgets.DataEnricherByMntNs) ([
 			continue
 		}
 
-		pid64, err := strconv.ParseUint(item.Name(), 10, 32)
+		pid, err := strconv.ParseInt(item.Name(), 10, strconv.IntSize)
 		if err != nil {
 			continue
 		}
 
-		if pid64 > math.MaxUint32 {
-			log.Errorf("PID (%d) exceeds math.MaxUint32 (%d)", pid64, math.MaxUint32)
-			continue
-		}
-
-		pid := int(pid64)
-
 		if config.ShowThreads {
-			pidEvents, err := getPidEvents(config, enricher, pid)
+			pidEvents, err := getPidEvents(config, enricher, int(pid))
 			if err != nil {
 				continue
 			}
 			events = append(events, pidEvents...)
 		} else {
-			event, err := getTidEvent(config, enricher, pid, pid)
+			event, err := getTidEvent(config, enricher, int(pid), int(pid))
 			if err != nil {
 				continue
 			}

pkg/runtime/grpc/grpc-runtime.go
@@ -21,7 +21,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"math"
 	"net"
 	"net/url"
 	"strings"
@@ -159,7 +158,7 @@ func (r *Runtime) GlobalParamDescs() params.ParamDescs {
 			Key:          ParamConnectionTimeout,
 			Description:  "Maximum time to establish a connection to remote target in seconds",
 			DefaultValue: fmt.Sprintf("%d", ConnectTimeout),
-			TypeHint:     params.TypeUint,
+			TypeHint:     params.TypeUint16,
 		},
 	}
 	switch r.connectionMode {
@@ -278,11 +277,8 @@ func (r *Runtime) getTargets(ctx context.Context, params *params.Params) ([]targ
 }
 
 func (r *Runtime) GetGadgetInfo(ctx context.Context, desc gadgets.GadgetDesc, gadgetParams *params.Params, args []string) (*runTypes.GadgetInfo, error) {
-	duration := r.globalParams.Get(ParamConnectionTimeout).AsUint()
-	if duration > math.MaxInt64 {
-		return nil, fmt.Errorf("duration (%d) exceeds math.MaxInt64 (%d)", duration, math.MaxInt64)
-	}
-	ctx, cancelDial := context.WithTimeout(ctx, time.Second*time.Duration(duration))
+	timeout := time.Second * time.Duration(r.globalParams.Get(ParamConnectionTimeout).AsUint16())
+	ctx, cancelDial := context.WithTimeout(ctx, timeout)
 	defer cancelDial()
 
 	// use default params for now
@@ -346,12 +342,7 @@ func (r *Runtime) getConnToRandomTarget(ctx context.Context, runtimeParams *para
 	target := targets[0]
 	log.Debugf("using target %q (%q)", target.addressOrPod, target.node)
 
-	duration := r.globalParams.Get(ParamConnectionTimeout).AsUint()
-	if duration > math.MaxInt64 {
-		return nil, fmt.Errorf("duration (%d) exceeds math.MaxInt64 (%d)", duration, math.MaxInt64)
-	}
-
-	timeout := time.Second * time.Duration(duration)
+	timeout := time.Second * time.Duration(r.globalParams.Get(ParamConnectionTimeout).AsUint16())
 	conn, err := r.dialContext(ctx, target, timeout)
 	if err != nil {
 		return nil, fmt.Errorf("dialing %q (%q): %w", target.addressOrPod, target.node, err)
@@ -438,7 +429,7 @@ func (r *Runtime) runGadget(gadgetCtx runtime.GadgetContext, target target, allP
 	connCtx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	timeout := time.Second * time.Duration(r.globalParams.Get(ParamConnectionTimeout).AsUint())
+	timeout := time.Second * time.Duration(r.globalParams.Get(ParamConnectionTimeout).AsUint16())
 	dialCtx, cancelDial := context.WithTimeout(gadgetCtx.Context(), timeout)
 	defer cancelDial()
 