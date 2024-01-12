inspektor-gadget/inspektor-gadget

commit 0071cee0ffc63da02c862eea99ecf589cac1d266

use client-go informers instead of active polling
Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

pkg/operators/common/kubeinventorycache.go
@@ -15,16 +15,14 @@
 package common
 
 import (
-	"context"
 	"fmt"
 	"sync"
-	"sync/atomic"
 	"time"
 
-	log "github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/informers"
+	listersv1 "k8s.io/client-go/listers/core/v1"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/k8sutil"
 )
@@ -32,14 +30,12 @@ import (
 // K8sInventoryCache is a cache of Kubernetes resources such as pods and services
 // that can be used by operators to enrich events.
 type K8sInventoryCache struct {
-	clientset *kubernetes.Clientset
+	factory informers.SharedInformerFactory
 
-	pods atomic.Pointer[v1.PodList]
-	svcs atomic.Pointer[v1.ServiceList]
+	pods listersv1.PodLister
+	svcs listersv1.ServiceLister
 
-	exit           chan struct{}
-	ticker         *time.Ticker
-	tickerDuration time.Duration
+	exit chan struct{}
 
 	useCount      int
 	useCountMutex sync.Mutex
@@ -53,42 +49,31 @@ var (
 
 func GetK8sInventoryCache() (*K8sInventoryCache, error) {
 	once.Do(func() {
-		cache, err = newCache(1 * time.Second)
+		cache, err = newCache(10 * time.Minute)
 	})
 	return cache, err
 }
 
-func newCache(tickerDuration time.Duration) (*K8sInventoryCache, error) {
+func newCache(defaultResync time.Duration) (*K8sInventoryCache, error) {
 	clientset, err := k8sutil.NewClientset("")
 	if err != nil {
 		return nil, fmt.Errorf("creating new k8s clientset: %w", err)
 	}
+	factory := informers.NewSharedInformerFactory(clientset, defaultResync)
+	pods := factory.Core().V1().Pods().Lister()
+	svcs := factory.Core().V1().Services().Lister()
 
 	return &K8sInventoryCache{
-		clientset:      clientset,
-		tickerDuration: tickerDuration,
+		factory: factory,
+		pods:    pods,
+		svcs:    svcs,
 	}, nil
-}
-
-func (cache *K8sInventoryCache) loop() {
-	for {
-		select {
-		case <-cache.exit:
-			return
-		case <-cache.ticker.C:
-			cache.update()
-		}
-	}
 }
 
 func (cache *K8sInventoryCache) Close() {
 	if cache.exit != nil {
 		close(cache.exit)
 		cache.exit = nil
-	}
-	if cache.ticker != nil {
-		cache.ticker.Stop()
-		cache.ticker = nil
 	}
 }
 
@@ -98,10 +83,9 @@ func (cache *K8sInventoryCache) Start() {
 
 	// No uses before us, we are the first one
 	if cache.useCount == 0 {
-		cache.update()
 		cache.exit = make(chan struct{})
-		cache.ticker = time.NewTicker(cache.tickerDuration)
-		go cache.loop()
+		cache.factory.Start(cache.exit)
+		cache.factory.WaitForCacheSync(cache.exit)
 	}
 	cache.useCount++
 }
@@ -117,26 +101,10 @@ func (cache *K8sInventoryCache) Stop() {
 	cache.useCount--
 }
 
-func (cache *K8sInventoryCache) update() {
-	pods, err := cache.clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		log.Errorf("listing pods: %v", err)
-		return
-	}
-	cache.pods.Store(pods)
-
-	svcs, err := cache.clientset.CoreV1().Services("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		log.Errorf("listing services: %s", err)
-		return
-	}
-	cache.svcs.Store(svcs)
+func (cache *K8sInventoryCache) GetPods() ([]*v1.Pod, error) {
+	return cache.pods.List(labels.Everything())
 }
 
-func (cache *K8sInventoryCache) GetPods() *v1.PodList {
-	return cache.pods.Load()
-}
-
-func (cache *K8sInventoryCache) GetSvcs() *v1.ServiceList {
-	return cache.svcs.Load()
+func (cache *K8sInventoryCache) GetSvcs() ([]*v1.Service, error) {
+	return cache.svcs.List(labels.Everything())
 }

pkg/operators/kubeipresolver/kubeipresolver.go
@@ -106,7 +106,7 @@ func (m *KubeIPResolverInstance) PostGadgetRun() error {
 }
 
 func (m *KubeIPResolverInstance) enrich(ev any) {
-	pods := m.manager.k8sInventory.GetPods()
+	pods, _ := m.manager.k8sInventory.GetPods()
 	endpoints := ev.(KubeIPResolverInterface).GetEndpoints()
 	for j := range endpoints {
 		// initialize to this default value if we don't find a match
@@ -114,7 +114,7 @@ func (m *KubeIPResolverInstance) enrich(ev any) {
 	}
 
 	found := 0
-	for _, pod := range pods.Items {
+	for _, pod := range pods {
 		if pod.Spec.HostNetwork {
 			continue
 		}
@@ -134,9 +134,9 @@ func (m *KubeIPResolverInstance) enrich(ev any) {
 		}
 	}
 
-	svcs := m.manager.k8sInventory.GetSvcs()
+	svcs, _ := m.manager.k8sInventory.GetSvcs()
 
-	for _, svc := range svcs.Items {
+	for _, svc := range svcs {
 		for _, endpoint := range endpoints {
 			if svc.Spec.ClusterIP == endpoint.Addr {
 				endpoint.Kind = types.EndpointKindService

pkg/operators/kubenameresolver/kubenameresolver.go
@@ -116,15 +116,15 @@ func (m *KubeNameResolverInstance) enrich(ev any) {
 	kubeNameResolver, _ := ev.(KubeNameResolverInterface)
 	containerInfo, _ := ev.(operators.ContainerInfoGetters)
 
-	pods := m.manager.k8sInventory.GetPods()
-	for i, pod := range pods.Items {
+	pods, _ := m.manager.k8sInventory.GetPods()
+	for _, pod := range pods {
 		if pod.Namespace == containerInfo.GetNamespace() && pod.Name == containerInfo.GetPod() {
 			owner := ""
 			// When the pod belongs to Deployment, ReplicaSet or DaemonSet, find the
 			// shorter name without the random suffix. That will be used to
 			// generate the network policy name.
-			if pods.Items[i].OwnerReferences != nil {
-				nameItems := strings.Split(pods.Items[i].Name, "-")
+			if pod.OwnerReferences != nil {
+				nameItems := strings.Split(pod.Name, "-")
 				if len(nameItems) > 2 {
 					owner = strings.Join(nameItems[:len(nameItems)-2], "-")
 				}

commit 18710d609697be430452894ebfc8de09d5504c6e

KubeCache: Don't call Close() directly
Do not call Close() directly if it is not neccessary. The useCount
handels starting and closing of the K8sInventoryCache. Close() should
only be called if the program is terminating anyway.

KubeIPResolver and KubeNameResolver should handle the cache in their own
instances. The underlying K8sInventoryCache is shared anyway

Fixes: 0071cee0ffc63da02c862eea99ecf589cac1d266
Signed-off-by: Burak Ok <burakok@microsoft.com>

pkg/operators/common/kubeinventorycache.go
@@ -22,6 +22,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
 	listersv1 "k8s.io/client-go/listers/core/v1"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/k8sutil"
@@ -30,16 +31,21 @@ import (
 // K8sInventoryCache is a cache of Kubernetes resources such as pods and services
 // that can be used by operators to enrich events.
 type K8sInventoryCache struct {
-	factory informers.SharedInformerFactory
+	clientset *kubernetes.Clientset
 
-	pods listersv1.PodLister
-	svcs listersv1.ServiceLister
+	factory informers.SharedInformerFactory
+	pods    listersv1.PodLister
+	svcs    listersv1.ServiceLister
 
 	exit chan struct{}
 
 	useCount      int
 	useCountMutex sync.Mutex
 }
+
+const (
+	informerResync = 10 * time.Minute
+)
 
 var (
 	cache *K8sInventoryCache
@@ -49,24 +55,19 @@ var (
 
 func GetK8sInventoryCache() (*K8sInventoryCache, error) {
 	once.Do(func() {
-		cache, err = newCache(10 * time.Minute)
+		cache, err = newCache()
 	})
 	return cache, err
 }
 
-func newCache(defaultResync time.Duration) (*K8sInventoryCache, error) {
+func newCache() (*K8sInventoryCache, error) {
 	clientset, err := k8sutil.NewClientset("")
 	if err != nil {
 		return nil, fmt.Errorf("creating new k8s clientset: %w", err)
 	}
-	factory := informers.NewSharedInformerFactory(clientset, defaultResync)
-	pods := factory.Core().V1().Pods().Lister()
-	svcs := factory.Core().V1().Services().Lister()
 
 	return &K8sInventoryCache{
-		factory: factory,
-		pods:    pods,
-		svcs:    svcs,
+		clientset: clientset,
 	}, nil
 }
 
@@ -74,6 +75,10 @@ func (cache *K8sInventoryCache) Close() {
 	if cache.exit != nil {
 		close(cache.exit)
 		cache.exit = nil
+	}
+	if cache.factory != nil {
+		cache.factory.Shutdown()
+		cache.factory = nil
 	}
 }
 
@@ -83,6 +88,10 @@ func (cache *K8sInventoryCache) Start() {
 
 	// No uses before us, we are the first one
 	if cache.useCount == 0 {
+		cache.factory = informers.NewSharedInformerFactory(cache.clientset, informerResync)
+		cache.pods = cache.factory.Core().V1().Pods().Lister()
+		cache.svcs = cache.factory.Core().V1().Services().Lister()
+
 		cache.exit = make(chan struct{})
 		cache.factory.Start(cache.exit)
 		cache.factory.WaitForCacheSync(cache.exit)

pkg/operators/kubeipresolver/kubeipresolver.go
@@ -36,9 +36,7 @@ type KubeIPResolverInterface interface {
 	GetEndpoints() []*types.L3Endpoint
 }
 
-type KubeIPResolver struct {
-	k8sInventory *common.K8sInventoryCache
-}
+type KubeIPResolver struct{}
 
 func (k *KubeIPResolver) Name() string {
 	return OperatorName
@@ -66,30 +64,29 @@ func (k *KubeIPResolver) CanOperateOn(gadget gadgets.GadgetDesc) bool {
 }
 
 func (k *KubeIPResolver) Init(params *params.Params) error {
-	k8sInventory, err := common.GetK8sInventoryCache()
-	if err != nil {
-		return fmt.Errorf("creating k8s inventory cache: %w", err)
-	}
-	k.k8sInventory = k8sInventory
 	return nil
 }
 
 func (k *KubeIPResolver) Close() error {
-	k.k8sInventory.Close()
 	return nil
 }
 
 func (k *KubeIPResolver) Instantiate(gadgetCtx operators.GadgetContext, gadgetInstance any, params *params.Params) (operators.OperatorInstance, error) {
+	k8sInventory, err := common.GetK8sInventoryCache()
+	if err != nil {
+		return nil, fmt.Errorf("creating k8s inventory cache: %w", err)
+	}
+
 	return &KubeIPResolverInstance{
 		gadgetCtx:      gadgetCtx,
-		manager:        k,
+		k8sInventory:   k8sInventory,
 		gadgetInstance: gadgetInstance,
 	}, nil
 }
 
 type KubeIPResolverInstance struct {
 	gadgetCtx      operators.GadgetContext
-	manager        *KubeIPResolver
+	k8sInventory   *common.K8sInventoryCache
 	gadgetInstance any
 }
 
@@ -98,17 +95,17 @@ func (m *KubeIPResolverInstance) Name() string {
 }
 
 func (m *KubeIPResolverInstance) PreGadgetRun() error {
-	m.manager.k8sInventory.Start()
+	m.k8sInventory.Start()
 	return nil
 }
 
 func (m *KubeIPResolverInstance) PostGadgetRun() error {
-	m.manager.k8sInventory.Stop()
+	m.k8sInventory.Stop()
 	return nil
 }
 
 func (m *KubeIPResolverInstance) enrich(ev any) {
-	pods, err := m.manager.k8sInventory.GetPods()
+	pods, err := m.k8sInventory.GetPods()
 	if err != nil {
 		log.Warnf("getting pods from k8s inventory: %v", err)
 		return
@@ -141,7 +138,7 @@ func (m *KubeIPResolverInstance) enrich(ev any) {
 		}
 	}
 
-	svcs, err := m.manager.k8sInventory.GetSvcs()
+	svcs, err := m.k8sInventory.GetSvcs()
 	if err != nil {
 		log.Warnf("getting services from k8s inventory: %v", err)
 		return

pkg/operators/kubenameresolver/kubenameresolver.go
@@ -39,9 +39,7 @@ type KubeNameResolverInterface interface {
 	SetLocalPodDetails(owner, hostIP, podIP string, labels map[string]string)
 }
 
-type KubeNameResolver struct {
-	k8sInventory *common.K8sInventoryCache
-}
+type KubeNameResolver struct{}
 
 func (k *KubeNameResolver) Name() string {
 	return OperatorName
@@ -73,30 +71,29 @@ func (k *KubeNameResolver) CanOperateOn(gadget gadgets.GadgetDesc) bool {
 }
 
 func (k *KubeNameResolver) Init(params *params.Params) error {
-	k8sInventory, err := common.GetK8sInventoryCache()
-	if err != nil {
-		return fmt.Errorf("creating k8s inventory cache: %w", err)
-	}
-	k.k8sInventory = k8sInventory
 	return nil
 }
 
 func (k *KubeNameResolver) Close() error {
-	k.k8sInventory.Close()
 	return nil
 }
 
 func (k *KubeNameResolver) Instantiate(gadgetCtx operators.GadgetContext, gadgetInstance any, params *params.Params) (operators.OperatorInstance, error) {
+	k8sInventory, err := common.GetK8sInventoryCache()
+	if err != nil {
+		return nil, fmt.Errorf("creating k8s inventory cache: %w", err)
+	}
+
 	return &KubeNameResolverInstance{
 		gadgetCtx:      gadgetCtx,
-		manager:        k,
+		k8sInventory:   k8sInventory,
 		gadgetInstance: gadgetInstance,
 	}, nil
 }
 
 type KubeNameResolverInstance struct {
 	gadgetCtx      operators.GadgetContext
-	manager        *KubeNameResolver
+	k8sInventory   *common.K8sInventoryCache
 	gadgetInstance any
 }
 
@@ -105,12 +102,12 @@ func (m *KubeNameResolverInstance) Name() string {
 }
 
 func (m *KubeNameResolverInstance) PreGadgetRun() error {
-	m.manager.k8sInventory.Start()
+	m.k8sInventory.Start()
 	return nil
 }
 
 func (m *KubeNameResolverInstance) PostGadgetRun() error {
-	m.manager.k8sInventory.Stop()
+	m.k8sInventory.Stop()
 	return nil
 }
 
@@ -118,7 +115,7 @@ func (m *KubeNameResolverInstance) enrich(ev any) {
 	kubeNameResolver, _ := ev.(KubeNameResolverInterface)
 	containerInfo, _ := ev.(operators.ContainerInfoGetters)
 
-	pods, err := m.manager.k8sInventory.GetPods()
+	pods, err := m.k8sInventory.GetPods()
 	if err != nil {
 		log.Warnf("getting pods from k8s inventory: %v", err)
 		return