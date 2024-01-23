inspektor-gadget/inspektor-gadget

commit 8c433c4b29399cabd6ac233933c602f76c3c7c5f

image-based trace exec: add upper_layer field
Tested with:

$ sudo -E ./ig image build -t execsnoop ./gadgets/trace_exec/

$ sudo -E ./ig run execsnoop
INFO[0000] Experimental features enabled
RUNTIME.CONTAINERNAME         PID              PPID            UID             GID             R… UPP… COMM
test                          97952            97928           0               0               0  fal… sh
test                          97976            97952           0               0               0  fal… cp
test                          97977            97952           0               0               0  fal… date
test                          97952            97928           0               0               0  true date

$ docker run -ti --rm --name=test busybox sh -c 'cp /bin/date /date ; date ; /date'

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

gadgets/trace_exec/program.bpf.c
@@ -10,6 +10,9 @@
 #include <gadget/mntns_filter.h>
 #include <gadget/types.h>
 
+// Defined in include/uapi/linux/magic.h
+#define OVERLAYFS_SUPER_MAGIC 0x794c7630
+
 #define ARGSIZE 128
 #define TOTAL_MAX_ARGS 60
 #define DEFAULT_MAXARGS 20
@@ -30,6 +33,7 @@ struct event {
 	__u32 sessionid;
 	int retval;
 	int args_count;
+	bool upper_layer;
 	unsigned int args_size;
 	__u8 comm[TASK_COMM_LEN];
 	__u8 args[FULL_MAX_ARGS_ARR];
@@ -144,6 +148,43 @@ int ig_execve_e(struct trace_event_raw_sys_enter *ctx)
 	return 0;
 }
 
+// struct ovl_inode defined in fs/overlayfs/ovl_entry.h
+// Unfortunately, not exported to vmlinux.h
+// and not available in /sys/kernel/btf/vmlinux
+// See https://github.com/cilium/ebpf/pull/1300
+// So we don't benefit from CO-RE's preserve_access_index
+// We only rely on vfs_inode and __upperdentry relative positions
+struct _ovl_inode {
+	struct inode vfs_inode;
+	struct dentry *__upperdentry;
+};
+
+static __always_inline bool upper_layer()
+{
+	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+	struct inode *inode = BPF_CORE_READ(task, mm, exe_file, f_inode);
+	if (!inode) {
+		return false;
+	}
+	unsigned long sb_magic = BPF_CORE_READ(inode, i_sb, s_magic);
+
+	if (sb_magic != OVERLAYFS_SUPER_MAGIC) {
+		return false;
+	}
+
+	struct _ovl_inode *oi =
+		container_of(inode, struct _ovl_inode, vfs_inode);
+	if (!oi) {
+		return false;
+	}
+
+	struct dentry *upperdentry;
+
+	bpf_probe_read_kernel(&upperdentry, sizeof upperdentry,
+			      &oi->__upperdentry);
+	return upperdentry != NULL;
+}
+
 SEC("tracepoint/syscalls/sys_exit_execve")
 int ig_execve_x(struct trace_event_raw_sys_exit *ctx)
 {
@@ -163,6 +204,9 @@ int ig_execve_x(struct trace_event_raw_sys_exit *ctx)
 	ret = ctx->ret;
 	if (ignore_failed && ret < 0)
 		goto cleanup;
+
+	if (ret == 0)
+		event->upper_layer = upper_layer();
 
 	event->retval = ret;
 	bpf_get_current_comm(&event->comm, sizeof(event->comm));

gadgets/trace_exec/gadget.yaml
@@ -67,9 +67,15 @@ structs:
         hidden: true
         ellipsis: end
     - name: mntns_id
-      description: 'Mount namespace inode id'
+      description: Mount namespace inode id
       attributes:
         template: ns
+    - name: upper_layer
+      description: Whether the executable is in the upper layer of the overlay filesystem
+      attributes:
+        width: 5
+        alignment: left
+        ellipsis: end
 ebpfParams:
   ignore_failed:
     key: ignore-failed

commit 7c8dd8f428de0dc1f02dd87585154e42af13221c

trace exec upper_layer: fix BTF type size relocation
bpf_core_type_size() has to be used explicitly to ensure the correct BTF
relocation will be used.

Using sizeof or other implicit methods don't work.

Fixes: 8c433c4b2939 ("image-based trace exec: add upper_layer field")
Fixes: a1d98b153cf1 ("built-in trace exec: add upper_layer field")

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

gadgets/trace_exec/program.bpf.c
@@ -148,18 +148,7 @@ int ig_execve_e(struct trace_event_raw_sys_enter *ctx)
 	return 0;
 }
 
-// struct ovl_inode defined in fs/overlayfs/ovl_entry.h
-// Unfortunately, not exported to vmlinux.h
-// and not available in /sys/kernel/btf/vmlinux
-// See https://github.com/cilium/ebpf/pull/1300
-// So we don't benefit from CO-RE's preserve_access_index
-// We only rely on vfs_inode and __upperdentry relative positions
-struct _ovl_inode {
-	struct inode vfs_inode;
-	struct dentry *__upperdentry;
-};
-
-static __always_inline bool upper_layer()
+static __always_inline bool has_upper_layer()
 {
 	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 	struct inode *inode = BPF_CORE_READ(task, mm, exe_file, f_inode);
@@ -172,16 +161,17 @@ static __always_inline bool upper_layer()
 		return false;
 	}
 
-	struct _ovl_inode *oi =
-		container_of(inode, struct _ovl_inode, vfs_inode);
-	if (!oi) {
-		return false;
-	}
-
 	struct dentry *upperdentry;
 
+	// struct ovl_inode defined in fs/overlayfs/ovl_entry.h
+	// Unfortunately, not exported to vmlinux.h
+	// and not available in /sys/kernel/btf/vmlinux
+	// See https://github.com/cilium/ebpf/pull/1300
+	// We only rely on vfs_inode and __upperdentry relative positions
 	bpf_probe_read_kernel(&upperdentry, sizeof upperdentry,
-			      &oi->__upperdentry);
+			      ((void *)inode) +
+				      bpf_core_type_size(struct inode));
+
 	return upperdentry != NULL;
 }
 
@@ -206,7 +196,7 @@ int ig_execve_x(struct trace_event_raw_sys_exit *ctx)
 		goto cleanup;
 
 	if (ret == 0)
-		event->upper_layer = upper_layer();
+		event->upper_layer = has_upper_layer();
 
 	event->retval = ret;
 	bpf_get_current_comm(&event->comm, sizeof(event->comm));

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -142,18 +142,7 @@ int ig_execve_e(struct trace_event_raw_sys_enter *ctx)
 	return 0;
 }
 
-// struct ovl_inode defined in fs/overlayfs/ovl_entry.h
-// Unfortunately, not exported to vmlinux.h
-// and not available in /sys/kernel/btf/vmlinux
-// See https://github.com/cilium/ebpf/pull/1300
-// So we don't benefit from CO-RE's preserve_access_index
-// We only rely on vfs_inode and __upperdentry relative positions
-struct _ovl_inode {
-	struct inode vfs_inode;
-	struct dentry *__upperdentry;
-};
-
-static __always_inline bool upper_layer()
+static __always_inline bool has_upper_layer()
 {
 	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 	struct inode *inode = BPF_CORE_READ(task, mm, exe_file, f_inode);
@@ -166,16 +155,16 @@ static __always_inline bool upper_layer()
 		return false;
 	}
 
-	struct _ovl_inode *oi =
-		container_of(inode, struct _ovl_inode, vfs_inode);
-	if (!oi) {
-		return false;
-	}
-
 	struct dentry *upperdentry;
 
+	// struct ovl_inode defined in fs/overlayfs/ovl_entry.h
+	// Unfortunately, not exported to vmlinux.h
+	// and not available in /sys/kernel/btf/vmlinux
+	// See https://github.com/cilium/ebpf/pull/1300
+	// We only rely on vfs_inode and __upperdentry relative positions
 	bpf_probe_read_kernel(&upperdentry, sizeof(upperdentry),
-			      &oi->__upperdentry);
+			      ((void *)inode) +
+				      bpf_core_type_size(struct inode));
 	return upperdentry != NULL;
 }
 
@@ -200,7 +189,7 @@ int ig_execve_x(struct trace_event_raw_sys_exit *ctx)
 		goto cleanup;
 
 	if (ret == 0) {
-		event->upper_layer = upper_layer();
+		event->upper_layer = has_upper_layer();
 	}
 
 	event->retval = ret;