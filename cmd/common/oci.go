ab4b8fb6842b9154ea38d373ee8c5cfb943b126c
Michael Friese <mfriese@microsoft.com>
2024-01-24 05:56:25 +0100

    cmd/ig: add dummy support for refactoring
    WIP.
    
    Signed-off-by: Michael Friese <mfriese@microsoft.com>

---
+++ cmd/common/oci.go
@@ -0,0 +1,153 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/inspektor-gadget/inspektor-gadget/cmd/common/frontends/console"
+	"github.com/inspektor-gadget/inspektor-gadget/cmd/common/utils"
+	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	clioperator "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/cli"
+	ocioperator "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/oci"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/runtime"
+)
+
+func NewRunCommand(rootCmd *cobra.Command, runtime runtime.Runtime, hiddenColumnTags []string) *cobra.Command {
+	runtimeGlobalParams := runtime.GlobalParamDescs().ToParams()
+	runtime.Init(runtimeGlobalParams)
+
+	ociParams := ocioperator.OciHandler.ParamDescs().ToParams()
+
+	// Add operator global flags
+	operatorsGlobalParamsCollection := operators.GlobalParamsCollection()
+
+	cmd := &cobra.Command{
+		Use:          "run-experimental",
+		Short:        "run gadget",
+		SilenceUsage: true, // do not print usage when there is an error
+		// We have to disable flag parsing in here to be able to handle certain
+		// flags more dynamically and have `--help` also react to those changes.
+		// Instead, we need to manually
+		// * call cmd.ParseFlags()
+		// * handle `--help` after changing the params dynamically
+		// * handle everything that could have been handled inside
+		//   `PersistentPreRun(E)` of a parent cmd, as the flags wouldn't have
+		//   been parsed there (e.g. --verbose)
+		DisableFlagParsing: true,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			// Manually register the clioperator
+			clioperator.Register()
+
+			err := runtime.Init(runtimeGlobalParams)
+			if err != nil {
+				return fmt.Errorf("initializing runtime: %w", err)
+			}
+			defer runtime.Close()
+
+			// we need to re-enable flag parsing, as utils.ParseEarlyFlags() would
+			// not do anything otherwise
+			cmd.DisableFlagParsing = false
+
+			// Parse flags that are known at this time, like the ones we get from the gadget descriptor
+			if err := utils.ParseEarlyFlags(cmd, args); err != nil {
+				return err
+			}
+
+			// gadget parameters that are only available after contacting the server
+			gadgetParams := make(params.Params, 0)
+
+			// Before running the gadget, we need to get the gadget info to perform
+			// different tasks like creating the parser and setting flags for the
+			// gadget's parameters.
+			actualArgs := cmd.Flags().Args()
+
+			gadgetCtx := gadgetcontext.NewOCI(
+				context.Background(),
+				actualArgs[0],
+				logger.DefaultLogger(),
+				params.Collection{
+					"oci": ociParams,
+				},
+			)
+
+			// Fetch gadget information; TODO: this can potentially be cached
+			info, err := runtime.GetOCIGadgetInfo(gadgetCtx)
+			if err != nil {
+				return fmt.Errorf("fetching gadget information: %w", err)
+			}
+
+			for _, p := range info.Params {
+				gadgetParams.Add(api.ParamToParamDesc(p).ToParam())
+			}
+
+			AddFlags(cmd, &gadgetParams, nil, runtime)
+
+			return cmd.ParseFlags(args)
+		},
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			// args from RunE still contains all flags, since we manually parsed them,
+			// so we need to manually pull the remaining args here
+			args := cmd.Flags().Args()
+
+			if len(args) == 0 {
+				if showHelp, _ := cmd.Flags().GetBool("help"); showHelp {
+					additionalMessage := "Specify the gadget image to get more information about it"
+					cmd.Long = fmt.Sprintf("%s\n\n%s", cmd.Short, additionalMessage)
+				}
+				return cmd.Help()
+			}
+
+			if showHelp, _ := cmd.Flags().GetBool("help"); showHelp {
+				return cmd.Help()
+			}
+
+			// we also manually need to check the verbose flag, as PersistentPreRunE in
+			// verbose.go will not have the correct information due to manually parsing
+			// the flags
+			checkVerboseFlag()
+
+			fe := console.NewFrontend()
+			defer fe.Close()
+
+			ctx := fe.GetContext()
+
+			gadgetCtx := gadgetcontext.NewOCI(ctx, args[0], logger.DefaultLogger(), params.Collection{
+				"oci": ociParams,
+			}) // TODO
+
+			err := runtime.RunOCIGadget(gadgetCtx)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	AddFlags(cmd, ociParams, nil, runtime)
+
+	for _, operatorParams := range operatorsGlobalParamsCollection {
+		AddFlags(cmd, operatorParams, nil, runtime)
+	}
+
+	return cmd
+}
