04275efe236a4e290fe12d56afdd917678c262bc
ghinks <ghinks@yahoo.com>
2024-01-30 09:18:01 -0500

    tests: unit/top/tcp/types
    - add a basic test for the GetEndpoints method

--- pkg/gadgets/top/tcp/types/types_test.go
+++ pkg/gadgets/top/tcp/types/types_test.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 	"syscall"
 	"testing"
 )
@@ -29,5 +30,46 @@ func TestParseFilterByFamilyFailing(t *testing.T) {
 	_, err := ParseFilterByFamily(family)
 	if err == nil {
 		t.Fatalf("ParseFilterByFamily didn't return an error")
+	}
+}
+
+func TestGetEndpoints(t *testing.T) {
+	// Create a Stats instance
+	stats := Stats{
+		Pid:       1234,
+		Comm:      "test",
+		IPVersion: 4,
+		SrcEndpoint: eventtypes.L4Endpoint{
+			L3Endpoint: eventtypes.L3Endpoint{
+				Addr:    "192.168.1.1",
+				Version: uint8(1),
+			},
+			Port: 8080,
+		},
+		DstEndpoint: eventtypes.L4Endpoint{
+			L3Endpoint: eventtypes.L3Endpoint{
+				Addr:    "192.168.1.2",
+				Version: uint8(1),
+			},
+			Port: 8080,
+		},
+		Sent:     1000,
+		Received: 2000,
+	}
+
+	// Call the GetEndpoints method
+	endpoints := stats.GetEndpoints()
+
+	// Verify the endpoints
+	if len(endpoints) != 2 {
+		t.Errorf("Expected 2 endpoints, got %d", len(endpoints))
+	}
+
+	if endpoints[0].Addr != "192.168.1.1" || endpoints[0].Version != 1 {
+		t.Errorf("Unexpected values for SrcEndpoint, got IP: %s, Version: %d", endpoints[0].Addr, endpoints[0].Version)
+	}
+
+	if endpoints[1].Addr != "192.168.1.2" || endpoints[1].Version != 1 {
+		t.Errorf("Unexpected values for DstEndpoint, got IP: %s, Version: %d", endpoints[1].Addr, endpoints[1].Version)
 	}
 }
