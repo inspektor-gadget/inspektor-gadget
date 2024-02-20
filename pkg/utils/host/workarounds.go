inspektor-gadget/inspektor-gadget

commit 073bcd016837af2335a8eb3d430bce21a0ec09e7

auto-mount-filesystems: add alternative path for tracefs
Amazon Linux does not use the new path "/sys/kernel/tracing" but only
the old path "/sys/kernel/debug/tracing".

If tracefs is mounted at one of those paths, it is good enough for
Inspektor Gadget. No need to error out or suggest anything.

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/utils/host/workarounds.go
@@ -218,39 +218,44 @@ func autoSdUnitRestart() (exit bool, err error) {
 func autoMountFilesystems(dryRun bool) (mountsSuggested []string, err error) {
 	fs := []struct {
 		name    string
-		path    string
+		paths   []string
 		magic   int64
 		suggest bool // suggest mounting this filesystem
 	}{
 		{
 			"bpf",
-			"/sys/fs/bpf",
+			[]string{"/sys/fs/bpf"},
 			unix.BPF_FS_MAGIC,
 			false, // do not make 'ig --auto-mount-filesystems=false' fail if bpffs is not mounted
 		},
 		{
 			"debugfs",
-			"/sys/kernel/debug",
+			[]string{"/sys/kernel/debug"},
 			unix.DEBUGFS_MAGIC,
 			true,
 		},
 		{
 			"tracefs",
-			"/sys/kernel/tracing",
+			[]string{"/sys/kernel/tracing", "/sys/kernel/debug/tracing"},
 			unix.TRACEFS_MAGIC,
 			true,
 		},
 	}
+
+filesystemLoop:
 	for _, f := range fs {
 		var statfs unix.Statfs_t
-		err = unix.Statfs(f.path, &statfs)
-		if err != nil {
-			return mountsSuggested, fmt.Errorf("statfs %s: %w", f.path, err)
+		for _, path := range f.paths {
+			err = unix.Statfs(path, &statfs)
+			if err != nil {
+				return mountsSuggested, fmt.Errorf("statfs %s: %w", path, err)
+			}
+			if statfs.Type == f.magic {
+				log.Debugf("%s already mounted", f.name)
+				continue filesystemLoop
+			}
 		}
-		if statfs.Type == f.magic {
-			log.Debugf("%s already mounted", f.name)
-			continue
-		}
+
 		if f.suggest {
 			mountsSuggested = append(mountsSuggested, f.name)
 		}
@@ -258,11 +263,11 @@ func autoMountFilesystems(dryRun bool) (mountsSuggested []string, err error) {
 			continue
 		}
 
-		err = unix.Mount("none", f.path, f.name, 0, "")
+		err = unix.Mount("none", f.paths[0], f.name, 0, "")
 		if err != nil {
-			return mountsSuggested, fmt.Errorf("mounting %s: %w", f.path, err)
+			return mountsSuggested, fmt.Errorf("mounting %s: %w", f.paths[0], err)
 		}
-		log.Debugf("%s mounted (%s)", f.name, f.path)
+		log.Debugf("%s mounted (%s)", f.name, f.paths[0])
 	}
 	return
 }

commit c0b6291ea6da6929c19a5adaf622d8c497ecdcd2

Fix auto-mount-filesystems
If there is an error calling statfs on the path, for instance, it doesn't
exist, the logic should continue without erroring out to suggest using
"--auto-mount-filesystems"

Fixes: 073bcd016837 ("auto-mount-filesystems: add alternative path for tracefs")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/utils/host/workarounds.go
@@ -215,7 +215,9 @@ func autoSdUnitRestart() (exit bool, err error) {
 // If dryRun is true, autoMount will only check if the filesystems need to be
 // mounted.
 // Returns the list of filesystems that need to be mounted.
-func autoMountFilesystems(dryRun bool) (mountsSuggested []string, err error) {
+func autoMountFilesystems(dryRun bool) ([]string, error) {
+	var mountsSuggested []string
+
 	fs := []struct {
 		name    string
 		paths   []string
@@ -246,9 +248,9 @@ filesystemLoop:
 	for _, f := range fs {
 		var statfs unix.Statfs_t
 		for _, path := range f.paths {
-			err = unix.Statfs(path, &statfs)
-			if err != nil {
-				return mountsSuggested, fmt.Errorf("statfs %s: %w", path, err)
+			if err := unix.Statfs(path, &statfs); err != nil {
+				log.Debugf("statfs returned error on %s: %s", path, err)
+				continue
 			}
 			if statfs.Type == f.magic {
 				log.Debugf("%s already mounted", f.name)
@@ -263,13 +265,12 @@ filesystemLoop:
 			continue
 		}
 
-		err = unix.Mount("none", f.paths[0], f.name, 0, "")
-		if err != nil {
+		if err := unix.Mount("none", f.paths[0], f.name, 0, ""); err != nil {
 			return mountsSuggested, fmt.Errorf("mounting %s: %w", f.paths[0], err)
 		}
 		log.Debugf("%s mounted (%s)", f.name, f.paths[0])
 	}
-	return
+	return mountsSuggested, nil
 }
 
 func suggestWSLWorkaround() error {