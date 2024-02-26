764a01372c80311b7d886450525b9811107bb6c3
Alban Crequy <albancrequy@linux.microsoft.com>
2024-02-26 11:10:50 +0100

    profile/block-io: use core type matches relocations
    Instead of making relocations from the Go code, use
    bpf_core_type_matches() to emit relocations of type BPF_TYPE_MATCHES.
    
    This requires support in cilium/ebpf.
    
    When no good type is detected, reject the bpf program with
    bpf_unreachable().
    
    Note about tcpdrop and enum skb_drop_reason:
    - Linux 5.17 introduced the enum:
      https://github.com/torvalds/linux/commit/c504e5c2f9648a1e5c2be01e8c3f59d394192bd3
    - Linux 5.18 added Adds SKB_NOT_DROPPED_YET:
      https://github.com/torvalds/linux/commit/1330b6ef3313fcec577d2b020c290dc8b9f11f1a
    
    Fixes: 7da4e7f5ae23 ("pkg/gadgets: Use BTF to check arguments number of block_rq_insert")
    
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

--- pkg/gadgets/profile/block-io/tracer/bpf/biolatency.bpf.c
+++ pkg/gadgets/profile/block-io/tracer/bpf/biolatency.bpf.c
@@ -15,8 +15,6 @@ const volatile bool targ_per_disk = false;
 const volatile bool targ_per_flag = false;
 const volatile bool targ_queued = false;
 const volatile bool targ_ms = false;
-const volatile bool insert_arg_single = true;
-const volatile bool issue_arg_single = true;
 const volatile bool filter_dev = false;
 const volatile __u32 targ_dev = 0;
 
@@ -64,21 +62,46 @@ static __always_inline int trace_rq_start(struct request *rq, int issue)
 	return 0;
 }
 
+// https://github.com/torvalds/linux/commit/a54895fa057c67700270777f7661d8d3c7fda88a
+// -       TP_PROTO(struct request_queue *q, struct request *rq),
+// +       TP_PROTO(struct request *rq),
+
+// struct request and struct request_queue are likely different in the current
+// kernel and in vmlinux.h. We don't need recursive compatibility checks on
+// each field of the struct because we only use this to check the prototype of
+// the tracepoints. We use empty structs so none of the fields are checked by
+// bpf_core_type_matches().
+struct request___empty {};
+struct request_queue___empty {};
+
+typedef void (*btf_trace_block_rq_insert___new)(void *,
+						struct request___empty *);
+typedef void (*btf_trace_block_rq_insert___old)(void *,
+						struct request_queue___empty *q,
+						struct request___empty *);
+typedef void (*btf_trace_block_rq_issue___new)(void *,
+					       struct request___empty *);
+typedef void (*btf_trace_block_rq_issue___old)(void *,
+					       struct request_queue___empty *q,
+					       struct request___empty *);
+
 SEC("raw_tp/block_rq_insert")
 int ig_profio_ins(u64 *ctx)
 {
 	if (filter_cg && !bpf_current_task_under_cgroup(&cgroup_map, 0))
 		return 0;
 
-	/**
-	 * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-	 * from TP_PROTO(struct request_queue *q, struct request *rq)
-	 * to TP_PROTO(struct request *rq)
-	 */
-	if (!insert_arg_single)
-		return trace_rq_start((void *)ctx[1], false);
-	else
+	if (bpf_core_type_matches(btf_trace_block_rq_insert___new)) {
+		// After commit a54895fa (v5.11-rc1)
 		return trace_rq_start((void *)ctx[0], false);
+	} else if (bpf_core_type_matches(btf_trace_block_rq_insert___old)) {
+		// Before commit a54895fa (v5.11-rc1)
+		return trace_rq_start((void *)ctx[1], false);
+	} else {
+		// Couldn't detect block/block_rq_insert tracepoint
+		bpf_core_unreachable();
+		return 0;
+	}
 }
 
 SEC("raw_tp/block_rq_issue")
@@ -87,15 +110,17 @@ int ig_profio_iss(u64 *ctx)
 	if (filter_cg && !bpf_current_task_under_cgroup(&cgroup_map, 0))
 		return 0;
 
-	/**
-	 * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-	 * from TP_PROTO(struct request_queue *q, struct request *rq)
-	 * to TP_PROTO(struct request *rq)
-	 */
-	if (!issue_arg_single)
-		return trace_rq_start((void *)ctx[1], true);
-	else
+	if (bpf_core_type_matches(btf_trace_block_rq_issue___new)) {
+		// After commit a54895fa (v5.11-rc1)
 		return trace_rq_start((void *)ctx[0], true);
+	} else if (bpf_core_type_matches(btf_trace_block_rq_issue___old)) {
+		// Before commit a54895fa (v5.11-rc1)
+		return trace_rq_start((void *)ctx[1], true);
+	} else {
+		// Couldn't detect block/block_rq_issue tracepoint
+		bpf_core_unreachable();
+		return 0;
+	}
 }
 
 SEC("raw_tp/block_rq_complete")
