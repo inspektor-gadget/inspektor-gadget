inspektor-gadget/inspektor-gadget

commit d772f2ef04c745c5cb27efe571fec5820e4ed3a9

dns: Parse DNS packet in user space
The current logic tries to parse the full DNS packet in eBPF. This is
diffcult to do a imposes some limitations, like not supporting:
- DNS over TCP
- Uncompressed messages
- More than 8 answers

This commit changes approach to parce the DNS packet on user space.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/gadgets/trace/dns/types/dns.go
@@ -54,7 +54,7 @@ type Event struct {
 	PktType    string        `json:"pktType,omitempty" column:"type,minWidth:7,maxWidth:9"`
 	QType      string        `json:"qtype,omitempty" column:"qtype,minWidth:5,maxWidth:10"`
 	DNSName    string        `json:"name,omitempty" column:"name,width:30"`
-	Rcode      string        `json:"rcode,omitempty" column:"rcode,minWidth:8"`
+	Rcode      string        `json:"rcode,omitempty" column:"rcode,minWidth:12"`
 	Latency    time.Duration `json:"latency,omitempty" column:"latency,hide"`
 	NumAnswers int           `json:"numAnswers,omitempty" column:"numAnswers,width:8,maxWidth:8" columnDesc:"Number of addresses contained in the response."`
 	Addresses  []string      `json:"addresses,omitempty" column:"addresses,width:32,hide" columnDesc:"Addresses in the response. Maximum 8 are reported. Only available if the response is compressed."`

go.mod
@@ -110,6 +110,7 @@ require (
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect

go.sum
@@ -195,6 +195,8 @@ github.com/google/gofuzz v1.0.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/
 github.com/google/gofuzz v1.1.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
 github.com/google/gofuzz v1.2.0 h1:xRy4A+RhZaiKjJ1bPfwQ8sedCA+YS2YcCHW6ec7JMi0=
 github.com/google/gofuzz v1.2.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
+github.com/google/gopacket v1.1.19 h1:ves8RnFZPGiFnTS0uPQStjwru6uO6h+nlr9j6fL7kF8=
+github.com/google/gopacket v1.1.19/go.mod h1:iJ8V8n6KS+z2U1A8pUwu8bW5SyEMkXJB8Yo/Vo+TKTo=
 github.com/google/pprof v0.0.0-20230323073829-e72429f035bd h1:r8yyd+DJDmsUhGrRBxH5Pj7KeFK5l+Y3FsgT8keqKtk=
 github.com/google/pprof v0.0.0-20230323073829-e72429f035bd/go.mod h1:79YE0hCXdHag9sBkw2o+N/YnZtTkXi0UT9Nnixa5eYk=
 github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 h1:El6M4kTTCOh6aBiKaUGG7oYTSPP8MxqL4YI3kZKwcP4=
@@ -486,6 +488,8 @@ golang.org/x/lint v0.0.0-20181026193005-c67002cb31c3/go.mod h1:UVdnD1Gm6xHRNCYTk
 golang.org/x/lint v0.0.0-20190227174305-5b3e6a55c961/go.mod h1:wehouNa3lNwaWXcvxsM5YxQ5yQlVC4a0KAMCusXpPoU=
 golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3/go.mod h1:6SW0HCj/g11FgYtHlgUYUwCkIfeOF89ocIRzGO/8vkc=
 golang.org/x/lint v0.0.0-20190930215403-16217165b5de/go.mod h1:6SW0HCj/g11FgYtHlgUYUwCkIfeOF89ocIRzGO/8vkc=
+golang.org/x/lint v0.0.0-20200302205851-738671d3881b/go.mod h1:3xt1FjdF8hUf6vQPIChWIBhFzV8gjjsPE/fR3IyQdNY=
+golang.org/x/mod v0.1.1-0.20191105210325-c90efee705ee/go.mod h1:QqPTAvyqsEbceGzBzNggFXnrqF1CaUcvgkdR5Ot7KZg=
 golang.org/x/mod v0.2.0/go.mod h1:s0Qsj1ACt9ePp/hMypM3fl4fZqREWJwdYDEqhRiZZUA=
 golang.org/x/mod v0.3.0/go.mod h1:s0Qsj1ACt9ePp/hMypM3fl4fZqREWJwdYDEqhRiZZUA=
 golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4/go.mod h1:jJ57K6gSWd91VN4djpZkiMVwK6gcyfeH4XE8wZrZaV4=
@@ -594,6 +598,7 @@ golang.org/x/tools v0.0.0-20190311212946-11955173bddd/go.mod h1:LCzVGOaR6xXOjkQ3
 golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135/go.mod h1:RgjU9mgBXZiqYHBnxXauZ1Gv1EHHAz9KjViQ78xBX0Q=
 golang.org/x/tools v0.0.0-20191108193012-7d206e10da11/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
 golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
+golang.org/x/tools v0.0.0-20200130002326-2f3ba24bd6e7/go.mod h1:TB2adYChydJhpapKDTa4BR/hXlZSLoq2Wpct/0txZ28=
 golang.org/x/tools v0.0.0-20200505023115-26f46d2f7ef8/go.mod h1:EkVYQZoAsY45+roYkvgYkIh4xh/qjgUK9TdY2XT94GE=
 golang.org/x/tools v0.0.0-20200619180055-7c47624df98f/go.mod h1:EkVYQZoAsY45+roYkvgYkIh4xh/qjgUK9TdY2XT94GE=
 golang.org/x/tools v0.0.0-20201224043029-2b0845dc783e/go.mod h1:emZCQorbCU4vsT4fOWvOPXz4eW1wZW4PmDk9uLelYpA=

integration/ig/k8s/trace_dns_test.go
@@ -95,7 +95,7 @@ func TestTraceDns(t *testing.T) {
 					PktType:    "HOST",
 					DNSName:    "fake.test.com.",
 					QType:      "A",
-					Rcode:      "NoError",
+					Rcode:      "No Error",
 					Latency:    1,
 					NumAnswers: 1,
 					Addresses:  []string{"127.0.0.1"},
@@ -135,7 +135,7 @@ func TestTraceDns(t *testing.T) {
 					PktType:    "HOST",
 					DNSName:    "fake.test.com.",
 					QType:      "AAAA",
-					Rcode:      "NoError",
+					Rcode:      "No Error",
 					Latency:    1,
 					NumAnswers: 1,
 					Addresses:  []string{"::1"},
@@ -175,7 +175,7 @@ func TestTraceDns(t *testing.T) {
 					PktType:    "HOST",
 					DNSName:    "nodomain.fake.test.com.",
 					QType:      "A",
-					Rcode:      "NXDomain",
+					Rcode:      "Non-Existent Domain",
 					Latency:    1,
 					NumAnswers: 0,
 					Uid:        1000,

integration/ig/non-k8s/trace_dns_test.go
@@ -69,7 +69,7 @@ func TestTraceDns(t *testing.T) {
 					PktType:    "HOST",
 					DNSName:    "fake.test.com.",
 					QType:      "A",
-					Rcode:      "NoError",
+					Rcode:      "No Error",
 					Latency:    1,
 					NumAnswers: 1,
 					Addresses:  []string{"127.0.0.1"},
@@ -111,7 +111,7 @@ func TestTraceDns(t *testing.T) {
 					PktType:    "HOST",
 					DNSName:    "fake.test.com.",
 					QType:      "AAAA",
-					Rcode:      "NoError",
+					Rcode:      "No Error",
 					Latency:    1,
 					NumAnswers: 1,
 					Addresses:  []string{"::1"},

integration/inspektor-gadget/trace_dns_test.go
@@ -88,7 +88,7 @@ func TestTraceDns(t *testing.T) {
 					PktType:    "HOST",
 					DNSName:    "fake.test.com.",
 					QType:      "A",
-					Rcode:      "NoError",
+					Rcode:      "No Error",
 					Latency:    1,
 					NumAnswers: 1,
 					Addresses:  []string{"127.0.0.1"},
@@ -120,7 +120,7 @@ func TestTraceDns(t *testing.T) {
 					PktType:    "HOST",
 					DNSName:    "fake.test.com.",
 					QType:      "AAAA",
-					Rcode:      "NoError",
+					Rcode:      "No Error",
 					Latency:    1,
 					NumAnswers: 1,
 					Addresses:  []string{"::1"},

pkg/gadgets/trace/dns/tracer/bpf/dns-common.h
@@ -1,18 +1,8 @@
 #ifndef GADGET_DNS_COMMON_H
 #define GADGET_DNS_COMMON_H
 
-// Max DNS name length: 255
-// https://datatracker.ietf.org/doc/html/rfc1034#section-3.1
-#define MAX_DNS_NAME 255
-
 #define TASK_COMM_LEN 16
 
-// Maximum number of A or AAAA answers to include in the DNS event.
-// The DNS reply could have more answers than this, but the additional
-// answers won't be sent to userspace.
-#define MAX_ADDR_ANSWERS 8
-
-// this needs to be manually kept in sync with dnsEventTAbbrev in tracer.go (without the anaddr field)
 struct event_t {
 	// Keep netns at the top: networktracer depends on it
 	__u32 netns;
@@ -40,22 +30,8 @@ struct event_t {
 	__u16 dport;
 	__u8 proto;
 
-	__u16 id;
-	unsigned short qtype;
-
-	// qr says if the dns message is a query (0), or a response (1)
-	unsigned char qr;
 	unsigned char pkt_type;
-	unsigned char rcode;
-
-	__u64 latency_ns; // Set only if qr is 1 (response) and pkt_type is 0 (Host).
-
-	__u8 name[MAX_DNS_NAME];
-
-	__u16 ancount;
-	__u16 anaddrcount;
-	__u8 anaddr[MAX_ADDR_ANSWERS]
-		   [16]; // Either IPv4-mapped-IPv6 (A record) or IPv6 (AAAA record) addresses.
+	__u64 latency_ns; // Set only if the packet is a response and pkt_type is 0 (Host).
 };
 
 #endif

pkg/gadgets/trace/dns/tracer/bpf/dns.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright (c) 2021 The Inspektor Gadget authors */
+/* Copyright (c) 2024 The Inspektor Gadget authors */
 
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
@@ -7,6 +7,7 @@
 #include <linux/in.h>
 #include <linux/udp.h>
 #include <sys/socket.h>
+#include <stdbool.h>
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
@@ -17,12 +18,6 @@
 #include "dns-common.h"
 
 #define DNS_OFF (ETH_HLEN + sizeof(struct iphdr) + sizeof(struct udphdr))
-
-#define DNS_CLASS_IN \
-	1 // https://datatracker.ietf.org/doc/html/rfc1035#section-3.2.4
-#define DNS_TYPE_A \
-	1 // https://datatracker.ietf.org/doc/html/rfc1035#section-3.2.2
-#define DNS_TYPE_AAAA 28 // https://www.rfc-editor.org/rfc/rfc3596#section-2.1
 
 #ifndef PACKET_HOST
 #define PACKET_HOST 0x0
@@ -35,11 +30,26 @@
 #define DNS_QR_QUERY 0
 #define DNS_QR_RESP 1
 
+#define MAX_PORTS 16
+const volatile __u16 ports[MAX_PORTS] = { 53, 5353 };
+const volatile __u16 ports_len = 2;
+
+static __always_inline bool is_dns_port(__u16 port)
+{
+	for (int i = 0; i < ports_len; i++) {
+		if (ports[i] == port)
+			return true;
+	}
+	return false;
+}
+
 // we need this to make sure the compiler doesn't remove our struct
 const struct event_t *unusedevent __attribute__((unused));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(key_size, sizeof(__u32));
+	__uint(value_size, sizeof(__u32));
 } events SEC(".maps");
 
 // https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.1
@@ -81,30 +91,11 @@ struct dnshdr {
 	__u16 arcount; // number of additional records
 };
 
-// DNS resource record
-// https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.3
-#pragma pack(2)
-struct dnsrr {
-	__u16 name; // Two octets when using message compression, see https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.4
-	__u16 type;
-	__u16 class;
-	__u32 ttl;
-	__u16 rdlength;
-	// Followed by rdata
-};
-
-// The stack is limited, so use a map to build the event
-struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__uint(max_entries, 1);
-	__type(key, __u32);
-	__type(value, struct event_t);
-} tmp_event SEC(".maps");
-
 // Map of DNS query to timestamp so we can calculate latency from query sent to answer received.
 struct query_key_t {
 	__u64 pid_tgid;
 	__u16 id;
+	__u16 pad[3]; // this is needed, otherwise the verifier claims an invalid read from stack
 };
 
 struct {
@@ -114,158 +105,64 @@ struct {
 	__uint(max_entries, 1024);
 } query_map SEC(".maps");
 
-static __always_inline __u32 dns_name_length(struct __sk_buff *skb)
+SEC("socket1")
+int ig_trace_dns(struct __sk_buff *skb)
 {
-	// This loop iterates over the DNS labels to find the total DNS name
-	// length.
-	unsigned int i;
-	unsigned int skip = 0;
-	for (i = 0; i < MAX_DNS_NAME; i++) {
-		if (skip != 0) {
-			skip--;
-		} else {
-			int label_len = load_byte(
-				skb, DNS_OFF + sizeof(struct dnshdr) + i);
-			if (label_len == 0)
-				break;
-			// The simple solution "i += label_len" gives verifier
-			// errors, so work around with skip.
-			skip = label_len;
-		}
-	}
+	struct event_t event;
+	__u16 sport, dport;
+	__u16 id;
+	int i;
 
-	return i < MAX_DNS_NAME ? i : MAX_DNS_NAME;
-}
-
-// Save the IPv4 and IPv6 addresses in event->anaddr. Returns the number of saved addresses.
-static __always_inline int load_addresses(struct __sk_buff *skb, int ancount,
-					  int anoffset, struct event_t *event)
-{
-	int rroffset = anoffset;
-	int index = 0;
-	for (int i = 0; i < ancount && i < MAX_ADDR_ANSWERS; i++) {
-		__u16 rrname =
-			load_byte(skb, rroffset + offsetof(struct dnsrr, name));
-
-		// In most cases, the name will be compressed to two octets (indicated by first two bits 0b11).
-		// The offset calculations below assume compression, so exit early if the name isn't compressed.
-		if ((rrname & 0xf0) != 0xc0)
-			return 0;
-
-		// Safe to assume that all answers refer to the same domain name
-		// because we verified earlier that there's exactly one question.
-
-		__u16 rrtype =
-			load_half(skb, rroffset + offsetof(struct dnsrr, type));
-		__u16 rrclass = load_half(skb, rroffset + offsetof(struct dnsrr,
-								   class));
-		__u16 rdlength = load_half(
-			skb, rroffset + offsetof(struct dnsrr, rdlength));
-
-		if (rrtype == DNS_TYPE_A && rrclass == DNS_CLASS_IN &&
-		    rdlength == 4) {
-			// A record contains an IPv4 address.
-			// Encode this as IPv4-mapped-IPv6 in the BPF event (::ffff:<ipv4>)
-			// https://datatracker.ietf.org/doc/html/rfc4291#section-2.5.5.2
-			__builtin_memset(&event->anaddr[index][0], 0x0, 10);
-			__builtin_memset(&event->anaddr[index][10], 0xff, 2);
-			bpf_skb_load_bytes(skb, rroffset + sizeof(struct dnsrr),
-					   &event->anaddr[index][12], rdlength);
-			index++;
-		} else if (rrtype == DNS_TYPE_AAAA && rrclass == DNS_CLASS_IN &&
-			   rdlength == 16) {
-			// AAAA record contains an IPv6 address.
-			bpf_skb_load_bytes(skb, rroffset + sizeof(struct dnsrr),
-					   &event->anaddr[index][0], rdlength);
-			index++;
-		}
-		rroffset += sizeof(struct dnsrr) + rdlength;
-	}
-	return index;
-}
-
-static __always_inline int output_dns_event(struct __sk_buff *skb,
-					    union dnsflags flags,
-					    __u32 name_len, __u16 ancount)
-{
-	__u32 zero = 0;
-	struct event_t *event = bpf_map_lookup_elem(&tmp_event, &zero);
-	if (!event)
+	// Skip non-IP packets
+	if (load_half(skb, offsetof(struct ethhdr, h_proto)) != ETH_P_IP)
 		return 0;
 
-	__builtin_memset(event, 0, sizeof(*event));
+	// Skip non-UDP packets
+	if (load_byte(skb, ETH_HLEN + offsetof(struct iphdr, protocol)) !=
+	    IPPROTO_UDP)
+		return 0;
 
-	event->netns = skb->cb[0]; // cb[0] initialized by dispatcher.bpf.c
-	event->timestamp = bpf_ktime_get_boot_ns();
-	event->id = load_half(skb, DNS_OFF + offsetof(struct dnshdr, id));
-	event->af = AF_INET;
-	event->daddr_v4 =
+	sport = load_half(skb, ETH_HLEN + sizeof(struct iphdr) +
+				       offsetof(struct udphdr, source));
+	dport = load_half(skb, ETH_HLEN + sizeof(struct iphdr) +
+				       offsetof(struct udphdr, dest));
+
+	if (!is_dns_port(sport) && !is_dns_port(dport))
+		return 0;
+
+	// Initialize event here only after we know we're interested in this packet to avoid
+	// spending useless cycles.
+	__builtin_memset(&event, 0, sizeof(event));
+
+	event.netns = skb->cb[0]; // cb[0] initialized by dispatcher.bpf.c
+	event.timestamp = bpf_ktime_get_boot_ns();
+	event.pkt_type = skb->pkt_type;
+
+	event.af = AF_INET;
+	event.daddr_v4 =
 		load_word(skb, ETH_HLEN + offsetof(struct iphdr, daddr));
-	event->saddr_v4 =
+	event.saddr_v4 =
 		load_word(skb, ETH_HLEN + offsetof(struct iphdr, saddr));
 	// load_word converts from network to host endianness. Convert back to
 	// network endianness because inet_ntop() requires it.
-	event->daddr_v4 = bpf_htonl(event->daddr_v4);
-	event->saddr_v4 = bpf_htonl(event->saddr_v4);
+	event.daddr_v4 = bpf_htonl(event.daddr_v4);
+	event.saddr_v4 = bpf_htonl(event.saddr_v4);
 
-	// Check network protocol.
-	// This only works with IPv4.
-	// For IPv6, gadget_socket_lookup() in sockets-map.h
-	// provides an example how to parse ip/ports on IPv6.
-	event->proto =
-		load_byte(skb, ETH_HLEN + offsetof(struct iphdr, protocol));
-	if (event->proto == IPPROTO_TCP) {
-		event->sport =
-			load_half(skb, ETH_HLEN + sizeof(struct iphdr) +
-					       offsetof(struct tcphdr, source));
-		event->dport =
-			load_half(skb, ETH_HLEN + sizeof(struct iphdr) +
-					       offsetof(struct tcphdr, dest));
-	} else if (event->proto == IPPROTO_UDP) {
-		event->sport =
-			load_half(skb, ETH_HLEN + sizeof(struct iphdr) +
-					       offsetof(struct udphdr, source));
-		event->dport =
-			load_half(skb, ETH_HLEN + sizeof(struct iphdr) +
-					       offsetof(struct udphdr, dest));
-	}
-
-	event->qr = flags.qr;
-
-	if (flags.qr == 1) {
-		// Response code set only for replies.
-		event->rcode = flags.rcode;
-	}
-
-	bpf_skb_load_bytes(skb, DNS_OFF + sizeof(struct dnshdr), event->name,
-			   name_len);
-
-	event->pkt_type = skb->pkt_type;
-
-	// Read QTYPE right after the QNAME (name_len + the zero length octet)
-	// https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.2
-	event->qtype =
-		load_half(skb, DNS_OFF + sizeof(struct dnshdr) + name_len + 1);
+	event.proto = IPPROTO_UDP;
+	event.sport = sport;
+	event.dport = dport;
 
 	// Enrich event with process metadata
 	struct sockets_value *skb_val = gadget_socket_lookup(skb);
 	if (skb_val != NULL) {
-		event->mount_ns_id = skb_val->mntns;
-		event->pid = skb_val->pid_tgid >> 32;
-		event->tid = (__u32)skb_val->pid_tgid;
-		__builtin_memcpy(&event->task, skb_val->task,
-				 sizeof(event->task));
-		event->uid = (__u32)skb_val->uid_gid;
-		event->gid = (__u32)(skb_val->uid_gid >> 32);
+		event.mount_ns_id = skb_val->mntns;
+		event.pid = skb_val->pid_tgid >> 32;
+		event.tid = (__u32)skb_val->pid_tgid;
+		__builtin_memcpy(&event.task, skb_val->task,
+				 sizeof(event.task));
+		event.uid = (__u32)skb_val->uid_gid;
+		event.gid = (__u32)(skb_val->uid_gid >> 32);
 	}
-
-	event->ancount = ancount;
-
-	// DNS answers start immediately after qname (name_len octets)
-	// + the zero length octet + qtype (2 octets) + qclass (2 octets).
-	int anoffset = DNS_OFF + sizeof(struct dnshdr) + name_len + 5;
-	int anaddrcount = load_addresses(skb, ancount, anoffset, event);
-	event->anaddrcount = anaddrcount;
 
 	// Calculate latency:
 	//
@@ -284,71 +181,40 @@ static __always_inline int output_dns_event(struct __sk_buff *skb,
 	// Skip this if skb_val == NULL (gadget_socket_lookup did not set pid_tgid we use in the query key)
 	// or if event->timestamp == 0 (kernels before 5.8 don't support bpf_ktime_get_boot_ns, and the patched
 	// version IG injects always returns zero).
-	if (skb_val != NULL && event->timestamp > 0) {
+	if (skb_val != NULL && event.timestamp > 0) {
+		union dnsflags flags;
+		flags.flags = load_half(skb, DNS_OFF + offsetof(struct dnshdr,
+								flags));
+		id = load_half(skb, DNS_OFF + offsetof(struct dnshdr, id));
+		__u8 qr = flags.qr;
+
 		struct query_key_t query_key = {
 			.pid_tgid = skb_val->pid_tgid,
-			.id = event->id,
+			.id = id,
 		};
-		if (event->qr == DNS_QR_QUERY &&
-		    event->pkt_type == PACKET_OUTGOING) {
+		if (qr == DNS_QR_QUERY && event.pkt_type == PACKET_OUTGOING) {
 			bpf_map_update_elem(&query_map, &query_key,
-					    &event->timestamp, BPF_NOEXIST);
-		} else if (event->qr == DNS_QR_RESP &&
-			   event->pkt_type == PACKET_HOST) {
+					    &event.timestamp, BPF_NOEXIST);
+		} else if (flags.qr == DNS_QR_RESP &&
+			   event.pkt_type == PACKET_HOST) {
 			__u64 *query_ts =
 				bpf_map_lookup_elem(&query_map, &query_key);
 			if (query_ts != NULL) {
 				// query ts should always be less than the event ts, but check anyway to be safe.
-				if (*query_ts < event->timestamp) {
-					event->latency_ns =
-						event->timestamp - *query_ts;
+				if (*query_ts < event.timestamp) {
+					event.latency_ns =
+						event.timestamp - *query_ts;
 				}
 				bpf_map_delete_elem(&query_map, &query_key);
 			}
 		}
 	}
 
-	// size of full structure - addresses + only used addresses
-	unsigned long long size =
-		sizeof(*event) - MAX_ADDR_ANSWERS * 16 + anaddrcount * 16;
-	bpf_perf_event_output(skb, &events, BPF_F_CURRENT_CPU, event, size);
+	__u64 skb_len = skb->len;
+	bpf_perf_event_output(skb, &events, skb_len << 32 | BPF_F_CURRENT_CPU,
+			      &event, sizeof(event));
 
 	return 0;
-}
-
-SEC("socket1")
-int ig_trace_dns(struct __sk_buff *skb)
-{
-	// Skip non-IP packets
-	if (load_half(skb, offsetof(struct ethhdr, h_proto)) != ETH_P_IP)
-		return 0;
-
-	// Skip non-UDP packets
-	if (load_byte(skb, ETH_HLEN + offsetof(struct iphdr, protocol)) !=
-	    IPPROTO_UDP)
-		return 0;
-
-	union dnsflags flags;
-	flags.flags = load_half(skb, DNS_OFF + offsetof(struct dnshdr, flags));
-
-	// Skip DNS packets with more than 1 question
-	if (load_half(skb, DNS_OFF + offsetof(struct dnshdr, qdcount)) != 1)
-		return 0;
-
-	__u16 ancount =
-		load_half(skb, DNS_OFF + offsetof(struct dnshdr, ancount));
-	__u16 nscount =
-		load_half(skb, DNS_OFF + offsetof(struct dnshdr, nscount));
-
-	// Skip DNS queries with answers
-	if ((flags.qr == 0) && (ancount + nscount != 0))
-		return 0;
-
-	__u32 name_len = dns_name_length(skb);
-	if (name_len == 0)
-		return 0;
-
-	return output_dns_event(skb, flags, name_len, ancount);
 }
 
 char _license[] SEC("license") = "GPL";

commit 48983b2dfa2bbaff34b4006b16e13afb6087e15a

trace/dns: Fix outdated description
Fixes: d772f2ef04c7 ("dns: Parse DNS packet in user space")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/gadgets/trace/dns/types/dns.go
@@ -57,7 +57,7 @@ type Event struct {
 	Rcode      string        `json:"rcode,omitempty" column:"rcode,minWidth:12"`
 	Latency    time.Duration `json:"latency,omitempty" column:"latency,hide"`
 	NumAnswers int           `json:"numAnswers,omitempty" column:"numAnswers,width:8,maxWidth:8" columnDesc:"Number of addresses contained in the response."`
-	Addresses  []string      `json:"addresses,omitempty" column:"addresses,width:32,hide" columnDesc:"Addresses in the response. Maximum 8 are reported. Only available if the response is compressed."`
+	Addresses  []string      `json:"addresses,omitempty" column:"addresses,width:32,hide" columnDesc:"Addresses in the response."`
 }
 
 func GetColumns() *columns.Columns[Event] {