inspektor-gadget/inspektor-gadget

commit 2c3d496577acf23baec5d596279a87cc3d9abde5

gadgets: Add trace_malloc
Signed-off-by: Tianyi Liu <i.pear@outlook.com>

gadgets/trace_malloc/gadget.yaml
@@ -1,0 +1,28 @@
+name: trace malloc
+description: use uprobe to trace malloc and free in libc.so
+tracers:
+  events:
+    mapName: events
+    structName: event
+structs:
+  event:
+    fields:
+    - name: pid
+      attributes:
+        template: pid
+    - name: comm
+      description: command
+      attributes:
+        template: comm
+    - name: operation
+      description: memory operation type
+      attributes:
+        width: 16
+        alignment: left
+        ellipsis: end
+    - name: addr
+      description: address of malloc/free
+      attributes:
+        width: 20
+        alignment: left
+        ellipsis: end

gadgets/trace_malloc/program.bpf.c
@@ -1,0 +1,57 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (c) 2024 The Inspektor Gadget authors */
+
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <gadget/buffer.h>
+#include <gadget/macros.h>
+
+enum memop {
+	MALLOC,
+	FREE,
+};
+
+struct event {
+	__u32 pid;
+	__u8 comm[TASK_COMM_LEN];
+	enum memop operation;
+	__u64 addr;
+};
+
+GADGET_TRACER_MAP(events, 1024 * 256);
+
+GADGET_TRACER(open, events, event);
+
+static __always_inline int submit_memop_event(struct pt_regs *ctx,
+					      enum memop operation, __u64 addr)
+{
+	struct event *event;
+
+	event = gadget_reserve_buf(&events, sizeof(*event));
+	if (!event)
+		return 0;
+
+	event->pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_get_current_comm(event->comm, sizeof(event->comm));
+	event->operation = operation;
+	event->addr = addr;
+
+	gadget_submit_buf(ctx, &events, event, sizeof(*event));
+
+	return 0;
+}
+
+SEC("uretprobe//usr/lib/libc.so.6:malloc")
+int trace_uprobe_malloc(struct pt_regs *ctx)
+{
+	return submit_memop_event(ctx, MALLOC, PT_REGS_RC(ctx));
+}
+
+SEC("uprobe//usr/lib/libc.so.6:free")
+int trace_uprobe_free(struct pt_regs *ctx)
+{
+	return submit_memop_event(ctx, FREE, PT_REGS_PARM1(ctx));
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";

gadgets/Makefile
@@ -7,6 +7,7 @@ IG ?= ig
 GADGETS = \
 	trace_dns \
 	trace_exec \
+	trace_malloc \
 	trace_mount \
 	trace_oomkill \
 	trace_open \

commit 969f569f4b87ab786493f7276e9f28d9170f6f9c

gadgets/trace_malloc: fix tracer's name
Fixes: 2c3d496577ac ("gadgets: Add trace_malloc")

Signed-off-by: Tianyi Liu <i.pear@outlook.com>

gadgets/trace_malloc/gadget.yaml
@@ -1,7 +1,7 @@
 name: trace malloc
 description: use uprobe to trace malloc and free in libc.so
 tracers:
-  events:
+  malloc:
     mapName: events
     structName: event
 structs:

gadgets/trace_malloc/program.bpf.c
@@ -21,7 +21,7 @@ struct event {
 
 GADGET_TRACER_MAP(events, 1024 * 256);
 
-GADGET_TRACER(open, events, event);
+GADGET_TRACER(malloc, events, event);
 
 static __always_inline int submit_memop_event(struct pt_regs *ctx,
 					      enum memop operation, __u64 addr)