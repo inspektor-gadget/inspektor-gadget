inspektor-gadget/inspektor-gadget

commit d28c76d0ef853f40fa5594b24e3febf044b757d6

pkg/operators/ebpf: ebpf layer operator
Handles ebpf binaries of image based gadgets - includes former logic of the run gadget.

Signed-off-by: Michael Friese <mfriese@microsoft.com>

pkg/operators/ebpf/tracer.go
@@ -1,0 +1,242 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpfoperator
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/btf"
+	"github.com/cilium/ebpf/perf"
+	"github.com/cilium/ebpf/ringbuf"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	metadatav1 "github.com/inspektor-gadget/inspektor-gadget/pkg/metadata/v1"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+)
+
+type Tracer struct {
+	metadatav1.Tracer
+
+	ds       datasource.DataSource
+	accessor datasource.FieldAccessor
+
+	mapType       ebpf.MapType
+	eventSize     uint32 // needed to trim trailing bytes when reading for perf event array
+	ringbufReader *ringbuf.Reader
+	perfReader    *perf.Reader
+}
+
+func validateTracerMap(traceMap *ebpf.MapSpec) error {
+	if traceMap.Type != ebpf.RingBuf && traceMap.Type != ebpf.PerfEventArray {
+		return fmt.Errorf("map %q has a wrong type, expected: ringbuf or perf event array, got: %s",
+			traceMap.Name, traceMap.Type.String())
+	}
+	return nil
+}
+
+func (i *ebpfInstance) populateTracer(t btf.Type, varName string) error {
+	i.logger.Debugf("populating tracer %q", varName)
+
+	parts := strings.Split(varName, typeSplitter)
+	if len(parts) != 3 {
+		return fmt.Errorf("invalid tracer info: %q", varName)
+	}
+
+	name := parts[0]
+	mapName := parts[1]
+	structName := parts[2]
+
+	i.logger.Debugf("> name       : %q", name)
+	i.logger.Debugf("> map name   : %q", mapName)
+	i.logger.Debugf("> struct name: %q", structName)
+
+	tracerConfig := i.config.Sub("tracers." + name)
+	if tracerConfig != nil {
+		if configMapName := tracerConfig.GetString("mapName"); configMapName != "" && configMapName != mapName {
+			return fmt.Errorf("validating tracer %q: mapName %q in eBPF program does not match %q from metadata file",
+				name, configMapName, mapName)
+		}
+		if configStructName := tracerConfig.GetString("structName"); configStructName != "" && configStructName != structName {
+			return fmt.Errorf("validating tracer %q: structName %q in eBPF program does not match %q from metadata file",
+				name, configStructName, structName)
+		}
+		i.logger.Debugf("> successfully validated with metadata")
+	}
+
+	if _, ok := i.tracers[name]; ok {
+		i.logger.Debugf("tracer %q already defined, skipping", name)
+		return nil
+	}
+
+	tracerMap, ok := i.collectionSpec.Maps[mapName]
+	if !ok {
+		return fmt.Errorf("map %q not found in eBPF object", mapName)
+	}
+
+	if err := validateTracerMap(tracerMap); err != nil {
+		return fmt.Errorf("trace map is invalid: %w", err)
+	}
+
+	var btfStruct *btf.Struct
+	if err := i.collectionSpec.Types.TypeByName(structName, &btfStruct); err != nil {
+		return fmt.Errorf("finding struct %q in eBPF object: %w", structName, err)
+	}
+
+	i.logger.Debugf("adding tracer %q", name)
+	i.tracers[name] = &Tracer{
+		Tracer: metadatav1.Tracer{
+			MapName:    mapName,
+			StructName: btfStruct.Name,
+		},
+		eventSize: btfStruct.Size,
+	}
+
+	err := i.populateStructDirect(btfStruct)
+	if err != nil {
+		return fmt.Errorf("populating struct %q for tracer %q: %w", btfStruct.Name, name, err)
+	}
+
+	return nil
+}
+
+func (t *Tracer) receiveEvents(gadgetCtx operators.GadgetContext) error {
+	switch t.mapType {
+	case ebpf.RingBuf:
+		return t.receiveEventsFromRingReader(gadgetCtx)
+	case ebpf.PerfEventArray:
+		return t.receiveEventsFromPerfReader(gadgetCtx)
+	default:
+		return fmt.Errorf("invalid map type")
+	}
+}
+
+func (t *Tracer) receiveEventsFromRingReader(gadgetCtx operators.GadgetContext) error {
+	slowBuf := make([]byte, t.eventSize)
+	lastSlowLen := 0
+	for {
+		rec, err := t.ringbufReader.Read()
+		if err != nil {
+			return err
+		}
+		data := t.ds.NewData()
+		sample := rec.RawSample
+		if uint32(len(rec.RawSample)) < t.eventSize {
+			// event is truncated; we need to copy
+			copy(slowBuf, rec.RawSample)
+
+			// zero difference; TODO: improve
+			if len(rec.RawSample) < lastSlowLen {
+				for i := len(rec.RawSample); i < lastSlowLen; i++ {
+					slowBuf[i] = 0
+				}
+			}
+			lastSlowLen = len(rec.RawSample)
+			sample = slowBuf
+		}
+		err = t.accessor.Set(data, sample)
+		if err != nil {
+			gadgetCtx.Logger().Warnf("error setting buffer: %v", err)
+			t.ds.Release(data)
+			continue
+		}
+		err = t.ds.EmitAndRelease(data)
+		if err != nil {
+			gadgetCtx.Logger().Warnf("error emitting data: %v", err)
+		}
+	}
+}
+
+func (t *Tracer) receiveEventsFromPerfReader(gadgetCtx operators.GadgetContext) error {
+	slowBuf := make([]byte, t.eventSize)
+	lastSlowLen := 0
+	for {
+		rec, err := t.perfReader.Read()
+		if err != nil {
+			return err
+		}
+		data := t.ds.NewData()
+		sample := rec.RawSample
+		if uint32(len(rec.RawSample)) < t.eventSize {
+			// event is truncated; we need to copy
+			copy(slowBuf, rec.RawSample)
+
+			// zero difference; TODO: improve
+			if len(rec.RawSample) < lastSlowLen {
+				for i := len(rec.RawSample); i < lastSlowLen; i++ {
+					slowBuf[i] = 0
+				}
+			}
+			lastSlowLen = len(rec.RawSample)
+			sample = slowBuf
+		}
+		err = t.accessor.Set(data, sample)
+		if err != nil {
+			gadgetCtx.Logger().Warnf("error setting buffer: %v", err)
+			t.ds.Release(data)
+			continue
+		}
+		err = t.ds.EmitAndRelease(data)
+		if err != nil {
+			gadgetCtx.Logger().Warnf("error emitting data: %v", err)
+		}
+		if rec.LostSamples > 0 {
+			t.ds.ReportLostData(rec.LostSamples)
+		}
+	}
+}
+
+func (i *ebpfInstance) runTracer(gadgetCtx operators.GadgetContext, tracer *Tracer) error {
+	if tracer.MapName == "" {
+		return fmt.Errorf("tracer map name empty")
+	}
+
+	m, ok := i.collection.Maps[tracer.MapName]
+	if !ok {
+		return fmt.Errorf("looking up tracer map %q: not found", tracer.MapName)
+	}
+
+	tracer.mapType = m.Type()
+
+	var err error
+	switch m.Type() {
+	case ebpf.RingBuf:
+		i.logger.Debugf("creating ringbuf reader for map %q", tracer.MapName)
+		tracer.ringbufReader, err = ringbuf.NewReader(m)
+	case ebpf.PerfEventArray:
+		i.logger.Debugf("creating perf reader for map %q", tracer.MapName)
+		tracer.perfReader, err = perf.NewReader(m, gadgets.PerfBufferPages*os.Getpagesize())
+	default:
+		return fmt.Errorf("unknown type for tracer map %q", tracer.MapName)
+	}
+	if err != nil {
+		return fmt.Errorf("creating BPF map reader: %w", err)
+	}
+
+	go tracer.receiveEvents(gadgetCtx)
+
+	<-gadgetCtx.Context().Done()
+
+	if tracer.ringbufReader != nil {
+		tracer.ringbufReader.Close()
+	}
+	if tracer.perfReader != nil {
+		tracer.perfReader.Close()
+	}
+	return nil
+}

pkg/operators/ebpf/attach.go
@@ -1,0 +1,107 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpfoperator
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+)
+
+const (
+	kprobePrefix    = "kprobe/"
+	kretprobePrefix = "kretprobe/"
+	iterPrefix      = "iter/"
+	fentryPrefix    = "fentry/"
+	fexitPrefix     = "fexit/"
+)
+
+func (i *ebpfInstance) attachProgram(gadgetCtx operators.GadgetContext, p *ebpf.ProgramSpec, prog *ebpf.Program) (link.Link, error) {
+	switch p.Type {
+	case ebpf.Kprobe:
+		switch {
+		case strings.HasPrefix(p.SectionName, kprobePrefix):
+			i.logger.Debugf("Attaching kprobe %q to %q", p.Name, p.AttachTo)
+			return link.Kprobe(p.AttachTo, prog, nil)
+		case strings.HasPrefix(p.SectionName, kretprobePrefix):
+			i.logger.Debugf("Attaching kretprobe %q to %q", p.Name, p.AttachTo)
+			return link.Kretprobe(p.AttachTo, prog, nil)
+		}
+		return nil, fmt.Errorf("unsupported section name %q for program %q", p.SectionName, p.Name)
+	case ebpf.TracePoint:
+		i.logger.Debugf("Attaching tracepoint %q to %q", p.Name, p.AttachTo)
+		parts := strings.Split(p.AttachTo, "/")
+		return link.Tracepoint(parts[0], parts[1], prog, nil)
+	case ebpf.SocketFilter:
+		i.logger.Debugf("Attaching socket filter %q to %q", p.Name, p.AttachTo)
+		networkTracer := i.networkTracers[p.Name]
+		return nil, networkTracer.AttachProg(prog)
+	case ebpf.Tracing:
+		switch {
+		case strings.HasPrefix(p.SectionName, iterPrefix):
+			i.logger.Debugf("Attaching iter %q to %q", p.Name, p.AttachTo)
+			switch p.AttachTo {
+			case "task", "tcp", "udp":
+				return link.AttachIter(link.IterOptions{
+					Program: prog,
+				})
+			}
+			return nil, fmt.Errorf("unsupported iter type %q", p.AttachTo)
+		case strings.HasPrefix(p.SectionName, fentryPrefix):
+			i.logger.Debugf("Attaching fentry %q to %q", p.Name, p.AttachTo)
+			return link.AttachTracing(link.TracingOptions{
+				Program:    prog,
+				AttachType: ebpf.AttachTraceFEntry,
+			})
+		case strings.HasPrefix(p.SectionName, fexitPrefix):
+			i.logger.Debugf("Attaching fexit %q to %q", p.Name, p.AttachTo)
+			return link.AttachTracing(link.TracingOptions{
+				Program:    prog,
+				AttachType: ebpf.AttachTraceFExit,
+			})
+		}
+		return nil, fmt.Errorf("unsupported section name %q for program %q as type ebpf.Tracing", p.SectionName, p.Name)
+	case ebpf.RawTracepoint:
+		i.logger.Debugf("Attaching raw tracepoint %q to %q", p.Name, p.AttachTo)
+		return link.AttachRawTracepoint(link.RawTracepointOptions{
+			Name:    p.AttachTo,
+			Program: prog,
+		})
+	case ebpf.SchedCLS:
+		handler := i.tcHandlers[p.Name]
+
+		ifaceName := i.paramValues[ParamIface]
+		if ifaceName != "" {
+			iface, err := net.InterfaceByName(ifaceName)
+			if err != nil {
+				return nil, fmt.Errorf("getting interface %q: %w", ifaceName, err)
+			}
+
+			if err := handler.AttachIface(iface); err != nil {
+				return nil, fmt.Errorf("attaching iface %q: %w", ifaceName, err)
+			}
+		}
+
+		i.logger.Debugf("Attaching sched_cls %q", p.Name)
+		return nil, handler.AttachProg(prog)
+	default:
+		return nil, fmt.Errorf("unsupported program %q of type %q", p.Name, p.Type)
+	}
+}

pkg/operators/ebpf/ebpf.go
@@ -1,0 +1,616 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ebpfoperator provides an operator that is capable of analyzing and running
+// an eBFP based gadget.
+package ebpfoperator
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strings"
+	"sync"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/spf13/viper"
+
+	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/networktracer"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/oci"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/socketenricher"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/tchandler"
+)
+
+const (
+	eBPFObjectMediaType = "application/vnd.gadget.ebpf.program.v1+binary"
+
+	typeSplitter = "___"
+
+	ParamIface       = "iface"
+	ParamTraceKernel = "trace-pipe"
+)
+
+type param struct {
+	*api.Param
+	fromEbpf bool
+}
+
+// ebpfOperator reads ebpf programs from OCI images and runs them
+type ebpfOperator struct{}
+
+func (o *ebpfOperator) Name() string {
+	return "ebpf"
+}
+
+func (o *ebpfOperator) Description() string {
+	return "handles ebpf programs"
+}
+
+func (o *ebpfOperator) InstantiateImageOperator(
+	gadgetCtx operators.GadgetContext,
+	desc ocispec.Descriptor,
+	paramValues api.ParamValues,
+) (
+	operators.ImageOperatorInstance, error,
+) {
+	r, err := oci.GetContentFromDescriptor(gadgetCtx.Context(), desc)
+	if err != nil {
+		return nil, fmt.Errorf("getting ebpf binary: %w", err)
+	}
+	program, err := io.ReadAll(r)
+	if err != nil {
+		r.Close()
+		return nil, fmt.Errorf("reading ebpf binary: %w", err)
+	}
+	r.Close()
+
+	// TODO: do some pre-checks in here, maybe validate hashes, signatures, etc.
+
+	newInstance := &ebpfInstance{
+		gadgetCtx: gadgetCtx, // context usually should not be stored, but should we really carry it through all funcs?
+
+		logger:  gadgetCtx.Logger(),
+		program: program,
+
+		// Preallocate maps
+		tracers:      make(map[string]*Tracer),
+		structs:      make(map[string]*Struct),
+		snapshotters: make(map[string]*Snapshotter),
+		params:       make(map[string]*param),
+
+		containers: make(map[string]*containercollection.Container),
+
+		vars: make(map[string]*ebpfVar),
+
+		networkTracers: make(map[string]*networktracer.Tracer[api.GadgetData]),
+		tcHandlers:     make(map[string]*tchandler.Handler),
+
+		paramValues: paramValues,
+	}
+
+	cfg, ok := gadgetCtx.GetVar("config")
+	if !ok {
+		return nil, fmt.Errorf("missing configuration")
+	}
+	v, ok := cfg.(*viper.Viper)
+	if !ok {
+		return nil, fmt.Errorf("invalid configuration format")
+	}
+	newInstance.config = v
+
+	err = newInstance.init(gadgetCtx)
+	if err != nil {
+		return nil, fmt.Errorf("initializing ebpf gadget: %w", err)
+	}
+
+	return newInstance, nil
+}
+
+type ebpfInstance struct {
+	mu sync.Mutex
+
+	config *viper.Viper
+
+	program        []byte
+	logger         logger.Logger
+	collectionSpec *ebpf.CollectionSpec
+	collection     *ebpf.Collection
+
+	tracers      map[string]*Tracer
+	structs      map[string]*Struct
+	snapshotters map[string]*Snapshotter
+	params       map[string]*param
+	paramValues  map[string]string
+
+	networkTracers map[string]*networktracer.Tracer[api.GadgetData]
+	tcHandlers     map[string]*tchandler.Handler
+
+	// map from ebpf variable name to ebpfVar struct
+	vars map[string]*ebpfVar
+
+	links             []link.Link
+	linksSnapshotters []*linkSnapshotter
+
+	containers map[string]*containercollection.Container
+
+	gadgetCtx operators.GadgetContext
+}
+
+func (i *ebpfInstance) loadSpec() error {
+	progReader := bytes.NewReader(i.program)
+	spec, err := ebpf.LoadCollectionSpecFromReader(progReader)
+	if err != nil {
+		return fmt.Errorf("loading spec: %w", err)
+	}
+	i.collectionSpec = spec
+	return nil
+}
+
+func (i *ebpfInstance) analyze() error {
+	prefixLookups := []populateEntry{
+		{
+			prefixFunc:   hasPrefix(tracerInfoPrefix),
+			validator:    i.validateGlobalConstVoidPtrVar,
+			populateFunc: i.populateTracer,
+		},
+		{
+			prefixFunc:   hasPrefix(snapshottersPrefix),
+			validator:    i.validateGlobalConstVoidPtrVar,
+			populateFunc: i.populateSnapshotter,
+		},
+		{
+			prefixFunc:   hasPrefix(paramPrefix),
+			validator:    i.validateGlobalConstVoidPtrVar,
+			populateFunc: i.populateParam,
+		},
+		// {
+		// 	prefixFunc:   hasPrefix(tracerMapPrefix),
+		// 	validator:    i.validateGlobalConstVoidPtrVar,
+		// 	populateFunc: i.populateMap,
+		// },
+		{
+			prefixFunc: func(s string) (string, bool) {
+				// Exceptions for backwards-compatibility
+				if s == gadgets.MntNsFilterMapName {
+					return gadgets.MntNsFilterMapName, true
+				}
+				if s == socketenricher.SocketsMapName {
+					return socketenricher.SocketsMapName, true
+				}
+				return "", false
+			},
+			populateFunc: i.populateMap,
+		},
+		{
+			prefixFunc: func(s string) (string, bool) {
+				// Exceptions for backwards-compatibility
+				if s == gadgets.FilterByMntNsName {
+					return gadgets.FilterByMntNsName, true
+				}
+				return hasPrefix(varPrefix)(s)
+			},
+			validator:    nil,
+			populateFunc: i.populateVar,
+		},
+	}
+
+	// Iterate over types and populate the gadget
+	it := i.collectionSpec.Types.Iterate()
+	for it.Next() {
+		for _, entry := range prefixLookups {
+			typeName, ok := entry.prefixFunc(it.Type.TypeName())
+			if !ok {
+				continue
+			}
+			if entry.validator != nil {
+				err := entry.validator(it.Type, strings.TrimPrefix(it.Type.TypeName(), typeName))
+				if err != nil {
+					i.logger.Debugf("type %q error: %v", it.Type.TypeName(), err)
+					continue
+				}
+			}
+			err := entry.populateFunc(it.Type, typeName)
+			if err != nil {
+				return fmt.Errorf("handling type by prefix %q: %w", typeName, err)
+			}
+		}
+	}
+
+	// Fill param defaults
+	err := i.fillParamDefaults()
+	if err != nil {
+		i.logger.Debugf("error extracting default values for params: %v", err)
+	}
+
+	// Iterate over programs
+	for name, program := range i.collectionSpec.Programs {
+		i.logger.Debugf("program %q", name)
+		i.logger.Debugf("> type       : %s", program.Type.String())
+		i.logger.Debugf("> attachType : %s", program.AttachType.String())
+		i.logger.Debugf("> sectionName: %s", program.SectionName)
+		i.logger.Debugf("> license    : %s", program.License)
+	}
+	return nil
+}
+
+func (i *ebpfInstance) init(gadgetCtx operators.GadgetContext) error {
+	// hack for backward-compability and until we have nicer interfaces available
+	gadgetCtx.SetVar("ebpfInstance", i)
+
+	// loadSpec and analyze could be lazily executed, if the gadget has been cached before
+	err := i.loadSpec()
+	if err != nil {
+		return fmt.Errorf("initializing: %w", err)
+	}
+	err = i.analyze()
+	if err != nil {
+		return fmt.Errorf("analyzing: %w", err)
+	}
+
+	err = i.register(gadgetCtx)
+	if err != nil {
+		return fmt.Errorf("registering datasources: %w", err)
+	}
+
+	return nil
+}
+
+func (i *ebpfInstance) addDataSource(
+	gadgetCtx operators.GadgetContext,
+	dsType datasource.Type,
+	name string,
+	size uint32,
+	fields []*Field,
+) (
+	datasource.DataSource, datasource.FieldAccessor, error,
+) {
+	ds, err := gadgetCtx.RegisterDataSource(dsType, name)
+	if err != nil {
+		return nil, nil, fmt.Errorf("adding tracer datasource: %w", err)
+	}
+	staticFields := make([]datasource.StaticField, 0, len(fields))
+	for _, field := range fields {
+		staticFields = append(staticFields, field)
+	}
+	accessor, err := ds.AddStaticFields(size, staticFields)
+	if err != nil {
+		return nil, nil, fmt.Errorf("adding fields for datasource: %w", err)
+	}
+	return ds, accessor, nil
+}
+
+func (i *ebpfInstance) register(gadgetCtx operators.GadgetContext) error {
+	// register datasources
+	for name, m := range i.tracers {
+		ds, accessor, err := i.addDataSource(gadgetCtx, datasource.TypeEvent, name, i.structs[m.StructName].Size, i.structs[m.StructName].Fields)
+		if err != nil {
+			return fmt.Errorf("adding datasource: %w", err)
+		}
+		m.accessor = accessor
+		m.ds = ds
+	}
+	for name, m := range i.snapshotters {
+		ds, accessor, err := i.addDataSource(gadgetCtx, datasource.TypeEvent, name, i.structs[m.StructName].Size, i.structs[m.StructName].Fields)
+		if err != nil {
+			return fmt.Errorf("adding datasource: %w", err)
+		}
+
+		// TODO: need a link to find out if this is a snapshotter for network; if so, we can add the netns id
+		m.netns, err = ds.AddField("netns",
+			datasource.WithTags("type:gadget_netns_id", "name:netns"),
+			datasource.WithKind(api.Kind_Uint64))
+		if err != nil {
+			return fmt.Errorf("adding netnsid")
+		}
+
+		m.accessor = accessor
+		m.ds = ds
+	}
+	return nil
+}
+
+func (i *ebpfInstance) Name() string {
+	return "ebpf"
+}
+
+func (i *ebpfInstance) ExtraParams(gadgetCtx operators.GadgetContext) api.Params {
+	res := make(api.Params, 0, len(i.params))
+	for _, p := range i.params {
+		res = append(res, p.Param)
+	}
+	return res
+}
+
+func (i *ebpfInstance) Prepare(gadgetCtx operators.GadgetContext) error {
+	// Create network tracers, one for each socket filter program
+	for _, p := range i.collectionSpec.Programs {
+		switch p.Type {
+		case ebpf.SocketFilter:
+			if strings.HasPrefix(p.SectionName, "socket") {
+				networkTracer, err := networktracer.NewTracer[api.GadgetData]()
+				if err != nil {
+					i.Close()
+					return fmt.Errorf("creating network tracer: %w", err)
+				}
+				i.networkTracers[p.Name] = networkTracer
+			}
+		case ebpf.SchedCLS:
+			parts := strings.Split(p.SectionName, "/")
+			if len(parts) != 3 {
+				return fmt.Errorf("invalid section name %q", p.SectionName)
+			}
+			if parts[0] != "classifier" {
+				return fmt.Errorf("invalid section name %q", p.SectionName)
+			}
+
+			var direction tchandler.AttachmentDirection
+
+			switch parts[1] {
+			case "ingress":
+				direction = tchandler.AttachmentDirectionIngress
+			case "egress":
+				direction = tchandler.AttachmentDirectionEgress
+			default:
+				return fmt.Errorf("unsupported hook type %q", parts[1])
+			}
+
+			handler, err := tchandler.NewHandler(direction)
+			if err != nil {
+				i.Close()
+				return fmt.Errorf("creating tc network tracer: %w", err)
+			}
+
+			i.tcHandlers[p.Name] = handler
+		}
+	}
+
+	if len(i.tcHandlers) > 0 {
+		// For now, override enrichment
+		gadgetCtx.SetVar("NeedContainerEvents", true)
+		i.params["iface"] = &param{
+			Param: &api.Param{
+				Key:         ParamIface,
+				Description: "Network interface to attach to",
+			},
+		}
+	}
+
+	i.params[ParamTraceKernel] = &param{
+		Param: &api.Param{
+			Key:          ParamTraceKernel,
+			DefaultValue: "false",
+			TypeHint:     api.TypeBool,
+		},
+	}
+	return nil
+}
+
+func (i *ebpfInstance) tracePipe(gadgetCtx operators.GadgetContext) error {
+	tracePipe, err := os.Open("/sys/kernel/debug/tracing/trace_pipe")
+	if err != nil {
+		return fmt.Errorf("opening trace_pipe: %w", err)
+	}
+	go func() {
+		<-gadgetCtx.Context().Done()
+		tracePipe.Close()
+	}()
+	go func() {
+		log := gadgetCtx.Logger()
+
+		defer tracePipe.Close()
+		scanner := bufio.NewScanner(tracePipe)
+		for scanner.Scan() {
+			log.Debug(scanner.Text())
+		}
+	}()
+	return nil
+}
+
+func (i *ebpfInstance) Start(gadgetCtx operators.GadgetContext) error {
+	i.logger.Debugf("starting ebpfInstance")
+
+	gadgets.FixBpfKtimeGetBootNs(i.collectionSpec.Programs)
+
+	parameters := params.Params{}              // used to CopyFromMap
+	paramMap := make(map[string]*params.Param) // used for second iteration
+	for name, p := range i.params {
+		param := apihelpers.ParamToParamDesc(p.Param).ToParam()
+		paramMap[name] = param
+		parameters = append(parameters, param)
+	}
+	err := parameters.CopyFromMap(i.paramValues, "")
+	if err != nil {
+		return fmt.Errorf("parsing parameter values: %w", err)
+	}
+
+	if paramMap[ParamTraceKernel].AsBool() {
+		err := i.tracePipe(gadgetCtx)
+		if err != nil {
+			return err
+		}
+	}
+
+	mapReplacements := make(map[string]*ebpf.Map)
+	constReplacements := make(map[string]any)
+
+	// Set gadget params
+	for name, p := range i.params {
+		if !p.fromEbpf {
+			continue
+		}
+		constReplacements[name] = paramMap[name].AsAny()
+		i.logger.Debugf("setting param value %q = %v", name, paramMap[name].AsAny())
+	}
+
+	for _, v := range i.vars {
+		res, ok := gadgetCtx.GetVar(v.name)
+		if !ok {
+			continue
+		}
+		i.logger.Debugf("got var %q: %+v", v.name, res)
+		switch t := res.(type) {
+		case *ebpf.Map:
+			if t == nil {
+				continue
+			}
+			i.logger.Debugf("replacing map %q", v.name)
+			mapReplacements[v.name] = t
+		default:
+			if !reflect.TypeOf(res).AssignableTo(v.refType) {
+				i.logger.Debugf("variable %q can not be set to type %T (expected %s)", v.name, res, v.refType.Name())
+				continue
+			}
+			i.logger.Debugf("setting var %q to %v", v.name, t)
+			constReplacements[v.name] = res
+		}
+	}
+
+	if err := i.collectionSpec.RewriteConstants(constReplacements); err != nil {
+		return fmt.Errorf("rewriting constants: %w", err)
+	}
+
+	i.logger.Debugf("creating ebpf collection")
+	collection, err := ebpf.NewCollectionWithOptions(i.collectionSpec, ebpf.CollectionOptions{
+		MapReplacements: mapReplacements,
+	})
+	if err != nil {
+		return fmt.Errorf("creating eBPF collection: %w", err)
+	}
+	i.collection = collection
+
+	for _, tracer := range i.tracers {
+		i.logger.Debugf("starting tracer %q", tracer.MapName)
+		go func(tracer *Tracer) {
+			err := i.runTracer(gadgetCtx, tracer)
+			if err != nil {
+				i.logger.Errorf("starting tracer: %w", err)
+			}
+		}(tracer)
+	}
+
+	// Attach programs
+	for progName, p := range i.collectionSpec.Programs {
+		l, err := i.attachProgram(gadgetCtx, p, i.collection.Programs[progName])
+		if err != nil {
+			i.Close()
+			return fmt.Errorf("attaching eBPF program %q: %w", progName, err)
+		}
+		if l != nil {
+			i.links = append(i.links, l)
+		}
+
+		// we need to store links to iterators on a separated list because we need them to run the programs.
+		if p.Type == ebpf.Tracing && strings.HasPrefix(p.SectionName, iterPrefix) {
+			lIter, ok := l.(*link.Iter)
+			if !ok {
+				return fmt.Errorf("link is not an iterator")
+			}
+			i.linksSnapshotters = append(i.linksSnapshotters, &linkSnapshotter{link: lIter, typ: p.AttachTo})
+		}
+	}
+
+	err = i.runSnapshotters()
+	if err != nil {
+		i.Close()
+		return fmt.Errorf("running snapshotters: %w", err)
+	}
+
+	return nil
+}
+
+func (i *ebpfInstance) Stop(gadgetCtx operators.GadgetContext) error {
+	i.Close()
+	return nil
+}
+
+func (i *ebpfInstance) Close() {
+	if i.collection != nil {
+		i.collection.Close()
+		i.collection = nil
+	}
+	for _, l := range i.links {
+		gadgets.CloseLink(l)
+	}
+	i.links = nil
+
+	for _, networkTracer := range i.networkTracers {
+		networkTracer.Close()
+	}
+	for _, handler := range i.tcHandlers {
+		handler.Close()
+	}
+}
+
+// Using Attacher interface for network tracers for now
+
+func (i *ebpfInstance) AttachContainer(container *containercollection.Container) error {
+	i.mu.Lock()
+	i.containers[container.Runtime.ContainerID] = container
+	i.mu.Unlock()
+
+	for _, networkTracer := range i.networkTracers {
+		if err := networkTracer.Attach(container.Pid); err != nil {
+			return err
+		}
+	}
+
+	if ifaceName := i.paramValues[ParamIface]; ifaceName == "" {
+		for _, handler := range i.tcHandlers {
+			if err := handler.AttachContainer(container); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (i *ebpfInstance) DetachContainer(container *containercollection.Container) error {
+	i.mu.Lock()
+	delete(i.containers, container.Runtime.ContainerID)
+	i.mu.Unlock()
+
+	for _, networkTracer := range i.networkTracers {
+		if err := networkTracer.Detach(container.Pid); err != nil {
+			return err
+		}
+	}
+
+	if ifaceName := i.paramValues[ParamIface]; ifaceName == "" {
+		for _, handler := range i.tcHandlers {
+			if err := handler.DetachContainer(container); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	operators.RegisterOperatorForMediaType(eBPFObjectMediaType, &ebpfOperator{})
+}

commit 9a2875dedd1c76fd85134b193905ee5cfe930003

ebpf: Remove trailing garbage when reading sample from perf event array
The perf event array can contain trailing garbage, we need to remove it
before emitting the sample.

Fixes: d28c76d0ef85 ("pkg/operators/ebpf: ebpf layer operator")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/ebpf/tracer.go
@@ -172,18 +172,22 @@ func (t *Tracer) receiveEventsFromPerfReader(gadgetCtx operators.GadgetContext) 
 		}
 		data := t.ds.NewData()
 		sample := rec.RawSample
-		if uint32(len(rec.RawSample)) < t.eventSize {
+		sampleLen := len(rec.RawSample)
+		if uint32(sampleLen) < t.eventSize {
 			// event is truncated; we need to copy
 			copy(slowBuf, rec.RawSample)
 
 			// zero difference; TODO: improve
-			if len(rec.RawSample) < lastSlowLen {
-				for i := len(rec.RawSample); i < lastSlowLen; i++ {
+			if sampleLen < lastSlowLen {
+				for i := sampleLen; i < lastSlowLen; i++ {
 					slowBuf[i] = 0
 				}
 			}
-			lastSlowLen = len(rec.RawSample)
+			lastSlowLen = sampleLen
 			sample = slowBuf
+		} else if uint32(sampleLen) > t.eventSize {
+			// event has trailing garbage, remove it
+			sample = sample[:t.eventSize]
 		}
 		err = t.accessor.Set(data, sample)
 		if err != nil {