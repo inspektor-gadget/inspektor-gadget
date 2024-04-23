2b37e285e0bf18eedef81cc892f1ae570fddc097
Mauricio Vásquez <mauriciov@microsoft.com>
2024-04-23 15:34:01 -0500

    gadgets/trace_open: Embed gadget image bundle
    Create a bundle with the gadget image.
    This will be consumed by golang applications.
    
    This was manually created with
    
    $ sudo -E ig image export ./trace_open.tar trace_open
    
    Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

---
+++ gadgets/trace_open/gadget.go
@@ -0,0 +1,8 @@
+package trace_open
+
+import (
+	_ "embed"
+)
+
+//go:embed trace_open.tar
+var TraceOpenBundle []byte
--- gadgets/trace_open/program.bpf.c
+++ gadgets/trace_open/program.bpf.c
@@ -1,3 +1,4 @@
+// +build ignore
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2019 Facebook
 // Copyright (c) 2020 Netflix
