inspektor-gadget/inspektor-gadget

commit dab012336fad94c38e420956ca7b6bcad1b406ff

operators: Add UidGidResolver
This operator enriches events which provides the UidGidResolverInterface
interface. It then enriches the event with the corresponding user name
for the given uid and the group name.

The user name and group name are always from the hosts point of view

Signed-off-by: Burak Ok <burakok@microsoft.com>

pkg/operators/uidgidresolver/usergroupcache.go
@@ -1,0 +1,251 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package uidgidresolver
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/cachedmap"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
+)
+
+// UserGroupCache is a cache of user names, uids, group names and gids
+type UserGroupCache interface {
+	Start() error
+	Stop()
+
+	GetUsername(uint32) string
+	GetGroupname(uint32) string
+}
+
+type userGroupCache struct {
+	userCache  cachedmap.CachedMap[uint32, string]
+	groupCache cachedmap.CachedMap[uint32, string]
+
+	loopFinished  chan struct{}
+	useCount      int
+	useCountMutex sync.Mutex
+	watcher       *fsnotify.Watcher
+}
+
+const (
+	passwdFileName = "passwd"
+	groupFileName  = "group"
+	baseDirPath    = "/etc"
+)
+
+var (
+	fullPasswdPath = filepath.Join(host.HostRoot, baseDirPath, passwdFileName)
+	fullGroupPath  = filepath.Join(host.HostRoot, baseDirPath, groupFileName)
+)
+
+func GetUserGroupCache() UserGroupCache {
+	return sync.OnceValue(func() *userGroupCache {
+		return &userGroupCache{}
+	})()
+}
+
+func (cache *userGroupCache) Start() error {
+	cache.useCountMutex.Lock()
+	defer cache.useCountMutex.Unlock()
+
+	// No uses before us, we are the first one
+	if cache.useCount == 0 {
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			return fmt.Errorf("UserGroupCache: create watcher: %w", err)
+		}
+		defer func() {
+			// Only close the watcher if we are not going to use it
+			if watcher != nil {
+				watcher.Close()
+			}
+		}()
+
+		err = watcher.Add(filepath.Join(host.HostRoot, baseDirPath))
+		if err != nil {
+			return fmt.Errorf("UserGroupCache: add watch: %w", err)
+		}
+
+		cache.userCache = cachedmap.NewCachedMap[uint32, string](2 * time.Second)
+		cache.groupCache = cachedmap.NewCachedMap[uint32, string](2 * time.Second)
+
+		// Initial read
+		cache.userCache.Clear()
+		cache.groupCache.Clear()
+		passwdFile, err := os.OpenFile(fullPasswdPath, os.O_RDONLY, 0)
+		if err != nil {
+			return fmt.Errorf("UserGroupCache: open %q: %w", fullPasswdPath, err)
+		}
+		defer passwdFile.Close()
+		updateEntries(passwdFile, cache.userCache)
+
+		groupFile, err := os.OpenFile(fullGroupPath, os.O_RDONLY, 0)
+		if err != nil {
+			return fmt.Errorf("UserGroupCache: open %q: %w", fullGroupPath, err)
+		}
+		defer groupFile.Close()
+		updateEntries(groupFile, cache.groupCache)
+
+		cache.watcher = watcher
+		watcher = nil
+		cache.loopFinished = make(chan struct{})
+		go cache.watchUserGroupLoop()
+	}
+	cache.useCount++
+	return nil
+}
+
+func (cache *userGroupCache) Close() {
+	if cache.watcher != nil {
+		err := cache.watcher.Close()
+		if err != nil {
+			log.Warnf("UserGroupCache: close watcher: %v", err)
+		}
+		// Wait until the loop is finished, should be fast
+		<-cache.loopFinished
+		cache.watcher = nil
+
+		cache.userCache.Close()
+		cache.groupCache.Close()
+	}
+}
+
+func (cache *userGroupCache) Stop() {
+	cache.useCountMutex.Lock()
+	defer cache.useCountMutex.Unlock()
+
+	// We are the last user, stop everything
+	if cache.useCount == 1 {
+		cache.Close()
+	}
+	cache.useCount--
+}
+
+func (cache *userGroupCache) watchUserGroupLoop() {
+	defer close(cache.loopFinished)
+	for {
+		select {
+		case event, ok := <-cache.watcher.Events:
+			if !ok {
+				log.Warnf("UserGroupCache: watcher event not ok")
+				return
+			}
+			cache.handleEvent(event)
+		case err, ok := <-cache.watcher.Errors:
+			if !ok {
+				if err == nil {
+					// Watcher closed
+					return
+				}
+				log.Warnf("UserGroupCache: watcher error not ok: %v", err)
+				return
+			}
+			log.Warnf("UserGroupCache: watcher error: %v", err)
+		}
+	}
+}
+
+func (cache *userGroupCache) handleEvent(event fsnotify.Event) {
+	// Filter out chmod events first, to keep string comparisons to a minimum
+	if event.Has(fsnotify.Chmod) {
+		return
+	}
+
+	targetFilePath := ""
+	var resourceCache cachedmap.CachedMap[uint32, string]
+	if event.Name == fullPasswdPath {
+		targetFilePath = fullPasswdPath
+		resourceCache = cache.userCache
+	} else if event.Name == fullGroupPath {
+		targetFilePath = fullGroupPath
+		resourceCache = cache.groupCache
+	} else {
+		return
+	}
+
+	var targetFile *os.File
+	if event.Has(fsnotify.Remove) || event.Has(fsnotify.Rename) {
+		targetFile = nil
+	} else if event.Has(fsnotify.Write) || event.Has(fsnotify.Create) {
+		var err error
+		targetFile, err = os.OpenFile(targetFilePath, os.O_RDONLY, 0)
+		if err != nil {
+			log.Warnf("UserGroupCache: open target file: %v", err)
+			return
+		}
+		defer targetFile.Close()
+	} else {
+		// Ignore all other events
+		return
+	}
+
+	updateEntries(targetFile, resourceCache)
+}
+
+func updateEntries(file *os.File, resourceCache cachedmap.CachedMap[uint32, string]) {
+	oldEntries := make(map[uint32]struct{})
+	for _, id := range resourceCache.Keys() {
+		oldEntries[id] = struct{}{}
+	}
+
+	if file != nil {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := strings.TrimLeft(scanner.Text(), " \t")
+			if len(line) == 0 || line[0] == '#' {
+				continue
+			}
+			split := strings.Split(line, ":")
+			// We are interested only in the first and third field
+			if len(split) < 3 {
+				continue
+			}
+			name := split[0]
+			id_u64, err := strconv.ParseUint(split[2], 10, 32)
+			if err != nil {
+				log.Warnf("UserGroupCache: convert id: %v", err)
+				continue
+			}
+			id := uint32(id_u64)
+			delete(oldEntries, id)
+			resourceCache.Add(id, name)
+		}
+	}
+
+	for id := range oldEntries {
+		resourceCache.Remove(id)
+	}
+}
+
+func (cache *userGroupCache) GetUsername(uid uint32) string {
+	name, _ := cache.userCache.Get(uid)
+	return name
+}
+
+func (cache *userGroupCache) GetGroupname(gid uint32) string {
+	name, _ := cache.groupCache.Get(gid)
+	return name
+}

cmd/ig/main.go
@@ -45,6 +45,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/localmanager"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/prometheus"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/socketenricher"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/uidgidresolver"
 )
 
 func main() {

gadget-container/gadgettracermanager/main.go
@@ -51,6 +51,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/formatters"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/kubemanager"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/socketenricher"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/uidgidresolver"
 
 	gadgetservice "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"

integration/k8s/trace_exec_test.go
@@ -94,6 +94,8 @@ func TestTraceExec(t *testing.T) {
 				e.SessionId = 0
 				e.Retval = 0
 				e.MountNsID = 0
+				e.Username = ""
+				e.Groupname = ""
 
 				if e.Comm == "sh" {
 					// Not tested because it varies depending on container runtime:
@@ -163,6 +165,8 @@ func TestTraceExecHost(t *testing.T) {
 				e.SessionId = 0
 				e.Retval = 0
 				e.MountNsID = 0
+				e.Username = ""
+				e.Groupname = ""
 			}
 
 			match.ExpectEntriesToMatch(t, output, normalize, expectedEntries...)

pkg/gadgets/trace/exec/types/types.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2023 The Inspektor Gadget authors
+// Copyright 2019-2024 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -32,12 +32,30 @@ type Event struct {
 	Retval     int      `json:"ret,omitempty" column:"ret,width:3,fixed"`
 	Args       []string `json:"args,omitempty" column:"args,width:40"`
 	Uid        uint32   `json:"uid" column:"uid,template:uid,hide"`
+	Username   string   `json:"user,omitempty" column:"user,hide"`
 	Gid        uint32   `json:"gid" column:"gid,template:gid,hide"`
+	Groupname  string   `json:"group,omitempty" column:"group,hide"`
 	UpperLayer bool     `json:"upperlayer" column:"upperlayer,width:10,fixed,hide"`
 	LoginUid   uint32   `json:"loginuid" column:"loginuid,template:uid,hide"`
 	SessionId  uint32   `json:"sessionid" column:"sessionid,minWidth:10,hide"`
 	Cwd        string   `json:"cwd,omitempty" column:"cwd,width:40" columnTags:"param:paths"`
 	ExePath    string   `json:"exepath,omitempty" column:"exepath,width:40" columnTags:"param:paths"`
+}
+
+func (e *Event) GetUid() uint32 {
+	return e.Uid
+}
+
+func (e *Event) SetUserName(username string) {
+	e.Username = username
+}
+
+func (e *Event) GetGid() uint32 {
+	return e.Gid
+}
+
+func (e *Event) SetGroupName(groupname string) {
+	e.Groupname = groupname
 }
 
 func GetColumns() *columns.Columns[Event] {

pkg/operators/uidgidresolver/uidgidresolver.go
@@ -1,0 +1,127 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package uidgidresolver provides an operator that enriches events by looking
+// up uid and gid resolving them to the corresponding username and groupname.
+// Only /etc/passwd and /etc/group is read on the host. Therefore the name for a
+// corresponding id could be wrong.
+package uidgidresolver
+
+import (
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
+)
+
+const (
+	OperatorName = "UidGidResolver"
+)
+
+type UidResolverInterface interface {
+	GetUid() uint32
+	SetUserName(string)
+}
+
+type GidResolverInterface interface {
+	GetGid() uint32
+	SetGroupName(string)
+}
+
+type UidGidResolver struct{}
+
+func (k *UidGidResolver) Name() string {
+	return OperatorName
+}
+
+func (k *UidGidResolver) Description() string {
+	return "UidGidResolver resolves uid and gid to username and groupname"
+}
+
+func (k *UidGidResolver) GlobalParamDescs() params.ParamDescs {
+	return nil
+}
+
+func (k *UidGidResolver) ParamDescs() params.ParamDescs {
+	return nil
+}
+
+func (k *UidGidResolver) Dependencies() []string {
+	return nil
+}
+
+func (k *UidGidResolver) CanOperateOn(gadget gadgets.GadgetDesc) bool {
+	_, hasUidResolverInterface := gadget.EventPrototype().(UidResolverInterface)
+	_, hasGidResolverInterface := gadget.EventPrototype().(GidResolverInterface)
+	return hasUidResolverInterface || hasGidResolverInterface
+}
+
+func (k *UidGidResolver) Init(params *params.Params) error {
+	return nil
+}
+
+func (k *UidGidResolver) Close() error {
+	return nil
+}
+
+func (k *UidGidResolver) Instantiate(gadgetCtx operators.GadgetContext, gadgetInstance any, params *params.Params) (operators.OperatorInstance, error) {
+	uidGidCache := GetUserGroupCache()
+
+	return &UidGidResolverInstance{
+		gadgetCtx:      gadgetCtx,
+		gadgetInstance: gadgetInstance,
+		uidGidCache:    uidGidCache,
+	}, nil
+}
+
+type UidGidResolverInstance struct {
+	gadgetCtx      operators.GadgetContext
+	gadgetInstance any
+	uidGidCache    UserGroupCache
+}
+
+func (m *UidGidResolverInstance) Name() string {
+	return "UidGidResolverInstance"
+}
+
+func (m *UidGidResolverInstance) PreGadgetRun() error {
+	return m.uidGidCache.Start()
+}
+
+func (m *UidGidResolverInstance) PostGadgetRun() error {
+	m.uidGidCache.Stop()
+	return nil
+}
+
+func (m *UidGidResolverInstance) enrich(ev any) {
+	uidResolver := ev.(UidResolverInterface)
+	if uidResolver != nil {
+		uid := uidResolver.GetUid()
+		uidResolver.SetUserName(m.uidGidCache.GetUsername(uid))
+	}
+
+	gidResolver := ev.(GidResolverInterface)
+	if gidResolver != nil {
+		gid := gidResolver.GetGid()
+		gidResolver.SetGroupName(m.uidGidCache.GetGroupname(gid))
+	}
+}
+
+func (m *UidGidResolverInstance) EnrichEvent(ev any) error {
+	m.enrich(ev)
+	return nil
+}
+
+func init() {
+	operators.Register(&UidGidResolver{})
+}

commit 932a3fd8a0f1db0d0d8f7042e5640c7c548bf9c0

UserGroupCache: Move OnceValue to global scope
Creating the OnceValue inside the function and then calling it doesn't
create the desired effect. It just creates new caches all the time.

Fixes: dab012336fad94c38e420956ca7b6bcad1b406ff
Signed-off-by: Burak Ok <burakok@microsoft.com>

pkg/operators/uidgidresolver/usergroupcache.go
@@ -57,15 +57,12 @@ const (
 )
 
 var (
-	fullPasswdPath = filepath.Join(host.HostRoot, baseDirPath, passwdFileName)
-	fullGroupPath  = filepath.Join(host.HostRoot, baseDirPath, groupFileName)
-)
-
-func GetUserGroupCache() UserGroupCache {
-	return sync.OnceValue(func() *userGroupCache {
+	fullPasswdPath    = filepath.Join(host.HostRoot, baseDirPath, passwdFileName)
+	fullGroupPath     = filepath.Join(host.HostRoot, baseDirPath, groupFileName)
+	GetUserGroupCache = sync.OnceValue(func() *userGroupCache {
 		return &userGroupCache{}
-	})()
-}
+	})
+)
 
 func (cache *userGroupCache) Start() error {
 	cache.useCountMutex.Lock()