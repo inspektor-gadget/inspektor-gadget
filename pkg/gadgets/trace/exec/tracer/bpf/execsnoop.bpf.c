inspektor-gadget/inspektor-gadget

commit 4f471903d5c9da31b1b8b046dc3b419dcab99c59

trace exec: add exe path
Thanks to this new field, we can distinguish the executable with its
full path when it is not visible in the args, e.g. when executing via a
symlink or via a procfs magic link such as /proc/self/exe.

Example of output:

```
$ sudo ig trace exec
RUNTIME.CONTAINERNAME PID    PPID   COMM  RET ARGS
test                  644871 639225 mkdir 0   /usr/bin/mkdir -p /tmp/bar/foo/
test                  644888 639225 cat   0   /usr/bin/cat /dev/null

$ sudo ig trace exec --paths
RUNTIME.CONTAINERN… PID    PPID   COMM  RET ARGS                     CWD EXEPATH
test                644377 639225 mkdir 0   /usr/bin/mkdir -p /tmp/… /   /usr/bin/mkdir
test                644497 639225 cat   0   /usr/bin/cat /dev/null   /   /usr/bin/cat
```

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -7,7 +7,7 @@
 #endif /* __TARGET_ARCH_arm64 */
 
 #include <gadget/mntns_filter.h>
-#ifdef WITH_CWD
+#ifdef WITH_LONG_PATHS
 #include <gadget/filesystem.h>
 #endif
 #include "execsnoop.h"
@@ -138,7 +138,7 @@ int ig_execve_e(struct syscall_trace_enter *ctx)
 	event->args_size = 0;
 	event->mntns_id = mntns_id;
 
-#ifdef WITH_CWD
+#ifdef WITH_LONG_PATHS
 	fs = BPF_CORE_READ(task, fs);
 	cwd = get_path_str(&fs->pwd);
 	bpf_probe_read_kernel_str(event->cwd, MAX_STRING_SIZE, cwd);
@@ -220,6 +220,8 @@ int ig_execve_x(struct syscall_trace_exit *ctx)
 	u32 uid = (u32)bpf_get_current_uid_gid();
 	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 	struct task_struct *parent = BPF_CORE_READ(task, real_parent);
+	struct file *exe_file;
+	char *exepath;
 
 	if (valid_uid(targ_uid) && targ_uid != uid)
 		return 0;
@@ -258,6 +260,12 @@ int ig_execve_x(struct syscall_trace_exit *ctx)
 	if (parent != NULL)
 		bpf_probe_read_kernel(&event->pcomm, sizeof(event->pcomm),
 				      parent->comm);
+
+#ifdef WITH_LONG_PATHS
+	exe_file = BPF_CORE_READ(task, mm, exe_file);
+	exepath = get_path_str(&exe_file->f_path);
+	bpf_probe_read_kernel_str(event->exepath, MAX_STRING_SIZE, exepath);
+#endif
 
 	size_t len = EVENT_SIZE(event);
 	if (len <= sizeof(*event))

integration/ig/k8s/trace_exec_test.go
@@ -36,7 +36,7 @@ func TestTraceExec(t *testing.T) {
 
 	traceExecCmd := &Command{
 		Name:         "TraceExec",
-		Cmd:          fmt.Sprintf("ig trace exec -o json --runtimes=%s --cwd", *containerRuntime),
+		Cmd:          fmt.Sprintf("ig trace exec -o json --runtimes=%s --paths", *containerRuntime),
 		StartAndStop: true,
 		ValidateOutput: func(t *testing.T, output string) {
 			isDockerRuntime := *containerRuntime == ContainerRuntimeDocker
@@ -48,10 +48,11 @@ func TestTraceExec(t *testing.T) {
 						WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime),
 						WithPodLabels("test-pod", ns, isCrioRuntime),
 					),
-					Comm:  "sh",
-					Pcomm: "", // Not tested, see normalize()
-					Args:  shArgs,
-					Cwd:   "/",
+					Comm:    "sh",
+					Pcomm:   "", // Not tested, see normalize()
+					Args:    shArgs,
+					Cwd:     "/",
+					ExePath: "/bin/sh",
 				},
 				{
 					Event: BuildBaseEvent(ns,
@@ -65,6 +66,7 @@ func TestTraceExec(t *testing.T) {
 					Uid:        1000,
 					Gid:        1111,
 					Cwd:        "/",
+					ExePath:    "/date",
 					UpperLayer: true,
 				},
 				{
@@ -73,12 +75,13 @@ func TestTraceExec(t *testing.T) {
 						WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime),
 						WithPodLabels("test-pod", ns, isCrioRuntime),
 					),
-					Comm:  "sleep",
-					Pcomm: "sh",
-					Args:  sleepArgs,
-					Uid:   1000,
-					Gid:   1111,
-					Cwd:   "/",
+					Comm:    "sleep",
+					Pcomm:   "sh",
+					Args:    sleepArgs,
+					Uid:     1000,
+					Gid:     1111,
+					Cwd:     "/",
+					ExePath: "/bin/sleep",
 				},
 			}
 

integration/inspektor-gadget/trace_exec_test.go
@@ -38,16 +38,17 @@ func TestTraceExec(t *testing.T) {
 
 	traceExecCmd := &Command{
 		Name:         "StartTraceExecGadget",
-		Cmd:          fmt.Sprintf("$KUBECTL_GADGET trace exec -n %s -o json --cwd", ns),
+		Cmd:          fmt.Sprintf("$KUBECTL_GADGET trace exec -n %s -o json --paths", ns),
 		StartAndStop: true,
 		ValidateOutput: func(t *testing.T, output string) {
 			expectedEntries := []*traceexecTypes.Event{
 				{
-					Event: BuildBaseEventK8s(ns, WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime)),
-					Comm:  "sh",
-					Pcomm: "", // Not tested, see normalize()
-					Args:  shArgs,
-					Cwd:   "/",
+					Event:   BuildBaseEventK8s(ns, WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime)),
+					Comm:    "sh",
+					Pcomm:   "", // Not tested, see normalize()
+					Args:    shArgs,
+					Cwd:     "/",
+					ExePath: "/bin/sh",
 				},
 				{
 					Event:      BuildBaseEventK8s(ns, WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime)),
@@ -57,16 +58,18 @@ func TestTraceExec(t *testing.T) {
 					Uid:        1000,
 					Gid:        1111,
 					Cwd:        "/",
+					ExePath:    "/date",
 					UpperLayer: true,
 				},
 				{
-					Event: BuildBaseEventK8s(ns, WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime)),
-					Comm:  "sleep",
-					Pcomm: "sh",
-					Args:  sleepArgs,
-					Uid:   1000,
-					Gid:   1111,
-					Cwd:   "/",
+					Event:   BuildBaseEventK8s(ns, WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime)),
+					Comm:    "sleep",
+					Pcomm:   "sh",
+					Args:    sleepArgs,
+					Uid:     1000,
+					Gid:     1111,
+					Cwd:     "/",
+					ExePath: "/bin/sleep",
 				},
 			}
 

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.h
@@ -28,8 +28,9 @@ struct event {
 	unsigned int args_size;
 	__u8 comm[TASK_COMM_LEN];
 	__u8 pcomm[TASK_COMM_LEN];
-#ifdef WITH_CWD
+#ifdef WITH_LONG_PATHS
 	__u8 cwd[MAX_STRING_SIZE];
+	__u8 exepath[MAX_STRING_SIZE];
 #endif
 	__u8 args[FULL_MAX_ARGS_ARR];
 };

pkg/gadgets/trace/exec/tracer/execsnoopwithcwd_bpfel.go
@@ -1,156 +1,0 @@
-// Code generated by bpf2go; DO NOT EDIT.
-//go:build 386 || amd64 || arm || arm64 || loong64 || mips64le || mipsle || ppc64le || riscv64
-
-package tracer
-
-import (
-	"bytes"
-	_ "embed"
-	"fmt"
-	"io"
-
-	"github.com/cilium/ebpf"
-)
-
-type execsnoopWithCwdBufT struct{ Buf [32768]uint8 }
-
-type execsnoopWithCwdEvent struct {
-	MntnsId    uint64
-	Timestamp  uint64
-	Pid        uint32
-	Ppid       uint32
-	Uid        uint32
-	Gid        uint32
-	Loginuid   uint32
-	Sessionid  uint32
-	Retval     int32
-	ArgsCount  int32
-	UpperLayer bool
-	_          [3]byte
-	ArgsSize   uint32
-	Comm       [16]uint8
-	Pcomm      [16]uint8
-	Cwd        [4096]uint8
-	Args       [7680]uint8
-}
-
-// loadExecsnoopWithCwd returns the embedded CollectionSpec for execsnoopWithCwd.
-func loadExecsnoopWithCwd() (*ebpf.CollectionSpec, error) {
-	reader := bytes.NewReader(_ExecsnoopWithCwdBytes)
-	spec, err := ebpf.LoadCollectionSpecFromReader(reader)
-	if err != nil {
-		return nil, fmt.Errorf("can't load execsnoopWithCwd: %w", err)
-	}
-
-	return spec, err
-}
-
-// loadExecsnoopWithCwdObjects loads execsnoopWithCwd and converts it into a struct.
-//
-// The following types are suitable as obj argument:
-//
-//	*execsnoopWithCwdObjects
-//	*execsnoopWithCwdPrograms
-//	*execsnoopWithCwdMaps
-//
-// See ebpf.CollectionSpec.LoadAndAssign documentation for details.
-func loadExecsnoopWithCwdObjects(obj interface{}, opts *ebpf.CollectionOptions) error {
-	spec, err := loadExecsnoopWithCwd()
-	if err != nil {
-		return err
-	}
-
-	return spec.LoadAndAssign(obj, opts)
-}
-
-// execsnoopWithCwdSpecs contains maps and programs before they are loaded into the kernel.
-//
-// It can be passed ebpf.CollectionSpec.Assign.
-type execsnoopWithCwdSpecs struct {
-	execsnoopWithCwdProgramSpecs
-	execsnoopWithCwdMapSpecs
-}
-
-// execsnoopWithCwdSpecs contains programs before they are loaded into the kernel.
-//
-// It can be passed ebpf.CollectionSpec.Assign.
-type execsnoopWithCwdProgramSpecs struct {
-	IgExecveE *ebpf.ProgramSpec `ebpf:"ig_execve_e"`
-	IgExecveX *ebpf.ProgramSpec `ebpf:"ig_execve_x"`
-}
-
-// execsnoopWithCwdMapSpecs contains maps before they are loaded into the kernel.
-//
-// It can be passed ebpf.CollectionSpec.Assign.
-type execsnoopWithCwdMapSpecs struct {
-	Bufs                 *ebpf.MapSpec `ebpf:"bufs"`
-	Events               *ebpf.MapSpec `ebpf:"events"`
-	Execs                *ebpf.MapSpec `ebpf:"execs"`
-	GadgetMntnsFilterMap *ebpf.MapSpec `ebpf:"gadget_mntns_filter_map"`
-	PidByTgid            *ebpf.MapSpec `ebpf:"pid_by_tgid"`
-}
-
-// execsnoopWithCwdObjects contains all objects after they have been loaded into the kernel.
-//
-// It can be passed to loadExecsnoopWithCwdObjects or ebpf.CollectionSpec.LoadAndAssign.
-type execsnoopWithCwdObjects struct {
-	execsnoopWithCwdPrograms
-	execsnoopWithCwdMaps
-}
-
-func (o *execsnoopWithCwdObjects) Close() error {
-	return _ExecsnoopWithCwdClose(
-		&o.execsnoopWithCwdPrograms,
-		&o.execsnoopWithCwdMaps,
-	)
-}
-
-// execsnoopWithCwdMaps contains all maps after they have been loaded into the kernel.
-//
-// It can be passed to loadExecsnoopWithCwdObjects or ebpf.CollectionSpec.LoadAndAssign.
-type execsnoopWithCwdMaps struct {
-	Bufs                 *ebpf.Map `ebpf:"bufs"`
-	Events               *ebpf.Map `ebpf:"events"`
-	Execs                *ebpf.Map `ebpf:"execs"`
-	GadgetMntnsFilterMap *ebpf.Map `ebpf:"gadget_mntns_filter_map"`
-	PidByTgid            *ebpf.Map `ebpf:"pid_by_tgid"`
-}
-
-func (m *execsnoopWithCwdMaps) Close() error {
-	return _ExecsnoopWithCwdClose(
-		m.Bufs,
-		m.Events,
-		m.Execs,
-		m.GadgetMntnsFilterMap,
-		m.PidByTgid,
-	)
-}
-
-// execsnoopWithCwdPrograms contains all programs after they have been loaded into the kernel.
-//
-// It can be passed to loadExecsnoopWithCwdObjects or ebpf.CollectionSpec.LoadAndAssign.
-type execsnoopWithCwdPrograms struct {
-	IgExecveE *ebpf.Program `ebpf:"ig_execve_e"`
-	IgExecveX *ebpf.Program `ebpf:"ig_execve_x"`
-}
-
-func (p *execsnoopWithCwdPrograms) Close() error {
-	return _ExecsnoopWithCwdClose(
-		p.IgExecveE,
-		p.IgExecveX,
-	)
-}
-
-func _ExecsnoopWithCwdClose(closers ...io.Closer) error {
-	for _, closer := range closers {
-		if err := closer.Close(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// Do not access this directly.
-//
-//go:embed execsnoopwithcwd_bpfel.o
-var _ExecsnoopWithCwdBytes []byte

commit 34c02c79c9b22205da34fcfb0a374aa61070264b

trace exec builtin gadget: fix memory usage
The define WITH_CWD was renamed to WITH_LONG_PATHS. But some occurrences
were forgotten.

Fixes: 4f471903d5c9 ("trace exec: add exe path")

See also: 8a4461a5738a ("trace exec: Revert map size to its previous value")

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -23,11 +23,11 @@ static const struct event empty_event = {};
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-#ifdef WITH_CWD
+#ifdef WITH_LONG_PATHS
 	__uint(max_entries, 1024);
-#else /* !WITH_CWD */
+#else /* !WITH_LONG_PATHS */
 	__uint(max_entries, 10240);
-#endif /* !WITH_CWD */
+#endif /* !WITH_LONG_PATHS */
 	__type(key, pid_t);
 	__type(value, struct event);
 } execs SEC(".maps");