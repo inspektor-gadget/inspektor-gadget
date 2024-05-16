inspektor-gadget/inspektor-gadget

commit 08e5409d0ed58ec933df204d272e90cfb371de3c

pkg/datasource: Don't use sync.Pool for data
The sync.Pool was useful in a previous refactoring iteration where we
were creating fixed sized buffers, which is not the case anymore.

Signed-off-by: Jose Blanquicet <josebl@microsoft.com>

pkg/datasource/data.go
@@ -70,7 +70,6 @@ type dataSource struct {
 	id   uint32
 
 	dType Type
-	dPool sync.Pool
 
 	// keeps information on registered fields
 	fields   []*field
@@ -104,9 +103,7 @@ func newDataSource(t Type, name string) *dataSource {
 }
 
 func New(t Type, name string) DataSource {
-	ds := newDataSource(t, name)
-	ds.registerPool()
-	return ds
+	return newDataSource(t, name)
 }
 
 func NewFromAPI(in *api.DataSource) (DataSource, error) {
@@ -122,21 +119,8 @@ func NewFromAPI(in *api.DataSource) (DataSource, error) {
 	} else {
 		ds.byteOrder = binary.LittleEndian
 	}
-	ds.registerPool()
 	// TODO: add more checks / validation
 	return ds, nil
-}
-
-func (ds *dataSource) registerPool() {
-	ds.dPool.New = func() any {
-		d := &data{
-			Payload: make([][]byte, ds.payloadCount),
-		}
-		for i := range d.Payload {
-			d.Payload[i] = make([]byte, 0)
-		}
-		return d
-	}
 }
 
 func (ds *dataSource) Name() string {
@@ -148,7 +132,13 @@ func (ds *dataSource) Type() Type {
 }
 
 func (ds *dataSource) NewData() Data {
-	return ds.dPool.Get().(Data)
+	d := &data{
+		Payload: make([][]byte, ds.payloadCount),
+	}
+	for i := range d.Payload {
+		d.Payload[i] = make([]byte, 0)
+	}
+	return d
 }
 
 func (ds *dataSource) ByteOrder() binary.ByteOrder {
@@ -336,7 +326,6 @@ func (ds *dataSource) Subscribe(fn DataFunc, priority int) {
 }
 
 func (ds *dataSource) EmitAndRelease(d Data) error {
-	defer ds.dPool.Put(d)
 	for _, sub := range ds.subscriptions {
 		err := sub.fn(ds, d)
 		if err != nil {
@@ -347,7 +336,6 @@ func (ds *dataSource) EmitAndRelease(d Data) error {
 }
 
 func (ds *dataSource) Release(d Data) {
-	ds.dPool.Put(d)
 }
 
 func (ds *dataSource) ReportLostData(ctr uint64) {

commit cbf5f9c95a7fbb5397eecc24ad15baa43af3144d

pkg/datasource: Release data after emitting
Re-introduce Release call which was wrongly removed.

Fixes: 08e5409 ('pkg/datasource: Don't use sync.Pool for data')

Signed-off-by: Jose Blanquicet <josebl@microsoft.com>

pkg/datasource/data.go
@@ -382,6 +382,7 @@ func (ds *dataSource) Subscribe(fn DataFunc, priority int) {
 }
 
 func (ds *dataSource) EmitAndRelease(d Data) error {
+	defer ds.Release(d)
 	for _, sub := range ds.subscriptions {
 		err := sub.fn(ds, d)
 		if err != nil {