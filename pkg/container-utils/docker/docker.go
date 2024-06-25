inspektor-gadget/inspektor-gadget

commit 4266f69c6f85efac69cd07d35cfd2c56d6ef902b

ig: Add flag for using CRI only to talk to container runtime
Signed-off-by: Burak Ok <burakok@microsoft.com>
Signed-off-by: Jose Blanquicet <josebl@microsoft.com>
Co-authored-by: Burak Ok <burakok@microsoft.com>
Co-authored-by: Jose Blanquicet <josebl@microsoft.com>

pkg/container-utils/docker/docker.go
@@ -28,6 +28,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/cgroups"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/cri"
 	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 )
@@ -46,9 +47,14 @@ type DockerClient struct {
 	socketPath string
 }
 
-func NewDockerClient(socketPath string) (runtimeclient.ContainerRuntimeClient, error) {
+func NewDockerClient(socketPath string, useCri bool) (runtimeclient.ContainerRuntimeClient, error) {
 	if socketPath == "" {
 		socketPath = runtimeclient.DockerDefaultSocketPath
+	}
+	if useCri {
+		// TODO: Configurable
+		socketPath = runtimeclient.CriDockerDefaultSocketPath
+		return cri.NewCRIClient(types.RuntimeNameDocker, socketPath, DefaultTimeout)
 	}
 
 	cli, err := client.NewClientWithOpts(

cmd/ig/utils/flags.go
@@ -56,6 +56,9 @@ type CommonFlags struct {
 
 	// ContainerdNamespace is the namespace used by containerd
 	ContainerdNamespace string
+
+	// UseCri specifies whether to use the CRI API to talk to the runtime
+	UseCri bool
 }
 
 func AddCommonFlags(command *cobra.Command, commonFlags *CommonFlags) {
@@ -95,12 +98,13 @@ func AddCommonFlags(command *cobra.Command, commonFlags *CommonFlags) {
 			r := &containerutilsTypes.RuntimeConfig{
 				Name:       runtimeName,
 				SocketPath: socketPath,
+				Extra: containerutilsTypes.ExtraConfig{
+					UseCri: commonFlags.UseCri,
+				},
 			}
 
 			if namespace != "" {
-				r.Extra = &containerutilsTypes.ExtraConfig{
-					Namespace: namespace,
-				}
+				r.Extra.Namespace = namespace
 			}
 
 			commonFlags.RuntimeConfigs = append(commonFlags.RuntimeConfigs, r)
@@ -155,6 +159,13 @@ func AddCommonFlags(command *cobra.Command, commonFlags *CommonFlags) {
 		"containerd-namespace",
 		constants.K8sContainerdNamespace,
 		"Namespace used by containerd",
+	)
+
+	command.PersistentFlags().BoolVar(
+		&commonFlags.UseCri,
+		"use-cri",
+		false,
+		"Use CRI API to retrieve more K8s information, but ignore non K8s containers",
 	)
 }
 

pkg/container-collection/options.go
@@ -38,6 +38,7 @@ import (
 	containerhook "github.com/inspektor-gadget/inspektor-gadget/pkg/container-hook"
 	containerutils "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/cgroups"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/cri"
 	ociannotations "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/oci-annotations"
 	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
 	containerutilsTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/types"
@@ -75,10 +76,31 @@ func containerRuntimeEnricher(
 	runtimeClient runtimeclient.ContainerRuntimeClient,
 	container *Container,
 ) bool {
-	// If the container is already enriched with the metadata a runtime client
-	// is able to provide, skip it.
+	// If the container is already enriched with all the metadata a runtime
+	// client is able to provide, skip it.
 	if runtimeclient.IsEnrichedWithK8sMetadata(container.K8s.BasicK8sMetadata) &&
 		runtimeclient.IsEnrichedWithRuntimeMetadata(container.Runtime.BasicRuntimeMetadata) {
+		return true
+	}
+
+	// For new CRI-O containers, the next GetContainer() call will always fail
+	// because the container doesn't exist yet. So, if we have the sandbox ID,
+	// let's enrich the container at least with the PodLabels as the PodSandbox
+	// do exist at this point.
+	if container.Runtime.RuntimeName == types.RuntimeNameCrio {
+		criClient, ok := runtimeClient.(*cri.CRIClient)
+		if ok && container.SandboxId != "" {
+			labels, err := criClient.GetPodLabels(container.SandboxId)
+			if err != nil {
+				log.Warnf("Runtime enricher (%s): failed to GetPodLabels: %s",
+					runtimeName, err)
+
+				// We couldn't get the labels, but don't drop the container.
+				return true
+			}
+			container.K8s.PodLabels = labels
+		}
+
 		return true
 	}
 

pkg/container-utils/containerd/containerd.go
@@ -27,6 +27,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/cri"
 	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
 	containerutilsTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/types"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/types"
@@ -47,12 +48,16 @@ type ContainerdClient struct {
 	ctx    context.Context
 }
 
-func NewContainerdClient(socketPath string, config *containerutilsTypes.ExtraConfig) (runtimeclient.ContainerRuntimeClient, error) {
+func NewContainerdClient(socketPath string, config containerutilsTypes.ExtraConfig) (runtimeclient.ContainerRuntimeClient, error) {
 	if socketPath == "" {
 		socketPath = runtimeclient.ContainerdDefaultSocketPath
 	}
+	if config.UseCri {
+		return cri.NewCRIClient(types.RuntimeNameContainerd, socketPath, DefaultTimeout)
+	}
+
 	namespace := constants.K8sContainerdNamespace
-	if config != nil && config.Namespace != "" {
+	if config.Namespace != "" {
 		namespace = config.Namespace
 	}
 

pkg/container-utils/containerd/containerd_test.go
@@ -16,7 +16,7 @@ func TestNamespace(t *testing.T) {
 	// validate the container in k8s.io (default for ig) namespace
 	c1 := testutils.NewContainerdContainer("test-k8s-io", "sleep inf")
 	c1.Start(t)
-	k8sClient, err := NewContainerdClient("", nil)
+	k8sClient, err := NewContainerdClient("", containerutilsTypes.ExtraConfig{})
 	t.Cleanup(func() {
 		k8sClient.Close()
 		c1.Stop(t)
@@ -33,7 +33,7 @@ func TestNamespace(t *testing.T) {
 	// validate the container in default (default for containerd) namespace
 	c2 := testutils.NewContainerdContainer("test-default", "sleep inf", testutils.WithNamespace("default"))
 	c2.Start(t)
-	defaultClient, err := NewContainerdClient("", &containerutilsTypes.ExtraConfig{Namespace: "default"})
+	defaultClient, err := NewContainerdClient("", containerutilsTypes.ExtraConfig{Namespace: "default"})
 	t.Cleanup(func() {
 		defaultClient.Close()
 		c2.Stop(t)
@@ -48,7 +48,7 @@ func TestNamespace(t *testing.T) {
 	require.Equal(t, "test-default", container.Runtime.ContainerName)
 
 	// validate we can't see the container in empty-ns namespace
-	emptyClient, err := NewContainerdClient("", &containerutilsTypes.ExtraConfig{Namespace: "empty-ns"})
+	emptyClient, err := NewContainerdClient("", containerutilsTypes.ExtraConfig{Namespace: "empty-ns"})
 	t.Cleanup(func() {
 		emptyClient.Close()
 	})

pkg/container-utils/containerutils.go
@@ -53,7 +53,7 @@ func NewContainerRuntimeClient(runtime *containerutilsTypes.RuntimeConfig) (runt
 		if envsp := os.Getenv("INSPEKTOR_GADGET_DOCKER_SOCKETPATH"); envsp != "" && socketPath == "" {
 			socketPath = filepath.Join(host.HostRoot, envsp)
 		}
-		return docker.NewDockerClient(socketPath)
+		return docker.NewDockerClient(socketPath, runtime.Extra.UseCri)
 	case types.RuntimeNameContainerd:
 		socketPath := runtime.SocketPath
 		if envsp := os.Getenv("INSPEKTOR_GADGET_CONTAINERD_SOCKETPATH"); envsp != "" && socketPath == "" {

pkg/container-utils/cri/cri.go
@@ -52,7 +52,7 @@ type CRIClient struct {
 	client runtime.RuntimeServiceClient
 }
 
-func NewCRIClient(name types.RuntimeName, socketPath string, timeout time.Duration) (CRIClient, error) {
+func NewCRIClient(name types.RuntimeName, socketPath string, timeout time.Duration) (*CRIClient, error) {
 	conn, err := grpc.Dial(
 		socketPath,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
@@ -62,18 +62,16 @@ func NewCRIClient(name types.RuntimeName, socketPath string, timeout time.Durati
 		}),
 	)
 	if err != nil {
-		return CRIClient{}, err
+		return nil, err
 	}
 
-	cc := CRIClient{
+	return &CRIClient{
 		Name:        name,
 		SocketPath:  socketPath,
 		ConnTimeout: timeout,
 		conn:        conn,
 		client:      runtime.NewRuntimeServiceClient(conn),
-	}
-
-	return cc, nil
+	}, nil
 }
 
 func listContainers(c *CRIClient, filter *runtime.ContainerFilter) ([]*runtime.Container, error) {

pkg/container-utils/crio/crio.go
@@ -26,21 +26,10 @@ const (
 	DefaultTimeout = 2 * time.Second
 )
 
-type CrioClient struct {
-	criclient.CRIClient
-}
-
 func NewCrioClient(socketPath string) (runtimeclient.ContainerRuntimeClient, error) {
 	if socketPath == "" {
 		socketPath = runtimeclient.CrioDefaultSocketPath
 	}
 
-	criClient, err := criclient.NewCRIClient(types.RuntimeNameCrio, socketPath, DefaultTimeout)
-	if err != nil {
-		return nil, err
-	}
-
-	return &CrioClient{
-		CRIClient: criClient,
-	}, nil
+	return criclient.NewCRIClient(types.RuntimeNameCrio, socketPath, DefaultTimeout)
 }

commit c8362da15f90bfb8f477d6ad4e063a48d59b0a37

pkg/container-utils: Preprend HOST_ROOT to /run/cri-dockerd.sock.
Fixes: 4266f69c6f85 ("ig: Add flag for using CRI only to talk to container runtime")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/container-utils/docker/docker.go
@@ -21,6 +21,7 @@ import (
 	"strings"
 	"time"
 
+	securejoin "github.com/cyphar/filepath-securejoin"
 	dockertypes "github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	dockerfilters "github.com/docker/docker/api/types/filters"
@@ -32,6 +33,7 @@ import (
 	runtimeclient "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/runtime-client"
 	containerutilsTypes "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/types"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
 )
 
 const (
@@ -56,8 +58,11 @@ func NewDockerClient(socketPath string, protocol string) (runtimeclient.Containe
 
 	case containerutilsTypes.RuntimeProtocolCRI:
 		// TODO: Configurable
-		socketPath = runtimeclient.CriDockerDefaultSocketPath
-		return cri.NewCRIClient(types.RuntimeNameDocker, socketPath, DefaultTimeout)
+		joinedSocketPath, err := securejoin.SecureJoin(host.HostRoot, runtimeclient.CriDockerDefaultSocketPath)
+		if err != nil {
+			return nil, fmt.Errorf("securejoining %v to %v: %w", host.HostRoot, runtimeclient.CriDockerDefaultSocketPath, err)
+		}
+		return cri.NewCRIClient(types.RuntimeNameDocker, joinedSocketPath, DefaultTimeout)
 
 	default:
 		return nil, fmt.Errorf("unknown runtime protocol %q", protocol)