6d7413113ef73fc16a03dfdfe9ceebef8016effe
Michael Friese <mfriese@microsoft.com>
2024-06-27 00:12:47 +0200

    pkg/datasources: add support for arrays
    This adds support for arrays of statically sized kinds (so this works for ints and floats,
    currently.)
    
    Signed-off-by: Michael Friese <mfriese@microsoft.com>

--- pkg/gadget-service/api/consts.go
+++ pkg/gadget-service/api/consts.go
@@ -34,6 +34,18 @@ const (
 )
 
 const (
+	KindFlagArray Kind = 0x10000000
+)
+
+func ArrayOf(kind Kind) Kind {
+	return kind | KindFlagArray
+}
+
+func IsArrayKind(kind Kind) bool {
+	return kind&KindFlagArray != 0
+}
+
+const (
 	GadgetServicePort = 8080
 	DefaultDaemonPath = "unix:///var/run/ig/ig.socket"
 )
