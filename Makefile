inspektor-gadget/inspektor-gadget

commit 7a25d47ec46edcae209d816ba7880e0d5b9740a9

add version skew warning
Show a warning message when kubectl-gadget and gadget pod versions are different

Signed-off-by: Shashank <shashanksingh819@gmail.com>
Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

Makefile
@@ -56,7 +56,7 @@ include crd.mk
 include tests.mk
 include minikube.mk
 
-LDFLAGS := "-X github.com/inspektor-gadget/inspektor-gadget/cmd/common.version=$(VERSION) \
+LDFLAGS := "-X github.com/inspektor-gadget/inspektor-gadget/internal/version.version=$(VERSION) \
 -X main.gadgetimage=$(CONTAINER_REPO):$(IMAGE_TAG) \
 -extldflags '-static'"
 
@@ -210,7 +210,7 @@ gadget-container:
 			BTFHUB_ARCHIVE=$(HOME)/btfhub-archive/ -j$(nproc); \
 	fi
 	docker buildx build --load -t $(CONTAINER_REPO):$(IMAGE_TAG) \
-		--build-arg GOPROXY=$(GOPROXY) \
+		--build-arg GOPROXY=$(GOPROXY) --build-arg VERSION=$(VERSION) \
 		-f Dockerfiles/gadget.Dockerfile .
 
 .PHONY: cross-gadget-container
@@ -223,7 +223,7 @@ cross-gadget-container:
 			ARCH=arm64 BTFHUB_ARCHIVE=$(HOME)/btfhub-archive/ -j$(nproc); \
 	fi
 	docker buildx build --platform=$(PLATFORMS) -t $(CONTAINER_REPO):$(IMAGE_TAG) \
-		--push --build-arg GOPROXY=$(GOPROXY) \
+		--push --build-arg GOPROXY=$(GOPROXY) --build-arg VERSION=$(VERSION) \
 		-f Dockerfiles/gadget.Dockerfile .
 
 push-gadget-container:

gadget-container/Makefile
@@ -3,6 +3,7 @@ gadget-container-deps: entrypoint cleanup ocihookgadget gadgettracermanager nrig
 
 
 TARGET_ARCH ?= $(shell go env GOHOSTARCH)
+VERSION ?= $(shell git describe --tags --always --dirty)
 
 # Entrypoint
 
@@ -26,6 +27,7 @@ cleanup:
 gadgettracermanager:
 	mkdir -p bin
 	GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=$(TARGET_ARCH) go build \
+	 -ldflags "-X github.com/inspektor-gadget/inspektor-gadget/internal/version.version=$(VERSION)" \
 		-o bin/gadgettracermanager \
 		./gadgettracermanager/
 

Dockerfiles/gadget.Dockerfile
@@ -12,6 +12,8 @@ FROM --platform=${BUILDPLATFORM} ${BUILDER_IMAGE} as builder
 
 ARG TARGETARCH
 ARG BUILDARCH
+ARG VERSION=v0.0.0
+ENV VERSION=${VERSION}
 
 ARG GOPROXY
 ENV GOPROXY=${GOPROXY}

Dockerfiles/ig.Dockerfile
@@ -5,7 +5,7 @@ FROM --platform=${BUILDPLATFORM} ${BUILDER_IMAGE} as builder
 
 ARG TARGETOS
 ARG TARGETARCH
-ARG VERSION=undefined
+ARG VERSION=v0.0.0
 ENV VERSION=${VERSION}
 ARG EBPF_BUILDER=ghcr.io/inspektor-gadget/ebpf-builder:latest
 ENV EBPF_BUILDER=${EBPF_BUILDER}
@@ -19,7 +19,7 @@ ADD . /go/src/github.com/inspektor-gadget/inspektor-gadget
 WORKDIR /go/src/github.com/inspektor-gadget/inspektor-gadget
 
 RUN CGO_ENABLED=0 GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build \
-		-ldflags "-X github.com/inspektor-gadget/inspektor-gadget/cmd/common.version=${VERSION} \
+		-ldflags "-X github.com/inspektor-gadget/inspektor-gadget/internal/version.version=${VERSION} \
                   -X github.com/inspektor-gadget/inspektor-gadget/cmd/common/image.builderImage=${EBPF_BUILDER} \
                   -extldflags '-static'" \
 		-tags "netgo" \

cmd/common/utils/version-checker.go
@@ -1,0 +1,45 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"fmt"
+
+	"github.com/blang/semver"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
+)
+
+func CheckServerVersionSkew(serverVersion string) error {
+	clientSemver := version.Version()
+
+	// Do not print any warning if this is a prerelease to avoid annoying developers
+	if len(clientSemver.Pre) > 0 {
+		return nil
+	}
+
+	serverSemver, err := semver.ParseTolerant(serverVersion)
+	if err != nil {
+		log.Debugf("parsing server version: %s", err)
+		return nil
+	}
+
+	if !serverSemver.EQ(clientSemver) {
+		return fmt.Errorf("version skew detected: client (v%s) vs server (v%s)", clientSemver, serverSemver)
+	}
+
+	return nil
+}

cmd/gadgetctl/main.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/inspektor-gadget/inspektor-gadget/cmd/common"
 	commonutils "github.com/inspektor-gadget/inspektor-gadget/cmd/common/utils"
+	"github.com/inspektor-gadget/inspektor-gadget/internal/deployinfo"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/all-gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/environment"
 	grpcruntime "github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/grpc"
@@ -76,6 +77,12 @@ func main() {
 			os.Exit(1)
 		}
 		runtime.InitDeployInfo()
+		info, err := deployinfo.Load()
+		if err != nil {
+			log.Warnf("Failed to load deploy info: %s", err)
+		} else if err := commonutils.CheckServerVersionSkew(info.ServerVersion); err != nil {
+			log.Warnf(err.Error())
+		}
 	}
 
 	hiddenColumnTags := []string{"kubernetes"}

cmd/kubectl-gadget/deploy.go
@@ -52,9 +52,9 @@ import (
 	watchtools "k8s.io/client-go/tools/watch"
 	"sigs.k8s.io/yaml"
 
-	"github.com/inspektor-gadget/inspektor-gadget/cmd/common"
 	commonutils "github.com/inspektor-gadget/inspektor-gadget/cmd/common/utils"
 	"github.com/inspektor-gadget/inspektor-gadget/cmd/kubectl-gadget/utils"
+	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/k8sutil"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/resources"
 	grpcruntime "github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/grpc"
@@ -478,7 +478,7 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 				case "GADGET_IMAGE":
 					gadgetContainer.Env[i].Value = image
 				case "INSPEKTOR_GADGET_VERSION":
-					gadgetContainer.Env[i].Value = common.Version()
+					gadgetContainer.Env[i].Value = version.Version().String()
 				case "INSPEKTOR_GADGET_OPTION_HOOK_MODE":
 					gadgetContainer.Env[i].Value = hookMode
 				case "INSPEKTOR_GADGET_OPTION_FALLBACK_POD_INFORMER":

cmd/kubectl-gadget/main.go
@@ -21,6 +21,8 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	"github.com/inspektor-gadget/inspektor-gadget/internal/deployinfo"
+
 	// Import this early to set the enrivonment variable before any other package is imported
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/environment/k8s"
 	paramsPkg "github.com/inspektor-gadget/inspektor-gadget/pkg/params"
@@ -126,6 +128,12 @@ func main() {
 
 	if !skipInfo {
 		grpcRuntime.InitDeployInfo()
+		info, err := deployinfo.Load()
+		if err != nil {
+			log.Warnf("Failed to load deploy info: %s", err)
+		} else if err := commonutils.CheckServerVersionSkew(info.ServerVersion); err != nil {
+			log.Warnf(err.Error())
+		}
 	}
 
 	namespace, _ := utils.GetNamespace()

commit e18f00b87ec8cf68b1fab4866b7216431bbdc748

Makefile: Fix how ig-debug version is set.
Fixes: 7a25d47ec46e ("add version skew warning")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

Makefile
@@ -116,7 +116,7 @@ ig: ig-$(GOHOSTOS)-$(GOHOSTARCH)
 # See: https://pkg.go.dev/cmd/compile
 debug-ig:
 	CGO_ENABLED=0 go build \
-		-ldflags "-X github.com/inspektor-gadget/inspektor-gadget/cmd/common.version=${VERSION} \
+		-ldflags "-X github.com/inspektor-gadget/inspektor-gadget/internal/version.version=${VERSION} \
 		-X github.com/inspektor-gadget/inspektor-gadget/cmd/common/image.builderImage=${EBPF_BUILDER} \
 		-extldflags '-static'" \
 		-gcflags='all=-N -l' \