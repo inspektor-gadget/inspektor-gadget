inspektor-gadget/inspektor-gadget

commit 7e62c1d95e6bd01b005a21af62c448286766e4e4

pkg/gadgets/run/types: Print warning for multiple net/mnt ns fields
During building, let users know that if they have multiple
gadget_mntns_id or gadget_netns_id fields, we can't guarantee which one
will be used during enrichment (if local/kube manager operators are
enabled).

Signed-off-by: Jose Blanquicet <josebl@microsoft.com>

pkg/gadgets/run/types/metadata.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/btfhelpers"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource/compat"
 	metadatav1 "github.com/inspektor-gadget/inspektor-gadget/pkg/metadata/v1"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 )
@@ -250,9 +251,28 @@ func validateStructs(m *metadatav1.GadgetMetadata, spec *ebpf.CollectionSpec) er
 			mapStructFields[f.Name] = f
 		}
 
+		mntnsFields := 0
+		netnsFields := 0
+
 		btfStructFields := make(map[string]btf.Member, len(btfStruct.Members))
 		for _, m := range btfStruct.Members {
 			btfStructFields[m.Name] = m
+
+			if m.Type.TypeName() != "" && strings.Contains(compat.MntNsIdType, m.Type.TypeName()) {
+				mntnsFields++
+			}
+			if m.Type.TypeName() != "" && strings.Contains(compat.NetNsIdType, m.Type.TypeName()) {
+				netnsFields++
+			}
+		}
+
+		if mntnsFields > 1 {
+			log.Warnf("Using multiple fields of %q may cause unpredictable behavior during enrichment",
+				compat.MntNsIdType)
+		}
+		if netnsFields > 1 {
+			log.Warnf("Using multiple fields of %q may cause unpredictable behavior during enrichment",
+				compat.NetNsIdType)
 		}
 
 		for fieldName := range mapStructFields {

commit 209444167107b5b171cff3cc90b3c2b99c075df0

pkg/gadgets/run/types: Test string equality rather than contain to detect several occurences of namespace type.
Before this commit, we used strings.Contains to test we do not have several
namespace type in a given structure.
Sadly, this leaded to the following warnings being printed for nothing:
WARN[0002] Using multiple fields of "type:gadget_netns_id" may cause unpredictable behavior during enrichment
It was indeed the case for trace_tcpretrans which has the following as event
structure:
struct event {
...
	gadget_netns_id netns;
	enum type type_raw;
...
	gadget_mntns_id mntns_id;
...
}
So, both "type:gadget_netns_id" and "type:gadget_mntns_id" would be matched by
the enum "type", hence the warning being printed.

We fix this behavior by removing the "type:" prefix for namespace types and
using strict string comparison.

Fixes: 7e62c1d95e6b ("pkg/gadgets/run/types: Print warning for multiple net/mnt ns fields")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/run/types/metadata.go
@@ -254,14 +254,17 @@ func validateStructs(m *metadatav1.GadgetMetadata, spec *ebpf.CollectionSpec) er
 		mntnsFields := 0
 		netnsFields := 0
 
+		mntNsIdType := strings.TrimPrefix(compat.MntNsIdType, "type:")
+		netNsIdType := strings.TrimPrefix(compat.NetNsIdType, "type:")
+
 		btfStructFields := make(map[string]btf.Member, len(btfStruct.Members))
 		for _, m := range btfStruct.Members {
 			btfStructFields[m.Name] = m
 
-			if m.Type.TypeName() != "" && strings.Contains(compat.MntNsIdType, m.Type.TypeName()) {
+			if mntNsIdType == m.Type.TypeName() {
 				mntnsFields++
 			}
-			if m.Type.TypeName() != "" && strings.Contains(compat.NetNsIdType, m.Type.TypeName()) {
+			if netNsIdType == m.Type.TypeName() {
 				netnsFields++
 			}
 		}