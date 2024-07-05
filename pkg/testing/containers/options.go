inspektor-gadget/inspektor-gadget

commit 3352bb98df1d71780ab82076bf0c2c0f2611e683

test/trace_oomkill: Use new testing framework
Signed-off-by: Burak Ok <burakok@microsoft.com>

gadgets/trace_oomkill/test/trace_oomkill_test.go
@@ -1,0 +1,129 @@
+// Copyright 2019-2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	gadgettesting "github.com/inspektor-gadget/inspektor-gadget/gadgets/testing"
+	igtesting "github.com/inspektor-gadget/inspektor-gadget/pkg/testing"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/containers"
+	igrunner "github.com/inspektor-gadget/inspektor-gadget/pkg/testing/ig"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/match"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/utils"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+)
+
+type traceOomKillEvent struct {
+	eventtypes.CommonData
+
+	Fpid      uint32 `json:"fpid"`
+	Fuid      uint32 `json:"fuid"`
+	Fgid      uint32 `json:"fgid"`
+	Tpid      uint32 `json:"tpid"`
+	Pages     uint64 `json:"pages"`
+	MountNsID uint64 `json:"mntns_id"`
+	Timestamp string `json:"timestamp"`
+	Fcomm     string `json:"fcomm"`
+	Tcomm     string `json:"tcomm"`
+}
+
+func TestTraceOomKill(t *testing.T) {
+	gadgettesting.RequireEnvironmentVariables(t)
+	utils.InitTest(t)
+
+	if utils.CurrentTestComponent != utils.KubectlGadgetTestComponent && utils.CurrentTestComponent != utils.IgK8sTestComponent {
+		// We have no general way to enforce memory limits for all container runtimes
+		t.Skip("Test only runs for kubectl-gadget and ig-k8s")
+	}
+
+	containerFactory := &containers.K8sManager{}
+	containerName := "test-trace-oomkill"
+	containerImage := "docker.io/library/busybox:latest"
+
+	var ns string
+	containerOpts := []containers.ContainerOption{containers.WithContainerImage(containerImage)}
+
+	if utils.CurrentTestComponent == utils.KubectlGadgetTestComponent {
+		ns = utils.GenerateTestNamespaceName(t, "test-trace-sni")
+		containerOpts = append(containerOpts, containers.WithContainerNamespace(ns))
+	}
+
+	containerOpts = append(containerOpts, containers.WithLimits(map[string]string{"memory": "128Mi"}), containers.WithStartAndStop())
+
+	testContainer := containerFactory.NewContainer(
+		containerName,
+		"while true; do tail /dev/zero; done",
+		containerOpts...,
+	)
+
+	var runnerOpts []igrunner.Option
+	var testingOpts []igtesting.Option
+	commonDataOpts := []utils.CommonDataOption{utils.WithContainerImageName(containerImage), utils.WithContainerID(testContainer.ID())}
+
+	switch utils.CurrentTestComponent {
+	case utils.IgK8sTestComponent:
+		runnerOpts = append(runnerOpts, igrunner.WithFlags(fmt.Sprintf("-r=%s", utils.Runtime)), igrunner.WithStartAndStop())
+	case utils.KubectlGadgetTestComponent:
+		runnerOpts = append(runnerOpts, igrunner.WithFlags(fmt.Sprintf("-n=%s", ns)), igrunner.WithStartAndStop())
+		testingOpts = append(testingOpts, igtesting.WithCbBeforeCleanup(utils.PrintLogsFn(ns)))
+		commonDataOpts = append(commonDataOpts, utils.WithK8sNamespace(ns))
+	}
+
+	runnerOpts = append(runnerOpts, igrunner.WithValidateOutput(
+		func(t *testing.T, output string) {
+			expectedEntry := &traceOomKillEvent{
+				CommonData: utils.BuildCommonData(containerName, commonDataOpts...),
+				Tcomm:      "tail",
+
+				// Check the existence of the following fields
+				Timestamp: utils.NormalizedStr,
+				MountNsID: utils.NormalizedInt,
+				Fpid:      utils.NormalizedInt,
+				Fuid:      utils.NormalizedInt,
+				Fgid:      utils.NormalizedInt,
+				Tpid:      utils.NormalizedInt,
+				Pages:     utils.NormalizedInt,
+				Fcomm:     utils.NormalizedStr,
+			}
+
+			normalize := func(e *traceOomKillEvent) {
+				utils.NormalizeCommonData(&e.CommonData)
+				utils.NormalizeString(&e.Timestamp)
+				utils.NormalizeInt(&e.MountNsID)
+				utils.NormalizeInt(&e.Fpid)
+				utils.NormalizeInt(&e.Fuid)
+				utils.NormalizeInt(&e.Fgid)
+				utils.NormalizeInt(&e.Tpid)
+				utils.NormalizeInt(&e.Pages)
+				utils.NormalizeString(&e.Fcomm)
+			}
+
+			match.MatchEntries(t, match.JSONMultiObjectMode, output, normalize, expectedEntry)
+		},
+	))
+
+	traceOomkillCmd := igrunner.New("trace_oomkill", runnerOpts...)
+
+	testSteps := []igtesting.TestStep{
+		traceOomkillCmd,
+		utils.Sleep(10 * time.Second),
+		testContainer,
+	}
+
+	igtesting.RunTestSteps(testSteps, t, testingOpts...)
+}

integration/k8s/run_trace_oomkill_test.go
@@ -1,135 +1,0 @@
-// Copyright 2023 The Inspektor Gadget authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package main
-
-import (
-	"fmt"
-	"testing"
-
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/run/types"
-
-	. "github.com/inspektor-gadget/inspektor-gadget/integration"
-)
-
-func runTraceOOMKill(t *testing.T, ns string, cmd string) {
-	traceOOMKillCmd := &Command{
-		Name:         "StartRunTraceOOMKillGadget",
-		Cmd:          cmd,
-		StartAndStop: true,
-		ValidateOutput: func(t *testing.T, output string) {
-			expectedBaseJsonObj := RunEventToObj(t, &types.Event{
-				CommonData: BuildCommonDataK8s(ns, WithContainerImageName("docker.io/library/busybox:latest", isDockerRuntime)),
-			})
-
-			expectedTraceOOMKillJsonObj := map[string]interface{}{
-				"fpid":          0,
-				"fuid":          0,
-				"fgid":          0,
-				"tpid":          0,
-				"pages":         0,
-				"mntns_id":      0,
-				"timestamp":     "",
-				"timestamp_raw": 0,
-				"fcomm":         "",
-				"tcomm":         "tail",
-			}
-
-			expectedJsonObj := MergeJsonObjs(t, expectedBaseJsonObj, expectedTraceOOMKillJsonObj)
-
-			normalize := func(m map[string]interface{}) {
-				SetEventK8sNode(m, "")
-
-				// TODO: Verify container runtime and container name
-				SetEventRuntimeName(m, "")
-				SetEventRuntimeContainerID(m, "")
-				SetEventRuntimeContainerName(m, "")
-
-				m["fcomm"] = ""
-				m["fpid"] = uint32(0)
-				m["fuid"] = uint32(0)
-				m["fgid"] = uint32(0)
-				m["tpid"] = uint32(0)
-				m["pages"] = uint32(0)
-				m["mntns_id"] = 0
-				m["timestamp"] = ""
-				m["timestamp_raw"] = 0
-			}
-
-			ExpectEntriesToMatchObj(t, output, normalize, expectedJsonObj)
-		},
-	}
-
-	limitPodYaml := fmt.Sprintf(`
-apiVersion: v1
-kind: Pod
-metadata:
-  name: test-pod
-  namespace: %s
-spec:
-  restartPolicy: Never
-  terminationGracePeriodSeconds: 0
-  containers:
-  - name: test-pod
-    image: busybox
-    resources:
-      limits:
-        memory: "128Mi"
-    command: ["/bin/sh", "-c"]
-    args:
-    - while true; do tail /dev/zero; done
-`, ns)
-
-	commands := []TestStep{
-		traceOOMKillCmd,
-		// Wait to ensure ig or kubectl-gadget has downloaded the image and started it
-		// before we start the test pod, which only gives us 1 event to trace (oneshot)
-		SleepForSecondsCommand(10),
-		&Command{
-			Name:           "RunOomkillTestPod",
-			Cmd:            fmt.Sprintf("echo '%s' | kubectl apply -f -", limitPodYaml),
-			ExpectedRegexp: "pod/test-pod created",
-		},
-		WaitUntilTestPodReadyOrOOMKilledCommand(ns),
-	}
-
-	RunTestSteps(commands, t, WithCbBeforeCleanup(PrintLogsFn(ns)))
-}
-
-func TestRunTraceOOMKill(t *testing.T) {
-	if DefaultTestComponent != InspektorGadgetTestComponent {
-		t.Skip("Skip running test with test component different than kubectl-gadget")
-	}
-
-	ns := GenerateTestNamespaceName("test-run-trace-oomkill")
-
-	t.Parallel()
-
-	commandsPreTest := []TestStep{
-		CreateTestNamespaceCommand(ns),
-	}
-
-	RunTestSteps(commandsPreTest, t)
-
-	t.Cleanup(func() {
-		commands := []TestStep{
-			DeleteTestNamespaceCommand(ns),
-		}
-		RunTestSteps(commands, t, WithCbBeforeCleanup(PrintLogsFn(ns)))
-	})
-
-	cmd := fmt.Sprintf("$KUBECTL_GADGET run %s/trace_oomkill:%s --verify-image=%t -n %s -o json", *gadgetRepository, *gadgetTag, *gadgetVerifyImage, ns)
-
-	runTraceOOMKill(t, ns, cmd)
-}

commit fcc0447e23e0f2aa2e9482b864910511f00cdcf2

gadgets/trace_oomkill/test: Use own namespace and ignore ContainerID
Signed-off-by: Burak Ok <burakok@microsoft.com>
Fixes: 3352bb98df1d71780ab82076bf0c2c0f2611e683
Signed-off-by: Burak Ok <burakok@microsoft.com>

pkg/container-utils/testutils/k8s.go
@@ -58,11 +58,16 @@ func (c *K8sContainer) Run(t *testing.T) {
 func (c *K8sContainer) Start(t *testing.T) {
 	// TODO: handle pid, portBindings.
 
+	waitCommand := waitUntilPodReadyCommand(t, c.options.namespace, c.name)
+	if c.options.waitOrOomKilled {
+		waitCommand = waitUntilPodReadyOrOOMKilledCommand(t, c.options.namespace, c.name)
+	}
+
 	igtesting.RunTestSteps([]igtesting.TestStep{
 		createTestNamespaceCommand(c.options.namespace),
 		podCommand(t, c.name, c.options.image, c.options.namespace, `["/bin/sh", "-c"]`, c.cmd, c.options.limits),
 		sleepForSecondsCommand(2),
-		waitUntilPodReadyCommand(t, c.options.namespace, c.name),
+		waitCommand,
 	}, t)
 
 	c.id = getContainerID(t, c.name, c.options.namespace)
@@ -187,6 +192,16 @@ func waitUntilPodReadyCommand(t *testing.T, namespace string, podname string) *c
 	return &command.Command{
 		Name:           "WaitForTestPod",
 		Cmd:            exec.Command("/bin/sh", "-c", fmt.Sprintf("kubectl wait pod --for condition=ready -n %s %s", namespace, podname)),
+		ValidateOutput: match.EqualString(t, fmt.Sprintf("pod/%s condition met\n", podname)),
+	}
+}
+
+// waitUntilPodReadyOrOOMKilledCommand returns a Command which waits until pod with the specified name in
+// the given as parameter namespace is ready or was oomkilled.
+func waitUntilPodReadyOrOOMKilledCommand(t *testing.T, namespace string, podname string) *command.Command {
+	return &command.Command{
+		Name:           "WaitForTestPod",
+		Cmd:            exec.Command("/bin/sh", "-c", fmt.Sprintf("kubectl wait pod --for condition=ready -n %s %s || kubectl wait pod --for jsonpath='{.status.containerStatuses[0].state.terminated.reason}'=OOMKilled -n %s %s", namespace, podname, namespace, podname)),
 		ValidateOutput: match.EqualString(t, fmt.Sprintf("pod/%s condition met\n", podname)),
 	}
 }

pkg/container-utils/testutils/options.go
@@ -36,6 +36,7 @@ type containerOptions struct {
 	seccompProfile   string
 	namespace        string
 	wait             bool
+	waitOrOomKilled  bool
 	logs             bool
 	removal          bool
 	portBindings     nat.PortMap
@@ -104,6 +105,12 @@ func WithNamespace(namespace string) Option {
 func WithoutWait() Option {
 	return func(opts *containerOptions) {
 		opts.wait = false
+	}
+}
+
+func WithWaitOrOomKilled() Option {
+	return func(opts *containerOptions) {
+		opts.waitOrOomKilled = true
 	}
 }
 

gadgets/trace_oomkill/test/trace_oomkill_test.go
@@ -59,11 +59,11 @@ func TestTraceOomKill(t *testing.T) {
 	containerOpts := []containers.ContainerOption{containers.WithContainerImage(containerImage)}
 
 	if utils.CurrentTestComponent == utils.KubectlGadgetTestComponent {
-		ns = utils.GenerateTestNamespaceName(t, "test-trace-sni")
+		ns = utils.GenerateTestNamespaceName(t, "test-trace-oomkill")
 		containerOpts = append(containerOpts, containers.WithContainerNamespace(ns))
 	}
 
-	containerOpts = append(containerOpts, containers.WithLimits(map[string]string{"memory": "128Mi"}), containers.WithStartAndStop())
+	containerOpts = append(containerOpts, containers.WithLimits(map[string]string{"memory": "128Mi"}), containers.WithStartAndStop(), containers.WithWaitOrOomKilled())
 
 	testContainer := containerFactory.NewContainer(
 		containerName,
@@ -73,7 +73,7 @@ func TestTraceOomKill(t *testing.T) {
 
 	var runnerOpts []igrunner.Option
 	var testingOpts []igtesting.Option
-	commonDataOpts := []utils.CommonDataOption{utils.WithContainerImageName(containerImage), utils.WithContainerID(testContainer.ID())}
+	commonDataOpts := []utils.CommonDataOption{utils.WithContainerImageName(containerImage)}
 
 	switch utils.CurrentTestComponent {
 	case utils.IgK8sTestComponent:
@@ -100,17 +100,22 @@ func TestTraceOomKill(t *testing.T) {
 				Pages:     utils.NormalizedInt,
 				Fcomm:     utils.NormalizedStr,
 			}
+			expectedEntry.Runtime.ContainerID = utils.NormalizedStr
 
 			normalize := func(e *traceOomKillEvent) {
 				utils.NormalizeCommonData(&e.CommonData)
+				utils.NormalizeString(&e.Runtime.ContainerID)
 				utils.NormalizeString(&e.Timestamp)
 				utils.NormalizeInt(&e.MountNsID)
 				utils.NormalizeInt(&e.Fpid)
-				utils.NormalizeInt(&e.Fuid)
-				utils.NormalizeInt(&e.Fgid)
 				utils.NormalizeInt(&e.Tpid)
 				utils.NormalizeInt(&e.Pages)
 				utils.NormalizeString(&e.Fcomm)
+
+				// NormalizeInt only normalizes if the value is not 0
+				// Fuid and Fgid might be 0, so do the normalization manually
+				e.Fuid = utils.NormalizedInt
+				e.Fgid = utils.NormalizedInt
 			}
 
 			match.MatchEntries(t, match.JSONMultiObjectMode, output, normalize, expectedEntry)

pkg/testing/containers/options.go
@@ -77,3 +77,9 @@ func WithStartAndStop() ContainerOption {
 		opts.startAndStop = true
 	}
 }
+
+func WithWaitOrOomKilled() ContainerOption {
+	return func(opts *cOptions) {
+		opts.options = append(opts.options, testutils.WithWaitOrOomKilled())
+	}
+}