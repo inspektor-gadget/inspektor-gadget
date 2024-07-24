inspektor-gadget/inspektor-gadget

commit fa251c90c4b4f58495eb4ab371577d118db98571

tree-wide: Pin base images on Dockerfiles
Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

Dockerfiles/kubectl-gadget.Dockerfile
@@ -6,8 +6,8 @@
 # image is valid, even scratch. Alpine is used by default as a tradeoff
 # between size and tools available in the image.
 
-ARG BUILDER_IMAGE=golang:1.22-bullseye
-ARG BASE_IMAGE=alpine:3.18
+ARG BUILDER_IMAGE=golang:1.22-bullseye@sha256:11ff2c5de8044d8d749cc684f6f3cdfbc9b2755960a62d4948b3483dc090e7aa
+ARG BASE_IMAGE=alpine:3.18@ sha256:1875c923b73448b558132e7d4a44b815d078779ed7a73f76209c6372de95ea8d
 
 FROM --platform=${BUILDPLATFORM} ${BUILDER_IMAGE} AS builder
 

Dockerfiles/ebpf-builder.Dockerfile
@@ -6,7 +6,7 @@ ARG TINYGO_VERSION=0.31.2
 # Args need to be redefined on each stage
 # https://docs.docker.com/engine/reference/builder/#understand-how-arg-and-from-interact
 
-FROM golang:1.22 AS builder
+FROM golang:1.22@sha256:829eff99a4b2abffe68f6a3847337bf6455d69d17e49ec1a97dac78834754bd6 AS builder
 ARG BPFTOOL_VERSION
 ARG LIBBPF_VERSION
 
@@ -23,7 +23,7 @@ RUN \
 	tar -C /usr/local/bin -xzf bpftool-${BPFTOOL_VERSION}-${ARCH}.tar.gz && \
 	chmod +x /usr/local/bin/bpftool
 
-FROM golang:1.22
+FROM golang:1.22@sha256:829eff99a4b2abffe68f6a3847337bf6455d69d17e49ec1a97dac78834754bd6
 ARG CLANG_LLVM_VERSION
 ARG TINYGO_VERSION
 # libc-dev is needed for various headers, among others

Dockerfiles/gadget.Dockerfile
@@ -1,7 +1,7 @@
 # Dockerfile for Inspektor Gadget.
 
-ARG BUILDER_IMAGE=golang:1.22-bullseye
-ARG BASE_IMAGE=gcr.io/distroless/static-debian12
+ARG BUILDER_IMAGE=golang:1.22-bullseye@sha256:11ff2c5de8044d8d749cc684f6f3cdfbc9b2755960a62d4948b3483dc090e7aa
+ARG BASE_IMAGE=gcr.io/distroless/static-debian12@sha256:ce46866b3a5170db3b49364900fb3168dc0833dfb46c26da5c77f22abb01d8c3
 
 # Prepare and build gadget artifacts in a container
 FROM --platform=${BUILDPLATFORM} ${BUILDER_IMAGE} AS builder

Dockerfiles/ig-tests.Dockerfile
@@ -1,5 +1,5 @@
-ARG BUILDER_IMAGE=golang:1.22-bullseye
-ARG BASE_IMAGE=gcr.io/distroless/static-debian11
+ARG BUILDER_IMAGE=golang:1.22-bullseye@sha256:11ff2c5de8044d8d749cc684f6f3cdfbc9b2755960a62d4948b3483dc090e7aa
+ARG BASE_IMAGE=gcr.io/distroless/static-debian11:latest@sha256:1dbe426d60caed5d19597532a2d74c8056cd7b1674042b88f7328690b5ead8ed
 
 FROM --platform=${BUILDPLATFORM} ${BUILDER_IMAGE} AS builder
 

Dockerfiles/ig.Dockerfile
@@ -1,5 +1,5 @@
-ARG BUILDER_IMAGE=golang:1.22
-ARG BASE_IMAGE=gcr.io/distroless/static-debian11
+ARG BUILDER_IMAGE=golang:1.22@sha256:829eff99a4b2abffe68f6a3847337bf6455d69d17e49ec1a97dac78834754bd6
+ARG BASE_IMAGE=gcr.io/distroless/static-debian11:latest@sha256:1dbe426d60caed5d19597532a2d74c8056cd7b1674042b88f7328690b5ead8ed
 
 FROM --platform=${BUILDPLATFORM} ${BUILDER_IMAGE} AS builder
 

Dockerfiles/linter.Dockerfile
@@ -1,5 +1,5 @@
-ARG VERSION
-FROM golangci/golangci-lint:${VERSION}
+ARG IMAGE
+FROM ${IMAGE}
 
 # The timeout specified below is used by 'make lint'. Please keep in sync with
 # the timeout specified in .golangci.yml used by the CI.

Makefile
@@ -25,7 +25,7 @@ BPFTOOL ?= bpftool
 ARCH ?= $(shell uname -m | sed 's/x86_64/x86/' | sed 's/aarch64/arm64/' | sed 's/ppc64le/powerpc/' | sed 's/mips.*/mips/')
 
 # This version number must be kept in sync with CI workflow lint one.
-LINTER_VERSION ?= v1.59.0
+LINTER_IMAGE ?= golangci/golangci-lint:v1.59.0@sha256:8ad7dc3d98d77dec753f07408c7683ab854752a3eb8dc6a5e5f0728f9a89ae2c
 
 EBPF_BUILDER ?= ghcr.io/inspektor-gadget/ebpf-builder:latest
 
@@ -308,7 +308,7 @@ website-local-update:
 	cp -r docs ../website/external-docs/inspektor-gadget.git_mainlatest/
 
 lint:
-	docker build -t linter -f Dockerfiles/linter.Dockerfile --build-arg VERSION=$(LINTER_VERSION) Dockerfiles
+	docker build -t linter -f Dockerfiles/linter.Dockerfile --build-arg IMAGE=$(LINTER_IMAGE) Dockerfiles
 # XDG_CACHE_HOME is necessary to avoid this type of errors:
 # ERRO Running error: context loading failed: failed to load packages: failed to load with go/packages: err: exit status 1: stderr: failed to initialize build cache at /.cache/go-build: mkdir /.cache: permission denied
 # Process 15167 has exited with status 3

examples/builtin-gadgets/withfilter/trace/network/Dockerfile
@@ -1,4 +1,4 @@
-FROM --platform=$BUILDPLATFORM golang:1.22-bullseye AS builder
+FROM --platform=$BUILDPLATFORM golang:1.22-bullseye@sha256:11ff2c5de8044d8d749cc684f6f3cdfbc9b2755960a62d4948b3483dc090e7aa AS builder
 ENV GO111MODULE=on
 ENV CGO_ENABLED=0
 WORKDIR /src
@@ -9,7 +9,7 @@ RUN --mount=target=. \
     GOOS=$TARGETOS GOARCH=$TARGETARCH \
     go build -o /out/container-network-tracer examples/builtin-gadgets/withfilter/trace/network/network.go
 
-FROM gcr.io/distroless/static-debian11:latest
+FROM gcr.io/distroless/static-debian11:latest@sha256:1dbe426d60caed5d19597532a2d74c8056cd7b1674042b88f7328690b5ead8ed
 COPY --from=builder /out/container-network-tracer /usr/bin/container-network-tracer
 WORKDIR /root
 ENTRYPOINT ["container-network-tracer"]

commit f492609245575ad15936dc74823481589ed571f1

Dockerfiles: Fix typo for kubectl-gadget.Dockerfile hash.
Fixes: fa251c90c4b4 ("tree-wide: Pin base images on Dockerfiles")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

Dockerfiles/kubectl-gadget.Dockerfile
@@ -7,7 +7,7 @@
 # between size and tools available in the image.
 
 ARG BUILDER_IMAGE=golang:1.22-bullseye@sha256:11ff2c5de8044d8d749cc684f6f3cdfbc9b2755960a62d4948b3483dc090e7aa
-ARG BASE_IMAGE=alpine:3.18@ sha256:1875c923b73448b558132e7d4a44b815d078779ed7a73f76209c6372de95ea8d
+ARG BASE_IMAGE=alpine:3.18@sha256:1875c923b73448b558132e7d4a44b815d078779ed7a73f76209c6372de95ea8d
 
 FROM --platform=${BUILDPLATFORM} ${BUILDER_IMAGE} AS builder
 