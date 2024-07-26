inspektor-gadget/inspektor-gadget

commit c2f6a5e1aee5142a2f67817ca6e54c0c071ec2db

treewide: Fix usage of empty public keys.
Fixes: c71d05d54f04 ("treewide: Remove unused verify-gadgets flag and adapt CI jobs to dependabot.")
Fixes: f51ff0eb2ec8 ("treewide: Allow to have several public keys.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

.github/actions/run-integration-tests/action.yml
@@ -52,7 +52,7 @@ runs:
         # this case as the bot does not have access to the secrets.
         # So let's deactivate the verifying.
         if [ "${{ inputs.gadget_verify_image }}" == 'false' ]; then
-          EXTRA_FLAGS="${EXTRA_FLAGS} --gadgets-public-keys=''"
+          EXTRA_FLAGS="${EXTRA_FLAGS} --gadgets-public-keys="
         fi
 
         ./kubectl-gadget deploy $EXTRA_FLAGS --debug --experimental --image=${{ inputs.container_repo }}:${{ inputs.image_tag }}

.github/workflows/inspektor-gadget.yml
@@ -1439,7 +1439,7 @@ jobs:
           # this case as the bot does not have access to the secrets.
           # So let's deactivate the verifying.
           if [ ${{ needs.check-secrets.outputs.cosign }} == 'false' ]; then
-            public_keys_flag='--public-keys=""'
+            public_keys_flag='--public-keys='
           fi
 
           make \
@@ -1515,7 +1515,7 @@ jobs:
         # this case as the bot does not have access to the secrets.
         # So let's deactivate the verifying.
         if [ "${{ needs.check-secrets.outputs.cosign }}" == 'false' ]; then
-          gadgets_public_keys_flag='--gadgets-public-keys=""'
+          gadgets_public_keys_flag='--gadgets-public-keys='
         fi
 
         tar zxvf /home/runner/work/inspektor-gadget/inspektor-gadget/kubectl-gadget-linux-amd64.tar.gz

Makefile
@@ -345,7 +345,7 @@ minikube-deploy: minikube-start gadget-container kubectl-gadget
 	$(MINIKUBE) image ls --format=table | grep "$(CONTAINER_REPO)\s*|\s*$(IMAGE_TAG)" || \
 		(echo "Image $(CONTAINER_REPO)\s*|\s*$(IMAGE_TAG) was not correctly loaded into Minikube" && false)
 	@echo
-	./kubectl-gadget deploy $(if $(findstring false,$(VERIFY_GADGETS)),--gadgets-public-keys='') --liveness-probe=$(LIVENESS_PROBE) \
+	./kubectl-gadget deploy $(if $(findstring false,$(VERIFY_GADGETS)),--gadgets-public-keys=) --liveness-probe=$(LIVENESS_PROBE) \
 		--image-pull-policy=Never
 	kubectl rollout status daemonset -n gadget gadget --timeout 30s
 	@echo "Image used by the gadget pod:"

commit 67843a41bb07f76a24f72719de4543b3d88794db

treewide: Add back --verify-gadgets.
This is easier to have two parameters:
1. --verify-gadgets: One the control the verification process.
2. --gadgets-public-keys: The other to specify the keys used to verify the
gadgets.

Fixes: c2f6a5e1aee5 ("treewide: Fix usage of empty public keys. ")
Fixes: c71d05d54f04 ("treewide: Remove unused verify-gadgets flag and adapt CI jobs to dependabot.")
Fixes: f51ff0eb2ec8 ("treewide: Allow to have several public keys.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

.github/actions/run-integration-tests/action.yml
@@ -47,15 +47,7 @@ runs:
           # Inspektor-Gadget container image is loaded into the cluster using "minikube image load" instead of pushing the container image to a registry.
           EXTRA_FLAGS='--image-pull-policy=Never'
         fi
-
-        # This is needed by dependabot, as the gadgets would not be signed in
-        # this case as the bot does not have access to the secrets.
-        # So let's deactivate the verifying.
-        if [ "${{ inputs.gadget_verify_image }}" == 'false' ]; then
-          EXTRA_FLAGS="${EXTRA_FLAGS} --gadgets-public-keys="
-        fi
-
-        ./kubectl-gadget deploy $EXTRA_FLAGS --debug --experimental --image=${{ inputs.container_repo }}:${{ inputs.image_tag }}
+        ./kubectl-gadget deploy --verify-gadgets=${{ inputs.gadget_verify_image }} $EXTRA_FLAGS --debug --experimental --image=${{ inputs.container_repo }}:${{ inputs.image_tag }}
     - name: Integration tests
       id: integration-tests
       shell: bash

.github/workflows/inspektor-gadget.yml
@@ -1434,18 +1434,10 @@ jobs:
       shell: bash
       run: |
           set -o pipefail
-
-          # This is needed by dependabot, as the gadgets would not be signed in
-          # this case as the bot does not have access to the secrets.
-          # So let's deactivate the verifying.
-          if [ ${{ needs.check-secrets.outputs.cosign }} == 'false' ]; then
-            public_keys_flag='--public-keys='
-          fi
-
           make \
           GADGET_REPOSITORY=${{ steps.set-repo-determine-image-tag.outputs.gadget-repository }} \
           GADGET_TAG=${{ steps.set-repo-determine-image-tag.outputs.gadget-tag }} \
-          IG_FLAGS="${public_keys_flag}" \
+          IG_FLAGS="--verify-image=${{ needs.check-secrets.outputs.cosign }}" \
           IG_RUNTIME=${{ matrix.runtime }} \
           -C gadgets/ test-local -o build |& tee gadgets-tests.log & wait $!
     - name: Prepare and publish test reports
@@ -1510,19 +1502,8 @@ jobs:
       shell: bash
       run: |
         set -o pipefail
-
-        # This is needed by dependabot, as the gadgets would not be signed in
-        # this case as the bot does not have access to the secrets.
-        # So let's deactivate the verifying.
-        if [ "${{ needs.check-secrets.outputs.cosign }}" == 'false' ]; then
-          gadgets_public_keys_flag='--gadgets-public-keys=""'
-          echo "Public key verification deactivated"
-        fi
-
-        echo "gadgets_public_keys_flag: ${gadgets_public_keys_flag}"
-
         tar zxvf /home/runner/work/inspektor-gadget/inspektor-gadget/kubectl-gadget-linux-amd64.tar.gz
-        ./kubectl-gadget deploy ${gadgets_public_keys_flag} --image-pull-policy=Never --debug --experimental --image=${{ steps.set-repo-determine-image-tag.outputs.container-repo }}:${{ steps.set-repo-determine-image-tag.outputs.image-tag }}
+        ./kubectl-gadget deploy --verify-gadgets=${{ needs.check-secrets.outputs.cosign }} --image-pull-policy=Never --debug --experimental --image=${{ steps.set-repo-determine-image-tag.outputs.container-repo }}:${{ steps.set-repo-determine-image-tag.outputs.image-tag }}
         make \
         GADGET_REPOSITORY=${{ steps.set-repo-determine-image-tag.outputs.gadget-repository }} \
         GADGET_TAG=${{ steps.set-repo-determine-image-tag.outputs.gadget-tag }} \

Makefile
@@ -345,7 +345,7 @@ minikube-deploy: minikube-start gadget-container kubectl-gadget
 	$(MINIKUBE) image ls --format=table | grep "$(CONTAINER_REPO)\s*|\s*$(IMAGE_TAG)" || \
 		(echo "Image $(CONTAINER_REPO)\s*|\s*$(IMAGE_TAG) was not correctly loaded into Minikube" && false)
 	@echo
-	./kubectl-gadget deploy $(if $(findstring false,$(VERIFY_GADGETS)),--gadgets-public-keys=) --liveness-probe=$(LIVENESS_PROBE) \
+	./kubectl-gadget deploy --verify-gadgets=$(VERIFY_GADGETS) --liveness-probe=$(LIVENESS_PROBE) \
 		--image-pull-policy=Never
 	kubectl rollout status daemonset -n gadget gadget --timeout 30s
 	@echo "Image used by the gadget pod:"

charts/gadget/templates/configmap.yaml
@@ -19,4 +19,5 @@ data:
       podman-socketpath: {{ .Values.config.podmanSocketPath }}
       operator:
         oci:
+          verify-image: {{ .Values.config.verifyGadgets }}
           public-keys:{{ toYaml $.Values.config.gadgetsPublicKeys | nindent 12 }}

cmd/kubectl-gadget/deploy.go
@@ -106,6 +106,7 @@ var (
 	verifyImage         bool
 	publicKey           string
 	strLevels           []string
+	verifyGadgets       bool
 	gadgetsPublicKeys   string
 )
 
@@ -233,12 +234,15 @@ func init() {
 	deployCmd.PersistentFlags().StringVarP(
 		&publicKey,
 		"public-key", "", resources.InspektorGadgetPublicKey, "Public key used to verify the container image")
+	deployCmd.PersistentFlags().BoolVar(
+		&verifyGadgets,
+		"verify-gadgets", true, "Verify gadgets using the provided public keys")
 	// WARNING For now, use StringVar() instead of StringSliceVar() as only the
 	// first line of the file will be taken when used with
 	// --gadgets-public-keys="$(cat inspektor-gadget.pub),$(cat your-key.pub)"
 	deployCmd.PersistentFlags().StringVar(
 		&gadgetsPublicKeys,
-		"gadgets-public-keys", resources.InspektorGadgetPublicKey, "Public keys used to verify the gadgets. If empty, verification will not occur.")
+		"gadgets-public-keys", resources.InspektorGadgetPublicKey, "Public keys used to verify the gadgets")
 	rootCmd.AddCommand(deployCmd)
 }
 
@@ -736,6 +740,7 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 				return fmt.Errorf("%s.%s not found in config.yaml", gadgettracermanagerconfig.Operator, gadgettracermanagerconfig.Oci)
 			}
 
+			opOciCfg[gadgettracermanagerconfig.VerifyImage] = verifyGadgets
 			opOciCfg[gadgettracermanagerconfig.PublicKeys] = strings.Split(gadgetsPublicKeys, ",")
 
 			data, err := yaml.Marshal(cfg)

pkg/config/gadgettracermanagerconfig/config.go
@@ -26,5 +26,6 @@ const (
 	PodmanSocketPath       = "podman-socketpath"
 	Operator               = "operator"
 	Oci                    = "oci"
+	VerifyImage            = "verify-image"
 	PublicKeys             = "public-keys"
 )

pkg/oci/oci.go
@@ -39,7 +39,6 @@ import (
 	"github.com/distribution/reference"
 	"github.com/docker/cli/cli/config"
 	"github.com/docker/cli/cli/config/configfile"
-	"github.com/hashicorp/go-multierror"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/sigstore/sigstore/pkg/signature"
 	"github.com/sigstore/sigstore/pkg/signature/payload"
@@ -61,7 +60,8 @@ type AuthOptions struct {
 }
 
 type VerifyOptions struct {
-	PublicKeys []string
+	VerifyPublicKey bool
+	PublicKeys      []string
 }
 
 type AllowedDigestsOptions struct {
@@ -784,7 +784,7 @@ func verifyImage(ctx context.Context, image string, imgOpts *ImageOptions) error
 	}
 
 	verified := false
-	var errors error
+	var errs error
 	for _, publicKey := range imgOpts.PublicKeys {
 		verifier, err := newVerifier([]byte(publicKey))
 		if err != nil {
@@ -798,11 +798,11 @@ func verifyImage(ctx context.Context, image string, imgOpts *ImageOptions) error
 			break
 		}
 
-		errors = multierror.Append(errors, err)
+		errs = errors.Join(errs, err)
 	}
 
 	if !verified {
-		return fmt.Errorf("the image was not signed by the provided keys: %w", errors)
+		return fmt.Errorf("the image was not signed by the provided keys: %w", errs)
 	}
 
 	// We should not read the payload before confirming it was signed, so let's
@@ -939,8 +939,8 @@ func ensureImage(ctx context.Context, imageStore oras.Target, image string, imgO
 		}
 	}
 
-	if len(imgOpts.PublicKeys) == 0 {
-		imgOpts.Logger.Warnf("image signature verification is disabled because no public keys were provided")
+	if !imgOpts.VerifyPublicKey {
+		imgOpts.Logger.Warnf("image signature verification is disabled due to using corresponding option")
 
 		return nil
 	}

pkg/operators/oci-handler/oci.go
@@ -39,6 +39,7 @@ const (
 	insecureParam         = "insecure"
 	pullParam             = "pull"
 	pullSecret            = "pull-secret"
+	verifyImage           = "verify-image"
 	publicKeys            = "public-keys"
 	allowedDigests        = "allowed-digests"
 	allowedRegistries     = "allowed-registries"
@@ -60,9 +61,16 @@ func (o *ociHandler) Init(params *params.Params) error {
 func (o *ociHandler) GlobalParams() api.Params {
 	return api.Params{
 		{
+			Key:          verifyImage,
+			Title:        "Verify image",
+			Description:  "Verify image using the provided public key",
+			DefaultValue: "true",
+			TypeHint:     api.TypeBool,
+		},
+		{
 			Key:          publicKeys,
 			Title:        "Public keys",
-			Description:  "Public keys used to verify the gadget. If empty, verification will not occur.",
+			Description:  "Public keys used to verify the gadgets",
 			DefaultValue: resources.InspektorGadgetPublicKey,
 			TypeHint:     api.TypeStringSlice,
 		},
@@ -198,7 +206,8 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 			Insecure:    o.ociParams.Get(insecureParam).AsBool(),
 		},
 		VerifyOptions: oci.VerifyOptions{
-			PublicKeys: o.ociParams.Get(publicKeys).AsStringSlice(),
+			VerifyPublicKey: o.ociParams.Get(verifyImage).AsBool(),
+			PublicKeys:      o.ociParams.Get(publicKeys).AsStringSlice(),
 		},
 		AllowedDigestsOptions: oci.AllowedDigestsOptions{
 			AllowedDigests: o.ociParams.Get(allowedDigests).AsStringSlice(),

pkg/resources/manifests/deploy.yaml
@@ -29,6 +29,7 @@ data:
       podman-socketpath: /run/podman/podman.sock
       operator:
         oci:
+          verify-image: true
           public-keys:
             - |
               -----BEGIN PUBLIC KEY-----

integration/k8s/run_insecure_test.go
@@ -67,9 +67,8 @@ func TestRunInsecure(t *testing.T) {
 	}
 
 	// TODO: Ideally it should not depend on a real gadget, but we don't have a "test gadget" available yet.
-	// As the image was not signed, we need to set --public-keys="" to deactivate
-	// image signature verification.
-	cmd := fmt.Sprintf("ig run --public-keys='' %s:5000/trace_open:%s -o json --insecure --timeout 2", registryIP, *gadgetTag)
+	// As the image was not signed, we need to set --verify-image=false.
+	cmd := fmt.Sprintf("ig run --verify-image=false %s:5000/trace_open:%s -o json --insecure --timeout 2", registryIP, *gadgetTag)
 
 	// run the gadget without verifying its output as we only need to check if it runs
 	traceOpenCmd := &Command{