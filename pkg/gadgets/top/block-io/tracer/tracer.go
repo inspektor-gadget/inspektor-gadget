inspektor-gadget/inspektor-gadget

commit df44b47b082aab0fad0d5d6c36ba9fa26ee8bbd0

pkg/gadgets: Use tp_btf for top block-io.
With recent kernel, the kprobes were moved and it was not possible to read from
/proc/kallsyms, so this gadget was not working [1].
Hopefully, BTF tracepoints were added to cope with this [2].

So, this commit adds a mechanism to first try the new BTF tracepoints and
downgrade to kprobes if the first were not available.
This commit is highly inspired by a commit from iovisor/bcc [3].

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
[1]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=450b7879e345
[2]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=5a80bd075f3b
[3]: https://github.com/iovisor/bcc/commit/a9e381f2dcdf

pkg/gadgets/top/block-io/tracer/tracer.go
@@ -114,23 +114,30 @@ func (t *Tracer) install() error {
 		return fmt.Errorf("loading kernel symbols: %w", err)
 	}
 
-	// __blk_account_io_start and __blk_account_io_done were inlined in:
-	// be6bfe36db17 ("block: inline hot paths of blk_account_io_*()").
-	// which was included in kernel 5.16.
-	// So let's be future proof and check if these symbols do not exist.
-	blkAccountIoStartFunction := "__blk_account_io_start"
-	if !kernelSymbols.SymbolExists(blkAccountIoStartFunction) {
-		blkAccountIoStartFunction = "blk_account_io_start"
-	}
-
-	blkAccountIoDoneFunction := "__blk_account_io_done"
-	if !kernelSymbols.SymbolExists(blkAccountIoDoneFunction) {
-		blkAccountIoDoneFunction = "blk_account_io_done"
-	}
-
-	t.ioStartLink, err = link.Kprobe(blkAccountIoStartFunction, t.objs.IgTopioStart, nil)
+	// blk_account_io_start and blk_account_io_done were moved in:
+	// 450b7879e345 ("block: move blk_account_io_{start,done} to blk-mq.c")
+	// which was included in kernel 5.17.
+	// Hopefully, two BTF tracepoints were added, so let's be future proof and
+	// first try to attach to these new tracepoints:
+	// 5a80bd075f3b ("block: introduce block_io_start/block_io_done tracepoints")
+	t.ioStartLink, err = link.AttachTracing(link.TracingOptions{
+		Program:    t.objs.IgTopioStartTp,
+		AttachType: ebpf.AttachTraceRawTp,
+	})
 	if err != nil {
-		return fmt.Errorf("attaching kprobe: %w", err)
+		// __blk_account_io_start and __blk_account_io_done were inlined in:
+		// be6bfe36db17 ("block: inline hot paths of blk_account_io_*()").
+		// which was included in kernel 5.16.
+		// So let's be future proof and check if these symbols do not exist.
+		blkAccountIoStartFunction := "__blk_account_io_start"
+		if !kernelSymbols.SymbolExists(blkAccountIoStartFunction) {
+			blkAccountIoStartFunction = "blk_account_io_start"
+		}
+
+		t.ioStartLink, err = link.Kprobe(blkAccountIoStartFunction, t.objs.IgTopioStart, nil)
+		if err != nil {
+			return fmt.Errorf("attaching kprobe: %w", err)
+		}
 	}
 
 	t.startRequestLink, err = link.Kprobe("blk_mq_start_request", t.objs.IgTopioReq, nil)
@@ -138,9 +145,20 @@ func (t *Tracer) install() error {
 		return fmt.Errorf("attaching kprobe: %w", err)
 	}
 
-	t.doneLink, err = link.Kprobe(blkAccountIoDoneFunction, t.objs.IgTopioDone, nil)
+	t.ioStartLink, err = link.AttachTracing(link.TracingOptions{
+		Program:    t.objs.IgTopioDoneTp,
+		AttachType: ebpf.AttachTraceRawTp,
+	})
 	if err != nil {
-		return fmt.Errorf("attaching kprobe: %w", err)
+		blkAccountIoDoneFunction := "__blk_account_io_done"
+		if !kernelSymbols.SymbolExists(blkAccountIoDoneFunction) {
+			blkAccountIoDoneFunction = "blk_account_io_done"
+		}
+
+		t.doneLink, err = link.Kprobe(blkAccountIoDoneFunction, t.objs.IgTopioDone, nil)
+		if err != nil {
+			return fmt.Errorf("attaching kprobe: %w", err)
+		}
 	}
 
 	return nil

pkg/gadgets/top/block-io/tracer/biotop_arm64_bpfel.go
@@ -81,9 +81,11 @@ type biotopSpecs struct {
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type biotopProgramSpecs struct {
-	IgTopioDone  *ebpf.ProgramSpec `ebpf:"ig_topio_done"`
-	IgTopioReq   *ebpf.ProgramSpec `ebpf:"ig_topio_req"`
-	IgTopioStart *ebpf.ProgramSpec `ebpf:"ig_topio_start"`
+	IgTopioDone    *ebpf.ProgramSpec `ebpf:"ig_topio_done"`
+	IgTopioDoneTp  *ebpf.ProgramSpec `ebpf:"ig_topio_done_tp"`
+	IgTopioReq     *ebpf.ProgramSpec `ebpf:"ig_topio_req"`
+	IgTopioStart   *ebpf.ProgramSpec `ebpf:"ig_topio_start"`
+	IgTopioStartTp *ebpf.ProgramSpec `ebpf:"ig_topio_start_tp"`
 }
 
 // biotopMapSpecs contains maps before they are loaded into the kernel.
@@ -134,16 +136,20 @@ func (m *biotopMaps) Close() error {
 //
 // It can be passed to loadBiotopObjects or ebpf.CollectionSpec.LoadAndAssign.
 type biotopPrograms struct {
-	IgTopioDone  *ebpf.Program `ebpf:"ig_topio_done"`
-	IgTopioReq   *ebpf.Program `ebpf:"ig_topio_req"`
-	IgTopioStart *ebpf.Program `ebpf:"ig_topio_start"`
+	IgTopioDone    *ebpf.Program `ebpf:"ig_topio_done"`
+	IgTopioDoneTp  *ebpf.Program `ebpf:"ig_topio_done_tp"`
+	IgTopioReq     *ebpf.Program `ebpf:"ig_topio_req"`
+	IgTopioStart   *ebpf.Program `ebpf:"ig_topio_start"`
+	IgTopioStartTp *ebpf.Program `ebpf:"ig_topio_start_tp"`
 }
 
 func (p *biotopPrograms) Close() error {
 	return _BiotopClose(
 		p.IgTopioDone,
+		p.IgTopioDoneTp,
 		p.IgTopioReq,
 		p.IgTopioStart,
+		p.IgTopioStartTp,
 	)
 }
 

pkg/gadgets/top/block-io/tracer/biotop_x86_bpfel.go
@@ -81,9 +81,11 @@ type biotopSpecs struct {
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type biotopProgramSpecs struct {
-	IgTopioDone  *ebpf.ProgramSpec `ebpf:"ig_topio_done"`
-	IgTopioReq   *ebpf.ProgramSpec `ebpf:"ig_topio_req"`
-	IgTopioStart *ebpf.ProgramSpec `ebpf:"ig_topio_start"`
+	IgTopioDone    *ebpf.ProgramSpec `ebpf:"ig_topio_done"`
+	IgTopioDoneTp  *ebpf.ProgramSpec `ebpf:"ig_topio_done_tp"`
+	IgTopioReq     *ebpf.ProgramSpec `ebpf:"ig_topio_req"`
+	IgTopioStart   *ebpf.ProgramSpec `ebpf:"ig_topio_start"`
+	IgTopioStartTp *ebpf.ProgramSpec `ebpf:"ig_topio_start_tp"`
 }
 
 // biotopMapSpecs contains maps before they are loaded into the kernel.
@@ -134,16 +136,20 @@ func (m *biotopMaps) Close() error {
 //
 // It can be passed to loadBiotopObjects or ebpf.CollectionSpec.LoadAndAssign.
 type biotopPrograms struct {
-	IgTopioDone  *ebpf.Program `ebpf:"ig_topio_done"`
-	IgTopioReq   *ebpf.Program `ebpf:"ig_topio_req"`
-	IgTopioStart *ebpf.Program `ebpf:"ig_topio_start"`
+	IgTopioDone    *ebpf.Program `ebpf:"ig_topio_done"`
+	IgTopioDoneTp  *ebpf.Program `ebpf:"ig_topio_done_tp"`
+	IgTopioReq     *ebpf.Program `ebpf:"ig_topio_req"`
+	IgTopioStart   *ebpf.Program `ebpf:"ig_topio_start"`
+	IgTopioStartTp *ebpf.Program `ebpf:"ig_topio_start_tp"`
 }
 
 func (p *biotopPrograms) Close() error {
 	return _BiotopClose(
 		p.IgTopioDone,
+		p.IgTopioDoneTp,
 		p.IgTopioReq,
 		p.IgTopioStart,
+		p.IgTopioStartTp,
 	)
 }
 

pkg/gadgets/top/block-io/tracer/bpf/biotop.bpf.c
@@ -31,8 +31,7 @@ struct {
 	__type(value, struct val_t);
 } counts SEC(".maps");
 
-SEC("kprobe/blk_account_io_start")
-int BPF_KPROBE(ig_topio_start, struct request *req)
+static __always_inline int trace_start(struct request *req)
 {
 	u64 mntns_id;
 
@@ -71,8 +70,7 @@ int BPF_KPROBE(ig_topio_req, struct request *req)
 	return 0;
 }
 
-SEC("kprobe/blk_account_io_done")
-int BPF_KPROBE(ig_topio_done, struct request *req, u64 now)
+static __always_inline int trace_done(struct request *req)
 {
 	struct val_t *valp, zero = {};
 	struct info_t info = {};
@@ -118,6 +116,30 @@ int BPF_KPROBE(ig_topio_done, struct request *req, u64 now)
 	bpf_map_delete_elem(&whobyreq, &req);
 
 	return 0;
+}
+
+SEC("kprobe/blk_account_io_start")
+int BPF_KPROBE(ig_topio_start, struct request *req)
+{
+	return trace_start(req);
+}
+
+SEC("kprobe/blk_account_io_done")
+int BPF_KPROBE(ig_topio_done, struct request *req)
+{
+	return trace_done(req);
+}
+
+SEC("tp_btf/block_io_start")
+int BPF_PROG(ig_topio_start_tp, struct request *req)
+{
+	return trace_start(req);
+}
+
+SEC("tp_btf/block_io_done")
+int BPF_PROG(ig_topio_done_tp, struct request *req)
+{
+	return trace_done(req);
 }
 
 char LICENSE[] SEC("license") = "GPL";

commit b9eb8cecc1f2c2f209be0dac7c2cf66ce63a7fe5

pkg/gadgets: Fix tracepoint for top block-io.
Fixes: df44b47b082a ("pkg/gadgets: Use tp_btf for top block-io.")
Reported-by: Burak Ok <burakok@microsoft.com>
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/top/block-io/tracer/tracer.go
@@ -145,7 +145,7 @@ func (t *Tracer) install() error {
 		return fmt.Errorf("attaching kprobe: %w", err)
 	}
 
-	t.ioStartLink, err = link.AttachTracing(link.TracingOptions{
+	t.doneLink, err = link.AttachTracing(link.TracingOptions{
 		Program:    t.objs.IgTopioDoneTp,
 		AttachType: ebpf.AttachTraceRawTp,
 	})