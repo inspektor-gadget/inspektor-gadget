inspektor-gadget/inspektor-gadget

commit c7c7371310161f038da4d91727e150f4e7472ca3

pkg/datasource: Add DataSource package
This introduces the new DataSource package that abstracts our event handling. Instead
of using Go types, this is based on virtual types. DataSources can handle Data units that
can be emitted, subscribed to and altered by operators. Data can be serialized using
protocol buffers and sent directly in binary form using gRPC.

Signed-off-by: Michael Friese <mfriese@microsoft.com>

pkg/datasource/data.go
@@ -1,0 +1,439 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datasource
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"maps"
+	"reflect"
+	"slices"
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+)
+
+type (
+	data  api.GadgetData
+	field api.Field
+)
+
+func (*data) private() {}
+
+func (f *field) ReflectType() reflect.Type {
+	switch f.Kind {
+	default:
+		return nil
+	case api.Kind_Int8:
+		return reflect.TypeOf(int8(0))
+	case api.Kind_Int16:
+		return reflect.TypeOf(int16(0))
+	case api.Kind_Int32:
+		return reflect.TypeOf(int32(0))
+	case api.Kind_Int64:
+		return reflect.TypeOf(int64(0))
+	case api.Kind_Uint8:
+		return reflect.TypeOf(uint8(0))
+	case api.Kind_Uint16:
+		return reflect.TypeOf(uint16(0))
+	case api.Kind_Uint32:
+		return reflect.TypeOf(uint32(0))
+	case api.Kind_Uint64:
+		return reflect.TypeOf(uint64(0))
+	case api.Kind_Float32:
+		return reflect.TypeOf(float32(0))
+	case api.Kind_Float64:
+		return reflect.TypeOf(float64(0))
+	case api.Kind_Bool:
+		return reflect.TypeOf(false)
+	}
+}
+
+type dataSource struct {
+	name string
+	id   uint32
+
+	dType Type
+	dPool sync.Pool
+
+	// keeps information on registered fields
+	fields   []*field
+	fieldMap map[string]*field
+
+	tags        []string
+	annotations map[string]string
+
+	payloadCount uint32
+
+	requestedFields map[string]bool
+
+	subscriptions []*subscription
+
+	requested bool
+
+	byteOrder binary.ByteOrder
+	lock      sync.RWMutex
+}
+
+func newDataSource(t Type, name string) *dataSource {
+	return &dataSource{
+		name:            name,
+		dType:           t,
+		requestedFields: make(map[string]bool),
+		fieldMap:        make(map[string]*field),
+		byteOrder:       binary.NativeEndian,
+		tags:            make([]string, 0),
+		annotations:     map[string]string{},
+	}
+}
+
+func New(t Type, name string) DataSource {
+	ds := newDataSource(t, name)
+	ds.registerPool()
+	return ds
+}
+
+func NewFromAPI(in *api.DataSource) (DataSource, error) {
+	ds := newDataSource(Type(in.Type), in.Name)
+	for _, f := range in.Fields {
+		ds.fields = append(ds.fields, (*field)(f))
+		if !FieldFlagUnreferenced.In(f.Flags) {
+			ds.fieldMap[f.Name] = (*field)(f)
+		}
+	}
+	if in.Flags&api.DataSourceFlagsBigEndian != 0 {
+		ds.byteOrder = binary.BigEndian
+	} else {
+		ds.byteOrder = binary.LittleEndian
+	}
+	ds.registerPool()
+	// TODO: add more checks / validation
+	return ds, nil
+}
+
+func (ds *dataSource) registerPool() {
+	ds.dPool.New = func() any {
+		d := &data{
+			Payload: make([][]byte, ds.payloadCount),
+		}
+		for i := range d.Payload {
+			d.Payload[i] = make([]byte, 0)
+		}
+		return d
+	}
+}
+
+func (ds *dataSource) Name() string {
+	return ds.name
+}
+
+func (ds *dataSource) Type() Type {
+	return ds.dType
+}
+
+func (ds *dataSource) NewData() Data {
+	return ds.dPool.Get().(Data)
+}
+
+func (ds *dataSource) ByteOrder() binary.ByteOrder {
+	return ds.byteOrder
+}
+
+func resolveNames(id uint32, fields []*field, parentOffset uint32) (string, error) {
+	if id >= uint32(len(fields)) {
+		return "", errors.New("invalid id")
+	}
+	out := ""
+	if FieldFlagHasParent.In(fields[id].Flags) {
+		p, err := resolveNames(fields[id].Parent-parentOffset, fields, parentOffset)
+		if err != nil {
+			return "", errors.New("parent not found")
+		}
+		out = p + "."
+	}
+	out += fields[id].Name
+	return out, nil
+}
+
+// AddStaticFields adds a statically sized container for fields to the payload and returns an accessor for the
+// container; if you want to access individual fields, get them from the DataSource directly
+func (ds *dataSource) AddStaticFields(size uint32, fields []StaticField) (FieldAccessor, error) {
+	ds.lock.Lock()
+	defer ds.lock.Unlock()
+
+	idx := ds.payloadCount
+
+	// temporary write to newFields to not write to ds.fields in case of errors
+	newFields := make([]*field, 0, len(fields))
+
+	parentOffset := len(ds.fields)
+	parentFields := make(map[int]struct{})
+	checkParents := make(map[*field]struct{})
+
+	for _, f := range fields {
+		fieldName := f.FieldName()
+		if _, ok := ds.fieldMap[fieldName]; ok {
+			return nil, fmt.Errorf("field %q already exists", fieldName)
+		}
+		nf := &field{
+			Name:         fieldName,
+			Index:        uint32(len(ds.fields) + len(newFields)),
+			PayloadIndex: idx,
+			Flags:        FieldFlagStaticMember.Uint32(),
+		}
+		nf.Size = f.FieldSize()
+		nf.Offs = f.FieldOffset()
+		if nf.Offs+nf.Size > size {
+			return nil, fmt.Errorf("field %q exceeds size of container (offs %d, size %d, container size %d)", nf.Name, nf.Offs, nf.Size, size)
+		}
+		if s, ok := f.(TypedField); ok {
+			nf.Kind = s.FieldType()
+		}
+		if tagger, ok := f.(TaggedField); ok {
+			nf.Tags = tagger.FieldTags()
+		}
+		if s, ok := f.(FlaggedField); ok {
+			nf.Flags |= uint32(s.FieldFlags())
+		}
+		if s, ok := f.(AnnotatedField); ok {
+			nf.Annotations = s.FieldAnnotations()
+		}
+		if s, ok := f.(ParentedField); ok {
+			parent := s.FieldParent()
+			if parent >= 0 {
+				nf.Parent = uint32(parent + parentOffset)
+				nf.Flags |= FieldFlagHasParent.Uint32()
+				parentFields[parent] = struct{}{}
+				checkParents[nf] = struct{}{}
+			}
+		}
+		newFields = append(newFields, nf)
+	}
+
+	// Unref parent fields
+	for p := range parentFields {
+		FieldFlagUnreferenced.AddTo(&newFields[p].Flags)
+	}
+
+	// Check whether parent id is valid
+	for f := range checkParents {
+		parentID := f.Parent - uint32(parentOffset) // adjust offset again to match offset in newFields for this check
+		if parentID >= uint32(len(newFields)) {
+			return nil, fmt.Errorf("invalid parent for field %q", f.Name)
+		}
+	}
+
+	var err error
+	for i, f := range newFields {
+		f.FullName, err = resolveNames(uint32(i), newFields, uint32(parentOffset))
+		if err != nil {
+			return nil, fmt.Errorf("resolving full fieldnames: %w", err)
+		}
+	}
+
+	ds.fields = append(ds.fields, newFields...)
+
+	for _, f := range newFields {
+		ds.fieldMap[f.Name] = f
+	}
+
+	ds.payloadCount++
+
+	return &fieldAccessor{ds: ds, f: &field{
+		PayloadIndex: idx,
+		Size:         size,
+		Flags:        FieldFlagContainer.Uint32(),
+	}}, nil
+}
+
+func (ds *dataSource) AddField(name string, opts ...FieldOption) (FieldAccessor, error) {
+	ds.lock.Lock()
+	defer ds.lock.Unlock()
+
+	if _, ok := ds.fieldMap[name]; ok {
+		return nil, fmt.Errorf("field %q already exists", name)
+	}
+
+	nf := &field{
+		Name:     name,
+		FullName: name,
+		Index:    uint32(len(ds.fields)),
+		Kind:     api.Kind_Invalid,
+	}
+	for _, opt := range opts {
+		opt(nf)
+	}
+
+	// Reserve new payload for non-empty fields
+	if !FieldFlagEmpty.In(nf.Flags) {
+		nf.PayloadIndex = ds.payloadCount
+		ds.payloadCount++
+	}
+
+	ds.fields = append(ds.fields, nf)
+	ds.fieldMap[nf.FullName] = nf
+	return &fieldAccessor{ds: ds, f: nf}, nil
+}
+
+func (ds *dataSource) GetField(name string) FieldAccessor {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+
+	f, ok := ds.fieldMap[name]
+	if !ok {
+		return nil
+	}
+	return &fieldAccessor{ds: ds, f: f}
+}
+
+func (ds *dataSource) GetFieldsWithTag(tag ...string) []FieldAccessor {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+
+	res := make([]FieldAccessor, 0)
+	for _, f := range ds.fields {
+		for _, t := range tag {
+			if slices.Contains(f.Tags, t) {
+				res = append(res, &fieldAccessor{ds: ds, f: f})
+				break
+			}
+		}
+	}
+	return res
+}
+
+func (ds *dataSource) Subscribe(fn DataFunc, priority int) {
+	if fn == nil {
+		return
+	}
+
+	ds.lock.Lock()
+	defer ds.lock.Unlock()
+
+	ds.subscriptions = append(ds.subscriptions, &subscription{
+		priority: priority,
+		fn:       fn,
+	})
+	sort.SliceStable(ds.subscriptions, func(i, j int) bool {
+		return ds.subscriptions[i].priority < ds.subscriptions[j].priority
+	})
+}
+
+func (ds *dataSource) EmitAndRelease(d Data) error {
+	defer ds.dPool.Put(d)
+	for _, sub := range ds.subscriptions {
+		err := sub.fn(ds, d)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (ds *dataSource) Release(d Data) {
+	ds.dPool.Put(d)
+}
+
+func (ds *dataSource) ReportLostData(ctr uint64) {
+	// TODO
+}
+
+func (ds *dataSource) IsRequestedField(fieldName string) bool {
+	return true
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+	return ds.requestedFields[fieldName]
+}
+
+func (ds *dataSource) Dump(xd Data, wr io.Writer) {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+
+	d := xd.(*data)
+	for _, f := range ds.fields {
+		if f.Offs+f.Size > uint32(len(d.Payload[f.PayloadIndex])) {
+			fmt.Fprintf(wr, "%s (%d): ! invalid size\n", f.Name, f.Size)
+			continue
+		}
+		fmt.Fprintf(wr, "%s (%d) [%s]: ", f.Name, f.Size, strings.Join(f.Tags, " "))
+		if f.Offs > 0 || f.Size > 0 {
+			fmt.Fprintf(wr, "%v\n", d.Payload[f.PayloadIndex][f.Offs:f.Offs+f.Size])
+		} else {
+			fmt.Fprintf(wr, "%v\n", d.Payload[f.PayloadIndex])
+		}
+	}
+}
+
+func (ds *dataSource) Fields() []*api.Field {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+
+	res := make([]*api.Field, 0, len(ds.fields))
+	for _, f := range ds.fields {
+		res = append(res, (*api.Field)(f))
+	}
+	return res
+}
+
+func (ds *dataSource) Accessors(rootOnly bool) []FieldAccessor {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+
+	res := make([]FieldAccessor, 0, len(ds.fields))
+	for _, f := range ds.fields {
+		if rootOnly && FieldFlagHasParent.In(f.Flags) {
+			continue
+		}
+		res = append(res, &fieldAccessor{
+			ds: ds,
+			f:  f,
+		})
+	}
+	return res
+}
+
+func (ds *dataSource) IsRequested() bool {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+	return ds.requested
+}
+
+func (ds *dataSource) AddAnnotation(key, value string) {
+	ds.lock.Lock()
+	defer ds.lock.Unlock()
+	ds.annotations[key] = value
+}
+
+func (ds *dataSource) AddTag(tag string) {
+	ds.lock.Lock()
+	defer ds.lock.Unlock()
+	ds.tags = append(ds.tags, tag)
+}
+
+func (ds *dataSource) Annotations() map[string]string {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+	return maps.Clone(ds.annotations)
+}
+
+func (ds *dataSource) Tags() []string {
+	ds.lock.RLock()
+	defer ds.lock.RUnlock()
+	return slices.Clone(ds.tags)
+}

pkg/datasource/accessors.go
@@ -1,0 +1,407 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datasource
+
+import (
+	"errors"
+	"fmt"
+	"maps"
+	"math"
+	"slices"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+)
+
+// FieldAccessor grants access to the underlying buffer of a field
+type FieldAccessor interface {
+	Name() string
+
+	// Size returns the expected size of the underlying field or zero, if the field has a dynamic size
+	Size() uint32
+
+	// Get returns the underlying memory of the field
+	Get(data Data) []byte
+
+	// Set sets value as the new reference for the field; if the FieldAccessor is used for the member of a
+	// statically sized payload (for example a member of an eBPF struct), value will be copied to the existing
+	// memory instead.
+	Set(data Data, value []byte) error
+
+	// IsRequested returns whether the consumer is interested in this field; if not, operators are not required
+	// to fill them out
+	IsRequested() bool
+
+	// AddSubField adds a new field as member of the current field; be careful when doing this on an existing
+	// non-empty field, as that might be dropped on serialization // TODO
+	AddSubField(name string, opts ...FieldOption) (FieldAccessor, error)
+
+	// GetSubFieldsWithTag returns all SubFields matching any given tag
+	GetSubFieldsWithTag(tag ...string) []FieldAccessor
+
+	// Parent returns the parent of this field, if this field is a SubField
+	Parent() FieldAccessor
+
+	// SubFields returns all existing SubFields of the current field
+	SubFields() []FieldAccessor
+
+	// SetHidden marks a field as hidden (by default) - it can still be requested
+	SetHidden(hidden bool, recurse bool)
+
+	// Type returns the underlying type of the field
+	Type() api.Kind
+
+	// Flags returns the flags of the field
+	Flags() uint32
+
+	// Annotations returns stored annotations of the field
+	Annotations() map[string]string
+
+	// RemoveReference removes the reference by name from the hierarchy, effectively freeing the name
+	// tbd: name
+	RemoveReference(recurse bool)
+
+	Uint8(Data) uint8
+	Uint16(Data) uint16
+	Uint32(Data) uint32
+	Uint64(Data) uint64
+	Int8(Data) int8
+	Int16(Data) int16
+	Int32(Data) int32
+	Int64(Data) int64
+
+	Float32(Data) float32
+	Float64(Data) float64
+
+	PutUint8(Data, uint8)
+	PutUint16(Data, uint16)
+	PutUint32(Data, uint32)
+	PutUint64(Data, uint64)
+	PutInt8(Data, int8)
+	PutInt16(Data, int16)
+	PutInt32(Data, int32)
+	PutInt64(Data, int64)
+
+	String(Data) string
+	CString(Data) string
+}
+
+type fieldAccessor struct {
+	ds *dataSource
+	f  *field
+}
+
+func (a *fieldAccessor) Name() string {
+	return a.f.Name
+}
+
+func (a *fieldAccessor) Size() uint32 {
+	return a.f.Size
+}
+
+func (a *fieldAccessor) Type() api.Kind {
+	return a.f.Kind
+}
+
+func (a *fieldAccessor) Get(d Data) []byte {
+	if FieldFlagEmpty.In(a.f.Flags) {
+		return nil
+	}
+	if a.f.Size > 0 {
+		// size and offset must be valid here; checks take place on initialization
+		return d.(*data).Payload[a.f.PayloadIndex][a.f.Offs : a.f.Offs+a.f.Size]
+	}
+	return d.(*data).Payload[a.f.PayloadIndex]
+}
+
+func (a *fieldAccessor) setHidden(hidden bool, recurse bool) {
+	if !hidden {
+		FieldFlagHidden.RemoveFrom(&a.f.Flags)
+	} else {
+		FieldFlagHidden.AddTo(&a.f.Flags)
+	}
+	if recurse {
+		for _, acc := range a.subFields() {
+			acc.(*fieldAccessor).setHidden(hidden, recurse)
+		}
+	}
+}
+
+func (a *fieldAccessor) SetHidden(hidden bool, recurse bool) {
+	a.ds.lock.Lock()
+	defer a.ds.lock.Unlock()
+	a.setHidden(hidden, recurse)
+}
+
+func (a *fieldAccessor) Set(d Data, b []byte) error {
+	if FieldFlagEmpty.In(a.f.Flags) {
+		return errors.New("field cannot contain a value")
+	}
+	if FieldFlagStaticMember.In(a.f.Flags) {
+		if uint32(len(b)) != a.f.Size {
+			return fmt.Errorf("invalid size, static member expected %d, got %d", a.f.Size, len(b))
+		}
+		// When accessing a member of a statically sized field, copy memory
+		copy(d.Raw().Payload[a.f.PayloadIndex][a.f.Offs:a.f.Offs+a.f.Size], b)
+		return nil
+	}
+	if FieldFlagContainer.In(a.f.Flags) {
+		if uint32(len(b)) != a.f.Size {
+			return fmt.Errorf("invalid size, container expected %d, got %d", a.f.Size, len(b))
+		}
+	}
+	d.(*data).Payload[a.f.PayloadIndex] = b
+	return nil
+}
+
+func (a *fieldAccessor) removeReference(recurse bool) {
+	// Add flag and remove from fieldMap
+	FieldFlagUnreferenced.AddTo(&a.f.Flags)
+	delete(a.ds.fieldMap, a.f.FullName)
+	if recurse {
+		for _, acc := range a.subFields() {
+			acc.(*fieldAccessor).removeReference(recurse)
+		}
+	}
+}
+
+func (a *fieldAccessor) RemoveReference(recurse bool) {
+	a.ds.lock.Lock()
+	defer a.ds.lock.Unlock()
+	a.removeReference(recurse)
+}
+
+func (a *fieldAccessor) AddSubField(name string, opts ...FieldOption) (FieldAccessor, error) {
+	a.ds.lock.Lock()
+	defer a.ds.lock.Unlock()
+
+	parentFullName, err := resolveNames(a.f.Index, a.ds.fields, 0)
+	if err != nil {
+		return nil, fmt.Errorf("resolving parent field name: %w", err)
+	}
+
+	nf := &field{
+		Name:     name,
+		FullName: parentFullName + "." + name,
+		Kind:     api.Kind_Invalid,
+		Parent:   a.f.Index,
+		Index:    uint32(len(a.ds.fields)),
+	}
+	for _, opt := range opts {
+		opt(nf)
+	}
+
+	if _, ok := a.ds.fieldMap[nf.FullName]; ok {
+		return nil, fmt.Errorf("field with name %q already exists", nf.FullName)
+	}
+
+	FieldFlagHasParent.AddTo(&nf.Flags)
+
+	if !FieldFlagEmpty.In(nf.Flags) {
+		nf.PayloadIndex = a.ds.payloadCount
+		a.ds.payloadCount++
+	}
+
+	a.ds.fields = append(a.ds.fields, nf)
+	a.ds.fieldMap[nf.FullName] = nf
+	return &fieldAccessor{ds: a.ds, f: nf}, nil
+}
+
+func (a *fieldAccessor) subFields() []FieldAccessor {
+	var res []FieldAccessor
+	for _, f := range a.ds.fields {
+		if !FieldFlagHasParent.In(f.Flags) {
+			continue
+		}
+		if f.Parent != a.f.Index {
+			continue
+		}
+		res = append(res, &fieldAccessor{
+			ds: a.ds,
+			f:  f,
+		})
+	}
+	return res
+}
+
+func (a *fieldAccessor) SubFields() []FieldAccessor {
+	a.ds.lock.RLock()
+	defer a.ds.lock.RUnlock()
+	return a.subFields()
+}
+
+func (a *fieldAccessor) Parent() FieldAccessor {
+	a.ds.lock.RLock()
+	defer a.ds.lock.RUnlock()
+
+	if !FieldFlagHasParent.In(a.f.Flags) {
+		return nil
+	}
+	if a.f.Parent >= uint32(len(a.ds.fields)) {
+		return nil
+	}
+	return &fieldAccessor{ds: a.ds, f: a.ds.fields[a.f.Parent]}
+}
+
+func (a *fieldAccessor) GetSubFieldsWithTag(tag ...string) []FieldAccessor {
+	a.ds.lock.RLock()
+	defer a.ds.lock.RUnlock()
+
+	res := make([]FieldAccessor, 0)
+	for _, f := range a.ds.fields {
+		if !FieldFlagHasParent.In(f.Flags) {
+			continue
+		}
+		if f.Parent != a.f.Index {
+			continue
+		}
+		for _, t := range tag {
+			if slices.Contains(f.Tags, t) {
+				res = append(res, &fieldAccessor{ds: a.ds, f: f})
+				break
+			}
+		}
+	}
+	return res
+}
+
+func (a *fieldAccessor) IsRequested() bool {
+	return a.ds.IsRequestedField(a.f.Name)
+}
+
+func (a *fieldAccessor) Flags() uint32 {
+	return a.f.Flags
+}
+
+func (a *fieldAccessor) Annotations() map[string]string {
+	if a.f.Annotations == nil {
+		// Return an empty map to allow access without prior checks
+		return map[string]string{}
+	}
+	// return a clone to avoid write access
+	return maps.Clone(a.f.Annotations)
+}
+
+func (a *fieldAccessor) Uint8(data Data) uint8 {
+	val := a.Get(data)
+	if len(val) < 1 {
+		return 0
+	}
+	return val[0]
+}
+
+func (a *fieldAccessor) Uint16(data Data) uint16 {
+	val := a.Get(data)
+	if len(val) < 2 {
+		return 0
+	}
+	return a.ds.byteOrder.Uint16(val)
+}
+
+func (a *fieldAccessor) Uint32(data Data) uint32 {
+	val := a.Get(data)
+	if len(val) < 4 {
+		return 0
+	}
+	return a.ds.byteOrder.Uint32(val)
+}
+
+func (a *fieldAccessor) Uint64(data Data) uint64 {
+	val := a.Get(data)
+	if len(val) < 8 {
+		return 0
+	}
+	return a.ds.byteOrder.Uint64(val)
+}
+
+func (a *fieldAccessor) Int8(data Data) int8 {
+	val := a.Get(data)
+	if len(val) < 1 {
+		return 0
+	}
+	return int8(val[0])
+}
+
+func (a *fieldAccessor) Int16(data Data) int16 {
+	val := a.Get(data)
+	if len(val) < 2 {
+		return 0
+	}
+	return int16(a.ds.byteOrder.Uint16(val))
+}
+
+func (a *fieldAccessor) Int32(data Data) int32 {
+	val := a.Get(data)
+	if len(val) < 4 {
+		return 0
+	}
+	return int32(a.ds.byteOrder.Uint32(val))
+}
+
+func (a *fieldAccessor) Int64(data Data) int64 {
+	val := a.Get(data)
+	if len(val) < 8 {
+		return 0
+	}
+	return int64(a.ds.byteOrder.Uint64(val))
+}
+
+func (a *fieldAccessor) Float32(data Data) float32 {
+	return math.Float32frombits(a.Uint32(data))
+}
+
+func (a *fieldAccessor) Float64(data Data) float64 {
+	return math.Float64frombits(a.Uint64(data))
+}
+
+func (a *fieldAccessor) String(data Data) string {
+	return string(a.Get(data))
+}
+
+func (a *fieldAccessor) CString(data Data) string {
+	return gadgets.FromCString(a.Get(data))
+}
+
+func (a *fieldAccessor) PutUint8(data Data, val uint8) {
+	a.Get(data)[0] = val
+}
+
+func (a *fieldAccessor) PutUint16(data Data, val uint16) {
+	a.ds.byteOrder.PutUint16(a.Get(data), val)
+}
+
+func (a *fieldAccessor) PutUint32(data Data, val uint32) {
+	a.ds.byteOrder.PutUint32(a.Get(data), val)
+}
+
+func (a *fieldAccessor) PutUint64(data Data, val uint64) {
+	a.ds.byteOrder.PutUint64(a.Get(data), val)
+}
+
+func (a *fieldAccessor) PutInt8(data Data, val int8) {
+	a.Get(data)[0] = uint8(val)
+}
+
+func (a *fieldAccessor) PutInt16(data Data, val int16) {
+	a.ds.byteOrder.PutUint16(a.Get(data), uint16(val))
+}
+
+func (a *fieldAccessor) PutInt32(data Data, val int32) {
+	a.ds.byteOrder.PutUint32(a.Get(data), uint32(val))
+}
+
+func (a *fieldAccessor) PutInt64(data Data, val int64) {
+	a.ds.byteOrder.PutUint64(a.Get(data), uint64(val))
+}

pkg/datasource/columns.go
@@ -1,0 +1,171 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datasource
+
+import (
+	"fmt"
+	"reflect"
+	"slices"
+	"strconv"
+	"unsafe"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns/ellipsis"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	metadatav1 "github.com/inspektor-gadget/inspektor-gadget/pkg/metadata/v1"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/parser"
+)
+
+type DataTuple struct {
+	ds   DataSource
+	data *data
+}
+
+func NewDataTuple(ds DataSource, d Data) *DataTuple {
+	return &DataTuple{
+		ds:   ds,
+		data: d.(*data),
+	}
+}
+
+func (ds *dataSource) Parser() (parser.Parser, error) {
+	cols, err := ds.Columns()
+	if err != nil {
+		return nil, err
+	}
+	return parser.NewParser(cols), nil
+}
+
+func (ds *dataSource) Columns() (*columns.Columns[DataTuple], error) {
+	cols, err := columns.NewColumns[DataTuple]()
+	if err != nil {
+		return nil, err
+	}
+
+	for i, f := range ds.fields {
+		if FieldFlagEmpty.In(f.Flags) || FieldFlagUnreferenced.In(f.Flags) {
+			continue
+		}
+
+		attributes := &columns.Attributes{
+			Name:    f.FullName,
+			Tags:    f.Tags,
+			Visible: !FieldFlagHidden.In(f.Flags),
+			Width:   columns.GetDefault().DefaultWidth,
+			Order:   i * 100,
+		}
+
+		df := columns.DynamicField{
+			Attributes: attributes,
+			Offset:     uintptr(f.Offs),
+		}
+
+		// extract attributes from annotations
+		for k, v := range f.Annotations {
+			switch k {
+			case "columns.alignment":
+				switch metadatav1.Alignment(v) {
+				case metadatav1.AlignmentLeft:
+					attributes.Alignment = columns.AlignLeft
+				case metadatav1.AlignmentRight:
+					attributes.Alignment = columns.AlignRight
+				default:
+					return nil, fmt.Errorf("invalid alignment type for column %q: %s", f.Name, v)
+				}
+			case "columns.ellipsis":
+				switch metadatav1.EllipsisType(v) {
+				case metadatav1.EllipsisNone:
+					attributes.EllipsisType = ellipsis.None
+				case metadatav1.EllipsisStart:
+					attributes.EllipsisType = ellipsis.Start
+				case metadatav1.EllipsisMiddle:
+					attributes.EllipsisType = ellipsis.Middle
+				case metadatav1.EllipsisEnd:
+					attributes.EllipsisType = ellipsis.End
+				default:
+					return nil, fmt.Errorf("invalid ellipsis type for column %q: %s", f.Name, v)
+				}
+			case "columns.width":
+				var err error
+				attributes.Width, err = strconv.Atoi(v)
+				if err != nil {
+					return nil, fmt.Errorf("reading width for column %q: %w", f.Name, err)
+				}
+			case "columns.minWidth":
+				var err error
+				attributes.MinWidth, err = strconv.Atoi(v)
+				if err != nil {
+					return nil, fmt.Errorf("reading minWidth for column %q: %w", f.Name, err)
+				}
+			case "columns.maxWidth":
+				var err error
+				attributes.MaxWidth, err = strconv.Atoi(v)
+				if err != nil {
+					return nil, fmt.Errorf("reading maxWidth for column %q: %w", f.Name, err)
+				}
+			case "columns.template":
+				attributes.Template = v
+				df.Template = v
+			case "columns.fixed":
+				if v == "true" {
+					attributes.FixedWidth = true
+				}
+			}
+		}
+
+		if f.ReflectType() == nil {
+			df.Type = reflect.TypeOf([]byte{})
+
+			acc := &fieldAccessor{
+				ds: ds,
+				f:  f,
+			}
+			fromC := slices.Contains(f.Tags, api.TagSrcEbpf)
+			err := cols.AddColumn(*df.Attributes, func(d *DataTuple) any {
+				if d.data == nil {
+					return ""
+				}
+
+				if fromC {
+					return gadgets.FromCString(acc.Get(d.data))
+				}
+				return string(acc.Get(d.data))
+			})
+			if err != nil {
+				return nil, fmt.Errorf("creating columns: %w", err)
+			}
+			continue
+		}
+
+		if attributes.Width == 0 {
+			attributes.Width = columns.GetWidthFromType(f.ReflectType().Kind())
+		}
+
+		df.Type = f.ReflectType()
+		idx := f.PayloadIndex
+
+		err := cols.AddFields([]columns.DynamicField{df}, func(d *DataTuple) unsafe.Pointer {
+			if len(d.data.Payload[idx]) == 0 {
+				return nil
+			}
+			return unsafe.Pointer(&d.data.Payload[idx][0])
+		})
+		if err != nil {
+			return nil, fmt.Errorf("creating columns: %w", err)
+		}
+	}
+	return cols, nil
+}

commit 571de4a44083a1722f5d63dec6ca6586989a588b

pkg/datasource: Use FullName instead of Name.
This commit extends d0254b2990ce ("pkg/datasource: Use FullName to add field.")
to use FullName instead of Name in all places where we add a fields.

Indeed, we use the FullName to retrieve the fields, so we need to be consistent
otherwise we may lack data printed.

Fixes: c7c737131016 ("pkg/datasource: Add DataSource package")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/datasource/data.go
@@ -419,7 +419,7 @@ func (ds *dataSource) AddStaticFields(size uint32, fields []StaticField) (FieldA
 	ds.fields = append(ds.fields, newFields...)
 
 	for _, f := range newFields {
-		ds.fieldMap[f.Name] = f
+		ds.fieldMap[f.FullName] = f
 	}
 
 	ds.payloadCount++
@@ -724,7 +724,7 @@ func (ds *dataSource) CopyFieldsTo(out DataSource) error {
 		nf, _ := msg.(*api.Field)
 		outDs.fields = append(outDs.fields, (*field)(nf))
 		if !FieldFlagUnreferenced.In(f.Flags) {
-			outDs.fieldMap[nf.Name] = (*field)(nf)
+			outDs.fieldMap[nf.FullName] = (*field)(nf)
 		}
 	}
 