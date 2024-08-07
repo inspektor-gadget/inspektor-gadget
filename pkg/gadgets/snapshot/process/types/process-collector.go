inspektor-gadget/inspektor-gadget

commit d61e5ef270f118e4f51f20c99224b2cc1dac83d2

cc: Move BasicK8sMetadata to pkg/types and use it in CommonData
This change is needed in order to be able to import it from the
runtimeclient package. Otherwise, we would create an import cycle.

Signed-off-by: Jose Blanquicet <josebl@microsoft.com>

pkg/gadgets/snapshot/process/types/process-collector.go
@@ -107,7 +107,7 @@ func treeToStringBuilder(node *processTree, builder *strings.Builder, depth int)
 func WriteTree(output io.Writer, processes []*Event) error {
 	containers := make(map[string][]*Event, len(processes))
 	for _, process := range processes {
-		containers[process.Container] = append(containers[process.Container], process)
+		containers[process.K8s.ContainerName] = append(containers[process.K8s.ContainerName], process)
 	}
 
 	for _, container := range containers {
@@ -116,13 +116,13 @@ func WriteTree(output io.Writer, processes []*Event) error {
 			return err
 		}
 
-		if tree.process.Namespace != "" {
-			fmt.Fprintf(output, "%s/", tree.process.Namespace)
+		if tree.process.K8s.Namespace != "" {
+			fmt.Fprintf(output, "%s/", tree.process.K8s.Namespace)
 		}
-		if tree.process.Pod != "" && tree.process.Pod != tree.process.Container {
-			fmt.Fprintf(output, "%s/", tree.process.Pod)
+		if tree.process.K8s.PodName != "" && tree.process.K8s.PodName != tree.process.K8s.ContainerName {
+			fmt.Fprintf(output, "%s/", tree.process.K8s.PodName)
 		}
-		fmt.Fprintln(output, tree.process.Container)
+		fmt.Fprintln(output, tree.process.K8s.ContainerName)
 
 		fmt.Fprint(output, tree)
 	}

commit 2262f77ceadd953ea2e989a1f5a84fe76bff8ca4

pkg/gadgets: Fix tree output for snapshot process.
With the changes added in d61e5ef270f1
("cc: Move BasicK8sMetadata to pkg/types and use it in CommonData") this output
mode no more worked in non k8s context as there would be an empty string for
K8S.ContainerName.
Let's use the mount namespace ID to group processes in a container instead.

Fixes: d61e5ef270f1 ("cc: Move BasicK8sMetadata to pkg/types and use it in CommonData")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/snapshot/process/types/process-collector.go
@@ -105,9 +105,10 @@ func treeToStringBuilder(node *processTree, builder *strings.Builder, depth int)
 }
 
 func WriteTree(output io.Writer, processes []*Event) error {
-	containers := make(map[string][]*Event, len(processes))
+	containers := make(map[uint64][]*Event, len(processes))
 	for _, process := range processes {
-		containers[process.K8s.ContainerName] = append(containers[process.K8s.ContainerName], process)
+		mntNsID := process.GetMountNSID()
+		containers[mntNsID] = append(containers[mntNsID], process)
 	}
 
 	for _, container := range containers {
@@ -122,7 +123,11 @@ func WriteTree(output io.Writer, processes []*Event) error {
 		if tree.process.K8s.PodName != "" && tree.process.K8s.PodName != tree.process.K8s.ContainerName {
 			fmt.Fprintf(output, "%s/", tree.process.K8s.PodName)
 		}
-		fmt.Fprintln(output, tree.process.K8s.ContainerName)
+		if tree.process.K8s.ContainerName != "" {
+			fmt.Fprintln(output, tree.process.K8s.ContainerName)
+		} else {
+			fmt.Fprintln(output, tree.process.Runtime.ContainerName)
+		}
 
 		fmt.Fprint(output, tree)
 	}