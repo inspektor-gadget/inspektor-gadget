a86f629fee983926c477046efd881423d91a384e
mastersans <sanskarsharma3110@gmail.com>
2024-09-23 14:12:24 +0530

    use validateEvent instead of matchEntries
    Signed-off-by: mastersans <sanskarsharma3110@gmail.com>

--- gadgets/trace_open/test/unit/trace_open_test.go
+++ gadgets/trace_open/test/unit/trace_open_test.go
@@ -15,7 +15,10 @@
 package tests
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -23,15 +26,22 @@ import (
 
 	utilstest "github.com/inspektor-gadget/inspektor-gadget/internal/test"
 
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/gadgetapi"
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/match"
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/utils"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	ocihandler "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/oci-handler"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/simple"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/local"
+
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/ebpf"
+
+	igjson "github.com/inspektor-gadget/inspektor-gadget/pkg/datasource/formatters/json"
 )
 
 type ExpectedTraceOpenEvent struct {
 	Comm  string `json:"comm"`
-	Pid   uint32 `json:"pid"`
-	Tid   uint32 `json:"tid"`
+	Pid   int    `json:"pid"`
+	Tid   int    `json:"tid"`
 	Uid   uint32 `json:"uid"`
 	Gid   uint32 `json:"gid"`
 	Fd    uint32 `json:"fd"`
@@ -41,83 +51,103 @@ type ExpectedTraceOpenEvent struct {
 type testDef struct {
 	runnerConfig  *utilstest.RunnerConfig
 	generateEvent func() (int, error)
-	expectedEntry *ExpectedTraceOpenEvent
-	normalize     func(e *ExpectedTraceOpenEvent)
+	validateEvent func(t *testing.T, info *utilstest.RunnerInfo, fd int, events []ExpectedTraceOpenEvent) error
 }
 
 func TestTraceOpenGadget(t *testing.T) {
 	utilstest.RequireRoot(t)
 	testCases := map[string]testDef{
-		"testcase1": {
+		"captures_all_events": {
 			runnerConfig:  &utilstest.RunnerConfig{},
 			generateEvent: generateEvent,
-			expectedEntry: &ExpectedTraceOpenEvent{
-				Comm:  "unit.test",
-				Pid:   utils.NormalizedInt,
-				Tid:   utils.NormalizedInt,
-				Uid:   0,
-				Gid:   0,
-				Fd:    utils.NormalizedInt,
-				FName: "/dev/null",
-			},
-			normalize: func(e *ExpectedTraceOpenEvent) {
-				utils.NormalizeInt(&e.Pid)
-				utils.NormalizeInt(&e.Tid)
-				utils.NormalizeInt(&e.Fd)
-			},
-		},
-		"testcase2": {
-			runnerConfig: &utilstest.RunnerConfig{},
-			generateEvent: func() (int, error) {
-				fd, err := unix.Open("/tmp/foo.test", unix.O_RDONLY|unix.O_CREAT, 0)
-				if err != nil {
-					return 0, fmt.Errorf("opening file: %w", err)
-				}
-				defer unix.Close(fd)
-
-				badfd, err := unix.Open("/tmp/quux.test", unix.O_RDONLY|unix.O_CREAT, 0)
-				if err != nil {
-					return 0, fmt.Errorf("opening file: %w", err)
-				}
-				defer unix.Close(badfd)
-
-				return fd, nil
-			},
-			expectedEntry: &ExpectedTraceOpenEvent{
-				Comm:  "unit.test",
-				Pid:   utils.NormalizedInt,
-				Tid:   utils.NormalizedInt,
-				Uid:   0,
-				Gid:   0,
-				Fd:    utils.NormalizedInt,
-				FName: "/tmp/foo.test",
-			},
-			normalize: func(e *ExpectedTraceOpenEvent) {
-				utils.NormalizeInt(&e.Pid)
-				utils.NormalizeInt(&e.Tid)
-				utils.NormalizeInt(&e.Fd)
+			validateEvent: func(t *testing.T, info *utilstest.RunnerInfo, fd int, events []ExpectedTraceOpenEvent) error {
+				utilstest.ExpectAtLeastOneEvent(func(info *utilstest.RunnerInfo, fd int) *ExpectedTraceOpenEvent {
+					return &ExpectedTraceOpenEvent{
+						Comm:  "unit.test",
+						Pid:   info.Pid,
+						Tid:   info.Tid,
+						Uid:   uint32(info.Uid),
+						Gid:   uint32(info.Gid),
+						Fd:    uint32(fd),
+						FName: "/dev/null",
+					}
+				})(t, info, fd, events)
+				return nil
 			},
 		},
 	}
 	for name, testCase := range testCases {
 		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-			timeout := 10 * time.Second
-			api, _ := gadgetapi.NewGadget("ghcr.io/inspektor-gadget/gadget/trace_open:latest", timeout)
-			params := map[string]string{
-				"operator.oci.ebpf.uid": "0",
-			}
+			var wg sync.WaitGroup
+			events := make(chan ExpectedTraceOpenEvent, 100)
+			subscribed := make(chan bool, 1)
+			wg.Add(1)
+			go func() {
+				timeout := 5 * time.Second
+				const opPriority = 50000
 
-			api.StartGadget(params)
+				myOperator := simple.New("gadget", simple.OnInit(func(gadgetCtx operators.GadgetContext) error {
+					for _, d := range gadgetCtx.GetDataSources() {
+						jsonFormatter, _ := igjson.New(d)
+						d.Subscribe(func(source datasource.DataSource, data datasource.Data) error {
+
+							jsonOutput := jsonFormatter.Marshal(data)
+
+							event := &ExpectedTraceOpenEvent{}
+							if err := json.Unmarshal(jsonOutput, event); err != nil {
+								return fmt.Errorf("unmarshaling event: %w", err)
+							}
+							events <- *event
+							return nil
+						}, opPriority)
+
+					}
+					subscribed <- true
+					return nil
+				}))
+
+				gadgetCtx := gadgetcontext.New(
+					context.Background(),
+					"ghcr.io/inspektor-gadget/gadget/trace_open:latest",
+					gadgetcontext.WithDataOperators(
+						ocihandler.OciHandler,
+						myOperator,
+					),
+					gadgetcontext.WithTimeout(timeout),
+				)
+				runtime := local.New()
+				if err := runtime.Init(nil); err != nil {
+					fmt.Errorf("runtime init: %w", err)
+				}
+				params := map[string]string{
+					// Filter only events from the root user
+					"operator.oci.ebpf.uid": "0",
+				}
+
+				if err := runtime.RunGadget(gadgetCtx, nil, params); err != nil {
+					fmt.Errorf("running gadget: %w", err)
+				}
+				wg.Done()
+
+			}()
+
+			<-subscribed
+			time.Sleep(1 * time.Second)
+			close(subscribed)
+			var fd int
 			runner := utilstest.NewRunnerWithTest(t, testCase.runnerConfig)
 			utilstest.RunWithRunner(t, runner, func() error {
-				testCase.generateEvent()
-				// Wait for the event to be processed
-				time.Sleep(1 * time.Second)
-				output := api.Output()
-				match.MatchEntries(t, match.JSONSingleArrayMode, string(output), testCase.normalize, testCase.expectedEntry)
+				fd, _ = testCase.generateEvent()
 				return nil
 			})
+
+			wg.Wait()
+			close(events)
+			var capturedEvents []ExpectedTraceOpenEvent
+			for event := range events {
+				capturedEvents = append(capturedEvents, event)
+			}
+			testCase.validateEvent(t, runner.Info, fd, capturedEvents)
 		})
 	}
 }
