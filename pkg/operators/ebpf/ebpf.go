a9fda68e124529beb5e4aa496a1af8ecfc9d1e00
Francis Laniel <flaniel@linux.microsoft.com>
2024-10-15 17:30:59 +0200

    operators/ebpf: Store eBPF maps as context variables.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- pkg/operators/ebpf/ebpf.go
+++ pkg/operators/ebpf/ebpf.go
@@ -642,6 +642,10 @@ func (i *ebpfInstance) Start(gadgetCtx operators.GadgetContext) error {
 	}
 	i.collection = collection
 
+	for name, m := range i.collection.Maps {
+		gadgetCtx.SetVar(operators.MapPrefix+name, m)
+	}
+
 	for _, tracer := range i.tracers {
 		i.logger.Debugf("starting tracer %q", tracer.mapName)
 		go func(tracer *Tracer) {
