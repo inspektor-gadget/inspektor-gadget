inspektor-gadget/inspektor-gadget

commit d5e0f1198bd74c9ef66dea1c53a84ea94b40cecc

formatters: enrich proto field to include protocol name in L4 endpoints
Signed-off-by: Snehil Shah <snehilshah.989@gmail.com>

pkg/operators/formatters/formatters.go
@@ -32,6 +32,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/annotations"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/protocols"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/syscalls"
 )
 
@@ -393,6 +394,23 @@ var replacers = []replacer{
 				return nil, fmt.Errorf("port size expected to be 2 bytes")
 			}
 
+			protos := in.GetSubFieldsWithTag("name:proto_raw")
+			var protoField datasource.FieldAccessor
+			var protoFieldName string
+
+			if len(protos) == 1 {
+				if protos[0].Size() != 2 {
+					return nil, fmt.Errorf("proto size expected to be 2 bytes")
+				}
+				protoFieldName = strings.TrimSuffix(protos[0].Name(), "_raw")
+				protoField, err = in.AddSubField(protoFieldName, api.Kind_String)
+				if err != nil {
+					return nil, err
+				}
+			} else if len(protos) > 1 {
+				return nil, fmt.Errorf("expected at most 1 proto_raw field, found %d", len(protos))
+			}
+
 			endpointF, err := in.AddSubField("endpoint", api.Kind_String,
 				datasource.WithAnnotations(map[string]string{
 					json.SkipFieldAnnotation: "true",
@@ -413,6 +431,15 @@ var replacers = []replacer{
 
 				port, _ := ports[0].Uint16(entry)
 				endpointF.PutString(entry, fmt.Sprintf("%s:%d", addrStr, port))
+
+				if len(protos) == 1 {
+					protoNumber, _ := protos[0].Uint16(entry)
+					protoName, exist := protocols.GetProtocolNameByNumber(int(protoNumber))
+					if !exist {
+						protoName = fmt.Sprintf("proto#%d", protoNumber)
+					}
+					protoField.PutString(entry, protoName)
+				}
 				return nil
 			}, nil
 		},

gadgets/snapshot_socket/program.bpf.c
@@ -109,7 +109,7 @@ socket_bpf_seq_write(struct seq_file *seq, __u16 family, __u16 proto,
 		return;
 	}
 
-	entry.src.proto = entry.dst.proto = proto;
+	entry.src.proto_raw = entry.dst.proto_raw = proto;
 	entry.src.port = bpf_htons(srcp);
 	entry.dst.port = bpf_htons(destp);
 	entry.state = state;

gadgets/top_tcp/program.bpf.c
@@ -78,7 +78,7 @@ static int probe_ip(bool receiving, struct sock *sk, size_t size)
 	bpf_get_current_comm(&ip_key.comm, sizeof(ip_key.comm));
 	ip_key.src.port = BPF_CORE_READ(sk, __sk_common.skc_num);
 	ip_key.dst.port = bpf_ntohs(BPF_CORE_READ(sk, __sk_common.skc_dport));
-	ip_key.src.proto = ip_key.dst.proto = IPPROTO_TCP;
+	ip_key.src.proto_raw = ip_key.dst.proto_raw = IPPROTO_TCP;
 	if (family == AF_INET) {
 		ip_key.src.version = ip_key.dst.version = 4;
 	} else {

gadgets/top_tcp/test/integration/top_tcp_test.go
@@ -110,13 +110,13 @@ func TestTopTcp(t *testing.T) {
 						Addr:    "127.0.0.1",
 						Version: 4,
 						Port:    utils.NormalizedInt,
-						Proto:   6,
+						Proto:   "TCP",
 					},
 					Dst: utils.L4Endpoint{
 						Addr:    "127.0.0.1",
 						Version: 4,
 						Port:    utils.NormalizedInt,
-						Proto:   6,
+						Proto:   "TCP",
 					},
 					Comm: "curl",
 

gadgets/trace_bind/program.bpf.c
@@ -208,7 +208,8 @@ static int probe_exit(struct pt_regs *ctx, short ver)
 
 	event->addr.port = sport;
 	event->addr.version = ver;
-	event->addr.proto = BPF_CORE_READ_BITFIELD_PROBED(sock, sk_protocol);
+	event->addr.proto_raw =
+		BPF_CORE_READ_BITFIELD_PROBED(sock, sk_protocol);
 
 	if (ver == 4) {
 		bpf_probe_read_kernel(&event->addr.addr_raw.v4,

gadgets/trace_bind/test/integration/trace_bind_test.go
@@ -96,7 +96,7 @@ func TestTraceBind(t *testing.T) {
 					Addr:    "127.0.0.1",
 					Version: 4,
 					Port:    9090,
-					Proto:   6,
+					Proto:   "TCP",
 				},
 				Comm: "nc",
 				Opts: "REUSEADDRESS",

gadgets/trace_dns/program.bpf.c
@@ -287,7 +287,7 @@ int ig_trace_dns(struct __sk_buff *skb)
 	event.data_len = skb->len;
 	event.dns_off = dns_off;
 	event.pkt_type_raw = skb->pkt_type;
-	event.src.proto = event.dst.proto = proto;
+	event.src.proto_raw = event.dst.proto_raw = proto;
 	event.src.port = sport;
 	event.dst.port = dport;
 

gadgets/trace_dns/test/integration/trace_dns_test.go
@@ -139,13 +139,13 @@ func TestTraceDNS(t *testing.T) {
 						Addr:    clientIP,
 						Version: 4,
 						Port:    utils.NormalizedInt,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Dst: utils.L4Endpoint{
 						Addr:    serverIP,
 						Version: 4,
 						Port:    53,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Pcomm:    "sh",
 					Comm:     "nslookup",
@@ -176,13 +176,13 @@ func TestTraceDNS(t *testing.T) {
 						Addr:    serverIP,
 						Version: 4,
 						Port:    53,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Dst: utils.L4Endpoint{
 						Addr:    clientIP,
 						Version: 4,
 						Port:    utils.NormalizedInt,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Pcomm:     "sh",
 					Comm:      "nslookup",
@@ -214,13 +214,13 @@ func TestTraceDNS(t *testing.T) {
 						Addr:    clientIP,
 						Version: 4,
 						Port:    utils.NormalizedInt,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Dst: utils.L4Endpoint{
 						Addr:    serverIP,
 						Version: 4,
 						Port:    53,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Pcomm:    "sh",
 					Comm:     "nslookup",
@@ -251,13 +251,13 @@ func TestTraceDNS(t *testing.T) {
 						Addr:    serverIP,
 						Version: 4,
 						Port:    53,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Dst: utils.L4Endpoint{
 						Addr:    clientIP,
 						Version: 4,
 						Port:    utils.NormalizedInt,
-						Proto:   17,
+						Proto:   "UDP",
 					},
 					Pcomm:     "sh",
 					Comm:      "nslookup",

commit 39b2d4fa226536832a425f1d45f7efa44a88f676

operators/formatters: Hide endpoint subfield proto
Fixes: d5e0f1198bd74c9ef66dea1c53a84ea94b40cecc

Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

pkg/operators/formatters/formatters.go
@@ -403,7 +403,7 @@ var replacers = []replacer{
 					return nil, fmt.Errorf("proto size expected to be 2 bytes")
 				}
 				protoFieldName = strings.TrimSuffix(protos[0].Name(), "_raw")
-				protoField, err = in.AddSubField(protoFieldName, api.Kind_String)
+				protoField, err = in.AddSubField(protoFieldName, api.Kind_String, datasource.WithFlags(datasource.FieldFlagHidden))
 				if err != nil {
 					return nil, err
 				}