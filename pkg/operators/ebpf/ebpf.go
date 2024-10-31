inspektor-gadget/inspektor-gadget

commit f6aedf67c48501ce5a8977656e2f55fa009e6f0c

pkg/operators/ebpf: introduce map iterators
Signed-off-by: Michael Friese <mfriese@microsoft.com>

pkg/operators/ebpf/ebpf.go
@@ -23,6 +23,7 @@ import (
 	"io"
 	"os"
 	"reflect"
+	"slices"
 	"strings"
 	"sync"
 
@@ -112,6 +113,7 @@ func (o *ebpfOperator) InstantiateImageOperator(
 		structs:      make(map[string]*Struct),
 		snapshotters: make(map[string]*Snapshotter),
 		params:       make(map[string]*param),
+		mapIters:     make(map[string]*mapIter),
 
 		containers: make(map[string]*containercollection.Container),
 
@@ -142,6 +144,11 @@ func (o *ebpfOperator) InstantiateImageOperator(
 		return nil, fmt.Errorf("initializing ebpf gadget: %w", err)
 	}
 
+	err = newInstance.evaluateMapParams(paramValues)
+	if err != nil {
+		return nil, fmt.Errorf("evaluating map params: %w", err)
+	}
+
 	return newInstance, nil
 }
 
@@ -158,6 +165,7 @@ type ebpfInstance struct {
 	tracers      map[string]*Tracer
 	structs      map[string]*Struct
 	snapshotters map[string]*Snapshotter
+	mapIters     map[string]*mapIter
 	params       map[string]*param
 	paramValues  map[string]string
 
@@ -212,6 +220,11 @@ func (i *ebpfInstance) analyze() error {
 		// 	validator:    i.validateGlobalConstVoidPtrVar,
 		// 	populateFunc: i.populateMap,
 		// },
+		{
+			prefixFunc:   hasPrefix(mapIterPrefix),
+			validator:    i.validateGlobalConstVoidPtrVar,
+			populateFunc: i.populateMapIter,
+		},
 		{
 			prefixFunc: func(s string) (string, bool) {
 				// Exceptions for backwards-compatibility
@@ -362,6 +375,38 @@ func (i *ebpfInstance) register(gadgetCtx operators.GadgetContext) error {
 		m.accessor = accessor
 		m.ds = ds
 	}
+	for name, m := range i.mapIters {
+		fields := make([]*Field, 0)
+		ds, err := gadgetCtx.RegisterDataSource(datasource.TypeArray, name)
+		if err != nil {
+			return fmt.Errorf("adding mapiter datasource: %w", err)
+		}
+		staticFields := make([]datasource.StaticField, 0, len(fields))
+		for _, field := range i.structs[m.keyStructName].Fields {
+			// check whether role tag needs to be added
+			if !slices.Contains(field.Tags, "role:key") {
+				field.Tags = append(field.Tags, "role:key")
+			}
+			staticFields = append(staticFields, field)
+		}
+		accessor, err := ds.AddStaticFields(i.structs[m.keyStructName].Size, staticFields)
+		if err != nil {
+			return fmt.Errorf("adding fields for datasource: %w", err)
+		}
+		m.keyAccessor = accessor
+
+		staticFields = make([]datasource.StaticField, 0, len(fields))
+		for _, field := range i.structs[m.valStructName].Fields {
+			staticFields = append(staticFields, field)
+		}
+		accessor, err = ds.AddStaticFields(i.structs[m.valStructName].Size, staticFields)
+		if err != nil {
+			return fmt.Errorf("adding fields for datasource: %w", err)
+		}
+		m.valAccessor = accessor
+
+		m.ds = ds
+	}
 	return nil
 }
 
@@ -374,6 +419,9 @@ func (i *ebpfInstance) ExtraParams(gadgetCtx operators.GadgetContext) api.Params
 	for _, p := range i.params {
 		res = append(res, p.Param)
 	}
+
+	// MapIter params
+	res = append(res, i.mapParams()...)
 	return res
 }
 
@@ -622,6 +670,12 @@ func (i *ebpfInstance) Start(gadgetCtx operators.GadgetContext) error {
 	if err != nil {
 		i.Close()
 		return fmt.Errorf("running snapshotters: %w", err)
+	}
+
+	err = i.runMapIterators()
+	if err != nil {
+		i.Close()
+		return fmt.Errorf("running map iterators: %w", err)
 	}
 
 	return nil

pkg/operators/ebpf/maps.go
@@ -15,11 +15,40 @@
 package ebpfoperator
 
 import (
+	"errors"
+	"fmt"
 	"reflect"
+	"strings"
+	"time"
+	"unsafe"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/btf"
+	"golang.org/x/sys/unix"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
 )
+
+const (
+	ParamMapIterInterval = "map-fetch-interval"
+	ParamMapIterCount    = "map-fetch-count"
+)
+
+type mapIter struct {
+	name          string
+	mapName       string
+	keyStructName string
+	valStructName string
+
+	ds          datasource.DataSource
+	keyAccessor datasource.FieldAccessor
+	valAccessor datasource.FieldAccessor
+
+	interval time.Duration
+	count    int
+}
 
 func (i *ebpfInstance) populateMap(t btf.Type, varName string) error {
 	i.logger.Debugf("populating map %q", varName)
@@ -35,5 +64,220 @@ func (i *ebpfInstance) populateMap(t btf.Type, varName string) error {
 	// Set variable to nil pointer to map, so it's present
 	var nilVal *ebpf.Map
 	i.gadgetCtx.SetVar(varName, nilVal)
+	return nil
+}
+
+func (i *ebpfInstance) mapParams() api.Params {
+	if len(i.mapIters) == 0 {
+		return nil
+	}
+	return api.Params{
+		{
+			Key:          ParamMapIterInterval,
+			Description:  "interval in which to iterate over maps",
+			DefaultValue: "1000ms",
+			TypeHint:     api.TypeString,
+			Title:        "Map fetch interval",
+		},
+		{
+			Key:          ParamMapIterCount,
+			Description:  "number of map fetch cycles - use 0 for unlimited",
+			DefaultValue: "0",
+			TypeHint:     api.TypeInt,
+			Title:        "Map fetch count",
+		},
+	}
+}
+
+func (i *ebpfInstance) evaluateMapParams(paramValues api.ParamValues) error {
+	if len(i.mapIters) == 0 {
+		return nil
+	}
+
+	globalDuration := time.Duration(0)
+	globalCount := 0
+
+	durations, err := apihelpers.GetDurationValuesPerDataSource(paramValues[ParamMapIterInterval])
+	if err != nil {
+		return fmt.Errorf("evaluating map fetch interval: %w", err)
+	}
+	for dsName, duration := range durations {
+		if dsName == "" {
+			globalDuration = duration
+			continue
+		}
+		iter, ok := i.mapIters[dsName]
+		if !ok {
+			return fmt.Errorf("map fetch interval found for non-existing iterator %q", dsName)
+		}
+		iter.interval = duration
+	}
+
+	counts, err := apihelpers.GetIntValuesPerDataSource(paramValues[ParamMapIterCount])
+	if err != nil {
+		return fmt.Errorf("evaluating map fetch interval: %w", err)
+	}
+	for dsName, count := range counts {
+		if dsName == "" {
+			globalCount = count
+			continue
+		}
+		iter, ok := i.mapIters[dsName]
+		if !ok {
+			return fmt.Errorf("map fetch count found for non-existing iterator %q", dsName)
+		}
+		iter.count = count
+	}
+
+	for _, iter := range i.mapIters {
+		if iter.interval == 0 {
+			iter.interval = globalDuration
+		}
+		if iter.count == 0 {
+			iter.count = globalCount
+		}
+		iter.ds.AddAnnotation("fetch-count", fmt.Sprintf("%d", iter.count))
+		iter.ds.AddAnnotation("fetch-interval", iter.interval.String())
+	}
+	return nil
+}
+
+func (i *ebpfInstance) runMapIterators() error {
+	for _, iter := range i.mapIters {
+		iterMap, ok := i.collection.Maps[iter.mapName]
+		if !ok {
+			return fmt.Errorf("map %q not found", iter.mapName)
+		}
+		fetch := func() {
+			p, err := iter.ds.NewPacketArray()
+			if err != nil {
+				i.logger.Errorf("error creating packet for map iterator: %v", err)
+				return
+			}
+
+			var prevKey []byte
+
+			batchSize := 100 // discuss
+
+			keySize := int(iterMap.KeySize())
+			valSize := int(iterMap.ValueSize())
+			for {
+				keys := make([]byte, keySize*batchSize)
+				vals := make([]byte, valSize*batchSize)
+				keysPtr := Pointer{ptr: unsafe.Pointer(&keys[0])}
+				valuesPtr := Pointer{ptr: unsafe.Pointer(&vals[0])}
+
+				// TODO: use cilium lib once raw byte access has been added
+				// TODO: open PR to actually make that happen
+				nk := make([]byte, keySize)
+				attr := MapLookupBatchAttr{
+					MapFd:    uint32(iterMap.FD()),
+					Keys:     keysPtr,
+					Values:   valuesPtr,
+					Count:    uint32(batchSize),
+					OutBatch: Pointer{ptr: unsafe.Pointer(&nk[0])},
+				}
+				if prevKey != nil {
+					attr.InBatch = Pointer{ptr: unsafe.Pointer(&prevKey[0])}
+				}
+
+				_, err := BPF(BPF_MAP_LOOKUP_AND_DELETE_BATCH, unsafe.Pointer(&attr), unsafe.Sizeof(attr))
+				if err != nil && !errors.Is(err, unix.ENOENT) {
+					i.logger.Warnf("error from map iterator: %v", err)
+					break
+				}
+				n := int(attr.Count)
+
+				prevKey = nk
+				for c := range n {
+					d := p.New()
+					iter.keyAccessor.Set(d, keys[keySize*c:keySize*(c+1)])
+					iter.valAccessor.Set(d, vals[valSize*c:valSize*(c+1)])
+					p.Append(d)
+				}
+				if errors.Is(err, unix.ENOENT) { // ebpf.ErrKeyNotExist when doing this with cilium/ebpf later on
+					break
+				}
+			}
+			iter.ds.EmitAndRelease(p)
+		}
+		go func() {
+			if iter.interval == 0 {
+				// Only a single time; is this really useful?
+				fetch()
+				return
+			}
+			ctr := 0
+			ticker := time.NewTicker(iter.interval)
+			for {
+				select {
+				case <-i.gadgetCtx.Context().Done():
+					return
+				case <-ticker.C:
+					fetch()
+					ctr++
+					if iter.count > 0 && ctr >= iter.count {
+						// TODO: close DS
+						return
+					}
+				}
+			}
+		}()
+	}
+	return nil
+}
+
+func (i *ebpfInstance) populateMapIter(t btf.Type, varName string) error {
+	i.logger.Debugf("populating mapiter %q", varName)
+
+	info := strings.Split(varName, typeSplitter)
+	if len(info) != 2 {
+		return fmt.Errorf("invalid name for gadget_mapiter type: %q", varName)
+	}
+
+	name := info[0]
+	mapName := info[1]
+
+	if _, ok := i.mapIters[name]; ok {
+		return fmt.Errorf("duplicate map iterator %q", varName)
+	}
+
+	// Get types
+	iterMap, ok := i.collectionSpec.Maps[mapName]
+	if !ok {
+		return fmt.Errorf("map %q not found in eBPF object", mapName)
+	}
+
+	keyStruct, ok := iterMap.Key.(*btf.Struct)
+	if !ok {
+		return fmt.Errorf("map %q key is not a struct", mapName)
+	}
+
+	valStruct, ok := iterMap.Value.(*btf.Struct)
+	if !ok {
+		return fmt.Errorf("map %q value is not a struct", mapName)
+	}
+
+	if iterMap.KeySize != keyStruct.Size || iterMap.ValueSize != valStruct.Size {
+		return fmt.Errorf("key/value sizes of map %q does not match size of structs", mapName)
+	}
+
+	err := i.populateStructDirect(keyStruct)
+	if err != nil {
+		return fmt.Errorf("populating key struct for map iter %q: %w", varName, err)
+	}
+
+	err = i.populateStructDirect(valStruct)
+	if err != nil {
+		return fmt.Errorf("populating value struct for map iter %q: %w", varName, err)
+	}
+
+	iter := &mapIter{
+		name:          name,
+		mapName:       mapName,
+		keyStructName: keyStruct.Name,
+		valStructName: valStruct.Name,
+	}
+	i.mapIters[name] = iter
 	return nil
 }

pkg/operators/ebpf/rawbpf.go
@@ -1,0 +1,57 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpfoperator
+
+import (
+	"runtime"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+// This is a patched version of their counterparts from cilium/ebpf; the upstream code doesn't allow reading bytes
+// from maps but instead wants to deserialize itself. However, we need to deserialize with our own libraries and
+// thus get the raw data from the map in a performant way.
+
+const (
+	BPF_MAP_LOOKUP_AND_DELETE_BATCH uintptr = 25
+)
+
+type Pointer struct {
+	ptr unsafe.Pointer
+}
+
+type MapLookupBatchAttr struct {
+	InBatch   Pointer
+	OutBatch  Pointer
+	Keys      Pointer
+	Values    Pointer
+	Count     uint32
+	MapFd     uint32
+	ElemFlags uint64
+	Flags     uint64
+}
+
+func BPF(cmd uintptr, attr unsafe.Pointer, size uintptr) (uintptr, error) {
+	r1, _, errNo := unix.Syscall(unix.SYS_BPF, uintptr(cmd), uintptr(attr), size)
+	runtime.KeepAlive(attr)
+
+	var err error
+	if errNo != 0 {
+		err = errNo
+	}
+
+	return r1, err
+}

pkg/operators/ebpf/types.go
@@ -29,6 +29,8 @@ const (
 	// include/gadget/buffer.h.
 	tracerMapPrefix = "gadget_map_tracer_"
 
+	mapIterPrefix = "gadget_mapiter_"
+
 	// Prefix used to mark variables used by operators
 	varPrefix = "gadget_var_"
 )

commit 26e5a591aaf3243ed1c2419df456d48c4718cc30

operators/ebpf: Fix map iterators waiting logic
The previous logic was relying on the gadget context, but it was never
being closed. This caused the map iterator to keep reading the map
forever, producing the following error after a timeout:

```bash
 sudo ig run mygadget:latest --verify-image=false -o json --timeout 2WARN[0001] image signature verification is disabled due to using corresponding option
WARN[0001] image signature verification is disabled due to using corresponding option
[]
[]
WARN[0004] error from map iterator: bad file descriptor
[]
WARN[0005] error from map iterator: bad file descriptor
[]
```
This logic switches to only rely on the Stop() operation and an internal
channel.

This bug only appeared after 85b756c881d3 ("operators/ebpf: Fix race condition with wasm operator")
as it introduced a waiting group, but the bug was present before.

Fixes: f6aedf67c485 ("pkg/operators/ebpf: introduce map iterators")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/ebpf/ebpf.go
@@ -108,6 +108,7 @@ func (o *ebpfOperator) InstantiateImageOperator(
 
 	newInstance := &ebpfInstance{
 		gadgetCtx: gadgetCtx, // context usually should not be stored, but should we really carry it through all funcs?
+		done:      make(chan struct{}),
 
 		logger:  gadgetCtx.Logger(),
 		program: program,
@@ -190,6 +191,7 @@ type ebpfInstance struct {
 	stackIdMap *ebpf.Map
 
 	gadgetCtx operators.GadgetContext
+	done      chan struct{}
 
 	// used to be sure all tracers are done before returning from Stop()
 	wg sync.WaitGroup
@@ -533,7 +535,7 @@ func (i *ebpfInstance) tracePipe(gadgetCtx operators.GadgetContext) error {
 		return fmt.Errorf("opening trace_pipe: %w", err)
 	}
 	go func() {
-		<-gadgetCtx.Context().Done()
+		<-i.done
 		tracePipe.Close()
 	}()
 	go func() {
@@ -735,6 +737,8 @@ func (i *ebpfInstance) Stop(gadgetCtx operators.GadgetContext) error {
 }
 
 func (i *ebpfInstance) Close() {
+	close(i.done)
+
 	for _, t := range i.tracers {
 		t.close()
 	}

pkg/operators/ebpf/maps.go
@@ -213,7 +213,7 @@ func (i *ebpfInstance) runMapIterators() error {
 			ticker := time.NewTicker(iter.interval)
 			for {
 				select {
-				case <-i.gadgetCtx.Context().Done():
+				case <-i.done:
 					return
 				case <-ticker.C:
 					fetch()