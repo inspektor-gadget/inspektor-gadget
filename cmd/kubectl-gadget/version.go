c159bbd89d1e18e34eeda90aedbd3a87adec0ea1
Priyansu Rout <priyansurout192004@gmail.com>
2024-10-31 13:14:07 +0530

    enhance version command output json format

--- cmd/kubectl-gadget/version.go
+++ cmd/kubectl-gadget/version.go
@@ -15,16 +15,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/spf13/cobra"
 
 	"github.com/inspektor-gadget/inspektor-gadget/cmd/kubectl-gadget/utils"
 	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
 	grpcruntime "github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/grpc"
+	"github.com/spf13/cobra"
 )
 
+// VersionInfo represents the structure for version information output
+type VersionInfo struct {
+	ClientVersion *Version `json:"clientVersion,omitempty"`
+	ServerVersion *Version `json:"serverVersion,omitempty"`
+}
+
+// Version contains detailed version information
+type Version struct {
+	Version string `json:"version"`
+	Status  string `json:"status,omitempty"`
+}
+
 func init() {
+	versionCmd.Flags().StringP("output", "o", "", "Output format. One of: json|''")
 	rootCmd.AddCommand(versionCmd)
 }
 
@@ -33,8 +46,19 @@ var versionCmd = &cobra.Command{
 	Short:        "Show version",
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Printf("Client version: v%s\n", version.Version())
+		outputFormat, err := cmd.Flags().GetString("output")
+		if err != nil {
+			return fmt.Errorf("error getting output format: %w", err)
+		}
 
+		// Initialize version info structure
+		versionInfo := &VersionInfo{
+			ClientVersion: &Version{
+				Version: version.Version().String(),
+			},
+		}
+
+		// Get server version information
 		gadgetNamespaces, err := utils.GetRunningGadgetNamespaces()
 		if err != nil {
 			return fmt.Errorf("searching for running Inspektor Gadget instances: %w", err)
@@ -42,23 +66,50 @@ var versionCmd = &cobra.Command{
 
 		switch len(gadgetNamespaces) {
 		case 0:
-			fmt.Println("Server version:", "not installed")
-			return nil
+			versionInfo.ServerVersion = &Version{
+				Status: "not installed",
+			}
 		case 1:
 			// Exactly one running gadget instance found, use it
 			runtimeGlobalParams.Set(grpcruntime.ParamGadgetNamespace, gadgetNamespaces[0])
+			info, err := grpcRuntime.InitDeployInfo()
+			if err != nil {
+				return fmt.Errorf("loading deploy info: %w", err)
+			}
+			versionInfo.ServerVersion = &Version{
+				Version: info.ServerVersion,
+			}
 		default:
-			// Multiple running gadget instances found, error out
 			return fmt.Errorf("multiple running Inspektor Gadget instances found in following namespaces: %v", gadgetNamespaces)
 		}
 
-		info, err := grpcRuntime.InitDeployInfo()
-		if err != nil {
-			return fmt.Errorf("loading deploy info: %w", err)
+		// Output based on format
+		switch outputFormat {
+		case "json":
+			return outputJSON(versionInfo)
+		case "":
+			return outputDefault(versionInfo)
+		default:
+			return fmt.Errorf("invalid output format: %s", outputFormat)
 		}
-
-		fmt.Printf("Server version: v%s\n", info.ServerVersion)
-
-		return nil
 	},
+}
+
+func outputJSON(info *VersionInfo) error {
+	output, err := json.MarshalIndent(info, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshaling version info: %w", err)
+	}
+	fmt.Println(string(output))
+	return nil
+}
+
+func outputDefault(info *VersionInfo) error {
+	fmt.Printf("Client version: v%s\n", info.ClientVersion.Version)
+	if info.ServerVersion.Status != "" {
+		fmt.Printf("Server version: %s\n", info.ServerVersion.Status)
+	} else {
+		fmt.Printf("Server version: v%s\n", info.ServerVersion.Version)
+	}
+	return nil
 }
