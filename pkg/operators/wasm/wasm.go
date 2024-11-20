4b5f01296e64a72e2f4748436d370d45322efa3b
Francis Laniel <flaniel@linux.microsoft.com>
2024-11-20 17:02:57 +0100

    wip: operators/wasm: Add functions to interact with perf buffer.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- pkg/operators/wasm/wasm.go
+++ pkg/operators/wasm/wasm.go
@@ -220,6 +220,7 @@ func (i *wasmOperatorInstance) init(
 	i.addParamsFuncs(env)
 	i.addConfigFuncs(env)
 	i.addMapFuncs(env)
+	i.addPerfFuncs(env)
 
 	if _, err := env.Instantiate(ctx); err != nil {
 		return fmt.Errorf("instantiating host module: %w", err)
