inspektor-gadget/inspektor-gadget

commit a46f5c2efc5541447ff0c4c41efb46e0ec334628

ig-k8s: Fix server version message
Use the version of the server instead of the client.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

gadget-container/gadgettracermanager/main.go
@@ -37,6 +37,7 @@ import (
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/status"
 
+	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
 	// Import this early to set the environment variable before any other package is imported
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/environment/k8s"
 	instancemanager "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/instance-manager"
@@ -119,6 +120,8 @@ func init() {
 }
 
 func main() {
+	log.Infof("Inspektor Gadget version: %s", version.Version().String())
+
 	flag.Parse()
 
 	if flag.NArg() > 0 {

charts/gadget/templates/daemonset.yaml
@@ -74,8 +74,6 @@ spec:
                   fieldPath: metadata.uid
             - name: GADGET_IMAGE
               value: "{{ .Values.image.repository }}"
-            - name: INSPEKTOR_GADGET_VERSION
-              value: {{ include "gadget.image.tag" . | quote }}
             - name: INSPEKTOR_GADGET_OPTION_HOOK_MODE
               value: {{ .Values.config.hookMode | quote }}
             - name: INSPEKTOR_GADGET_OPTION_FALLBACK_POD_INFORMER

cmd/kubectl-gadget/deploy.go
@@ -60,7 +60,6 @@ import (
 
 	commonutils "github.com/inspektor-gadget/inspektor-gadget/cmd/common/utils"
 	"github.com/inspektor-gadget/inspektor-gadget/cmd/kubectl-gadget/utils"
-	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/config/gadgettracermanagerconfig"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/k8sutil"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/resources"
@@ -672,8 +671,6 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 				switch gadgetContainer.Env[i].Name {
 				case "GADGET_IMAGE":
 					gadgetContainer.Env[i].Value = image
-				case "INSPEKTOR_GADGET_VERSION":
-					gadgetContainer.Env[i].Value = version.Version().String()
 				case "INSPEKTOR_GADGET_OPTION_HOOK_MODE":
 					gadgetContainer.Env[i].Value = hookMode
 				case "INSPEKTOR_GADGET_OPTION_FALLBACK_POD_INFORMER":

gadget-container/entrypoint/entrypoint.go
@@ -261,8 +261,6 @@ func main() {
 		log.Infof("%s: %v", k, v)
 	}
 
-	log.Infof("Inspektor Gadget version: %s", os.Getenv("INSPEKTOR_GADGET_VERSION"))
-
 	if hasGadgetPullSecret() {
 		err = prepareGadgetPullSecret()
 		if err != nil {

pkg/resources/manifests/deploy.yaml
@@ -189,8 +189,6 @@ spec:
                   fieldPath: metadata.uid
             - name: GADGET_IMAGE
               value: "ghcr.io/inspektor-gadget/inspektor-gadget"
-            - name: INSPEKTOR_GADGET_VERSION
-              value: "latest"
             - name: INSPEKTOR_GADGET_OPTION_HOOK_MODE
               value: "auto"
             - name: INSPEKTOR_GADGET_OPTION_FALLBACK_POD_INFORMER

commit a9a920c01b7527207b25ed9e52fcf742bcea13c9

ig-k8s: Fix fix server version message
Print the version only when running with -serve, otherwise it messes up
some json output used by the advise network-policy gadget.

Fixes: a46f5c2efc55 ("ig-k8s: Fix server version message")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

gadget-container/gadgettracermanager/main.go
@@ -120,8 +120,6 @@ func init() {
 }
 
 func main() {
-	log.Infof("Inspektor Gadget version: %s", version.Version().String())
-
 	flag.Parse()
 
 	if flag.NArg() > 0 {
@@ -284,6 +282,8 @@ func main() {
 	}
 
 	if serve {
+		log.Infof("Inspektor Gadget version: %s", version.Version().String())
+
 		if experimental.Enabled() {
 			log.Info("Experimental features enabled")
 		}