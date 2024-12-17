286e8e2d66b42803372a7ceb8602adf8128e7045
Francis Laniel <flaniel@linux.microsoft.com>
2024-12-17 08:22:48 +0100

    treewide: Format eBPF code using clang-format-18.
    CI uses clang-format-18 to check that eBPF code is correctly formatted [1].
    Let's use the same to fix errors discovered by CI.
    
    Fixes: de894c6c4d60 ("tree-wide: Fix clang-format target and apply it")
    Fixes: https://github.com/inspektor-gadget/inspektor-gadget/actions/runs/12349927639/job/34461748991?pr=3803#step:3:151
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
    [1]: https://github.com/inspektor-gadget/inspektor-gadget/actions/runs/12349927639/job/34461748991?pr=3803#step:3:91

--- pkg/gadgets/top/file/tracer/bpf/stat.h
+++ pkg/gadgets/top/file/tracer/bpf/stat.h
@@ -16,12 +16,12 @@
 #define S_ISGID 0002000
 #define S_ISVTX 0001000
 
-#define S_ISLNK(m) (((m)&S_IFMT) == S_IFLNK)
-#define S_ISREG(m) (((m)&S_IFMT) == S_IFREG)
-#define S_ISDIR(m) (((m)&S_IFMT) == S_IFDIR)
-#define S_ISCHR(m) (((m)&S_IFMT) == S_IFCHR)
-#define S_ISBLK(m) (((m)&S_IFMT) == S_IFBLK)
-#define S_ISFIFO(m) (((m)&S_IFMT) == S_IFIFO)
-#define S_ISSOCK(m) (((m)&S_IFMT) == S_IFSOCK)
+#define S_ISLNK(m) (((m) & S_IFMT) == S_IFLNK)
+#define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
+#define S_ISDIR(m) (((m) & S_IFMT) == S_IFDIR)
+#define S_ISCHR(m) (((m) & S_IFMT) == S_IFCHR)
+#define S_ISBLK(m) (((m) & S_IFMT) == S_IFBLK)
+#define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)
+#define S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK)
 
 #endif /* __STAT_H */
