inspektor-gadget/inspektor-gadget

commit b7d6c1bec7562f54b82977adbbdd7010edcd7ccb

builtin trace-dns: use new fields from socket enricher
This adds the new following fields in the builtin trace-dns gadget:
- ppid
- pcomm
- cwd
- exepath

Example of output:
```
$ go run -exec 'sudo -E' ./cmd/ig/... trace dns
INFO[0000] Experimental features enabled
WARN[0000] Ignoring runtime "cri-o" with non-existent socketPath "/run/crio/crio.sock"
WARN[0000] Ignoring runtime "podman" with non-existent socketPath "/run/podman/podman.sock"
RUNTIME.CONTAINERN… PID         TID         PPID       COMM       PCOMM      CWD                       EXEPATH                   QR TYPE      QTYPE      NAME               RCODE        NUMA…
ubuntu              1619798     1619798     1555268    wget       bash       /tmp                      /usr/bin/wget             Q  OUTGOING  A          www.wikipedia.org.              0
ubuntu              1619798     1619798     1555268    wget       bash       /tmp                      /usr/bin/wget             Q  OUTGOING  AAAA       www.wikipedia.org.              0
ubuntu              1619798     1619798     1555268    wget       bash       /tmp                      /usr/bin/wget             R  HOST      A          www.wikipedia.org. No Error     2
ubuntu              1619798     1619798     1555268    wget       bash       /tmp                      /usr/bin/wget             R  HOST      AAAA       www.wikipedia.org. No Error     2
```

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/gadgets/trace/dns/tracer/bpf/dns.c
@@ -62,8 +62,16 @@ static __always_inline bool is_dns_port(__u16 port)
 	return false;
 }
 
-// we need this to make sure the compiler doesn't remove our struct
-const struct event_t *unusedevent __attribute__((unused));
+// Cannot use gadget_reserve_buf() because this does not support
+// bpf_perf_event_output with packet appended
+static const struct event_t empty_event = {};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(max_entries, 1);
+	__uint(key_size, sizeof(__u32));
+	__uint(value_size, sizeof(struct event_t));
+} tmp_events SEC(".maps");
 
 struct {
 	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
@@ -127,10 +135,11 @@ struct {
 SEC("socket1")
 int ig_trace_dns(struct __sk_buff *skb)
 {
-	struct event_t event;
+	struct event_t *event;
 	__u16 sport, dport, l4_off, dns_off, h_proto, id;
 	__u8 proto;
 	int i;
+	int zero = 0;
 
 	// Do a first pass only to extract the port and drop the packet if it's not DNS
 	h_proto = load_half(skb, offsetof(struct ethhdr, h_proto));
@@ -213,38 +222,42 @@ int ig_trace_dns(struct __sk_buff *skb)
 
 	// Initialize event here only after we know we're interested in this packet to avoid
 	// spending useless cycles.
-	__builtin_memset(&event, 0, sizeof(event));
+	bpf_map_update_elem(&tmp_events, &zero, &empty_event, BPF_NOEXIST);
+	event = bpf_map_lookup_elem(&tmp_events, &zero);
+	if (!event)
+		return 0;
+	bpf_map_delete_elem(&tmp_events, &zero);
 
-	event.netns = skb->cb[0]; // cb[0] initialized by dispatcher.bpf.c
-	event.timestamp = bpf_ktime_get_boot_ns();
-	event.proto = proto;
-	event.dns_off = dns_off;
-	event.pkt_type = skb->pkt_type;
-	event.sport = sport;
-	event.dport = dport;
+	event->netns = skb->cb[0]; // cb[0] initialized by dispatcher.bpf.c
+	event->timestamp = bpf_ktime_get_boot_ns();
+	event->proto = proto;
+	event->dns_off = dns_off;
+	event->pkt_type = skb->pkt_type;
+	event->sport = sport;
+	event->dport = dport;
 
 	// The packet is DNS: Do a second pass to extract all the information we need
 	switch (h_proto) {
 	case ETH_P_IP:
-		event.af = AF_INET;
-		event.daddr_v4 = load_word(
+		event->af = AF_INET;
+		event->daddr_v4 = load_word(
 			skb, ETH_HLEN + offsetof(struct iphdr, daddr));
-		event.saddr_v4 = load_word(
+		event->saddr_v4 = load_word(
 			skb, ETH_HLEN + offsetof(struct iphdr, saddr));
 		// load_word converts from network to host endianness. Convert back to
 		// network endianness because inet_ntop() requires it.
-		event.daddr_v4 = bpf_htonl(event.daddr_v4);
-		event.saddr_v4 = bpf_htonl(event.saddr_v4);
+		event->daddr_v4 = bpf_htonl(event->daddr_v4);
+		event->saddr_v4 = bpf_htonl(event->saddr_v4);
 		break;
 	case ETH_P_IPV6:
-		event.af = AF_INET6;
+		event->af = AF_INET6;
 		if (bpf_skb_load_bytes(
 			    skb, ETH_HLEN + offsetof(struct ipv6hdr, saddr),
-			    &event.saddr_v6, sizeof(event.saddr_v6)))
+			    &event->saddr_v6, sizeof(event->saddr_v6)))
 			return 0;
 		if (bpf_skb_load_bytes(
 			    skb, ETH_HLEN + offsetof(struct ipv6hdr, daddr),
-			    &event.daddr_v6, sizeof(event.daddr_v6)))
+			    &event->daddr_v6, sizeof(event->daddr_v6)))
 			return 0;
 		break;
 	}
@@ -252,13 +265,23 @@ int ig_trace_dns(struct __sk_buff *skb)
 	// Enrich event with process metadata
 	struct sockets_value *skb_val = gadget_socket_lookup(skb);
 	if (skb_val != NULL) {
-		event.mount_ns_id = skb_val->mntns;
-		event.pid = skb_val->pid_tgid >> 32;
-		event.tid = (__u32)skb_val->pid_tgid;
-		__builtin_memcpy(&event.task, skb_val->task,
-				 sizeof(event.task));
-		event.uid = (__u32)skb_val->uid_gid;
-		event.gid = (__u32)(skb_val->uid_gid >> 32);
+		event->mount_ns_id = skb_val->mntns;
+		event->pid = skb_val->pid_tgid >> 32;
+		event->tid = (__u32)skb_val->pid_tgid;
+		event->ppid = skb_val->ppid;
+		__builtin_memcpy(&event->comm, skb_val->task,
+				 sizeof(event->comm));
+		__builtin_memcpy(&event->pcomm, skb_val->ptask,
+				 sizeof(event->pcomm));
+		event->uid = (__u32)skb_val->uid_gid;
+		event->gid = (__u32)(skb_val->uid_gid >> 32);
+#ifdef WITH_LONG_PATHS
+		bpf_probe_read_kernel_str(&event->cwd, sizeof(event->cwd),
+					  skb_val->cwd);
+		bpf_probe_read_kernel_str(&event->exepath,
+					  sizeof(event->exepath),
+					  skb_val->exepath);
+#endif
 	}
 
 	// Calculate latency:
@@ -278,7 +301,7 @@ int ig_trace_dns(struct __sk_buff *skb)
 	// Skip this if skb_val == NULL (gadget_socket_lookup did not set pid_tgid we use in the query key)
 	// or if event->timestamp == 0 (kernels before 5.8 don't support bpf_ktime_get_boot_ns, and the patched
 	// version IG injects always returns zero).
-	if (skb_val != NULL && event.timestamp > 0) {
+	if (skb_val != NULL && event->timestamp > 0) {
 		union dnsflags flags;
 		flags.flags = load_half(skb, dns_off + offsetof(struct dnshdr,
 								flags));
@@ -289,18 +312,18 @@ int ig_trace_dns(struct __sk_buff *skb)
 			.pid_tgid = skb_val->pid_tgid,
 			.id = id,
 		};
-		if (qr == DNS_QR_QUERY && event.pkt_type == PACKET_OUTGOING) {
+		if (qr == DNS_QR_QUERY && event->pkt_type == PACKET_OUTGOING) {
 			bpf_map_update_elem(&query_map, &query_key,
-					    &event.timestamp, BPF_NOEXIST);
+					    &event->timestamp, BPF_NOEXIST);
 		} else if (flags.qr == DNS_QR_RESP &&
-			   event.pkt_type == PACKET_HOST) {
+			   event->pkt_type == PACKET_HOST) {
 			__u64 *query_ts =
 				bpf_map_lookup_elem(&query_map, &query_key);
 			if (query_ts != NULL) {
 				// query ts should always be less than the event ts, but check anyway to be safe.
-				if (*query_ts < event.timestamp) {
-					event.latency_ns =
-						event.timestamp - *query_ts;
+				if (*query_ts < event->timestamp) {
+					event->latency_ns =
+						event->timestamp - *query_ts;
 				}
 				bpf_map_delete_elem(&query_map, &query_key);
 			}
@@ -309,7 +332,7 @@ int ig_trace_dns(struct __sk_buff *skb)
 
 	__u64 skb_len = skb->len;
 	bpf_perf_event_output(skb, &events, skb_len << 32 | BPF_F_CURRENT_CPU,
-			      &event, sizeof(event));
+			      event, sizeof(*event));
 
 	return 0;
 }

examples/builtin-gadgets/basic/trace/dns/dns.go
@@ -53,7 +53,7 @@ func main() {
 	}
 
 	// Create tracer. In this case no parameters are passed.
-	tracer, err := tracer.NewTracer()
+	tracer, err := tracer.NewTracer(&tracer.Config{})
 	if err != nil {
 		fmt.Printf("error creating tracer: %s\n", err)
 		return

integration/ig/non-k8s/trace_dns_test.go
@@ -27,9 +27,13 @@ import (
 )
 
 func newTraceDnsSteps(cn string, dnsServerArgs string) []TestStep {
+	// dnsTesterImage (image dnstester) has the following symlink:
+	// /usr/bin/nslookup -> /bin/busybox
+	exepath := "/bin/busybox"
+
 	traceDNSCmd := &Command{
 		Name:         "TraceDns",
-		Cmd:          fmt.Sprintf("./ig trace dns -o json --runtimes=%s -c %s", *runtime, cn),
+		Cmd:          fmt.Sprintf("./ig trace dns --paths -o json --runtimes=%s -c %s", *runtime, cn),
 		StartAndStop: true,
 		ValidateOutput: func(t *testing.T, output string) {
 			expectedEntries := []*dnsTypes.Event{
@@ -44,6 +48,9 @@ func newTraceDnsSteps(cn string, dnsServerArgs string) []TestStep {
 						},
 					},
 					Qr:         dnsTypes.DNSPktTypeQuery,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Nameserver: "127.0.0.1",
 					PktType:    "OUTGOING",
@@ -63,6 +70,9 @@ func newTraceDnsSteps(cn string, dnsServerArgs string) []TestStep {
 						},
 					},
 					Qr:         dnsTypes.DNSPktTypeResponse,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Nameserver: "127.0.0.1",
 					PktType:    "HOST",
@@ -86,6 +96,9 @@ func newTraceDnsSteps(cn string, dnsServerArgs string) []TestStep {
 						},
 					},
 					Qr:         dnsTypes.DNSPktTypeQuery,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Nameserver: "127.0.0.1",
 					PktType:    "OUTGOING",
@@ -105,6 +118,9 @@ func newTraceDnsSteps(cn string, dnsServerArgs string) []TestStep {
 						},
 					},
 					Qr:         dnsTypes.DNSPktTypeResponse,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Nameserver: "127.0.0.1",
 					PktType:    "HOST",
@@ -124,6 +140,7 @@ func newTraceDnsSteps(cn string, dnsServerArgs string) []TestStep {
 				e.Timestamp = 0
 				e.MountNsID = 0
 				e.NetNsID = 0
+				e.Ppid = 0
 				e.Pid = 0
 				e.Tid = 0
 

integration/k8s/trace_dns_test.go
@@ -47,6 +47,13 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 		fmt.Sprintf("setuidgid 1000:1111 nslookup -type=aaaa fake.test.com. %s", dnsServer),
 	}
 
+	// Image from BusyboxPodRepeatCommand (image "busybox") has the
+	// following files:
+	// - /bin/busybox
+	// - /bin/nslookup
+	// They are hard links (they share the same inode)
+	exepath := "/bin/nslookup"
+
 	var extraArgs string
 	var expectedEvent eventtypes.Event
 
@@ -74,12 +81,15 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 	busyBoxIP := GetTestPodIP(t, ns, "test-pod")
 	traceDNSCmd := &Command{
 		Name:         "TraceDns",
-		Cmd:          fmt.Sprintf("%s trace dns -o json %s", DefaultTestComponent, extraArgs),
+		Cmd:          fmt.Sprintf("%s trace dns --paths -o json %s", DefaultTestComponent, extraArgs),
 		StartAndStop: true,
 		ValidateOutput: func(t *testing.T, output string) {
 			expectedEntries := []*dnsTypes.Event{
 				{
 					Event:      expectedEvent,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Qr:         dnsTypes.DNSPktTypeQuery,
 					Nameserver: dnsServer,
@@ -94,6 +104,9 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 				},
 				{
 					Event:      expectedEvent,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Qr:         dnsTypes.DNSPktTypeResponse,
 					Nameserver: dnsServer,
@@ -112,6 +125,9 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 				},
 				{
 					Event:      expectedEvent,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Qr:         dnsTypes.DNSPktTypeQuery,
 					Nameserver: dnsServer,
@@ -126,6 +142,9 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 				},
 				{
 					Event:      expectedEvent,
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "nslookup",
 					Qr:         dnsTypes.DNSPktTypeResponse,
 					Nameserver: dnsServer,
@@ -148,6 +167,9 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 				expectedEntries = append(expectedEntries,
 					&dnsTypes.Event{
 						Event:      expectedEvent,
+						Cwd:        "/",
+						Pcomm:      "sh",
+						Exepath:    exepath,
 						Comm:       "nslookup",
 						Qr:         dnsTypes.DNSPktTypeQuery,
 						Nameserver: dnsServer,
@@ -162,6 +184,9 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 					},
 					&dnsTypes.Event{
 						Event:      expectedEvent,
+						Cwd:        "/",
+						Pcomm:      "sh",
+						Exepath:    exepath,
 						Comm:       "nslookup",
 						Qr:         dnsTypes.DNSPktTypeResponse,
 						Nameserver: dnsServer,
@@ -185,6 +210,7 @@ func newTraceDnsCmd(t *testing.T, ns string, dnsServerArgs string) *Command {
 				e.ID = ""
 				e.MountNsID = 0
 				e.NetNsID = 0
+				e.Ppid = 0
 				e.Pid = 0
 				e.Tid = 0
 
@@ -250,9 +276,12 @@ func TestTraceDnsHost(t *testing.T) {
 	RunTestSteps(commandsPreTest, t)
 	dnsServer := GetTestPodIP(t, ns, "dnstester")
 
+	// On the host, nslookup is in /usr/bin.
+	exepath := "/usr/bin/nslookup"
+
 	traceDNSCmd := &Command{
 		Name:         "TraceDnsHost",
-		Cmd:          "ig trace dns -o json --host",
+		Cmd:          "ig trace dns --paths -o json --host",
 		StartAndStop: true,
 		ValidateOutput: func(t *testing.T, output string) {
 			expectedEntries := []*dnsTypes.Event{
@@ -260,6 +289,9 @@ func TestTraceDnsHost(t *testing.T) {
 					Event: eventtypes.Event{
 						Type: eventtypes.NORMAL,
 					},
+					Cwd:     "/",
+					Pcomm:   "sh",
+					Exepath: exepath,
 					// nslookup has several threads and isc-net-0000 will do the DNS
 					// request.
 					Comm:       "isc-net-0000",
@@ -275,6 +307,9 @@ func TestTraceDnsHost(t *testing.T) {
 					Event: eventtypes.Event{
 						Type: eventtypes.NORMAL,
 					},
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "isc-net-0000",
 					Qr:         dnsTypes.DNSPktTypeQuery,
 					Nameserver: dnsServer,
@@ -288,6 +323,9 @@ func TestTraceDnsHost(t *testing.T) {
 					Event: eventtypes.Event{
 						Type: eventtypes.NORMAL,
 					},
+					Cwd:        "/",
+					Pcomm:      "sh",
+					Exepath:    exepath,
 					Comm:       "isc-net-0000",
 					Qr:         dnsTypes.DNSPktTypeQuery,
 					Nameserver: dnsServer,
@@ -304,6 +342,7 @@ func TestTraceDnsHost(t *testing.T) {
 				e.ID = ""
 				e.MountNsID = 0
 				e.NetNsID = 0
+				e.Ppid = 0
 				e.Pid = 0
 				e.Tid = 0
 
@@ -322,7 +361,7 @@ func TestTraceDnsHost(t *testing.T) {
 		},
 	}
 
-	cmd := fmt.Sprintf(`sh -c 'for i in $(seq 1 30); do nslookup -type=a fake.test.com. %s; nslookup -type=aaaa fake.test.com. %s; done'`, dnsServer, dnsServer)
+	cmd := fmt.Sprintf(`sh -c 'cd / ; for i in $(seq 1 30); do nslookup -type=a fake.test.com. %s; nslookup -type=aaaa fake.test.com. %s; done'`, dnsServer, dnsServer)
 
 	commands := []TestStep{
 		CreateTestNamespaceCommand(ns),

pkg/gadget-collection/gadgets/trace/dns/gadget.go
@@ -111,7 +111,7 @@ func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
 	}
 
 	var err error
-	t.tracer, err = dnsTracer.NewTracer()
+	t.tracer, err = dnsTracer.NewTracer(&dnsTracer.Config{})
 	if err != nil {
 		trace.Status.OperationError = fmt.Sprintf("Failed to start dns tracer: %s", err)
 		return

pkg/gadgets/trace/dns/tracer/bpf/dns-common.h
@@ -2,6 +2,7 @@
 #define GADGET_DNS_COMMON_H
 
 #define TASK_COMM_LEN 16
+#define MAX_STRING_SIZE 4096
 
 struct event_t {
 	// Keep netns at the top: networktracer depends on it
@@ -11,9 +12,11 @@ struct event_t {
 	__u64 mount_ns_id;
 	__u32 pid;
 	__u32 tid;
+	__u32 ppid;
 	__u32 uid;
 	__u32 gid;
-	__u8 task[TASK_COMM_LEN];
+	__u8 comm[TASK_COMM_LEN];
+	__u8 pcomm[TASK_COMM_LEN];
 
 	union {
 		__u8 saddr_v6[16];
@@ -33,6 +36,11 @@ struct event_t {
 
 	unsigned char pkt_type;
 	__u64 latency_ns; // Set only if the packet is a response and pkt_type is 0 (Host).
+
+#ifdef WITH_LONG_PATHS
+	__u8 cwd[MAX_STRING_SIZE];
+	__u8 exepath[MAX_STRING_SIZE];
+#endif
 };
 
 #endif

commit 40c193a830fdc95510ac7635efd786624b8ba88a

(built-in)/trace/dns: Fix logic for percpu array map
The BPF_NOTEXIST flag fails when used on a percpu array map as the entry
for index zero is always present. The same applies to the bpf_map_delete
operation.

This commit removes those two invocations and improves the logic to only
clear fields when really needed.

Fixes: b7d6c1bec756 ("builtin trace-dns: use new fields from socket enricher")

Signed-off-by: Mauricio Vasquez <mauriciov@microsft.com>

pkg/gadgets/trace/dns/tracer/bpf/dns.c
@@ -62,10 +62,11 @@ static __always_inline bool is_dns_port(__u16 port)
 	return false;
 }
 
+// we need this to make sure the compiler doesn't remove our struct
+const struct event_t *unusedevent __attribute__((unused));
+
 // Cannot use gadget_reserve_buf() because this does not support
 // bpf_perf_event_output with packet appended
-static const struct event_t empty_event = {};
-
 struct {
 	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
 	__uint(max_entries, 1);
@@ -222,12 +223,11 @@ int ig_trace_dns(struct __sk_buff *skb)
 
 	// Initialize event here only after we know we're interested in this packet to avoid
 	// spending useless cycles.
-	bpf_map_update_elem(&tmp_events, &zero, &empty_event, BPF_NOEXIST);
 	event = bpf_map_lookup_elem(&tmp_events, &zero);
 	if (!event)
 		return 0;
-	bpf_map_delete_elem(&tmp_events, &zero);
 
+	event->latency_ns = 0;
 	event->netns = skb->cb[0]; // cb[0] initialized by dispatcher.bpf.c
 	event->timestamp = bpf_ktime_get_boot_ns();
 	event->proto = proto;
@@ -281,6 +281,21 @@ int ig_trace_dns(struct __sk_buff *skb)
 		bpf_probe_read_kernel_str(&event->exepath,
 					  sizeof(event->exepath),
 					  skb_val->exepath);
+#endif
+	} else {
+		event->mount_ns_id = 0;
+		event->pid = 0;
+		event->tid = 0;
+		event->ppid = 0;
+		event->comm[0] = 0;
+		event->pcomm[0] = 0;
+		event->uid = 0;
+		event->gid = 0;
+#ifdef WITH_LONG_PATHS
+		// __builtin_memset() can't be used here because the buffer is too big.
+		// Since it's a string, optimize it further and only clear the first byte.
+		event->cwd[0] = 0;
+		event->exepath[0] = 0;
 #endif
 	}
 