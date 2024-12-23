inspektor-gadget/inspektor-gadget

commit b2a99f2a0a0f8480e2e33ead18b80eefb4f71f96

cilium/ebpf: Update to v0.17.1
Signed-off-by: Burak Ok <burakok@microsoft.com>

pkg/gadgets/trace/dns/tracer/dns_bpfel.go
@@ -101,9 +101,10 @@ func loadDnsObjects(obj interface{}, opts *ebpf.CollectionOptions) error {
 type dnsSpecs struct {
 	dnsProgramSpecs
 	dnsMapSpecs
+	dnsVariableSpecs
 }
 
-// dnsSpecs contains programs before they are loaded into the kernel.
+// dnsProgramSpecs contains programs before they are loaded into the kernel.
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type dnsProgramSpecs struct {
@@ -120,12 +121,21 @@ type dnsMapSpecs struct {
 	TmpEvents     *ebpf.MapSpec `ebpf:"tmp_events"`
 }
 
+// dnsVariableSpecs contains global variables before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type dnsVariableSpecs struct {
+	Ports    *ebpf.VariableSpec `ebpf:"ports"`
+	PortsLen *ebpf.VariableSpec `ebpf:"ports_len"`
+}
+
 // dnsObjects contains all objects after they have been loaded into the kernel.
 //
 // It can be passed to loadDnsObjects or ebpf.CollectionSpec.LoadAndAssign.
 type dnsObjects struct {
 	dnsPrograms
 	dnsMaps
+	dnsVariables
 }
 
 func (o *dnsObjects) Close() error {
@@ -152,6 +162,14 @@ func (m *dnsMaps) Close() error {
 		m.QueryMap,
 		m.TmpEvents,
 	)
+}
+
+// dnsVariables contains all global variables after they have been loaded into the kernel.
+//
+// It can be passed to loadDnsObjects or ebpf.CollectionSpec.LoadAndAssign.
+type dnsVariables struct {
+	Ports    *ebpf.Variable `ebpf:"ports"`
+	PortsLen *ebpf.Variable `ebpf:"ports_len"`
 }
 
 // dnsPrograms contains all programs after they have been loaded into the kernel.

pkg/gadgets/trace/dns/tracer/dnswithlongpaths_bpfel.go
@@ -103,9 +103,10 @@ func loadDnsWithLongPathsObjects(obj interface{}, opts *ebpf.CollectionOptions) 
 type dnsWithLongPathsSpecs struct {
 	dnsWithLongPathsProgramSpecs
 	dnsWithLongPathsMapSpecs
+	dnsWithLongPathsVariableSpecs
 }
 
-// dnsWithLongPathsSpecs contains programs before they are loaded into the kernel.
+// dnsWithLongPathsProgramSpecs contains programs before they are loaded into the kernel.
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type dnsWithLongPathsProgramSpecs struct {
@@ -122,12 +123,21 @@ type dnsWithLongPathsMapSpecs struct {
 	TmpEvents     *ebpf.MapSpec `ebpf:"tmp_events"`
 }
 
+// dnsWithLongPathsVariableSpecs contains global variables before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type dnsWithLongPathsVariableSpecs struct {
+	Ports    *ebpf.VariableSpec `ebpf:"ports"`
+	PortsLen *ebpf.VariableSpec `ebpf:"ports_len"`
+}
+
 // dnsWithLongPathsObjects contains all objects after they have been loaded into the kernel.
 //
 // It can be passed to loadDnsWithLongPathsObjects or ebpf.CollectionSpec.LoadAndAssign.
 type dnsWithLongPathsObjects struct {
 	dnsWithLongPathsPrograms
 	dnsWithLongPathsMaps
+	dnsWithLongPathsVariables
 }
 
 func (o *dnsWithLongPathsObjects) Close() error {
@@ -154,6 +164,14 @@ func (m *dnsWithLongPathsMaps) Close() error {
 		m.QueryMap,
 		m.TmpEvents,
 	)
+}
+
+// dnsWithLongPathsVariables contains all global variables after they have been loaded into the kernel.
+//
+// It can be passed to loadDnsWithLongPathsObjects or ebpf.CollectionSpec.LoadAndAssign.
+type dnsWithLongPathsVariables struct {
+	Ports    *ebpf.Variable `ebpf:"ports"`
+	PortsLen *ebpf.Variable `ebpf:"ports_len"`
 }
 
 // dnsWithLongPathsPrograms contains all programs after they have been loaded into the kernel.

commit 60f2b1b41bb901a41705632ff8bd065335c93235

pkg/gadgets: dns: Update generated bpf2go files with cilium/ebpf v0.17.1.
Fixes: b2a99f2a0a0f ("cilium/ebpf: Update to v0.17.1")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/trace/dns/tracer/dns_bpfel.go
@@ -125,8 +125,9 @@ type dnsMapSpecs struct {
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type dnsVariableSpecs struct {
-	Ports    *ebpf.VariableSpec `ebpf:"ports"`
-	PortsLen *ebpf.VariableSpec `ebpf:"ports_len"`
+	Ports       *ebpf.VariableSpec `ebpf:"ports"`
+	PortsLen    *ebpf.VariableSpec `ebpf:"ports_len"`
+	Unusedevent *ebpf.VariableSpec `ebpf:"unusedevent"`
 }
 
 // dnsObjects contains all objects after they have been loaded into the kernel.
@@ -168,8 +169,9 @@ func (m *dnsMaps) Close() error {
 //
 // It can be passed to loadDnsObjects or ebpf.CollectionSpec.LoadAndAssign.
 type dnsVariables struct {
-	Ports    *ebpf.Variable `ebpf:"ports"`
-	PortsLen *ebpf.Variable `ebpf:"ports_len"`
+	Ports       *ebpf.Variable `ebpf:"ports"`
+	PortsLen    *ebpf.Variable `ebpf:"ports_len"`
+	Unusedevent *ebpf.Variable `ebpf:"unusedevent"`
 }
 
 // dnsPrograms contains all programs after they have been loaded into the kernel.

pkg/gadgets/trace/dns/tracer/dnswithlongpaths_bpfel.go
@@ -127,8 +127,9 @@ type dnsWithLongPathsMapSpecs struct {
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type dnsWithLongPathsVariableSpecs struct {
-	Ports    *ebpf.VariableSpec `ebpf:"ports"`
-	PortsLen *ebpf.VariableSpec `ebpf:"ports_len"`
+	Ports       *ebpf.VariableSpec `ebpf:"ports"`
+	PortsLen    *ebpf.VariableSpec `ebpf:"ports_len"`
+	Unusedevent *ebpf.VariableSpec `ebpf:"unusedevent"`
 }
 
 // dnsWithLongPathsObjects contains all objects after they have been loaded into the kernel.
@@ -170,8 +171,9 @@ func (m *dnsWithLongPathsMaps) Close() error {
 //
 // It can be passed to loadDnsWithLongPathsObjects or ebpf.CollectionSpec.LoadAndAssign.
 type dnsWithLongPathsVariables struct {
-	Ports    *ebpf.Variable `ebpf:"ports"`
-	PortsLen *ebpf.Variable `ebpf:"ports_len"`
+	Ports       *ebpf.Variable `ebpf:"ports"`
+	PortsLen    *ebpf.Variable `ebpf:"ports_len"`
+	Unusedevent *ebpf.Variable `ebpf:"unusedevent"`
 }
 
 // dnsWithLongPathsPrograms contains all programs after they have been loaded into the kernel.