inspektor-gadget/inspektor-gadget

commit e689576d89bc8fc94fd0c8b8cfbb6bf37c33adbc

pkg/gadget-service: update to support refactoring work for OCI gadgets
Signed-off-by: Michael Friese <mfriese@microsoft.com>

pkg/gadget-service/service-oci.go
@@ -1,0 +1,205 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gadgetservice
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/simple"
+)
+
+func (s *Service) GetOCIGadgetInfo(ctx context.Context, req *api.GetOCIGadgetInfoRequest) (*api.GetOCIGadgetInfoResponse, error) {
+	if req.Version != api.VersionGadgetInfo {
+		return nil, fmt.Errorf("expected version to be %d, got %d", api.VersionGadgetInfo, req.Version)
+	}
+
+	// Get all available operators
+	ops := make([]operators.DataOperator, 0)
+	for _, op := range operators.GetDataOperators() {
+		ops = append(ops, op)
+	}
+
+	gadgetCtx := gadgetcontext.NewOCI(ctx, req.ImageName, gadgetcontext.WithDataOperators(ops...))
+
+	gi, err := s.runtime.GetOCIGadgetInfo(gadgetCtx, s.runtime.ParamDescs().ToParams(), req.ParamValues)
+	if err != nil {
+		return nil, fmt.Errorf("getting gadget info: %w", err)
+	}
+	return &api.GetOCIGadgetInfoResponse{GadgetInfo: gi}, nil
+}
+
+func (s *Service) RunOCIGadget(runGadget api.OCIGadgetManager_RunOCIGadgetServer) error {
+	ctrl, err := runGadget.Recv()
+	if err != nil {
+		return err
+	}
+
+	ociRequest := ctrl.GetOciRunRequest()
+	if ociRequest == nil {
+		return fmt.Errorf("expected first control message to be gadget run request")
+	}
+
+	if ociRequest.Version != api.VersionGadgetRunProtocol {
+		return fmt.Errorf("expected version to be %d, got %d", api.VersionGadgetRunProtocol, ociRequest.Version)
+	}
+
+	// Create a new logger that logs to gRPC and falls back to the standard logger when it failed to send the message
+	logger := logger.NewFromGenericLogger(&Logger{
+		send:           runGadget.Send,
+		level:          logger.Level(ociRequest.LogLevel),
+		fallbackLogger: s.logger,
+	})
+
+	for k, v := range ociRequest.ParamValues {
+		logger.Debugf("param %s: %s", k, v)
+	}
+
+	done := make(chan bool)
+	defer func() {
+		done <- true
+	}()
+
+	// Build a simple operator that subscribes to all events and forwards them
+	svc := simple.New("svc",
+		simple.WithPriority(50000),
+		simple.OnInit(func(gadgetCtx operators.GadgetContext) error {
+			// Create payload buffer
+			outputBuffer := make(chan *api.GadgetEvent, s.eventBufferLength)
+
+			log := gadgetCtx.Logger()
+
+			go func() {
+				// Receive control messages
+				for {
+					msg, err := runGadget.Recv()
+					if err != nil {
+						s.logger.Warnf("error on connection: %v", err)
+						gadgetCtx.Cancel()
+						return
+					}
+					switch msg.Event.(type) {
+					case *api.OCIGadgetControlRequest_StopRequest:
+						log.Debugf("received stop request")
+						gadgetCtx.Cancel()
+						return
+					default:
+						s.logger.Warn("received unexpected request")
+					}
+				}
+			}()
+
+			go func() {
+				// Message pump to handle slow readers
+				for {
+					select {
+					case ev := <-outputBuffer:
+						runGadget.Send(ev)
+					case <-done:
+						return
+					}
+				}
+			}()
+
+			seq := uint32(0)
+			var seqLock sync.Mutex
+
+			gi, err := gadgetCtx.SerializeGadgetInfo()
+			if err != nil {
+				return fmt.Errorf("serializing gadget info: %w", err)
+			}
+
+			// datasource mapping; we're sending an array of available DataSources including a
+			// DataSourceID; this ID will be used when sending actual data and needs to be remapped
+			// to the actual DataSource on the client later on
+			dsLookup := make(map[string]uint32)
+			for i, ds := range gi.DataSources {
+				ds.Id = uint32(i)
+				dsLookup[ds.Name] = ds.Id
+			}
+
+			// todo: skip DataSources we're not interested in
+
+			for _, ds := range gadgetCtx.GetDataSources() {
+				dsID := dsLookup[ds.Name()]
+				ds.Subscribe(func(ds datasource.DataSource, data datasource.Data) error {
+					d, _ := proto.Marshal(data.Raw())
+
+					event := &api.GadgetEvent{
+						Type:         api.EventTypeGadgetPayload,
+						Payload:      d,
+						DataSourceID: dsID,
+					}
+
+					seqLock.Lock()
+					seq++
+					event.Seq = seq
+
+					// Try to send event; if outputBuffer is full, it will be dropped by taking
+					// the default path.
+					select {
+					case outputBuffer <- event:
+					default:
+					}
+					seqLock.Unlock()
+					return nil
+				}, 1000000) // TODO: static int?
+			}
+
+			// Send gadget information
+			d, _ := proto.Marshal(gi)
+			err = runGadget.Send(&api.GadgetEvent{
+				Type:    api.EventTypeGadgetInfo,
+				Payload: d,
+			})
+			if err != nil {
+				s.logger.Warnf("sending gadgetInfo: %v", err)
+			}
+			s.logger.Debugf("sent gadget info")
+
+			return nil
+		}),
+	)
+
+	ops := make([]operators.DataOperator, 0)
+	for _, op := range operators.GetDataOperators() {
+		ops = append(ops, op)
+	}
+	ops = append(ops, svc)
+
+	gadgetCtx := gadgetcontext.NewOCI(
+		runGadget.Context(),
+		ociRequest.ImageName,
+		gadgetcontext.WithLogger(logger),
+		gadgetcontext.WithDataOperators(ops...),
+	)
+
+	runtimeParams := s.runtime.ParamDescs().ToParams()
+	runtimeParams.CopyFromMap(ociRequest.ParamValues, "runtime.")
+
+	err = s.runtime.RunOCIGadget(gadgetCtx, runtimeParams, ociRequest.ParamValues)
+	if err != nil {
+		return err
+	}
+	return nil
+}

pkg/gadget-service/service.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The Inspektor Gadget authors
+// Copyright 2023-2024 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -56,6 +56,7 @@ type RunConfig struct {
 
 type Service struct {
 	api.UnimplementedGadgetManagerServer
+	api.UnimplementedOCIGadgetManagerServer
 	listener          net.Listener
 	runtime           runtime.Runtime
 	logger            logger.Logger
@@ -120,7 +121,7 @@ func (s *Service) RunGadget(runGadget api.GadgetManager_RunGadgetServer) error {
 
 	request := ctrl.GetRunRequest()
 	if request == nil {
-		return fmt.Errorf("expected first control message to be gadget request")
+		return fmt.Errorf("expected first control message to be gadget run request")
 	}
 
 	// Create a new logger that logs to gRPC and falls back to the standard logger when it failed to send the message
@@ -376,6 +377,7 @@ func (s *Service) Run(runConfig RunConfig, serverOptions ...grpc.ServerOption) e
 
 	server := grpc.NewServer(serverOptions...)
 	api.RegisterGadgetManagerServer(server, s)
+	api.RegisterOCIGadgetManagerServer(server, s)
 
 	s.servers[server] = struct{}{}
 

commit bdb060a965dd459118bc4c2c44c90f9d1a7911cb

oci-service: Fix closing logic
The previous logic was causing issues when running the gadget failed
because the client was hanging forever. This is because if
`s.runtime.RunGadget` fails early the simple operator won't be
initialized and hence the done <- true operation will block as there
is not any goroutine reading from the channel.

This commit fixes that by closing the channel instead of writing to it.

Fixes: e689576d89bc ("pkg/gadget-service: update to support refactoring work for OCI gadgets")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/gadget-service/service-oci.go
@@ -149,7 +149,7 @@ func (s *Service) RunGadget(runGadget api.GadgetManager_RunGadgetServer) error {
 
 	done := make(chan bool)
 	defer func() {
-		done <- true
+		close(done)
 	}()
 
 	// Build a simple operator that subscribes to all events and forwards them