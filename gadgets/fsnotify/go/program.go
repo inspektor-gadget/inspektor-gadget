2654b0c92f1ca7a07034bb3b6da79eefeb489769
Alban Crequy <albancrequy@linux.microsoft.com>
2025-01-09 16:45:29 +0100

    fsnotify: fix for Linux < 5.16
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

---
+++ gadgets/fsnotify/go/program.go
@@ -0,0 +1,38 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	api "github.com/inspektor-gadget/inspektor-gadget/wasmapi/go"
+)
+
+//export gadgetPreStart
+func gadgetPreStart() int {
+	// Linux 5.16 renamed fsnotify_add_event to fsnotify_insert_event
+	// https://github.com/torvalds/linux/commit/1ad03c3a326a86e259389592117252c851873395
+	if api.KallsymsSymbolExists("fsnotify_insert_event") {
+		return 0
+	}
+
+	if api.KallsymsSymbolExists("fsnotify_add_event") {
+		api.SetConfig("programs.fsnotify_insert_event_e.attach_to", "fsnotify_add_event")
+		return 0
+	}
+
+	api.Errorf("kernel symbol not found: fsnotify_add_event or fsnotify_insert_event")
+	return 0
+}
+
+func main() {}
