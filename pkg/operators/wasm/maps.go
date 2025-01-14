inspektor-gadget/inspektor-gadget

commit 2e3e77a573d57cdd7cf6881e3b18ab0218b22abb

operators/wasm: Add support for map of maps.
This commits adds support for map of maps [1].
All the operations on these maps are hidden behind map handle to ensure no file
descriptor is leaked to wasm guest.
On lookup, a map handle is returned, instead of map ID, which has to be released
by caller.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
[1]: https://docs.kernel.org/bpf/map_of_maps.html

pkg/operators/wasm/maps.go
@@ -16,6 +16,8 @@ package wasm
 
 import (
 	"context"
+	"encoding/binary"
+	"fmt"
 
 	"github.com/cilium/ebpf"
 	"github.com/tetratelabs/wazero"
@@ -76,6 +78,36 @@ func (i *wasmOperatorInstance) addMapFuncs(env wazero.HostModuleBuilder) {
 	)
 }
 
+func isMapTypeKnown(typ ebpf.MapType) bool {
+	// Keep in sync with with wasmapi/go/map.go.
+	return typ <= ebpf.TaskStorage
+}
+
+func isMapTypeFDArray(typ ebpf.MapType) bool {
+	return typ == ebpf.ProgramArray || typ == ebpf.PerfEventArray || typ == ebpf.CGroupArray
+}
+
+func isMapOfMaps(typ ebpf.MapType) bool {
+	return typ == ebpf.ArrayOfMaps || typ == ebpf.HashOfMaps
+}
+
+func isMapTypeSupported(typ ebpf.MapType) error {
+	// We do not permit operation on map unknown to us.
+	// A newer kernel can introduce a new type of map taking an FD as a value,
+	// this way user could use this new map to leak FD in guest WASM.
+	if !isMapTypeKnown(typ) {
+		return fmt.Errorf("unknown map type %v", typ)
+	}
+
+	// We do not support map taking FD different than map FD to avoid leaking them
+	// in guest wasm.
+	if isMapTypeFDArray(typ) {
+		return fmt.Errorf("FD array maps like %v are not supported", typ)
+	}
+
+	return nil
+}
+
 // newMap creates a new map.
 // Params:
 // - stack[0] is the map name
@@ -91,6 +123,13 @@ func (i *wasmOperatorInstance) newMap(ctx context.Context, m wapi.Module, stack 
 	keySize := wapi.DecodeU32(stack[2])
 	valueSize := wapi.DecodeU32(stack[3])
 	maxEntries := wapi.DecodeU32(stack[4])
+
+	err := isMapTypeSupported(mapType)
+	if err != nil {
+		i.logger.Warnf("newMap: %v", err)
+		stack[0] = 0
+		return
+	}
 
 	mapName, err := stringFromStack(m, mapNamePtr)
 	if err != nil {
@@ -136,16 +175,23 @@ func (i *wasmOperatorInstance) getMap(ctx context.Context, m wapi.Module, stack 
 		return
 	}
 
-	ebpfMap, ok := i.gadgetCtx.GetVar(operators.MapPrefix + mapName)
+	ebpfMapAny, ok := i.gadgetCtx.GetVar(operators.MapPrefix + mapName)
 	if !ok {
-		i.logger.Warnf("get map: no map for name %q", mapName)
+		i.logger.Warnf("getMap: no map for name %q", mapName)
 		stack[0] = 0
 		return
 	}
 
-	ebpfMap, ok = ebpfMap.(*ebpf.Map)
+	ebpfMap, ok := ebpfMapAny.(*ebpf.Map)
 	if !ok {
-		i.logger.Warnf("get map: map %q is not an ebpf map", mapName)
+		i.logger.Warnf("getMap: map %q is not an ebpf map", mapName)
+		stack[0] = 0
+		return
+	}
+
+	err = isMapTypeSupported(ebpfMap.Type())
+	if err != nil {
+		i.logger.Warnf("getMap: %v", err)
 		stack[0] = 0
 		return
 	}
@@ -183,6 +229,19 @@ func (i *wasmOperatorInstance) mapLookup(ctx context.Context, m wapi.Module, sta
 		i.logger.Warnf("mapLookup: getting value: %v", err)
 		stack[0] = 1
 		return
+	}
+
+	if isMapOfMaps(ebpfMap.Type()) {
+		mapID := ebpf.MapID(binary.NativeEndian.Uint32(value))
+
+		innerMap, err := ebpf.NewMapFromID(mapID)
+		if err != nil {
+			i.logger.Warnf("mapLookup: getting inner map from ID %d: %v", mapID, err)
+			stack[0] = 1
+			return
+		}
+
+		binary.NativeEndian.PutUint32(value, i.addHandle(innerMap))
 	}
 
 	err = bufToStack(m, value, valuePtr)
@@ -236,12 +295,29 @@ func (i *wasmOperatorInstance) mapUpdate(ctx context.Context, m wapi.Module, sta
 		return
 	}
 
-	err = ebpfMap.Update(key, value, ebpfFlags)
+	if isMapOfMaps(ebpfMap.Type()) {
+		// We hide the FD behind the handle, this way users can interact with
+		// *OfMaps maps but without handling the FD themselves.
+		// So, for mapUpdate() users give the map handle and we translate it to the
+		// map FD to please eBPF side.
+		targetMapHandle := binary.NativeEndian.Uint32(value)
+		targetMap, ok := getHandle[*ebpf.Map](i, targetMapHandle)
+		if !ok {
+			stack[0] = 1
+			return
+		}
+
+		err = ebpfMap.Update(key, uint32(targetMap.FD()), ebpfFlags)
+	} else {
+		err = ebpfMap.Update(key, value, ebpfFlags)
+	}
+
 	if err != nil {
 		i.logger.Warnf("mapUpdate: updating value: %v", err)
 		stack[0] = 1
 		return
 	}
+
 	stack[0] = 0
 }
 

pkg/operators/wasm/testdata/Makefile
@@ -9,6 +9,7 @@ TEST_ARTIFACTS = \
 	params \
 	config \
 	map \
+	mapofmap \
 	#
 
 all: $(TEST_ARTIFACTS)

pkg/operators/wasm/testdata/map/program.go
@@ -145,6 +145,7 @@ func gadgetStart() int {
 	newMap, err := api.NewMap(mapSpec)
 	if err != nil {
 		api.Errorf("creating map %s", mapSpec.Name)
+		return 1
 	}
 	defer newMap.Close()
 

pkg/operators/wasm/testdata/mapofmap/build.yaml
@@ -1,0 +1,1 @@
+wasm: program.go

pkg/operators/wasm/testdata/mapofmap/go.mod
@@ -1,0 +1,8 @@
+module main
+
+go 1.23.0
+
+require github.com/inspektor-gadget/inspektor-gadget v0.27.0
+
+// use this to be able to compile it locally
+replace github.com/inspektor-gadget/inspektor-gadget => ../../../../../

pkg/operators/wasm/testdata/mapofmap/program.bpf.c
@@ -1,0 +1,22 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+
+struct map_test_struct {
+	__u32 a;
+	__u32 b;
+	__u8 c;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH_OF_MAPS);
+	__uint(key_size, sizeof(struct map_test_struct));
+	__uint(value_size, sizeof(u32));
+	__uint(max_entries, 1024);
+	__array(
+		values, struct {
+			__uint(type, BPF_MAP_TYPE_HASH);
+			__uint(key_size, sizeof(u32));
+			__uint(value_size, sizeof(u32));
+			__uint(max_entries, 1);
+		});
+} map_of_map SEC(".maps");

commit c31107d67e761b76acae9d1a978d999117ac372b

operators/wasm: Check map type is supported in operation functions.
We want to allow creating all types of maps, as thy can be used in eBPF, but
we want to restrain operations on maps manipulating FD.

Fixes: 2e3e77a573d5 ("operators/wasm: Add support for map of maps.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/wasm/maps.go
@@ -91,7 +91,7 @@ func isMapOfMaps(typ ebpf.MapType) bool {
 	return typ == ebpf.ArrayOfMaps || typ == ebpf.HashOfMaps
 }
 
-func isMapTypeSupported(typ ebpf.MapType) error {
+func isMapTypeSupportedForAccess(typ ebpf.MapType) error {
 	// We do not permit operation on map unknown to us.
 	// A newer kernel can introduce a new type of map taking an FD as a value,
 	// this way user could use this new map to leak FD in guest WASM.
@@ -124,9 +124,8 @@ func (i *wasmOperatorInstance) newMap(ctx context.Context, m wapi.Module, stack 
 	valueSize := wapi.DecodeU32(stack[3])
 	maxEntries := wapi.DecodeU32(stack[4])
 
-	err := isMapTypeSupported(mapType)
-	if err != nil {
-		i.logger.Warnf("newMap: %v", err)
+	if !isMapTypeKnown(mapType) {
+		i.logger.Warnf("newMap: unknown map type %v", mapType)
 		stack[0] = 0
 		return
 	}
@@ -189,9 +188,9 @@ func (i *wasmOperatorInstance) getMap(ctx context.Context, m wapi.Module, stack 
 		return
 	}
 
-	err = isMapTypeSupported(ebpfMap.Type())
-	if err != nil {
-		i.logger.Warnf("getMap: %v", err)
+	mapType := ebpfMap.Type()
+	if !isMapTypeKnown(mapType) {
+		i.logger.Warnf("getMap: unknown map type %v", mapType)
 		stack[0] = 0
 		return
 	}
@@ -217,6 +216,14 @@ func (i *wasmOperatorInstance) mapLookup(ctx context.Context, m wapi.Module, sta
 		return
 	}
 
+	mapType := ebpfMap.Type()
+	err := isMapTypeSupportedForAccess(mapType)
+	if err != nil {
+		i.logger.Warnf("mapLookup: %v", err)
+		stack[0] = 1
+		return
+	}
+
 	key, err := bufFromStack(m, keyPtr)
 	if err != nil {
 		i.logger.Warnf("mapLookup: getting a buf for key pointer: %v", err)
@@ -231,7 +238,7 @@ func (i *wasmOperatorInstance) mapLookup(ctx context.Context, m wapi.Module, sta
 		return
 	}
 
-	if isMapOfMaps(ebpfMap.Type()) {
+	if isMapOfMaps(mapType) {
 		mapID := ebpf.MapID(binary.NativeEndian.Uint32(value))
 
 		innerMap, err := ebpf.NewMapFromID(mapID)
@@ -281,6 +288,14 @@ func (i *wasmOperatorInstance) mapUpdate(ctx context.Context, m wapi.Module, sta
 		return
 	}
 
+	mapType := ebpfMap.Type()
+	err := isMapTypeSupportedForAccess(mapType)
+	if err != nil {
+		i.logger.Warnf("mapUpdate: %v", err)
+		stack[0] = 1
+		return
+	}
+
 	key, err := bufFromStack(m, keyPtr)
 	if err != nil {
 		i.logger.Warnf("mapUpdate: getting a buf for key pointer: %v", err)
@@ -295,7 +310,7 @@ func (i *wasmOperatorInstance) mapUpdate(ctx context.Context, m wapi.Module, sta
 		return
 	}
 
-	if isMapOfMaps(ebpfMap.Type()) {
+	if isMapOfMaps(mapType) {
 		// We hide the FD behind the handle, this way users can interact with
 		// *OfMaps maps but without handling the FD themselves.
 		// So, for mapUpdate() users give the map handle and we translate it to the
@@ -333,6 +348,13 @@ func (i *wasmOperatorInstance) mapDelete(ctx context.Context, m wapi.Module, sta
 
 	ebpfMap, ok := getHandle[*ebpf.Map](i, mapHandle)
 	if !ok {
+		stack[0] = 1
+		return
+	}
+
+	err := isMapTypeSupportedForAccess(ebpfMap.Type())
+	if err != nil {
+		i.logger.Warnf("mapDelete: %v", err)
 		stack[0] = 1
 		return
 	}