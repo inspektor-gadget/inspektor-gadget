inspektor-gadget/inspektor-gadget

commit 041b9df834bf415e4f186d82220e5480e0719935

cmd: Add inspect subcommand to image.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/oci/oci.go
@@ -417,45 +417,51 @@ func tarFolderToFile(src, filePath string, headerTime time.Time) error {
 	})
 }
 
+func getGadgetImageDescriptor(ctx context.Context, store *oci.Store, fullTag string) (*GadgetImageDesc, error) {
+	parsed, err := reference.Parse(fullTag)
+	if err != nil {
+		return nil, fmt.Errorf("parsing image %q: %w", fullTag, err)
+	}
+
+	var repository string
+	if named, ok := parsed.(reference.Named); ok {
+		repository = named.Name()
+	}
+
+	tag := "latest"
+	if tagged, ok := parsed.(reference.Tagged); ok {
+		tag = tagged.Tag()
+	}
+
+	image := &GadgetImageDesc{
+		Repository: repository,
+		Tag:        tag,
+	}
+
+	desc, err := store.Resolve(ctx, fullTag)
+	if err != nil {
+		return nil, fmt.Errorf("found tag %q but couldn't get a descriptor for it: %w", fullTag, err)
+	}
+	image.Digest = desc.Digest.String()
+
+	manifest, err := getManifestForHost(ctx, store, fullTag)
+	if err != nil {
+		return nil, fmt.Errorf("getting manifest for %q: %w", fullTag, err)
+	}
+
+	image.Created = getTimeFromAnnotations(manifest.Annotations)
+
+	return image, nil
+}
+
 func listGadgetImages(ctx context.Context, store *oci.Store) ([]*GadgetImageDesc, error) {
 	images := []*GadgetImageDesc{}
 	err := store.Tags(ctx, "", func(tags []string) error {
 		for _, fullTag := range tags {
-			parsed, err := reference.Parse(fullTag)
+			image, err := getGadgetImageDescriptor(ctx, store, fullTag)
 			if err != nil {
-				log.Debugf("parsing image %q: %s", fullTag, err)
-				continue
+				log.Debugf("getting gadget image descriptor for %s: %v", fullTag, err)
 			}
-
-			var repository string
-			if named, ok := parsed.(reference.Named); ok {
-				repository = named.Name()
-			}
-
-			tag := "latest"
-			if tagged, ok := parsed.(reference.Tagged); ok {
-				tag = tagged.Tag()
-			}
-
-			image := &GadgetImageDesc{
-				Repository: repository,
-				Tag:        tag,
-			}
-
-			desc, err := store.Resolve(ctx, fullTag)
-			if err != nil {
-				log.Debugf("Found tag %q but couldn't get a descriptor for it: %v", fullTag, err)
-				continue
-			}
-			image.Digest = desc.Digest.String()
-
-			manifest, err := getManifestForHost(ctx, store, fullTag)
-			if err != nil {
-				log.Debugf("Getting manifest for %q: %v", fullTag, err)
-				continue
-			}
-
-			image.Created = getTimeFromAnnotations(manifest.Annotations)
 
 			images = append(images, image)
 		}
@@ -482,6 +488,26 @@ func ListGadgetImages(ctx context.Context) ([]*GadgetImageDesc, error) {
 	}
 
 	return images, nil
+}
+
+func GetGadgetImageDesc(ctx context.Context, image string) (*GadgetImageDesc, error) {
+	ociStore, err := GetLocalOciStore()
+	if err != nil {
+		return nil, fmt.Errorf("getting oci store: %w", err)
+	}
+
+	targetImage, err := normalizeImageName(image)
+	if err != nil {
+		return nil, fmt.Errorf("normalizing image: %w", err)
+	}
+
+	fullName := targetImage.String()
+	desc, err := getGadgetImageDescriptor(ctx, ociStore, fullName)
+	if err != nil {
+		return nil, fmt.Errorf("getting gadget image descriptor: %w", err)
+	}
+
+	return desc, nil
 }
 
 // DeleteGadgetImage removes the given image.

cmd/common/image/image.go
@@ -31,6 +31,7 @@ func NewImageCmd() *cobra.Command {
 	cmd.AddCommand(NewPullCmd())
 	cmd.AddCommand(NewTagCmd())
 	cmd.AddCommand(NewListCmd())
+	cmd.AddCommand(NewInspectCmd())
 	cmd.AddCommand(NewRemoveCmd())
 
 	return cmd

cmd/common/image/inspect.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The Inspektor Gadget authors
+// Copyright 2024 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,66 +24,67 @@ import (
 
 	"github.com/docker/go-units"
 	"github.com/spf13/cobra"
-	"golang.org/x/term"
 
 	"github.com/inspektor-gadget/inspektor-gadget/cmd/common/utils"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns/formatter/textcolumns"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/oci"
+
+	"golang.org/x/term"
 )
 
-func NewListCmd() *cobra.Command {
-	var noTrunc bool
+func NewInspectCmd() *cobra.Command {
 	var outputMode string
 
 	outputModes := []string{utils.OutputModeColumns, utils.OutputModeJSON, utils.OutputModeJSONPretty}
 
 	cmd := &cobra.Command{
-		Use:          "list",
-		Short:        "List gadget images on the host",
+		Use:          "inspect",
+		Short:        "Inspect the local gadget image",
 		SilenceUsage: true,
-		Args:         cobra.NoArgs,
+		Args:         cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			images, err := oci.ListGadgetImages(context.TODO())
+			image, err := oci.GetGadgetImageDesc(context.TODO(), args[0])
 			if err != nil {
-				return fmt.Errorf("list gadgets: %w", err)
+				return fmt.Errorf("inspecting image: %w", err)
 			}
 
-			isTerm := term.IsTerminal(int(os.Stdout.Fd()))
-
-			cols := columns.MustCreateColumns[oci.GadgetImageDesc]()
-			if !noTrunc && isTerm {
-				cols.MustSetExtractor("digest", func(i *oci.GadgetImageDesc) any {
-					if i.Digest == "" {
-						return ""
-					}
-					// Return the shortened digest and remove the sha256: prefix
-					return strings.TrimPrefix(i.Digest, "sha256:")[:12]
-				})
-				now := time.Now()
-				cols.MustSetExtractor("created", func(i *oci.GadgetImageDesc) any {
-					if t, err := time.Parse(time.RFC3339, i.Created); err == nil {
-						return fmt.Sprintf("%s ago", strings.ToLower(units.HumanDuration(now.Sub(t))))
-					}
-					return ""
-				})
-			}
 			switch outputMode {
 			case utils.OutputModeJSON:
-				bytes, err := json.Marshal(images)
+				bytes, err := json.Marshal(image)
 				if err != nil {
-					return fmt.Errorf("marshalling images to JSON: %w", err)
+					return fmt.Errorf("marshalling image to JSON: %w", err)
 				}
 				fmt.Fprint(cmd.OutOrStdout(), string(bytes))
 			case utils.OutputModeJSONPretty:
-				bytes, err := json.MarshalIndent(images, "", "  ")
+				bytes, err := json.MarshalIndent(image, "", "  ")
 				if err != nil {
-					return fmt.Errorf("marshalling images to JSON: %w", err)
+					return fmt.Errorf("marshalling image to JSON: %w", err)
 				}
 				fmt.Fprint(cmd.OutOrStdout(), string(bytes))
 			case utils.OutputModeColumns:
-				formatter := textcolumns.NewFormatter(cols.GetColumnMap(), textcolumns.WithShouldTruncate(!noTrunc && isTerm))
-				formatter.WriteTable(cmd.OutOrStdout(), images)
+				isTerm := term.IsTerminal(int(os.Stdout.Fd()))
+
+				cols := columns.MustCreateColumns[oci.GadgetImageDesc]()
+				if isTerm {
+					cols.MustSetExtractor("digest", func(i *oci.GadgetImageDesc) any {
+						if i.Digest == "" {
+							return ""
+						}
+						// Return the shortened digest and remove the sha256: prefix
+						return strings.TrimPrefix(i.Digest, "sha256:")[:12]
+					})
+					now := time.Now()
+					cols.MustSetExtractor("created", func(i *oci.GadgetImageDesc) any {
+						if t, err := time.Parse(time.RFC3339, i.Created); err == nil {
+							return fmt.Sprintf("%s ago", strings.ToLower(units.HumanDuration(now.Sub(t))))
+						}
+						return ""
+					})
+				}
+
+				formatter := textcolumns.NewFormatter(cols.GetColumnMap(), textcolumns.WithShouldTruncate(isTerm))
+				formatter.WriteTable(cmd.OutOrStdout(), []*oci.GadgetImageDesc{image})
 			default:
 				return fmt.Errorf("invalid output mode %q, valid values are: %s", outputMode, strings.Join(outputModes, ", "))
 			}
@@ -91,7 +92,6 @@ func NewListCmd() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().BoolVar(&noTrunc, "no-trunc", false, "Don't truncate output, this option is only valid when used in a terminal")
 	cmd.Flags().StringVarP(
 		&outputMode,
 		"output",

commit f9b850b2c9d2c45116d26d8bfc84041e6ceb3492

oci: Fix image list crash
Avoid crashing if the returned descriptor is nil.

Fixes: 041b9df834bf ("cmd: Add inspect subcommand to image.")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/oci/oci.go
@@ -474,6 +474,7 @@ func getGadgetImages(ctx context.Context, store *oci.Store) ([]*GadgetImageDesc,
 			image, err := getGadgetImageDescriptor(ctx, store, fullTag)
 			if err != nil {
 				log.Debugf("getting gadget image descriptor for %s: %v", fullTag, err)
+				continue
 			}
 
 			images = append(images, image)