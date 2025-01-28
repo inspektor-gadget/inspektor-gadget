64501652140af42c49ef977801a07b0d6e6256cf
Qasim Sarfraz <qasimsarfraz@microsoft.com>
2025-01-28 16:00:51 +0100

    perf/ring buffer: Continue reading in case of errors
    Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

--- pkg/gadgets/trace/oomkill/tracer/tracer.go
+++ pkg/gadgets/trace/oomkill/tracer/tracer.go
@@ -118,9 +118,9 @@ func (t *Tracer) run() {
 				return
 			}
 
-			msg := fmt.Sprintf("Error reading perf ring buffer: %s", err)
-			t.eventCallback(types.Base(eventtypes.Err(msg)))
-			return
+			msg := fmt.Sprintf("reading perf ring buffer: %s", err)
+			t.eventCallback(types.Base(eventtypes.Warn(msg)))
+			continue
 		}
 
 		bpfEvent := (*oomkillDataT)(unsafe.Pointer(&record.RawSample[0]))
