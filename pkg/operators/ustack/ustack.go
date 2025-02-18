899daf67cc780df8d287f0ab88727a5f28dd01ec
Alban Crequy <albancrequy@linux.microsoft.com>
2025-02-18 17:52:16 +0100

    ustack: don't warn about processes on the host
    Processes in containers have several pids from different pid namespace
    point of view. But processes on the host have only one pid. This is
    expected, so the warnings were unwarranted.
    
    This patch remove the warning in case of host process. This also makes
    the warnings clearer.
    
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

--- pkg/operators/ustack/ustack.go
+++ pkg/operators/ustack/ustack.go
@@ -167,8 +167,12 @@ func (o *OperatorInstance) init(gadgetCtx operators.GadgetContext) error {
 				pidnsLevel0, _ := pidnsLevel0Field[0].Uint32(data)
 				pidLevel1, _ := pidLevel1Field[0].Uint32(data)
 				pidnsLevel1, _ := pidnsLevel1Field[0].Uint32(data)
-				if pidLevel0 == 0 || pidnsLevel0 == 0 || pidLevel1 == 0 || pidnsLevel1 == 0 {
-					logger.Warn("invalid pid or pidns")
+				if pidLevel0 == 0 {
+					logger.Warn("user stack with invalid pid")
+					return nil
+				}
+				if pidnsLevel0 == 0 {
+					logger.Warn("user stack with invalid pidns")
 					return nil
 				}
 				pidNumbers := []symbolizer.PidNumbers{
