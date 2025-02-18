inspektor-gadget/inspektor-gadget

commit c04bab7e12e28321354de4b43ba7cfbd6266832f

Add warning if gadget is run with a different IG version
Ideally Inspektor Gagget should allow running gadgets built with different
ig versions. However, we haven't been able to implement this feature.
To avoid user confusion, print a warning if the gadget is run with a different
version of IG than the one it was built with.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/oci-handler/oci.go
@@ -21,14 +21,18 @@ import (
 	"io"
 	"strings"
 
+	"github.com/blang/semver"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/spf13/viper"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/environment"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
 	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/k8sutil"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/oci"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
@@ -46,6 +50,10 @@ const (
 	verifyImage             = "verify-image"
 	publicKeys              = "public-keys"
 	allowedGadgets          = "allowed-gadgets"
+)
+
+const (
+	builderVersionAnnotation = "io.inspektor-gadget.builder.version"
 )
 
 type ociHandler struct {
@@ -201,6 +209,32 @@ func (o *OciHandlerInstance) ExtraParams(gadgetCtx operators.GadgetContext) api.
 	return o.extraParams
 }
 
+func checkBuilderVersion(manifest *v1.Manifest, logger logger.Logger) {
+	currentVersion := version.Version()
+
+	// Do not print any warning if this is a prerelease to avoid annoying developers
+	if len(currentVersion.Pre) > 0 {
+		return
+	}
+
+	builderVersionAnn := manifest.Annotations[builderVersionAnnotation]
+	if builderVersionAnn == "" {
+		logger.Warnf("Builder version not found in the gadget image. Gadget could be incompatible")
+		return
+	}
+
+	builderVersion, err := semver.ParseTolerant(builderVersionAnn)
+	if err != nil {
+		logger.Warnf("parsing builder version: %s", err)
+		return
+	}
+
+	if !builderVersion.EQ(currentVersion) {
+		logger.Warnf("This gadget was built with ig %s and it's being run with v%s. Gadget could be incompatible",
+			builderVersionAnn, currentVersion)
+	}
+}
+
 func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 	if len(gadgetCtx.ImageName()) == 0 {
 		return fmt.Errorf("imageName empty")
@@ -260,6 +294,7 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 	}
 
 	log := gadgetCtx.Logger()
+	checkBuilderVersion(manifest, log)
 
 	r, err := oci.GetContentFromDescriptor(gadgetCtx.Context(), target, manifest.Config)
 	if err != nil {

internal/version/version.go
@@ -33,3 +33,7 @@ func init() {
 func Version() semver.Version {
 	return parsedVersion
 }
+
+func VersionString() string {
+	return version
+}

pkg/oci/build.go
@@ -33,6 +33,7 @@ import (
 	"oras.land/oras-go/v2/content"
 	"oras.land/oras-go/v2/errdef"
 
+	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
 	metadatav1 "github.com/inspektor-gadget/inspektor-gadget/pkg/metadata/v1"
 )
 
@@ -48,6 +49,10 @@ const (
 	wasmObjectMediaType = "application/vnd.gadget.wasm.program.v1+binary"
 	btfgenMediaType     = "application/vnd.gadget.btfgen.v1+binary"
 	metadataMediaType   = "application/vnd.gadget.config.v1+yaml"
+)
+
+const (
+	builderVersionAnnotation = "io.inspektor-gadget.builder.version"
 )
 
 type ObjectPath struct {
@@ -181,6 +186,7 @@ func annotationsFromMetadata(metadataBytes []byte) (map[string]string, error) {
 		ocispec.AnnotationURL:           metadata.HomepageURL,
 		ocispec.AnnotationDocumentation: metadata.DocumentationURL,
 		ocispec.AnnotationSource:        metadata.SourceURL,
+		builderVersionAnnotation:        version.VersionString(),
 	}
 
 	for k, v := range metadata.Annotations {

commit fcc9e4f818a84215d15e073934ba18a3a3264ab8

Improve gadget versin skew warning
Do not print anything if parsing the version fails. It means the the
gadget could have been built with a development version of Inspektor Gadget,
hence it'll create a lot of noise when using the :latest version.

Fixes: c04bab7e12e2 ("Add warning if gadget is run with a different IG version")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/oci-handler/oci.go
@@ -225,7 +225,8 @@ func checkBuilderVersion(manifest *v1.Manifest, logger logger.Logger) {
 
 	builderVersion, err := semver.ParseTolerant(builderVersionAnn)
 	if err != nil {
-		logger.Warnf("parsing builder version: %s", err)
+		// it could happen on development versions
+		logger.Debugf("parsing builder version: %s", err)
 		return
 	}
 