inspektor-gadget/inspektor-gadget

commit 62f43ea7b872e2e63e695298caf5b8d6c61ac241

operators/wasm: Add function to get syscall ID from syscall name.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/wasm/syscalls.go
@@ -18,12 +18,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
+	"math"
 	"sync"
 
 	"github.com/tetratelabs/wazero"
 	wapi "github.com/tetratelabs/wazero/api"
 
 	syscallhelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/traceloop/syscall-helpers"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/syscalls"
 )
 
 func (i *wasmOperatorInstance) addSyscallsDeclarationsFuncs(env wazero.HostModuleBuilder) {
@@ -33,6 +35,11 @@ func (i *wasmOperatorInstance) addSyscallsDeclarationsFuncs(env wazero.HostModul
 			wapi.ValueTypeI64, // Buffer to save syscall name
 		},
 		[]wapi.ValueType{wapi.ValueTypeI32}, // Error
+	)
+
+	exportFunction(env, "getSyscallID", i.getSyscallID,
+		[]wapi.ValueType{wapi.ValueTypeI64}, // Syscall Name
+		[]wapi.ValueType{wapi.ValueTypeI64}, // Syscall ID
 	)
 
 	exportFunction(env, "getSyscallDeclaration", i.getSyscallDeclaration,
@@ -63,6 +70,31 @@ func (i *wasmOperatorInstance) getSyscallName(ctx context.Context, m wapi.Module
 	}
 
 	stack[0] = 0
+}
+
+// getSyscalID returns the syscall ID corresponding to the given name.
+// Params:
+// - stack[0]
+// Return value:
+// - Syscall ID on success, math.MaxUint64 on error
+func (i *wasmOperatorInstance) getSyscallID(ctx context.Context, m wapi.Module, stack []uint64) {
+	syscallNamePtr := stack[0]
+
+	syscallName, err := stringFromStack(m, syscallNamePtr)
+	if err != nil {
+		i.logger.Warnf("getSyscallID: reading string from stack: %v", err)
+		stack[0] = math.MaxUint64
+		return
+	}
+
+	id, ok := syscalls.GetSyscallNumberByName(syscallName)
+	if !ok {
+		i.logger.Warnf("getSyscallID: no syscall %s", syscallName)
+		stack[0] = math.MaxUint64
+		return
+	}
+
+	stack[0] = uint64(id)
 }
 
 // Keep in sync with wasmapi/go/syscall.go.

wasmapi/go/syscall.go
@@ -16,6 +16,7 @@ package api
 
 import (
 	"fmt"
+	"math"
 	"runtime"
 	"unsafe"
 	_ "unsafe"
@@ -24,6 +25,10 @@ import (
 //go:wasmimport env getSyscallName
 //go:linkname getSyscallName getSyscallName
 func getSyscallName(id uint32, dst uint64) uint32
+
+//go:wasmimport env getSyscallID
+//go:linkname getSyscallID getSyscallID
+func getSyscallID(name uint64) uint64
 
 //go:wasmimport env getSyscallDeclaration
 //go:linkname getSyscallDeclaration getSyscallDeclaration
@@ -67,6 +72,15 @@ func GetSyscallName(id uint16) (string, error) {
 		return "", fmt.Errorf("getting syscall name for syscall id %d", id)
 	}
 	return fromCString(dst), nil
+}
+
+func GetSyscallID(name string) (uint64, error) {
+	id := getSyscallID(uint64(stringToBufPtr(name)))
+	if id == math.MaxUint64 {
+		return 0, fmt.Errorf("getting syscall ID for syscall %s", name)
+	}
+
+	return id, nil
 }
 
 func GetSyscallDeclaration(name string) (SyscallDeclaration, error) {

pkg/operators/wasm/testdata/syscall/program.go
@@ -55,7 +55,25 @@ func gadgetInit() int32 {
 		return 1
 	}
 
+	syscallName = "open_tree"
+	id, err := api.GetSyscallID(syscallName)
+	if err != nil {
+		api.Errorf("%v", err)
+		return 1
+	}
+
+	if uint16(id) != openTreeSyscallID {
+		api.Errorf("mismatch for syscall %q: expected %d, got %d", syscallName, openTreeSyscallID, id)
+		return 1
+	}
+
 	syscallName = "foobar"
+	id, err = api.GetSyscallID(syscallName)
+	if err == nil {
+		api.Errorf("expected no syscall ID for syscall %q, got %v", syscallName, id)
+		return 1
+	}
+
 	declaration, err := api.GetSyscallDeclaration(syscallName)
 	if err == nil {
 		api.Errorf("expected no declaration for syscall %q, got %v", syscallName, declaration)

commit 501a433777f2795b4b0a1b3c9e1495513ae56d5d

operators: wasm: Have getSyscallID() returns -1 on error.
Fixes: 62f43ea7b872 ("operators/wasm: Add function to get syscall ID from syscall name.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/wasm/syscalls.go
@@ -18,7 +18,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
-	"math"
 	"sync"
 
 	"github.com/tetratelabs/wazero"
@@ -39,7 +38,7 @@ func (i *wasmOperatorInstance) addSyscallsDeclarationsFuncs(env wazero.HostModul
 
 	exportFunction(env, "getSyscallID", i.getSyscallID,
 		[]wapi.ValueType{wapi.ValueTypeI64}, // Syscall Name
-		[]wapi.ValueType{wapi.ValueTypeI64}, // Syscall ID
+		[]wapi.ValueType{wapi.ValueTypeI32}, // Syscall ID
 	)
 
 	exportFunction(env, "getSyscallDeclaration", i.getSyscallDeclaration,
@@ -76,21 +75,21 @@ func (i *wasmOperatorInstance) getSyscallName(ctx context.Context, m wapi.Module
 // Params:
 // - stack[0]
 // Return value:
-// - Syscall ID on success, math.MaxUint64 on error
+// - Syscall ID on success, -1 on error
 func (i *wasmOperatorInstance) getSyscallID(ctx context.Context, m wapi.Module, stack []uint64) {
 	syscallNamePtr := stack[0]
 
 	syscallName, err := stringFromStack(m, syscallNamePtr)
 	if err != nil {
 		i.logger.Warnf("getSyscallID: reading string from stack: %v", err)
-		stack[0] = math.MaxUint64
+		stack[0] = wapi.EncodeI32(-1)
 		return
 	}
 
 	id, ok := syscalls.GetSyscallNumberByName(syscallName)
 	if !ok {
 		i.logger.Warnf("getSyscallID: no syscall %s", syscallName)
-		stack[0] = math.MaxUint64
+		stack[0] = wapi.EncodeI32(-1)
 		return
 	}
 

wasmapi/go/syscall.go
@@ -16,7 +16,6 @@ package api
 
 import (
 	"fmt"
-	"math"
 	"runtime"
 	"unsafe"
 	_ "unsafe"
@@ -28,7 +27,7 @@ func getSyscallName(id uint32, dst uint64) uint32
 
 //go:wasmimport env getSyscallID
 //go:linkname getSyscallID getSyscallID
-func getSyscallID(name uint64) uint64
+func getSyscallID(name uint64) int32
 
 //go:wasmimport env getSyscallDeclaration
 //go:linkname getSyscallDeclaration getSyscallDeclaration
@@ -74,9 +73,9 @@ func GetSyscallName(id uint16) (string, error) {
 	return fromCString(dst), nil
 }
 
-func GetSyscallID(name string) (uint64, error) {
+func GetSyscallID(name string) (int32, error) {
 	id := getSyscallID(uint64(stringToBufPtr(name)))
-	if id == math.MaxUint64 {
+	if id == -1 {
 		return 0, fmt.Errorf("getting syscall ID for syscall %s", name)
 	}
 