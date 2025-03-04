inspektor-gadget/inspektor-gadget

commit b1bcb83bc5f6f470e3ecebf7bb450e9168e8f467

operators: kubeipresolver for image-based gadgets
Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

pkg/operators/kubeipresolver/kubeipresolver.go
@@ -17,8 +17,12 @@
 package kubeipresolver
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/common"
@@ -28,6 +32,12 @@ import (
 
 const (
 	OperatorName = "KubeIPResolver"
+	Priority     = 10
+)
+
+const (
+	endpointL4Type = "gadget_l4endpoint_t"
+	ipAddrType     = "gadget_ip_addr_t"
 )
 
 type KubeIPResolverInterface interface {
@@ -83,9 +93,10 @@ func (k *KubeIPResolver) Instantiate(gadgetCtx operators.GadgetContext, gadgetIn
 }
 
 type KubeIPResolverInstance struct {
-	gadgetCtx      operators.GadgetContext
-	k8sInventory   common.K8sInventoryCache
-	gadgetInstance any
+	gadgetCtx          operators.GadgetContext
+	k8sInventory       common.K8sInventoryCache
+	gadgetInstance     any
+	endpointsAccessors map[datasource.DataSource][]endpointAccessors
 }
 
 func (m *KubeIPResolverInstance) Name() string {
@@ -134,6 +145,184 @@ func (m *KubeIPResolverInstance) EnrichEvent(ev any) error {
 	return nil
 }
 
+func (k *KubeIPResolver) GlobalParams() api.Params {
+	return nil
+}
+
+func (k *KubeIPResolver) InstanceParams() api.Params {
+	return nil
+}
+
+type endpointAccessors struct {
+	root            datasource.FieldAccessor
+	subK8sKind      datasource.FieldAccessor
+	subK8sName      datasource.FieldAccessor
+	subK8sNamespace datasource.FieldAccessor
+	subK8sLabels    datasource.FieldAccessor
+}
+
+func (k *KubeIPResolver) InstantiateDataOperator(gadgetCtx operators.GadgetContext, instanceParamValues api.ParamValues) (operators.DataOperatorInstance, error) {
+	logger := gadgetCtx.Logger()
+	epAccessors := make(map[datasource.DataSource][]endpointAccessors)
+	for _, ds := range gadgetCtx.GetDataSources() {
+		logger.Debugf("KubeIPResolverOperator inspecting datasource %q", ds.Name())
+
+		endpoints := ds.GetFieldsWithTag("type:" + endpointL4Type)
+		if len(endpoints) == 0 {
+			logger.Debugf("> no endpoint fields found")
+			continue
+		}
+
+		logger.Debugf("> found %d endpoint fields", len(endpoints))
+		for _, ep := range endpoints {
+			// validate the endpoint fields
+			ips := ep.GetSubFieldsWithTag("type:" + ipAddrType)
+			if len(ips) != 1 {
+				return nil, fmt.Errorf("%s: expected %d %q field, got %d", ep.Name(), 1, ipAddrType, len(ips))
+			}
+
+			if ips[0].Size() != 16 {
+				return nil, fmt.Errorf("%s: expected %q field to have size %d, got %d", ep.Name(), ipAddrType, 16, ips[0].Size())
+			}
+
+			version := ep.GetSubFieldsWithTag("name:version")
+			if len(version) != 1 {
+				return nil, fmt.Errorf("%s: expected %d %q field, got %d", ep.Name(), 1, "version", len(version))
+			}
+
+			// Add subfields for k8s metadata to the endpoint
+			k8sSubAcc, err := ep.AddSubField("k8s", api.Kind_Invalid, datasource.WithFlags(datasource.FieldFlagEmpty))
+			if err != nil {
+				return nil, fmt.Errorf("adding field %q: %w", "k8s", err)
+			}
+			k8sKindAcc, err := k8sSubAcc.AddSubField("kind", api.Kind_String,
+				datasource.WithAnnotations(map[string]string{
+					datasource.ColumnsMaxWidthAnnotation: "12",
+				}),
+				datasource.WithFlags(datasource.FieldFlagHidden),
+			)
+			if err != nil {
+				return nil, fmt.Errorf("adding field %q: %w", "kind", err)
+			}
+			k8sNameAcc, err := k8sSubAcc.AddSubField("name",
+				api.Kind_String,
+				datasource.WithAnnotations(map[string]string{
+					datasource.TemplateAnnotation: "pod",
+				}),
+				datasource.WithFlags(datasource.FieldFlagHidden),
+			)
+			if err != nil {
+				return nil, fmt.Errorf("adding field %q: %w", "name", err)
+			}
+			k8sNamespaceAcc, err := k8sSubAcc.AddSubField("namespace",
+				api.Kind_String,
+				datasource.WithAnnotations(map[string]string{
+					datasource.TemplateAnnotation: "namespace",
+				}),
+				datasource.WithFlags(datasource.FieldFlagHidden),
+			)
+			if err != nil {
+				return nil, fmt.Errorf("adding field %q: %w", "namespace", err)
+			}
+			k8sLabelsAcc, err := k8sSubAcc.AddSubField("labels", api.Kind_String, datasource.WithFlags(datasource.FieldFlagHidden))
+			if err != nil {
+				return nil, fmt.Errorf("adding field %q: %w", "labels", err)
+			}
+			ea := endpointAccessors{
+				root:            ep,
+				subK8sKind:      k8sKindAcc,
+				subK8sName:      k8sNameAcc,
+				subK8sNamespace: k8sNamespaceAcc,
+				subK8sLabels:    k8sLabelsAcc,
+			}
+			epAccessors[ds] = append(epAccessors[ds], ea)
+		}
+	}
+
+	// No endpoints found, nothing to do
+	if len(epAccessors) == 0 {
+		return nil, nil
+	}
+
+	k8sInventory, err := common.GetK8sInventoryCache()
+	if err != nil {
+		return nil, fmt.Errorf("creating k8s inventory cache: %w", err)
+	}
+
+	return &KubeIPResolverInstance{
+		k8sInventory:       k8sInventory,
+		endpointsAccessors: epAccessors,
+	}, nil
+}
+
+func (k *KubeIPResolver) Priority() int {
+	return Priority
+}
+
+func (m *KubeIPResolverInstance) PreStart(gadgetCtx operators.GadgetContext) error {
+	m.k8sInventory.Start()
+	return nil
+}
+
+func (m *KubeIPResolverInstance) Start(gadgetCtx operators.GadgetContext) error {
+	for ds, acc := range m.endpointsAccessors {
+		ds.Subscribe(func(source datasource.DataSource, data datasource.Data) error {
+			var errs error
+			for _, a := range acc {
+				ip := a.root.GetSubFieldsWithTag("type:" + ipAddrType)[0]
+				version := a.root.GetSubFieldsWithTag("name:version")[0]
+				addrStr, err := common.GetIPForVersion(data, version, ip)
+				if err != nil {
+					errors.Join(errs, fmt.Errorf("%s: getting IP: %w", a.root.Name(), err))
+					continue
+				}
+
+				pod := m.k8sInventory.GetPodByIp(addrStr)
+				if pod != nil {
+					if pod.Spec.HostNetwork {
+						continue
+					}
+					a.subK8sName.Set(data, []byte(pod.Name))
+					a.subK8sKind.Set(data, []byte("pod"))
+					a.subK8sNamespace.Set(data, []byte(pod.Namespace))
+					// TODO: labels should be a map/slice
+					var labels []string
+					for key, val := range pod.Labels {
+						labels = append(labels, fmt.Sprintf("%s=%s", key, val))
+					}
+					a.subK8sLabels.Set(data, []byte(strings.Join(labels, ",")))
+					continue
+				}
+
+				svc := m.k8sInventory.GetSvcByIp(addrStr)
+				if svc != nil {
+					a.subK8sKind.Set(data, []byte("svc"))
+					a.subK8sName.Set(data, []byte(svc.Name))
+					a.subK8sNamespace.Set(data, []byte(svc.Namespace))
+					// TODO: labels should be a map/slice
+					var labels []string
+					for key, val := range svc.Labels {
+						labels = append(labels, fmt.Sprintf("%s=%s", key, val))
+					}
+					a.subK8sLabels.Set(data, []byte(strings.Join(labels, ",")))
+				}
+			}
+			return errs
+		}, 0)
+	}
+	return nil
+}
+
+func (m *KubeIPResolverInstance) PostStop(gadgetCtx operators.GadgetContext) error {
+	m.k8sInventory.Stop()
+	return nil
+}
+
+func (m *KubeIPResolverInstance) Stop(gadgetCtx operators.GadgetContext) error {
+	return nil
+}
+
 func init() {
 	operators.Register(&KubeIPResolver{})
+	operators.RegisterDataOperator(&KubeIPResolver{})
 }

gadget-container/gadgettracermanager/main.go
@@ -53,6 +53,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/ebpf"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/filter"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/formatters"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/kubeipresolver"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/kubemanager"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/limiter"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/socketenricher"

pkg/operators/common/datasource.go
@@ -1,0 +1,49 @@
+// Copyright 2023-2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+)
+
+func GetIPForVersion(data datasource.Data, version, ipAddr datasource.FieldAccessor) (string, error) {
+	ip := ipAddr.Get(data)
+	if ip == nil {
+		return "", fmt.Errorf("IP field not found")
+	}
+
+	v, err := version.Uint8(data)
+	if err != nil {
+		return "", fmt.Errorf("getting version: %w", err)
+	}
+
+	var ipStr string
+	switch v {
+	case 4:
+		ipStr, err = net.IP(ip[:4]).String(), nil
+	case 6:
+		ipStr, err = net.IP(ip).String(), nil
+	default:
+		err = fmt.Errorf("unknown IP version: %d", v)
+	}
+	if err != nil {
+		return "", fmt.Errorf("getting IP: %w", err)
+	}
+
+	return ipStr, nil
+}

pkg/operators/common/kubeinventorycache.go
@@ -170,6 +170,9 @@ func transformObject(obj any) (any, error) {
 				HostIP: t.Status.HostIP,
 				PodIP:  t.Status.PodIP,
 			},
+			Spec: v1.PodSpec{
+				HostNetwork: t.Spec.HostNetwork,
+			},
 		}
 		return p, nil
 	case *v1.Service:

pkg/operators/formatters/formatters.go
@@ -16,7 +16,6 @@ package formatters
 
 import (
 	"fmt"
-	"net"
 	"strings"
 	"syscall"
 	"time"
@@ -30,6 +29,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/common"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/annotations"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/protocols"
@@ -181,20 +181,9 @@ func handleL3Endpoint(in datasource.FieldAccessor) (func(entry datasource.Data) 
 	in.SetHidden(false, false)
 
 	return func(entry datasource.Data) (string, error) {
-		ip := ips[0].Get(entry)
-		v, err := versions[0].Uint8(entry)
+		addrStr, err := common.GetIPForVersion(entry, versions[0], ips[0])
 		if err != nil {
-			return "", err
-		}
-
-		var addrStr string
-		switch v {
-		case 4:
-			addrStr = net.IP(ip[:4]).String()
-		case 6:
-			addrStr = net.IP(ip).String()
-		default:
-			return "", fmt.Errorf("invalid IP version %d for l4endpoint", v)
+			return "", fmt.Errorf("getting IP address: %w", err)
 		}
 
 		addrF.PutString(entry, addrStr)

commit ad63bcb938733ec0510caab4ca34fb1cd62476ce

operators/KubeIPResolver: Set kind to raw when its not a pod and svc
Fixes: b1bcb83bc5f6f470e3ecebf7bb450e9168e8f467
Signed-off-by: Burak Ok <burakok@microsoft.com>

pkg/operators/kubeipresolver/kubeipresolver.go
@@ -333,7 +333,10 @@ func (m *KubeIPResolverInstance) Start(gadgetCtx operators.GadgetContext) error 
 						v := fmt.Sprintf("s/%s/%s:%d", svc.Namespace, svc.Name, p)
 						a.column.Set(data, []byte(v))
 					}
+					continue
 				}
+
+				a.subK8sKind.Set(data, []byte("raw"))
 			}
 			return errs
 		}, Priority)