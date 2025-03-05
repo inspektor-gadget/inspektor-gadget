inspektor-gadget/inspektor-gadget

commit 7a3e62698bbc6ed763ea0b448197de2e093ed510

operators: Add wasm operator
The wasm operator handles wasm programs present on the gadget.
This commit introduces the operator, next commits will implement
functions that are exposed to the wasm guests.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>
Signed-off-by: Michael Friese <mfriese@microsoft.com>

pkg/operators/wasm/fields.go
@@ -1,0 +1,248 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wasm
+
+import (
+	"context"
+
+	"github.com/tetratelabs/wazero"
+	wapi "github.com/tetratelabs/wazero/api"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+)
+
+func (i *wasmOperatorInstance) addFieldFuncs(env wazero.HostModuleBuilder) {
+	exportFunction(env, "fieldGet", i.fieldGet,
+		[]wapi.ValueType{
+			wapi.ValueTypeI32, // Accessor
+			wapi.ValueTypeI32, // Data
+			wapi.ValueTypeI32, // Kind
+		},
+		[]wapi.ValueType{wapi.ValueTypeI64}, // Value
+	)
+
+	exportFunction(env, "fieldSet", i.fieldSet,
+		[]wapi.ValueType{
+			wapi.ValueTypeI32, // Accessor
+			wapi.ValueTypeI32, // Data
+			wapi.ValueTypeI32, // Kind
+			wapi.ValueTypeI64, // Value
+		},
+		[]wapi.ValueType{wapi.ValueTypeI32}, // Error
+	)
+}
+
+// fieldGet returns the field's value.
+// Params:
+// - stack[0]: Field handle
+// - stack[1]: Data handle
+// - stack[2]: Kind
+// Return value:
+// - Uint64 representation of the value of the field, depending on the type
+// requested, or a pointer, 0 on error.
+// TODO: error handling is still TBD as there not a way to differentiate between
+// a field with value 0 and an error.
+func (i *wasmOperatorInstance) fieldGet(ctx context.Context, m wapi.Module, stack []uint64) {
+	fieldHandle := wapi.DecodeU32(stack[0])
+	dataHandle := wapi.DecodeU32(stack[1])
+	fieldKind := api.Kind(wapi.DecodeU32(stack[2]))
+
+	field, ok := getHandle[datasource.FieldAccessor](i, fieldHandle)
+	if !ok {
+		stack[0] = 0
+		return
+	}
+	data, ok := getHandle[datasource.Data](i, dataHandle)
+	if !ok {
+		stack[0] = 0
+		return
+	}
+
+	handleBytes := func(buf []byte) uint64 {
+		res, err := i.guestMalloc.Call(ctx, uint64(len(buf)))
+		if err != nil {
+			i.logger.Warnf("malloc failed: %v", err)
+			stack[0] = 0
+			return 0
+
+		}
+
+		if !m.Memory().Write(uint32(res[0]), buf) {
+			i.logger.Warnf("out of memory write")
+			stack[0] = 0
+			return 0
+		}
+
+		return uint64(len(buf))<<32 | uint64(res[0])
+	}
+
+	var val uint64
+	var err error
+
+	switch fieldKind {
+	case api.Kind_Bool:
+		var ret bool
+		ret, err = field.Bool(data)
+		if ret {
+			val = 1
+		} else {
+			val = 0
+		}
+	case api.Kind_Int8:
+		var ret int8
+		ret, err = field.Int8(data)
+		val = uint64(ret)
+	case api.Kind_Int16:
+		var ret int16
+		ret, err = field.Int16(data)
+		val = uint64(ret)
+	case api.Kind_Int32:
+		var ret int32
+		ret, err = field.Int32(data)
+		val = uint64(ret)
+	case api.Kind_Int64:
+		var ret int64
+		ret, err = field.Int64(data)
+		val = uint64(ret)
+	case api.Kind_Uint8:
+		var ret uint8
+		ret, err = field.Uint8(data)
+		val = uint64(ret)
+	case api.Kind_Uint16:
+		var ret uint16
+		ret, err = field.Uint16(data)
+		val = uint64(ret)
+	case api.Kind_Uint32:
+		var ret uint32
+		ret, err = field.Uint32(data)
+		val = uint64(ret)
+	case api.Kind_Uint64:
+		val, err = field.Uint64(data)
+	// The guest has to pass floats using the IEEE 754 binary representation
+	case api.Kind_Float32:
+		var ret uint32
+		ret, err = field.Uint32(data)
+		val = uint64(ret)
+	case api.Kind_Float64:
+		val, err = field.Uint64(data)
+	// These are a bit special as they don't fit in the return value, so we have to
+	// allocate an array in the guest memory and return a pointer to it.
+	case api.Kind_String:
+		str, err := field.String(data)
+		if err == nil {
+			val = handleBytes([]byte(str))
+		}
+	case api.Kind_Bytes:
+		bytes, err := field.Bytes(data)
+		if err == nil {
+			val = handleBytes(bytes)
+		}
+	default:
+		i.logger.Warnf("unknown field kind: %d", stack[2])
+		stack[0] = 0
+		return
+	}
+
+	if err != nil {
+		i.logger.Warnf("fieldGet failed: %v", err)
+		stack[0] = 0
+		return
+	}
+
+	stack[0] = val
+}
+
+// fieldSet sets the field's value
+// Params:
+// - stack[0]: Field handle
+// - stack[1]: Data handle
+// - stack[2]: Kind
+// - stack[3]: Value to store
+// Return value:
+// - 0 on success, 1 on error
+func (i *wasmOperatorInstance) fieldSet(ctx context.Context, m wapi.Module, stack []uint64) {
+	fieldHandle := wapi.DecodeU32(stack[0])
+	dataHandle := wapi.DecodeU32(stack[1])
+	fieldKind := api.Kind(wapi.DecodeU32(stack[2]))
+	value := stack[3]
+
+	field, ok := getHandle[datasource.FieldAccessor](i, fieldHandle)
+	if !ok {
+		stack[0] = 1
+		return
+	}
+	data, ok := getHandle[datasource.Data](i, dataHandle)
+	if !ok {
+		stack[0] = 1
+		return
+	}
+
+	var err error
+
+	switch fieldKind {
+	case api.Kind_Bool:
+		err = field.PutBool(data, value != 0)
+	case api.Kind_Int8:
+		err = field.PutInt8(data, int8(value))
+	case api.Kind_Int16:
+		err = field.PutInt16(data, int16(value))
+	case api.Kind_Int32:
+		err = field.PutInt32(data, int32(value))
+	case api.Kind_Int64:
+		err = field.PutInt64(data, int64(value))
+	case api.Kind_Uint8:
+		err = field.PutUint8(data, uint8(value))
+	case api.Kind_Uint16:
+		err = field.PutUint16(data, uint16(value))
+	case api.Kind_Uint32:
+		err = field.PutUint32(data, uint32(value))
+	case api.Kind_Uint64:
+		err = field.PutUint64(data, uint64(value))
+	case api.Kind_Float32:
+		// The guest has to pass it using the IEEE 754 binary representation
+		err = field.PutUint32(data, uint32(value))
+	case api.Kind_Float64:
+		err = field.PutUint64(data, uint64(value))
+	case api.Kind_String:
+		var str string
+		str, err = stringFromStack(m, value)
+		if err != nil {
+			i.logger.Warnf("fieldSet: reading string from stack: %v", err)
+			stack[0] = 1
+			return
+		}
+		err = field.PutString(data, str)
+	case api.Kind_Bytes:
+		var buf []byte
+		buf, err = bufFromStack(m, value)
+		if err != nil {
+			i.logger.Warnf("reading bytes from stack: %v", err)
+			stack[0] = 1
+			return
+		}
+		err = field.PutBytes(data, buf)
+	default:
+		i.logger.Warnf("unknown field kind: %d", uint32(stack[2]))
+		stack[0] = 1
+		return
+	}
+
+	if err != nil {
+		i.logger.Warnf("fieldSet failed: %v", err)
+		stack[0] = 1
+		return
+	}
+}

wasmapi/go/fields.go
@@ -1,0 +1,191 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"errors"
+	"math"
+	"runtime"
+)
+
+//go:wasmimport env fieldGet
+func fieldGet(field uint32, data uint32, kind uint32) uint64
+
+//go:wasmimport env fieldSet
+func fieldSet(field uint32, data uint32, kind uint32, value uint64) uint32
+
+var errSetField = errors.New("error setting field")
+
+func (f Field) Int8(data Data) (int8, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Int8))
+	return int8(val), nil
+}
+
+func (f Field) SetInt8(data Data, value int8) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Int8), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Int16(data Data) (int16, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Int16))
+	return int16(val), nil
+}
+
+func (f Field) SetInt16(data Data, value int16) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Int16), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Int32(data Data) (int32, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Int32))
+	return int32(val), nil
+}
+
+func (f Field) SetInt32(data Data, value int32) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Int32), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Int64(data Data) (int64, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Int64))
+	return int64(val), nil
+}
+
+func (f Field) SetInt64(data Data, value int64) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Int64), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Uint8(data Data) (uint8, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Uint8))
+	return uint8(val), nil
+}
+
+func (f Field) SetUint8(data Data, value uint8) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Uint8), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Uint16(data Data) (uint16, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Uint16))
+	return uint16(val), nil
+}
+
+func (f Field) SetUint16(data Data, value uint16) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Uint16), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Uint32(data Data) (uint32, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Uint32))
+	return uint32(val), nil
+}
+
+func (f Field) SetUint32(data Data, value uint32) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Uint32), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Uint64(data Data) (uint64, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Uint64))
+	return uint64(val), nil
+}
+
+func (f Field) SetUint64(data Data, value uint64) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Uint64), uint64(value))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Float32(data Data) (float32, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Float32))
+	return math.Float32frombits(uint32(val)), nil
+}
+
+func (f Field) SetFloat32(data Data, value float32) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Float32), uint64(math.Float32bits(value)))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Float64(data Data) (float64, error) {
+	val := fieldGet(uint32(f), uint32(data), uint32(Kind_Float64))
+	return math.Float64frombits(uint64(val)), nil
+}
+
+func (f Field) SetFloat64(data Data, value float64) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Float64), uint64(math.Float64bits(value)))
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) String(data Data) (string, error) {
+	str := bufPtr(fieldGet(uint32(f), uint32(data), uint32(Kind_String)))
+	ret := str.string()
+	str.free()
+	return ret, nil
+}
+
+func (f Field) SetString(data Data, str string) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_String), uint64(stringToBufPtr(str)))
+	runtime.KeepAlive(str)
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}
+
+func (f Field) Bytes(data Data) ([]byte, error) {
+	buf := bufPtr(fieldGet(uint32(f), uint32(data), uint32(Kind_Bytes)))
+	ret := buf.bytes()
+	buf.free()
+	return ret, nil
+}
+
+func (f Field) SetBytes(data Data, buf []byte) error {
+	ret := fieldSet(uint32(f), uint32(data), uint32(Kind_Bytes), uint64(bytesToBufPtr(buf)))
+	runtime.KeepAlive(buf)
+	if ret != 0 {
+		return errSetField
+	}
+	return nil
+}

cmd/ig/main.go
@@ -48,6 +48,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/prometheus"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/socketenricher"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/uidgidresolver"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/wasm"
 )
 
 func main() {

gadget-container/gadgettracermanager/main.go
@@ -51,6 +51,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/kubemanager"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/socketenricher"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/uidgidresolver"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/wasm"
 
 	gadgetservice "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"

go.mod
@@ -73,6 +73,7 @@ require (
 	github.com/golang/protobuf v1.5.4
 	github.com/google/gopacket v1.1.19
 	github.com/sigstore/sigstore v1.8.4
+	github.com/tetratelabs/wazero v1.7.1
 )
 
 require (

go.sum
@@ -433,6 +433,8 @@ github.com/subosito/gotenv v1.6.0 h1:9NlTDc1FTs4qu0DDq7AEtTPNw6SVm7uBMsUCUjABIf8
 github.com/subosito/gotenv v1.6.0/go.mod h1:Dk4QP5c2W3ibzajGcXpNraDfq2IrhjMIvMSWPKKo0FU=
 github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635 h1:kdXcSzyDtseVEc4yCz2qF8ZrQvIDBJLl4S1c3GCXmoI=
 github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635/go.mod h1:hkRG7XYTFWNJGYcbNJQlaLq0fg1yr4J4t/NcTQtrfww=
+github.com/tetratelabs/wazero v1.7.1 h1:QtSfd6KLc41DIMpDYlJdoMc6k7QTN246DM2+n2Y/Dx8=
+github.com/tetratelabs/wazero v1.7.1/go.mod h1:ytl6Zuh20R/eROuyDaGPkp82O9C/DJfXAwJfQ3X6/7Y=
 github.com/titanous/rocacheck v0.0.0-20171023193734-afe73141d399 h1:e/5i7d4oYZ+C1wj2THlRK+oAhjeS/TRQwMfkIuet3w0=
 github.com/titanous/rocacheck v0.0.0-20171023193734-afe73141d399/go.mod h1:LdwHTNJT99C5fTAzDz0ud328OgXz+gierycbcIx2fRs=
 github.com/tklauser/numcpus v0.8.0 h1:Mx4Wwe/FjZLeQsK/6kt2EOepwwSl7SmJrK5bV/dXYgY=

commit 114579bbf9e510b33a7eeaa15d3bb6995a56917b

operators/wasm: Fix return code for fieldSet
Stack[0] was never assigned to zero, it caused the different set functions
to return an error when the operation was successful.

Fixes: 7a3e62698bbc ("operators: Add wasm operator")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/wasm/fields.go
@@ -293,6 +293,8 @@ func (i *wasmOperatorInstance) fieldSet(ctx context.Context, m wapi.Module, stac
 		stack[0] = 1
 		return
 	}
+
+	stack[0] = 0
 }
 
 // fieldAddTag adds a tag to the field

pkg/operators/wasm/testdata/badguest/program.go
@@ -294,7 +294,7 @@ func gadgetInit() int32 {
 	dataHandle := dataSourceNewPacketSingle(dsSingleHandle)
 	assertNonZero(dataHandle, "dataSourceNewPacketSingle: creating new")
 
-	assertNonZero(fieldSet(fieldHandle, dataHandle, uint32(api.Kind_Uint32), 1234), "fieldSet: ok")
+	assertZero(fieldSet(fieldHandle, dataHandle, uint32(api.Kind_Uint32), 1234), "fieldSet: ok")
 	assertNonZero(fieldSet(fieldHandle, dataHandle, uint32(api.Kind_Uint64), 1234), "fieldSet: bad kind")
 	assertNonZero(fieldSet(fieldHandle, dataHandle, 1005, 1234), "fieldSet: bad kind")
 	assertNonZero(fieldSet(fieldHandle, fieldHandle, uint32(api.Kind_Uint32), 1234), "fieldSet: bad data handle")

pkg/operators/wasm/testdata/fields/program.go
@@ -15,6 +15,8 @@
 package main
 
 import (
+	"fmt"
+
 	api "github.com/inspektor-gadget/inspektor-gadget/wasmapi/go"
 )
 
@@ -76,29 +78,33 @@ func gadgetInit() int32 {
 		for _, f := range fields {
 			switch f.typ {
 			case api.Kind_Int8:
-				f.acc.SetInt8(data, f.val.(int8))
+				err = f.acc.SetInt8(data, f.val.(int8))
 			case api.Kind_Int16:
-				f.acc.SetInt16(data, f.val.(int16))
+				err = f.acc.SetInt16(data, f.val.(int16))
 			case api.Kind_Int32:
-				f.acc.SetInt32(data, f.val.(int32))
+				err = f.acc.SetInt32(data, f.val.(int32))
 			case api.Kind_Int64:
-				f.acc.SetInt64(data, f.val.(int64))
+				err = f.acc.SetInt64(data, f.val.(int64))
 			case api.Kind_Uint8:
-				f.acc.SetUint8(data, f.val.(uint8))
+				err = f.acc.SetUint8(data, f.val.(uint8))
 			case api.Kind_Uint16:
-				f.acc.SetUint16(data, f.val.(uint16))
+				err = f.acc.SetUint16(data, f.val.(uint16))
 			case api.Kind_Uint32:
-				f.acc.SetUint32(data, f.val.(uint32))
+				err = f.acc.SetUint32(data, f.val.(uint32))
 			case api.Kind_Uint64:
-				f.acc.SetUint64(data, f.val.(uint64))
+				err = f.acc.SetUint64(data, f.val.(uint64))
 			case api.Kind_Float32:
-				f.acc.SetFloat32(data, f.val.(float32))
+				err = f.acc.SetFloat32(data, f.val.(float32))
 			case api.Kind_Float64:
-				f.acc.SetFloat64(data, f.val.(float64))
+				err = f.acc.SetFloat64(data, f.val.(float64))
 			case api.Kind_String:
-				f.acc.SetString(data, f.val.(string))
+				err = f.acc.SetString(data, f.val.(string))
 			case api.Kind_Bytes:
-				f.acc.SetBytes(data, f.val.([]byte))
+				err = f.acc.SetBytes(data, f.val.([]byte))
+			}
+
+			if err != nil {
+				panic(fmt.Sprintf("failed to set field: %v", err))
 			}
 		}
 	}, 0)