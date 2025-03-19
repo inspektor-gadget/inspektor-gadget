inspektor-gadget/inspektor-gadget

commit 12453a012de6187edb93f2ee9f25d3bbadd45059

pkg/oci: Use a file lock to avoid concurrency issues
Invoking multiple instances of the ig binary on parallel causes issues
as the oras library doesn't provide any multi process syncronization
guarantees on the index.json file. This commit fixes that by using a
file lock and some logic to detect when this file has been updated by
another process.

Specifically, this commit::
- Disables AutoSaveIndex to have control over when the index.json file
  is written.
- Uses a file lock to be sure only one process is writing to the
  index.json file at a time.
- Adds some logic to retry an operation when the index.json file has been
  updated by another process.

Different alternatives were considered:
- big lock: It caused issues as the critical section was too big and
  different instances of ig timeout while running gadgets.
- merge index: Instead of retry the whole operation when the index.json
  file was written by another process, we could merge it with the changes.
  However it's difficult to implement as it'll need the oras library to
  provide a list of changes on the index to apply.
- use a db to store the index: It probably requires a lot of changes to
  the oras library.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/oci/oci.go
@@ -33,7 +33,6 @@ import (
 	"runtime"
 	"slices"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/distribution/reference"
@@ -96,15 +95,6 @@ const (
 	localhost = "localhost"
 )
 
-// getLocalOciStore returns a single local oci store. oci.Store is concurrently safe only
-// against its own instance inside the same go program
-var getLocalOciStore = sync.OnceValues(func() (*oci.Store, error) {
-	if err := os.MkdirAll(filepath.Dir(defaultOciStore), 0o700); err != nil {
-		return nil, err
-	}
-	return oci.New(defaultOciStore)
-})
-
 // GadgetImageDesc is the description of a gadget image.
 type GadgetImageDesc struct {
 	Repository string `column:"repository"`
@@ -126,12 +116,32 @@ func getTimeFromAnnotations(annotations map[string]string) string {
 
 // PullGadgetImage pulls the gadget image into the local oci store and returns its descriptor.
 func PullGadgetImage(ctx context.Context, image string, authOpts *AuthOptions) (*GadgetImageDesc, error) {
-	ociStore, err := getLocalOciStore()
+	var desc *GadgetImageDesc
+	err := retry("PullGadgetImage", func() error {
+		var err error
+		desc, err = pullGadgetImage(ctx, image, authOpts)
+		return err
+	})
+
+	return desc, err
+}
+
+func pullGadgetImage(ctx context.Context, image string, authOpts *AuthOptions) (*GadgetImageDesc, error) {
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return nil, fmt.Errorf("getting oci store: %w", err)
 	}
 
-	return pullGadgetImageToStore(ctx, ociStore, image, authOpts)
+	desc, err := pullGadgetImageToStore(ctx, ociStore.Store, image, authOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ociStore.saveIndexWithLock(); err != nil {
+		return nil, err
+	}
+
+	return desc, nil
 }
 
 // pullGadgetImageToStore pulls the gadget image into the given store and returns its descriptor.
@@ -201,7 +211,7 @@ func pullImage(ctx context.Context, targetImage reference.Named, imageStore oras
 
 // PushGadgetImage pushes the gadget image and returns its descriptor.
 func PushGadgetImage(ctx context.Context, image string, authOpts *AuthOptions) (*GadgetImageDesc, error) {
-	ociStore, err := getLocalOciStore()
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return nil, fmt.Errorf("getting oci store: %w", err)
 	}
@@ -233,6 +243,17 @@ func PushGadgetImage(ctx context.Context, image string, authOpts *AuthOptions) (
 
 // TagGadgetImage tags the src image with the dst image.
 func TagGadgetImage(ctx context.Context, srcImage, dstImage string) (*GadgetImageDesc, error) {
+	var desc *GadgetImageDesc
+	err := retry("TagGadgetImage", func() error {
+		var err error
+		desc, err = tagGadgetImage(ctx, srcImage, dstImage)
+		return err
+	})
+
+	return desc, err
+}
+
+func tagGadgetImage(ctx context.Context, srcImage, dstImage string) (*GadgetImageDesc, error) {
 	src, err := normalizeImageName(srcImage)
 	if err != nil {
 		return nil, fmt.Errorf("normalizing src image: %w", err)
@@ -242,7 +263,7 @@ func TagGadgetImage(ctx context.Context, srcImage, dstImage string) (*GadgetImag
 		return nil, fmt.Errorf("normalizing dst image: %w", err)
 	}
 
-	ociStore, err := getLocalOciStore()
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return nil, fmt.Errorf("getting oci store: %w", err)
 	}
@@ -252,7 +273,14 @@ func TagGadgetImage(ctx context.Context, srcImage, dstImage string) (*GadgetImag
 		// Error message not that helpful
 		return nil, fmt.Errorf("resolving src: %w", err)
 	}
-	ociStore.Tag(context.TODO(), targetDescriptor, dst.String())
+
+	if err := ociStore.Tag(context.TODO(), targetDescriptor, dst.String()); err != nil {
+		return nil, fmt.Errorf("tagging image: %w", err)
+	}
+
+	if err := ociStore.saveIndexWithLock(); err != nil {
+		return nil, err
+	}
 
 	imageDesc := &GadgetImageDesc{
 		Repository: dst.Name(),
@@ -296,7 +324,7 @@ func sortIndex(indexPath string) (*ocispec.Index, error) {
 }
 
 func ExportGadgetImages(ctx context.Context, dstFile string, images ...string) error {
-	ociStore, err := getLocalOciStore()
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return fmt.Errorf("getting oci store: %w", err)
 	}
@@ -353,7 +381,7 @@ func ImportGadgetImages(ctx context.Context, srcFile string) ([]string, error) {
 		return nil, fmt.Errorf("loading src bundle: %w", err)
 	}
 
-	ociStore, err := getLocalOciStore()
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return nil, fmt.Errorf("getting oci store: %w", err)
 	}
@@ -482,12 +510,12 @@ func getGadgetImages(ctx context.Context, store *oci.Store) ([]*GadgetImageDesc,
 
 // GetGadgetImages gets all the gadget images.
 func GetGadgetImages(ctx context.Context) ([]*GadgetImageDesc, error) {
-	ociStore, err := getLocalOciStore()
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return nil, fmt.Errorf("getting oci store: %w", err)
 	}
 
-	images, err := getGadgetImages(ctx, ociStore)
+	images, err := getGadgetImages(ctx, ociStore.Store)
 	if err != nil {
 		return nil, fmt.Errorf("listing all tags: %w", err)
 	}
@@ -500,7 +528,7 @@ func GetGadgetImages(ctx context.Context) ([]*GadgetImageDesc, error) {
 }
 
 func GetGadgetImageDesc(ctx context.Context, image string) (*GadgetImageDesc, error) {
-	ociStore, err := getLocalOciStore()
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return nil, fmt.Errorf("getting oci store: %w", err)
 	}
@@ -511,7 +539,7 @@ func GetGadgetImageDesc(ctx context.Context, image string) (*GadgetImageDesc, er
 	}
 
 	fullName := targetImage.String()
-	desc, err := getGadgetImageDescriptor(ctx, ociStore, fullName)
+	desc, err := getGadgetImageDescriptor(ctx, ociStore.Store, fullName)
 	if err != nil {
 		return nil, fmt.Errorf("getting gadget image descriptor: %w", err)
 	}
@@ -521,7 +549,13 @@ func GetGadgetImageDesc(ctx context.Context, image string) (*GadgetImageDesc, er
 
 // DeleteGadgetImage removes the given image.
 func DeleteGadgetImage(ctx context.Context, image string) error {
-	ociStore, err := getLocalOciStore()
+	return retry("DeleteGadgetImage", func() error {
+		return deleteGadgetImage(ctx, image)
+	})
+}
+
+func deleteGadgetImage(ctx context.Context, image string) error {
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return fmt.Errorf("getting oci store: %w", err)
 	}
@@ -537,7 +571,7 @@ func DeleteGadgetImage(ctx context.Context, image string) error {
 		return fmt.Errorf("resolving image: %w", err)
 	}
 
-	images, err := getGadgetImages(ctx, ociStore)
+	images, err := getGadgetImages(ctx, ociStore.Store)
 	if err != nil {
 		return fmt.Errorf("listing images: %w", err)
 	}
@@ -555,15 +589,23 @@ func DeleteGadgetImage(ctx context.Context, image string) error {
 			// Where foo and bar are different names referencing the same image, as
 			// the digest shows.
 			// In this case, we just untag the image name given by the user.
-			return ociStore.Untag(ctx, fullName)
+			if err := ociStore.Untag(ctx, fullName); err != nil {
+				return err
+			}
+
+			return ociStore.saveIndexWithLock()
 		}
 	}
 
-	err = ociStore.Delete(ctx, descriptor)
-	if err != nil {
+	if err = ociStore.Delete(ctx, descriptor); err != nil {
 		return err
 	}
 
+	if err := ociStore.saveIndexWithLock(); err != nil {
+		return err
+	}
+
+	// TODO: GC() could race with other processes calling it a the same time.
 	return ociStore.GC(ctx)
 }
 
@@ -976,12 +1018,18 @@ func ensureImage(ctx context.Context, imageStore oras.Target, image string, imgO
 
 // EnsureImage ensures the image is present in the local store
 func EnsureImage(ctx context.Context, image string, imgOpts *ImageOptions, pullPolicy string) error {
-	imageStore, err := getLocalOciStore()
-	if err != nil {
-		return fmt.Errorf("getting local oci store: %w", err)
-	}
+	return retry("EnsureImage", func() error {
+		imageStore, err := newLocalOciStore()
+		if err != nil {
+			return fmt.Errorf("getting local oci store: %w", err)
+		}
 
-	return ensureImage(ctx, imageStore, image, imgOpts, pullPolicy)
+		if err := ensureImage(ctx, imageStore, image, imgOpts, pullPolicy); err != nil {
+			return err
+		}
+
+		return imageStore.saveIndexWithLock()
+	})
 }
 
 func getManifestForHost(ctx context.Context, target oras.ReadOnlyTarget, image string) (*ocispec.Manifest, error) {
@@ -1018,7 +1066,7 @@ func getManifestForHost(ctx context.Context, target oras.ReadOnlyTarget, image s
 func GetManifestForHost(ctx context.Context, target oras.ReadOnlyTarget, image string) (*ocispec.Manifest, error) {
 	if target == nil {
 		var err error
-		target, err = getLocalOciStore()
+		target, err = newLocalOciStore()
 		if err != nil {
 			return nil, fmt.Errorf("getting local oci store: %w", err)
 		}
@@ -1044,7 +1092,7 @@ func getIndex(ctx context.Context, target oras.ReadOnlyTarget, image string) (*o
 func GetContentFromDescriptor(ctx context.Context, target oras.ReadOnlyTarget, desc ocispec.Descriptor) (io.ReadCloser, error) {
 	if target == nil {
 		var err error
-		target, err = getLocalOciStore()
+		target, err = newLocalOciStore()
 		if err != nil {
 			return nil, fmt.Errorf("getting local oci store: %w", err)
 		}

examples/go.mod
@@ -58,6 +58,7 @@ require (
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gofrs/flock v0.12.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect

examples/go.sum
@@ -120,6 +120,8 @@ github.com/go-test/deep v1.1.1/go.mod h1:5C2ZWiW0ErCdrYzpqxLbTX7MG14M9iiw8DgHncV
 github.com/godbus/dbus/v5 v5.0.4/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
 github.com/godbus/dbus/v5 v5.1.0 h1:4KLkAxT3aOY8Li4FRJe/KvhoNFFxo0m6fNuFUO8QJUk=
 github.com/godbus/dbus/v5 v5.1.0/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
+github.com/gofrs/flock v0.12.1 h1:MTLVXXHf8ekldpJk3AKicLij9MdwOWkZ+a/jHHZby9E=
+github.com/gofrs/flock v0.12.1/go.mod h1:9zxTsyu5xtJ9DK+1tFZyibEV7y3uwDxPPfbxeeHCoD0=
 github.com/gogo/protobuf v1.3.2 h1:Ov1cvc58UF3b5XjBnZv7+opcTcQFZebYjWzi34vdm4Q=
 github.com/gogo/protobuf v1.3.2/go.mod h1:P1XiOD3dCwIKUDQYPy72D8LYyHL2YPYrpS2s69NZV8Q=
 github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b/go.mod h1:SBH7ygxi8pfUlaOkMMuAQtPIUF8ecWP5IEl/CR7VP2Q=

go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/giantswarm/crd-docs-generator v0.11.2
 	github.com/godbus/dbus/v5 v5.1.0
+	github.com/gofrs/flock v0.12.1
 	github.com/google/go-cmp v0.6.0
 	github.com/google/uuid v1.6.0
 	github.com/gopacket/gopacket v1.3.1

go.sum
@@ -167,6 +167,8 @@ github.com/go-test/deep v1.1.1/go.mod h1:5C2ZWiW0ErCdrYzpqxLbTX7MG14M9iiw8DgHncV
 github.com/godbus/dbus/v5 v5.0.4/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
 github.com/godbus/dbus/v5 v5.1.0 h1:4KLkAxT3aOY8Li4FRJe/KvhoNFFxo0m6fNuFUO8QJUk=
 github.com/godbus/dbus/v5 v5.1.0/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
+github.com/gofrs/flock v0.12.1 h1:MTLVXXHf8ekldpJk3AKicLij9MdwOWkZ+a/jHHZby9E=
+github.com/gofrs/flock v0.12.1/go.mod h1:9zxTsyu5xtJ9DK+1tFZyibEV7y3uwDxPPfbxeeHCoD0=
 github.com/gogo/protobuf v1.3.2 h1:Ov1cvc58UF3b5XjBnZv7+opcTcQFZebYjWzi34vdm4Q=
 github.com/gogo/protobuf v1.3.2/go.mod h1:P1XiOD3dCwIKUDQYPy72D8LYyHL2YPYrpS2s69NZV8Q=
 github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b/go.mod h1:SBH7ygxi8pfUlaOkMMuAQtPIUF8ecWP5IEl/CR7VP2Q=

pkg/oci/build.go
@@ -79,7 +79,18 @@ type BuildGadgetImageOpts struct {
 // the "name:tag" format is used to name and tag the created image. If it's empty the image is not
 // named.
 func BuildGadgetImage(ctx context.Context, opts *BuildGadgetImageOpts, image string) (*GadgetImageDesc, error) {
-	ociStore, err := getLocalOciStore()
+	var desc *GadgetImageDesc
+	err := retry("BuildGadgetImage", func() error {
+		var err error
+		desc, err = buildGadgetImage(ctx, opts, image)
+		return err
+	})
+
+	return desc, err
+}
+
+func buildGadgetImage(ctx context.Context, opts *BuildGadgetImageOpts, image string) (*GadgetImageDesc, error) {
+	ociStore, err := newLocalOciStore()
 	if err != nil {
 		return nil, fmt.Errorf("getting oci store: %w", err)
 	}
@@ -120,6 +131,10 @@ func BuildGadgetImage(ctx context.Context, opts *BuildGadgetImageOpts, image str
 		if ref, ok := targetImage.(reference.Tagged); ok {
 			imageDesc.Tag = ref.Tag()
 		}
+	}
+
+	if err := ociStore.saveIndexWithLock(); err != nil {
+		return nil, err
 	}
 
 	if err := fixGeneratedFilesOwner(opts); err != nil {

pkg/oci/local_oci_store.go
@@ -1,0 +1,115 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package oci
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"reflect"
+
+	"github.com/gofrs/flock"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"oras.land/oras-go/v2/content/oci"
+)
+
+type localOciStore struct {
+	*oci.Store
+
+	indexPath  string
+	oldIndex   *ocispec.Index
+	indexFlock *flock.Flock
+}
+
+// newLocalOciStore returns a localOciStore that is safe when executed
+// concurrently, even from different processes.
+func newLocalOciStore() (*localOciStore, error) {
+	if err := os.MkdirAll(filepath.Dir(defaultOciStore), 0o700); err != nil {
+		return nil, err
+	}
+
+	indexPath := path.Join(defaultOciStore, "index.json")
+	indexLock := flock.New(path.Join(defaultOciStore, "index.json.lock"))
+
+	// lock the file before reading the index below
+	indexLock.RLock()
+	defer indexLock.Unlock()
+
+	ociStore, err := oci.New(defaultOciStore)
+	if err != nil {
+		return nil, err
+	}
+	ociStore.AutoSaveIndex = false
+
+	oldIndex, err := readIndexFile(indexPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &localOciStore{
+		Store:      ociStore,
+		indexPath:  indexPath,
+		oldIndex:   oldIndex,
+		indexFlock: indexLock,
+	}, nil
+}
+
+func (o *localOciStore) saveIndexWithLock() error {
+	o.indexFlock.Lock()
+	defer o.indexFlock.Unlock()
+
+	currentIndex, err := readIndexFile(o.indexPath)
+	if err != nil {
+		return err
+	}
+
+	if !reflect.DeepEqual(currentIndex, o.oldIndex) {
+		return errRetry
+	}
+
+	if err := o.SaveIndex(); err != nil {
+		return fmt.Errorf("saving index: %w", err)
+	}
+
+	// Update the old index to the new one. Ideally we should get index from
+	// oci.Store but it's not exposed, so read this from the index file instead.
+	indexUpdated, err := readIndexFile(o.indexPath)
+	if err != nil {
+		return err
+	}
+
+	o.oldIndex = indexUpdated
+
+	return nil
+}
+
+// readIndexFile reads index.json from the file system.
+func readIndexFile(indexPath string) (*ocispec.Index, error) {
+	indexFile, err := os.Open(indexPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer indexFile.Close()
+
+	var index ocispec.Index
+	if err := json.NewDecoder(indexFile).Decode(&index); err != nil {
+		return nil, fmt.Errorf("decoding index file: %w", err)
+	}
+
+	return &index, nil
+}

pkg/oci/retry.go
@@ -1,0 +1,56 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package oci
+
+import (
+	"errors"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	retryLimit = 20
+	retryDelay = 500 * time.Millisecond
+)
+
+// errRetry hints that the function can safely be tried again after a small
+// time.
+var errRetry = errors.New("retry")
+
+// ErrRetryLimitExceeded indicates the function was retried too many times.
+var ErrRetryLimitExceeded = errors.New("retry limit exceeded")
+
+// retry calls the fn callback multiple times until it succeeds.
+//
+// The fn callback should return errRetry to indicate that it failed but it is
+// safe to try it again after waiting a small time. If the retry limit is
+// reached, the function returns ErrRetryLimitExceeded.
+func retry(name string, fn func() error) error {
+	for i := 0; i < retryLimit; i++ {
+		err := fn()
+		if err == nil {
+			return nil
+		}
+		if !errors.Is(err, errRetry) {
+			return err
+		}
+
+		log.Debugf("retrying %s (%d of %d)", name, i+1, retryLimit)
+		time.Sleep(retryDelay)
+	}
+
+	return ErrRetryLimitExceeded
+}

commit d7a466665873a4c0fa6900d3cbcc22e56fb34b63

oci: call `saveIndexWithLock` after importing gadget images
The updated OCI store didn't get written to disk after importing a gadget image,
so that importing had no effect.

Fixes: 12453a012de6 ("pkg/oci: Use a file lock to avoid concurrency issues")
Fixes: #4208

Signed-off-by: Michael Friese <mfriese@microsoft.com>

pkg/oci/oci.go
@@ -1,4 +1,4 @@
-// Copyright 2023-2024 The Inspektor Gadget authors
+// Copyright 2023-2025 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -399,6 +399,10 @@ func ImportGadgetImages(ctx context.Context, srcFile string) ([]string, error) {
 		}
 		return nil
 	})
+
+	if err := ociStore.saveIndexWithLock(); err != nil {
+		return nil, err
+	}
 
 	return ret, err
 }