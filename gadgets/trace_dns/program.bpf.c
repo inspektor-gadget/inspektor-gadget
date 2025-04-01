inspektor-gadget/inspektor-gadget

commit 04c60a7421824cded611598b25f714a3ca63e845

gadgets: Uniform filtering for networking gadgets
Add a new gadget_should_discard_data_by_skb() helper and use it from
different neworking gadgets.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

gadgets/trace_dns/program.bpf.c
@@ -10,16 +10,16 @@
 #include <linux/types.h>
 #include <linux/udp.h>
 #include <sys/socket.h>
-#include <stdbool.h>
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
+#define GADGET_TYPE_NETWORKING
+
 #include <gadget/macros.h>
 #include <gadget/types.h>
-
-#define GADGET_TYPE_NETWORKING
 #include <gadget/sockets-map.h>
+#include <gadget/filter.h>
 
 // Don't include <gadget/filesystem.h> in networking gadgets
 #define GADGET_PATH_MAX 512
@@ -356,9 +356,13 @@ int ig_trace_dns(struct __sk_buff *skb)
 		break;
 	}
 
-	// Enrich event with process metadata
 	struct sockets_value *skb_val = gadget_socket_lookup(skb);
+	if (gadget_should_discard_data_by_skb(skb_val))
+		return 0;
+
+	// Enrich event with process metadata
 	gadget_process_populate_from_socket(skb_val, &event->proc);
+
 	if (paths) {
 		bpf_probe_read_kernel_str(&event->cwd, sizeof(event->cwd),
 					  skb_val->cwd);

include/gadget/filter.h
@@ -12,8 +12,11 @@
 #include <gadget/macros.h>
 #include <gadget/types.h>
 #include <gadget/mntns_filter.h>
+#ifndef GADGET_TYPE_NETWORKING
+#include <gadget/mntns.h>
+#endif
 
-#define GADGET_INVALID_ID ((uid_t) - 1)
+#define GADGET_INVALID_ID ((gadget_uid) - 1)
 
 const volatile gadget_pid targ_pid = 0;
 GADGET_PARAM(targ_pid);
@@ -78,6 +81,7 @@ gadget_should_discard_data(gadget_mntns_id mntns_id, gadget_pid pid,
 	       gadget_should_discard_comm(comm);
 }
 
+#ifndef GADGET_TYPE_NETWORKING
 // gadget_should_discard_data_current returns true if the gadget should drop
 // this event. This function uses the current task mount namespace, pid, tid,
 // uid, and gid to determine if the event should be dropped. This function is
@@ -118,6 +122,25 @@ static __always_inline bool gadget_should_discard_data_current()
 	}
 
 	return false;
+}
+#endif
+
+// gadget_should_discard_data_by_skb returns true if the gadget should skip this
+// event based on the fields of the skb_val struct.
+static __always_inline bool
+gadget_should_discard_data_by_skb(struct sockets_value *skb_val)
+{
+	if (!skb_val) {
+		// drop the packet if any filtering is specified
+		if (gadget_filter_by_mntns || targ_pid != 0 || targ_tid != 0 ||
+		    targ_uid != GADGET_INVALID_ID ||
+		    targ_gid != GADGET_INVALID_ID || targ_comm[0] != '\0')
+			return true;
+	}
+
+	return gadget_should_discard_data(
+		skb_val->mntns, skb_val->pid_tgid >> 32, skb_val->pid_tgid,
+		skb_val->task, skb_val->uid_gid, skb_val->uid_gid >> 32);
 }
 
 #endif

gadgets/fsnotify/program.bpf.c
@@ -10,6 +10,7 @@
 #include <gadget/buffer.h>
 #include <gadget/filesystem.h>
 #include <gadget/macros.h>
+#include <gadget/mntns.h>
 #include <gadget/mntns_filter.h>
 #include <gadget/types.h>
 #include <gadget/core_fixes.bpf.h>

gadgets/trace_sni/program.bpf.c
@@ -13,12 +13,12 @@
 
 #define GADGET_NO_BUF_RESERVE
 #define MAX_EVENT_SIZE 512
+#define GADGET_TYPE_NETWORKING
 #include <gadget/macros.h>
 #include <gadget/buffer.h>
 #include <gadget/types.h>
-
-#define GADGET_TYPE_NETWORKING
 #include <gadget/sockets-map.h>
+#include <gadget/filter.h>
 
 #define TLS_CONTENT_TYPE_HANDSHAKE 0x16
 #define TLS_HANDSHAKE_TYPE_CLIENT_HELLO 0x1
@@ -208,6 +208,10 @@ int ig_trace_sni(struct __sk_buff *skb)
 	if (read == 0)
 		return 0;
 
+	struct sockets_value *skb_val = gadget_socket_lookup(skb);
+	if (gadget_should_discard_data_by_skb(skb_val))
+		return 0;
+
 	struct event_t event = {
 		0,
 	};
@@ -221,8 +225,7 @@ int ig_trace_sni(struct __sk_buff *skb)
 	event.timestamp_raw = bpf_ktime_get_boot_ns();
 
 	// Enrich event with process metadata
-	gadget_process_populate_from_socket(gadget_socket_lookup(skb),
-					    &event.proc);
+	gadget_process_populate_from_socket(skb_val, &event.proc);
 
 	gadget_output_buf(skb, &events, &event, sizeof(event));
 

commit 6a2c5e949cbfc464b40d7b61196c9f2e1cffffaf

filter: fix verifier error in gadget_should_discard_data_by_skb
gadget_should_discard_data_by_skb() dereferences a NULL pointer with
"skb_val->mntns" when skb_val is NULL.

Fixes: 04c60a742182 ("gadgets: Uniform filtering for networking gadgets")

Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

gadgets/trace_dns/program.bpf.c
@@ -363,7 +363,7 @@ int ig_trace_dns(struct __sk_buff *skb)
 	// Enrich event with process metadata
 	gadget_process_populate_from_socket(skb_val, &event->proc);
 
-	if (paths) {
+	if (paths && skb_val != NULL) {
 		if (bpf_core_enum_value_exists(
 			    enum bpf_func_id, BPF_FUNC_probe_read_kernel_str)) {
 			bpf_probe_read_kernel_str(

include/gadget/filter.h
@@ -136,6 +136,7 @@ gadget_should_discard_data_by_skb(struct sockets_value *skb_val)
 		    targ_uid != GADGET_INVALID_ID ||
 		    targ_gid != GADGET_INVALID_ID || targ_comm[0] != '\0')
 			return true;
+		return false;
 	}
 
 	return gadget_should_discard_data(