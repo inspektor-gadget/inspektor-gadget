inspektor-gadget/inspektor-gadget

commit 7386530c3378b147ed838bf5b987c7e6dea1cb3c

operators/wasm: Add functions to interact with perf buffer.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/wasm/perf.go
@@ -1,0 +1,224 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wasm
+
+import (
+	"context"
+	"errors"
+	"os"
+	"time"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/perf"
+	"github.com/tetratelabs/wazero"
+	wapi "github.com/tetratelabs/wazero/api"
+)
+
+func (i *wasmOperatorInstance) addPerfFuncs(env wazero.HostModuleBuilder) {
+	exportFunction(env, "newPerfReader", i.newPerfReader,
+		[]wapi.ValueType{
+			wapi.ValueTypeI32, // Perf map handle
+			wapi.ValueTypeI32, // Size
+			wapi.ValueTypeI32, // Overwritable
+		},
+		[]wapi.ValueType{wapi.ValueTypeI32}, // PerfReader
+	)
+
+	exportFunction(env, "perfReaderPause", i.perfReaderPause,
+		[]wapi.ValueType{
+			wapi.ValueTypeI32, // PerfReader
+		},
+		[]wapi.ValueType{wapi.ValueTypeI32}, // Error
+	)
+
+	exportFunction(env, "perfReaderResume", i.perfReaderResume,
+		[]wapi.ValueType{
+			wapi.ValueTypeI32, // PerfReader
+		},
+		[]wapi.ValueType{wapi.ValueTypeI32}, // Error
+	)
+
+	exportFunction(env, "perfReaderRead", i.perfReaderRead,
+		[]wapi.ValueType{
+			wapi.ValueTypeI32, // PerfReader
+			wapi.ValueTypeI32, // Buf pointer address
+		},
+		[]wapi.ValueType{wapi.ValueTypeI32}, // Error
+	)
+
+	exportFunction(env, "perfReaderClose", i.perfReaderClose,
+		[]wapi.ValueType{
+			wapi.ValueTypeI32, // PerfReader
+		},
+		[]wapi.ValueType{wapi.ValueTypeI32}, // Error
+	)
+}
+
+// newPerfReader creates a new perf reader.
+// Params:
+// - stack[0] is the perf map handle
+// - stack[1] is the size
+// - stack[2] is a boolean indicating whether the perf buffer is overwritable
+// - stack[3] is the value size
+// - stack[4] is the max entries
+// Return value:
+// - Perf reader handle on success, 0 on error
+func (i *wasmOperatorInstance) newPerfReader(ctx context.Context, m wapi.Module, stack []uint64) {
+	perfMapHandle := wapi.DecodeU32(stack[0])
+	size := int(wapi.DecodeU32(stack[1]))
+	isOverwritable := wapi.DecodeU32(stack[2]) > 0
+
+	perfMap, ok := getHandle[*ebpf.Map](i, perfMapHandle)
+	if !ok {
+		stack[0] = 1
+		return
+	}
+
+	perfReader, err := perf.NewReaderWithOptions(perfMap, size, perf.ReaderOptions{Overwritable: isOverwritable})
+	if err != nil {
+		i.logger.Warnf("newPerfReader: creating perf reader: %v", err)
+		stack[0] = 0
+		return
+	}
+
+	if isOverwritable {
+		// Calling SetDeadline() from guest would open the possibility for the guest
+		// to block the host by setting a deadline to 0 or far in the future.
+		// To avoid this, we set the deadline to creation time of the buffer.
+		// The only downside is that an user would not be able to read a part of the
+		// buffer from guest side and the whole buffer up to its creation time.
+		// We can work later on to add an hardened implementation of SetDeadline()
+		// if there is a need for it.
+		perfReader.SetDeadline(time.Now())
+	}
+
+	stack[0] = wapi.EncodeU32(i.addHandle(perfReader))
+}
+
+// perfReaderPause() pauses the perf reader
+// Params:
+// - stack[0]: Perf reader handle
+// Return value:
+// - 0 on success, 1 on error
+func (i *wasmOperatorInstance) perfReaderPause(ctx context.Context, m wapi.Module, stack []uint64) {
+	perfReaderHandle := wapi.DecodeU32(stack[0])
+
+	perfReader, ok := getHandle[*perf.Reader](i, perfReaderHandle)
+	if !ok {
+		stack[0] = 1
+		return
+	}
+
+	err := perfReader.Pause()
+	if err != nil {
+		i.logger.Warnf("perfReaderPause: pausing perf reader: %v", err)
+		stack[0] = 1
+		return
+	}
+
+	stack[0] = 0
+}
+
+// perfReaderResume() resume the perf reader
+// Params:
+// - stack[0]: Perf reader handle
+// Return value:
+// - 0 on success, 1 on error
+func (i *wasmOperatorInstance) perfReaderResume(ctx context.Context, m wapi.Module, stack []uint64) {
+	perfReaderHandle := wapi.DecodeU32(stack[0])
+
+	perfReader, ok := getHandle[*perf.Reader](i, perfReaderHandle)
+	if !ok {
+		stack[0] = 1
+		return
+	}
+
+	err := perfReader.Resume()
+	if err != nil {
+		i.logger.Warnf("perfReaderResume: resuming perf reader: %v", err)
+		stack[0] = 1
+		return
+	}
+
+	stack[0] = 0
+}
+
+// perfReaderRead read one record from the perf reader.
+// Params:
+// - stack[0]: Perf reader handle
+// - stack[1]: bufPtr address
+// Return value:
+// - 0 on success, 1 on error, 2 on deadline exceeded
+func (i *wasmOperatorInstance) perfReaderRead(ctx context.Context, m wapi.Module, stack []uint64) {
+	perfReaderHandle := wapi.DecodeU32(stack[0])
+	addrBufPtr := wapi.DecodeU32(stack[1])
+
+	perfReader, ok := getHandle[*perf.Reader](i, perfReaderHandle)
+	if !ok {
+		stack[0] = 1
+		return
+	}
+
+	record, err := perfReader.Read()
+	if err != nil {
+		i.logger.Warnf("perfReaderRead: reading perf buffer: %v", err)
+		stack[0] = 1
+		if errors.Is(err, os.ErrDeadlineExceeded) {
+			stack[0] = 2
+		}
+		return
+	}
+
+	bufPtr, err := i.writeToGuestMemory(ctx, record.RawSample)
+	if err != nil {
+		i.logger.Warnf("perfReaderRead: writing record raw bytes to guest memory: %v", err)
+		stack[0] = 1
+		return
+	}
+
+	if !m.Memory().WriteUint64Le(addrBufPtr, bufPtr) {
+		i.logger.Warnf("perfReaderRead: writing %x at %x", bufPtr, addrBufPtr)
+		stack[0] = 1
+		return
+	}
+
+	stack[0] = 0
+}
+
+// perfReaderClose() close the perf reader
+// Params:
+// - stack[0]: Perf reader handle
+// Return value:
+// - 0 on success, 1 on error
+func (i *wasmOperatorInstance) perfReaderClose(ctx context.Context, m wapi.Module, stack []uint64) {
+	perfReaderHandle := wapi.DecodeU32(stack[0])
+
+	perfReader, ok := getHandle[*perf.Reader](i, perfReaderHandle)
+	if !ok {
+		stack[0] = 1
+		return
+	}
+
+	err := perfReader.Close()
+	if err != nil {
+		i.logger.Warnf("perfReaderClose: closing perf reader: %v", err)
+		stack[0] = 1
+		return
+	}
+
+	i.delHandle(perfReaderHandle)
+
+	stack[0] = 0
+}

wasmapi/go/perf.go
@@ -1,0 +1,98 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"unsafe"
+)
+
+//go:wasmimport env newPerfReader
+func newPerfReader(mapHandle uint32, size uint32, isOverwritable uint32) uint32
+
+//go:wasmimport env perfReaderPause
+func perfReaderPause(perfMapHandle uint32) uint32
+
+//go:wasmimport env perfReaderResume
+func perfReaderResume(perfMapHandle uint32) uint32
+
+//go:wasmimport env perfReaderRead
+func perfReaderRead(perfMapHandle uint32, addrBufPtr uint32) uint32
+
+//go:wasmimport env perfReaderClose
+func perfReaderClose(perfMapHandle uint32) uint32
+
+type PerfReader uint32
+
+func NewPerfReader(m Map, size uint32, isOverwritable bool) (PerfReader, error) {
+	var isOverwritableUint32 uint32
+	if isOverwritable {
+		isOverwritableUint32 = 1
+	}
+
+	ret := newPerfReader(uint32(m), size, isOverwritableUint32)
+	if ret == 0 {
+		return 0, errors.New("creating perf reader")
+	}
+
+	return PerfReader(ret), nil
+}
+
+func (p PerfReader) Pause() error {
+	ret := perfReaderPause(uint32(p))
+	if ret != 0 {
+		return errors.New("pausing perf reader")
+	}
+
+	return nil
+}
+
+func (p PerfReader) Resume() error {
+	ret := perfReaderResume(uint32(p))
+	if ret != 0 {
+		return errors.New("resuming perf reader")
+	}
+
+	return nil
+}
+
+func (p PerfReader) Read() ([]byte, error) {
+	var buf bufPtr
+
+	ret := perfReaderRead(uint32(p), uint32(uintptr(unsafe.Pointer(&buf))))
+	switch ret {
+	case 0:
+		bytes := buf.bytes()
+		buf.free()
+		return bytes, nil
+	case 1:
+		return nil, errors.New("reading perf reader record")
+	case 2:
+		return nil, os.ErrDeadlineExceeded
+	default:
+		return nil, fmt.Errorf("bad return value: expected 0, 1 or 2, got %d", ret)
+	}
+}
+
+func (p PerfReader) Close() error {
+	ret := perfReaderClose(uint32(p))
+	if ret != 0 {
+		return errors.New("closing perf reader")
+	}
+
+	return nil
+}

pkg/operators/wasm/testdata/Makefile
@@ -11,6 +11,7 @@ TEST_ARTIFACTS = \
 	map \
 	mapofmap \
 	syscall \
+	perf \
 	#
 
 all: $(TEST_ARTIFACTS)

pkg/operators/wasm/testdata/perf/build.yaml
@@ -1,0 +1,1 @@
+wasm: program.go

pkg/operators/wasm/testdata/perf/go.mod
@@ -1,0 +1,8 @@
+module main
+
+go 1.23.0
+
+require github.com/inspektor-gadget/inspektor-gadget v0.27.0
+
+// use this to be able to compile it locally
+replace github.com/inspektor-gadget/inspektor-gadget => ../../../../../

pkg/operators/wasm/testdata/perf/program.bpf.c
@@ -1,0 +1,41 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+
+struct event {
+	__u32 a;
+	__u32 b;
+	__u8 c;
+	__u8 unused[247];
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(key_size, sizeof(__u32));
+	__uint(value_size, sizeof(__u32));
+} events SEC(".maps");
+
+static __always_inline int trace_enter(struct syscall_trace_enter *ctx)
+{
+	struct event event = { .a = 42, .b = 42, .c = 43 };
+
+	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event,
+			      sizeof(event));
+
+	return 0;
+}
+
+#ifndef __TARGET_ARCH_arm64
+SEC("tracepoint/syscalls/sys_enter_open")
+int test_open_e(struct syscall_trace_enter *ctx)
+{
+	return trace_enter(ctx);
+}
+#endif /* !__TARGET_ARCH_arm64 */
+
+SEC("tracepoint/syscalls/sys_enter_openat")
+int test_openat_e(struct syscall_trace_enter *ctx)
+{
+	return trace_enter(ctx);
+}
+
+char LICENSE[] SEC("license") = "GPL";

pkg/operators/wasm/testdata/perf/program.go
@@ -1,0 +1,90 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"unsafe"
+
+	api "github.com/inspektor-gadget/inspektor-gadget/wasmapi/go"
+)
+
+//export gadgetInit
+func gadgetInit() int {
+	return 0
+}
+
+//export gadgetStart
+func gadgetStart() int {
+	type event struct {
+		a      uint32
+		b      uint32
+		c      uint8
+		unused [247]uint8
+	}
+
+	mapName := "events"
+	perfArray, err := api.GetMap(mapName)
+	if err != nil {
+		api.Errorf("%s map exists", mapName)
+		return 1
+	}
+
+	perfReader, err := api.NewPerfReader(perfArray, uint32(4096), true)
+	if err != nil {
+		api.Errorf("creating perf reader")
+		return 1
+	}
+	defer perfReader.Close()
+
+	_, err = perfReader.Read()
+	if err == nil {
+		api.Errorf("perf over writable reader must be paused before reading")
+		return 1
+	}
+
+	err = perfReader.Pause()
+	if err != nil {
+		api.Errorf("pausing perf reader")
+		return 1
+	}
+
+	buf, err := perfReader.Read()
+	if err != nil {
+		api.Errorf("reading perf record")
+		return 1
+	}
+
+	if buf == nil {
+		api.Errorf("buffer should not be nil")
+		return 1
+	}
+
+	expectedEvent := event{a: 42, b: 42, c: 43}
+	ev := *(*event)(unsafe.Pointer(&buf[0]))
+	if ev != expectedEvent {
+		api.Errorf("record read mismatch: expected %v, got %v", expectedEvent, ev)
+		return 1
+	}
+
+	err = perfReader.Resume()
+	if err != nil {
+		api.Errorf("resuming perf reader")
+		return 1
+	}
+
+	return 0
+}
+
+func main() {}

commit 4b48e7944f47cd6373fe05b228eaf4d4e5e522f3

operators/wasm: Skip warning message when reading perf returns ErrDeadlineExceeded.
We already return a special value for ErrDeadlineExceeded, no need to print a
warning message, as this would be misleading for users.

Fixes: 7386530c3378 ("operators/wasm: Add functions to interact with perf buffer.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/wasm/perf.go
@@ -173,10 +173,11 @@ func (i *wasmOperatorInstance) perfReaderRead(ctx context.Context, m wapi.Module
 
 	record, err := perfReader.Read()
 	if err != nil {
-		i.logger.Warnf("perfReaderRead: reading perf buffer: %v", err)
-		stack[0] = 1
 		if errors.Is(err, os.ErrDeadlineExceeded) {
 			stack[0] = 2
+		} else {
+			stack[0] = 1
+			i.logger.Warnf("perfReaderRead: reading perf buffer: %v", err)
 		}
 		return
 	}