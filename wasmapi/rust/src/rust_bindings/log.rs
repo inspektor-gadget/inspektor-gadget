472d4ac34700d30444ec89f4d23dce1c40ca6744
Daksh Kaushik <dakshkaushik20040126@gmail.com>
2025-04-13 00:06:42 +0530

    rust binding log.rs
    Signed-off-by: Daksh Kaushik <dakshkaushik20040126@gmail.com>

--- wasmapi/rust/src/rust_bindings/log.rs
+++ wasmapi/rust/src/rust_bindings/log.rs
@@ -1,5 +1,22 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::rust_bindings::helpers::string_to_buf_ptr; //relative paths may hinder in testing.
+
 extern "C" {
-    fn gadgetLog(level: u32, msg: u64);
+    #[link_name = "gadgetLog"]
+    fn gadget_log(level: u32, msg: u64);
 }
 
 pub enum LogLevel {
@@ -10,16 +27,95 @@ pub enum LogLevel {
     Trace = 4,
 }
 
-fn string_to_buf_ptr(s: &str) -> u64 {
-    let ptr = s.as_ptr() as u32;
-    
-    let len = s.len() as u32;
-    
-    (u64::from(len) << 32) | u64::from(ptr)
-}
-
-pub fn log_message(level: LogLevel, message: &str) {
+pub fn log(level: LogLevel, message: &str) {
     unsafe {
-        gadgetLog(level as u32, string_to_buf_ptr(message));
+        gadget_log(level as u32, string_to_buf_ptr(message));
     }
 }
+
+// Rust doesn't support default vardiac, but similar functionality are provided by macros allow for multiple arguments
+#[macro_export]
+macro_rules! log {
+     ($level:expr, $($arg:expr),+ $(,)?) => {{
+         let message = [$($arg),+].join(" ");
+         $crate::log($level, &message);
+     }};
+ }
+
+#[macro_export]
+macro_rules! logf {
+     ($level:expr, $fmt:literal $(, $arg:tt)* ) => {{
+         let message = format!($fmt $(, $arg)*);
+         $crate::log($level, &message);
+     }};
+ }
+
+#[macro_export]
+macro_rules! error {
+     ($($arg:tt)*) => {
+         $crate::log!(LogLevel::Error, $($arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! errorf {
+     ($fmt:literal $(, $arg:tt)* ) => {
+         $crate::logf!(LogLevel::Error, $fmt $(, $arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! warn {
+     ($($arg:tt)*) => {
+         $crate::log!(LogLevel::Warn, $($arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! warnf {
+     ($fmt:literal $(, $arg:tt)* ) => {
+         $crate::logf!(LogLevel::Warn, $fmt $(, $arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! info {
+     ($($arg:tt)*) => {
+         $crate::log!(LogLevel::Info, $($arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! infof {
+     ($fmt:literal $(, $arg:tt)* ) => {
+         $crate::logf!(LogLevel::Info, $fmt $(, $arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! debug {
+     ($($arg:tt)*) => {
+         $crate::log!(LogLevel::Debug, $($arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! debugf {
+     ($fmt:literal $(, $arg:tt)* ) => {
+         $crate::logf!(LogLevel::Debug, $fmt $(, $arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! trace {
+     ($($arg:tt)*) => {
+         $crate::log!(LogLevel::Trace, $($arg)*);
+     };
+ }
+
+#[macro_export]
+macro_rules! tracef {
+     ($fmt:literal $(, $arg:tt)* ) => {
+         $crate::logf!(LogLevel::Trace, $fmt $(, $arg)*);
+     };
+ }
