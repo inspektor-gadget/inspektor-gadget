97894b5f648ef8d618d3e0391b87d1eda41d63ca
Mauricio Vásquez <mauriciov@microsoft.com>
2025-04-23 14:19:20 -0500

    ci: Automatically update the latest tag on release
    This commit adds a new script and updates the workflow to update the
    latest tag for the container images to point to the latest release.
    
    Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

---
+++ tools/update_latest_tag.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script updates the latest tag for all images in the inspektor-gadget
+# Github organization to point to the latest release.
+
+set -euo pipefail
+
+OWNER="inspektor-gadget"
+OLD_TAG="$1"
+NEW_TAG="latest"
+
+get_all_repos() {
+  local page=1
+  local per_page=100
+  local repos=()
+
+  while :; do
+    response=$(gh api -H "Accept: application/vnd.github.v3+json" \
+      "/orgs/$OWNER/packages?package_type=container&per_page=$per_page&page=$page")
+
+    current_repos=$(echo "$response" | jq -r '.[].name')
+    if [[ -z "$current_repos" ]]; then
+      break
+    fi
+
+    repos+=($current_repos)
+    ((page++))
+  done
+
+  echo "${repos[@]}"
+}
+
+REPOS=$(get_all_repos)
+
+for REPO in $REPOS; do
+    echo "Checking image: $REPO"
+    if crane manifest "ghcr.io/$OWNER/$REPO:$OLD_TAG" > /dev/null 2>&1; then
+        echo "Tag $OLD_TAG exists for $REPO"
+        crane copy "ghcr.io/$OWNER/$REPO:$OLD_TAG" "ghcr.io/$OWNER/$REPO:$NEW_TAG"
+    fi
+done
