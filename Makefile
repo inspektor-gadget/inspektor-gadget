inspektor-gadget/inspektor-gadget

commit a0ad5aab6b3d5c18fa3537f529d05a3aa969fc05

Makefile: Build ig for host architecture with go build instead of docker.
Building ig with docker can takes some time.
So, to build it for the host architecture, let's us go build instead.
Docker should continue to be used for different architecture, thus it is still
used when wanting to build ig when specifying the architecture like
ig-linux-amd64 or ig-linux-arm64.

Also, a new ig-on-docker targets was added to keep the old behavior in case
someone wants to build ig and does not have go installed.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

Makefile
@@ -109,7 +109,16 @@ list-ig-targets:
 .PHONY: ig-all
 ig-all: $(IG_TARGETS) ig
 
-ig: ig-$(GOHOSTOS)-$(GOHOSTARCH)
+.PHONY: ig
+ig:
+	CGO_ENABLED=0 go build \
+        -ldflags "-X github.com/inspektor-gadget/inspektor-gadget/internal/version.version=${VERSION} \
+        -X github.com/inspektor-gadget/inspektor-gadget/cmd/common/image.builderImage=${EBPF_BUILDER} \
+        -extldflags '-static'" \
+        -tags "netgo" \
+        ./cmd/ig
+
+ig-on-docker: ig-$(GOHOSTOS)-$(GOHOSTARCH)
 	cp ig-$(GOHOSTOS)-$(GOHOSTARCH) ig
 
 # Compile ig with debug options and debug it using delve:
@@ -414,6 +423,7 @@ help:
 	@echo  '  all		  		- Build all targets marked with [*]'
 	@echo  '* ig		  		- Build the ig cli tool'
 	@echo  '  ig-all	  		- Build the ig cli tool for all architectures'
+	@echo  '  ig-on-docker                  - Build the ig cli tool using docker'
 	@echo  '* build		  		- Build all targets marked with [o]'
 	@echo  'o manifests			- Generate WebhookConfiguration, ClusterRole and CustomResourceDefinition objects'
 	@echo  'o generate			- Generate client API code and DeepCopy related code'

commit d30b50000d4cb8eda674950a622e9e34adefca86

Makefile: Update missing reference of EBPF_BUILDER
Commit 9bd1f3e0aa08 ("Rename ebpf-builder to gadget-builder") renamed
EBPF_BUILDER to GADGET_BUILDER, but commit a0ad5aab6b3d ("Makefile: Build ig for host architecture with go build instead of docker.")
missed it and used the old EBPF_BUILDER.

This caused the builder image to be empty when ig was compiled with
`make ig` producing an error when building a Gadget:

```bash
$ ig image build -h | grep builder
      --builder-image string        Builder image to use

$ sudo ig image build .
Pulling builder image
Error: pulling builder image: invalid reference format
```
Fixes: a0ad5aab6b3d ("Makefile: Build ig for host architecture with go build instead of docker.")
Link: https://kubernetes.slack.com/archives/CSYL75LF6/p1745944201173279
Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

Makefile
@@ -123,7 +123,7 @@ ig-all: $(IG_TARGETS) ig
 ig:
 	CGO_ENABLED=0 go build \
         -ldflags "-X github.com/inspektor-gadget/inspektor-gadget/internal/version.version=${VERSION} \
-        -X github.com/inspektor-gadget/inspektor-gadget/cmd/common/image.builderImage=${EBPF_BUILDER} \
+        -X github.com/inspektor-gadget/inspektor-gadget/cmd/common/image.builderImage=${GADGET_BUILDER} \
         -extldflags '-static'" \
         -tags "netgo" \
         ./cmd/ig