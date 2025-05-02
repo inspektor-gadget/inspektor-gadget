inspektor-gadget/inspektor-gadget

commit 7648f63d1b7ba2a4a5d8ee11736e0165e897fff6

treewide: Change verify-image and public-key to be global param.
This way, in the context of kubectl-gadget, we can specify them once at deploy
time instead of runtime.

Suggested-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/oci-handler/oci.go
@@ -45,18 +45,36 @@ const (
 	allowedRegistries     = "allowed-registries"
 )
 
-type ociHandler struct{}
+type ociHandler struct {
+	globalParams *params.Params
+}
 
 func (o *ociHandler) Name() string {
 	return "oci"
 }
 
-func (o *ociHandler) Init(*params.Params) error {
+func (o *ociHandler) Init(params *params.Params) error {
+	o.globalParams = params
 	return nil
 }
 
 func (o *ociHandler) GlobalParams() api.Params {
-	return nil
+	return api.Params{
+		{
+			Key:          verifyImage,
+			Title:        "Verify image",
+			Description:  "Verify image using the provided public key",
+			DefaultValue: "true",
+			TypeHint:     api.TypeBool,
+		},
+		{
+			Key:          publicKey,
+			Title:        "Public key",
+			Description:  "Public key used to verify the gadget",
+			DefaultValue: resources.InspektorGadgetPublicKey,
+			TypeHint:     api.TypeString,
+		},
+	}
 }
 
 func (o *ociHandler) InstanceParams() api.Params {
@@ -102,20 +120,6 @@ func (o *ociHandler) InstanceParams() api.Params {
 			TypeHint:    api.TypeString,
 		},
 		{
-			Key:          verifyImage,
-			Title:        "Verify image",
-			Description:  "Verify image using the provided public key",
-			DefaultValue: "true",
-			TypeHint:     api.TypeBool,
-		},
-		{
-			Key:          publicKey,
-			Title:        "Public key",
-			Description:  "Public key used to verify the image based gadget",
-			DefaultValue: resources.InspektorGadgetPublicKey,
-			TypeHint:     api.TypeString,
-		},
-		{
 			Key:         allowedDigests,
 			Title:       "Allowed Digests",
 			Description: "List of allowed digests, if image digest is not part of it, execution will be denied. By default, all digests are allowed",
@@ -148,7 +152,12 @@ func getPullSecret(pullSecretString string, gadgetNamespace string) ([]byte, err
 func (o *ociHandler) InstantiateDataOperator(gadgetCtx operators.GadgetContext, instanceParamValues api.ParamValues) (
 	operators.DataOperatorInstance, error,
 ) {
-	ociParams := apihelpers.ToParamDescs(o.InstanceParams()).ToParams()
+	ociParams := o.globalParams
+	if ociParams == nil {
+		ociParams = apihelpers.ToParamDescs(o.GlobalParams()).ToParams()
+	}
+
+	*ociParams = append(*ociParams, *apihelpers.ToParamDescs(o.InstanceParams()).ToParams()...)
 	err := ociParams.CopyFromMap(instanceParamValues, "")
 	if err != nil {
 		return nil, err
@@ -208,6 +217,8 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 		},
 		Logger: gadgetCtx.Logger(),
 	}
+
+	gadgetCtx.Logger().Debugf("image options: %+v", imgOpts)
 
 	target := gadgetCtx.OrasTarget()
 	// If the target wasn't explicitly set, use the local store. In this case we

pkg/oci/oci.go
@@ -928,7 +928,7 @@ func ensureImage(ctx context.Context, imageStore oras.Target, image string, imgO
 	}
 
 	if !imgOpts.VerifyPublicKey {
-		imgOpts.Logger.Warnf("you set --verify-image=false, image will not be verified")
+		imgOpts.Logger.Warnf("image signature verification is disabled due to using corresponding CLI options")
 
 		return nil
 	}

.github/actions/run-integration-tests/action.yml
@@ -47,7 +47,7 @@ runs:
           # Inspektor-Gadget container image is loaded into the cluster using "minikube image load" instead of pushing the container image to a registry.
           EXTRA_FLAGS='--image-pull-policy=Never'
         fi
-        ./kubectl-gadget deploy $EXTRA_FLAGS --debug --experimental --image=${{ inputs.container_repo }}:${{ inputs.image_tag }}
+        ./kubectl-gadget deploy --verify-gadgets=${{ inputs.gadget_verify_image }} $EXTRA_FLAGS --debug --experimental --image=${{ inputs.container_repo }}:${{ inputs.image_tag }}
     - name: Integration tests
       id: integration-tests
       shell: bash
@@ -87,7 +87,6 @@ runs:
           DNSTESTER_IMAGE=${{ inputs.dnstester_image }} \
           GADGET_REPOSITORY=${{ inputs.gadget_repository }} \
           GADGET_TAG=${{ inputs.gadget_tag }} \
-          GADGET_VERIFY_IMAGE=${{ inputs.gadget_verify_image }} \
           -o kubectl-gadget integration-tests |& tee integration.log & wait $!
         echo "IntegrationTestsJob: Done"
     - name: Undeploy Inspektor Gadget

.github/workflows/inspektor-gadget.yml
@@ -1503,12 +1503,11 @@ jobs:
       run: |
         set -o pipefail
         tar zxvf /home/runner/work/inspektor-gadget/inspektor-gadget/kubectl-gadget-linux-amd64.tar.gz
-        ./kubectl-gadget deploy --image-pull-policy=Never --debug --experimental --image=${{ steps.set-repo-determine-image-tag.outputs.container-repo }}:${{ steps.set-repo-determine-image-tag.outputs.image-tag }}
+        ./kubectl-gadget deploy --verify-gadgets=${{ needs.check-secrets.outputs.cosign }} --image-pull-policy=Never --debug --experimental --image=${{ steps.set-repo-determine-image-tag.outputs.container-repo }}:${{ steps.set-repo-determine-image-tag.outputs.image-tag }}
         make \
         GADGET_REPOSITORY=${{ steps.set-repo-determine-image-tag.outputs.gadget-repository }} \
         GADGET_TAG=${{ steps.set-repo-determine-image-tag.outputs.gadget-tag }} \
         KUBECTL_GADGET=/home/runner/work/inspektor-gadget/inspektor-gadget/kubectl-gadget \
-        IG_FLAGS="--verify-image=${{ needs.check-secrets.outputs.cosign }}" \
         IG_RUNTIME=kubernetes \
         -C gadgets/ test-k8s -o build |& tee gadgets-tests.log & wait $!
     - name: Undeploy Inspektor Gadget

Makefile
@@ -8,7 +8,7 @@ MINIKUBE ?= minikube
 KUBERNETES_DISTRIBUTION ?= ""
 GADGET_TAG ?= $(shell ./tools/image-tag branch)
 GADGET_REPOSITORY ?= ghcr.io/inspektor-gadget/gadget
-GADGET_VERIFY_IMAGE ?= true
+VERIFY_GADGETS ?= true
 TEST_COMPONENT ?= inspektor-gadget
 
 GOHOSTOS ?= $(shell go env GOHOSTOS)
@@ -286,7 +286,6 @@ integration-tests: kubectl-gadget
 			-dnstester-image $(DNSTESTER_IMAGE) \
 			-gadget-repository $(GADGET_REPOSITORY) \
 			-gadget-tag $(GADGET_TAG) \
-			-gadget-verify-image=$(GADGET_VERIFY_IMAGE) \
 			-test-component $(TEST_COMPONENT) \
 			$$INTEGRATION_TESTS_PARAMS
 
@@ -346,7 +345,7 @@ minikube-deploy: minikube-start gadget-container kubectl-gadget
 	$(MINIKUBE) image ls --format=table | grep "$(CONTAINER_REPO)\s*|\s*$(IMAGE_TAG)" || \
 		(echo "Image $(CONTAINER_REPO)\s*|\s*$(IMAGE_TAG) was not correctly loaded into Minikube" && false)
 	@echo
-	./kubectl-gadget deploy --liveness-probe=$(LIVENESS_PROBE) \
+	./kubectl-gadget deploy --verify-gadgets=$(VERIFY_GADGETS) --liveness-probe=$(LIVENESS_PROBE) \
 		--image-pull-policy=Never
 	kubectl rollout status daemonset -n gadget gadget --timeout 30s
 	@echo "Image used by the gadget pod:"

charts/gadget/templates/configmap.yaml
@@ -17,3 +17,7 @@ data:
       crio-socketpath: {{ .Values.config.crioSocketPath }}
       docker-socketpath: {{ .Values.config.dockerSocketPath }}
       podman-socketpath: {{ .Values.config.podmanSocketPath }}
+      operator:
+        oci:
+          verify-image: {{ .Values.config.verifyGadgets }}
+          public-key: {{ .Values.config.gadgetsPublicKey | quote }}

charts/gadget/values.yaml
@@ -27,6 +27,16 @@ config:
   # -- Daemon Log Level. Valid values are: "trace", "debug", "info", "warning", "error", "fatal", "panic"
   daemonLogLevel: "info"
 
+  # -- Verify image-based gadgets
+  verifyGadgets: true
+
+  # -- Public key used to verify image-based gadgets
+  gadgetsPublicKey: |
+    -----BEGIN PUBLIC KEY-----
+    MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEoDOC0gYSxZTopenGmX3ZFvQ1DSfh
+    Ir4EKRt5jC+mXaJ7c7J+oREskYMn/SfZdRHNSOjLTZUMDm60zpXGhkFecg==
+    -----END PUBLIC KEY-----
+
   # -- Mount pull secret (gadget-pull-secret) to pull image-based gadgets from private registry
   mountPullSecret: false
 

cmd/kubectl-gadget/deploy.go
@@ -105,6 +105,8 @@ var (
 	verifyImage         bool
 	publicKey           string
 	strLevels           []string
+	verifyGadgets       bool
+	gadgetsPublicKey    string
 )
 
 var supportedHooks = []string{"auto", "crio", "podinformer", "nri", "fanotify", "fanotify+ebpf"}
@@ -231,6 +233,14 @@ func init() {
 	deployCmd.PersistentFlags().StringVarP(
 		&publicKey,
 		"public-key", "", resources.InspektorGadgetPublicKey, "Public key used to verify the container image")
+	deployCmd.PersistentFlags().BoolVarP(
+		&verifyGadgets,
+		"verify-gadgets", "",
+		true,
+		"verify gadgets using the provided public key")
+	deployCmd.PersistentFlags().StringVarP(
+		&gadgetsPublicKey,
+		"gadgets-public-key", "", resources.InspektorGadgetPublicKey, "Public key used to verify the gadgets")
 	rootCmd.AddCommand(deployCmd)
 }
 
@@ -246,7 +256,7 @@ func info(format string, args ...any) {
 // It was adapted from:
 // https://github.com/kubernetes/client-go/issues/193#issuecomment-363318588
 func parseK8sYaml(content string) ([]runtime.Object, error) {
-	sepYamlfiles := strings.Split(content, "---")
+	sepYamlfiles := strings.Split(content, "---\n")
 	retVal := make([]runtime.Object, 0, len(sepYamlfiles))
 
 	sch := runtime.NewScheme()
@@ -720,6 +730,14 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 			cfg[gadgettracermanagerconfig.CrioSocketPath] = runtimesConfig.Crio
 			cfg[gadgettracermanagerconfig.DockerSocketPath] = runtimesConfig.Docker
 			cfg[gadgettracermanagerconfig.PodmanSocketPath] = runtimesConfig.Podman
+
+			opOciCfg, ok := cfg[gadgettracermanagerconfig.Operator].(map[string]interface{})[gadgettracermanagerconfig.Oci].(map[string]interface{})
+			if !ok {
+				return fmt.Errorf("%s.%s not found in config.yaml", gadgettracermanagerconfig.Operator, gadgettracermanagerconfig.Oci)
+			}
+
+			opOciCfg[gadgettracermanagerconfig.VerifyImage] = verifyGadgets
+			opOciCfg[gadgettracermanagerconfig.PublicKey] = gadgetsPublicKey
 
 			data, err := yaml.Marshal(cfg)
 			if err != nil {

commit bc6f56391fced4a130518142f7c315f639483bc2

oci: Fix handling of global and instance params
The logic to handle global and instance params was wrong as both kind of
params were mixed in the same variable. This allowed the client to
override global params previously defined by the admin, which in practice
means a malicious user could bypass security mechanisms like image
signature verification, list of allowed gadgets, etc.

Fixes: https://github.com/inspektor-gadget/inspektor-gadget/security/advisories/GHSA-pv22-fqcj-7xwh
Fixes: 7648f63d1b7b ("treewide: Change verify-image and public-key to be global param.")

Reported-by: Michael Friese <mfriese@microsoft.com>
Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/oci-handler/oci.go
@@ -68,6 +68,10 @@ func (o *ociHandler) Name() string {
 
 func (o *ociHandler) Init(params *params.Params) error {
 	o.globalParams = params
+	if o.globalParams == nil {
+		o.globalParams = apihelpers.ToParamDescs(o.GlobalParams()).ToParams()
+	}
+
 	return nil
 }
 
@@ -174,22 +178,18 @@ func getPullSecret(pullSecretString string, gadgetNamespace string, k8sClient ku
 func (o *ociHandler) InstantiateDataOperator(gadgetCtx operators.GadgetContext, instanceParamValues api.ParamValues) (
 	operators.DataOperatorInstance, error,
 ) {
-	ociParams := o.globalParams
-	if ociParams == nil {
-		ociParams = apihelpers.ToParamDescs(o.GlobalParams()).ToParams()
-	}
-
-	*ociParams = append(*ociParams, *apihelpers.ToParamDescs(o.InstanceParams()).ToParams()...)
-	err := ociParams.CopyFromMap(instanceParamValues, "")
+	instanceParams := apihelpers.ToParamDescs(o.InstanceParams()).ToParams()
+	err := instanceParams.CopyFromMap(instanceParamValues, "")
 	if err != nil {
 		return nil, err
 	}
 
 	instance := &OciHandlerInstance{
-		ociHandler:  o,
-		gadgetCtx:   gadgetCtx,
-		ociParams:   ociParams,
-		paramValues: instanceParamValues,
+		ociHandler:     o,
+		gadgetCtx:      gadgetCtx,
+		globalParams:   o.globalParams,
+		instanceParams: instanceParams,
+		paramValues:    instanceParamValues,
 	}
 
 	err = instance.init(gadgetCtx)
@@ -308,8 +308,8 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 	var secretBytes []byte
 
 	// TODO: move to a place without dependency on k8s
-	if pullSecretParam := o.ociParams.Get(pullSecret); pullSecretParam != nil {
-		pullSecretString := o.ociParams.Get(pullSecret).AsString()
+	if pullSecretParam := o.globalParams.Get(pullSecret); pullSecretParam != nil {
+		pullSecretString := o.globalParams.Get(pullSecret).AsString()
 
 		if pullSecretString != "" {
 			var err error
@@ -327,17 +327,17 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 
 	imgOpts := &oci.ImageOptions{
 		AuthOptions: oci.AuthOptions{
-			AuthFile:           o.ociParams.Get(authfileParam).AsString(),
+			AuthFile:           o.globalParams.Get(authfileParam).AsString(),
 			SecretBytes:        secretBytes,
-			InsecureRegistries: o.ociParams.Get(insecureRegistriesParam).AsStringSlice(),
-			DisallowPulling:    o.ociParams.Get(disallowPulling).AsBool(),
+			InsecureRegistries: o.globalParams.Get(insecureRegistriesParam).AsStringSlice(),
+			DisallowPulling:    o.globalParams.Get(disallowPulling).AsBool(),
 		},
 		VerifyOptions: oci.VerifyOptions{
-			VerifyPublicKey: o.ociParams.Get(verifyImage).AsBool(),
-			PublicKeys:      o.ociParams.Get(publicKeys).AsStringSlice(),
+			VerifyPublicKey: o.globalParams.Get(verifyImage).AsBool(),
+			PublicKeys:      o.globalParams.Get(publicKeys).AsStringSlice(),
 		},
 		AllowedGadgetsOptions: oci.AllowedGadgetsOptions{
-			AllowedGadgets: o.ociParams.Get(allowedGadgets).AsStringSlice(),
+			AllowedGadgets: o.globalParams.Get(allowedGadgets).AsStringSlice(),
 		},
 		Logger: gadgetCtx.Logger(),
 	}
@@ -351,7 +351,7 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 		// Make sure the image is available, either through pulling or by just accessing a local copy
 		// TODO: add security constraints (e.g. don't allow pulling - add GlobalParams for that)
 		err := oci.EnsureImage(gadgetCtx.Context(), gadgetCtx.ImageName(),
-			imgOpts, o.ociParams.Get(pullParam).AsString())
+			imgOpts, o.instanceParams.Get(pullParam).AsString())
 		if err != nil {
 			return fmt.Errorf("ensuring image: %w", err)
 		}
@@ -386,7 +386,7 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 		return fmt.Errorf("unmarshalling metadata: %w", err)
 	}
 
-	for _, ann := range o.ociParams.Get(annotate).AsStringSlice() {
+	for _, ann := range o.instanceParams.Get(annotate).AsStringSlice() {
 		if len(ann) == 0 {
 			continue
 		}
@@ -528,7 +528,8 @@ type OciHandlerInstance struct {
 	imageOperatorInstances []operators.ImageOperatorInstance
 	extraParams            api.Params
 	paramValues            api.ParamValues
-	ociParams              *params.Params
+	globalParams           *params.Params
+	instanceParams         *params.Params
 }
 
 func (o *OciHandlerInstance) Name() string {