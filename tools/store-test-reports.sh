inspektor-gadget/inspektor-gadget

commit bd82e6199bafa2d8faebd86e145a7e647f0d42d0

ci: Use differnt name for test-report artifacts
Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

.github/workflows/inspektor-gadget.yml
@@ -2177,8 +2177,8 @@ jobs:
     - name: Get all reports
       uses: actions/download-artifact@cc203385981b70ca67e1cc392babf9cc229d5806 # v4.1.9
       with:
-        name: "test-reports"
-    - name: Store test reports
+        patterns: "test-reports-*-*"
+    - name: Store test report
       shell: bash {0}
       run: ./tools/store-test-reports.sh
       env:

tools/store-test-reports.sh
@@ -35,7 +35,7 @@ function store-reports {
     mkdir -p data
     echo '{}' > data/workflows.json
   fi
-  for i in ../test-report_*_*.json; do
+  for i in ../test-reports-*-*/test-report_*_*.json; do
       job_key=$(basename $i .json | sed 's/test-report_//')
       jq --slurpfile obj $i \
       --arg job_key ${job_key} \

.github/actions/prepare-and-publish-test-reports/action.yml
@@ -39,5 +39,5 @@ runs:
     - name: Upload test reports summary as artifacts
       uses: actions/upload-artifact@0b2256b8c012f0828dc542b3febcab082c67f72b # v4.3.4
       with:
-        name: test-reports
+        name: test-reports-${{ github.job }}-${{ inputs.test-summary-suffix }}
         path: ./test-report_*_*.json

commit a03f5d9d93bb4473a1fefc0bff5519ae91167f3b

ci: Fix storing test reports
Fixes: bd82e6199bafa2d8faebd86e145a7e647f0d42d0

Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

.github/workflows/inspektor-gadget.yml
@@ -2209,7 +2209,7 @@ jobs:
     - name: Get all reports
       uses: actions/download-artifact@d3f86a106a0bac45b974a628896c90dbdf5c8093 # v4.3.0
       with:
-        patterns: "test-reports-*-*"
+        pattern: "test-reports-*-*"
     - name: Store test report
       shell: bash {0}
       run: ./tools/store-test-reports.sh

tools/store-test-reports.sh
@@ -41,6 +41,11 @@ function store-reports {
       --arg job_key ${job_key} \
       '.[$job_key] = (.[$job_key][-99:] + $obj)' \
       data/workflows.json > workflows.json.tmp
+      if [ ! -s workflows.json.tmp ]; then
+        echo "Skipping empty file: $i" >&2
+        rm workflows.json.tmp
+        continue
+      fi
       mv workflows.json.tmp data/workflows.json
   done
 