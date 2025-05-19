43cb7fd93410b35e4dcf4a4bb2e751779b63012d
Daksh Kaushik <dakshkaushik20040126@gmail.com>
2025-05-20 03:32:13 +0530

    rust api params.rs
    Signed-off-by: Daksh Kaushik <dakshkaushik20040126@gmail.com>

---
+++ pkg/operators/wasm/rusttestdata/params/src/lib.rs
@@ -0,0 +1,26 @@
+use api::{
+    errorf,
+    {log::LogLevel, params::get_param_value},
+};
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub fn gadgetStart() -> i32 {
+    let Ok(val) = get_param_value("param-key".to_string(), 32) else {
+        errorf!("failed to get param: {:?}", e);
+        return 1;
+    };
+
+    let expected = "param-value".to_string();
+    if val != expected {
+        errorf!("param value should be {:?}, got: {:?}", expected, val);
+        return 1;
+    }
+
+    if let Ok(_) = get_param_value("non-existing-param".to_string(), 32) {
+        errorf!("looking for non-existing-param succeeded");
+        return 1;
+    }
+
+    0
+}
