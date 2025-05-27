inspektor-gadget/inspektor-gadget

commit b1f24a0bcb7fb3839129da8e2055ca89ac7fba06

container-collection: publish oci config as a string
The container-collection package detects when the container runtime
starts a new container and reads the oci config (config.json from the
container runtime spec). This config is used internally in the
container-collection package for two purposes:
1. Look up the mount for the Kubernetes ServiceAccount token volume to
   distinguish several containers from the same pod.
2. Look up the annotations for Kubernetes enrichment (pod name, etc.)

This patch makes the config available in the containers data source as a
string. It is now unmarshalled partially only when needed.

Before this patch, the config was completely unmarshalled using the
opencontainers/runtime-spec package v1.2.1 (version specified in
go.mod):
https://github.com/opencontainers/runtime-spec/blob/v1.2.1/specs-go/config.go#L6

```
$ sudo ig list-containers -o json -w
{
  "timestamp": "2025-05-20T11:04:34+02:00",
  "event": "CREATED",
  "container": {
    "runtime": {
      "runtimeName": "docker",
      "containerId": "a5c7f2341346455531e3b5c4ca9fa96ed598d2c8d157ff617bfce1f5a948be57",
      "containerName": "ecstatic_tesla",
      "containerPid": 49978,
      "containerImageName": "busybox",
      "containerStartedAt": 1747731874644255522
    },
    "k8s": {},
    "ociConfig": {
      "ociVersion": "1.2.0",
      "process": {
        "terminal": true,
```

This means that if users run runc/crun with a newer version of
runtime-spec with new fields, they will be lost.

After this patch, the config is displayed as a string without changes:
```
$ sudo ig list-containers -o json -w
{
  "timestamp": "2025-05-20T11:02:36+02:00",
  "event": "CREATED",
  "container": {
    "runtime": {
      "runtimeName": "docker",
      "containerId": "e63be34d020f4b3f44cd7cba885511f751dc901ab605324551d7e18ede482ca1",
      "containerName": "ecstatic_solomon",
      "containerPid": 48248,
      "containerImageName": "busybox",
      "containerStartedAt": 1747731756812735022

pkg/container-collection/ocispec.go
@@ -1,0 +1,52 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file provides lightweight functions to parse the OCI config
+// The oci config is expected to be in the format of
+// https://github.com/opencontainers/runtime-spec/blob/v1.2.1/specs-go/config.go#L18
+
+package containercollection
+
+import (
+	"encoding/json"
+)
+
+// ociConfigGetSourceMounts returns the source mounts from the oci config
+func ociConfigGetSourceMounts(ociConfig string) (out []string, err error) {
+	var config struct {
+		Mounts []struct {
+			Source string `json:"source,omitempty"`
+		} `json:"mounts,omitempty"`
+	}
+	err = json.Unmarshal([]byte(ociConfig), &config)
+	if err != nil {
+		return nil, err
+	}
+	for _, m := range config.Mounts {
+		out = append(out, m.Source)
+	}
+	return out, nil
+}
+
+// ociConfigGetAnnotations returns the annotations from the oci config
+func ociConfigGetAnnotations(ociConfig string) (map[string]string, error) {
+	var config struct {
+		Annotations map[string]string `json:"annotations,omitempty"`
+	}
+	err := json.Unmarshal([]byte(ociConfig), &config)
+	if err != nil {
+		return nil, err
+	}
+	return config.Annotations, nil
+}

pkg/container-collection/options.go
@@ -548,6 +548,11 @@ func WithKubernetesEnrichment(nodeName string, kubeconfig *rest.Config) Containe
 						return false
 					}
 				}
+				srcs, err := ociConfigGetSourceMounts(container.OciConfig)
+				if err != nil {
+					log.Warnf("kubernetes enricher: failed to get source mounts for container %s: %s", container.Runtime.ContainerID, err)
+					// We won't get ContainerName but keep going
+				}
 
 				if container.K8s.ContainerName == "" {
 					var containerName string
@@ -564,9 +569,9 @@ func WithKubernetesEnrichment(nodeName string, kubeconfig *rest.Config) Containe
 					}
 				outerLoop:
 					for _, name := range containerNames {
-						for _, m := range container.OciConfig.Mounts {
+						for _, src := range srcs {
 							pattern := fmt.Sprintf("pods/%s/containers/%s/", uid, name)
-							if strings.Contains(m.Source, pattern) {
+							if strings.Contains(src, pattern) {
 								containerName = name
 								break outerLoop
 							}
@@ -715,7 +720,7 @@ func WithLinuxNamespaceEnrichment() ContainerCollectionOption {
 // metadata from OCI config that WithOCIConfigEnrichment is able to provide.
 // Keep in sync with what WithOCIConfigEnrichment does.
 func isEnrichedWithOCIConfigInfo(container *Container) bool {
-	return container.OciConfig != nil &&
+	return container.OciConfig != "" &&
 		container.Runtime.RuntimeName != "" &&
 		container.Runtime.ContainerImageName != "" &&
 		container.K8s.ContainerName != "" &&
@@ -729,15 +734,21 @@ func isEnrichedWithOCIConfigInfo(container *Container) bool {
 func WithOCIConfigEnrichment() ContainerCollectionOption {
 	return func(cc *ContainerCollection) error {
 		cc.containerEnrichers = append(cc.containerEnrichers, func(container *Container) bool {
-			if container.OciConfig == nil || isEnrichedWithOCIConfigInfo(container) {
+			if container.OciConfig == "" || isEnrichedWithOCIConfigInfo(container) {
 				return true
 			}
 
-			if cm, ok := container.OciConfig.Annotations["io.container.manager"]; ok && cm == "libpod" {
+			annotations, err := ociConfigGetAnnotations(container.OciConfig)
+			if err != nil {
+				log.Errorf("OCIConfig enricher: failed to get annotations from OCI config (len=%d): %s", len(container.OciConfig), err)
+				return true
+			}
+
+			if cm, ok := annotations["io.container.manager"]; ok && cm == "libpod" {
 				container.Runtime.RuntimeName = types.RuntimeNamePodman
 			}
 
-			resolver, err := ociannotations.NewResolverFromAnnotations(container.OciConfig.Annotations)
+			resolver, err := ociannotations.NewResolverFromAnnotations(annotations)
 			// ignore if annotations aren't supported for runtime e.g docker
 			if err != nil {
 				log.Debugf("OCIConfig enricher: failed to initialize annotation resolver: %s", err)
@@ -746,28 +757,28 @@ func WithOCIConfigEnrichment() ContainerCollectionOption {
 
 			// TODO: handle this once we support pod sandboxes via WithContainerRuntimeEnrichment
 			// Issue: https://github.com/inspektor-gadget/inspektor-gadget/issues/1095
-			if ct := resolver.ContainerType(container.OciConfig.Annotations); ct == "sandbox" {
+			if ct := resolver.ContainerType(annotations); ct == "sandbox" {
 				return false
 			}
 
 			// Enrich the container. Keep in sync with isEnrichedWithOCIConfigInfo.
 			container.Runtime.RuntimeName = resolver.Runtime()
-			if name := resolver.ContainerName(container.OciConfig.Annotations); name != "" {
+			if name := resolver.ContainerName(annotations); name != "" {
 				container.K8s.ContainerName = name
 			}
-			if podName := resolver.PodName(container.OciConfig.Annotations); podName != "" {
+			if podName := resolver.PodName(annotations); podName != "" {
 				container.K8s.PodName = podName
 			}
-			if podNamespace := resolver.PodNamespace(container.OciConfig.Annotations); podNamespace != "" {
+			if podNamespace := resolver.PodNamespace(annotations); podNamespace != "" {
 				container.K8s.Namespace = podNamespace
 			}
-			if podUID := resolver.PodUID(container.OciConfig.Annotations); podUID != "" {
+			if podUID := resolver.PodUID(annotations); podUID != "" {
 				container.K8s.PodUID = podUID
 			}
-			if imageName := resolver.ContainerImageName(container.OciConfig.Annotations); imageName != "" {
+			if imageName := resolver.ContainerImageName(annotations); imageName != "" {
 				container.Runtime.ContainerImageName = imageName
 			}
-			if podSandboxId := resolver.PodSandboxId(container.OciConfig.Annotations); podSandboxId != "" {
+			if podSandboxId := resolver.PodSandboxId(annotations); podSandboxId != "" {
 				container.SandboxId = podSandboxId
 			}
 

examples/container-hook/main.go
@@ -32,7 +32,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-hook"
+	containerhook "github.com/inspektor-gadget/inspektor-gadget/pkg/container-hook"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/k8sutil"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
 )
@@ -123,43 +123,37 @@ func callback(notif containerhook.ContainerEvent) {
 	// command in the same way that OCI hooks would do. For this we need to
 	// synthesise the OCI State.
 	ociState := &ocispec.State{
-		Version: ocispec.Version,
-		ID:      notif.ContainerID,
-		Pid:     int(notif.ContainerPID),
-		Bundle:  notif.Bundle,
+		Version:     ocispec.Version,
+		ID:          notif.ContainerID,
+		Pid:         int(notif.ContainerPID),
+		Bundle:      notif.Bundle,
+		Annotations: make(map[string]string),
 	}
-	if notif.ContainerConfig != nil && notif.ContainerConfig.Annotations != nil {
-		ociState.Annotations = notif.ContainerConfig.Annotations
-	} else {
-		ociState.Annotations = make(map[string]string)
+	if notif.ContainerConfig != "" {
+		ociSpec := &ocispec.Spec{}
+		_ = json.Unmarshal([]byte(notif.ContainerConfig), ociSpec)
+		if ociSpec.Annotations != nil {
+			ociState.Annotations = ociSpec.Annotations
+		}
 	}
 
 	var cmd string
 	switch notif.Type {
 	case containerhook.EventTypeAddContainer:
 		ociState.Status = ocispec.StateCreated
-		var config string
-		if notif.ContainerConfig != nil {
-			b, err := json.Marshal(notif.ContainerConfig)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to marshal ContainerConfig: %s\n", err)
-			} else {
-				config = string(b)
-			}
-		}
 		if outputAdd {
 			fmt.Printf("Container added: %v pid %d\n", notif.ContainerID, notif.ContainerPID)
 		}
 		if outputConfig {
-			if config != "" {
-				fmt.Printf("%s\n", config)
+			if notif.ContainerConfig != "" {
+				fmt.Printf("%s\n", notif.ContainerConfig)
 			} else {
 				fmt.Fprintf(os.Stderr, "Error: container config not found for container %s\n", notif.ContainerID)
 			}
 		}
 		if *publishKubernetesEvent {
-			if config != "" {
-				publishEvent("NewContainerConfig", config)
+			if notif.ContainerConfig != "" {
+				publishEvent("NewContainerConfig", notif.ContainerConfig)
 			} else {
 				publishEvent("ContainerConfigNotFound", "")
 			}

examples/kube-container-collection/main.go
@@ -16,7 +16,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -81,13 +80,8 @@ func callback(notif containercollection.PubSubEvent) {
 	switch notif.Type {
 	case containercollection.EventTypeAddContainer:
 		fmt.Printf("Container added: %v pid %d\n", notif.Container.Runtime.ContainerID, notif.Container.ContainerPid())
-		if notif.Container.OciConfig != nil {
-			config, err := json.Marshal(notif.Container.OciConfig)
-			if err != nil {
-				publishEvent(notif.Container, "CannotMarshalContainerConfig", err.Error())
-			} else {
-				publishEvent(notif.Container, "NewContainerConfig", string(config))
-			}
+		if notif.Container.OciConfig != "" {
+			publishEvent(notif.Container, "NewContainerConfig", notif.Container.OciConfig)
 		} else {
 			publishEvent(notif.Container, "ContainerConfigNotFound", "")
 		}

gadgets/ci/datasource-containers/test/integration/datasource_test.go
@@ -30,10 +30,12 @@ import (
 )
 
 type DatasourceContainersEvent struct {
-	CgroupID  uint64 `json:"cgroup_id"`
-	EventType string `json:"event_type"`
-	MntnsID   uint64 `json:"mntns_id"`
-	Name      string `json:"name"`
+	ContainerID     string `json:"container_id"`
+	ContainerConfig string `json:"container_config"`
+	CgroupID        uint64 `json:"cgroup_id"`
+	EventType       string `json:"event_type"`
+	MntnsID         uint64 `json:"mntns_id"`
+	Name            string `json:"name"`
 }
 
 func TestDatasourceContainers(t *testing.T) {
@@ -80,15 +82,21 @@ func TestDatasourceContainers(t *testing.T) {
 	runnerOpts = append(runnerOpts, igrunner.WithValidateOutput(
 		func(t *testing.T, output string) {
 			expectedEntry := &DatasourceContainersEvent{
-				EventType: "CREATED",
-				Name:      containerName,
-				MntnsID:   utils.NormalizedInt,
-				CgroupID:  utils.NormalizedInt,
+				EventType:       "CREATED",
+				Name:            containerName,
+				MntnsID:         utils.NormalizedInt,
+				CgroupID:        utils.NormalizedInt,
+				ContainerID:     utils.NormalizedStr,
+				ContainerConfig: "", // See issue 3884
 			}
 
 			normalize := func(e *DatasourceContainersEvent) {
 				utils.NormalizeInt(&e.CgroupID)
 				utils.NormalizeInt(&e.MntnsID)
+				utils.NormalizeString(&e.ContainerID)
+				// ContainerConfig is sometimes empty:
+				// https://github.com/inspektor-gadget/inspektor-gadget/issues/3884
+				e.ContainerConfig = ""
 			}
 
 			match.MatchEntries(t, match.JSONMultiObjectMode, output, normalize, expectedEntry)

integration/ig/non-k8s/list_containers_test.go
@@ -43,7 +43,7 @@ func TestFilterByContainerName(t *testing.T) {
 			}
 
 			normalize := func(c *containercollection.Container) {
-				c.OciConfig = nil
+				c.OciConfig = ""
 				c.Bundle = ""
 				c.Mntns = 0
 				c.Netns = 0
@@ -110,7 +110,7 @@ func TestWatchContainers(t *testing.T) {
 			}
 
 			normalize := func(e *containercollection.PubSubEvent) {
-				e.Container.OciConfig = nil
+				e.Container.OciConfig = ""
 				e.Container.Bundle = ""
 				e.Container.Mntns = 0
 				e.Container.Netns = 0

integration/k8s/enrichment_pod_label_test.go
@@ -90,7 +90,7 @@ func TestEnrichmentPodLabelExistingPod(t *testing.T) {
 			}
 
 			normalize := func(c *containercollection.Container) {
-				c.OciConfig = nil
+				c.OciConfig = ""
 				c.Bundle = ""
 				c.Mntns = 0
 				c.Netns = 0
@@ -163,7 +163,7 @@ func TestEnrichmentPodLabelNewPod(t *testing.T) {
 			}
 
 			normalize := func(e *containercollection.PubSubEvent) {
-				e.Container.OciConfig = nil
+				e.Container.OciConfig = ""
 				e.Container.Bundle = ""
 				e.Container.Mntns = 0
 				e.Container.Netns = 0

integration/k8s/list_containers_test.go
@@ -66,7 +66,7 @@ func newListContainerTestStep(
 			}
 
 			normalize := func(c *containercollection.Container) {
-				c.OciConfig = nil
+				c.OciConfig = ""
 				c.Bundle = ""
 				c.Mntns = 0
 				c.Netns = 0
@@ -199,7 +199,7 @@ func TestWatchCreatedContainers(t *testing.T) {
 			}
 
 			normalize := func(e *containercollection.PubSubEvent) {
-				e.Container.OciConfig = nil
+				e.Container.OciConfig = ""
 				e.Container.Bundle = ""
 				e.Container.Mntns = 0
 				e.Container.Netns = 0
@@ -295,7 +295,7 @@ func TestWatchDeletedContainers(t *testing.T) {
 			}
 
 			normalize := func(e *containercollection.PubSubEvent) {
-				e.Container.OciConfig = nil
+				e.Container.OciConfig = ""
 				e.Container.Bundle = ""
 				e.Container.Mntns = 0
 				e.Container.Netns = 0
@@ -394,7 +394,7 @@ func TestPodWithSecurityContext(t *testing.T) {
 			}
 
 			normalize := func(e *containercollection.PubSubEvent) {
-				e.Container.OciConfig = nil
+				e.Container.OciConfig = ""
 				e.Container.Bundle = ""
 				e.Container.Mntns = 0
 				e.Container.Netns = 0

commit 3940f9db1e16e18d0a429e50ecea84f26eadb85e

container-collection: improve error messages
When ig fails to get the OCI config, the error messages were confusing:

WARN[0001] kubernetes enricher: failed to get source mounts for container 0cae3a07a7f140e770c415d421d7ff641b46ca2bff898e2226d8069e6e086e5d: unexpected end of JSON input
WARN[0001] kubernetes enricher: failed to get source mounts for container 6a01354d1e6a0d7902b8cda9f3b15ab982d4915c62172a6ebecc197aa0131445: unexpected end of JSON input
WARN[0001] kubernetes enricher: failed to get source mounts for container 9b30e0412a63f36fbd959ffe913d8618b1bfec555cc9d322904991e723e20cae: unexpected end of JSON input

To reproduce:
$ minikube cp ig /bin/ig
$ minikube ssh sudo chmod +x /bin/ig
$ minikube ssh "sudo NODE_NAME=minikube-docker ig run trace_exec:main --enrich-with-k8s-apiserver --kubeconfig=/etc/kubernetes/admin.conf"

This patch:
- improves the warning message
- removes the warning message if the OCI config is not actually needed

Fixes: b1f24a0bcb7f ("container-collection: publish oci config as a string")

Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

pkg/container-collection/ocispec.go
@@ -20,10 +20,15 @@ package containercollection
 
 import (
 	"encoding/json"
+	"errors"
 )
 
 // ociConfigGetSourceMounts returns the source mounts from the oci config
 func ociConfigGetSourceMounts(ociConfig string) (out []string, err error) {
+	if ociConfig == "" {
+		return nil, errors.New("ociConfig is empty")
+	}
+
 	var config struct {
 		Mounts []struct {
 			Source string `json:"source,omitempty"`
@@ -41,6 +46,10 @@ func ociConfigGetSourceMounts(ociConfig string) (out []string, err error) {
 
 // ociConfigGetAnnotations returns the annotations from the oci config
 func ociConfigGetAnnotations(ociConfig string) (map[string]string, error) {
+	if ociConfig == "" {
+		return nil, errors.New("ociConfig is empty")
+	}
+
 	var config struct {
 		Annotations map[string]string `json:"annotations,omitempty"`
 	}

pkg/container-collection/options.go
@@ -539,11 +539,6 @@ func WithKubernetesEnrichment(nodeName string) ContainerCollectionOption {
 						return false
 					}
 				}
-				srcs, err := ociConfigGetSourceMounts(container.OciConfig)
-				if err != nil {
-					log.Warnf("kubernetes enricher: failed to get source mounts for container %s: %s", container.Runtime.ContainerID, err)
-					// We won't get ContainerName but keep going
-				}
 
 				if container.K8s.ContainerName == "" {
 					var containerName string
@@ -558,6 +553,13 @@ func WithKubernetesEnrichment(nodeName string) ContainerCollectionOption {
 					for _, c := range pod.Spec.EphemeralContainers {
 						containerNames = append(containerNames, c.Name)
 					}
+
+					srcs, err := ociConfigGetSourceMounts(container.OciConfig)
+					if err != nil {
+						log.Warnf("kubernetes enricher: failed to get source mounts for container %s: %s", container.Runtime.ContainerID, err)
+						// We won't get ContainerName but keep going
+					}
+
 				outerLoop:
 					for _, name := range containerNames {
 						for _, src := range srcs {