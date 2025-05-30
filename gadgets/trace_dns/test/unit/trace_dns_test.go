2e57d8bea2157f238b5fde1b398dd7be8962f8c1
Mauricio Vásquez <mauriciov@microsoft.com>
2025-05-30 16:54:44 -0500

    gadgets: Add dummy unit tests for all gadgets
    Add dummy unit tests to verify that at least those gadgets are running
    fine on different kernel versions. For sure, we need to extend them
    later on to verify the data provided by the gadget.
    
    Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

---
+++ gadgets/trace_dns/test/unit/trace_dns_test.go
@@ -0,0 +1,44 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"testing"
+	"time"
+
+	gadgettesting "github.com/inspektor-gadget/inspektor-gadget/gadgets/testing"
+	utilstest "github.com/inspektor-gadget/inspektor-gadget/internal/test"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/gadgetrunner"
+)
+
+func TestTraceDns(t *testing.T) {
+	// TODO: This is a dummy test to check that the gadget runs without errors.
+	// It should be extended to check that the gadget produces the right events.
+	gadgettesting.InitUnitTest(t)
+
+	runner := utilstest.NewRunnerWithTest(t, &utilstest.RunnerConfig{})
+
+	opts := gadgetrunner.GadgetRunnerOpts[any]{
+		Image:          "trace_dns",
+		Timeout:        5 * time.Second,
+		MntnsFilterMap: utilstest.CreateMntNsFilterMap(t, runner.Info.MountNsID),
+		ParamValues: map[string]string{
+			"operator.oci.ebpf.paths": "true",
+		},
+	}
+
+	gadgetRunner := gadgetrunner.NewGadgetRunner(t, opts)
+	gadgetRunner.RunGadget()
+}
