5c743c64a6d959ed7a618d70062d1bee8cb8f7cb
Mauricio Vásquez <mauriciov@microsoft.com>
2025-05-30 16:54:44 -0500

    gadgets: Add dummy unit tests for all gadgets
    Add dummy unit tests to verify that at least those gadgets are running
    fine on different kernel versions. For sure, we need to extend them
    later on to verify the data provided by the gadget.
    
    Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

---
+++ gadgets/profile_blockio/test/unit/profile_blockio_test.go
@@ -0,0 +1,33 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"testing"
+
+	gadgettesting "github.com/inspektor-gadget/inspektor-gadget/gadgets/testing"
+)
+
+func TestProfileBlockio(t *testing.T) {
+	// TODO: This is a dummy test to check that the gadget runs without errors.
+	// It should be extended to check that the gadget produces correct data.
+
+	// TODO: This is the minimum kernel version the gadget works on among the
+	// ones we test. We need to check if it works with other versions that we
+	// don't test, like 5.9
+	gadgettesting.MinimumKernelVersion(t, "5.10")
+
+	gadgettesting.DummyGadgetTest(t, "profile_blockio")
+}
