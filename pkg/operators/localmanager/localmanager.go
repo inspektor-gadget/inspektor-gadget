inspektor-gadget/inspektor-gadget

commit 0a60b97d4a151bb57c093332265f6c735e647c14

pkg/containers-publisher: Add new containerspublisher package.
This package contains the previous containers datasource which is used to inform
WASM about CREATED and DELETED containers.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/localmanager/localmanager.go
@@ -40,6 +40,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	igmanager "github.com/inspektor-gadget/inspektor-gadget/pkg/ig-manager"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/common"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
@@ -321,36 +322,11 @@ type localManagerTrace struct {
 
 	eventWrappers map[datasource.DataSource]*compat.EventWrapperBase
 
-	containersDs              datasource.DataSource
-	eventTypeField            datasource.FieldAccessor
-	cgroupIDField             datasource.FieldAccessor
-	mountNsIDField            datasource.FieldAccessor
-	nameField                 datasource.FieldAccessor
-	containersSubscriptionKey string
+	containersPublisher *common.ContainersPublisher
 }
 
 func (l *localManagerTrace) Name() string {
 	return OperatorName
-}
-
-func (l *localManagerTrace) emitContainersDatasourceEvent(eventType containercollection.EventType, container *containercollection.Container) error {
-	ev, err := l.containersDs.NewPacketSingle()
-	if err != nil {
-		return fmt.Errorf("creating new containers datasource packet: %w", err)
-	}
-
-	l.eventTypeField.PutString(ev, eventType.String())
-	l.cgroupIDField.PutUint64(ev, container.CgroupID)
-	l.mountNsIDField.PutUint64(ev, container.Mntns)
-	l.nameField.PutString(ev, container.Runtime.ContainerName)
-
-	err = l.containersDs.EmitAndRelease(ev)
-	if err != nil {
-		log.Errorf("%v", err)
-		return fmt.Errorf("emitting containers datasource event: %w", err)
-	}
-
-	return nil
 }
 
 func (l *localManagerTrace) PreGadgetRun() error {
@@ -486,9 +462,6 @@ func (l *localManagerTrace) PostGadgetRun() error {
 			}
 		}
 	}
-	if l.containersSubscriptionKey != "" {
-		l.manager.igManager.Unsubscribe(l.containersSubscriptionKey)
-	}
 	return nil
 }
 
@@ -542,37 +515,11 @@ func (l *localManager) InstantiateDataOperator(gadgetCtx operators.GadgetContext
 
 	enableContainersDs := v.GetBool("annotations.enable-containers-datasource")
 
-	var containersDs datasource.DataSource
-	var eventTypeField datasource.FieldAccessor
-	var cgroupIDField datasource.FieldAccessor
-	var mountNsIDField datasource.FieldAccessor
-	var nameField datasource.FieldAccessor
-
+	var containersPublisher *common.ContainersPublisher
 	if enableContainersDs {
-		containersDs, err = gadgetCtx.RegisterDataSource(datasource.TypeSingle, "containers")
+		containersPublisher, err = common.NewContainersPublisher(gadgetCtx, &l.igManager.ContainerCollection)
 		if err != nil {
-			return nil, fmt.Errorf("creating datasource: %w", err)
-		}
-		containersDs.AddAnnotation("cli.default-output-mode", "none")
-
-		eventTypeField, err = containersDs.AddField("event_type", api.Kind_String)
-		if err != nil {
-			return nil, fmt.Errorf("adding field event_type: %w", err)
-		}
-
-		cgroupIDField, err = containersDs.AddField("cgroup_id", api.Kind_Uint64)
-		if err != nil {
-			return nil, fmt.Errorf("adding field cgroup_id: %w", err)
-		}
-
-		mountNsIDField, err = containersDs.AddField("mntns_id", api.Kind_Uint64)
-		if err != nil {
-			return nil, fmt.Errorf("adding field mntns_id: %w", err)
-		}
-
-		nameField, err = containersDs.AddField("name", api.Kind_String)
-		if err != nil {
-			return nil, fmt.Errorf("adding field name: %w", err)
+			return nil, fmt.Errorf("creating containers publisher: %w", err)
 		}
 	}
 
@@ -584,13 +531,10 @@ func (l *localManager) InstantiateDataOperator(gadgetCtx operators.GadgetContext
 			attachedContainers: make(map[*containercollection.Container]struct{}),
 			params:             params,
 			gadgetCtx:          gadgetCtx,
-			eventWrappers:      make(map[datasource.DataSource]*compat.EventWrapperBase),
 
-			containersDs:   containersDs,
-			eventTypeField: eventTypeField,
-			cgroupIDField:  cgroupIDField,
-			mountNsIDField: mountNsIDField,
-			nameField:      nameField,
+			eventWrappers: make(map[datasource.DataSource]*compat.EventWrapperBase),
+
+			containersPublisher: containersPublisher,
 		},
 	}
 
@@ -703,50 +647,30 @@ func (l *localManagerTraceWrapper) PreStart(gadgetCtx operators.GadgetContext) e
 }
 
 func (l *localManagerTraceWrapper) Start(gadgetCtx operators.GadgetContext) error {
-	host := l.params.Get(Host).AsBool()
-	var containers []*containercollection.Container
-
-	if l.containersDs == nil {
+	if l.containersPublisher == nil {
 		return nil
 	}
 
-	if l.manager.igManager != nil {
-		containerSelector := containercollection.ContainerSelector{
-			Runtime: containercollection.RuntimeSelector{
-				ContainerName: l.params.Get(ContainerName).AsString(),
-			},
-		}
-
-		l.containersSubscriptionKey = uuid.New().String()
-
-		log.Debugf("add datasource containers subscription to igManager")
-		containers = l.manager.igManager.Subscribe(
-			l.containersSubscriptionKey,
-			containerSelector,
-			func(event containercollection.PubSubEvent) {
-				err := l.emitContainersDatasourceEvent(event.Type, event.Container)
-				if err != nil {
-					log.Errorf("emitting containers datasource event: %v", err)
-				}
-			},
-		)
+	host := l.params.Get(Host).AsBool()
+	containerSelector := containercollection.ContainerSelector{
+		Runtime: containercollection.RuntimeSelector{
+			ContainerName: l.params.Get(ContainerName).AsString(),
+		},
 	}
 
+	extraContainers := []*containercollection.Container{}
 	if host {
-		containers = append(containers, l.manager.fakeContainer)
+		extraContainers = append(extraContainers, l.manager.fakeContainer)
 	}
 
-	for _, container := range containers {
-		err := l.emitContainersDatasourceEvent(containercollection.EventTypeAddContainer, container)
-		if err != nil {
-			return fmt.Errorf("emitting containers datasource event: %w", err)
-		}
-	}
-
-	return nil
+	return l.containersPublisher.PublishContainers(extraContainers, containerSelector)
 }
 
 func (l *localManagerTraceWrapper) Stop(gadgetCtx operators.GadgetContext) error {
+	if l.containersPublisher != nil {
+		l.containersPublisher.Unsubscribe()
+	}
+
 	return l.PostGadgetRun()
 }
 

pkg/operators/common/containers-publisher.go
@@ -1,0 +1,131 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+
+	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+)
+
+type ContainersPublisher struct {
+	collection *containercollection.ContainerCollection
+
+	containersDs              datasource.DataSource
+	eventTypeField            datasource.FieldAccessor
+	cgroupIDField             datasource.FieldAccessor
+	mountNsIDField            datasource.FieldAccessor
+	nameField                 datasource.FieldAccessor
+	containersSubscriptionKey string
+}
+
+func NewContainersPublisher(gadgetCtx operators.GadgetContext, collection *containercollection.ContainerCollection) (*ContainersPublisher, error) {
+	publisher := &ContainersPublisher{}
+	var err error
+
+	publisher.containersDs, err = gadgetCtx.RegisterDataSource(datasource.TypeSingle, "containers")
+	if err != nil {
+		return nil, fmt.Errorf("creating datasource: %w", err)
+	}
+	publisher.containersDs.AddAnnotation("cli.default-output-mode", "none")
+
+	publisher.eventTypeField, err = publisher.containersDs.AddField("event_type", api.Kind_String)
+	if err != nil {
+		return nil, fmt.Errorf("adding field event_type: %w", err)
+	}
+
+	publisher.cgroupIDField, err = publisher.containersDs.AddField("cgroup_id", api.Kind_Uint64)
+	if err != nil {
+		return nil, fmt.Errorf("adding field cgroup_id: %w", err)
+	}
+
+	publisher.mountNsIDField, err = publisher.containersDs.AddField("mntns_id", api.Kind_Uint64)
+	if err != nil {
+		return nil, fmt.Errorf("adding field mntns_id: %w", err)
+	}
+
+	publisher.nameField, err = publisher.containersDs.AddField("name", api.Kind_String)
+	if err != nil {
+		return nil, fmt.Errorf("adding field name: %w", err)
+	}
+
+	publisher.collection = collection
+
+	return publisher, nil
+}
+
+func (publisher *ContainersPublisher) emitContainersDatasourceEvent(eventType containercollection.EventType, container *containercollection.Container) error {
+	ev, err := publisher.containersDs.NewPacketSingle()
+	if err != nil {
+		return fmt.Errorf("creating new containers datasource packet: %w", err)
+	}
+
+	publisher.eventTypeField.PutString(ev, eventType.String())
+	publisher.cgroupIDField.PutUint64(ev, container.CgroupID)
+	publisher.mountNsIDField.PutUint64(ev, container.Mntns)
+	publisher.nameField.PutString(ev, container.Runtime.ContainerName)
+
+	err = publisher.containersDs.EmitAndRelease(ev)
+	if err != nil {
+		return fmt.Errorf("emitting containers datasource event: %w", err)
+	}
+
+	return nil
+}
+
+func (publisher *ContainersPublisher) PublishContainers(extraContainers []*containercollection.Container, containerSelector containercollection.ContainerSelector) error {
+	var containers []*containercollection.Container
+
+	if publisher.collection != nil {
+		publisher.containersSubscriptionKey = uuid.New().String()
+
+		log.Debugf("add datasource containers subscription to container collection")
+		containers = publisher.collection.Subscribe(
+			publisher.containersSubscriptionKey,
+			containerSelector,
+			func(event containercollection.PubSubEvent) {
+				err := publisher.emitContainersDatasourceEvent(event.Type, event.Container)
+				if err != nil {
+					log.Errorf("publishing new container event: %v", err)
+				}
+			},
+		)
+	}
+
+	if len(extraContainers) > 0 {
+		containers = append(containers, extraContainers...)
+	}
+
+	for _, container := range containers {
+		err := publisher.emitContainersDatasourceEvent(containercollection.EventTypeAddContainer, container)
+		if err != nil {
+			return fmt.Errorf("publishing existing container event: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (publisher *ContainersPublisher) Unsubscribe() {
+	if publisher.containersSubscriptionKey != "" {
+		publisher.collection.Unsubscribe(publisher.containersSubscriptionKey)
+	}
+}

commit cacae6e8a03beadee80dc6e9d171bfec446a7faa

localmanager: Fix crash when container collection isn't available
If the container collection is not available and the containerDS is
requested, the code crashes as l.igManager is nil. Add an explicit
check for it.

Fixes: 0a60b97d4a15 ("pkg/containers-publisher: Add new containerspublisher package.")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/localmanager/localmanager.go
@@ -544,6 +544,10 @@ func (l *localManager) InstantiateDataOperator(gadgetCtx operators.GadgetContext
 
 	var containersPublisher *common.ContainersPublisher
 	if enableContainersDs {
+		if l.igManager == nil {
+			return nil, fmt.Errorf("container-collection isn't available, but containers datasource is enabled")
+		}
+
 		containersPublisher, err = common.NewContainersPublisher(gadgetCtx, &l.igManager.ContainerCollection)
 		if err != nil {
 			return nil, fmt.Errorf("creating containers publisher: %w", err)