699e246012e72f86b41e8f9f314ff618996d94dc
Alban Crequy <alban.crequy@gmail.com>
2025-06-04 11:53:44 +0200

    test: precreate events on containers datasource
    Add a new test for the containers datasource. The container is started
    after the datasource-containers gadget, so that the precreate event can
    be recorded.
    
    Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

--- gadgets/ci/datasource-containers/go/program.go
+++ gadgets/ci/datasource-containers/go/program.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -50,7 +50,7 @@ func gadgetPreStart() int {
 	}
 
 	ds.Subscribe(func(ds api.DataSource, data api.Data) {
-		_, err := eventTypeField.String(data, 7)
+		_, err := eventTypeField.String(data, api.DataSourceContainersEventTypeMaxSize)
 		if err != nil {
 			api.Errorf("getting event_type from corresponding field: %v", err)
 			return
