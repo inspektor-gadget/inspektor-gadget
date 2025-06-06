53bc6e6150851e7ff6d537b5bdd2241b361df123
bdharsan04 <bdharsan79@gmail.com>
2025-06-07 01:40:33 +0530

    The histogram was showing misaligned data due to an off-by-one error in the get_slot_idx() function. This caused values to be assigned to the wrong bins, skewing the output. This commit corrects the binning logic so values now map to the intended histogram slots. As a result, the histogram output more accurately reflects the actual data distribution.

--- gadgets/profile_tcprtt/program.bpf.c
+++ gadgets/profile_tcprtt/program.bpf.c
@@ -170,7 +170,7 @@ static int handle_tcp_rcv_established(struct sock *sk)
 	srtt = BPF_CORE_READ(ts, srtt_us) >> 3;
 	if (targ_ms)
 		srtt /= 1000U;
-	slot = log2l(srtt);
+	slot = get_slot_idx(srtt);
 	if (slot >= PROFILER_MAX_SLOTS)
 		slot = PROFILER_MAX_SLOTS - 1;
 	__sync_fetch_and_add(&histp->latency[slot], 1);
