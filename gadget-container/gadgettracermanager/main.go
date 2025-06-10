567d76cb120abd201ae5a5ea7724edded992152a
Michael Friese <mfriese@microsoft.com>
2025-06-10 18:19:51 +0200

    register expr operator
    Signed-off-by: Michael Friese <mfriese@microsoft.com>

--- gadget-container/gadgettracermanager/main.go
+++ gadget-container/gadgettracermanager/main.go
@@ -58,6 +58,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/btfgen"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/ebpf"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/env"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/expr"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/filter"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/formatters"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/kubeipresolver"
