67b2b91e990adc65b6d1ee582af6a97fffee1a97
Michael Friese <mfriese@microsoft.com>
2025-06-16 22:18:57 +0200

    pkg/trace: add global tracing facility
    Signed-off-by: Michael Friese <mfriese@microsoft.com>

--- pkg/trace/trace.go
+++ pkg/trace/trace.go
@@ -16,38 +16,78 @@ package trace
 
 import (
 	"context"
+	"fmt"
+	"os"
 
-	"github.com/spf13/viper"
+	log "github.com/sirupsen/logrus"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 
 	"github.com/inspektor-gadget/inspektor-gadget/internal/version"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/config"
 )
 
-func RegisterGlobalProvider(ctx context.Context) error {
-	endpoint := viper.GetString("otel.trace.endpoint")
-	insecure := viper.GetBool("otel.trace.insecure")
+func RegisterGlobalProvider(ctx context.Context) (func(context.Context) error, error) {
+	expType := config.Config.GetString("otel.trace.exporter")
 
-	if endpoint == "" {
-		// not enabled
+	var funcs []func(context.Context) error
+
+	sf := func(ctx context.Context) error {
+		for _, fn := range funcs {
+			fn(ctx)
+		}
 		return nil
 	}
 
-	var options []otlptracegrpc.Option
-
-	if insecure {
-		options = append(options, otlptracegrpc.WithInsecure())
+	if expType == "" {
+		// not enabled
+		return sf, nil
 	}
-	options = append(options, otlptracegrpc.WithEndpoint(endpoint))
 
-	client := otlptracegrpc.NewClient(options...)
-	exp, err := otlptrace.New(ctx, client)
-	if err != nil {
-		return err
+	var err error
+	var exp sdktrace.SpanExporter
+
+	switch expType {
+	case "file":
+		filename := config.Config.GetString("otel.trace.filename")
+		if filename == "" {
+			return sf, fmt.Errorf("otel.trace.filename not set")
+		}
+
+		f, err := os.Create(filename)
+		if err != nil {
+			return sf, fmt.Errorf("creating otel.trace.filename: %w", err)
+		}
+		funcs = append(funcs, func(ctx context.Context) error {
+			return f.Close()
+		})
+
+		exp, err = stdouttrace.New(
+			stdouttrace.WithPrettyPrint(),
+			stdouttrace.WithWriter(f),
+		)
+		if err != nil {
+			return nil, fmt.Errorf("creating stdout exporter: %w", err)
+		}
+	case "otlp-grpc":
+		endpoint := config.Config.GetString("otel.trace.endpoint")
+		insecure := config.Config.GetBool("otel.trace.insecure")
+
+		var options []otlptracegrpc.Option
+		if insecure {
+			options = append(options, otlptracegrpc.WithInsecure())
+		}
+		options = append(options, otlptracegrpc.WithEndpoint(endpoint))
+		client := otlptracegrpc.NewClient(options...)
+		exp, err = otlptrace.New(ctx, client)
+		if err != nil {
+			return sf, err
+		}
 	}
 
 	res, err := resource.New(ctx,
@@ -57,7 +97,7 @@ func RegisterGlobalProvider(ctx context.Context) error {
 		),
 	)
 	if err != nil {
-		return err
+		return sf, err
 	}
 
 	tp := sdktrace.NewTracerProvider(
@@ -68,5 +108,9 @@ func RegisterGlobalProvider(ctx context.Context) error {
 
 	// Register as global
 	otel.SetTracerProvider(tp)
-	return nil
+
+	funcs = append([]func(context.Context) error{tp.Shutdown}, funcs...)
+
+	log.Infof("initialized tracing")
+	return sf, nil
 }
