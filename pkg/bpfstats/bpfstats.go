inspektor-gadget/inspektor-gadget

commit d5f64719b55d1986ae728d1aba55026e5ad4942a

new gadget: ebpftop

pkg/bpfstats/bpfstats.go
@@ -1,0 +1,121 @@
+// Copyright 2019-2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bpfstats
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"sync"
+
+	"github.com/cilium/ebpf"
+	"golang.org/x/sys/unix"
+)
+
+type BPFStatsMethod int
+
+const (
+	// MethodNone means that no call to EnableBPFStats() has been made or was unsuccessful
+	MethodNone BPFStatsMethod = iota
+
+	// MethodBPFFunc uses stats collection via BPF(BPF_ENABLE_STATS)
+	MethodBPFFunc
+
+	// MethodSysctl uses stats collection via sysctl (/proc/sys/kernel/bpf_stats_enabled)
+	MethodSysctl
+)
+
+var (
+	mutex     sync.Mutex
+	refCnt    int
+	statsSock io.Closer
+	method    = MethodNone
+)
+
+// EnableBPFStats enables collection of bpf program stats. It tries to use BPF_ENABLE_STATS first
+// (which requires Linux >= 5.8). If that fails, it will fall back to trying to
+// enable via sysctl (/proc/sys/kernel/bpf_stats_enabled). This function will make
+// sure that repeated calls will not enable stats collection more than once. Instead,
+// it will keep track of the number of calls and only stop stat collection, when
+// DisableBPFStats() has been called the same number of times.
+func EnableBPFStats() error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if refCnt != 0 {
+		return nil
+	}
+
+	// Actually enable
+	s, err := ebpf.EnableStats(unix.BPF_STATS_RUN_TIME)
+	if err != nil {
+		// Use fallback method
+		err = ioutil.WriteFile("/proc/sys/kernel/bpf_stats_enabled", []byte("1"), 0o644)
+		if err != nil {
+			return fmt.Errorf("could not enable stat collection: %w", err)
+		}
+		method = MethodSysctl
+	} else {
+		statsSock = s
+		method = MethodBPFFunc
+	}
+
+	refCnt++
+
+	return nil
+}
+
+// DisableBPFStats disables collection of bpf program stats if no consumer
+func DisableBPFStats() error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	refCnt--
+
+	if refCnt < 0 {
+		refCnt = 0
+		return errors.New("bpf stat collection already disabled")
+	}
+
+	if refCnt != 0 {
+		return nil
+	}
+
+	// Actually disable
+	switch method {
+	case MethodBPFFunc:
+		err := statsSock.Close()
+		statsSock = nil
+		if err != nil {
+			return fmt.Errorf("could not disable stat collection using BPF(): %w", err)
+		}
+	case MethodSysctl:
+		err := ioutil.WriteFile("/proc/sys/kernel/bpf_stats_enabled", []byte("0"), 0o644)
+		if err != nil {
+			return fmt.Errorf("could not disable stat collection using sysctl: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// GetMethod returns the currently used method to enable stats collection. If
+// EnableBPFStats() has not yet been called, it will return MethodNone.
+func GetMethod() BPFStatsMethod {
+	mutex.Lock()
+	defer mutex.Unlock()
+	return method
+}

cmd/kubectl-gadget/top/ebpf.go
@@ -1,0 +1,307 @@
+// Copyright 2019-2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package top
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	commonutils "github.com/kinvolk/inspektor-gadget/cmd/common/utils"
+	"github.com/kinvolk/inspektor-gadget/cmd/kubectl-gadget/utils"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/top/ebpf/types"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
+)
+
+var ebpfNodeStats map[string][]types.Stats
+
+var ebpfSortBy types.SortBy
+
+var ebpfCmd = &cobra.Command{
+	Use:   fmt.Sprintf("ebpf [interval=%d]", types.IntervalDefault),
+	Short: "Periodically report ebpf runtime stats",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+
+		if params.NamespaceOverridden {
+			return commonutils.WrapInErrInvalidArg("--namespace / -n",
+				fmt.Errorf("this gadget cannot filter by namespace"))
+		}
+		if params.Podname != "" {
+			return commonutils.WrapInErrInvalidArg("--podname / -p",
+				fmt.Errorf("this gadget cannot filter by pod name"))
+		}
+		if params.Containername != "" {
+			return commonutils.WrapInErrInvalidArg("--containername / -c",
+				fmt.Errorf("this gadget cannot filter by container name"))
+		}
+		if len(params.Labels) > 0 {
+			return commonutils.WrapInErrInvalidArg("--selector / -l",
+				fmt.Errorf("this gadget cannot filter by selector"))
+		}
+
+		ebpfNodeStats = make(map[string][]types.Stats)
+
+		if len(args) == 1 {
+			outputInterval, err = strconv.Atoi(args[0])
+			if err != nil {
+				return commonutils.WrapInErrInvalidArg("<interval>",
+					fmt.Errorf("%q is not a valid value", args[0]))
+			}
+		} else {
+			outputInterval = types.IntervalDefault
+		}
+
+		config := &utils.TraceConfig{
+			GadgetName:       "ebpftop",
+			Operation:        "start",
+			TraceOutputMode:  "Stream",
+			TraceOutputState: "Started",
+			CommonFlags:      &params,
+			Parameters: map[string]string{
+				types.MaxRowsParam:  strconv.Itoa(maxRows),
+				types.IntervalParam: strconv.Itoa(outputInterval),
+				types.SortByParam:   sortBy,
+			},
+		}
+
+		// when params.Timeout == interval it means the user
+		// only wants to run for a given amount of time and print
+		// that result.
+		singleShot := params.Timeout == outputInterval
+
+		// start print loop if this is not a "single shoot" operation
+		if singleShot {
+			ebpfPrintHeader()
+		} else {
+			ebpfStartPrintLoop()
+		}
+
+		if err := utils.RunTraceStreamCallback(config, ebpfCallback); err != nil {
+			return commonutils.WrapInErrRunGadget(err)
+		}
+
+		if singleShot {
+			ebpfPrintEvents()
+		}
+
+		return nil
+	},
+	SilenceUsage: true,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		ebpfSortBy, err = types.ParseSortBy(sortBy)
+		if err != nil {
+			return commonutils.WrapInErrInvalidArg("--sort", err)
+		}
+
+		return nil
+	},
+	Args: cobra.MaximumNArgs(1),
+}
+
+func init() {
+	addTopCommand(ebpfCmd, types.MaxRowsDefault, types.SortBySlice)
+}
+
+func ebpfCallback(line string, node string) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	var event types.Event
+
+	if err := json.Unmarshal([]byte(line), &event); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s", commonutils.WrapInErrUnmarshalOutput(err, line))
+		return
+	}
+
+	if event.Error != "" {
+		fmt.Fprintf(os.Stderr, "Error: failed on node %q: %s", event.Node, event.Error)
+		return
+	}
+
+	ebpfNodeStats[node] = event.Stats
+}
+
+func ebpfStartPrintLoop() {
+	go func() {
+		ticker := time.NewTicker(time.Duration(outputInterval) * time.Second)
+		ebpfPrintHeader()
+		for {
+			_ = <-ticker.C
+			ebpfPrintHeader()
+			ebpfPrintEvents()
+		}
+	}()
+}
+
+func ebpfPrintHeader() {
+	switch params.OutputMode {
+	case commonutils.OutputModeColumns:
+		if term.IsTerminal(int(os.Stdout.Fd())) {
+			utils.ClearScreen()
+		} else {
+			fmt.Println("")
+		}
+		fmt.Printf("%-16s %-8s %-16s %-16s %-7s %-20s %12s %10s\n",
+			"NODE",
+			"PROGID",
+			"TYPE",
+			"NAME",
+			"PID", "COMM",
+			"RUNTIME",
+			"RUNCOUNT",
+		)
+	case commonutils.OutputModeCustomColumns:
+		if term.IsTerminal(int(os.Stdout.Fd())) {
+			utils.ClearScreen()
+		} else {
+			fmt.Println("")
+		}
+		fmt.Println(ebpfGetCustomColsHeader(params.CustomColumns))
+	}
+}
+
+func ebpfPrintEvents() {
+	// sort and print events
+	mutex.Lock()
+
+	stats := []types.Stats{}
+	for node, stat := range ebpfNodeStats {
+		for i := range stat {
+			stat[i].Node = node
+		}
+		stats = append(stats, stat...)
+	}
+	ebpfNodeStats = make(map[string][]types.Stats)
+
+	mutex.Unlock()
+
+	types.SortStats(stats, ebpfSortBy)
+
+	switch params.OutputMode {
+	case commonutils.OutputModeColumns:
+		for idx, event := range stats {
+			if idx == maxRows {
+				break
+			}
+			pid := ""
+			comm := ""
+			if len(event.Pids) > 0 {
+				pid = strconv.Itoa(int(event.Pids[0].Pid))
+				comm = event.Pids[0].Comm
+			}
+			fmt.Printf("%-16s %-8d %-16s %-16s %-7s %-20s %12v %10d\n",
+				event.Node,
+				event.ProgramID,
+				event.Type,
+				event.Name,
+				pid, comm,
+				time.Duration(event.CurrentRuntime),
+				event.CurrentRunCount,
+			)
+
+		}
+	case commonutils.OutputModeJSON:
+		b, err := json.Marshal(stats)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s", commonutils.WrapInErrMarshalOutput(err))
+			return
+		}
+		fmt.Println(string(b))
+	case commonutils.OutputModeCustomColumns:
+		for idx, stat := range stats {
+			if idx == maxRows {
+				break
+			}
+			fmt.Println(ebpfFormatEventCustomCols(&stat, params.CustomColumns))
+		}
+	}
+}
+
+func ebpfGetCustomColsHeader(cols []string) string {
+	var sb strings.Builder
+
+	for _, col := range cols {
+		switch col {
+		case "node":
+			sb.WriteString(fmt.Sprintf("%-16s", "NODE"))
+		case "progid":
+			sb.WriteString(fmt.Sprintf("%-8s", "PROGID"))
+		case "type":
+			sb.WriteString(fmt.Sprintf("%-16s", "TYPE"))
+		case "name":
+			sb.WriteString(fmt.Sprintf("%-16s", "NAME"))
+		case "pid":
+			sb.WriteString(fmt.Sprintf("%-7s", "PID"))
+		case "comm":
+			sb.WriteString(fmt.Sprintf("%-20s", "COMM"))
+		case "runtime":
+			sb.WriteString(fmt.Sprintf("%12s", "RUNTIME"))
+		case "runcount":
+			sb.WriteString(fmt.Sprintf("%10s", "RUNCOUNT"))
+		case "totalruntime":
+			sb.WriteString(fmt.Sprintf("%12s", "T-RUNTIME"))
+		case "totalruncount":
+			sb.WriteString(fmt.Sprintf("%11s", "T-RUNCOUNT"))
+		}
+		sb.WriteRune(' ')
+	}
+
+	return sb.String()
+}
+
+func ebpfFormatEventCustomCols(stats *types.Stats, cols []string) string {
+	var sb strings.Builder
+	for _, col := range cols {
+		switch col {
+		case "node":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Node))
+		case "progid":
+			sb.WriteString(fmt.Sprintf("%-8d", stats.ProgramID))
+		case "type":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Type))
+		case "name":
+			sb.WriteString(fmt.Sprintf("%-16s", stats.Name))
+		case "pid":
+			pid := ""
+			if len(stats.Pids) > 0 {
+				pid = fmt.Sprintf("%d", stats.Pids[0].Pid)
+			}
+			sb.WriteString(fmt.Sprintf("%-7s", pid))
+		case "comm":
+			comm := ""
+			if len(stats.Pids) > 0 {
+				comm = stats.Pids[0].Comm
+			}
+			sb.WriteString(fmt.Sprintf("%-20s", comm))
+		case "runtime":
+			sb.WriteString(fmt.Sprintf("%12v", time.Duration(stats.CurrentRuntime)))
+		case "runcount":
+			sb.WriteString(fmt.Sprintf("%10d", stats.CurrentRunCount))
+		case "totalruntime":
+			sb.WriteString(fmt.Sprintf("%12v", time.Duration(stats.TotalRuntime)))
+		case "totalruncount":
+			sb.WriteString(fmt.Sprintf("%11d", stats.TotalRunCount))
+		}
+		sb.WriteRune(' ')
+	}
+	return sb.String()
+}

integration/integration_test.go
@@ -442,6 +442,23 @@ func TestDns(t *testing.T) {
 	runCommands(commands, t)
 }
 
+func TestEbpftop(t *testing.T) {
+	t.Parallel()
+
+	ebpftopCmd := &command{
+		name:           "StartEbpftopGadget",
+		cmd:            fmt.Sprintf("$KUBECTL_GADGET top ebpf"),
+		expectedRegexp: fmt.Sprintf(`\S*\s+\d+\s+Tracing\s+gadget_ebpftop\s+\d+\s+\S*\s+`),
+		startAndStop:   true,
+	}
+
+	commands := []*command{
+		ebpftopCmd,
+	}
+
+	runCommands(commands, t)
+}
+
 func TestExecsnoop(t *testing.T) {
 	ns := generateTestNamespaceName("test-execsnoop")
 

pkg/gadget-collection/gadget-collection.go
@@ -23,6 +23,7 @@ import (
 	processcollector "github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets/snapshot/process"
 	socketcollector "github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets/snapshot/socket"
 	biotop "github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets/top/block-io"
+	ebpftop "github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets/top/ebpf"
 	filetop "github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets/top/file"
 	tcptop "github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets/top/tcp"
 	bindsnoop "github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets/trace/bind"
@@ -49,6 +50,7 @@ func TraceFactories() map[string]gadgets.TraceFactory {
 		"biotop":            biotop.NewFactory(),
 		"capabilities":      capabilities.NewFactory(),
 		"dns":               dns.NewFactory(),
+		"ebpftop":           ebpftop.NewFactory(),
 		"execsnoop":         execsnoop.NewFactory(),
 		"filetop":           filetop.NewFactory(),
 		"fsslower":          fsslower.NewFactory(),
@@ -73,6 +75,7 @@ func TraceFactoriesForLocalGadget() map[string]gadgets.TraceFactory {
 	return map[string]gadgets.TraceFactory{
 		"audit-seccomp":     auditseccomp.NewFactory(),
 		"dns":               dns.NewFactory(),
+		"ebpftop":           ebpftop.NewFactory(),
 		"network-graph":     networkgraph.NewFactory(),
 		"process-collector": processcollector.NewFactory(),
 		"socket-collector":  socketcollector.NewFactory(),

pkg/gadget-collection/gadgets/top/ebpf/gadget.go
@@ -1,0 +1,209 @@
+// Copyright 2019-2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpf
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	gadgetv1alpha1 "github.com/kinvolk/inspektor-gadget/pkg/apis/gadget/v1alpha1"
+	"github.com/kinvolk/inspektor-gadget/pkg/bpfstats"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadget-collection/gadgets"
+	ebpftoptracer "github.com/kinvolk/inspektor-gadget/pkg/gadgets/top/ebpf/tracer"
+	"github.com/kinvolk/inspektor-gadget/pkg/gadgets/top/ebpf/types"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Trace struct {
+	helpers gadgets.GadgetHelpers
+
+	traceName string
+	node      string
+	started   bool
+
+	tracer *ebpftoptracer.Tracer
+}
+
+type TraceFactory struct {
+	gadgets.BaseFactory
+}
+
+func NewFactory() gadgets.TraceFactory {
+	return &TraceFactory{
+		BaseFactory: gadgets.BaseFactory{DeleteTrace: deleteTrace},
+	}
+}
+
+func (f *TraceFactory) Description() string {
+	t := `ebpftop shows cpu time used by ebpf programs.
+
+The following parameters are supported:
+ - %s: Output interval, in seconds. (default %d)
+ - %s: Maximum rows to print. (default %d)
+ - %s: The field to sort the results by (%s). (default %s)`
+	return fmt.Sprintf(t, types.IntervalParam, types.IntervalDefault,
+		types.MaxRowsParam, types.MaxRowsDefault,
+		types.SortByParam, strings.Join(types.SortBySlice, ","), types.SortByDefault)
+}
+
+func (f *TraceFactory) OutputModesSupported() map[string]struct{} {
+	return map[string]struct{}{
+		"Stream": {},
+	}
+}
+
+func deleteTrace(name string, t interface{}) {
+	trace := t.(*Trace)
+	if trace.started {
+		trace.stop()
+	}
+}
+
+func (f *TraceFactory) Operations() map[string]gadgets.TraceOperation {
+	n := func() interface{} {
+		return &Trace{
+			helpers: f.Helpers,
+		}
+	}
+
+	return map[string]gadgets.TraceOperation{
+		"start": {
+			Doc: "Start ebpftop gadget",
+			Operation: func(name string, trace *gadgetv1alpha1.Trace) {
+				f.LookupOrCreate(name, n).(*Trace).Start(trace)
+			},
+		},
+		"stop": {
+			Doc: "Stop ebpftop gadget",
+			Operation: func(name string, trace *gadgetv1alpha1.Trace) {
+				f.LookupOrCreate(name, n).(*Trace).Stop(trace)
+			},
+		},
+	}
+}
+
+func (t *Trace) Start(trace *gadgetv1alpha1.Trace) {
+	if t.started {
+		trace.Status.State = "Started"
+		return
+	}
+
+	t.traceName = gadgets.TraceName(trace.ObjectMeta.Namespace, trace.ObjectMeta.Name)
+	t.node = trace.Spec.Node
+
+	maxRows := types.MaxRowsDefault
+	intervalSeconds := types.IntervalDefault
+	sortBy := types.SortByDefault
+
+	if trace.Spec.Parameters != nil {
+		params := trace.Spec.Parameters
+		var err error
+
+		if val, ok := params[types.MaxRowsParam]; ok {
+			maxRows, err = strconv.Atoi(val)
+			if err != nil {
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for %s: %v", val, types.MaxRowsParam, err)
+				return
+			}
+		}
+
+		if val, ok := params[types.IntervalParam]; ok {
+			intervalSeconds, err = strconv.Atoi(val)
+			if err != nil {
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for %s: %v", val, types.IntervalParam, err)
+				return
+			}
+		}
+
+		if val, ok := params[types.SortByParam]; ok {
+			sortBy, err = types.ParseSortBy(val)
+			if err != nil {
+				trace.Status.OperationError = fmt.Sprintf("%q is not valid for %s: %v", val, types.SortByParam, err)
+				return
+			}
+		}
+	}
+
+	config := &ebpftoptracer.Config{
+		MaxRows:  maxRows,
+		Interval: time.Second * time.Duration(intervalSeconds),
+		SortBy:   sortBy,
+	}
+
+	statsCallback := func(stats []types.Stats) {
+		ev := types.Event{
+			Stats: stats,
+		}
+		ev.Node = trace.Spec.Node
+
+		r, err := json.Marshal(ev)
+		if err != nil {
+			log.Warnf("Gadget %s: Failed to marshal event: %s", trace.Spec.Gadget, err)
+			return
+		}
+		t.helpers.PublishEvent(t.traceName, string(r))
+	}
+
+	errorCallback := func(err error) {
+		ev := types.Event{
+			Error: fmt.Sprintf("Gadget failed with: %v", err),
+		}
+		ev.Node = trace.Spec.Node
+
+		r, err := json.Marshal(&ev)
+		if err != nil {
+			log.Warnf("Gadget %s: Failed to marshall event: %s", trace.Spec.Gadget, err)
+			return
+		}
+		t.helpers.PublishEvent(t.traceName, string(r))
+	}
+
+	tracer, err := ebpftoptracer.NewTracer(config, statsCallback, errorCallback)
+	if err != nil {
+		trace.Status.OperationError = fmt.Sprintf("failed to create tracer: %s", err)
+		return
+	}
+
+	t.tracer = tracer
+	t.started = true
+
+	trace.Status.State = "Started"
+}
+
+func (t *Trace) Stop(trace *gadgetv1alpha1.Trace) {
+	if !t.started {
+		trace.Status.OperationError = "Not started"
+		return
+	}
+
+	err := t.stop()
+	if err != nil {
+		trace.Status.OperationWarning = err.Error()
+	}
+
+	trace.Status.State = "Stopped"
+}
+
+func (t *Trace) stop() error {
+	t.tracer.Stop()
+	t.tracer = nil
+	t.started = false
+
+	return bpfstats.DisableBPFStats()
+}

pkg/gadgets/top/ebpf/piditer/bpf/pid_iter.bpf.c
@@ -1,0 +1,38 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright (c) 2020 Facebook, (c) 2022 The Inspektor Gadget authors */
+#include <vmlinux/vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include "pid_iter.h"
+
+const volatile __u64 bpf_prog_fops_addr = 0;
+
+struct pid_iter_entry *unused __attribute__((unused));
+
+SEC("iter/task_file")
+int gadget_ebpftop(struct bpf_iter__task_file *ctx)
+{
+	struct file *file = ctx->file;
+	struct task_struct *task = ctx->task;
+	struct pid_iter_entry e;
+
+	if (!file || !task)
+		return 0;
+
+	// We need to have an address of bpf_prog_fops to run
+	// TODO: Currently cilium/ebpf doesn't support .ksyms, this is why we get the info from userspace right now
+	if (bpf_prog_fops_addr == 0 || (__u64)(file->f_op) != bpf_prog_fops_addr)
+		return 0;
+
+	__builtin_memset(&e, 0, sizeof(e));
+	e.pid = task->tgid;
+	e.id = BPF_CORE_READ((struct bpf_prog *)(file->private_data), aux, id);
+
+	bpf_probe_read_kernel_str(&e.comm, sizeof(e.comm), task->group_leader->comm);
+	bpf_seq_write(ctx->meta->seq, &e, sizeof(e));
+
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";

pkg/gadgets/top/ebpf/piditer/bpf/pid_iter.h
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright (c) 2020 Facebook, (c) 2022 The Inspektor Gadget authors */
+#ifndef __PID_ITER_H
+#define __PID_ITER_H
+
+struct pid_iter_entry {
+	__u32 id;
+	__u32 pid;
+	__u8 comm[16];
+};
+
+#endif

pkg/gadgets/top/ebpf/piditer/iter.go
@@ -1,0 +1,193 @@
+// Copyright 2019-2022 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package piditer
+
+import "C"
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET -cc clang -type pid_iter_entry piditer ./bpf/pid_iter.bpf.c -- -I./bpf/ -I../../../../${TARGET}
+
+type PidIter struct {
+	objs piditerObjects
+	iter *link.Iter
+}
+
+type PidIterEntry struct {
+	ProgID uint32
+	Pid    uint32
+	Comm   string
+}
+
+var (
+	addrLock        sync.Mutex
+	bpfProgFopsAddr uint64
+	triedGetAddr    bool
+
+	iterEntrySize = int(unsafe.Sizeof(piditerPidIterEntry{}))
+)
+
+// getBpfProgFopsAddr reads the address of bpf_prog_fops from /proc/kallsyms. It
+// only does this on first call and caches the address.
+func getBpfProgFopsAddr() (uint64, error) {
+	addrLock.Lock()
+	defer addrLock.Unlock()
+
+	if triedGetAddr {
+		if bpfProgFopsAddr == 0 {
+			return 0, os.ErrNotExist
+		}
+		return bpfProgFopsAddr, nil
+	}
+
+	triedGetAddr = true
+
+	file, err := os.Open("/proc/kallsyms")
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		fields := strings.Fields(line)
+		if len(fields) < 3 {
+			continue
+		}
+
+		if fields[2] == "bpf_prog_fops" {
+			addr, err := strconv.ParseUint(fields[0], 16, 64)
+			if err != nil {
+				return 0, err
+			}
+
+			// cache addr
+			bpfProgFopsAddr = addr
+			return bpfProgFopsAddr, nil
+		}
+	}
+	return 0, os.ErrNotExist
+}
+
+func NewTracer() (iter *PidIter, err error) {
+	p := &PidIter{}
+	defer func() {
+		if err != nil {
+			if p.iter != nil {
+				p.iter.Close()
+			}
+			p.objs.Close()
+		}
+	}()
+
+	addr, err := getBpfProgFopsAddr()
+	if err != nil {
+		return nil, fmt.Errorf("could not get address for bpf_prog_fops")
+	}
+
+	spec, err := loadPiditer()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load ebpf program: %w", err)
+	}
+
+	consts := map[string]interface{}{
+		"bpf_prog_fops_addr": addr,
+	}
+
+	if err = spec.RewriteConstants(consts); err != nil {
+		return nil, fmt.Errorf("error RewriteConstants: %w", err)
+	}
+
+	opts := ebpf.CollectionOptions{}
+
+	if err = spec.LoadAndAssign(&p.objs, &opts); err != nil {
+		return nil, fmt.Errorf("failed to load ebpf program: %w", err)
+	}
+
+	p.iter, err = link.AttachIter(link.IterOptions{
+		Program: p.objs.GadgetEbpftop,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to attach iter: %w", err)
+	}
+
+	return p, nil
+}
+
+// DumpPids returns an array of PidIterEntry containing information
+// on which pid (and comm) has an open fd to which eBPF Program ID.
+func (p *PidIter) DumpPids() ([]*PidIterEntry, error) {
+	rc, err := p.iter.Open()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open iter: %w", err)
+	}
+	defer rc.Close()
+
+	res := make([]*PidIterEntry, 0)
+
+	buf := make([]byte, 4096/iterEntrySize*iterEntrySize)
+	for {
+		n, err := io.ReadFull(rc, buf)
+		if err != nil {
+			if errors.Is(err, unix.EAGAIN) {
+				continue
+			}
+		}
+		if n == 0 {
+			break
+		}
+		if n%iterEntrySize != 0 {
+			return nil, fmt.Errorf("invalid format: %d", n)
+		}
+		for i := 0; i < n/iterEntrySize; i++ {
+			entry := (*piditerPidIterEntry)(unsafe.Pointer(&buf[i*iterEntrySize]))
+			res = append(res, &PidIterEntry{
+				ProgID: entry.Id,
+				Pid:    entry.Pid,
+				Comm:   C.GoString((*C.char)(unsafe.Pointer(&entry.Comm[0]))),
+			})
+		}
+	}
+
+	return res, nil
+}
+
+func (p *PidIter) Close() (err error) {
+	// If there's an error, return the last one
+	if tmpErr := p.iter.Close(); tmpErr != nil {
+		err = tmpErr
+	}
+	if tmpErr := p.objs.Close(); tmpErr != nil {
+		err = tmpErr
+	}
+	return
+}

commit 6ecdc7edeef1fff4f9fc566165b3875a047d23b8

pkg/bpfstats: Fix updating refcount
Fixes: d5f64719b55d ("new gadget: ebpftop")

Signed-off-by: vivekbisen04 <vivekbisen204@gmail.com>
Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/bpfstats/bpfstats.go
@@ -62,6 +62,7 @@ func EnableBPFStats() error {
 	defer mutex.Unlock()
 
 	if refCnt != 0 {
+		refCnt++
 		return nil
 	}
 