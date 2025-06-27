inspektor-gadget/inspektor-gadget

commit 5b9adb1c7cc2b4c574decb18774fd1fa0080ddf5

operators: Clean up and document lifecycle
1. Implement Close() method: Not having a Close() method was confusing
and led to situations when some resources could be leaked if there was a
failure in the operator.

2. Remove Init() and move that logic intro the corresponding Instantiate
method.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/ebpf/stats.go
@@ -566,6 +566,10 @@ func (i *ebpfOperatorDataInstance) Start(gadgetCtx operators.GadgetContext) erro
 }
 
 func (i *ebpfOperatorDataInstance) Stop(gadgetCtx operators.GadgetContext) error {
+	return nil
+}
+
+func (i *ebpfOperatorDataInstance) Close(gadgetCtx operators.GadgetContext) error {
 	defer close(i.done)
 
 	if err := bpfstats.DisableBPFStats(); err != nil {

pkg/gadget-context/context_test.go
@@ -21,10 +21,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/simple"
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 )
 
 // newSimpleOperator takes a name, a priority and a pointer to a string that the operator writes its name
@@ -84,56 +82,6 @@ func TestOperatorOrder(t *testing.T) {
 			assert.Equal(t, tc.expectedOrder, out)
 		})
 	}
-}
-
-type fakeOperator struct {
-	name     string
-	priority int
-}
-
-func (s *fakeOperator) Name() string {
-	return s.name
-}
-
-func (s *fakeOperator) Init(*params.Params) error {
-	return nil
-}
-
-func (s *fakeOperator) GlobalParams() api.Params {
-	return nil
-}
-
-func (s *fakeOperator) InstanceParams() api.Params {
-	return api.Params{
-		{
-			Key:          "foo",
-			DefaultValue: "567",
-		},
-	}
-}
-
-func (s *fakeOperator) InstantiateDataOperator(operators.GadgetContext, api.ParamValues) (operators.DataOperatorInstance, error) {
-	return s, nil
-}
-
-func (s *fakeOperator) Priority() int {
-	return s.priority
-}
-
-func (s *fakeOperator) PreStart(operators.GadgetContext) error {
-	return nil
-}
-
-func (s *fakeOperator) Start(operators.GadgetContext) error {
-	return nil
-}
-
-func (s *fakeOperator) Stop(operators.GadgetContext) error {
-	return nil
-}
-
-func (s *fakeOperator) PostStop(operators.GadgetContext) error {
-	return nil
 }
 
 func TestParamsDefault(t *testing.T) {

pkg/gadget-context/gadget-context.go
@@ -320,15 +320,17 @@ func (c *GadgetContext) LoadGadgetInfo(info *api.GadgetInfo, paramValues api.Par
 
 	c.Logger().Debug("loaded gadget info")
 
-	var err error
-	// After loading gadget info, get local operators params as well
-	c.localOperators, err = c.initAndPrepareOperators(paramValues)
+	// After loading gadget info, start local operators as well
+	err := c.instantiateOperators(paramValues)
 	if err != nil {
 		return fmt.Errorf("initializing local operators: %w", err)
 	}
 
 	if run {
-		if err := c.start(c.localOperators); err != nil {
+		if err := c.preStart(); err != nil {
+			return fmt.Errorf("pre-starting operators: %w", err)
+		}
+		if err := c.start(); err != nil {
 			return fmt.Errorf("starting local operators: %w", err)
 		}
 		c.Logger().Debugf("running...")
@@ -355,7 +357,8 @@ func (c *GadgetContext) StopLocalOperators() {
 	if c.localOperators == nil {
 		return
 	}
-	c.stop(c.localOperators)
+	c.stop()
+	c.postStop()
 	c.localOperators = nil
 }
 

pkg/gadget-context/helpers_test.go
@@ -1,0 +1,96 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gadgetcontext
+
+import (
+	"errors"
+	"slices"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
+)
+
+type fakeOperator struct {
+	name     string
+	priority int
+
+	// list of methods that will fail
+	fails []string
+
+	// list of methods called which didn't return an error
+	called *[]string
+}
+
+func (s *fakeOperator) Name() string {
+	return s.name
+}
+
+func (s *fakeOperator) Init(*params.Params) error {
+	return nil
+}
+
+func (s *fakeOperator) GlobalParams() api.Params {
+	return nil
+}
+
+func (s *fakeOperator) InstanceParams() api.Params {
+	return api.Params{
+		{
+			Key:          "foo",
+			DefaultValue: "567",
+		},
+	}
+}
+
+func (s *fakeOperator) InstantiateDataOperator(operators.GadgetContext, api.ParamValues) (operators.DataOperatorInstance, error) {
+	return s, nil
+}
+
+func (s *fakeOperator) Priority() int {
+	return s.priority
+}
+
+func (s *fakeOperator) runMethod(name string) error {
+	if slices.Contains(s.fails, name) {
+		return errors.New("needs to fail")
+	}
+
+	if s.called != nil {
+		*s.called = append(*s.called, s.name+"_"+name)
+	}
+
+	return nil
+}
+
+func (s *fakeOperator) PreStart(operators.GadgetContext) error {
+	return s.runMethod("prestart")
+}
+
+func (s *fakeOperator) Start(operators.GadgetContext) error {
+	return s.runMethod("start")
+}
+
+func (s *fakeOperator) Stop(operators.GadgetContext) error {
+	return s.runMethod("stop")
+}
+
+func (s *fakeOperator) PostStop(operators.GadgetContext) error {
+	return s.runMethod("poststop")
+}
+
+func (s *fakeOperator) Close(operators.GadgetContext) error {
+	return s.runMethod("close")
+}

pkg/gadget-context/run.go
@@ -16,6 +16,7 @@ package gadgetcontext
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"sort"
 
@@ -29,7 +30,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 )
 
-func (c *GadgetContext) initAndPrepareOperators(paramValues api.ParamValues) ([]operators.DataOperatorInstance, error) {
+func (c *GadgetContext) instantiateOperators(paramValues api.ParamValues) error {
 	log := c.Logger()
 
 	ops := c.DataOperators()
@@ -48,7 +49,7 @@ func (c *GadgetContext) initAndPrepareOperators(paramValues api.ParamValues) ([]
 
 	params := make([]*api.Param, 0)
 
-	dataOperatorInstances := make([]operators.DataOperatorInstance, 0, len(ops))
+	c.localOperators = make([]operators.DataOperatorInstance, 0, len(ops))
 	for _, op := range ops {
 		log.Debugf("initializing data op %q", op.Name())
 		opParamPrefix := fmt.Sprintf("operator.%s", op.Name())
@@ -60,36 +61,36 @@ func (c *GadgetContext) initAndPrepareOperators(paramValues api.ParamValues) ([]
 		// Ensure all params are present
 		err := apihelpers.NormalizeWithDefaults(instanceParams, opParamValues)
 		if err != nil {
-			return nil, fmt.Errorf("normalizing instance params for operator %q: %w", op.Name(), err)
+			return fmt.Errorf("normalizing instance params for operator %q: %w", op.Name(), err)
 		}
 
 		err = apihelpers.Validate(instanceParams, opParamValues)
 		if err != nil {
-			return nil, fmt.Errorf("validating instance params for operator %q: %w", op.Name(), err)
+			return fmt.Errorf("validating instance params for operator %q: %w", op.Name(), err)
 		}
 
 		opInst, err := op.InstantiateDataOperator(c, opParamValues)
 		if err != nil {
-			return nil, fmt.Errorf("instantiating operator %q: %w", op.Name(), err)
+			return fmt.Errorf("instantiating operator %q: %w", op.Name(), err)
 		}
 		if opInst == nil {
 			log.Debugf("> skipped %s", op.Name())
 			continue
 		}
-		dataOperatorInstances = append(dataOperatorInstances, opInst)
+		c.localOperators = append(c.localOperators, opInst)
 
 		// Add instance params only if operator was actually instantiated (i.e., activated)
 		params = append(params, instanceParams...)
 	}
 
-	for _, opInst := range dataOperatorInstances {
+	for _, opInst := range c.localOperators {
 		log.Debugf("preparing op %q", opInst.Name())
 		opParamPrefix := fmt.Sprintf("operator.%s", opInst.Name())
 
 		// Second pass params; this time the operator had the chance to prepare itself based on DataSources, etc.
 		// this mainly is postponed to read default values that might differ from before; this second pass is
 		// what is handed over to the remote end
-		if extra, ok := opInst.(operators.DataOperatorExtraParams); ok {
+		if extra, ok := opInst.(operators.ExtraParams); ok {
 			pd := extra.ExtraParams(c)
 			params = append(params, pd.AddPrefix(opParamPrefix)...)
 		}
@@ -110,76 +111,114 @@ func (c *GadgetContext) initAndPrepareOperators(paramValues api.ParamValues) ([]
 
 	c.SetParams(params)
 
-	return dataOperatorInstances, nil
+	return nil
 }
 
-func (c *GadgetContext) start(dataOperatorInstances []operators.DataOperatorInstance) error {
-	for _, opInst := range dataOperatorInstances {
-		preStart, ok := opInst.(operators.PreStart)
-		if !ok {
-			continue
-		}
-		c.Logger().Debugf("pre-starting op %q", opInst.Name())
-		err := preStart.PreStart(c)
-		if err != nil {
-			c.cancel()
-			return fmt.Errorf("pre-starting operator %q: %w", opInst.Name(), err)
-		}
-	}
-	for _, opInst := range dataOperatorInstances {
-		c.Logger().Debugf("starting op %q", opInst.Name())
-		err := opInst.Start(c)
-		if err != nil {
-			c.cancel()
-			return fmt.Errorf("starting operator %q: %w", opInst.Name(), err)
+func (c *GadgetContext) preStart() error {
+	for _, opInst := range c.localOperators {
+		if preStart, ok := opInst.(operators.PreStart); ok {
+			c.Logger().Debugf("pre-starting op %q", opInst.Name())
+			err := preStart.PreStart(c)
+			if err != nil {
+				return fmt.Errorf("pre-starting operator %q: %w", opInst.Name(), err)
+			}
 		}
 	}
 	return nil
 }
 
-func (c *GadgetContext) stop(dataOperatorInstances []operators.DataOperatorInstance) {
-	// Stop/DeInit in reverse order
-	for i := len(dataOperatorInstances) - 1; i >= 0; i-- {
-		opInst := dataOperatorInstances[i]
-		preStop, ok := opInst.(operators.PreStop)
-		if !ok {
-			continue
-		}
-		c.Logger().Debugf("pre-stopping op %q", opInst.Name())
-		err := preStop.PreStop(c)
+func (c *GadgetContext) start() error {
+	started := []operators.DataOperatorInstance{}
+
+	for _, opInst := range c.localOperators {
+		c.Logger().Debugf("starting op %q", opInst.Name())
+		err := opInst.Start(c)
 		if err != nil {
-			c.Logger().Errorf("pre-stopping operator %q: %v", opInst.Name(), err)
+			// Stop all operators that were started to be sure they're able to
+			// release resources there
+			c.stopOperators(started)
+			c.postStopOperators(started)
+			return fmt.Errorf("starting operator %q: %w", opInst.Name(), err)
+		}
+		started = append(started, opInst)
+	}
+	return nil
+}
+
+func (c *GadgetContext) preStop() error {
+	var errs []error
+
+	// PreStop in reverse order
+	for i := len(c.localOperators) - 1; i >= 0; i-- {
+		opInst := c.localOperators[i]
+		if preStop, ok := opInst.(operators.PreStop); ok {
+			c.Logger().Debugf("pre-stopping op %q", opInst.Name())
+			err := preStop.PreStop(c)
+			if err != nil {
+				errs = append(errs, fmt.Errorf("pre-stopping operator %q: %w", opInst.Name(), err))
+			}
 		}
 	}
-	for i := len(dataOperatorInstances) - 1; i >= 0; i-- {
-		opInst := dataOperatorInstances[i]
+	return errors.Join(errs...)
+}
+
+func (c *GadgetContext) stopOperators(ops []operators.DataOperatorInstance) error {
+	var errs []error
+
+	// Stop in reverse order
+	for i := len(ops) - 1; i >= 0; i-- {
+		opInst := ops[i]
 		c.Logger().Debugf("stopping op %q", opInst.Name())
-		err := opInst.Stop(c)
-		if err != nil {
-			c.Logger().Errorf("stopping operator %q: %v", opInst.Name(), err)
+		errs = append(errs, opInst.Stop(c))
+	}
+	return errors.Join(errs...)
+}
+
+func (c *GadgetContext) stop() error {
+	return c.stopOperators(c.localOperators)
+}
+
+func (c *GadgetContext) postStopOperators(ops []operators.DataOperatorInstance) error {
+	var errs []error
+
+	// PostStop in reverse order
+	for i := len(ops) - 1; i >= 0; i-- {
+		opInst := ops[i]
+		if postStop, ok := opInst.(operators.PostStop); ok {
+			c.Logger().Debugf("post-stopping op %q", opInst.Name())
+			errs = append(errs, postStop.PostStop(c))
 		}
 	}
-	for i := len(dataOperatorInstances) - 1; i >= 0; i-- {
-		opInst := dataOperatorInstances[i]
-		postStop, ok := opInst.(operators.PostStop)
-		if !ok {
-			continue
-		}
-		c.Logger().Debugf("post-stopping op %q", opInst.Name())
-		err := postStop.PostStop(c)
-		if err != nil {
-			c.Logger().Errorf("post-stopping operator %q: %v", opInst.Name(), err)
+	return errors.Join(errs...)
+}
+
+func (c *GadgetContext) postStop() error {
+	return c.postStopOperators(c.localOperators)
+}
+
+func (c *GadgetContext) close() error {
+	var errs []error
+
+	// Close in reverse order
+	for i := len(c.localOperators) - 1; i >= 0; i-- {
+		opInst := c.localOperators[i]
+		c.Logger().Debugf("closing op %q", opInst.Name())
+		if err := opInst.Close(c); err != nil {
+			errs = append(errs, fmt.Errorf("closing operator %q: %w", opInst.Name(), err))
 		}
 	}
+	return errors.Join(errs...)
 }
 
 func (c *GadgetContext) PrepareGadgetInfo(paramValues api.ParamValues) error {
-	_, err := c.initAndPrepareOperators(paramValues)
+	err := c.instantiateOperators(paramValues)
+	c.close()
 	return err
 }
 
 func (c *GadgetContext) Run(paramValues api.ParamValues) error {
 	defer c.cancel()
+	defer c.close()
 
 	metricAttribs := attribute.NewSet(
 		attribute.KeyValue{Key: "gadget_image", Value: attribute.StringValue(c.imageName)},
@@ -188,21 +227,35 @@ func (c *GadgetContext) Run(paramValues api.ParamValues) error {
 	udCtrRunningGadgets.Add(context.Background(), 1, metric.WithAttributeSet(metricAttribs))
 	defer udCtrRunningGadgets.Add(context.Background(), -1, metric.WithAttributeSet(metricAttribs))
 
-	dataOperatorInstances, err := c.initAndPrepareOperators(paramValues)
+	err := c.instantiateOperators(paramValues)
 	if err != nil {
 		return fmt.Errorf("initializing and preparing operators: %w", err)
 	}
 
-	if err := c.start(dataOperatorInstances); err != nil {
+	if err := c.preStart(); err != nil {
+		return fmt.Errorf("pre-starting operators: %w", err)
+	}
+
+	if err := c.start(); err != nil {
 		return fmt.Errorf("starting operators: %w", err)
 	}
 
 	c.Logger().Debugf("running...")
-
 	WaitForTimeoutOrDone(c)
-	c.stop(dataOperatorInstances)
 
-	return nil
+	var errs []error
+
+	if err := c.preStop(); err != nil {
+		errs = append(errs, fmt.Errorf("pre-stopping operators: %w", err))
+	}
+	if err := c.stop(); err != nil {
+		errs = append(errs, fmt.Errorf("stopping operators: %w", err))
+	}
+	if err := c.postStop(); err != nil {
+		errs = append(errs, fmt.Errorf("post-stopping operators: %w", err))
+	}
+
+	return errors.Join(errs...)
 }
 
 var udCtrRunningGadgets, _ = metrics.Int64UpDownCounter("ig_gadgets_running",

pkg/gadget-context/run_test.go
@@ -1,0 +1,314 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gadgetcontext
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+)
+
+func TestRun(t *testing.T) {
+	type testCase struct {
+		name          string
+		ops           []*fakeOperator
+		expectedCalls []string
+		expectedErr   bool
+	}
+
+	tests := []testCase{
+		// all operations should be called if there is not any error
+		{
+			name: "single_operator_all_work",
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+				},
+			},
+			expectedCalls: []string{
+				"op1_prestart",
+				"op1_start",
+				"op1_stop",
+				"op1_poststop",
+				"op1_close",
+			},
+		},
+		// close should always be called if another operation fails
+		{
+			name:        "single_operator_prestart_fails",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"prestart"},
+				},
+			},
+			expectedCalls: []string{
+				"op1_close",
+			},
+		},
+		{
+			name:        "single_operator_start_fails",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"start"},
+				},
+			},
+			expectedCalls: []string{
+				"op1_prestart",
+				"op1_close",
+			},
+		},
+		{
+			name:        "single_operator_stop_fails",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"stop"},
+				},
+			},
+			expectedCalls: []string{
+				"op1_prestart",
+				"op1_start",
+				"op1_poststop", // TODO: Ideally poststop shouldn't be called if stop failed
+				"op1_close",
+			},
+		},
+		{
+			name:        "single_operator_poststop_fails",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"poststop"},
+				},
+			},
+			expectedCalls: []string{
+				"op1_prestart",
+				"op1_start",
+				"op1_stop",
+				"op1_close",
+			},
+		},
+		{
+			name: "multiple_operators_all_work",
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+				},
+				{
+					name:     "op2",
+					priority: 2,
+				},
+			},
+			expectedCalls: []string{
+				// prestart and start in order
+				"op1_prestart",
+				"op2_prestart",
+
+				"op1_start",
+				"op2_start",
+
+				// stop, prestop and close in inverse order
+				"op2_stop",
+				"op1_stop",
+
+				"op2_poststop",
+				"op1_poststop",
+
+				"op2_close",
+				"op1_close",
+			},
+		},
+		{
+			name:        "multiple_operators_with_prestart_failure",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"prestart"},
+				},
+				{
+					name:     "op2",
+					priority: 2,
+				},
+			},
+			expectedCalls: []string{
+				"op2_close",
+				"op1_close",
+			},
+		},
+		{
+			name:        "multiple_operators_with_start_failure",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"start"},
+				},
+				{
+					name:     "op2",
+					priority: 2,
+				},
+			},
+			expectedCalls: []string{
+				"op1_prestart",
+				"op2_prestart",
+
+				// close is always called
+				"op2_close",
+				"op1_close",
+			},
+		},
+		{
+			name:        "multiple_operators_with_start_failure_2",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+				},
+				{
+					name:     "op2",
+					priority: 2,
+					fails:    []string{"start"},
+				},
+			},
+			expectedCalls: []string{
+				"op1_prestart",
+				"op2_prestart",
+
+				"op1_start",
+
+				// stop and poststop should be called on op1 as it was succesfully started
+				"op1_stop",
+				"op1_poststop",
+
+				"op2_close",
+				"op1_close",
+			},
+		},
+		// a failure on stop or prestop shouldn't prevent the other operator to be stopped
+		{
+			name:        "multiple_operators_with_stop_failure",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"stop"},
+				},
+				{
+					name:     "op2",
+					priority: 2,
+				},
+			},
+			expectedCalls: []string{
+				// prestart and start in order
+				"op1_prestart",
+				"op2_prestart",
+
+				"op1_start",
+				"op2_start",
+
+				// stop, pre stop and close in inverse order
+				"op2_stop",
+
+				"op2_poststop",
+				"op1_poststop",
+
+				"op2_close",
+				"op1_close",
+			},
+		},
+		{
+			name:        "multiple_operators_with_poststop_failure",
+			expectedErr: true,
+			ops: []*fakeOperator{
+				{
+					name:     "op1",
+					priority: 1,
+					fails:    []string{"poststop"},
+				},
+				{
+					name:     "op2",
+					priority: 2,
+				},
+			},
+			expectedCalls: []string{
+				// prestart and start in order
+				"op1_prestart",
+				"op2_prestart",
+
+				"op1_start",
+				"op2_start",
+
+				// stop, pre stop and close in inverse order
+				"op2_stop",
+				"op1_stop",
+
+				"op2_poststop",
+
+				"op2_close",
+				"op1_close",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			called := []string{}
+
+			ops := make([]operators.DataOperator, 0, len(test.ops))
+			for _, op := range test.ops {
+				op.called = &called
+				ops = append(ops, op)
+			}
+			opts := WithDataOperators(ops...)
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			gadgetCtx := New(ctx, "x.com/do-not-run-this", opts)
+			require.NotNil(t, gadgetCtx)
+
+			err := gadgetCtx.Run(nil)
+			if test.expectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			require.Equal(t, test.expectedCalls, called)
+		})
+	}
+}

pkg/operators/btfgen/btfgen.go
@@ -66,48 +66,38 @@ func (o *btfgenOperator) InstantiateImageOperator(
 		return nil, nil
 	}
 
-	return &btfgenOperatorInstance{
-		target: target,
-		desc:   desc,
-	}, nil
-}
-
-type btfgenOperatorInstance struct {
-	target oras.ReadOnlyTarget
-	desc   ocispec.Descriptor
-}
-
-func (i *btfgenOperatorInstance) Name() string {
-	return "btfgenInstance"
-}
-
-func (i *btfgenOperatorInstance) Prepare(gadgetCtx operators.GadgetContext) error {
 	info, err := btfgen.GetOSInfo()
 	if err != nil {
-		return fmt.Errorf("getting OS info: %w", err)
+		return nil, fmt.Errorf("getting OS info: %w", err)
 	}
 
-	r, err := oci.GetContentFromDescriptor(gadgetCtx.Context(), i.target, i.desc)
+	r, err := oci.GetContentFromDescriptor(gadgetCtx.Context(), target, desc)
 	if err != nil {
-		return fmt.Errorf("getting ebpf binary: %w", err)
+		return nil, fmt.Errorf("getting ebpf binary: %w", err)
 	}
 	defer r.Close()
 
 	btfFileName := fmt.Sprintf("%s/%s/%s/%s.btf", info.ID, info.VersionID, info.Arch, info.Kernel)
 	btfBytes, err := getBTFFile(r, btfFileName)
 	if err != nil {
-		return fmt.Errorf("getting BTF file: %w", err)
+		return nil, fmt.Errorf("getting BTF file: %w", err)
 	}
 
 	btfSpec, err := btf.LoadSpecFromReader(bytes.NewReader(btfBytes))
 	if err != nil {
-		return fmt.Errorf("loading BTF spec: %w", err)
+		return nil, fmt.Errorf("loading BTF spec: %w", err)
 	}
 
 	// save the kernel types to be used by the ebpf operator when loading bpf the spec.
 	gadgetCtx.SetVar(kernelTypesVar, btfSpec)
 
-	return nil
+	return &btfgenOperatorInstance{}, nil
+}
+
+type btfgenOperatorInstance struct{}
+
+func (i *btfgenOperatorInstance) Name() string {
+	return "btfgenInstance"
 }
 
 func (i *btfgenOperatorInstance) Start(gadgetCtx operators.GadgetContext) error {
@@ -115,6 +105,10 @@ func (i *btfgenOperatorInstance) Start(gadgetCtx operators.GadgetContext) error 
 }
 
 func (i *btfgenOperatorInstance) Stop(gadgetCtx operators.GadgetContext) error {
+	return nil
+}
+
+func (i *btfgenOperatorInstance) Close(gadgetCtx operators.GadgetContext) error {
 	return nil
 }
 
@@ -143,10 +137,6 @@ func getBTFFile(r io.Reader, filename string) ([]byte, error) {
 			return b, nil
 		}
 	}
-}
-
-func (i *btfgenOperatorInstance) ExtraParams(gadgetCtx operators.GadgetContext) api.Params {
-	return nil
 }
 
 func init() {

pkg/operators/cli/clioperator.go
@@ -495,4 +495,8 @@ func (o *cliOperatorInstance) Stop(gadgetCtx operators.GadgetContext) error {
 	return nil
 }
 
+func (o *cliOperatorInstance) Close(gadgetCtx operators.GadgetContext) error {
+	return nil
+}
+
 var CLIOperator = &cliOperator{}

commit 6d6e257078dba3e1ba25db923efe15b905ab99ca

operators/ebpf: Move disabling bpf stats to Stop()
They are enabled in Start(), so they should be disabled in Stop()

Fixes: 5b9adb1c7cc2 ("operators: Clean up and document lifecycle")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/ebpf/stats.go
@@ -566,15 +566,14 @@ func (i *ebpfOperatorDataInstance) Start(gadgetCtx operators.GadgetContext) erro
 }
 
 func (i *ebpfOperatorDataInstance) Stop(gadgetCtx operators.GadgetContext) error {
+	if err := bpfstats.DisableBPFStats(); err != nil {
+		return fmt.Errorf("disabling bpf stats: %w", err)
+	}
+
 	return nil
 }
 
 func (i *ebpfOperatorDataInstance) Close(gadgetCtx operators.GadgetContext) error {
 	defer close(i.done)
-
-	if err := bpfstats.DisableBPFStats(); err != nil {
-		return fmt.Errorf("disabling bpf stats: %w", err)
-	}
-
 	return nil
 }