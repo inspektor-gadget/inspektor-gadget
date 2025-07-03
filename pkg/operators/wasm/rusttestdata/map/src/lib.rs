inspektor-gadget/inspektor-gadget

commit 9c6e27a5f887c1389a0144bd5920a3026b8203e9

rust api map.rs
Signed-off-by: Daksh Kaushik <dakshkaushik20040126@gmail.com>

pkg/operators/wasm/rusttestdata/map/src/lib.rs
@@ -1,0 +1,188 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use api::{
+    errorf,
+    {
+        log::LogLevel,
+        map::{Map, MapSpec, MapType, MapUpdateFlags},
+    },
+};
+
+#[no_mangle]
+#[allow(non_snake_case)]
+fn gadgetInit() -> i32 {
+    let map_name = "test_map";
+    if let Ok(_) = Map::get(map_name) {
+        api::errorf!("{} map does not exist", map_name);
+        return 1;
+    }
+    0
+}
+
+// #[repr(C)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct MapTestStruct {
+    a: i32,
+    b: i32,
+    c: i8,
+    _pad: [i8; 3],
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+fn gadgetStart() -> i32 {
+    let map_name = "test_map";
+    let mut expected_val: i32 = 42;
+    let new_val: i32 = 43;
+    let key = MapTestStruct {
+        a: 42,
+        b: 42,
+        c: 43,
+        _pad: [0; 3],
+    };
+    let mut val: i32 = 0;
+    //Defining m inside a block to test Map drop trait.
+    {
+        let Ok(m) = Map::get(map_name) else {
+            errorf!("map doesn't exists: {}", map_name);
+            return 1;
+        };
+
+        if let Err(err) = m.put(&key, &expected_val) {
+            api::errorf!(
+                "setting {} value for {:?} key in {}: {}",
+                expected_val,
+                key,
+                map_name,
+                err
+            );
+            return 1;
+        }
+
+        if let Err(_) = m.lookup(&key, &mut val) {
+            api::errorf!("no value found for key {:?} in {}", key, map_name);
+            return 1;
+        }
+
+        if val != expected_val {
+            api::errorf!("expected value {}, got {}", expected_val, val);
+            return 1;
+        }
+
+        //     Code is not required as parameter's type are checked at compile time
+        //     match m.lookup(&key, val) {
+        //     Ok(_) => {api::error!("lookup only accepts pointer for value argument");
+        //             return 1;},
+        //     Err(_) => {
+        //     }
+        // }
+
+        if let Err(_) = m.update(&key, &new_val, MapUpdateFlags::UpdateExist) {
+            api::errorf!("updating value for key {:?} in {}", key, map_name);
+            return 1;
+        }
+
+        if let Err(_) = m.lookup(&key, &mut val) {
+            api::errorf!("no value found for key {:?} in {}", key, map_name);
+            return 1;
+        }
+        if val != new_val {
+            api::errorf!("expected value {}, got {}", new_val, val);
+            return 1;
+        }
+
+        if let Err(_) = m.delete(&key) {
+            api::errorf!("deleting value for key {:?} in {}", key, map_name);
+            return 1;
+        }
+
+        if let Err(_) = m.put(&key, &val) {
+            api::errorf!("setting {} value for key {:?} in {:?}", val, key, map_name);
+            return 1;
+        }
+
+        if let Ok(_) = m.update(&key, &new_val, MapUpdateFlags::UpdateNoExist) {
+            api::errorf!(
+                "cannot update value for key {:?} in {} as it is not already present",
+                key,
+                map_name
+            );
+            return 1;
+        }
+
+        if let Err(_) = m.update(&key, &new_val, MapUpdateFlags::UpdateExist) {
+            api::errorf!(
+                "cannot update value for key {:?} in {} as it is already present",
+                key,
+                map_name
+            );
+            return 1;
+        }
+        if let Err(_) = m.delete(&key) {
+            api::errorf!("deleting value for key {:?} in {}", key, map_name);
+            return 1;
+        }
+
+        if let Ok(_) = m.delete(&key) {
+            api::errorf!("there is value for key {:?} in {}", key, map_name);
+            return 1;
+        }
+    }
+    // As the block ends, the map tries to be dropped, but cannot close map got with get()
+
+    let map_spec = MapSpec {
+        name: "map_test".to_string(),
+        typ: MapType::Hash,
+        key_size: 4,
+        value_size: 4,
+        max_entries: 1,
+    };
+
+    let Ok(new_map) = Map::new(map_spec.clone()) else {
+        let name = map_spec.name.clone();
+        api::errorf!("creating map {:?}", name);
+        return 1;
+    };
+
+    let k: i32 = 42;
+    val = 43;
+    if let Err(_) = new_map.put(&k, &val) {
+        let name = map_spec.name.clone();
+        api::errorf!("setting {} value for key {} in {:?}", val, k, name);
+        return 1;
+    }
+
+    if let Err(_) = new_map.lookup(&k, &mut val) {
+        let name = map_spec.name.clone();
+        api::errorf!("no value found for key {} in {:?}", k, name);
+        return 1;
+    }
+
+    expected_val = 43;
+    if val != expected_val {
+        api::errorf!("expected value {}, got {}", expected_val, val);
+        return 1;
+    }
+
+    let k2 = 0xdead;
+    val = 0xcafe;
+    if let Ok(_) = new_map.put(&k2, &val) {
+        let name = map_spec.name.clone();
+        api::errorf!("map {:?} has one max entry, trying to put two", name);
+        return 1;
+    }
+
+    0
+}

pkg/operators/wasm/rusttestdata/mapofmap/src/lib.rs
@@ -1,0 +1,99 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use api::errorf;
+use api::{
+    log::LogLevel,
+    map::{Map, MapSpec, MapType},
+};
+
+#[repr(C)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct MapTestStruct {
+    a: i32,
+    b: i32,
+    c: i8,
+    _pad: [i8; 3],
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+fn gadgetStart() -> i32 {
+    let map_of_map_name = "map_of_map";
+    let mut inner_map: Map = Map(0);
+    let key = MapTestStruct {
+        a: 42,
+        b: 42,
+        c: 43,
+        _pad: [0; 3],
+    };
+
+    let Ok(map_of_map) = Map::get(map_of_map_name) else {
+        errorf!("{} map must exist", map_of_map_name);
+        return 1;
+    };
+    let hash_map_name = "test_hash".to_string();
+
+    let inner_map_spec = MapSpec {
+        name: hash_map_name.clone(),
+        typ: MapType::Hash,
+        key_size: 4,
+        value_size: 4,
+        max_entries: 1,
+    };
+
+    let Ok(hash_map) = Map::new(inner_map_spec.clone()) else {
+        errorf!("creating map {}", hash_map_name);
+        return 1;
+    };
+
+    if let Err(e) = map_of_map.put(&key, &hash_map) {
+        errorf!(
+            "setting {} inner map value for key {:?} in {}: {}",
+            hash_map_name,
+            key,
+            map_of_map_name,
+            e
+        );
+        return 1;
+    }
+    if let Err(_) = map_of_map.lookup(&key, &mut inner_map) {
+        errorf!("no value found for key {:?} in {}", key, map_of_map_name);
+        return 1;
+    }
+
+    if inner_map.0 == 0 {
+        errorf!("expected handle to be different than 0");
+        return 1;
+    }
+
+    if inner_map.0 == hash_map.0 {
+        errorf!("expected handle to be different than hashMap");
+        return 1;
+    }
+
+    let k: u32 = 42;
+    let v: u32 = 43;
+    if let Err(e) = inner_map.put(&k, &v) {
+        errorf!("putting value in inner map {}: {}", hash_map_name, e);
+        return 1;
+    }
+
+    if let Err(_) = map_of_map.delete(&key) {
+        errorf!("deleting map {:?}", hash_map);
+        return 1;
+    }
+
+    0
+}

wasmapi/rust/src/map.rs
@@ -1,0 +1,175 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::{
+    error,
+    helpers::{any_to_buf_ptr, string_to_buf_ptr},
+};
+
+#[link(wasm_import_module = "ig")]
+extern "C" {
+    #[link_name = "newMap"]
+    fn _map_new(name: u64, typ: u32, key_size: u32, value_size: u32, max_entries: u32) -> u32;
+    #[link_name = "getMap"]
+    fn _map_get(name: u64) -> u32;
+    #[link_name = "mapLookup"]
+    fn _map_lookup(map: u32, key_ptr: u64, value_ptr: u64) -> u32;
+    #[link_name = "mapUpdate"]
+    fn _map_update(map: u32, key_ptr: u64, value_ptr: u64, flags: u64) -> u32;
+    #[link_name = "mapDelete"]
+    fn _map_delete(map: u32, key_ptr: u64) -> u32;
+    #[link_name = "mapRelease"]
+    fn _map_release(map: u32) -> u32;
+}
+
+#[repr(u32)]
+#[derive(Debug, Clone, Copy)]
+pub enum MapType {
+    Unspecified = 0,
+    Hash,
+    Array,
+    ProgramArray,
+    PerfEventArray,
+    PerCPUHash,
+    PerCPUArray,
+    StackTrace,
+    CGroupArray,
+    LRUHash,
+    LRUCPUHash,
+    LPMTrie,
+    ArrayOfMaps,
+    HashOfMaps,
+    DevMap,
+    SockMap,
+    CPUMap,
+    XSKMap,
+    SockHash,
+    CGroupStorage,
+    ReusePortSockArray,
+    PerCPUCGroupStorage,
+    Queue,
+    Stack,
+    SkStorage,
+    DevMapHash,
+    StructOpsMap,
+    RingBuf,
+    InodeStorage,
+    TaskStorage,
+    BloomFilter,
+    UserRingbuf,
+    CgroupStorage,
+    Arena,
+}
+
+pub type Result<T> = std::result::Result<T, String>;
+
+#[repr(u64)]
+#[derive(Clone, Copy)]
+pub enum MapUpdateFlags {
+    UpdateAny = 0,
+    UpdateNoExist = 1,
+    UpdateExist = 2,
+    UpdateLock = 4,
+}
+
+#[derive(Debug)]
+pub struct Map(pub u32);
+
+#[derive(Clone)]
+pub struct MapSpec {
+    pub name: String,
+    pub typ: MapType,
+    pub key_size: u32,
+    pub value_size: u32,
+    pub max_entries: u32,
+}
+
+impl Drop for Map {
+    fn drop(&mut self) {
+        let ret = unsafe { _map_release(self.0) };
+        if ret != 0 {
+            error!("Failed to release map");
+        }
+    }
+}
+
+impl Map {
+    pub fn new(spec: MapSpec) -> Result<Self> {
+        let name_ptr = string_to_buf_ptr(&spec.name);
+        let ret = unsafe {
+            _map_new(
+                name_ptr.0,
+                spec.typ as u32,
+                spec.key_size,
+                spec.value_size,
+                spec.max_entries,
+            )
+        };
+        if ret == 0 {
+            Err(format!("Failed to create map {}", spec.name))
+        } else {
+            Ok(Map(ret))
+        }
+    }
+
+    pub fn get(name: &str) -> Result<Self> {
+        let ret = unsafe { _map_get(string_to_buf_ptr(name).0) };
+        if ret == 0 {
+            Err(format!("Map {} not found", name))
+        } else {
+            Ok(Map(ret))
+        }
+    }
+    // Only allows pointers to key and value
+    pub fn lookup<T, U>(&self, key: &T, value: &mut U) -> Result<()> {
+        let key_ptr = any_to_buf_ptr(key)?;
+        let value_ptr = any_to_buf_ptr(value)?;
+
+        let ret = unsafe { _map_lookup(self.0, key_ptr.0, value_ptr.0) };
+        if ret != 0 {
+            return Err(String::from("Error looking up map"));
+        }
+        let val_len = value_ptr.0 >> 32;
+        if let Some(bytes) = value_ptr.bytes() {
+            unsafe {
+                std::ptr::copy(bytes.as_ptr(), value as *mut U as *mut u8, val_len as usize);
+            }
+        };
+        Ok(())
+    }
+
+    pub fn put<T, U>(&self, key: &T, value: &U) -> Result<()> {
+        self.update(key, value, MapUpdateFlags::UpdateAny)
+    }
+
+    pub fn update<T, U>(&self, key: &T, value: &U, flags: MapUpdateFlags) -> Result<()> {
+        let key_ptr = any_to_buf_ptr(key)?;
+        let value_ptr = any_to_buf_ptr(value)?;
+
+        let ret = unsafe { _map_update(self.0, key_ptr.0, value_ptr.0, flags as u64) };
+        if ret != 0 {
+            return Err(String::from("Failed to update map"));
+        }
+        Ok(())
+    }
+
+    pub fn delete<T>(&self, key: &T) -> Result<()> {
+        let key_ptr = any_to_buf_ptr(key)?;
+        let ret = unsafe { _map_delete(self.0, key_ptr.0) };
+        if ret != 0 {
+            return Err(String::from("Failed to delete key"));
+        }
+        Ok(())
+    }
+}

wasmapi/rust/src/lib.rs
@@ -25,5 +25,6 @@ pub mod datasources;
 pub mod fields;
 pub mod helpers;
 pub mod log;
+pub mod map;
 pub mod params;
 pub mod version;

pkg/operators/wasm/rusttestdata/Makefile
@@ -7,6 +7,8 @@ TEST_ARTIFACTS = \
 	fields \
 	params \
 	config \
+	map \
+	mapofmap \
 
 all: $(TEST_ARTIFACTS)
 

pkg/operators/wasm/rusttestdata/map/Cargo.toml
@@ -1,0 +1,11 @@
+[package]
+name = "rust_wasm"
+version = "0.1.0"
+edition = "2021"
+
+[lib]
+crate-type=["cdylib"]
+
+[dependencies]
+once_cell="1.18"
+api={path="../../../../../wasmapi/rust"}

pkg/operators/wasm/rusttestdata/map/build.yaml
@@ -1,0 +1,1 @@
+wasm: src/lib.rs

pkg/operators/wasm/rusttestdata/map/program.bpf.c
@@ -1,0 +1,15 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+
+struct map_test_struct {
+	__u32 a;
+	__u32 b;
+	__u8 c;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, struct map_test_struct);
+	__type(value, __u32);
+} test_map SEC(".maps");

commit e923f41cc50560bd9894b43de853fb853d6feff7

wasmapi: rust: Use references for some function arguments.
Fixes: 9c6e27a5f887 ("rust api map.rs")
Fixes: 33971ff7f1e3 ("rust api perf.rs")
Fixes: 170bb31e3014 ("rust api syscall.rs")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/operators/wasm/rusttestdata/map/src/lib.rs
@@ -147,23 +147,20 @@ fn gadgetStart() -> i32 {
         max_entries: 1,
     };
 
-    let Ok(new_map) = Map::new(map_spec.clone()) else {
-        let name = map_spec.name.clone();
-        api::errorf!("creating map {:?}", name);
+    let Ok(new_map) = Map::new(&map_spec) else {
+        api::errorf!("creating map {:?}", (&map_spec.name));
         return 1;
     };
 
     let k: i32 = 42;
     val = 43;
     if let Err(_) = new_map.put(&k, &val) {
-        let name = map_spec.name.clone();
-        api::errorf!("setting {} value for key {} in {:?}", val, k, name);
+        api::errorf!("setting {} value for key {} in {:?}", val, k, (&map_spec.name));
         return 1;
     }
 
     if let Err(_) = new_map.lookup(&k, &mut val) {
-        let name = map_spec.name.clone();
-        api::errorf!("no value found for key {} in {:?}", k, name);
+        api::errorf!("no value found for key {} in {:?}", k, (&map_spec.name));
         return 1;
     }
 
@@ -176,8 +173,7 @@ fn gadgetStart() -> i32 {
     let k2 = 0xdead;
     val = 0xcafe;
     if let Ok(_) = new_map.put(&k2, &val) {
-        let name = map_spec.name.clone();
-        api::errorf!("map {:?} has one max entry, trying to put two", name);
+        api::errorf!("map {:?} has one max entry, trying to put two", (&map_spec.name));
         return 1;
     }
 

pkg/operators/wasm/rusttestdata/mapofmap/src/lib.rs
@@ -52,7 +52,7 @@ fn gadgetStart() -> i32 {
         max_entries: 1,
     };
 
-    let Ok(hash_map) = Map::new(inner_map_spec.clone()) else {
+    let Ok(hash_map) = Map::new(&inner_map_spec) else {
         errorf!("creating map {}", hash_map_name);
         return 1;
     };

pkg/operators/wasm/rusttestdata/perf/src/lib.rs
@@ -36,7 +36,7 @@ fn gadgetStart() -> i32 {
         return 1;
     };
 
-    let Ok(perf_reader) = PerfReader::new(perf_array, 4096, true) else {
+    let Ok(perf_reader) = PerfReader::new(&perf_array, 4096, true) else {
         errorf!("creating perf reader");
         return 1;
     };

pkg/operators/wasm/rusttestdata/syscall/src/lib.rs
@@ -63,7 +63,7 @@ fn gadgetInit() -> i32 {
 
     // open_tree has the same ID for both amd64 and arm64.
     syscall_name = "open_tree".to_string();
-    let Ok(id) = get_syscall_id(syscall_name.clone()) else {
+    let Ok(id) = get_syscall_id(&syscall_name) else {
         errorf!("failed to get ID for syscall {}", syscall_name);
         return 1;
     };
@@ -80,7 +80,7 @@ fn gadgetInit() -> i32 {
 
     // Test invalid syscall name
     let syscall_name = "foobar".to_string();
-    if let Ok(val) = get_syscall_id(syscall_name.clone()) {
+    if let Ok(val) = get_syscall_id(&syscall_name) {
         errorf!(
             "expected no syscall ID for syscall {}, got {}",
             syscall_name,

wasmapi/rust/src/map.rs
@@ -105,7 +105,7 @@ impl Drop for Map {
 }
 
 impl Map {
-    pub fn new(spec: MapSpec) -> Result<Self> {
+    pub fn new(spec: &MapSpec) -> Result<Self> {
         let name_ptr = string_to_buf_ptr(&spec.name);
         let ret = unsafe {
             _map_new(

wasmapi/rust/src/perf.rs
@@ -47,7 +47,7 @@ impl Drop for PerfReader {
 }
 
 impl PerfReader {
-    pub fn new(map: Map, size: u32, is_overwritable: bool) -> Result<Self> {
+    pub fn new(map: &Map, size: u32, is_overwritable: bool) -> Result<Self> {
         let flag = if is_overwritable { 1 } else { 0 };
         let handle = unsafe { _perfreader_new(map.0, size, flag) };
         if handle == 0 {

wasmapi/rust/src/syscall.rs
@@ -68,7 +68,7 @@ pub fn get_syscall_name(id: u16) -> Result<String> {
     }
 }
 
-pub fn get_syscall_id(name: String) -> Result<i32> {
+pub fn get_syscall_id(name: &str) -> Result<i32> {
     let id = unsafe { _get_syscall_id(string_to_buf_ptr(&name).0) };
 
     if id == -1 {