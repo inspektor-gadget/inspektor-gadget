inspektor-gadget/inspektor-gadget

commit 490785f4138cbf4aa9bfcdd18f90d369116a39ca

wasm: Remove malloc requirement for guest
Malloc() was required to be able to allocate and write data into the
guest memory. This requirement was a bit strict as:
- malloc() is exposed when using tinygo by a obscure undocumented way
- This isn't exposed when using go 1.24
- It can be challenging to expose on another programming languages

This commit removes this requirement. The main change is that the guest
now has to pass a preallocated buffer where the host will write the data.
It can be suboptimal for some cases when the guest doesn't know the
size, but it can help to reduce the allocations if the guest passes a
static buffer in multiple calls. (As it was done in the trace_exec and
trace_dns gadgets).

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

wasmapi/go/syscall.go
@@ -18,17 +18,21 @@ import (
 	"fmt"
 	"runtime"
 	"unsafe"
+	_ "unsafe"
 )
 
 //go:wasmimport env getSyscallName
-func getSyscallName(id uint32) uint64
+//go:linkname getSyscallName getSyscallName
+func getSyscallName(id uint32, dst uint64) uint32
 
 //go:wasmimport env getSyscallDeclaration
+//go:linkname getSyscallDeclaration getSyscallDeclaration
 func getSyscallDeclaration(name uint64, pointer uint64) uint32
 
 // Keep in sync with pkg/operators/wasm/syscalls.go.
 const (
-	isPointerFlag = 1 << iota
+	isPointerFlag    = 1 << iota
+	maxSyscallLength = 64
 )
 
 // Keep in sync with pkg/operators/wasm/syscalls.go.
@@ -56,15 +60,13 @@ type SyscallDeclaration struct {
 }
 
 func GetSyscallName(id uint16) (string, error) {
-	ptr := bufPtr(getSyscallName(uint32(id)))
-	if ptr == 0 {
+	dst := make([]byte, maxSyscallLength)
+
+	ret := getSyscallName(uint32(id), uint64(bytesToBufPtr(dst)))
+	if ret == 1 {
 		return "", fmt.Errorf("getting syscall name for syscall id %d", id)
 	}
-
-	str := ptr.string()
-	ptr.free()
-
-	return str, nil
+	return fromCString(dst), nil
 }
 
 func GetSyscallDeclaration(name string) (SyscallDeclaration, error) {

gadgets/deadlock/go/program.go
@@ -189,7 +189,7 @@ func gadgetInit() int32 {
 
 			// Record process information for the PID
 			if _, exists := pidInfo[pid]; !exists {
-				comm, err := commF.String(datum)
+				comm, err := commF.String(datum, 16)
 				if err != nil {
 					api.Warnf("failed to get comm: %s", err)
 					continue

gadgets/trace_dns/go/program.go
@@ -237,9 +237,14 @@ func gadgetInit() int32 {
 			return
 		}
 
-		n := dataF.BytesToSlice(data, payload)
+		n, err := dataF.Bytes(data, payload)
+		if err != nil {
+			api.Warnf("failed to get data: %s", err)
+			return
+		}
+
 		if n == 0 {
-			api.Warnf("failed to get data")
+			api.Warnf("empty data")
 			return
 		}
 

gadgets/trace_exec/go/program.go
@@ -55,9 +55,14 @@ func gadgetInit() int32 {
 
 	ds.Subscribe(func(source api.DataSource, data api.Data) {
 		// Get all fields sent by ebpf
-		n := argsF.BytesToSlice(data, payload)
+		n, err := argsF.Bytes(data, payload)
+		if err != nil {
+			api.Warnf("failed to get args: %s", err)
+			return
+		}
+
 		if n == 0 {
-			api.Warnf("failed to get args")
+			api.Warnf("empty args")
 			return
 		}
 

gadgets/trace_fsslower/go/program.go
@@ -75,7 +75,7 @@ var fsConfMap = map[string]fsConf{
 
 //go:wasmexport gadgetPreStart
 func gadgetPreStart() int32 {
-	value, err := api.GetParamValue("filesystem")
+	value, err := api.GetParamValue("filesystem", 32)
 	if err != nil {
 		api.Errorf("failed to get param value: %s", err)
 		return 1

gadgets/trace_mount/go/program.go
@@ -93,12 +93,12 @@ func gadgetInit() int32 {
 	}
 
 	ds.Subscribe(func(source api.DataSource, data api.Data) {
-		flags, _ := flagsField.String(data)
+		flags, _ := flagsField.String(data, 512)
 		opRaw, _ := opRawField.Int32(data)
-		src, _ := srcField.String(data)
-		dest, _ := destField.String(data)
-		fs, _ := fsField.String(data)
-		dataStr, _ := dataField.String(data)
+		src, _ := srcField.String(data, 4096)
+		dest, _ := destField.String(data, 4096)
+		fs, _ := fsField.String(data, 4096)
+		dataStr, _ := dataField.String(data, 512)
 		errorRaw, _ := errorRawField.Uint32(data)
 
 		callField.SetString(data, getCallStr(opRaw, src, dest, fs, flags, dataStr, errorRaw))

pkg/operators/wasm/fields.go
@@ -25,7 +25,7 @@ import (
 )
 
 func (i *wasmOperatorInstance) addFieldFuncs(env wazero.HostModuleBuilder) {
-	exportFunction(env, "fieldGet", i.fieldGet,
+	exportFunction(env, "fieldGetScalar", i.fieldGetScalar,
 		[]wapi.ValueType{
 			wapi.ValueTypeI32, // Accessor
 			wapi.ValueTypeI32, // Data
@@ -34,14 +34,14 @@ func (i *wasmOperatorInstance) addFieldFuncs(env wazero.HostModuleBuilder) {
 		[]wapi.ValueType{wapi.ValueTypeI64}, // Value
 	)
 
-	exportFunction(env, "fieldGetToBuffer", i.fieldGetToBuffer,
+	exportFunction(env, "fieldGetBuffer", i.fieldGetBuffer,
 		[]wapi.ValueType{
 			wapi.ValueTypeI32, // Accessor
 			wapi.ValueTypeI32, // Data
 			wapi.ValueTypeI32, // Kind
 			wapi.ValueTypeI64, // Dest buffer
 		},
-		[]wapi.ValueType{wapi.ValueTypeI32}, // N bytes
+		[]wapi.ValueType{wapi.ValueTypeI32}, // N bytes or Error
 	)
 
 	exportFunction(env, "fieldSet", i.fieldSet,
@@ -78,17 +78,16 @@ func (i *wasmOperatorInstance) getDataFromDatasourceHandle(dataHandle uint32) (d
 	return data, data != nil
 }
 
-// fieldGet returns the field's value.
+// fieldGetScalar returns the field's value for scalar fields.
 // Params:
 // - stack[0]: Field handle
 // - stack[1]: Data handle
 // - stack[2]: Kind
 // Return value:
-// - Uint64 representation of the value of the field, depending on the type
-// requested, or a pointer, 0 on error.
+// - Field's value, 0 on error
 // TODO: error handling is still TBD as there not a way to differentiate between
 // a field with value 0 and an error.
-func (i *wasmOperatorInstance) fieldGet(ctx context.Context, m wapi.Module, stack []uint64) {
+func (i *wasmOperatorInstance) fieldGetScalar(ctx context.Context, m wapi.Module, stack []uint64) {
 	fieldHandle := wapi.DecodeU32(stack[0])
 	dataHandle := wapi.DecodeU32(stack[1])
 	fieldKind := api.Kind(wapi.DecodeU32(stack[2]))
@@ -102,16 +101,6 @@ func (i *wasmOperatorInstance) fieldGet(ctx context.Context, m wapi.Module, stac
 	if !ok {
 		stack[0] = 0
 		return
-	}
-
-	handleBytes := func(buf []byte) uint64 {
-		val, err := i.writeToGuestMemory(ctx, buf)
-		if err != nil {
-			i.logger.Warnf("fieldGet: writing bytes to guest memory: %v", err)
-			return 0
-		}
-
-		return val
 	}
 
 	var val uint64
@@ -163,18 +152,10 @@ func (i *wasmOperatorInstance) fieldGet(ctx context.Context, m wapi.Module, stac
 		val = uint64(ret)
 	case api.Kind_Float64:
 		val, err = field.Uint64(data)
-	// These are a bit special as they don't fit in the return value, so we have to
-	// allocate an array in the guest memory and return a pointer to it.
-	case api.Kind_String:
-		str, err := field.String(data)
-		if err == nil {
-			val = handleBytes([]byte(str))
-		}
-	case api.Kind_Bytes:
-		bytes, err := field.Bytes(data)
-		if err == nil {
-			val = handleBytes(bytes)
-		}
+	case api.Kind_String, api.Kind_Bytes:
+		i.logger.Warnf("fieldGetScalar: field kind %q not supported, use fieldGetBuffer instead()", fieldKind)
+		stack[0] = 0
+		return
 	default:
 		i.logger.Warnf("unknown field kind: %d", stack[2])
 		stack[0] = 0
@@ -182,7 +163,7 @@ func (i *wasmOperatorInstance) fieldGet(ctx context.Context, m wapi.Module, stac
 	}
 
 	if err != nil {
-		i.logger.Warnf("fieldGet for field %q failed: %v", field.Name(), err)
+		i.logger.Warnf("fieldGetScalar for field %q failed: %v", field.Name(), err)
 		stack[0] = 0
 		return
 	}
@@ -190,71 +171,45 @@ func (i *wasmOperatorInstance) fieldGet(ctx context.Context, m wapi.Module, stac
 	stack[0] = val
 }
 
-// fieldGetToBuffer returns the field's value.
+// fieldGetBuffer returns the field's value for string and bytes fields.
 // Params:
 // - stack[0]: Field handle
 // - stack[1]: Data handle
 // - stack[2]: Kind
 // - stack[3]: Destination buffer
 // Return value:
-// - Uint32 the number of bytes copied
-func (i *wasmOperatorInstance) fieldGetToBuffer(ctx context.Context, m wapi.Module, stack []uint64) {
+// - The number of bytes copied or -1 in case of error
+func (i *wasmOperatorInstance) fieldGetBuffer(ctx context.Context, m wapi.Module, stack []uint64) {
 	fieldHandle := wapi.DecodeU32(stack[0])
 	dataHandle := wapi.DecodeU32(stack[1])
 	fieldKind := api.Kind(wapi.DecodeU32(stack[2]))
-	fieldDst := stack[3]
+	dst := stack[3]
 
 	field, ok := getHandle[datasource.FieldAccessor](i, fieldHandle)
 	if !ok {
-		stack[0] = 0
+		stack[0] = wapi.EncodeI32(-1)
 		return
 	}
 	data, ok := i.getDataFromDatasourceHandle(dataHandle)
 	if !ok {
-		stack[0] = 0
+		stack[0] = wapi.EncodeI32(-1)
 		return
 	}
-
-	handleBytes := func(buf []byte) uint64 {
-		if getLength(fieldDst) < uint32(len(buf)) {
-			i.logger.Warnf("fieldGet: writing %d bytes to guest memory buffer of %d bytes", len(buf), getLength(fieldDst))
-			return 0
-		}
-		if !i.mod.Memory().Write(getAddress(fieldDst), buf) {
-			i.logger.Warnf("fieldGet: writing bytes to guest memory: out of memory write")
-			return 0
-		}
-
-		return uint64(len(buf))
-	}
-
-	var val uint64
-	var err error
 
 	switch fieldKind {
-	case api.Kind_String:
-		str, err := field.String(data)
-		if err == nil {
-			val = handleBytes([]byte(str))
+	case api.Kind_String, api.Kind_Bytes:
+		bytes := field.Get(data)
+		if err := i.writeToDstBuffer(bytes, dst); err != nil {
+			i.logger.Warnf("fieldGetBuffer: %v", err)
+			stack[0] = wapi.EncodeI32(-1)
+			return
 		}
-	case api.Kind_Bytes:
-		bytes, err := field.Bytes(data)
-		if err == nil {
-			val = handleBytes(bytes)
-		}
+
+		stack[0] = uint64(len(bytes))
 	default:
 		i.logger.Warnf("unknown field kind: %d", stack[2])
-		stack[0] = 0
-		return
+		stack[0] = wapi.EncodeI32(-1)
 	}
-
-	if err != nil {
-		i.logger.Warnf("fieldGetToBuffer for field %q failed: %v", field.Name(), err)
-		stack[0] = 0
-		return
-	}
-
-	stack[0] = val
 }
 
 // fieldSet sets the field's value

pkg/operators/wasm/helpers.go
@@ -81,17 +81,14 @@ func exportFunction(
 		Export(name)
 }
 
-func (i *wasmOperatorInstance) writeToGuestMemory(ctx context.Context, buf []byte) (uint64, error) {
-	res, err := i.guestMalloc.Call(ctx, uint64(len(buf)))
-	if err != nil {
-		return 0, fmt.Errorf("malloc failed: %w", err)
+func (i *wasmOperatorInstance) writeToDstBuffer(src []byte, dstBuf uint64) error {
+	if getLength(dstBuf) < uint32(len(src)) {
+		return fmt.Errorf("writing %d bytes to guest memory buffer of %d bytes: not enough memory", len(src), getLength(dstBuf))
 	}
-
-	if !i.mod.Memory().Write(uint32(res[0]), buf) {
-		return 0, fmt.Errorf("out of memory write")
+	if !i.mod.Memory().Write(getAddress(dstBuf), src) {
+		return fmt.Errorf("writing bytes to guest memory: out of memory write")
 	}
-
-	return uint64(len(buf))<<32 | uint64(res[0]), nil
+	return nil
 }
 
 func isDataArrayHandle(handle uint32) bool {

commit 074dfda7e00dafe26f6efa8b2175baaf400dc5a1

wasmapi: go: syscall: Remove unused unsafe blank import.
We already properly import it.

Fixes: 490785f4138c ("wasm: Remove malloc requirement for guest")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

wasmapi/go/syscall.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"runtime"
 	"unsafe"
-	_ "unsafe"
 )
 
 //go:wasmimport ig getSyscallName