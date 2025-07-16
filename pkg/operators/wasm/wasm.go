inspektor-gadget/inspektor-gadget

commit ef09eaea2e445e393dbbf413d47e2e7918312e08

wasm: Use cache to speed-up things
After switching to golang in guest (previously tinygo) the compile
time (InstantiateWithConfig) for wasm modules has noticeably increased.
Wazero allows using a cache to avoid compiling these modules everytime.
The cache is key is based of SHA of wasm program and cache directory uses
wazero version to ensure no surprises. The change uses the cache accross different
gadgets runs to speed-up starting a gadget.

Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

pkg/operators/wasm/wasm.go
@@ -48,9 +48,25 @@ const (
 
 	// Indicates the handle encodes a member of a data array as index << 16 | arrayHandle
 	dataArrayHandleFlag = uint32(1 << 31)
+
+	// cache path for the wasm compilation
+	cacheDir = "/var/run/ig/wasm-cache"
 )
 
-type wasmOperator struct{}
+type wasmOperator struct {
+	cache wazero.CompilationCache
+}
+
+func newWasmOperator() *wasmOperator {
+	cache, err := wazero.NewCompilationCacheWithDir(cacheDir)
+	if err != nil {
+		logger.DefaultLogger().Debugf("failed to create wasm compilation cache: %v", err)
+		return &wasmOperator{}
+	}
+	return &wasmOperator{
+		cache: cache,
+	}
+}
 
 func (w *wasmOperator) Name() string {
 	return "wasm"
@@ -76,7 +92,7 @@ func (w *wasmOperator) InstantiateImageOperator(
 		createdMap:  map[uint32]struct{}{},
 	}
 
-	if err := instance.init(gadgetCtx, target, desc); err != nil {
+	if err := instance.init(gadgetCtx, target, desc, w.cache); err != nil {
 		instance.close(gadgetCtx)
 		return nil, fmt.Errorf("initializing wasm: %w", err)
 	}
@@ -212,11 +228,13 @@ func (i *wasmOperatorInstance) init(
 	gadgetCtx operators.GadgetContext,
 	target oras.ReadOnlyTarget,
 	desc ocispec.Descriptor,
+	cache wazero.CompilationCache,
 ) error {
 	ctx := gadgetCtx.Context()
 	rtConfig := wazero.NewRuntimeConfig().
 		WithCloseOnContextDone(true).
-		WithMemoryLimitPages(256) // 16MB (64KB per page)
+		WithMemoryLimitPages(256). // 16MB (64KB per page)
+		WithCompilationCache(cache)
 	i.rt = wazero.NewRuntimeWithConfig(ctx, rtConfig)
 
 	igModuleBuilder := i.rt.NewHostModuleBuilder("ig")
@@ -354,5 +372,5 @@ func (i *wasmOperatorInstance) close(gadgetCtx operators.GadgetContext) error {
 }
 
 func init() {
-	operators.RegisterOperatorForMediaType(wasmObjectMediaType, &wasmOperator{})
+	operators.RegisterOperatorForMediaType(wasmObjectMediaType, newWasmOperator())
 }

commit f0a9406ee617a8412b774330b989d922601b7ba3

charts: Add mount for wasm-cache
Fixes: ef09eaea2e445e393dbbf413d47e2e7918312e08

Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

charts/gadget/templates/daemonset.yaml
@@ -221,6 +221,11 @@ spec:
             - mountPath: /etc/ig
               name: config
               readOnly: true
+            # We need a dedicated volume to store the wasm cache
+            # otherwise it will fail as the container root filesystem is read only.
+            - mountPath: /var/run/ig/wasm-cache
+              name: wasm-cache
+              readOnly: false
       nodeSelector:
         {{- .Values.nodeSelector | toYaml | nindent 8 }}
       affinity:
@@ -290,3 +295,5 @@ spec:
           configMap:
             name: {{ include "gadget.fullname" . }}
             defaultMode: 0o400
+        - name: wasm-cache
+          emptyDir: {}

pkg/operators/wasm/wasm.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/cilium/ebpf"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/tetratelabs/wazero"
 	wapi "github.com/tetratelabs/wazero/api"
@@ -62,7 +63,7 @@ type wasmOperator struct {
 func newWasmOperator() *wasmOperator {
 	cache, err := wazero.NewCompilationCacheWithDir(cacheDir)
 	if err != nil {
-		logger.DefaultLogger().Debugf("failed to create wasm compilation cache: %v", err)
+		log.Warnf("failed to setup wasm compilation cache, skipping cache: %v", err)
 		return &wasmOperator{}
 	}
 	return &wasmOperator{

pkg/resources/manifests/deploy.yaml
@@ -322,6 +322,11 @@ spec:
             - mountPath: /etc/ig
               name: config
               readOnly: true
+            # We need a dedicated volume to store the wasm cache
+            # otherwise it will fail as the container root filesystem is read only.
+            - mountPath: /var/run/ig/wasm-cache
+              name: wasm-cache
+              readOnly: false
       nodeSelector:
         kubernetes.io/os: linux
       affinity:
@@ -379,3 +384,5 @@ spec:
           configMap:
             name: gadget
             defaultMode: 0o400
+        - name: wasm-cache
+          emptyDir: {}