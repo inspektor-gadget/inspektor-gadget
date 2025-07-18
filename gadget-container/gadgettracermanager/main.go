inspektor-gadget/inspektor-gadget

commit 11f9c48b7230fb00cfc6964a3fdff0e634a4b6f7

gadgettracermanager: Use log-level from config
Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

gadget-container/gadgettracermanager/main.go
@@ -73,7 +73,6 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgettracermanager"
 	pb "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgettracermanager/api"
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/gadgettracermanagerloglevel"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
 )
 
@@ -121,8 +120,18 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	tracerManLogLvl := gadgettracermanagerloglevel.LogLevel()
-	log.SetLevel(tracerManLogLvl)
+
+	if err := gadgettracermanagerconfig.Init(); err != nil {
+		log.Fatalf("Initializing config: %v", err)
+	}
+
+	logLevel, err := log.ParseLevel(config.Config.GetString(gadgettracermanagerconfig.DaemonLogLevel))
+	if err != nil {
+		log.Fatalf("Parsing log level %q: %v", logLevel, err)
+	}
+	log.SetLevel(logLevel)
+	log.Infof("Config: %s=%s", gadgettracermanagerconfig.DaemonLogLevel, logLevel)
+
 	labels := []*pb.Label{}
 	if label != "" {
 		pairs := strings.Split(label, ",")
@@ -263,22 +272,12 @@ func main() {
 			log.Info("Experimental features enabled")
 		}
 
-		config.Config = config.NewWithPath(gadgettracermanagerconfig.ConfigPath)
-		if err := config.Config.ReadInConfig(); err != nil {
-			log.Warnf("reading config: %v", err)
-		}
-
-		// Set default values for config options
-		config.Config.SetDefault(gadgettracermanagerconfig.HookModeKey, "auto")
-		config.Config.SetDefault(gadgettracermanagerconfig.FallbackPodInformerKey, "true")
-		config.Config.SetDefault(gadgettracermanagerconfig.EventsBufferLengthKey, 16384)
-
 		operators.RegisterDataOperator(ocihandler.OciHandler)
 
 		hostConfig := host.Config{
 			AutoMountFilesystems: true,
 		}
-		err := host.Init(hostConfig)
+		err = host.Init(hostConfig)
 		if err != nil {
 			log.Fatalf("host.Init() failed: %v", err)
 		}
@@ -310,7 +309,7 @@ func main() {
 		var tracerManager *gadgettracermanager.GadgetTracerManager
 		hookMode := config.Config.GetString(gadgettracermanagerconfig.HookModeKey)
 		log.Infof("Config: %s=%s", gadgettracermanagerconfig.HookModeKey, hookMode)
-		hookMode, err = entrypoint.Init(hookMode)
+		hookMode, err = entrypoint.Init(hookMode, logLevel)
 		if err != nil {
 			log.Fatalf("entrypoint.Init() failed: %v", err)
 		}

charts/gadget/templates/configmap.yaml
@@ -18,5 +18,6 @@ data:
       docker-socketpath: {{ .Values.config.dockerSocketPath }}
       podman-socketpath: {{ .Values.config.podmanSocketPath }}
       gadget-namespace: {{ .Values.config.gadgetNamespace }}
+      daemon-log-level: {{ .Values.config.daemonLogLevel }}
       operator:
         {{- include "gadget.operatorConfig" . | nindent 8 -}}

charts/gadget/templates/daemonset.yaml
@@ -88,8 +88,6 @@ spec:
               value: "/host"
             - name: IG_EXPERIMENTAL
               value: {{ .Values.config.experimental | quote }}
-            - name: GADGET_TRACER_MANAGER_LOG_LEVEL
-              value: {{ .Values.config.daemonLogLevel | quote }}
             {{- if .Values.additionalEnv }}
             {{- toYaml .Values.additionalEnv | nindent 12 }}
             {{- end }}

cmd/kubectl-gadget/deploy.go
@@ -24,7 +24,6 @@ import (
 	"os"
 	"reflect"
 	"regexp"
-	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -228,6 +227,7 @@ func init() {
 	deployCmd.PersistentFlags().StringVarP(
 		&daemonLogLevel,
 		"daemon-log-level", "", "info", fmt.Sprintf("Set the ig-k8s log level, valid values are: %v", strings.Join(strLevels, ", ")))
+	deployCmd.PersistentFlags().MarkDeprecated("daemon-log-level", "This flag is deprecated and will be removed in v0.43.0+ release. Use --daemon-config instead")
 	deployCmd.PersistentFlags().StringVarP(
 		&appArmorprofile,
 		"apparmor-profile", "", "unconfined", "AppArmor profile to use")
@@ -843,11 +843,6 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 				case experimental.EnvName:
 					value := experimental.Enabled() || experimentalVar
 					gadgetContainer.Env[i].Value = strconv.FormatBool(value)
-				case "GADGET_TRACER_MANAGER_LOG_LEVEL":
-					if !slices.Contains(strLevels, daemonLogLevel) {
-						return fmt.Errorf("invalid log level %q, valid levels are: %v", daemonLogLevel, strings.Join(strLevels, ", "))
-					}
-					gadgetContainer.Env[i].Value = daemonLogLevel
 				}
 			}
 

gadget-container/cleanup/cleanup.go
@@ -19,11 +19,11 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/gadgettracermanagerloglevel"
-
 	nriv1 "github.com/containerd/nri/types/v1"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/config"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/config/gadgettracermanagerconfig"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
 )
 
@@ -87,8 +87,16 @@ func removeNRIHooks() {
 }
 
 func main() {
-	tracerManLogLvl := gadgettracermanagerloglevel.LogLevel()
-	log.SetLevel(tracerManLogLvl)
+	if err := gadgettracermanagerconfig.Init(); err != nil {
+		log.Fatalf("Initializing config: %v", err)
+	}
+	logLevel, err := log.ParseLevel(config.Config.GetString(gadgettracermanagerconfig.DaemonLogLevel))
+	if err != nil {
+		log.Fatalf("Parsing log level %q: %v", logLevel, err)
+	}
+
+	log.SetLevel(logLevel)
+	log.Infof("Cleanup started")
 	removeCRIOHooks()
 	removeNRIHooks()
 

gadget-container/entrypoint/entrypoint.go
@@ -30,7 +30,6 @@ import (
 	"golang.org/x/sys/unix"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/oci"
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/gadgettracermanagerloglevel"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
 )
 
@@ -217,9 +216,8 @@ func prepareGadgetPullSecret() error {
 	return nil
 }
 
-func Init(hookMode string) (string, error) {
-	tracerManLogLvl := gadgettracermanagerloglevel.LogLevel()
-	log.SetLevel(tracerManLogLvl)
+func Init(hookMode string, logLevel log.Level) (string, error) {
+	log.SetLevel(logLevel)
 	if _, err := os.Stat(filepath.Join(host.HostRoot, "/bin")); os.IsNotExist(err) {
 		return "", fmt.Errorf("%s must be executed in a pod with access to the host via %s", os.Args[0], host.HostRoot)
 	}

pkg/config/gadgettracermanagerconfig/config.go
@@ -14,6 +14,12 @@
 
 package gadgettracermanagerconfig
 
+import (
+	"fmt"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/config"
+)
+
 const ConfigPath = "/etc/ig/config.yaml"
 
 const (
@@ -25,6 +31,7 @@ const (
 	DockerSocketPath       = "docker-socketpath"
 	PodmanSocketPath       = "podman-socketpath"
 	GadgetNamespace        = "gadget-namespace"
+	DaemonLogLevel         = "daemon-log-level"
 
 	VerifyImage        = "verify-image"
 	PublicKeys         = "public-keys"
@@ -35,6 +42,21 @@ const (
 	OtelMetricsListen        = "otel-metrics-listen"
 	OtelMetricsListenAddress = "otel-metrics-listen-address"
 )
+
+func Init() error {
+	config.Config = config.NewWithPath(ConfigPath)
+
+	config.Config.SetDefault(HookModeKey, "auto")
+	config.Config.SetDefault(FallbackPodInformerKey, "true")
+	config.Config.SetDefault(EventsBufferLengthKey, 16384)
+	config.Config.SetDefault(DaemonLogLevel, "info")
+
+	err := config.Config.ReadInConfig()
+	if err != nil {
+		return fmt.Errorf("reading config: %w", err)
+	}
+	return nil
+}
 
 // IsValidKey checks if the given key is a valid configuration key for the GadgetTracerManager.
 // TODO: Remove in the future once we remove the flags from kubectl-gadget deploy.
@@ -60,7 +82,7 @@ func isRootKey(key string) bool {
 	switch key {
 	case HookModeKey, FallbackPodInformerKey, EventsBufferLengthKey,
 		ContainerdSocketPath, CrioSocketPath, DockerSocketPath,
-		PodmanSocketPath, GadgetNamespace:
+		PodmanSocketPath, GadgetNamespace, DaemonLogLevel:
 		return true
 	default:
 		return false

pkg/resources/manifests/deploy.yaml
@@ -28,6 +28,7 @@ data:
       docker-socketpath: /run/docker.sock
       podman-socketpath: /run/podman/podman.sock
       gadget-namespace: gadget
+      daemon-log-level: info
       operator:
         oci:
           allowed-gadgets: []
@@ -204,8 +205,6 @@ spec:
               value: "/host"
             - name: IG_EXPERIMENTAL
               value: "false"
-            - name: GADGET_TRACER_MANAGER_LOG_LEVEL
-              value: "info"
           securityContext:
             readOnlyRootFilesystem: true
             # With hostPID/hostNetwork/privileged [1] set to false, we need to set appropriate

commit 0ff58623406591d5145e00396de794412f18bc3d

gadgettracermanager: Skip reading config file for each liveness probe check
Fixes: 11f9c48b7230fb00cfc6964a3fdff0e634a4b6f7

It seems we reading the config file for each liveness probe check, which
isn't ideal. I move the logic of reading the config in 'serve'. Also,
I don't see anything relying on log level before 'serve'. 'liveness' block
currently uses fmt.Printf().

Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

gadget-container/gadgettracermanager/main.go
@@ -121,17 +121,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := gadgettracermanagerconfig.Init(); err != nil {
-		log.Fatalf("Initializing config: %v", err)
-	}
-
-	logLevel, err := log.ParseLevel(config.Config.GetString(gadgettracermanagerconfig.DaemonLogLevel))
-	if err != nil {
-		log.Fatalf("Parsing log level %q: %v", logLevel, err)
-	}
-	log.SetLevel(logLevel)
-	log.Infof("Config: %s=%s", gadgettracermanagerconfig.DaemonLogLevel, logLevel)
-
 	labels := []*pb.Label{}
 	if label != "" {
 		pairs := strings.Split(label, ",")
@@ -265,6 +254,16 @@ func main() {
 	}
 
 	if serve {
+		if err := gadgettracermanagerconfig.Init(); err != nil {
+			log.Fatalf("Initializing config: %v", err)
+		}
+
+		logLevel, err := log.ParseLevel(config.Config.GetString(gadgettracermanagerconfig.DaemonLogLevel))
+		if err != nil {
+			log.Fatalf("Parsing log level %q: %v", logLevel, err)
+		}
+		log.SetLevel(logLevel)
+		log.Infof("Config: %s=%s", gadgettracermanagerconfig.DaemonLogLevel, logLevel)
 		log.Infof("Inspektor Gadget version: %s", version.Version().String())
 		log.Infof("Inspektor Gadget User Agent: %s", version.UserAgent())
 