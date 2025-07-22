inspektor-gadget/inspektor-gadget

commit 9bae036afa8fea3e1be1ca95384715efe23dc6fc

gadgets: traceloop: Use bytes.IndexBytes() in fromCString*().
Fixes: 3690cd4a066c ("gadgets: Convert traceloop to image-based gadgets.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

gadgets/traceloop/go/program.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"os"
@@ -242,12 +243,15 @@ func (t *tracelooper) detach(mntnsID uint64) error {
 }
 
 func fromCString(in []byte) string {
-	for i := 0; i < len(in); i++ {
-		if in[i] == 0 {
-			return string(in[:i])
-		}
+	idx := bytes.IndexByte(in, 0)
+	switch {
+	case idx == -1:
+		return string(in)
+	case idx < len(in):
+		return string(in[:idx])
+	default:
+		return string(in)
 	}
-	return string(in)
 }
 
 func fromCStringN(in []byte, length int) string {
@@ -256,12 +260,16 @@ func fromCStringN(in []byte, length int) string {
 		l = length
 	}
 
-	for i := 0; i < l; i++ {
-		if in[i] == 0 {
-			return string(in[:i])
-		}
+	buf := in[:l]
+	idx := bytes.IndexByte(buf, 0)
+	switch {
+	case idx == -1:
+		return string(in)
+	case idx < l:
+		return string(in[:idx])
+	default:
+		return string(in)
 	}
-	return string(in[:l])
 }
 
 func timestampFromEvent(event *syscallEvent) int64 {

commit 79e8e029e3929d8d402efef5c2152f59934b30f7

traceloop: get buf length correctly for read/write syscalls
The buffer given to the read and write syscalls are not NULL-terminated
strings.

Before this patch:

```
RUNTIME.CONTAINERNAME           CPU         PID COMM     SYSCALL       PARAMETERS                          RET
test                            1        741846 cat      write         fd=1, buf="foo\n\x00\x00\x00\x…       4
```

After this patch:

```
RUNTIME.CONTAINERNAME           CPU         PID COMM     SYSCALL       PARAMETERS                          RET
test                            1        741616 cat      write         fd=1, buf="foo\n", count=4            4
```

Fixes: 9bae036afa8f ("gadgets: traceloop: Use bytes.IndexBytes() in fromCString*().")

Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

gadgets/traceloop/go/program.go
@@ -257,24 +257,6 @@ func fromCString(in []byte) string {
 	}
 }
 
-func fromCStringN(in []byte, length int) string {
-	l := len(in)
-	if length < l {
-		l = length
-	}
-
-	buf := in[:l]
-	idx := bytes.IndexByte(buf, 0)
-	switch {
-	case idx == -1:
-		return string(in)
-	case idx < l:
-		return string(in[:idx])
-	default:
-		return string(in)
-	}
-}
-
 func timestampFromEvent(event *syscallEvent) int64 {
 	return time.Unix(0, int64(event.bootTimestamp)).Add(0).UnixNano()
 }
@@ -431,7 +413,11 @@ func (t *tracelooper) read(mntnsID uint64, reader *containerRingReader) ([]*even
 				// 0 byte at [C.PARAM_LENGTH - 1] is enforced in BPF code
 				event.param = fromCString(sysEventCont.Param[:])
 			} else {
-				event.param = fromCStringN(sysEventCont.Param[:], int(sysEventCont.Length))
+				if sysEventCont.Length < uint64(len(sysEventCont.Param[:])) {
+					event.param = string(sysEventCont.Param[:sysEventCont.Length])
+				} else {
+					event.param = string(sysEventCont.Param[:])
+				}
 			}
 
 			// Remove all non unicode character from the string.