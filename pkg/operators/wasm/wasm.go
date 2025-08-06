inspektor-gadget/inspektor-gadget

commit f0a9406ee617a8412b774330b989d922601b7ba3

charts: Add mount for wasm-cache
Fixes: ef09eaea2e445e393dbbf413d47e2e7918312e08

Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

pkg/operators/wasm/wasm.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/cilium/ebpf"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/tetratelabs/wazero"
 	wapi "github.com/tetratelabs/wazero/api"
@@ -62,7 +63,7 @@ type wasmOperator struct {
 func newWasmOperator() *wasmOperator {
 	cache, err := wazero.NewCompilationCacheWithDir(cacheDir)
 	if err != nil {
-		logger.DefaultLogger().Debugf("failed to create wasm compilation cache: %v", err)
+		log.Warnf("failed to setup wasm compilation cache, skipping cache: %v", err)
 		return &wasmOperator{}
 	}
 	return &wasmOperator{

charts/gadget/templates/daemonset.yaml
@@ -221,6 +221,11 @@ spec:
             - mountPath: /etc/ig
               name: config
               readOnly: true
+            # We need a dedicated volume to store the wasm cache
+            # otherwise it will fail as the container root filesystem is read only.
+            - mountPath: /var/run/ig/wasm-cache
+              name: wasm-cache
+              readOnly: false
       nodeSelector:
         {{- .Values.nodeSelector | toYaml | nindent 8 }}
       affinity:
@@ -290,3 +295,5 @@ spec:
           configMap:
             name: {{ include "gadget.fullname" . }}
             defaultMode: 0o400
+        - name: wasm-cache
+          emptyDir: {}

pkg/resources/manifests/deploy.yaml
@@ -322,6 +322,11 @@ spec:
             - mountPath: /etc/ig
               name: config
               readOnly: true
+            # We need a dedicated volume to store the wasm cache
+            # otherwise it will fail as the container root filesystem is read only.
+            - mountPath: /var/run/ig/wasm-cache
+              name: wasm-cache
+              readOnly: false
       nodeSelector:
         kubernetes.io/os: linux
       affinity:
@@ -379,3 +384,5 @@ spec:
           configMap:
             name: gadget
             defaultMode: 0o400
+        - name: wasm-cache
+          emptyDir: {}

commit 6ae9635b8590b148ce35980ad17ae4b394915a2d

wasm: only log cache setup failure for root user
Fixes: f0a9406ee617a8412b774330b989d922601b7ba3

Signed-off-by: Qasim Sarfraz <qasimsarfraz@microsoft.com>

pkg/operators/wasm/wasm.go
@@ -20,6 +20,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
 	"sync"
 	"time"
 
@@ -63,7 +64,9 @@ type wasmOperator struct {
 func newWasmOperator() *wasmOperator {
 	cache, err := wazero.NewCompilationCacheWithDir(cacheDir)
 	if err != nil {
-		log.Warnf("failed to setup wasm compilation cache, skipping cache: %v", err)
+		if os.Geteuid() == 0 {
+			log.Warnf("failed to setup wasm compilation cache, skipping cache: %v", err)
+		}
 		return &wasmOperator{}
 	}
 	return &wasmOperator{