8d24f897aee1647ae76586fed65714d6b348ce33
Alban Crequy <alban.crequy@gmail.com>
2025-09-18 13:22:56 +0200

    symbolizer: move common functions to new file
    Functions resolveStackItemsWithTable, newSymbolTableFromFile and
    lookupByAddr will be used both by the symtab symbolizer and the
    debuginfod symbolizer.
    
    Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

--- pkg/symbolizer/symtab_linux.go
+++ pkg/symbolizer/symtab_linux.go
@@ -18,27 +18,17 @@ package symbolizer
 
 import (
 	"bufio"
-	"debug/elf"
 	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
-	"slices"
 	"strconv"
 	"strings"
 	"syscall"
-	"time"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
-)
-
-const (
-	maxExecutableSize   = 512 * 1024 * 1024 // 512MB
-	maxSymbolLength     = 256
-	maxSymbolCount      = 10 * 1000 * 1000
-	maxSymbolCountTotal = 50 * 1000 * 1000
 )
 
 func getHostProcFsPidNs() (uint32, error) {
@@ -145,91 +135,6 @@ func (s *Symbolizer) newSymbolTableFromPid(pid uint32, expectedExeKey exeKey) (*
 	}
 
 	return s.newSymbolTableFromFile(file)
-}
-
-func (s *Symbolizer) newSymbolTableFromFile(file *os.File) (*symbolTable, error) {
-	var symbols []*symbol
-
-	elfFile, err := elf.NewFile(file)
-	if err != nil {
-		return nil, fmt.Errorf("parsing ELF file: %w", err)
-	}
-	defer elfFile.Close()
-
-	symtab, err := elfFile.Symbols()
-	if err != nil {
-		// No symbols found. This is not an error.
-		return &symbolTable{}, nil
-	}
-
-	symbolCount := 0
-	for _, sym := range symtab {
-		if sym.Name == "" {
-			continue
-		}
-		if sym.Size == 0 {
-			continue
-		}
-		if len(sym.Name) > maxSymbolLength {
-			sym.Name = sym.Name[:maxSymbolLength]
-		}
-		symbols = append(symbols, &symbol{
-			name:  sym.Name,
-			value: sym.Value,
-			size:  sym.Size,
-		})
-		symbolCount++
-	}
-	if symbolCount > maxSymbolCount {
-		return nil, fmt.Errorf("too many symbols: %d", symbolCount)
-	}
-	slices.SortFunc(symbols, func(a, b *symbol) int {
-		if a.value < b.value {
-			return -1
-		}
-		if a.value > b.value {
-			return 1
-		}
-		return 0
-	})
-
-	return &symbolTable{
-		symbols:   symbols,
-		isPIE:     elfFile.Type == elf.ET_DYN,
-		timestamp: time.Now(),
-	}, nil
-}
-
-func (s *Symbolizer) resolveStackItemsWithTable(table *symbolTable, baseAddress uint64, stackQueries []StackItemQuery, res []StackItemResponse) {
-	table.timestamp = time.Now()
-	for idx := range stackQueries {
-		symbol := table.lookupByAddr(stackQueries[idx].Addr - baseAddress)
-		if symbol != "" {
-			res[idx].Found = true
-			res[idx].Symbol = symbol
-		}
-	}
-}
-
-// lookupByAddr returns the symbol name for the given address.
-func (e *symbolTable) lookupByAddr(address uint64) string {
-	// Similar to a trivial binary search, but each symbol is a range.
-	n, found := slices.BinarySearchFunc(e.symbols, address, func(a *symbol, b uint64) int {
-		if a.value <= b && a.value+a.size > b {
-			return 0
-		}
-		if a.value > b {
-			return 1
-		}
-		if a.value < b {
-			return -1
-		}
-		return 0
-	})
-	if found {
-		return e.symbols[n].name
-	}
-	return "[unknown]"
 }
 
 // getBaseAddress gets the runtime base address of the main executable from /proc/pid/maps
