inspektor-gadget/inspektor-gadget

commit aaa2ffd7bebdf0e4da05f5f8149071fc5047ed34

socketenricher: Use CO-RE to make it resilient against changes
The socketenricher operator is used  to enrich events with process
information from a socket. This is implemented as an ebpf map that
stores a list of tuples (IPs+ports) and the information of the process
that created the socket. This map is an API contract between the gadgets
and Inspektor Gadget, any change to the layout of the data on it will
require the gadgets to be recompiled. Of course, this is a very big
limitation.

This commit uses the existing CO-RE technology to make it possible to
update the layout of this map without breaking the gadgets. This is done
by generating a BTF data that represents the structure stored on this map,
then this data is fed to the ebpf-go library when loading the program,
ensuring it relocates the access to that structure.

This in practice allows to:
- Introduce new information to the socket enricher
- Change the type or position of existing fields on it
- Dynamically disable or reduce the maximum size of some expensive fields (like paths).
  - Reduce the size of the map
  - Avoid spending CPU cycles on disabled fields

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/socketenricher/socketenricher.go
@@ -20,6 +20,8 @@ package socketenricher
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/cilium/ebpf"
@@ -32,6 +34,9 @@ import (
 
 const (
 	OperatorName = "SocketEnricher"
+	BTFSpecKey   = "socketEnricherbtf"
+	BTFStructKey = "socketEnricherStruct"
+	fieldsParam  = "socket-enricher-fields"
 )
 
 type SocketEnricherInterface interface {
@@ -42,6 +47,7 @@ type SocketEnricher struct {
 	mu             sync.Mutex
 	socketEnricher *tracer.SocketEnricher
 	refCount       int
+	seConfig       *tracer.Config
 }
 
 func (s *SocketEnricher) Name() string {
@@ -61,6 +67,36 @@ func (s *SocketEnricher) ParamDescs() params.ParamDescs {
 }
 
 func (s *SocketEnricher) Init(params *params.Params) error {
+	fields := params.Get(fieldsParam).AsStringSlice()
+
+	s.seConfig = &tracer.Config{}
+
+	for _, field := range fields {
+		sizeStr := "512"
+
+		parts := strings.SplitN(field, "=", 2)
+		field := parts[0]
+		if len(parts) >= 2 {
+			sizeStr = parts[1]
+		}
+
+		size, err := strconv.ParseUint(sizeStr, 10, 32)
+		if err != nil {
+			return fmt.Errorf("invalid size for field %s: %w", field, err)
+		}
+
+		switch field {
+		case "cwd":
+			s.seConfig.Cwd.Enabled = true
+			s.seConfig.Cwd.Size = uint32(size)
+		case "exepath":
+			s.seConfig.Exepath.Enabled = true
+			s.seConfig.Exepath.Size = uint32(size)
+		default:
+			return fmt.Errorf("unsupported field: %s", field)
+		}
+	}
+
 	return nil
 }
 
@@ -95,7 +131,7 @@ func (i *SocketEnricherInstance) PreGadgetRun() error {
 	defer i.manager.mu.Unlock()
 
 	if i.manager.refCount == 0 {
-		t, err := tracer.NewSocketEnricher()
+		t, err := tracer.NewSocketEnricher(*i.manager.seConfig)
 		if err != nil {
 			return err
 		}
@@ -127,7 +163,17 @@ func (i *SocketEnricherInstance) EnrichEvent(ev any) error {
 }
 
 func (s *SocketEnricher) GlobalParams() api.Params {
-	return nil
+	return api.Params{
+		{
+			Key:   fieldsParam,
+			Title: "Socket enricher fields",
+			Description: "List of optional fields and their sizes to be enabled on the socket enricher using the `field0=size,field1=size,...` format. " +
+				"Disabling or reducing the size of the optional fields can reduce the memory and CPU usage of Inspektor Gadget. " +
+				"Passing 0 as the size will use the default size of 512 bytes. If a field is not present on the list, then it's disabled.",
+			DefaultValue: "cwd=512,exepath=512",
+			TypeHint:     api.TypeStringSlice,
+		},
+	}
 }
 
 func (s *SocketEnricher) InstanceParams() api.Params {
@@ -152,7 +198,19 @@ func (s *SocketEnricher) Priority() int {
 }
 
 func (i *SocketEnricherInstance) PreStart(gadgetCtx operators.GadgetContext) error {
-	return i.PreGadgetRun()
+	err := i.PreGadgetRun()
+	if err != nil {
+		return err
+	}
+
+	btfSpec, btfStruct, err := i.manager.socketEnricher.BTFSpec()
+	if err != nil {
+		return err
+	}
+
+	gadgetCtx.SetVar(BTFSpecKey, btfSpec)
+	gadgetCtx.SetVar(BTFStructKey, btfStruct)
+	return nil
 }
 
 func (i *SocketEnricherInstance) Start(gadgetCtx operators.GadgetContext) error {

examples/go.mod
@@ -176,4 +176,4 @@ replace github.com/inspektor-gadget/inspektor-gadget => ../
 // - https://github.com/cilium/ebpf/pull/1772
 // - https://github.com/cilium/ebpf/pull/1778
 // - https://github.com/cilium/ebpf/pull/1794
-replace github.com/cilium/ebpf => github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4
+replace github.com/cilium/ebpf => github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03

examples/go.sum
@@ -23,8 +23,6 @@ github.com/cenkalti/backoff/v5 v5.0.2/go.mod h1:rkhZdG3JZukswDf7f0cwqPNk4K0sa+F9
 github.com/census-instrumentation/opencensus-proto v0.2.1/go.mod h1:f6KPmirojxKA12rnyqOA5BBL4O983OfeGPqjHWSTneU=
 github.com/cespare/xxhash/v2 v2.3.0 h1:UL815xU9SqsFlibzuggzjXhog7bL6oX9BbNZnL2UFvs=
 github.com/cespare/xxhash/v2 v2.3.0/go.mod h1:VGX0DQ3Q6kWi7AoAeZDth3/j3BFtOZR5XLFGgcrjCOs=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4 h1:dOy5i7+1OT+jD2Mj8Xda+pzrOK3HxDoHFyYduPziOME=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/client9/misspell v0.3.4/go.mod h1:qj6jICC3Q7zFZvVWo7KLAzC3yx5G7kyvSDkc90ppPyw=
 github.com/cncf/udpa/go v0.0.0-20191209042840-269d4d468f6f/go.mod h1:M8M6+tZqaGXZJjfX53e64911xZQV5JYwmTeXPW+k8Sc=
 github.com/containerd/cgroups/v3 v3.0.5 h1:44na7Ud+VwyE7LIoJ8JTNQOa549a8543BmzaJHo6Bzo=
@@ -173,6 +171,8 @@ github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 h1:5ZPtiqj0JL5oKWmcsq4VMaAW5uk
 github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3/go.mod h1:ndYquD05frm2vACXE1nsccT4oJzjhw2arTS2cpUD1PI=
 github.com/inconshreveable/mousetrap v1.1.0 h1:wN+x4NVGpMsO7ErUn/mUI3vEoE6Jt13X2s0bqwp9tc8=
 github.com/inconshreveable/mousetrap v1.1.0/go.mod h1:vpF70FUmC8bwa3OWnCshd2FqLfsEA9PFc4w1p2J65bw=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03 h1:+Ap2E1Qtov3B744+gt6qSjqEglkdC8ymiPuYr7FfHO0=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/jmhodges/clock v1.2.0 h1:eq4kys+NI0PLngzaHEe7AmPT90XMGIEySD1JfV1PDIs=
 github.com/jmhodges/clock v1.2.0/go.mod h1:qKjhA7x7u/lQpPB1XAqX1b1lCI/w3/fNuYpI/ZjLynI=
 github.com/josharian/intern v1.0.0 h1:vlS4z54oSdjm0bgjRigI+G1HpF+tI+9rE5LLzOg8HmY=

go.mod
@@ -210,4 +210,4 @@ replace github.com/gopacket/gopacket => github.com/gopacket/gopacket v1.3.2-0.20
 // - https://github.com/cilium/ebpf/pull/1772
 // - https://github.com/cilium/ebpf/pull/1778
 // - https://github.com/cilium/ebpf/pull/1794
-replace github.com/cilium/ebpf => github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4
+replace github.com/cilium/ebpf => github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03

go.sum
@@ -32,8 +32,6 @@ github.com/cenkalti/backoff/v5 v5.0.2/go.mod h1:rkhZdG3JZukswDf7f0cwqPNk4K0sa+F9
 github.com/census-instrumentation/opencensus-proto v0.2.1/go.mod h1:f6KPmirojxKA12rnyqOA5BBL4O983OfeGPqjHWSTneU=
 github.com/cespare/xxhash/v2 v2.3.0 h1:UL815xU9SqsFlibzuggzjXhog7bL6oX9BbNZnL2UFvs=
 github.com/cespare/xxhash/v2 v2.3.0/go.mod h1:VGX0DQ3Q6kWi7AoAeZDth3/j3BFtOZR5XLFGgcrjCOs=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4 h1:dOy5i7+1OT+jD2Mj8Xda+pzrOK3HxDoHFyYduPziOME=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/client9/misspell v0.3.4/go.mod h1:qj6jICC3Q7zFZvVWo7KLAzC3yx5G7kyvSDkc90ppPyw=
 github.com/cncf/udpa/go v0.0.0-20191209042840-269d4d468f6f/go.mod h1:M8M6+tZqaGXZJjfX53e64911xZQV5JYwmTeXPW+k8Sc=
 github.com/containerd/cgroups/v3 v3.0.5 h1:44na7Ud+VwyE7LIoJ8JTNQOa549a8543BmzaJHo6Bzo=
@@ -210,6 +208,8 @@ github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 h1:5ZPtiqj0JL5oKWmcsq4VMaAW5uk
 github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3/go.mod h1:ndYquD05frm2vACXE1nsccT4oJzjhw2arTS2cpUD1PI=
 github.com/inconshreveable/mousetrap v1.1.0 h1:wN+x4NVGpMsO7ErUn/mUI3vEoE6Jt13X2s0bqwp9tc8=
 github.com/inconshreveable/mousetrap v1.1.0/go.mod h1:vpF70FUmC8bwa3OWnCshd2FqLfsEA9PFc4w1p2J65bw=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03 h1:+Ap2E1Qtov3B744+gt6qSjqEglkdC8ymiPuYr7FfHO0=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/jmhodges/clock v1.2.0 h1:eq4kys+NI0PLngzaHEe7AmPT90XMGIEySD1JfV1PDIs=
 github.com/jmhodges/clock v1.2.0/go.mod h1:qKjhA7x7u/lQpPB1XAqX1b1lCI/w3/fNuYpI/ZjLynI=
 github.com/josharian/intern v1.0.0 h1:vlS4z54oSdjm0bgjRigI+G1HpF+tI+9rE5LLzOg8HmY=

include/gadget/filesystem.h
@@ -5,8 +5,9 @@
 #define __COMMON_FILESYSTEM_H__
 
 // clang-format off
-#define MAX_PERCPU_BUFSIZE (1 << 15)  // set by the kernel as an upper bound
-#define GADGET_PATH_MAX    512        // smaller than PATH_MAX to reduce memory consumption
+#define MAX_PERCPU_BUFSIZE  (1 << 15)  // set by the kernel as an upper bound
+#define GADGET_PATH_MAX     512        // smaller than PATH_MAX to reduce memory consumption
+#define PATH_MAX            4096
 #define MAX_STR_FILTER_SIZE 16        // bounded to size of the compared values (comm)
 #define MAX_BIN_PATH_SIZE   256       // max binary path size
 #define FILE_MAGIC_HDR_SIZE 32        // magic_write: bytes to save from a file's header
@@ -124,7 +125,7 @@ static __always_inline void *get_path_str(struct path *path)
 		}
 		// Add this dentry name to path
 		d_name = get_d_name_from_dentry(dentry);
-		len = (d_name.len + 1) & (GADGET_PATH_MAX - 1);
+		len = (d_name.len + 1) & (PATH_MAX - 1);
 		off = buf_off - len;
 
 		// Is string buffer big enough for dentry name?
@@ -150,7 +151,7 @@ static __always_inline void *get_path_str(struct path *path)
 		// memfd files have no path in the filesystem -> extract their name
 		buf_off = 0;
 		d_name = get_d_name_from_dentry(dentry);
-		bpf_probe_read_str(&(string_p->buf[0]), GADGET_PATH_MAX, (void *) d_name.name);
+		bpf_probe_read_str(&(string_p->buf[0]), PATH_MAX, (void *) d_name.name);
 	} else {
 		// Add leading slash
 		buf_off -= 1;

include/gadget/types.h
@@ -127,9 +127,18 @@ struct gadget_process {
 	struct gadget_parent parent;
 };
 
-#define SE_PATH_MAX 512
+#ifndef BPF_NO_PRESERVE_ACCESS_INDEX
+#pragma clang attribute push(__attribute__((preserve_access_index)), \
+			     apply_to = record)
+#endif
 
+#define SE_PATH_MAX 4096
+
+// Keep aligned with the ValueBtf function in pkg/socketenricher/tracer.go
 struct sockets_value {
+	// Fields that are always present must go at the beginning. Ideally these
+	// fields aren't never changed, however it's possible to change their order,
+	// type, etc. as CO-RE will produce relocations for the gadgets using them.
 	__u64 mntns;
 	__u64 pid_tgid;
 	__u64 uid_gid;
@@ -137,10 +146,20 @@ struct sockets_value {
 	char ptask[TASK_COMM_LEN];
 	__u64 sock;
 	__u64 deletion_timestamp;
-	char cwd[SE_PATH_MAX];
-	char exepath[SE_PATH_MAX];
 	__u32 ppid;
 	char ipv6only;
+
+	char optional_fields_start[0] __attribute__((aligned(8)));
+
+	// These fields are optional and can be disabled in the socket enricher
+	// operator. Gadgets using these fields MUST check if they exist by using
+	// bpf_core_field_exists() and get their size with bpf_core_field_size().
+	char cwd[SE_PATH_MAX];
+	char exepath[SE_PATH_MAX];
 };
+
+#ifndef BPF_NO_PRESERVE_ACCESS_INDEX
+#pragma clang attribute pop
+#endif
 
 #endif /* __TYPES_H */

pkg/btfhelpers/btfhelpers.go
@@ -17,6 +17,8 @@
 package btfhelpers
 
 import (
+	"bytes"
+	"fmt"
 	"reflect"
 
 	"github.com/cilium/ebpf/btf"
@@ -149,4 +151,23 @@ func getSimpleType(typ btf.Type) reflect.Type {
 		}
 	}
 	return nil
+}
+
+func BuildSpec(types []btf.Type) (*btf.Spec, error) {
+	builder, err := btf.NewBuilder(types)
+	if err != nil {
+		return nil, fmt.Errorf("creating BTF builder: %w", err)
+	}
+
+	buf, err := builder.Marshal(nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling BTF: %w", err)
+	}
+
+	spec, err := btf.LoadSpecFromReader(bytes.NewReader(buf))
+	if err != nil {
+		return nil, fmt.Errorf("loading BTF spec: %w", err)
+	}
+
+	return spec, nil
 }

commit 11b05288477d38dbcf1e58fa079e180e63d3738d

socketenricher operator: fix params
Fixes: aaa2ffd7bebd ("socketenricher: Use CO-RE to make it resilient against changes")

Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

pkg/operators/socketenricher/socketenricher.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cilium/ebpf"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 	tracer "github.com/inspektor-gadget/inspektor-gadget/pkg/socketenricher"
@@ -59,7 +60,17 @@ func (s *SocketEnricher) Description() string {
 }
 
 func (s *SocketEnricher) GlobalParamDescs() params.ParamDescs {
-	return nil
+	return params.ParamDescs{
+		{
+			Key:   fieldsParam,
+			Title: "Socket enricher fields",
+			Description: "List of optional fields and their sizes to be enabled on the socket enricher using the `field0=size,field1=size,...` format. " +
+				"Disabling or reducing the size of the optional fields can reduce the memory and CPU usage of Inspektor Gadget. " +
+				"Passing 0 as the size will use the default size of 512 bytes. If a field is not present on the list, then it's disabled.",
+			DefaultValue: "cwd=512,exepath=512",
+			TypeHint:     params.TypeStringSlice,
+		},
+	}
 }
 
 func (s *SocketEnricher) ParamDescs() params.ParamDescs {
@@ -163,17 +174,7 @@ func (i *SocketEnricherInstance) EnrichEvent(ev any) error {
 }
 
 func (s *SocketEnricher) GlobalParams() api.Params {
-	return api.Params{
-		{
-			Key:   fieldsParam,
-			Title: "Socket enricher fields",
-			Description: "List of optional fields and their sizes to be enabled on the socket enricher using the `field0=size,field1=size,...` format. " +
-				"Disabling or reducing the size of the optional fields can reduce the memory and CPU usage of Inspektor Gadget. " +
-				"Passing 0 as the size will use the default size of 512 bytes. If a field is not present on the list, then it's disabled.",
-			DefaultValue: "cwd=512,exepath=512",
-			TypeHint:     api.TypeStringSlice,
-		},
-	}
+	return apihelpers.ParamDescsToParams(s.GlobalParamDescs())
 }
 
 func (s *SocketEnricher) InstanceParams() api.Params {