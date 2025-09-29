inspektor-gadget/inspektor-gadget

commit aaa2ffd7bebdf0e4da05f5f8149071fc5047ed34

socketenricher: Use CO-RE to make it resilient against changes
The socketenricher operator is used  to enrich events with process
information from a socket. This is implemented as an ebpf map that
stores a list of tuples (IPs+ports) and the information of the process
that created the socket. This map is an API contract between the gadgets
and Inspektor Gadget, any change to the layout of the data on it will
require the gadgets to be recompiled. Of course, this is a very big
limitation.

This commit uses the existing CO-RE technology to make it possible to
update the layout of this map without breaking the gadgets. This is done
by generating a BTF data that represents the structure stored on this map,
then this data is fed to the ebpf-go library when loading the program,
ensuring it relocates the access to that structure.

This in practice allows to:
- Introduce new information to the socket enricher
- Change the type or position of existing fields on it
- Dynamically disable or reduce the maximum size of some expensive fields (like paths).
  - Reduce the size of the map
  - Avoid spending CPU cycles on disabled fields

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/operators/socketenricher/socketenricher.go
@@ -20,6 +20,8 @@ package socketenricher
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/cilium/ebpf"
@@ -32,6 +34,9 @@ import (
 
 const (
 	OperatorName = "SocketEnricher"
+	BTFSpecKey   = "socketEnricherbtf"
+	BTFStructKey = "socketEnricherStruct"
+	fieldsParam  = "socket-enricher-fields"
 )
 
 type SocketEnricherInterface interface {
@@ -42,6 +47,7 @@ type SocketEnricher struct {
 	mu             sync.Mutex
 	socketEnricher *tracer.SocketEnricher
 	refCount       int
+	seConfig       *tracer.Config
 }
 
 func (s *SocketEnricher) Name() string {
@@ -61,6 +67,36 @@ func (s *SocketEnricher) ParamDescs() params.ParamDescs {
 }
 
 func (s *SocketEnricher) Init(params *params.Params) error {
+	fields := params.Get(fieldsParam).AsStringSlice()
+
+	s.seConfig = &tracer.Config{}
+
+	for _, field := range fields {
+		sizeStr := "512"
+
+		parts := strings.SplitN(field, "=", 2)
+		field := parts[0]
+		if len(parts) >= 2 {
+			sizeStr = parts[1]
+		}
+
+		size, err := strconv.ParseUint(sizeStr, 10, 32)
+		if err != nil {
+			return fmt.Errorf("invalid size for field %s: %w", field, err)
+		}
+
+		switch field {
+		case "cwd":
+			s.seConfig.Cwd.Enabled = true
+			s.seConfig.Cwd.Size = uint32(size)
+		case "exepath":
+			s.seConfig.Exepath.Enabled = true
+			s.seConfig.Exepath.Size = uint32(size)
+		default:
+			return fmt.Errorf("unsupported field: %s", field)
+		}
+	}
+
 	return nil
 }
 
@@ -95,7 +131,7 @@ func (i *SocketEnricherInstance) PreGadgetRun() error {
 	defer i.manager.mu.Unlock()
 
 	if i.manager.refCount == 0 {
-		t, err := tracer.NewSocketEnricher()
+		t, err := tracer.NewSocketEnricher(*i.manager.seConfig)
 		if err != nil {
 			return err
 		}
@@ -127,7 +163,17 @@ func (i *SocketEnricherInstance) EnrichEvent(ev any) error {
 }
 
 func (s *SocketEnricher) GlobalParams() api.Params {
-	return nil
+	return api.Params{
+		{
+			Key:   fieldsParam,
+			Title: "Socket enricher fields",
+			Description: "List of optional fields and their sizes to be enabled on the socket enricher using the `field0=size,field1=size,...` format. " +
+				"Disabling or reducing the size of the optional fields can reduce the memory and CPU usage of Inspektor Gadget. " +
+				"Passing 0 as the size will use the default size of 512 bytes. If a field is not present on the list, then it's disabled.",
+			DefaultValue: "cwd=512,exepath=512",
+			TypeHint:     api.TypeStringSlice,
+		},
+	}
 }
 
 func (s *SocketEnricher) InstanceParams() api.Params {
@@ -152,7 +198,19 @@ func (s *SocketEnricher) Priority() int {
 }
 
 func (i *SocketEnricherInstance) PreStart(gadgetCtx operators.GadgetContext) error {
-	return i.PreGadgetRun()
+	err := i.PreGadgetRun()
+	if err != nil {
+		return err
+	}
+
+	btfSpec, btfStruct, err := i.manager.socketEnricher.BTFSpec()
+	if err != nil {
+		return err
+	}
+
+	gadgetCtx.SetVar(BTFSpecKey, btfSpec)
+	gadgetCtx.SetVar(BTFStructKey, btfStruct)
+	return nil
 }
 
 func (i *SocketEnricherInstance) Start(gadgetCtx operators.GadgetContext) error {

examples/go.mod
@@ -176,4 +176,4 @@ replace github.com/inspektor-gadget/inspektor-gadget => ../
 // - https://github.com/cilium/ebpf/pull/1772
 // - https://github.com/cilium/ebpf/pull/1778
 // - https://github.com/cilium/ebpf/pull/1794
-replace github.com/cilium/ebpf => github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4
+replace github.com/cilium/ebpf => github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03

examples/go.sum
@@ -23,8 +23,6 @@ github.com/cenkalti/backoff/v5 v5.0.2/go.mod h1:rkhZdG3JZukswDf7f0cwqPNk4K0sa+F9
 github.com/census-instrumentation/opencensus-proto v0.2.1/go.mod h1:f6KPmirojxKA12rnyqOA5BBL4O983OfeGPqjHWSTneU=
 github.com/cespare/xxhash/v2 v2.3.0 h1:UL815xU9SqsFlibzuggzjXhog7bL6oX9BbNZnL2UFvs=
 github.com/cespare/xxhash/v2 v2.3.0/go.mod h1:VGX0DQ3Q6kWi7AoAeZDth3/j3BFtOZR5XLFGgcrjCOs=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4 h1:dOy5i7+1OT+jD2Mj8Xda+pzrOK3HxDoHFyYduPziOME=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/client9/misspell v0.3.4/go.mod h1:qj6jICC3Q7zFZvVWo7KLAzC3yx5G7kyvSDkc90ppPyw=
 github.com/cncf/udpa/go v0.0.0-20191209042840-269d4d468f6f/go.mod h1:M8M6+tZqaGXZJjfX53e64911xZQV5JYwmTeXPW+k8Sc=
 github.com/containerd/cgroups/v3 v3.0.5 h1:44na7Ud+VwyE7LIoJ8JTNQOa549a8543BmzaJHo6Bzo=
@@ -173,6 +171,8 @@ github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 h1:5ZPtiqj0JL5oKWmcsq4VMaAW5uk
 github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3/go.mod h1:ndYquD05frm2vACXE1nsccT4oJzjhw2arTS2cpUD1PI=
 github.com/inconshreveable/mousetrap v1.1.0 h1:wN+x4NVGpMsO7ErUn/mUI3vEoE6Jt13X2s0bqwp9tc8=
 github.com/inconshreveable/mousetrap v1.1.0/go.mod h1:vpF70FUmC8bwa3OWnCshd2FqLfsEA9PFc4w1p2J65bw=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03 h1:+Ap2E1Qtov3B744+gt6qSjqEglkdC8ymiPuYr7FfHO0=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/jmhodges/clock v1.2.0 h1:eq4kys+NI0PLngzaHEe7AmPT90XMGIEySD1JfV1PDIs=
 github.com/jmhodges/clock v1.2.0/go.mod h1:qKjhA7x7u/lQpPB1XAqX1b1lCI/w3/fNuYpI/ZjLynI=
 github.com/josharian/intern v1.0.0 h1:vlS4z54oSdjm0bgjRigI+G1HpF+tI+9rE5LLzOg8HmY=

go.mod
@@ -210,4 +210,4 @@ replace github.com/gopacket/gopacket => github.com/gopacket/gopacket v1.3.2-0.20
 // - https://github.com/cilium/ebpf/pull/1772
 // - https://github.com/cilium/ebpf/pull/1778
 // - https://github.com/cilium/ebpf/pull/1794
-replace github.com/cilium/ebpf => github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4
+replace github.com/cilium/ebpf => github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03

go.sum
@@ -32,8 +32,6 @@ github.com/cenkalti/backoff/v5 v5.0.2/go.mod h1:rkhZdG3JZukswDf7f0cwqPNk4K0sa+F9
 github.com/census-instrumentation/opencensus-proto v0.2.1/go.mod h1:f6KPmirojxKA12rnyqOA5BBL4O983OfeGPqjHWSTneU=
 github.com/cespare/xxhash/v2 v2.3.0 h1:UL815xU9SqsFlibzuggzjXhog7bL6oX9BbNZnL2UFvs=
 github.com/cespare/xxhash/v2 v2.3.0/go.mod h1:VGX0DQ3Q6kWi7AoAeZDth3/j3BFtOZR5XLFGgcrjCOs=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4 h1:dOy5i7+1OT+jD2Mj8Xda+pzrOK3HxDoHFyYduPziOME=
-github.com/cilium/ebpf v0.18.1-0.20250623141833-9958a4f58fb4/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/client9/misspell v0.3.4/go.mod h1:qj6jICC3Q7zFZvVWo7KLAzC3yx5G7kyvSDkc90ppPyw=
 github.com/cncf/udpa/go v0.0.0-20191209042840-269d4d468f6f/go.mod h1:M8M6+tZqaGXZJjfX53e64911xZQV5JYwmTeXPW+k8Sc=
 github.com/containerd/cgroups/v3 v3.0.5 h1:44na7Ud+VwyE7LIoJ8JTNQOa549a8543BmzaJHo6Bzo=
@@ -210,6 +208,8 @@ github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 h1:5ZPtiqj0JL5oKWmcsq4VMaAW5uk
 github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3/go.mod h1:ndYquD05frm2vACXE1nsccT4oJzjhw2arTS2cpUD1PI=
 github.com/inconshreveable/mousetrap v1.1.0 h1:wN+x4NVGpMsO7ErUn/mUI3vEoE6Jt13X2s0bqwp9tc8=
 github.com/inconshreveable/mousetrap v1.1.0/go.mod h1:vpF70FUmC8bwa3OWnCshd2FqLfsEA9PFc4w1p2J65bw=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03 h1:+Ap2E1Qtov3B744+gt6qSjqEglkdC8ymiPuYr7FfHO0=
+github.com/inspektor-gadget/ebpf v0.0.0-20250625205447-f72d1b709b03/go.mod h1:fLCgMo3l8tZmAdM3B2XqdFzXBpwkcSTroaVqN08OWVY=
 github.com/jmhodges/clock v1.2.0 h1:eq4kys+NI0PLngzaHEe7AmPT90XMGIEySD1JfV1PDIs=
 github.com/jmhodges/clock v1.2.0/go.mod h1:qKjhA7x7u/lQpPB1XAqX1b1lCI/w3/fNuYpI/ZjLynI=
 github.com/josharian/intern v1.0.0 h1:vlS4z54oSdjm0bgjRigI+G1HpF+tI+9rE5LLzOg8HmY=

include/gadget/filesystem.h
@@ -5,8 +5,9 @@
 #define __COMMON_FILESYSTEM_H__
 
 // clang-format off
-#define MAX_PERCPU_BUFSIZE (1 << 15)  // set by the kernel as an upper bound
-#define GADGET_PATH_MAX    512        // smaller than PATH_MAX to reduce memory consumption
+#define MAX_PERCPU_BUFSIZE  (1 << 15)  // set by the kernel as an upper bound
+#define GADGET_PATH_MAX     512        // smaller than PATH_MAX to reduce memory consumption
+#define PATH_MAX            4096
 #define MAX_STR_FILTER_SIZE 16        // bounded to size of the compared values (comm)
 #define MAX_BIN_PATH_SIZE   256       // max binary path size
 #define FILE_MAGIC_HDR_SIZE 32        // magic_write: bytes to save from a file's header
@@ -124,7 +125,7 @@ static __always_inline void *get_path_str(struct path *path)
 		}
 		// Add this dentry name to path
 		d_name = get_d_name_from_dentry(dentry);
-		len = (d_name.len + 1) & (GADGET_PATH_MAX - 1);
+		len = (d_name.len + 1) & (PATH_MAX - 1);
 		off = buf_off - len;
 
 		// Is string buffer big enough for dentry name?
@@ -150,7 +151,7 @@ static __always_inline void *get_path_str(struct path *path)
 		// memfd files have no path in the filesystem -> extract their name
 		buf_off = 0;
 		d_name = get_d_name_from_dentry(dentry);
-		bpf_probe_read_str(&(string_p->buf[0]), GADGET_PATH_MAX, (void *) d_name.name);
+		bpf_probe_read_str(&(string_p->buf[0]), PATH_MAX, (void *) d_name.name);
 	} else {
 		// Add leading slash
 		buf_off -= 1;

include/gadget/types.h
@@ -127,9 +127,18 @@ struct gadget_process {
 	struct gadget_parent parent;
 };
 
-#define SE_PATH_MAX 512
+#ifndef BPF_NO_PRESERVE_ACCESS_INDEX
+#pragma clang attribute push(__attribute__((preserve_access_index)), \
+			     apply_to = record)
+#endif
 
+#define SE_PATH_MAX 4096
+
+// Keep aligned with the ValueBtf function in pkg/socketenricher/tracer.go
 struct sockets_value {
+	// Fields that are always present must go at the beginning. Ideally these
+	// fields aren't never changed, however it's possible to change their order,
+	// type, etc. as CO-RE will produce relocations for the gadgets using them.
 	__u64 mntns;
 	__u64 pid_tgid;
 	__u64 uid_gid;
@@ -137,10 +146,20 @@ struct sockets_value {
 	char ptask[TASK_COMM_LEN];
 	__u64 sock;
 	__u64 deletion_timestamp;
-	char cwd[SE_PATH_MAX];
-	char exepath[SE_PATH_MAX];
 	__u32 ppid;
 	char ipv6only;
+
+	char optional_fields_start[0] __attribute__((aligned(8)));
+
+	// These fields are optional and can be disabled in the socket enricher
+	// operator. Gadgets using these fields MUST check if they exist by using
+	// bpf_core_field_exists() and get their size with bpf_core_field_size().
+	char cwd[SE_PATH_MAX];
+	char exepath[SE_PATH_MAX];
 };
+
+#ifndef BPF_NO_PRESERVE_ACCESS_INDEX
+#pragma clang attribute pop
+#endif
 
 #endif /* __TYPES_H */

pkg/btfhelpers/btfhelpers.go
@@ -17,6 +17,8 @@
 package btfhelpers
 
 import (
+	"bytes"
+	"fmt"
 	"reflect"
 
 	"github.com/cilium/ebpf/btf"
@@ -149,4 +151,23 @@ func getSimpleType(typ btf.Type) reflect.Type {
 		}
 	}
 	return nil
+}
+
+func BuildSpec(types []btf.Type) (*btf.Spec, error) {
+	builder, err := btf.NewBuilder(types)
+	if err != nil {
+		return nil, fmt.Errorf("creating BTF builder: %w", err)
+	}
+
+	buf, err := builder.Marshal(nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling BTF: %w", err)
+	}
+
+	spec, err := btf.LoadSpecFromReader(bytes.NewReader(buf))
+	if err != nil {
+		return nil, fmt.Errorf("loading BTF spec: %w", err)
+	}
+
+	return spec, nil
 }

commit d2b9e83c96c80ced7bb023a684ca078a0a0b859a

operators: remove GlobalParamDescs in favour of GlobalParams
Commit 2c81bb6cff35 ("tree-wide: Remove legacy operators code") says:

> Remove some code from operators and related things only needed for
> built-in gadgets. There is still some clean up to do:
> - Remove GlobalParamDescs, ParamDescs, PreGadgetRun and PostGadgetRun

This commit removes GlobalParamDescs as suggested. The other functions
remain unchanged.

Fixes: aaa2ffd7bebd ("socketenricher: Use CO-RE to make it resilient against changes")
Fixes: 2c81bb6cff35 ("tree-wide: Remove legacy operators code")

Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

docs/api/_golang/operators/main.go
@@ -22,6 +22,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
 	igjson "github.com/inspektor-gadget/inspektor-gadget/pkg/datasource/formatters/json"
 	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/ebpf"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/formatters"
@@ -49,7 +50,7 @@ func do() error {
 
 	// Configure the local manager operator
 	localManagerOp := localmanager.LocalManagerOperator
-	localManagerParams := localManagerOp.GlobalParamDescs().ToParams()
+	localManagerParams := apihelpers.ToParamDescs(localManagerOp.GlobalParams()).ToParams()
 	localManagerParams.Get(localmanager.Runtimes).Set("docker")
 	if err := localManagerOp.Init(localManagerParams); err != nil {
 		return fmt.Errorf("init local manager: %w", err)

examples/gadgets/trace_dns/main.go
@@ -22,6 +22,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
 	igjson "github.com/inspektor-gadget/inspektor-gadget/pkg/datasource/formatters/json"
 	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/ebpf"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/localmanager"
@@ -58,7 +59,8 @@ func do() error {
 
 	// Create the local manager operator
 	localManagerOp := localmanager.LocalManagerOperator
-	localManagerParams := localManagerOp.GlobalParamDescs().ToParams()
+	localManagerParams := apihelpers.ToParamDescs(localManagerOp.GlobalParams()).ToParams()
+
 	localManagerParams.Get(localmanager.Runtimes).Set("docker")
 	if err := localManagerOp.Init(localManagerParams); err != nil {
 		return fmt.Errorf("init local manager: %w", err)

pkg/operators/kubemanager/kubemanager.go
@@ -79,30 +79,6 @@ func (k *KubeManager) Description() string {
 	return "KubeManager handles container/pod/namespace information using Container-Collection and Tracer-Collection."
 }
 
-func (k *KubeManager) GlobalParamDescs() params.ParamDescs {
-	return params.ParamDescs{
-		{
-			Key:          ParamFallbackPodInformer,
-			DefaultValue: "true",
-			Description:  "Use pod informer as a fallback for the main hook",
-			TypeHint:     params.TypeBool,
-		},
-		{
-			Key:            ParamHookMode,
-			DefaultValue:   hookModeAuto,
-			Description:    "Mechanism to collect container information",
-			TypeHint:       params.TypeString,
-			PossibleValues: supportedHookModes,
-		},
-		{
-			Key:          ParamHookLivenessSocketFile,
-			DefaultValue: types.DefaultHookAndLivenessSocketFile,
-			Description:  "Path to the socket file for serving hook's requests for adding/removing containers and for liveness checks",
-			TypeHint:     params.TypeString,
-		},
-	}
-}
-
 func (k *KubeManager) ParamDescs() params.ParamDescs {
 	return append(common.GetContainerSelectorParams(true),
 		&params.ParamDesc{
@@ -371,7 +347,27 @@ func (m *KubeManagerInstance) EnrichEvent(ev any) error {
 }
 
 func (k *KubeManager) GlobalParams() api.Params {
-	return apihelpers.ParamDescsToParams(k.GlobalParamDescs())
+	return apihelpers.ParamDescsToParams(params.ParamDescs{
+		{
+			Key:          ParamFallbackPodInformer,
+			DefaultValue: "true",
+			Description:  "Use pod informer as a fallback for the main hook",
+			TypeHint:     params.TypeBool,
+		},
+		{
+			Key:            ParamHookMode,
+			DefaultValue:   hookModeAuto,
+			Description:    "Mechanism to collect container information",
+			TypeHint:       params.TypeString,
+			PossibleValues: supportedHookModes,
+		},
+		{
+			Key:          ParamHookLivenessSocketFile,
+			DefaultValue: types.DefaultHookAndLivenessSocketFile,
+			Description:  "Path to the socket file for serving hook's requests for adding/removing containers and for liveness checks",
+			TypeHint:     params.TypeString,
+		},
+	})
 }
 
 func (k *KubeManager) InstanceParams() api.Params {

pkg/operators/kubenameresolver/kubenameresolver.go
@@ -48,10 +48,6 @@ func (k *KubeNameResolver) Description() string {
 	return "KubeNameResolver resolves pod name/namespace to IP addresses"
 }
 
-func (k *KubeNameResolver) GlobalParamDescs() params.ParamDescs {
-	return nil
-}
-
 func (k *KubeNameResolver) ParamDescs() params.ParamDescs {
 	return nil
 }

pkg/operators/localmanager/localmanager.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/rlimit"
-	"github.com/containerd/containerd/pkg/cri/constants"
 	securejoin "github.com/cyphar/filepath-securejoin"
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
@@ -84,60 +83,6 @@ func (l *localManager) Name() string {
 
 func (l *localManager) Description() string {
 	return "Handles enrichment of container data and attaching/detaching to and from containers"
-}
-
-func (l *localManager) GlobalParamDescs() params.ParamDescs {
-	return params.ParamDescs{
-		{
-			Key:          Runtimes,
-			Alias:        "r",
-			DefaultValue: strings.Join(containerutils.AvailableRuntimes, ","),
-			Description: fmt.Sprintf("Comma-separated list of container runtimes. Supported values are: %s",
-				strings.Join(containerutils.AvailableRuntimes, ", ")),
-			// PossibleValues: containerutils.AvailableRuntimes, // TODO
-		},
-		{
-			Key:          DockerSocketPath,
-			DefaultValue: runtimeclient.DockerDefaultSocketPath,
-			Description:  "Docker Engine API Unix socket path",
-		},
-		{
-			Key:          ContainerdSocketPath,
-			DefaultValue: runtimeclient.ContainerdDefaultSocketPath,
-			Description:  "Containerd CRI Unix socket path",
-		},
-		{
-			Key:          CrioSocketPath,
-			DefaultValue: runtimeclient.CrioDefaultSocketPath,
-			Description:  "CRI-O CRI Unix socket path",
-		},
-		{
-			Key:          PodmanSocketPath,
-			DefaultValue: runtimeclient.PodmanDefaultSocketPath,
-			Description:  "Podman Unix socket path",
-		},
-		{
-			Key:          ContainerdNamespace,
-			DefaultValue: constants.K8sContainerdNamespace,
-			Description:  "Containerd namespace to use",
-		},
-		{
-			Key:          RuntimeProtocol,
-			DefaultValue: "internal",
-			Description:  "Container runtime protocol. Supported values are: internal, cri",
-		},
-		{
-			Key:          EnrichWithK8sApiserver,
-			DefaultValue: "false",
-			Description:  "Connect to the K8s API server to get further K8s enrichment",
-			TypeHint:     params.TypeBool,
-		},
-		{
-			Key:          KubeconfigPath,
-			DefaultValue: "", // Try in-cluster config by default
-			Description:  "Path to kubeconfig file. If not set, in-cluster config will be used.",
-		},
-	}
 }
 
 func (l *localManager) ParamDescs() params.ParamDescs {
@@ -505,7 +450,57 @@ type localManagerTraceWrapper struct {
 }
 
 func (l *localManager) GlobalParams() api.Params {
-	return apihelpers.ParamDescsToParams(l.GlobalParamDescs())
+	return apihelpers.ParamDescsToParams(params.ParamDescs{
+		{
+			Key:          Runtimes,
+			Alias:        "r",
+			DefaultValue: strings.Join(containerutils.AvailableRuntimes, ","),
+			Description: fmt.Sprintf("Comma-separated list of container runtimes. Supported values are: %s",
+				strings.Join(containerutils.AvailableRuntimes, ", ")),
+			// PossibleValues: containerutils.AvailableRuntimes, // TODO
+		},
+		{
+			Key:          DockerSocketPath,
+			DefaultValue: runtimeclient.DockerDefaultSocketPath,
+			Description:  "Docker Engine API Unix socket path",
+		},
+		{
+			Key:          ContainerdSocketPath,
+			DefaultValue: runtimeclient.ContainerdDefaultSocketPath,
+			Description:  "Containerd CRI Unix socket path",
+		},
+		{
+			Key:          CrioSocketPath,
+			DefaultValue: runtimeclient.CrioDefaultSocketPath,
+			Description:  "CRI-O CRI Unix socket path",
+		},
+		{
+			Key:          PodmanSocketPath,
+			DefaultValue: runtimeclient.PodmanDefaultSocketPath,
+			Description:  "Podman Unix socket path",
+		},
+		{
+			Key:          ContainerdNamespace,
+			DefaultValue: "k8s.io",
+			Description:  "Containerd namespace to use",
+		},
+		{
+			Key:          RuntimeProtocol,
+			DefaultValue: "internal",
+			Description:  "Container runtime protocol. Supported values are: internal, cri",
+		},
+		{
+			Key:          EnrichWithK8sApiserver,
+			DefaultValue: "false",
+			Description:  "Connect to the K8s API server to get further K8s enrichment",
+			TypeHint:     params.TypeBool,
+		},
+		{
+			Key:          KubeconfigPath,
+			DefaultValue: "", // Try in-cluster config by default
+			Description:  "Path to kubeconfig file. If not set, in-cluster config will be used.",
+		},
+	})
 }
 
 func (l *localManager) InstanceParams() api.Params {

pkg/operators/socketenricher/socketenricher.go
@@ -58,10 +58,6 @@ func (s *SocketEnricher) Description() string {
 	return "Socket enricher provides process information for sockets"
 }
 
-func (s *SocketEnricher) GlobalParamDescs() params.ParamDescs {
-	return nil
-}
-
 func (s *SocketEnricher) ParamDescs() params.ParamDescs {
 	return nil
 }

pkg/operators/uidgidresolver/uidgidresolver.go
@@ -55,10 +55,6 @@ func (k *UidGidResolver) Description() string {
 	return "UidGidResolver resolves uid and gid to username and groupname"
 }
 
-func (k *UidGidResolver) GlobalParamDescs() params.ParamDescs {
-	return nil
-}
-
 func (k *UidGidResolver) GlobalParams() api.Params {
 	return nil
 }

pkg/operators/localmanager/localmanager_test.go
@@ -26,6 +26,7 @@ import (
 
 	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils/testutils"
+	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/utils"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/utils/host"
 )
@@ -49,7 +50,7 @@ func TestLocalManagerBasic(t *testing.T) {
 	require.NoError(t, err, "Failed to initialize host")
 
 	lm := &localManager{}
-	gParams := lm.GlobalParamDescs().ToParams()
+	gParams := apihelpers.ToParamDescs(lm.GlobalParams()).ToParams()
 	err = lm.Init(gParams)
 	require.NoError(t, err, "Failed to initialize localManager")
 	lm.Close()
@@ -64,7 +65,7 @@ func TestLocalManagerMountNsMap(t *testing.T) {
 	require.NoError(t, err, "Failed to initialize host")
 
 	lm := &localManager{}
-	gParams := lm.GlobalParamDescs().ToParams()
+	gParams := apihelpers.ToParamDescs(lm.GlobalParams()).ToParams()
 	err = lm.Init(gParams)
 	require.NoError(t, err, "Failed to initialize localManager")
 	defer func() {
@@ -137,7 +138,7 @@ func TestLocalManagerClose(t *testing.T) {
 	initialFdList := currentFdList(t)
 
 	lm := &localManager{}
-	gParams := lm.GlobalParamDescs().ToParams()
+	gParams := apihelpers.ToParamDescs(lm.GlobalParams()).ToParams()
 	gParams.Set(Runtimes, "docker")
 
 	for i := range 4 {