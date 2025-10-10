fcb94d9888ddb17daab83493754d958418cd8d15
Mauricio Vásquez <mauriciov@microsoft.com>
2025-10-10 18:11:02 -0500

    Add opentelemetry profiles exporter
    OpenTelemetry is adding support for a new profiling signal[0]. This
    commit implements an exporter for it.
    
    [0]: https://opentelemetry.io/blog/2024/profiling/
    
    Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

--- cmd/ig/main.go
+++ cmd/ig/main.go
@@ -44,6 +44,7 @@ import (
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/filter"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/formatters"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/localmanager"
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/otel-profiles"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/process"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/socketenricher"
 	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/sort"
