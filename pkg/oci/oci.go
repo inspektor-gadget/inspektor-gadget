inspektor-gadget/inspektor-gadget

commit 7eebaf15a39a25a48180aa6903ff65049ee49428

pkg: signature: Add notation verifying.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/signature/notation/verifier.go
@@ -1,0 +1,140 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package notation
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/distribution/reference"
+	"github.com/notaryproject/notation-go"
+	"github.com/notaryproject/notation-go/dir"
+	"github.com/notaryproject/notation-go/registry"
+	"github.com/notaryproject/notation-go/verifier"
+	"github.com/notaryproject/notation-go/verifier/trustpolicy"
+	"github.com/notaryproject/notation-go/verifier/truststore"
+	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/registry/remote"
+)
+
+type VerifierOptions struct {
+	Certificates   []string
+	PolicyDocument string
+}
+
+type Verifier struct {
+	notation.Verifier
+}
+
+const maxSignatureAttempts = 50
+
+func getImageDigest(ctx context.Context, store oras.Target, imageRef string) (string, error) {
+	desc, err := store.Resolve(ctx, imageRef)
+	if err != nil {
+		return "", fmt.Errorf("resolving image %q: %w", imageRef, err)
+	}
+
+	return desc.Digest.String(), nil
+}
+
+func getAndValidateTrustPolicy(policy string) (*trustpolicy.Document, error) {
+	policyDocument := &trustpolicy.Document{}
+	if err := json.Unmarshal([]byte(policy), policyDocument); err != nil {
+		return nil, fmt.Errorf("unmarshalling trust policy Document: %w", err)
+	}
+
+	if err := policyDocument.Validate(); err != nil {
+		return nil, fmt.Errorf("validating policy document: %w", err)
+	}
+
+	// This comes as a limitation on how certificates are given on the CLI,
+	// as we do not know to which trust store they should belong, we all store
+	// them in the same trust store.
+	if len(policyDocument.TrustPolicies[0].TrustStores) > 1 {
+		return nil, errors.New("trust policies with multiple trust stores are unsupported")
+	}
+
+	return policyDocument, nil
+}
+
+func addCertificatesToTrustStore(trustStore string, certificates []string) (string, error) {
+	trustStoreDir, err := os.MkdirTemp("", "ig-trust-store-")
+	if err != nil {
+		return "", err
+	}
+	path := filepath.Join(trustStoreDir, "truststore", "x509", trustStore)
+	if err := os.MkdirAll(path, 0o700); err != nil {
+		return "", err
+	}
+
+	for i, certificate := range certificates {
+		if err := os.WriteFile(filepath.Join(path, fmt.Sprintf("certificate_%d.pem", i)), []byte(certificate), 0o600); err != nil {
+			return "", err
+		}
+	}
+
+	return trustStoreDir, nil
+}
+
+func NewVerifier(opts VerifierOptions) (*Verifier, error) {
+	if len(opts.Certificates) == 0 {
+		return nil, errors.New("no certificates given")
+	}
+
+	policy, err := getAndValidateTrustPolicy(opts.PolicyDocument)
+	if err != nil {
+		return nil, fmt.Errorf("getting and validating trust policy: %w", err)
+	}
+
+	parts := strings.Split(policy.TrustPolicies[0].TrustStores[0], ":")
+
+	// Above call to Validate() ensure there are two parts:
+	// https://github.com/notaryproject/notation-go/blob/e83edbc388a9/verifier/trustpolicy/trustpolicy.go#L308-L311
+	// Moreover, using filepath.Join() is OK, as the name should respect this
+	// regexp ^[a-zA-Z0-9_.-]+$:
+	// https://github.com/notaryproject/notation-go/blob/e83edbc388a9/internal/file/file.go#L39-L41
+	trustStore := filepath.Join(parts[0], parts[1])
+	trustStoreDir, err := addCertificatesToTrustStore(trustStore, opts.Certificates)
+	if err != nil {
+		return nil, fmt.Errorf("adding certificates to trust store: %w", err)
+	}
+
+	verif, err := verifier.New(policy, truststore.NewX509TrustStore(dir.NewSysFS(trustStoreDir)), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Verifier{verif}, nil
+}
+
+func (n *Verifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.Target, ref reference.Named) error {
+	imageDigest, err := getImageDigest(ctx, imageStore, ref.String())
+	if err != nil {
+		return fmt.Errorf("getting image digest: %w", err)
+	}
+
+	verifyOptions := notation.VerifyOptions{
+		ArtifactReference:    fmt.Sprintf("%s@%s", ref.Name(), imageDigest),
+		MaxSignatureAttempts: maxSignatureAttempts,
+	}
+	_, _, err = notation.Verify(ctx, n.Verifier, registry.NewRepository(repo), verifyOptions)
+
+	return err
+}

pkg/signature/verifier.go
@@ -17,6 +17,7 @@ package signature
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/distribution/reference"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
@@ -24,6 +25,7 @@ import (
 	"oras.land/oras-go/v2/registry/remote"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature/cosign"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature/notation"
 )
 
 type Verifier interface {
@@ -31,19 +33,30 @@ type Verifier interface {
 }
 
 type SignatureVerifier struct {
-	*cosign.Verifier
+	verifiers map[string]Verifier
 }
 
 type VerifierOptions struct {
-	CosignVerifierOpts cosign.VerifierOptions
+	CosignVerifierOpts   cosign.VerifierOptions
+	NotationVerifierOpts notation.VerifierOptions
 }
 
 func (v *SignatureVerifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.Target, ref reference.Named) error {
-	if v.Verifier == nil {
+	if len(v.verifiers) == 0 {
 		return errors.New("no verification method available")
 	}
 
-	return v.Verifier.Verify(ctx, repo, imageStore, ref)
+	errs := make([]error, 0)
+	for method, verifier := range v.verifiers {
+		err := verifier.Verify(ctx, repo, imageStore, ref)
+		if err == nil {
+			return nil
+		}
+
+		errs = append(errs, fmt.Errorf("verifying with %s: %w", method, err))
+	}
+
+	return errors.Join(errs...)
 }
 
 func ExportSigningInformation(ctx context.Context, src oras.ReadOnlyTarget, dst oras.Target, desc ocispec.Descriptor) error {
@@ -55,6 +68,25 @@ func PullSigningInformation(ctx context.Context, repo *remote.Repository, imageS
 }
 
 func NewSignatureVerifier(opts VerifierOptions) (*SignatureVerifier, error) {
-	cosignVerifier, err := cosign.NewVerifier(opts.CosignVerifierOpts)
-	return &SignatureVerifier{cosignVerifier}, err
+	ret := &SignatureVerifier{verifiers: make(map[string]Verifier)}
+
+	if len(opts.CosignVerifierOpts.PublicKeys) > 0 {
+		verifier, err := cosign.NewVerifier(opts.CosignVerifierOpts)
+		if err != nil {
+			return nil, fmt.Errorf("creating cosign verifier: %w", err)
+		}
+
+		ret.verifiers["cosign"] = verifier
+	}
+
+	if len(opts.NotationVerifierOpts.Certificates) > 0 && len(opts.NotationVerifierOpts.PolicyDocument) > 0 {
+		verifier, err := notation.NewVerifier(opts.NotationVerifierOpts)
+		if err != nil {
+			return nil, fmt.Errorf("creating notation verifier: %w", err)
+		}
+
+		ret.verifiers["notation"] = verifier
+	}
+
+	return ret, nil
 }

pkg/signature/cosign/verifier_test.go
@@ -56,14 +56,32 @@ func TestNewVerifier(t *testing.T) {
 	}
 
 	tests := map[string]testDefinition{
-		"no public key": {
+		"no_public_key": {
 			shouldErr: true,
 		},
-		"malformed public key": {
+		"malformed_public_key": {
 			opts: VerifierOptions{
 				PublicKeys: []string{"foobar"},
 			},
 			shouldErr: true,
+		},
+		"correct_public_key": {
+			opts: VerifierOptions{
+				PublicKeys: []string{resources.InspektorGadgetPublicKey},
+			},
+		},
+		"correct_public_keys": {
+			opts: VerifierOptions{
+				PublicKeys: []string{
+					`
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEIur1/9dgnL6gwRsXRoE5tgpiZX0V
+wE3h/OMa2IqglFFvk8Qh1EX9zr5aASFdRcTKScjrU7uS1y6Z1z3NQe2P+g==
+-----END PUBLIC KEY-----
+`,
+					resources.InspektorGadgetPublicKey,
+				},
+			},
 		},
 	}
 
@@ -99,13 +117,13 @@ func TestVerify(t *testing.T) {
 	nonSignedImage := "ghcr.io/inspektor-gadget/gadget/trace_open@sha256:a5de3655d6c7640eb6d43f7d9d7182b233ac86aedddfe6c132cba6b876264d97"
 
 	tests := map[string]testDefinition{
-		"good public key with signed gadget": {
+		"good_public_key_with_signed_gadget": {
 			opts: VerifierOptions{
 				PublicKeys: []string{resources.InspektorGadgetPublicKey},
 			},
 			image: signedImage,
 		},
-		"wrong public key with signed gadget": {
+		"wrong_public_key_with_signed_gadget": {
 			opts: VerifierOptions{
 				PublicKeys: []string{
 					`
@@ -119,14 +137,14 @@ wE3h/OMa2IqglFFvk8Qh1EX9zr5aASFdRcTKScjrU7uS1y6Z1z3NQe2P+g==
 			image:     signedImage,
 			shouldErr: true,
 		},
-		"public key with unsigned gadget": {
+		"public_key_with_unsigned_gadget": {
 			opts: VerifierOptions{
 				PublicKeys: []string{resources.InspektorGadgetPublicKey},
 			},
 			image:     nonSignedImage,
 			shouldErr: true,
 		},
-		"several public keys with signed gadget": {
+		"several_public_keys_with_signed_gadget": {
 			opts: VerifierOptions{
 				PublicKeys: []string{
 					`

pkg/operators/oci-handler/oci.go
@@ -41,6 +41,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/resources"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature/cosign"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature/notation"
 )
 
 const (
@@ -53,6 +54,8 @@ const (
 	annotate                = "annotate"
 	verifyImage             = "verify-image"
 	publicKeys              = "public-keys"
+	certificates            = "notation-certificates"
+	policyDocument          = "notation-policy-document"
 	allowedGadgets          = "allowed-gadgets"
 )
 
@@ -89,6 +92,10 @@ func (o *ociHandler) Init(params *params.Params) error {
 				CosignVerifierOpts: cosign.VerifierOptions{
 					PublicKeys: o.globalParams.Get(publicKeys).AsStringSlice(),
 				},
+				NotationVerifierOpts: notation.VerifierOptions{
+					Certificates:   o.globalParams.Get(certificates).AsStringSlice(),
+					PolicyDocument: o.globalParams.Get(policyDocument).AsString(),
+				},
 			},
 		)
 		if err != nil {
@@ -114,9 +121,21 @@ func (o *ociHandler) GlobalParams() api.Params {
 		{
 			Key:          publicKeys,
 			Title:        "Public keys",
-			Description:  "Public keys used to verify the gadgets",
+			Description:  "Public keys used to verify the gadgets with cosign",
 			DefaultValue: resources.InspektorGadgetPublicKey,
 			TypeHint:     api.TypeStringSlice,
+		},
+		{
+			Key:         certificates,
+			Title:       "Notation certificates",
+			Description: "Certificates used to verify the gadgets with notation",
+			TypeHint:    api.TypeStringSlice,
+		},
+		{
+			Key:         policyDocument,
+			Title:       "Notation policy Document",
+			Description: "Policy Document used to verify the gadgets with notation",
+			TypeHint:    api.TypeString,
 		},
 		{
 			Key:         allowedGadgets,

pkg/signature/notation/verifier_test.go
@@ -1,0 +1,135 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package notation
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewVerifier(t *testing.T) {
+	t.Parallel()
+
+	type testDefinition struct {
+		opts      VerifierOptions
+		shouldErr bool
+	}
+
+	goodPolicyDocument := `
+{
+    "version": "1.0",
+    "trustPolicies": [
+        {
+            "name": "inspektor-gadget-policy",
+            "registryScopes": [ "*" ],
+            "signatureVerification": {
+              "level" : "strict"
+            },
+            "trustStores": ["ca:inspektor-gadget.io"],
+            "trustedIdentities": [
+              "x509.subject: CN=inspektor-gadget.io, O=Notary, L=Seattle, ST=WA, C=US"
+            ]
+        }
+    ]
+}
+`
+
+	goodCertificate := `
+-----BEGIN CERTIFICATE-----
+MIIDRTCCAi2gAwIBAgICAIEwDQYJKoZIhvcNAQELBQAwUTELMAkGA1UEBhMCVVMx
+CzAJBgNVBAgTAldBMRAwDgYDVQQHEwdTZWF0dGxlMQ8wDQYDVQQKEwZOb3Rhcnkx
+EjAQBgNVBAMTCWJhcmZvby5pbzAeFw0yNTEwMDExNTQ2MzVaFw0yNTEwMDIxNTQ2
+MzVaMFExCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJXQTEQMA4GA1UEBxMHU2VhdHRs
+ZTEPMA0GA1UEChMGTm90YXJ5MRIwEAYDVQQDEwliYXJmb28uaW8wggEiMA0GCSqG
+SIb3DQEBAQUAA4IBDwAwggEKAoIBAQDirG5+wdavupHd+K3M4hvPAzQu1NE6Edur
+VsptZTBXnZNCT3/rVlzIF6uoExUe9k8xdBV86aVlI1KUKXb3jWtq4u+9ACcmZ3nO
++JzhkOZEGN7XTf7axDWIZWv5mye655shIwtLrrguNBs+0lXqO+x5uImqdGtjgH5S
+RrDpHzSNiRziyqym1fCgJI3lwO/sWIZK+2oJLPvCYzS4k0sNo50wB6JI+Eu/qtBK
+c0OCXLhKr0ml9bRpDaoMyIhQrD0+k5JWSDPlNCwIEYueJpf3Ua38YuuM3pPEj8MA
+/hzEyrFpqeJV2Nn5JHRjAecSZbFRHyRtzETn9NKaz00EnoE3/O3nAgMBAAGjJzAl
+MA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcDAzANBgkqhkiG9w0B
+AQsFAAOCAQEANjfgsg6Zxc/g+8T3ZLNMj0myd8yciiL16iS4+LQKViRGNmAjRbic
+eslrAFXSiZHI/RIJxKnQBxtDiXs8UlknV2mLnxZP+S2vk3kW/z0w6gRPRC8t3w3K
+BcsDhE//PzLmnXdEYSZQvgXfdv2JujEb1euspyTjRL1vBlxdPh5eeevZsysHrt6q
+c2bFCX/8nicguibCC4ybKQazsbQK07tIPDwlbu0+sCk6ojmhdMZeOaXVcUoaisgq
+dKl8IemhdshLNii2f05fLb1ftcMaG0XIT7i86txiDbiBs+srcjW4bqfhSc201jHu
+/ftuKYyrrWBgnWZ6gmzqC5SR4pBdZVAp3w==
+-----END CERTIFICATE-----
+`
+
+	tests := map[string]testDefinition{
+		"no_policy_document_and_no_certificates": {
+			shouldErr: true,
+		},
+		"no_policy_document": {
+			opts: VerifierOptions{
+				Certificates: []string{goodCertificate},
+			},
+			shouldErr: true,
+		},
+		"no_certificates": {
+			opts: VerifierOptions{
+				PolicyDocument: goodPolicyDocument,
+			},
+			shouldErr: true,
+		},
+		"policy_document_with_several_trust_stores": {
+			opts: VerifierOptions{
+				Certificates: []string{goodCertificate},
+				PolicyDocument: `
+{
+	"version": "1.0",
+	"trustPolicies": [
+		{
+			"name": "inspektor-gadget-policy",
+			"registryScopes": [ "*" ],
+			"signatureVerification": {
+				"level" : "strict"
+			},
+			"trustStores": ["ca:inspektor-gadget.io", "ca:foobar.io"],
+			"trustedIdentities": [
+				"x509.subject: CN=inspektor-gadget.io, O=Notary, L=Seattle, ST=WA, C=US"
+			]
+		}
+	]
+}
+`,
+			},
+			shouldErr: true,
+		},
+		"ok": {
+			opts: VerifierOptions{
+				Certificates:   []string{goodCertificate},
+				PolicyDocument: goodPolicyDocument,
+			},
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			_, err := NewVerifier(test.opts)
+			if test.shouldErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
+}

commit 7537095abda9e4f3fbebb7eb0f1a0c84694dcf58

verifier: Use local store to load notation signatures
Many uses cases of Inspektor Gadget require it to run without an internet
connection. This commit fixes notation to avoid always trying to pull
the signature. This is done by passing the local store instead of the
remote one as target for the notation.Verify() logic.

Fixes: 7eebaf15a39a ("pkg: signature: Add notation verifying.")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/oci/oci.go
@@ -781,7 +781,7 @@ func getContentBytesFromDescriptor(ctx context.Context, fetcher content.Fetcher,
 	return bytes, nil
 }
 
-func ensureImage(ctx context.Context, imageStore oras.Target, image string, imgOpts *ImageOptions, pullPolicy string) error {
+func ensureImage(ctx context.Context, imageStore oras.GraphTarget, image string, imgOpts *ImageOptions, pullPolicy string) error {
 	switch pullPolicy {
 	case PullImageAlways:
 		_, err := pullGadgetImageToStore(ctx, imageStore, image, &imgOpts.AuthOptions)

pkg/signature/cosign/verifier.go
@@ -248,7 +248,7 @@ func checkPayloadImage(payloadBytes []byte, imageDigest string) error {
 	return nil
 }
 
-func (c *Verifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.Target, ref reference.Named) error {
+func (c *Verifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.GraphTarget, ref reference.Named) error {
 	imageDigest, err := getImageDigest(ctx, imageStore, ref.String())
 	if err != nil {
 		return fmt.Errorf("getting image digest: %w", err)

pkg/signature/notation/verifier.go
@@ -124,7 +124,7 @@ func NewVerifier(opts VerifierOptions) (*Verifier, error) {
 	return &Verifier{verif}, nil
 }
 
-func (n *Verifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.Target, ref reference.Named) error {
+func (n *Verifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.GraphTarget, ref reference.Named) error {
 	imageDigest, err := getImageDigest(ctx, imageStore, ref.String())
 	if err != nil {
 		return fmt.Errorf("getting image digest: %w", err)
@@ -134,7 +134,7 @@ func (n *Verifier) Verify(ctx context.Context, repo *remote.Repository, imageSto
 		ArtifactReference:    fmt.Sprintf("%s@%s", ref.Name(), imageDigest),
 		MaxSignatureAttempts: maxSignatureAttempts,
 	}
-	_, _, err = notation.Verify(ctx, n.Verifier, registry.NewRepository(repo), verifyOptions)
+	_, _, err = notation.Verify(ctx, n.Verifier, registry.NewRepository(imageStore), verifyOptions)
 
 	return err
 }

pkg/signature/verifier.go
@@ -29,7 +29,7 @@ import (
 )
 
 type Verifier interface {
-	Verify(ctx context.Context, repo *remote.Repository, imageStore oras.Target, ref reference.Named) error
+	Verify(ctx context.Context, repo *remote.Repository, imageStore oras.GraphTarget, ref reference.Named) error
 }
 
 type SignatureVerifier struct {
@@ -41,7 +41,7 @@ type VerifierOptions struct {
 	NotationVerifierOpts notation.VerifierOptions
 }
 
-func (v *SignatureVerifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.Target, ref reference.Named) error {
+func (v *SignatureVerifier) Verify(ctx context.Context, repo *remote.Repository, imageStore oras.GraphTarget, ref reference.Named) error {
 	if len(v.verifiers) == 0 {
 		return errors.New("no verification method available")
 	}

pkg/signature/cosign/verifier_test.go
@@ -28,7 +28,7 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/resources"
 )
 
-func createTestPrerequisities(t *testing.T, image string) (oras.Target, *remote.Repository, reference.Named) {
+func createTestPrerequisities(t *testing.T, image string) (oras.GraphTarget, *remote.Repository, reference.Named) {
 	store, err := oci.New(t.TempDir())
 	require.NoError(t, err)
 