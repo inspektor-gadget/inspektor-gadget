734971cc691f3e5c971c38160695af7d194a6ba1
Mauricio Vásquez <mauriciov@microsoft.com>
2025-10-16 16:24:23 -0500

    more changes
    - configurable exporters
    - set annotations on profile_cpu

--- pkg/operators/otel-profiles/otel-profiles.go
+++ pkg/operators/otel-profiles/otel-profiles.go
@@ -15,28 +15,46 @@
 package otelprofiles
 
 import (
-	"context"
+	"fmt"
 	"strings"
-	"time"
 
+	log "github.com/sirupsen/logrus"
 	"go.opentelemetry.io/collector/pdata/pprofile"
 	"go.opentelemetry.io/collector/pdata/pprofile/pprofileotlp"
 	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/encoding/gzip"
 
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/config"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	apihelpers "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api-helpers"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/otel-profiles/orderedset"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
 )
 
-const ()
+const (
+	ParamOtelProfilesExporter = "otel-profiles-exporter"
+
+	ExporterOTLPGRPC = "otlp-grpc"
+
+	CompressionNone = "none"
+	CompressionGZIP = "gzip"
+)
+
+var supportedExporters = []string{ExporterOTLPGRPC}
+
+type profileConfig struct {
+	Exporter    string `json:"exporter" yaml:"exporter"`
+	Endpoint    string `json:"endpoint" yaml:"endpoint"`
+	Insecure    bool   `json:"insecure" yaml:"insecure"`
+	Compression string `json:"compression" yaml:"compression"`
+}
 
 type otelProfilesOperator struct {
-	client pprofileotlp.GRPCClient
-	conn   *grpc.ClientConn
+	clients map[string]pprofileotlp.GRPCClient
 }
 
 func (o *otelProfilesOperator) Name() string {
@@ -44,16 +62,53 @@ func (o *otelProfilesOperator) Name() string {
 }
 
 func (o *otelProfilesOperator) Init(params *params.Params) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	conn, err := grpc.DialContext(ctx, "localhost:4317", grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		return err
+	if config.Config == nil {
+		return nil
 	}
 
-	o.conn = conn
-	o.client = pprofileotlp.NewGRPCClient(conn)
+	o.clients = make(map[string]pprofileotlp.GRPCClient)
+
+	configs := make(map[string]*profileConfig, 0)
+	log.Infof("loading profile exporters")
+	err := config.Config.UnmarshalKey("operator.otel-profiles.exporters", &configs)
+	if err != nil {
+		log.Warnf("failed to load operator.otel-profiles.exporters: %v", err)
+	}
+
+	log.Infof("loaded %d profile exporters", len(configs))
+
+	for k, v := range configs {
+		log.Infof("loading profile exporter %q", k)
+
+		if v.Exporter != ExporterOTLPGRPC {
+			return fmt.Errorf("unsupported log exporter %q; expected one of %s", v.Exporter,
+				strings.Join(supportedExporters, ", "))
+		}
+		var options []grpc.DialOption
+		var callOpts []grpc.CallOption
+
+		//options = append(options, otlploggrpc.WithEndpoint(v.Endpoint))
+		if v.Insecure {
+			options = append(options, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		}
+		switch v.Compression {
+		default:
+			return fmt.Errorf("unsupported log compression %q", v.Compression)
+		case "", CompressionNone:
+		case CompressionGZIP:
+			callOpts = append(callOpts, grpc.UseCompressor(gzip.Name))
+		}
+
+		conn, err := grpc.NewClient(v.Endpoint, options...)
+		if err != nil {
+			log.Errorf("creating gRPC connection to %q: %v", v.Endpoint, err)
+			return fmt.Errorf("creating gRPC connection to %q: %w", v.Endpoint, err)
+		}
+
+		// TODO: there's not profiles processor?
+		o.clients[k] = pprofileotlp.NewGRPCClient(conn)
+		log.Infof("> log exporter %q with endpoint %q loaded", k, v.Endpoint)
+	}
 
 	return nil
 }
@@ -64,18 +119,29 @@ func (o *otelProfilesOperator) GlobalParams() api.Params {
 
 func (o *otelProfilesOperator) InstanceParams() api.Params {
 	return api.Params{
-		//&api.Param{
-		//	Key:          ParamOtelLogsExporter,
-		//	Description:  "Exporter to use for log exporting",
-		//	DefaultValue: "",
-		//},
+		&api.Param{
+			Key:         ParamOtelProfilesExporter,
+			Description: "Exporter to use for profiles exporting",
+		},
 	}
 }
 
 func (o *otelProfilesOperator) InstantiateDataOperator(gadgetCtx operators.GadgetContext, instanceParamValues api.ParamValues) (operators.DataOperatorInstance, error) {
-	return &otelProfilesOperatorInstance{
-		o: o,
-	}, nil
+	if len(o.clients) == 0 {
+		return nil, nil
+	}
+
+	mappings, err := apihelpers.GetStringValuesPerDataSource(instanceParamValues[ParamOtelProfilesExporter])
+	if err != nil {
+		return nil, fmt.Errorf("parsing name mappings: %w", err)
+	}
+
+	inst := &otelProfilesOperatorInstance{
+		o:        o,
+		mappings: mappings,
+	}
+
+	return inst, nil
 }
 
 func (o *otelProfilesOperator) Priority() int {
@@ -83,34 +149,48 @@ func (o *otelProfilesOperator) Priority() int {
 }
 
 type otelProfilesOperatorInstance struct {
-	o *otelProfilesOperator
+	o        *otelProfilesOperator
+	mappings map[string]string
 }
 
 func (o *otelProfilesOperatorInstance) Name() string {
-	return "otel-logs"
+	return "otel-profiles"
 }
 
 func (o *otelProfilesOperatorInstance) PreStart(gadgetCtx operators.GadgetContext) error {
-	return nil
-
-}
-
-func (o *otelProfilesOperatorInstance) Start(gadgetCtx operators.GadgetContext) error {
-
 	// subscribe to data sources
 
 	// TODO: move to init to understand if an instance needs to be created?
 	for _, ds := range gadgetCtx.GetDataSources() {
+		exporterName, ok := o.mappings[ds.Name()]
+		if !ok {
+			exporterName, ok = o.mappings[""]
+			if !ok {
+				continue
+			}
+		}
+
 		gadgetCtx.Logger().Infof("Trying to data source %s", ds.Name())
+
+		stackFieldAnn := ds.Annotations()["profiles.stack-field"]
+		if stackFieldAnn == "" {
+			gadgetCtx.Logger().Infof("skipping data source %s: no profiles.stack-field annotation", ds.Name())
+			continue
+		}
+		valueFieldAnn := ds.Annotations()["profiles.value-field"]
+		if valueFieldAnn == "" {
+			gadgetCtx.Logger().Infof("skipping data source %s: no profiles.value-field annotation", ds.Name())
+			continue
+		}
 
 		// TODO: an annotation instead
 
-		kStackField := ds.GetField("kern_stack")
+		kStackField := ds.GetField(stackFieldAnn)
 		if kStackField == nil {
 			continue
 		}
 
-		samplesField := ds.GetField("samples")
+		samplesField := ds.GetField(valueFieldAnn)
 		if samplesField == nil {
 			continue
 		}
@@ -236,9 +316,14 @@ func (o *otelProfilesOperatorInstance) Start(gadgetCtx operators.GadgetContext) 
 
 			req := pprofileotlp.NewExportRequestFromProfiles(profiles)
 
-			var gzipOption = grpc.UseCompressor(gzip.Name)
+			//var gzipOption = grpc.UseCompressor(gzip.Name)
 
-			res, err := o.o.client.Export(gadgetCtx.Context(), req, gzipOption)
+			client, ok := o.o.clients[exporterName]
+			if !ok {
+				return fmt.Errorf("client not found: %q", exporterName)
+			}
+
+			res, err := client.Export(gadgetCtx.Context(), req)
 			if err != nil {
 				gadgetCtx.Logger().Errorf("Exporting profile: %v", err)
 				return err
@@ -260,6 +345,12 @@ func (o *otelProfilesOperatorInstance) Start(gadgetCtx operators.GadgetContext) 
 	}
 
 	return nil
+
+}
+
+func (o *otelProfilesOperatorInstance) Start(gadgetCtx operators.GadgetContext) error {
+
+	return nil
 }
 
 func (o *otelProfilesOperatorInstance) Stop(gadgetCtx operators.GadgetContext) error {
@@ -267,9 +358,6 @@ func (o *otelProfilesOperatorInstance) Stop(gadgetCtx operators.GadgetContext) e
 }
 
 func (o *otelProfilesOperatorInstance) Close(gadgetCtx operators.GadgetContext) error {
-	//if o.o.conn != nil {
-	//	o.o.conn.Close()
-	//}
 	return nil
 }
 
