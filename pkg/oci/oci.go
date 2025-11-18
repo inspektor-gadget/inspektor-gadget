inspektor-gadget/inspektor-gadget

commit 9f93957e461260a30bcabe8f628b9d34986bc258

pkg: oci: Move verification code to VerifyGadgetImage().
Previously, the code to call verification process was in ensureImage(). It is
now moved to a dedicated function.
OCI operator was updated to call this new function after oci.EnsureImage().

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/oci/oci.go
@@ -124,6 +124,38 @@ func PullGadgetImage(ctx context.Context, image string, authOpts *AuthOptions) (
 	return desc, err
 }
 
+func VerifyGadgetImage(ctx context.Context, image string, imgOpts *ImageOptions) error {
+	return retry("VerifyGadgetImage", func() error {
+		if !imgOpts.VerifySignature {
+			log.Warnf("gadget signature verification is disabled due to using corresponding option")
+
+			return nil
+		}
+
+		imageStore, err := newLocalOciStore()
+		if err != nil {
+			return fmt.Errorf("getting oci store: %w", err)
+		}
+
+		imageRef, err := normalizeImageName(image)
+		if err != nil {
+			return fmt.Errorf("normalizing image name: %w", err)
+		}
+
+		repo, err := newRepository(imageRef, &imgOpts.AuthOptions)
+		if err != nil {
+			return fmt.Errorf("creating remote repository: %w", err)
+		}
+
+		err = imgOpts.Verifier.Verify(ctx, repo, imageStore, imageRef)
+		if err != nil {
+			return fmt.Errorf("verifying gadget signature %q: %w", image, err)
+		}
+
+		return nil
+	})
+}
+
 func pullGadgetImage(ctx context.Context, image string, authOpts *AuthOptions) (*GadgetImageDesc, error) {
 	ociStore, err := newLocalOciStore()
 	if err != nil {
@@ -838,27 +870,6 @@ func ensureImage(ctx context.Context, imageStore oras.GraphTarget, image string,
 		if !found {
 			return fmt.Errorf("%s is not part of allowed gadgets: %v", image, strings.Join(imgOpts.AllowedGadgets, ", "))
 		}
-	}
-
-	if !imgOpts.VerifySignature {
-		log.Warnf("gadget signature verification is disabled due to using corresponding option")
-
-		return nil
-	}
-
-	imageRef, err := normalizeImageName(image)
-	if err != nil {
-		return fmt.Errorf("normalizing image name: %w", err)
-	}
-
-	repo, err := newRepository(imageRef, &imgOpts.AuthOptions)
-	if err != nil {
-		return fmt.Errorf("creating remote repository: %w", err)
-	}
-
-	err = imgOpts.Verifier.Verify(ctx, repo, imageStore, imageRef)
-	if err != nil {
-		return fmt.Errorf("verifying gadget signature %q: %w", image, err)
 	}
 
 	return nil

pkg/operators/oci-handler/oci.go
@@ -403,6 +403,11 @@ func (o *OciHandlerInstance) init(gadgetCtx operators.GadgetContext) error {
 		if err != nil {
 			return fmt.Errorf("ensuring image: %w", err)
 		}
+
+		err = oci.VerifyGadgetImage(gadgetCtx.Context(), gadgetCtx.ImageName(), imgOpts)
+		if err != nil {
+			return fmt.Errorf("verifying image: %w", err)
+		}
 	}
 
 	manifest, err := oci.GetManifestForHost(gadgetCtx.Context(), target, gadgetCtx.ImageName())

commit 37308b8f75e5ce14419f2d666745455e7e19561c

pkg: oci: Add nil check for Verifier in VerifyGadgetImage().
Fixes: 9f93957e4612 ("pkg: oci: Move verification code to VerifyGadgetImage().")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/oci/oci.go
@@ -132,6 +132,10 @@ func VerifyGadgetImage(ctx context.Context, image string, imgOpts *ImageOptions)
 			return nil
 		}
 
+		if imgOpts.Verifier == nil {
+			return errors.New("signature verification requested but no verifier provided")
+		}
+
 		imageStore, err := newLocalOciStore()
 		if err != nil {
 			return fmt.Errorf("getting oci store: %w", err)