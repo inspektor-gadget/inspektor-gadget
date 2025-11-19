6359c4637d3f8794d3daa93322fc7b3b68edc4e5
Francis Laniel <flaniel@linux.microsoft.com>
2025-11-19 17:40:47 +0200

    pkg: signature: Add support for cosign bundle format to Exporter.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- pkg/signature/helpers/helpers.go
+++ pkg/signature/helpers/helpers.go
@@ -16,9 +16,17 @@ package helpers
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"reflect"
 	"strings"
+	"unsafe"
 
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"oras.land/oras-go/v2"
 )
 
@@ -54,6 +62,65 @@ func CraftCosignSignatureTag(digest string) (string, error) {
 	}
 
 	return fmt.Sprintf("%s-%s.sig", parts[0], parts[1]), nil
+}
+
+// Taken from:
+// https://github.com/sigstore/cosign/blob/ee3d9fe1c55e/pkg/cosign/bundle/protobundle.go#L36
+const bundleV03MediaType = "application/vnd.dev.sigstore.bundle.v0.3+json"
+
+func FindBundleTag(imageStore oras.ReadOnlyTarget, imageDigest string) (string, error) {
+	reflectedStore := reflect.ValueOf(imageStore).Elem()
+	rootField := reflectedStore.FieldByName("root")
+	imageStoreRootPath := *(*string)(unsafe.Pointer(rootField.UnsafeAddr()))
+	if imageStoreRootPath == "" {
+		return "", errors.New("local image store path cannot be empty")
+	}
+
+	blobsPath := filepath.Join(imageStoreRootPath, "blobs", "sha256")
+	bundleTag := ""
+	err := fs.WalkDir(os.DirFS(blobsPath), ".", func (path string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if entry.IsDir() {
+			return nil
+		}
+
+		manifestBytes, err := os.ReadFile(filepath.Join(blobsPath, path))
+		if err != nil {
+			return fmt.Errorf("reading %q: %w", path, err)
+		}
+
+		manifest := &ocispec.Manifest{}
+		err = json.Unmarshal(manifestBytes, manifest)
+		if err != nil {
+			return fmt.Errorf("decoding manifest: %w", err)
+		}
+
+		if manifest.Subject == nil {
+			return nil
+		}
+
+		if manifest.Subject.Digest.String() != imageDigest {
+			return nil
+		}
+
+		for _, layer := range manifest.Layers {
+			if layer.MediaType == bundleV03MediaType {
+				bundleTag = "sha256:" + entry.Name()
+
+				return fs.SkipAll
+			}
+		}
+
+		return filepath.SkipAll
+	})
+	if err != nil {
+		return "", fmt.Errorf("walking %q: %w", blobsPath, err)
+	}
+
+	return bundleTag, nil
 }
 
 func CopySigningInformation(ctx context.Context, src oras.ReadOnlyTarget, dst oras.Target, digest string, craftSigningInfoTag func(digest string) (string, error)) error {
