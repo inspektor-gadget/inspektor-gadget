inspektor-gadget/inspektor-gadget

commit 509e83be4a85e5faf4990bbd172430254c2aea3e

Adding ptid for socket enricher and defining gadget_ptid type
Signed-off-by: Amit Schendel <amitschendel@gmail.com>
Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

include/gadget/sockets-map.h
@@ -264,6 +264,7 @@ gadget_process_populate_from_socket(const struct gadget_socket_value *skb_val,
 	__builtin_memcpy(p->parent.comm, skb_val->ptask,
 			 sizeof(p->parent.comm));
 	p->parent.pid = skb_val->ppid;
+	p->parent.tid = skb_val->ptid;
 }
 
 #endif

gadgets/fdpass/test/unit/fdpass_test.go
@@ -51,8 +51,10 @@ func TestFdpassGadget(t *testing.T) {
 			generateEvent: generateEvent,
 			validateEvent: func(t *testing.T, info *utils.RunnerInfo, inodeNum uint64, sockfd int, fd int, events []ExpectedFdpassEvent) {
 				utils.ExpectAtLeastOneEvent(func(info *utils.RunnerInfo, fd int) *ExpectedFdpassEvent {
+					proc := info.Proc
+					utils.NormalizeProc(&proc)
 					return &ExpectedFdpassEvent{
-						Proc:      info.Proc,
+						Proc:      proc,
 						SocketIno: inodeNum,
 						Sockfd:    uint32(sockfd),
 						Fd:        uint32(fd),
@@ -85,11 +87,15 @@ func TestFdpassGadget(t *testing.T) {
 				})
 				return nil
 			}
+			normalizeEvent := func(event *ExpectedFdpassEvent) {
+				utils.NormalizeProc(&event.Proc)
+			}
 			opts := gadgetrunner.GadgetRunnerOpts[ExpectedFdpassEvent]{
 				Image:          "fdpass",
 				Timeout:        5 * time.Second,
 				MntnsFilterMap: mntnsFilterMap,
 				OnGadgetRun:    onGadgetRun,
+				NormalizeEvent: normalizeEvent,
 			}
 			gadgetRunner := gadgetrunner.NewGadgetRunner(t, opts)
 

gadgets/snapshot_process/program.bpf.c
@@ -50,7 +50,8 @@ int ig_snap_proc(struct bpf_iter__task *ctx)
 
 	parent = task->real_parent;
 	if (parent) {
-		process.parent.pid = parent->pid;
+		process.parent.pid = parent->tgid;
+		process.parent.tid = parent->pid;
 		__builtin_memcpy(process.parent.comm, parent->comm,
 				 TASK_COMM_LEN);
 	}

gadgets/snapshot_process/test/unit/snapshot_process_test.go
@@ -54,7 +54,8 @@ func TestSnapshotProcessGadget(t *testing.T) {
 						Pid:  uint32(sleepPid),
 						Tid:  uint32(sleepPid),
 						Parent: utils.Parent{
-							Pid:  uint32(info.Tid),
+							Pid:  uint32(info.Pid),
+							Tid:  uint32(info.Tid),
 							Comm: info.Comm,
 						},
 						MntNsID: info.MountNsID,
@@ -85,7 +86,8 @@ func TestSnapshotProcessGadget(t *testing.T) {
 						Pid:  uint32(sleepPid),
 						Tid:  uint32(sleepPid),
 						Parent: utils.Parent{
-							Pid:  uint32(info.Tid),
+							Pid:  uint32(info.Pid),
+							Tid:  uint32(info.Tid),
 							Comm: info.Comm,
 						},
 						MntNsID: info.MountNsID,

gadgets/top_file/test/unit/top_file_test.go
@@ -63,8 +63,10 @@ func TestTopFileGadget(t *testing.T) {
 			},
 			validateEvent: func(t *testing.T, info *utils.RunnerInfo, filepath string, events []ExpectedTopFileEvent) {
 				utils.ExpectAtLeastOneEvent(func(info *utils.RunnerInfo, pid int) *ExpectedTopFileEvent {
+					proc := info.Proc
+					utils.NormalizeProc(&proc)
 					return &ExpectedTopFileEvent{
-						Proc: info.Proc,
+						Proc: proc,
 						T:    "R",
 						File: filepath,
 
@@ -97,6 +99,7 @@ func TestTopFileGadget(t *testing.T) {
 			}
 			normalizeEvent := func(event *ExpectedTopFileEvent) {
 				utils.NormalizeInt(&event.Writes)
+				utils.NormalizeProc(&event.Proc)
 			}
 			onGadgetRun := func(gadgetCtx operators.GadgetContext) error {
 				utils.RunWithRunner(t, runner, func() error {

gadgets/trace_capabilities/test/unit/trace_capabilities_test.go
@@ -175,6 +175,7 @@ func TestNonAuditCapabilities(t *testing.T) {
 				Parent: utils.Parent{
 					Comm: "unit.test",
 					Pid:  uint32(os.Getpid()),
+					Tid:  info.Proc.Tid,
 				},
 			},
 			Cap:           "CAP_SYSLOG",

gadgets/trace_tcp/test/unit/trace_tcp_test.go
@@ -66,8 +66,10 @@ func TestTraceTcpGadget(t *testing.T) {
 			generateEvent: generateConnectEvent,
 			validateEvent: func(t *testing.T, info *utils.RunnerInfo, fd int, _ int, events []ExpectedTraceTcpEvent) error {
 				utils.ExpectAtLeastOneEvent(func(info *utils.RunnerInfo, pid int) *ExpectedTraceTcpEvent {
+					proc := info.Proc
+					utils.NormalizeProc(&proc)
 					return &ExpectedTraceTcpEvent{
-						Proc:    info.Proc,
+						Proc:    proc,
 						Type:    "close",
 						NetNsId: int(info.NetworkNsID),
 						Src: utils.L4Endpoint{
@@ -99,8 +101,10 @@ func TestTraceTcpGadget(t *testing.T) {
 			generateEvent: generateConnectEvent,
 			validateEvent: func(t *testing.T, info *utils.RunnerInfo, fd int, _ int, events []ExpectedTraceTcpEvent) error {
 				utils.ExpectAtLeastOneEvent(func(info *utils.RunnerInfo, pid int) *ExpectedTraceTcpEvent {
+					proc := info.Proc
+					utils.NormalizeProc(&proc)
 					return &ExpectedTraceTcpEvent{
-						Proc:    info.Proc,
+						Proc:    proc,
 						Type:    "connect",
 						NetNsId: int(info.NetworkNsID),
 						Src: utils.L4Endpoint{
@@ -132,8 +136,10 @@ func TestTraceTcpGadget(t *testing.T) {
 			generateEvent: generateConnectEvent,
 			validateEvent: func(t *testing.T, info *utils.RunnerInfo, fd int, _ int, events []ExpectedTraceTcpEvent) error {
 				utils.ExpectAtLeastOneEvent(func(info *utils.RunnerInfo, pid int) *ExpectedTraceTcpEvent {
+					proc := info.Proc
+					utils.NormalizeProc(&proc)
 					return &ExpectedTraceTcpEvent{
-						Proc:    info.Proc,
+						Proc:    proc,
 						Type:    "connect",
 						NetNsId: int(info.NetworkNsID),
 						Src: utils.L4Endpoint{
@@ -163,8 +169,10 @@ func TestTraceTcpGadget(t *testing.T) {
 			generateEvent: generateAcceptConnectEvent,
 			validateEvent: func(t *testing.T, info *utils.RunnerInfo, fd int, acceptFd int, events []ExpectedTraceTcpEvent) error {
 				utils.ExpectAtLeastOneEvent(func(info *utils.RunnerInfo, pid int) *ExpectedTraceTcpEvent {
+					proc := info.Proc
+					utils.NormalizeProc(&proc)
 					return &ExpectedTraceTcpEvent{
-						Proc:    info.Proc,
+						Proc:    proc,
 						Type:    "accept",
 						NetNsId: int(info.NetworkNsID),
 						Src: utils.L4Endpoint{
@@ -199,6 +207,7 @@ func TestTraceTcpGadget(t *testing.T) {
 				} else {
 					utils.NormalizeInt(&event.Src.Port)
 				}
+				utils.NormalizeProc(&event.Proc)
 			}
 			fd := -1
 			acceptFd := -1

include/gadget/common.h
@@ -32,6 +32,7 @@ void static __always_inline gadget_process_populate(struct gadget_process *p)
 	bpf_probe_read_kernel(&p->parent.comm, sizeof(p->parent.comm),
 			      parent->comm);
 	p->parent.pid = BPF_CORE_READ(parent, tgid);
+	p->parent.tid = BPF_CORE_READ(parent, pid);
 }
 
 #endif

commit bca2814419f1c25c8a8b2a57a2255bd420e5fcbc

socket-enricher: Use bpf_core_field_exists() for new fields
Use bpf_core_field_exists() when accessing new fields added to the socket
enricher to ensure they are present and avoid CO-RE failures when running
a newer gadget with an older IG version.

All fields added after aaa2ffd7bebd ("socketenricher: Use CO-RE to make it resilient against changes")
should include this conditional.

Fixes: 509e83be4a85 ("Adding ptid for socket enricher and defining gadget_ptid type")

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

include/gadget/sockets-map.h
@@ -12,6 +12,7 @@
 
 // Necessary for the SEC() definition
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
 #include <gadget/types.h>
 
 // This file is shared between the networking and tracing programs.
@@ -264,7 +265,9 @@ gadget_process_populate_from_socket(const struct gadget_socket_value *skb_val,
 	__builtin_memcpy(p->parent.comm, skb_val->ptask,
 			 sizeof(p->parent.comm));
 	p->parent.pid = skb_val->ppid;
-	p->parent.tid = skb_val->ptid;
+
+	if (bpf_core_field_exists(skb_val->ptid))
+		p->parent.tid = skb_val->ptid;
 }
 
 #endif