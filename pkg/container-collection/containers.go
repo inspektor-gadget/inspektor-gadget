inspektor-gadget/inspektor-gadget

commit 4dedafc8565dd183b2553f97a046dab52a82d515

Fix race condition on enrichers
Different gadgets use the different enricher interfaces to add
container information to the events received from the eBPF programs.
It could happen that by the time the tracer calls the enricher, the
container is gone and then the container information from the event
is missing.

This case is specially relevant when the container is killed after
executing the event, like using a capability not allowed or called
a syscall blocked by seccomp.

This commit fixes this by creating a container cache system: containers
are retained and made available to enrichers for a while after those
are removed from the container collection package, this allows the
enrichers to provide the container data even if the container is gone.

The container collection package is generic enough, hence this behavior
is only enabled when "WithTracerCollection" is used.

pkg/container-collection/containers.go
@@ -65,6 +65,13 @@ type Container struct {
 	PodUID    string            `json:"podUID,omitempty"`
 
 	ownerReference *metav1.OwnerReference
+
+	// We keep an open file descriptor of the containers mount namespace to be sure the kernel
+	// doesn't reuse the inode id before we get rid of this container. This logic avoids a race
+	// condition when the mnt ns inode id is reused by a new container and we erroneously pick
+	// events from it.
+	// This is only used when cachedContainers are enabled through WithTracerCollection().
+	mntNsFd int
 }
 
 type ContainerSelector struct {

examples/gadgets/formatter/trace/exec/exec.go
@@ -64,7 +64,7 @@ func main() {
 	opts := []containercollection.ContainerCollectionOption{
 		// Indicate the callback that will be invoked each time
 		// there is an event
-		containercollection.WithPubSub(tracerCollection.TracerMapsUpdater()),
+		containercollection.WithTracerCollection(tracerCollection),
 
 		// Get containers created with runc
 		containercollection.WithRuncFanotify(),

examples/gadgets/withfilter/trace/exec/exec.go
@@ -63,7 +63,7 @@ func main() {
 	opts := []containercollection.ContainerCollectionOption{
 		// Indicate the callback that will be invoked each time
 		// there is an event
-		containercollection.WithPubSub(tracerCollection.TracerMapsUpdater()),
+		containercollection.WithTracerCollection(tracerCollection),
 
 		// Get containers created with runc
 		containercollection.WithRuncFanotify(),

pkg/container-collection/container-collection.go
@@ -23,8 +23,10 @@ package containercollection
 
 import (
 	"sync"
+	"time"
 
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -38,6 +40,11 @@ type ContainerCollection struct {
 	// Keys:   containerID string
 	// Values: container   Container
 	containers sync.Map
+
+	// Saves containers for "cacheDelay" to be able to enrich events after the container is
+	// removed. This is enabled by using WithTracerCollection().
+	cachedContainers *sync.Map
+	cacheDelay       time.Duration
 
 	// subs contains a list of subscribers of container events
 	pubsub *GadgetPubSub
@@ -127,8 +134,20 @@ func (cc *ContainerCollection) RemoveContainer(id string) {
 		return
 	}
 
+	container := v.(*Container)
+
 	if cc.pubsub != nil {
-		cc.pubsub.Publish(EventTypeRemoveContainer, v.(*Container))
+		cc.pubsub.Publish(EventTypeRemoveContainer, container)
+	}
+
+	// Save the container in the cache as enrichers might need the container some time after it
+	// has been removed.
+	if cc.cachedContainers != nil {
+		cc.cachedContainers.Store(id, v)
+		time.AfterFunc(cc.cacheDelay, func() {
+			cc.cachedContainers.Delete(id)
+			unix.Close(container.mntNsFd)
+		})
 	}
 
 	// Remove the container from the collection after publishing the event as
@@ -173,12 +192,10 @@ func (cc *ContainerCollection) LookupMntnsByContainer(namespace, pod, container 
 	return
 }
 
-// LookupContainerByMntns returns a container by its mount namespace
-// inode id. If not found nil is returned.
-func (cc *ContainerCollection) LookupContainerByMntns(mntnsid uint64) *Container {
+func lookupContainerByMntns(m *sync.Map, mntnsid uint64) *Container {
 	var container *Container
 
-	cc.containers.Range(func(key, value interface{}) bool {
+	m.Range(func(key, value interface{}) bool {
 		c := value.(*Container)
 		if c.Mntns == mntnsid {
 			container = c
@@ -188,6 +205,12 @@ func (cc *ContainerCollection) LookupContainerByMntns(mntnsid uint64) *Container
 		return true
 	})
 	return container
+}
+
+// LookupContainerByMntns returns a container by its mount namespace
+// inode id. If not found nil is returned.
+func (cc *ContainerCollection) LookupContainerByMntns(mntnsid uint64) *Container {
+	return lookupContainerByMntns(&cc.containers, mntnsid)
 }
 
 // LookupContainersByNetns returns a slice of containers that run in a given
@@ -328,7 +351,11 @@ func (cc *ContainerCollection) EnrichNode(event *eventtypes.CommonData) {
 func (cc *ContainerCollection) EnrichByMntNs(event *eventtypes.CommonData, mountnsid uint64) {
 	event.Node = cc.nodeName
 
-	container := cc.LookupContainerByMntns(mountnsid)
+	container := lookupContainerByMntns(&cc.containers, mountnsid)
+	if container == nil && cc.cachedContainers != nil {
+		container = lookupContainerByMntns(cc.cachedContainers, mountnsid)
+	}
+
 	if container != nil {
 		event.Container = container.Name
 		event.Pod = container.Podname

pkg/container-collection/options.go
@@ -20,8 +20,11 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
+	"time"
 
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -639,6 +642,45 @@ func WithOCIConfigEnrichment() ContainerCollectionOption {
 func WithNodeName(nodeName string) ContainerCollectionOption {
 	return func(cc *ContainerCollection) error {
 		cc.nodeName = nodeName
+		return nil
+	}
+}
+
+type TracerCollection interface {
+	TracerMapsUpdater() FuncNotify
+}
+
+// WithTracerCollection enables the interation between the TracerCollection and ContainerCollection
+// packages. When this option is used:
+// - A cache mechanism to keep containers after they are removed is enabled.
+// - The tracer collection TracerMapsUpdater() receives notifications from containers created /
+// removed.
+func WithTracerCollection(tc TracerCollection) ContainerCollectionOption {
+	return func(cc *ContainerCollection) error {
+		// 2 seconds should enough time for the tracer to read the event from the perf ring
+		// buffer and enrich it after the container has been terminated.
+		cc.cacheDelay = 2 * time.Second
+		cc.cachedContainers = &sync.Map{}
+
+		cc.containerEnrichers = append(cc.containerEnrichers, func(container *Container) bool {
+			path := fmt.Sprintf("/proc/%d/ns/mnt", container.Pid)
+			fd, err := unix.Open(path, unix.O_RDONLY|unix.O_CLOEXEC, 0)
+			if err != nil {
+				log.Warnf("WithTracerCollection: failed to open mntns reference for container %s: %s",
+					container.ID, err)
+				return false
+			}
+
+			container.mntNsFd = fd
+			return true
+		})
+
+		if cc.pubsub == nil {
+			cc.pubsub = NewGadgetPubSub()
+		}
+
+		cc.pubsub.Subscribe("tracercollection", tc.TracerMapsUpdater(), nil)
+
 		return nil
 	}
 }

pkg/gadgets/test/gadgets_test.go
@@ -62,7 +62,7 @@ func createTestEnv(
 	t.Cleanup(tc.Close)
 
 	opts := []containercollection.ContainerCollectionOption{
-		containercollection.WithPubSub(tc.TracerMapsUpdater()),
+		containercollection.WithTracerCollection(tc),
 	}
 
 	if err := cc.Initialize(opts...); err != nil {

pkg/gadgettracermanager/gadgettracermanager.go
@@ -235,7 +235,9 @@ func NewServer(conf *Conf) (*GadgetTracerManager, error) {
 		return nil, err
 	}
 
-	containerEventFuncs := []containercollection.FuncNotify{}
+	opts := []containercollection.ContainerCollectionOption{
+		containercollection.WithNodeName(conf.NodeName),
+	}
 
 	if !conf.TestOnly {
 		if err := rlimit.RemoveMemlock(); err != nil {
@@ -247,19 +249,12 @@ func NewServer(conf *Conf) (*GadgetTracerManager, error) {
 			return nil, fmt.Errorf("error creating containers map: %w", err)
 		}
 
-		containerEventFuncs = append(containerEventFuncs, g.containersMap.ContainersMapUpdater())
-		containerEventFuncs = append(containerEventFuncs, g.tracerCollection.TracerMapsUpdater())
-	}
-
-	opts := []containercollection.ContainerCollectionOption{
-		containercollection.WithPubSub(containerEventFuncs...),
-		containercollection.WithNodeName(conf.NodeName),
-	}
-	if !conf.TestOnly {
+		opts = append(opts, containercollection.WithPubSub(g.containersMap.ContainersMapUpdater()))
 		opts = append(opts, containercollection.WithOCIConfigEnrichment())
 		opts = append(opts, containercollection.WithCgroupEnrichment())
 		opts = append(opts, containercollection.WithLinuxNamespaceEnrichment())
 		opts = append(opts, containercollection.WithKubernetesEnrichment(g.nodeName, nil))
+		opts = append(opts, containercollection.WithTracerCollection(g.tracerCollection))
 	}
 
 	podInformerUsed := false

pkg/local-gadget-manager/local-gadget-manager.go
@@ -91,17 +91,15 @@ func NewManager(runtimes []*containerutils.RuntimeConfig) (*LocalGadgetManager, 
 	if err != nil {
 		return nil, fmt.Errorf("error creating containers map: %w", err)
 	}
-	containerEventFuncs := []containercollection.FuncNotify{}
-	containerEventFuncs = append(containerEventFuncs, l.containersMap.ContainersMapUpdater())
-	containerEventFuncs = append(containerEventFuncs, l.tracerCollection.TracerMapsUpdater())
 
 	err = l.ContainerCollection.Initialize(
-		containercollection.WithPubSub(containerEventFuncs...),
+		containercollection.WithPubSub(l.containersMap.ContainersMapUpdater()),
 		containercollection.WithOCIConfigEnrichment(),
 		containercollection.WithCgroupEnrichment(),
 		containercollection.WithLinuxNamespaceEnrichment(),
 		containercollection.WithMultipleContainerRuntimesEnrichment(runtimes),
 		containercollection.WithRuncFanotify(),
+		containercollection.WithTracerCollection(l.tracerCollection),
 	)
 	if err != nil {
 		return nil, err

commit 2a0ac06bf04f0483a787ae1cb4566215cb689bde

containers: Avoid blocking creation of new containers
Inspektor Gadget keeps a reference of the mount namespace few seconds
after a container has been terminated in order to avoid its inode to be
reused by newer containers which causes a race condition when enriching
events [0].

This reference is closed few seconds after the container is terminated,
however, if the container had a nfs mount when terminated, this call
gets blocked (up to 6mins in our testing), which causes the whole
container creation logic to get stuck, i.e. not new containers can be
created on the system.

In order to avoid this issue, this commits moves this logic into an
independent goroutine.

[0]: 4dedafc8565d ("Fix race condition on enrichers")

Fixes: 4dedafc8565d ("Fix race condition on enrichers")

Suggested-by:  Alban Crequy <alban.crequy@gmail.com
Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

pkg/container-collection/containers.go
@@ -79,14 +79,18 @@ type Container struct {
 
 // close releases any resources (like  file descriptors) the container is using.
 func (c *Container) close() {
-	if c.mntNsFd != 0 {
-		unix.Close(c.mntNsFd)
-		c.mntNsFd = 0
-	}
-	if c.netNsFd != 0 {
-		unix.Close(c.netNsFd)
-		c.netNsFd = 0
-	}
+	// we run this in a goroutine to avoid blocking the caller as it can happen
+	// if the container has an NFS mount
+	go func() {
+		if c.mntNsFd != 0 {
+			unix.Close(c.mntNsFd)
+			c.mntNsFd = 0
+		}
+		if c.netNsFd != 0 {
+			unix.Close(c.netNsFd)
+			c.netNsFd = 0
+		}
+	}()
 }
 
 type RuntimeMetadata struct {