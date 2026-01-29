inspektor-gadget/inspektor-gadget

commit da4727389ce12919b1636d84661c40c639466b5d

cmd: Update image build command to actually build the eBPF bytecode
This commit updates the "image build" command to compile the eBPF code
from source instead of taking an already compiled bpf object.

The building process is controlled by a build file (build.yaml by
default), if that's not found the command assumes some defaults, like
the program.bpf.c and definition.yaml names.

Signed-off-by: Mauricio Vásquez <mauriciov@microsoft.com>

cmd/common/image/build.go
@@ -17,57 +17,160 @@ package image
 import (
 	"bytes"
 	"context"
+	_ "embed"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"path/filepath"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/opencontainers/image-spec/specs-go"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content"
 	"oras.land/oras-go/v2/errdef"
 
+	"github.com/inspektor-gadget/inspektor-gadget/cmd/common"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/oci"
 )
 
-type buildOptions struct {
-	progAmd64FilePath  string
-	progArm64FilePath  string
-	definitionFilePath string
-	image              string
+//go:embed Makefile.build
+var makefile []byte
+
+const (
+	DEFAULT_BUILDER_IMAGE = "ghcr.io/inspektor-gadget/inspektor-gadget-ebpf-builder:latest"
+	DEFAULT_EBPF_SOURCE   = "program.bpf.c"
+	DEFAULT_METADATA      = "gadget.yaml"
+	ARCH_AMD64            = "amd64"
+	ARCH_ARM64            = "arm64"
+)
+
+type buildFile struct {
+	EBPFSource string `yaml:"ebpfsource"`
+	Metadata   string `yaml:"metadata"`
+	CFlags     string `yaml:"cflags"`
+	// TODO: custom build script
+	// TODO: author, etc.
+}
+
+type cmdOpts struct {
+	path         string
+	file         string
+	fileChanged  bool
+	image        string
+	local        bool
+	builderImage string
 }
 
 func NewBuildCmd() *cobra.Command {
+	opts := &cmdOpts{}
+
 	cmd := &cobra.Command{
-		Use:          "build IMAGE",
+		Use:          "build PATH",
 		Short:        "Build a gadget image",
 		SilenceUsage: true,
-		RunE:         runBuild,
+		Args:         cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if opts.local && opts.builderImage != DEFAULT_BUILDER_IMAGE {
+				return fmt.Errorf("--local and --builder-image cannot be used at the same time")
+			}
+
+			fFlag := cmd.Flags().Lookup("file")
+			opts.fileChanged = fFlag.Changed
+
+			opts.path = args[0]
+
+			return runBuild(opts)
+		},
 	}
 
-	cmd.Flags().String("prog-amd64", "", "path to the amd64 variant of the eBPF program")
-	cmd.Flags().String("prog-arm64", "", "path to the arm64 variant of the eBPF program")
-	cmd.Flags().String("definition", "", "path to the definition file")
+	cmd.Flags().StringVarP(&opts.file, "file", "f", "build.yaml", "Path to build.yaml")
+	cmd.Flags().BoolVarP(&opts.local, "local", "l", false, "Build using local tools")
+	cmd.Flags().StringVarP(&opts.image, "tag", "t", "", "Name for the built image (format name:tag)")
+	cmd.Flags().StringVar(&opts.builderImage, "builder-image", DEFAULT_BUILDER_IMAGE, "Builder image to use")
 
 	return cmd
 }
 
-func runBuild(cmd *cobra.Command, args []string) error {
-	if len(args) != 1 {
-		return fmt.Errorf("expected exactly one unnamed argument")
-	}
-	o := &buildOptions{
-		progAmd64FilePath:  cmd.Flag("prog-amd64").Value.String(),
-		progArm64FilePath:  cmd.Flag("prog-arm64").Value.String(),
-		definitionFilePath: cmd.Flag("definition").Value.String(),
-		image:              args[0],
+type imageIndexOpts struct {
+	// key is the architecture, value the path
+	progPaths    map[string]string
+	metadataPath string
+	image        string
+}
+
+func runBuild(opts *cmdOpts) error {
+	conf := &buildFile{
+		EBPFSource: DEFAULT_EBPF_SOURCE,
+		Metadata:   DEFAULT_METADATA,
 	}
 
-	if o.progArm64FilePath == "" && o.progAmd64FilePath == "" {
-		return fmt.Errorf("at least one of --prog-amd64 or --prog-arm64 must be specified")
+	var buildContent []byte
+	var err error
+
+	if opts.fileChanged {
+		buildContent, err = os.ReadFile(opts.file)
+		if err != nil {
+			return fmt.Errorf("reading build file: %w", err)
+		}
+	} else {
+		// The user specified the path but not the file. Use the default file build.yaml
+		buildContent, err = os.ReadFile(filepath.Join(opts.path, opts.file))
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("reading build file: %w", err)
+		}
+	}
+
+	if err := yaml.Unmarshal(buildContent, conf); err != nil {
+		return fmt.Errorf("unmarshal build.yaml: %w", err)
+	}
+
+	// make a temp folder to store the build results
+	tmpDir, err := os.MkdirTemp("", "gadget-build-")
+	if err != nil {
+		return fmt.Errorf("create temp dir: %w", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	if opts.path != "." {
+		cwd, err := os.Getwd()
+		if err != nil {
+			return fmt.Errorf("getting current directory: %w", err)
+		}
+		defer os.Chdir(cwd)
+
+		if err := os.Chdir(opts.path); err != nil {
+			return fmt.Errorf("changing directory: %w", err)
+		}
+	}
+
+	if opts.local {
+		if err := buildLocal(opts, conf, tmpDir); err != nil {
+			return err
+		}
+	} else {
+		if err := buildInContainer(opts, conf, tmpDir); err != nil {
+			return err
+		}
+	}
+
+	imageIndexOpts := &imageIndexOpts{
+		image:        opts.image,
+		metadataPath: conf.Metadata,
+		progPaths: map[string]string{
+			ARCH_AMD64: filepath.Join(tmpDir, ARCH_AMD64+".bpf.o"),
+			ARCH_ARM64: filepath.Join(tmpDir, ARCH_ARM64+".bpf.o"),
+		},
 	}
 
 	ociStore, err := oci.GetLocalOciStore()
@@ -75,22 +178,158 @@ func runBuild(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("get oci store: %w", err)
 	}
 
-	indexDesc, err := createImageIndex(ociStore, o)
+	indexDesc, err := createImageIndex(ociStore, imageIndexOpts)
 	if err != nil {
 		return fmt.Errorf("create image index: %w", err)
 	}
 
-	targetImage, err := oci.NormalizeImage(o.image)
-	if err != nil {
-		return fmt.Errorf("normalize image: %w", err)
+	if imageIndexOpts.image != "" {
+		targetImage, err := oci.NormalizeImage(imageIndexOpts.image)
+		if err != nil {
+			return fmt.Errorf("normalize image: %w", err)
+		}
+
+		err = ociStore.Tag(context.TODO(), indexDesc, targetImage)
+		if err != nil {
+			return fmt.Errorf("tag manifest: %w", err)
+		}
+		fmt.Printf("Successfully built %s@%s\n", targetImage, indexDesc.Digest)
+	} else {
+		fmt.Printf("Successfully built %s\n", indexDesc.Digest)
 	}
 
-	err = ociStore.Tag(context.TODO(), indexDesc, targetImage)
-	if err != nil {
-		return fmt.Errorf("tag manifest: %w", err)
+	return nil
+}
+
+func buildLocal(opts *cmdOpts, conf *buildFile, output string) error {
+	makefilePath := filepath.Join(output, "Makefile")
+	if err := os.WriteFile(makefilePath, makefile, 0o644); err != nil {
+		return fmt.Errorf("writing Makefile: %w", err)
 	}
 
-	fmt.Printf("Successfully built %s@%s\n", targetImage, indexDesc.Digest)
+	buildCmd := exec.Command(
+		"make", "-f", makefilePath, "-j", "2",
+		"EBPFSOURCE="+conf.EBPFSource,
+		"OUTPUTDIR="+output,
+		"CFLAGS="+conf.CFlags,
+	)
+	if out, err := buildCmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("build script: %w: %s", err, out)
+	}
+
+	return nil
+}
+
+func buildInContainer(opts *cmdOpts, conf *buildFile, output string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("getting current directory: %w", err)
+	}
+
+	ctx := context.TODO()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return fmt.Errorf("creating docker client: %w", err)
+	}
+	defer cli.Close()
+
+	f := filters.NewArgs()
+	f.Add("reference", opts.builderImage)
+
+	images, err := cli.ImageList(ctx, types.ImageListOptions{Filters: f})
+	if err != nil {
+		return fmt.Errorf("listing images: %w", err)
+	}
+
+	var found bool
+
+	for _, img := range images {
+		for _, tag := range img.RepoTags {
+			if tag == opts.builderImage {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			break
+		}
+	}
+
+	if !found {
+		fmt.Printf("Pulling builder image %s\n", opts.builderImage)
+		reader, err := cli.ImagePull(ctx, opts.builderImage, types.ImagePullOptions{})
+		if err != nil {
+			return fmt.Errorf("pulling builder image: %w", err)
+		}
+		io.Copy(io.Discard, reader)
+		reader.Close()
+	}
+
+	resp, err := cli.ContainerCreate(
+		ctx,
+		&container.Config{
+			Image: opts.builderImage,
+			Cmd: []string{
+				"make", "-f", "/Makefile", "-j", "2",
+				"EBPFSOURCE=" + filepath.Join("/work", conf.EBPFSource),
+				"OUTPUTDIR=/out",
+				"CFLAGS=" + conf.CFlags,
+			},
+		},
+		&container.HostConfig{
+			AutoRemove: true,
+			Mounts: []mount.Mount{
+				{
+					Type:     mount.TypeBind,
+					Target:   "/work",
+					Source:   cwd,
+					ReadOnly: true,
+				},
+				{
+					Type:   mount.TypeBind,
+					Target: "/out",
+					Source: output,
+				},
+			},
+		},
+		nil, nil, "",
+	)
+	if err != nil {
+		return fmt.Errorf("creating builder container: %w", err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return fmt.Errorf("starting builder container: %w", err)
+	}
+
+	var status container.WaitResponse
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			return fmt.Errorf("waiting for builder container: %w", err)
+		}
+	case status = <-statusCh:
+	}
+
+	if status.StatusCode != 0 || common.Verbose {
+		opts := types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true}
+		out, err := cli.ContainerLogs(ctx, resp.ID, opts)
+		if err != nil {
+			return fmt.Errorf("getting builder container logs: %w", err)
+		}
+
+		fmt.Printf("Builder container logs start:\n")
+		stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+		fmt.Printf("Builder container logs end\n")
+	}
+
+	if status.StatusCode != 0 {
+		return fmt.Errorf("builder container exited with status %d", status.StatusCode)
+	}
+
 	return nil
 }
 
@@ -182,22 +421,16 @@ func createManifestForTarget(target oras.Target, definitionFilePath, progFilePat
 	return manifestDesc, nil
 }
 
-func createImageIndex(target oras.Target, o *buildOptions) (ocispec.Descriptor, error) {
+func createImageIndex(target oras.Target, o *imageIndexOpts) (ocispec.Descriptor, error) {
 	// Read the eBPF program files and push them to the memory store
 	layers := []ocispec.Descriptor{}
-	if o.progAmd64FilePath != "" {
-		amd64ManifestDesc, err := createManifestForTarget(target, o.definitionFilePath, o.progAmd64FilePath, "amd64")
+
+	for arch, path := range o.progPaths {
+		manifestDesc, err := createManifestForTarget(target, o.metadataPath, path, arch)
 		if err != nil {
-			return ocispec.Descriptor{}, fmt.Errorf("create amd64 manifest: %w", err)
+			return ocispec.Descriptor{}, fmt.Errorf("creating %s manifest: %w", arch, err)
 		}
-		layers = append(layers, amd64ManifestDesc)
-	}
-	if o.progArm64FilePath != "" {
-		arm64ManifestDesc, err := createManifestForTarget(target, o.definitionFilePath, o.progArm64FilePath, "arm64")
-		if err != nil {
-			return ocispec.Descriptor{}, fmt.Errorf("create arm64 manifest: %w", err)
-		}
-		layers = append(layers, arm64ManifestDesc)
+		layers = append(layers, manifestDesc)
 	}
 
 	// Create the index which combines the architectures and push it to the memory store

cmd/common/image/Makefile.build
@@ -1,0 +1,19 @@
+# This makefile is used by the build command, don't execute it manually
+
+BASECLAGS = -target bpf -Wall -g -O2
+CFLAGS ?=
+OUTPUTDIR ?= /tmp
+EBPFSOURCE ?= program.bpf.c
+
+TARGETS = \
+	$(OUTPUTDIR)/amd64.bpf.o \
+	$(OUTPUTDIR)/arm64.bpf.o \
+	#
+
+.PHONY: all
+all: $(TARGETS)
+
+$(OUTPUTDIR)/%.bpf.o: $(EBPFSOURCE)
+	clang $(BASECLAGS) $(CFLAGS) -D __TARGET_ARCH_$(subst amd64,x86,$*) \
+		-c $< -I /usr/include/gadget/$*/ -o $@
+	llvm-strip -g $@

Dockerfiles/ebpf-builder.Dockerfile
@@ -32,3 +32,7 @@ ENV GOCACHE=/tmp/
 # avoid trouble when running as non root.
 RUN mkdir -m 777 /work
 WORKDIR /work
+
+# Add files used to build containerized gadgets
+ADD include /usr/include
+ADD cmd/common/image/Makefile.build /Makefile

cmd/common/verbose.go
@@ -19,17 +19,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var verbose bool
+var Verbose bool
 
 func checkVerboseFlag() {
-	if verbose {
+	if Verbose {
 		log.SetLevel(log.DebugLevel)
 	}
 }
 
 func AddVerboseFlag(rootCmd *cobra.Command) {
 	rootCmd.PersistentFlags().BoolVarP(
-		&verbose,
+		&Verbose,
 		"verbose", "v",
 		false,
 		"Print debug information",

commit b27e4a1985ae5f1778d2a44efead26062c9bed16

cmd: Remove possibility to customize CFLAGS from build.yaml.
Before this commit, it was possible to customize CFLAGS from a gadget build.yaml
file by using the "cflags" key.
This feature could be abuse to inject shell command in Makefile.build.
For example, having a gadget with the following build.yaml:
ebpfsource: "program.bpf.c"
metadata: "gadget.yaml"
cflags: "; touch poc.txt;"
Would then lead the clang line to be expanded like in the Makefile.build:
$(CLANG) $(BASECFLAGS) $(subst @ARCH@,$*,$(CFLAGS)) -D __TARGET_ARCH_$(subst amd64,x86,$*) \
		-c $< -I /usr/include/gadget/$*/ -o $@
clang -target bpf -Wall -g -O2 -I/usr/include/$(shell uname -m)-linux-gnu; touch poc.txt; -D __TARGET_ARCH_$(subst amd64,x86,$*) \
		-c $< -I /usr/include/gadget/$*/ -o $@
So, the Makefile.build would then execute the nasty command given in the
build.yaml.

As a consequence, this commit removes totally the possibility to customize
CFLAGS from build.yaml. Indeed, since its introduction, this feature was almost
never used.

Fixes: da4727389ce1 ("cmd: Update image build command to actually build the eBPF bytecode")
Reported-by: Nicolò Daprelà <nicolo.daprela@shielder.it>
Reported-by: Pietro Tirenna <pietro.tirenna@shielder.it>
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

cmd/common/image/build.go
@@ -59,7 +59,6 @@ type buildFile struct {
 	EBPFSource string `yaml:"ebpfsource"`
 	Wasm       string `yaml:"wasm"`
 	Metadata   string `yaml:"metadata"`
-	CFlags     string `yaml:"cflags"`
 }
 
 type cmdOpts struct {
@@ -122,12 +121,12 @@ func NewBuildCmd() *cobra.Command {
 
 type buildOptions struct {
 	outputDir         string
-	cFlags            string
 	forceColorsFlag   string
 	ebpfSourcePath    string
 	wasmSourcePath    string
 	btfgen            bool
 	btfHubArchivePath string
+	useInTreeHeaders  string
 }
 
 func buildCmd(options buildOptions) []string {
@@ -135,8 +134,8 @@ func buildCmd(options buildOptions) []string {
 		"make", "-f", filepath.Join(options.outputDir, "Makefile.build"),
 		"-j", fmt.Sprintf("%d", runtime.NumCPU()),
 		"OUTPUTDIR=" + options.outputDir,
-		"CFLAGS=" + options.cFlags,
 		"FORCE_COLORS=" + options.forceColorsFlag,
+		"USE_IN_TREE_HEADERS=" + options.useInTreeHeaders,
 	}
 
 	if options.ebpfSourcePath != "" {
@@ -284,7 +283,6 @@ func runBuild(cmd *cobra.Command, opts *cmdOpts) error {
 				outputDir:         opts.outputDir,
 				ebpfSourcePath:    conf.EBPFSource,
 				wasmSourcePath:    conf.Wasm,
-				cFlags:            conf.CFlags,
 				btfHubArchivePath: opts.btfhubarchive,
 				btfgen:            opts.btfgen,
 			})
@@ -450,6 +448,7 @@ func buildInContainer(opts *cmdOpts, conf *buildFile) error {
 	pathHost := cwd
 
 	inspektorGadetSrcPath := os.Getenv("IG_SOURCE_PATH")
+	useInTreeHeaders := "false"
 	if inspektorGadetSrcPath != "" {
 		pathHost = inspektorGadetSrcPath
 		// find the gadget relative path to the inspektor-gadget source
@@ -460,14 +459,14 @@ func buildInContainer(opts *cmdOpts, conf *buildFile) error {
 		gadgetSourcePath = filepath.Join("/work", gadgetRelativePath)
 
 		// use in-tree headers too
-		conf.CFlags += " -I /work/include/ -I /work/include/gadget/@ARCH@/ "
+		useInTreeHeaders = "true"
 	}
 
 	buildOpts := buildOptions{
 		outputDir:         "/out",
-		cFlags:            conf.CFlags,
 		btfgen:            opts.btfgen,
 		btfHubArchivePath: "/btfhub-archive",
+		useInTreeHeaders:  useInTreeHeaders,
 	}
 
 	if conf.Wasm != "" {
@@ -476,10 +475,6 @@ func buildInContainer(opts *cmdOpts, conf *buildFile) error {
 
 	if conf.EBPFSource != "" {
 		buildOpts.ebpfSourcePath = filepath.Join(gadgetSourcePath, conf.EBPFSource)
-	}
-
-	if cflags, set := os.LookupEnv("CFLAGS"); set {
-		buildOpts.cFlags += " " + cflags
 	}
 
 	if term.IsTerminal(int(os.Stdout.Fd())) && term.IsTerminal(int(os.Stderr.Fd())) {

cmd/common/image/helpers/Makefile.build
@@ -14,14 +14,19 @@ LLVM_STRIP ?= llvm-strip
 # Since Ubuntu does not install it in a standard path, add a compiler flag for
 # it.
 BASECFLAGS = -target bpf -Wall -g -O2 -I/usr/include/$(shell uname -m)-linux-gnu
-CFLAGS ?=
 OUTPUTDIR ?= /tmp
 EBPFSOURCE ?= program.bpf.c
-CARGOFLAGS ?=
+CARGOFLAGS =
+INCLUDEFLAGS =
 FORCE_COLORS ?= false
+USE_IN_TREE_HEADERS ?= false
 
 ifeq ($(FORCE_COLORS), true)
     CARGOFLAGS += --color always
+endif
+
+ifeq ($(USE_IN_TREE_HEADERS), true)
+    INCLUDEFLAGS += -I /work/include/ -I /work/include/gadget/@ARCH@/
 endif
 
 ARCHS = amd64 arm64
@@ -31,7 +36,7 @@ TARGETS = $(foreach ARCH,$(ARCHS),$(OUTPUTDIR)/$(ARCH).bpf.o)
 ebpf: $(TARGETS)
 
 $(OUTPUTDIR)/%.bpf.o: $(EBPFSOURCE)
-	$(CLANG) $(BASECFLAGS) $(subst @ARCH@,$*,$(CFLAGS)) -D __TARGET_ARCH_$(subst amd64,x86,$*) \
+	$(CLANG) $(BASECFLAGS) $(subst @ARCH@,$*,$(INCLUDEFLAGS)) -D __TARGET_ARCH_$(subst amd64,x86,$*) \
 		-c $< -I /usr/include/gadget/$*/ -o $@
 	$(LLVM_STRIP) -g $@
 

gadgets/traceloop/build.yaml
@@ -1,5 +1,1 @@
 wasm: go/program.go
-# To print debug messages:
-# cflags: '-DSHOW_DEBUG'
-# To print error messages:
-# cflags: '-DSHOW_ERROR'

gadgets/traceloop/program.bpf.c
@@ -107,10 +107,8 @@ struct remembered_args {
 #error "Traceloop is not supported on your architecture."
 #endif
 
-/*
- * Add the following in build.yaml to print debug messages:
- *  cflags: '-DSHOW_DEBUG'
- */
+/* Uncomment the following line to show debug messages. */
+/* #define SHOW_DEBUG */
 #if defined(SHOW_DEBUG)
 #define bpf_debug_printk(fmt, ...) bpf_printk(fmt, ##__VA_ARGS__)
 #else /* !defined(SHOW_DEBUG) */
@@ -119,10 +117,8 @@ struct remembered_args {
 	} while (0)
 #endif /* !defined(SHOW_DEBUG) */
 
-/*
- * Add the following in build.yaml to print error messages:
- *  cflags: '-DSHOW_ERROR'
- */
+/* Uncomment the following line to show errors. */
+/* #define SHOW_ERROR */
 #if defined(SHOW_ERROR)
 #define bpf_error_printk(fmt, ...) bpf_printk(fmt, ##__VA_ARGS__)
 #else /* !defined(SHOW_ERROR) */