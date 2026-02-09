7f29c26d6c6b8c0f74907c54cc730d72071e25be
Francis Laniel <flaniel@linux.microsoft.com>
2026-02-09 19:08:12 +0200

    pkg: columns: Escape strings before printing them.
    The columns output did not run any sanitization of the output before printing it
    which could lead to terminal interpreting escape sequence.
    Consider the following C code:
    open("/etc/shadow", O_RDONLY);
    // ...
    open("/etc\x1b[1A/bashrc\x1b[1B\x1b[13C", O_CREAT | O_WRONLY | O_TRUNC, 0644);
    The first open() opens /etc/shadow, while the second open() creates a file which
    names contains shell escape sequence.
    With the current behavior, the trace_open gadget output is modified by the shell
    escape sequence to hide the malicious read to /etc/shadow:
    "RUNTIME.CONTAINER… "COMM"           "PID"      "TID" "TID"      "F… "FNAME"                  "FPATH"                  "ERROR" "MODE"
    ...
    poc-escape-inject   escape_inj…      25641      25641 25591        3 /etc/bashrc                                               ----------
    poc-escape-inject   escape_inj…      25641      25641 25591        0 /etc                                              ENOENT  -rw-r--r--
    
    By using strconv.Quote() on the strings which are about to be outputted, we
    prevent the escape sequence to be interpreted and the output is now correct:
    "RUNTIME.CONTAINER… "COMM"           "PID"      "TID" "TID"      "F… "FNAME"                  "FPATH"                  "ERROR" "MODE"
    ...
    "poc-escape-inject" "escape_in…    "22409"    "22409" "22358"    "3" "/etc/shadow"            ""                       ""      "--------…
    "poc-escape-inject" "escape_in…    "22409"    "22409" "22358"    "0" "/etc\x1b[1A/bashrc\x1b… ""                       "ENOENT "-rw-r--r…
    
    Fixes: GHSA-34r5-6j7w-235f ("Unsanitized ANSI Escape Sequences In `columns` Output Mode")
    Reported-by: Nicolò Daprelà <nicolo.daprela@shielder.it>
    Reported-by: Pietro Tirenna <pietro.tirenna@shielder.it>
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

--- pkg/columns/formatter/textcolumns/output.go
+++ pkg/columns/formatter/textcolumns/output.go
@@ -17,6 +17,7 @@ package textcolumns
 import (
 	"bytes"
 	"io"
+	"strconv"
 	"strings"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/columns"
@@ -36,6 +37,9 @@ func (tf *TextColumnsFormatter[T]) buildFixedString(s string, length int, ellips
 		return ""
 	}
 
+	// Escape the string to avoid the terminal to interpret escape sequence when
+	// printing.
+	s = strconv.Quote(s)
 	if !tf.options.ShouldTruncate {
 		return s
 	}
