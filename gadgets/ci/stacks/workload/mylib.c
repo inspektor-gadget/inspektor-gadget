b1d76d7390651b7880f3ca7fb3a6563491a742da
Alban Crequy <alban.crequy@gmail.com>
2026-02-19 15:17:02 +0100

    otel stacks: add gadget for testing
    make -C gadgets/ ci/stacks
    
    sudo ig run ghcr.io/inspektor-gadget/gadget/ci/stacks:alban_ustack_otel4 --verify-image=false --collect-ustack --symbolizers otel-ebpf-profiler --collect-otel-stack=true --host --comm=pydemo.py -v --trace-pipe
    
    make -C gadgets/ci/stacks/workload all test
    
    Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

---
+++ gadgets/ci/stacks/workload/mylib.c
@@ -0,0 +1,34 @@
+#define _POSIX_C_SOURCE 199309L
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+void print_hello_world(void)
+{
+	printf("Hello, World!\n");
+}
+
+void sleep_one_second(void)
+{
+	sleep(1);
+}
+
+void busy_loop_500ms(void)
+{
+	struct timespec start, current;
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	long elapsed_ms = 0;
+	while (elapsed_ms < 500) {
+		clock_gettime(CLOCK_MONOTONIC, &current);
+		elapsed_ms = (current.tv_sec - start.tv_sec) * 1000 +
+			     (current.tv_nsec - start.tv_nsec) / 1000000;
+	}
+}
+
+void *allocate_memory(size_t size)
+{
+	printf("Calling malloc.\n");
+	return malloc(size);
+}
