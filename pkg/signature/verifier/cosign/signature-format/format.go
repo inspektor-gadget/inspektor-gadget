inspektor-gadget/inspektor-gadget

commit f65d2d78c62bb5f38091c2d2062483cb80ecff3c

pkg: signature: Move checkPayload() to signature-format.
Each signature format defines its own way to check the corresponding payload.
For now, legacy and OCI 1.1 format shares the same implementation.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/signature/verifier/cosign/signature-format/format.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/sigstore/sigstore/pkg/signature/payload"
 	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content"
 )
@@ -30,50 +31,65 @@ import (
 const simpleSigningMediaType = "application/vnd.dev.cosign.simplesigning.v1+json"
 
 type SignatureFormat interface {
+	CheckPayloadImage(payloadBytes []byte, imageDigest string) error
 	CraftSigningInfoTag(imageDigest string) (string, error)
 	FindSignatureTag(ctx context.Context, imageStore oras.GraphTarget, signingInfoTag string) (string, error)
-	LoadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, error)
+	LoadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, []byte, error)
 	Name() string
 }
 
-func loadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, error) {
+func checkPayloadImage(payloadBytes []byte, imageDigest string) error {
+	payloadImage := &payload.SimpleContainerImage{}
+	err := json.Unmarshal(payloadBytes, payloadImage)
+	if err != nil {
+		return fmt.Errorf("unmarshalling payload: %w", err)
+	}
+
+	if payloadImage.Critical.Image.DockerManifestDigest != imageDigest {
+		return fmt.Errorf("payload digest does not correspond to image: expected %s, got %s", imageDigest, payloadImage.Critical.Image.DockerManifestDigest)
+	}
+
+	return nil
+}
+
+func loadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, []byte, error) {
 	_, signatureManifestBytes, err := oras.FetchBytes(ctx, imageStore, signatureTag, oras.DefaultFetchBytesOptions)
 	if err != nil {
-		return nil, nil, fmt.Errorf("getting signature bytes: %w", err)
+		return nil, nil, nil, fmt.Errorf("getting signature bytes: %w", err)
 	}
 
 	signatureManifest := &ocispec.Manifest{}
 	err = json.Unmarshal(signatureManifestBytes, signatureManifest)
 	if err != nil {
-		return nil, nil, fmt.Errorf("decoding signature manifest: %w", err)
+		return nil, nil, nil, fmt.Errorf("decoding signature manifest: %w", err)
 	}
 
 	layers := signatureManifest.Layers
 	expectedLen := 1
 	layersLen := len(layers)
 	if layersLen != expectedLen {
-		return nil, nil, fmt.Errorf("wrong number of signature manifest layers: expected %d, got %d", expectedLen, layersLen)
+		return nil, nil, nil, fmt.Errorf("wrong number of signature manifest layers: expected %d, got %d", expectedLen, layersLen)
 	}
 
 	payloadDescriptor := layers[0]
 	if payloadDescriptor.MediaType != simpleSigningMediaType {
-		return nil, nil, fmt.Errorf("wrong payloadDescriptor media type: expected %s, got %s", simpleSigningMediaType, payloadDescriptor.MediaType)
+		return nil, nil, nil, fmt.Errorf("wrong payloadDescriptor media type: expected %s, got %s", simpleSigningMediaType, payloadDescriptor.MediaType)
 	}
 
 	signature, ok := payloadDescriptor.Annotations["dev.cosignproject.cosign/signature"]
 	if !ok {
-		return nil, nil, fmt.Errorf("no signature in payloadDescriptor")
+		return nil, nil, nil, fmt.Errorf("no signature in payloadDescriptor")
 	}
 
 	signatureBytes, err := base64.StdEncoding.DecodeString(signature)
 	if err != nil {
-		return nil, nil, fmt.Errorf("decoding signature: %w", err)
+		return nil, nil, nil, fmt.Errorf("decoding signature: %w", err)
 	}
 
 	payloadBytes, err := content.FetchAll(ctx, imageStore, payloadDescriptor)
 	if err != nil {
-		return nil, nil, fmt.Errorf("getting payload bytes: %w", err)
+		return nil, nil, nil, fmt.Errorf("getting payload bytes: %w", err)
 	}
 
-	return signatureBytes, payloadBytes, nil
+	return signatureBytes, payloadBytes, payloadBytes, nil
 }

pkg/signature/verifier/cosign/signature-format/legacy.go
@@ -24,6 +24,10 @@ import (
 
 type LegacyFormat struct{}
 
+func (*LegacyFormat) CheckPayloadImage(payloadBytes []byte, imageDigest string) error {
+	return checkPayloadImage(payloadBytes, imageDigest)
+}
+
 func (*LegacyFormat) CraftSigningInfoTag(imageDigest string) (string, error) {
 	return helpers.CraftCosignSignatureTag(imageDigest)
 }
@@ -32,7 +36,7 @@ func (*LegacyFormat) FindSignatureTag(_ context.Context, _ oras.GraphTarget, sig
 	return signingInfoTag, nil
 }
 
-func (*LegacyFormat) LoadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, error) {
+func (*LegacyFormat) LoadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, []byte, error) {
 	return loadSignatureAndPayload(ctx, imageStore, signatureTag)
 }
 

pkg/signature/verifier/cosign/signature-format/oci11.go
@@ -31,6 +31,10 @@ const signatureArtifactType = "application/vnd.dev.cosign.artifact.sig.v1+json"
 
 type OCI11Format struct{}
 
+func (*OCI11Format) CheckPayloadImage(payloadBytes []byte, imageDigest string) error {
+	return checkPayloadImage(payloadBytes, imageDigest)
+}
+
 func (*OCI11Format) CraftSigningInfoTag(imageDigest string) (string, error) {
 	return helpers.CraftSignatureIndexTag(imageDigest)
 }
@@ -56,7 +60,7 @@ func (*OCI11Format) FindSignatureTag(ctx context.Context, imageStore oras.GraphT
 	return "", fmt.Errorf("signature tag not found for index %q", signingInfoTag)
 }
 
-func (*OCI11Format) LoadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, error) {
+func (*OCI11Format) LoadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, []byte, error) {
 	return loadSignatureAndPayload(ctx, imageStore, signatureTag)
 }
 

pkg/signature/verifier/cosign/verifier.go
@@ -19,14 +19,12 @@ import (
 	"context"
 	"crypto"
 	"crypto/x509"
-	"encoding/json"
 	"encoding/pem"
 	"errors"
 	"fmt"
 
 	"github.com/distribution/reference"
 	"github.com/sigstore/sigstore/pkg/signature"
-	"github.com/sigstore/sigstore/pkg/signature/payload"
 	"oras.land/oras-go/v2"
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature/helpers"
@@ -46,7 +44,7 @@ var supportedFormats = []signatureformat.SignatureFormat{
 	&signatureformat.OCI11Format{},
 }
 
-func loadSigningInformation(ctx context.Context, imageDigest string, imageStore oras.GraphTarget) ([]byte, []byte, error) {
+func loadSigningInformation(ctx context.Context, imageDigest string, imageStore oras.GraphTarget) ([]byte, []byte, []byte, signatureformat.SignatureFormat, error) {
 	errs := make([]error, 0)
 	for _, format := range supportedFormats {
 		signingInfoTag, err := format.CraftSigningInfoTag(imageDigest)
@@ -63,17 +61,17 @@ func loadSigningInformation(ctx context.Context, imageDigest string, imageStore 
 			continue
 		}
 
-		signature, payload, err := format.LoadSignatureAndPayload(ctx, imageStore, signatureTag)
+		signature, payload, rawPayload, err := format.LoadSignatureAndPayload(ctx, imageStore, signatureTag)
 		if err != nil {
 			errs = append(errs, fmt.Errorf("loading signature and payload for %s format: %w", format.Name(), err))
 
 			continue
 		}
 
-		return signature, payload, nil
+		return signature, payload, rawPayload, format, nil
 	}
 
-	return nil, nil, errors.Join(errs...)
+	return nil, nil, nil, nil, errors.Join(errs...)
 }
 
 func newVerifier(publicKey []byte) (signature.Verifier, error) {
@@ -95,27 +93,13 @@ func newVerifier(publicKey []byte) (signature.Verifier, error) {
 	return verifier, nil
 }
 
-func checkPayloadImage(payloadBytes []byte, imageDigest string) error {
-	payloadImage := &payload.SimpleContainerImage{}
-	err := json.Unmarshal(payloadBytes, payloadImage)
-	if err != nil {
-		return fmt.Errorf("unmarshalling payload: %w", err)
-	}
-
-	if payloadImage.Critical.Image.DockerManifestDigest != imageDigest {
-		return fmt.Errorf("payload digest does not correspond to image: expected %s, got %s", imageDigest, payloadImage.Critical.Image.DockerManifestDigest)
-	}
-
-	return nil
-}
-
 func (c *Verifier) Verify(ctx context.Context, imageStore oras.GraphTarget, ref reference.Named) error {
 	imageDigest, err := helpers.GetImageDigest(ctx, imageStore, ref.String())
 	if err != nil {
 		return fmt.Errorf("getting image digest: %w", err)
 	}
 
-	signatureBytes, payloadBytes, err := loadSigningInformation(ctx, imageDigest, imageStore)
+	signatureBytes, payloadBytes, rawPayload, format, err := loadSigningInformation(ctx, imageDigest, imageStore)
 	if err != nil {
 		return fmt.Errorf("getting signing information: %w", err)
 	}
@@ -140,7 +124,7 @@ func (c *Verifier) Verify(ctx context.Context, imageStore oras.GraphTarget, ref 
 	// We should not read the payload before confirming it was signed, so let's
 	// do this check once it was confirmed to be signed:
 	// https://github.com/containers/image/blob/main/docs/containers-signature.5.md#the-cryptographic-signature
-	err = checkPayloadImage(payloadBytes, imageDigest)
+	err = format.CheckPayloadImage(rawPayload, imageDigest)
 	if err != nil {
 		return fmt.Errorf("checking payload image: %w", err)
 	}

commit 1e0e96efea7b592ac329882ff88bec8cae85a714

pkg: signature: Check cosign critical type for legacy and OCI 1.1 format.
Fixes: f65d2d78c62b ("pkg: signature: Move checkPayload() to signature-format.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/signature/verifier/cosign/signature-format/format.go
@@ -26,9 +26,15 @@ import (
 	"oras.land/oras-go/v2/content"
 )
 
-// Taken from:
-// https://github.com/sigstore/cosign/blob/45bda40b8ef4/pkg/types/media.go#L28
-const simpleSigningMediaType = "application/vnd.dev.cosign.simplesigning.v1+json"
+const (
+	// Taken from:
+	// https://github.com/sigstore/cosign/blob/45bda40b8ef4/pkg/types/media.go#L28
+	simpleSigningMediaType = "application/vnd.dev.cosign.simplesigning.v1+json"
+
+	// Taken from:
+	// https://github.com/sigstore/sigstore/blob/632c6fe358b6/pkg/signature/payload/payload.go#L27
+	cosignSignatureType = "cosign container image signature"
+)
 
 type SignatureFormat interface {
 	CheckPayloadImage(payloadBytes []byte, imageDigest string) error
@@ -45,8 +51,13 @@ func checkPayloadImage(payloadBytes []byte, imageDigest string) error {
 		return fmt.Errorf("unmarshalling payload: %w", err)
 	}
 
-	if payloadImage.Critical.Image.DockerManifestDigest != imageDigest {
-		return fmt.Errorf("payload digest does not correspond to image: expected %s, got %s", imageDigest, payloadImage.Critical.Image.DockerManifestDigest)
+	critical := payloadImage.Critical
+	if critical.Type != cosignSignatureType {
+		return fmt.Errorf("unexpected critical type: expected %q, got %q", cosignSignatureType, critical.Type)
+	}
+
+	if critical.Image.DockerManifestDigest != imageDigest {
+		return fmt.Errorf("payload digest does not correspond to image: expected %s, got %s", imageDigest, critical.Image.DockerManifestDigest)
 	}
 
 	return nil