inspektor-gadget/inspektor-gadget

commit 01cf64de8fbed7fd5d616e5a9695b65b02fd40aa

pkg: signature: Add support for cosign bundle format to Verifier.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/signature/verifier/cosign/signature-format/bundle.go
@@ -1,0 +1,121 @@
+// Copyright 2025 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package signatureformat
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	intoto "github.com/in-toto/in-toto-golang/in_toto"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/secure-systems-lab/go-securesystemslib/dsse"
+	sigstorebundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
+	"google.golang.org/protobuf/encoding/protojson"
+	"oras.land/oras-go/v2"
+
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature/helpers"
+)
+
+type BundleFormat struct{}
+
+func (*BundleFormat) CheckPayloadImage(payloadBytes []byte, imageDigest string) error {
+	var statement intoto.Statement
+	err := json.Unmarshal(payloadBytes, &statement)
+	if err != nil {
+		return fmt.Errorf("unmarshalling payload: %w", err)
+	}
+
+	if len(statement.Subject) == 0 {
+		return fmt.Errorf("payload has no subject: %v", statement)
+	}
+
+	for algorithm, hash := range statement.Subject[0].Digest {
+		digest := fmt.Sprintf("%s:%s", algorithm, hash)
+		if digest == imageDigest {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("payload digest does not correspond to image: expected %s", imageDigest)
+}
+
+func (*BundleFormat) CraftSigningInfoTag(imageDigest string) (string, error) {
+	// The bundle format uses the image digest to find referrers in the registry,
+	// not a tag.
+	// Return the imageDigest here, since the logic for finding the signature tag
+	// is handled in FindSignatureTag().
+	return imageDigest, nil
+}
+
+func (*BundleFormat) FindSignatureTag(ctx context.Context, imageStore oras.GraphTarget, imageDigest string) (string, error) {
+	signingInfoTag, err := helpers.FindBundleTag(ctx, imageStore, imageDigest)
+	if err != nil {
+		return "", fmt.Errorf("finding bundle tag for %q: %w", imageDigest, err)
+	}
+
+	_, manifestBytes, err := oras.FetchBytes(ctx, imageStore, signingInfoTag, oras.DefaultFetchBytesOptions)
+	if err != nil {
+		return "", fmt.Errorf("getting manifest bytes: %w", err)
+	}
+
+	manifest := &ocispec.Manifest{}
+	err = json.Unmarshal(manifestBytes, manifest)
+	if err != nil {
+		return "", fmt.Errorf("decoding manifest: %w", err)
+	}
+
+	for _, layer := range manifest.Layers {
+		if layer.MediaType == helpers.BundleV03MediaType {
+			return layer.Digest.String(), nil
+		}
+	}
+
+	return "", fmt.Errorf("signature tag not found for index %q", signingInfoTag)
+}
+
+func (*BundleFormat) LoadSignatureAndPayload(ctx context.Context, imageStore oras.GraphTarget, signatureTag string) ([]byte, []byte, []byte, error) {
+	_, bundleBytes, err := oras.FetchBytes(ctx, imageStore, signatureTag, oras.DefaultFetchBytesOptions)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("getting bundle bytes: %w", err)
+	}
+
+	bundle := &sigstorebundle.Bundle{}
+	err = protojson.Unmarshal(bundleBytes, bundle)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("decoding bundle: %w", err)
+	}
+
+	envelope := bundle.GetDsseEnvelope()
+	if envelope == nil {
+		return nil, nil, nil, errors.New("DSSE envelope not found in bundle")
+	}
+
+	signatures := envelope.GetSignatures()
+	expectedLen := 1
+	signaturesLen := len(signatures)
+	if signaturesLen != expectedLen {
+		return nil, nil, nil, fmt.Errorf("wrong number of signatures: expected %d, got %d", expectedLen, signaturesLen)
+	}
+
+	rawPayload := envelope.GetPayload()
+
+	return signatures[0].Sig, dsse.PAE(envelope.GetPayloadType(), rawPayload), rawPayload, nil
+}
+
+func (*BundleFormat) Name() string {
+	return "bundle"
+}

examples/go.mod
@@ -74,6 +74,7 @@ require (
 	github.com/grafana/regexp v0.0.0-20240518133315-a468a5bfb3bc // indirect
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.2 // indirect
+	github.com/in-toto/in-toto-golang v0.9.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/josharian/native v1.1.0 // indirect
@@ -119,6 +120,7 @@ require (
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/secure-systems-lab/go-securesystemslib v0.9.0 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
+	github.com/shibumi/go-pathspec v1.3.0 // indirect
 	github.com/sigstore/protobuf-specs v0.4.1 // indirect
 	github.com/sigstore/sigstore v1.9.5 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect

examples/go.sum
@@ -36,6 +36,8 @@ github.com/client9/misspell v0.3.4/go.mod h1:qj6jICC3Q7zFZvVWo7KLAzC3yx5G7kyvSDk
 github.com/cloudflare/cbpfc v0.0.0-20240920015331-ff978e94500b h1:EgR1t4Lnq6uP6QxJQ+oIFtENOHUY3/7gMOE76vL0KcA=
 github.com/cloudflare/cbpfc v0.0.0-20240920015331-ff978e94500b/go.mod h1:X/9cHz8JVzKlvoZyKBgMgrogKZlLf+pWjmm5gSUm5dI=
 github.com/cncf/udpa/go v0.0.0-20191209042840-269d4d468f6f/go.mod h1:M8M6+tZqaGXZJjfX53e64911xZQV5JYwmTeXPW+k8Sc=
+github.com/codahale/rfc6979 v0.0.0-20141003034818-6a90f24967eb h1:EDmT6Q9Zs+SbUoc7Ik9EfrFqcylYqgPZ9ANSbTAntnE=
+github.com/codahale/rfc6979 v0.0.0-20141003034818-6a90f24967eb/go.mod h1:ZjrT6AXHbDs86ZSdt/osfBi5qfexBrKUdONk989Wnk4=
 github.com/containerd/cgroups/v3 v3.0.5 h1:44na7Ud+VwyE7LIoJ8JTNQOa549a8543BmzaJHo6Bzo=
 github.com/containerd/cgroups/v3 v3.0.5/go.mod h1:SA5DLYnXO8pTGYiAHXz94qvLQTKfVM5GEVisn4jpins=
 github.com/containerd/containerd v1.7.29 h1:90fWABQsaN9mJhGkoVnuzEY+o1XDPbg9BTC9QTAHnuE=
@@ -192,6 +194,8 @@ github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.2/go.mod h1:pkJQ2tZHJ0aFOVEEot6o
 github.com/hashicorp/go-uuid v1.0.2/go.mod h1:6SBZvOh/SIDV7/2o3Jml5SYk/TvGqwFJ/bN7x4byOro=
 github.com/hashicorp/go-uuid v1.0.3 h1:2gKiV6YVmrJ1i2CKKa9obLvRieoRGviZFL26PcT/Co8=
 github.com/hashicorp/go-uuid v1.0.3/go.mod h1:6SBZvOh/SIDV7/2o3Jml5SYk/TvGqwFJ/bN7x4byOro=
+github.com/in-toto/in-toto-golang v0.9.0 h1:tHny7ac4KgtsfrG6ybU8gVOZux2H8jN05AXJ9EBM1XU=
+github.com/in-toto/in-toto-golang v0.9.0/go.mod h1:xsBVrVsHNsB61++S6Dy2vWosKhuA3lUTQd+eF9HdeMo=
 github.com/inconshreveable/mousetrap v1.1.0 h1:wN+x4NVGpMsO7ErUn/mUI3vEoE6Jt13X2s0bqwp9tc8=
 github.com/inconshreveable/mousetrap v1.1.0/go.mod h1:vpF70FUmC8bwa3OWnCshd2FqLfsEA9PFc4w1p2J65bw=
 github.com/jcmturner/aescts/v2 v2.0.0 h1:9YKLH6ey7H4eDBXW8khjYslgyqG2xZikXP0EQFKrle8=
@@ -354,6 +358,8 @@ github.com/secure-systems-lab/go-securesystemslib v0.9.0 h1:rf1HIbL64nUpEIZnjLZ3
 github.com/secure-systems-lab/go-securesystemslib v0.9.0/go.mod h1:DVHKMcZ+V4/woA/peqr+L0joiRXbPpQ042GgJckkFgw=
 github.com/sergi/go-diff v1.3.1 h1:xkr+Oxo4BOQKmkn/B9eMK0g5Kg/983T9DqqPHwYqD+8=
 github.com/sergi/go-diff v1.3.1/go.mod h1:aMJSSKb2lpPvRNec0+w3fl7LP9IOFzdc9Pa4NFbPK1I=
+github.com/shibumi/go-pathspec v1.3.0 h1:QUyMZhFo0Md5B8zV8x2tesohbb5kfbpTi9rBnKh5dkI=
+github.com/shibumi/go-pathspec v1.3.0/go.mod h1:Xutfslp817l2I1cZvgcfeMQJG5QnU2lh5tVaaMCl3jE=
 github.com/sigstore/protobuf-specs v0.4.1 h1:5SsMqZbdkcO/DNHudaxuCUEjj6x29tS2Xby1BxGU7Zc=
 github.com/sigstore/protobuf-specs v0.4.1/go.mod h1:+gXR+38nIa2oEupqDdzg4qSBT0Os+sP7oYv6alWewWc=
 github.com/sigstore/sigstore v1.9.5 h1:Wm1LT9yF4LhQdEMy5A2JeGRHTrAWGjT3ubE5JUSrGVU=

go.mod
@@ -168,8 +168,8 @@ require (
 	github.com/prometheus/procfs v0.17.0 // indirect
 	github.com/rogpeppe/go-internal v1.13.2-0.20241226121412-a5dc8ff20d0a // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
-	github.com/secure-systems-lab/go-securesystemslib v0.9.0 // indirect
-	github.com/sigstore/protobuf-specs v0.4.1 // indirect
+	github.com/secure-systems-lab/go-securesystemslib v0.9.0
+	github.com/sigstore/protobuf-specs v0.4.1
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
@@ -203,6 +203,8 @@ require (
 	sigs.k8s.io/release-utils v0.11.1 // indirect
 )
 
+require github.com/in-toto/in-toto-golang v0.9.0
+
 require (
 	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
 	github.com/go-asn1-ber/asn1-ber v1.5.7 // indirect
@@ -213,6 +215,7 @@ require (
 	github.com/notaryproject/notation-plugin-framework-go v1.0.0 // indirect
 	github.com/notaryproject/tspclient-go v1.0.0 // indirect
 	github.com/prometheus/otlptranslator v0.0.2 // indirect
+	github.com/shibumi/go-pathspec v1.3.0 // indirect
 	github.com/veraison/go-cose v1.3.0 // indirect
 	golang.org/x/mod v0.29.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v6 v6.3.0 // indirect

go.sum
@@ -45,6 +45,8 @@ github.com/client9/misspell v0.3.4/go.mod h1:qj6jICC3Q7zFZvVWo7KLAzC3yx5G7kyvSDk
 github.com/cloudflare/cbpfc v0.0.0-20240920015331-ff978e94500b h1:EgR1t4Lnq6uP6QxJQ+oIFtENOHUY3/7gMOE76vL0KcA=
 github.com/cloudflare/cbpfc v0.0.0-20240920015331-ff978e94500b/go.mod h1:X/9cHz8JVzKlvoZyKBgMgrogKZlLf+pWjmm5gSUm5dI=
 github.com/cncf/udpa/go v0.0.0-20191209042840-269d4d468f6f/go.mod h1:M8M6+tZqaGXZJjfX53e64911xZQV5JYwmTeXPW+k8Sc=
+github.com/codahale/rfc6979 v0.0.0-20141003034818-6a90f24967eb h1:EDmT6Q9Zs+SbUoc7Ik9EfrFqcylYqgPZ9ANSbTAntnE=
+github.com/codahale/rfc6979 v0.0.0-20141003034818-6a90f24967eb/go.mod h1:ZjrT6AXHbDs86ZSdt/osfBi5qfexBrKUdONk989Wnk4=
 github.com/containerd/cgroups/v3 v3.0.5 h1:44na7Ud+VwyE7LIoJ8JTNQOa549a8543BmzaJHo6Bzo=
 github.com/containerd/cgroups/v3 v3.0.5/go.mod h1:SA5DLYnXO8pTGYiAHXz94qvLQTKfVM5GEVisn4jpins=
 github.com/containerd/containerd v1.7.29 h1:90fWABQsaN9mJhGkoVnuzEY+o1XDPbg9BTC9QTAHnuE=
@@ -229,6 +231,8 @@ github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.2/go.mod h1:pkJQ2tZHJ0aFOVEEot6o
 github.com/hashicorp/go-uuid v1.0.2/go.mod h1:6SBZvOh/SIDV7/2o3Jml5SYk/TvGqwFJ/bN7x4byOro=
 github.com/hashicorp/go-uuid v1.0.3 h1:2gKiV6YVmrJ1i2CKKa9obLvRieoRGviZFL26PcT/Co8=
 github.com/hashicorp/go-uuid v1.0.3/go.mod h1:6SBZvOh/SIDV7/2o3Jml5SYk/TvGqwFJ/bN7x4byOro=
+github.com/in-toto/in-toto-golang v0.9.0 h1:tHny7ac4KgtsfrG6ybU8gVOZux2H8jN05AXJ9EBM1XU=
+github.com/in-toto/in-toto-golang v0.9.0/go.mod h1:xsBVrVsHNsB61++S6Dy2vWosKhuA3lUTQd+eF9HdeMo=
 github.com/inconshreveable/mousetrap v1.1.0 h1:wN+x4NVGpMsO7ErUn/mUI3vEoE6Jt13X2s0bqwp9tc8=
 github.com/inconshreveable/mousetrap v1.1.0/go.mod h1:vpF70FUmC8bwa3OWnCshd2FqLfsEA9PFc4w1p2J65bw=
 github.com/jcmturner/aescts/v2 v2.0.0 h1:9YKLH6ey7H4eDBXW8khjYslgyqG2xZikXP0EQFKrle8=
@@ -405,6 +409,8 @@ github.com/secure-systems-lab/go-securesystemslib v0.9.0 h1:rf1HIbL64nUpEIZnjLZ3
 github.com/secure-systems-lab/go-securesystemslib v0.9.0/go.mod h1:DVHKMcZ+V4/woA/peqr+L0joiRXbPpQ042GgJckkFgw=
 github.com/sergi/go-diff v1.3.1 h1:xkr+Oxo4BOQKmkn/B9eMK0g5Kg/983T9DqqPHwYqD+8=
 github.com/sergi/go-diff v1.3.1/go.mod h1:aMJSSKb2lpPvRNec0+w3fl7LP9IOFzdc9Pa4NFbPK1I=
+github.com/shibumi/go-pathspec v1.3.0 h1:QUyMZhFo0Md5B8zV8x2tesohbb5kfbpTi9rBnKh5dkI=
+github.com/shibumi/go-pathspec v1.3.0/go.mod h1:Xutfslp817l2I1cZvgcfeMQJG5QnU2lh5tVaaMCl3jE=
 github.com/sigstore/protobuf-specs v0.4.1 h1:5SsMqZbdkcO/DNHudaxuCUEjj6x29tS2Xby1BxGU7Zc=
 github.com/sigstore/protobuf-specs v0.4.1/go.mod h1:+gXR+38nIa2oEupqDdzg4qSBT0Os+sP7oYv6alWewWc=
 github.com/sigstore/sigstore v1.9.5 h1:Wm1LT9yF4LhQdEMy5A2JeGRHTrAWGjT3ubE5JUSrGVU=

pkg/signature/helpers/helpers.go
@@ -16,10 +16,12 @@ package helpers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 
 	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/registry"
 )
 
 func GetImageDigest(ctx context.Context, store oras.Target, imageRef string) (string, error) {
@@ -54,6 +56,32 @@ func CraftCosignSignatureTag(digest string) (string, error) {
 	}
 
 	return fmt.Sprintf("%s-%s.sig", parts[0], parts[1]), nil
+}
+
+const BundleV03MediaType = "application/vnd.dev.sigstore.bundle.v0.3+json" // https://github.com/sigstore/cosign/blob/ee3d9fe1c55e/pkg/cosign/bundle/protobundle.go#L36
+
+func FindBundleTag(ctx context.Context, imageStore oras.ReadOnlyGraphTarget, imageDigest string) (string, error) {
+	desc, err := imageStore.Resolve(ctx, imageDigest)
+	if err != nil {
+		return "", fmt.Errorf("resolving %s: %w", imageDigest, err)
+	}
+
+	descriptors, err := registry.Referrers(ctx, imageStore, desc, BundleV03MediaType)
+	if err != nil {
+		return "", fmt.Errorf("searching for bundle referring %q: %w", imageDigest, err)
+	}
+
+	if len(descriptors) == 0 {
+		return "", errors.New("no bundle found")
+	}
+
+	if len(descriptors) > 1 {
+		return "", errors.New("images with several bundles are not supported")
+	}
+
+	signingInfoTag := descriptors[0].Digest.String()
+
+	return signingInfoTag, nil
 }
 
 func CopySigningInformation(ctx context.Context, src oras.ReadOnlyGraphTarget, dst oras.Target, digest string, craftSigningInfoTag func(digest string) (string, error)) error {

pkg/signature/testdata/oci-store/blobs/sha256/3dd951802bf189790e9a5cff9b269fd295896dbd3938f5518ec8f2e93c05fb57
@@ -1,0 +1,1 @@
+{"schemaVersion":2,"mediaType":"application/vnd.oci.image.manifest.v1+json","config":{"mediaType":"application/vnd.oci.empty.v1+json","size":2,"digest":"sha256:44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a","artifactType":"application/vnd.dev.sigstore.bundle.v0.3+json"},"layers":[{"mediaType":"application/vnd.dev.sigstore.bundle.v0.3+json","size":4107,"digest":"sha256:47b7ebef46fb8de5bdd1d911867e916c421871b9ebc694c31cbe3e8a74ee7a33"}],"annotations":{"dev.sigstore.bundle.content":"dsse-envelope","dev.sigstore.bundle.predicateType":"https://sigstore.dev/cosign/sign/v1","org.opencontainers.image.created":"2025-10-29T16:28:44Z"},"subject":{"mediaType":"application/vnd.oci.image.index.v1+json","size":1586,"digest":"sha256:d217fb2d81d4314d48ade41ad3cad13324861d296e4e786e6498f3123951709c"},"artifactType":"application/vnd.dev.sigstore.bundle.v0.3+json"}

pkg/signature/testdata/oci-store/blobs/sha256/47b7ebef46fb8de5bdd1d911867e916c421871b9ebc694c31cbe3e8a74ee7a33
@@ -1,0 +1,1 @@
+{"mediaType":"application/vnd.dev.sigstore.bundle.v0.3+json","verificationMaterial":{"publicKey":{"hint":"fnjDrI7ue5FV5s2ImCVduM26jN1pF7a4FBRNcNjNhr8="},"tlogEntries":[{"logIndex":"652716871","logId":{"keyId":"wNI9atQGlz+VWfO6LRygH4QUfY/8W4RFwiT5i5WRgB0="},"kindVersion":{"kind":"dsse","version":"0.0.1"},"integratedTime":"1761755324","inclusionPromise":{"signedEntryTimestamp":"MEUCIQC1OO/vQ6ifWRqKi3xZb2Why2OB/H6LDzXd0wVp2LHANQIgVHSxRLSePXzzdHuVBArvh/mNAA7UnT5ZKfMm9ZXYrgA="},"inclusionProof":{"logIndex":"530812609","rootHash":"mxMMaoxYbscO2nh4bD0ynyOPCuufw1JigTBww/P3VTo=","treeSize":"530812616","hashes":["7m4eMTHdDv+gdFlPYU+vQZ9wcVi6FNduLf4Pt2QNKLI=","7bhqtZm6YhCDApj+3wOcWvUv+SRVjPcu7KadLYOHIqw=","7+gr4jKQjEBh4x0jXwBiiZXstJsjJgLRsKQy5UfxaPY=","IKQoVHEBE+kQeDWvN5HBizDwpJE66WNo9aJpyP5N0Bg=","RDlD40MtehnTtxxLPte6/+mDHKO8tgDHK6N7RPD7XqA=","52eV0UdK440VIu1FLn9h9cIMHngXpOb9ParJWaXwo34=","fZNSdyXYYBNs8PyiiOspRIuQufgScDeSoskfUhi64yU=","njV1iETHZ4DgWH0M+q2CLqdW7KnOMVtX0oMgwO33PLc=","aqMIsDFyRTOVMVGgC5AGC0rQJ3+mSny8+ooR2/6mNHQ=","ODscaZxYtgjErfV5oikj2of9lr/MWSEACugWg3EFU0w=","YgGj/jF+tz7lVramF707BAIoPsK/AjlGlpZey3MpDf0=","3UOCg8LnUB9cs9wIVvoXiO1dyxJ9L5+xA6EA52QZQ2I=","/ZlzrGPS0eq2rjgtegMbCBBRoQ9PIxVWEx+ZymODyyw=","oUpQPKjwnG3jhQtgDGLtyjyTJmrqoENa8C6gx+qWIls=","HAOTg+Fg3H1Ej24mw+P9lXfi+4mPL5EKwAd4aNQOV6M=","2Wv4GiithwNukRKV06clevnQQYCzXmSS/+/OJtXgsXQ=","1mfy94KpcItqshH9+gwqV6jccupcaMpVsF28New8zDY=","vS7O4ozHIQZJWBiov+mkpI27GE8zAmVCEkRcP3NDyNE="],"checkpoint":{"envelope":"rekor.sigstore.dev - 1193050959916656506\n530812616\nmxMMaoxYbscO2nh4bD0ynyOPCuufw1JigTBww/P3VTo=\n\n— rekor.sigstore.dev wNI9ajBEAiB67hCEk3iQSsvIuk2+KBe2CQ2bqSpR688AKpiyYrQktQIgEQDG2bjsSQr5EJL2z0MdcBNyXksoqC2Qzy4BnM1tiDk=\n"}},"canonicalizedBody":"eyJhcGlWZXJzaW9uIjoiMC4wLjEiLCJraW5kIjoiZHNzZSIsInNwZWMiOnsiZW52ZWxvcGVIYXNoIjp7ImFsZ29yaXRobSI6InNoYTI1NiIsInZhbHVlIjoiMTYxOGNkZTU2YTVlYjdkZDlkMzQ1NDEwNTQyMzJhNTg3MGFjNzBjOWI3NzFmZjkxZjE4ZjlkZmNlMTA4Y2U5YiJ9LCJwYXlsb2FkSGFzaCI6eyJhbGdvcml0aG0iOiJzaGEyNTYiLCJ2YWx1ZSI6ImNlNTlkMGM4ZmRiOWNmMjdkNjRhZTBiZmYxZWFhNzE4NTYwYWRmZTI3NGY5MWRlYWQwYmU3ZWUyYzBkMTQyZTEifSwic2lnbmF0dXJlcyI6W3sic2lnbmF0dXJlIjoiTUVZQ0lRREwxcmIzUld0Nmg3NEZKR3BkU2JXVWI1aHBBNFJWeWVmeE9oZm1NS0xKOGdJaEFOSVBLb1VnSGFDeDdvZlBoTXprY2ZjTHJ6bHIrNFVzeEJGUXdIeDZnd3BTIiwidmVyaWZpZXIiOiJMUzB0TFMxQ1JVZEpUaUJRVlVKTVNVTWdTMFZaTFMwdExTMEtUVVpyZDBWM1dVaExiMXBKZW1vd1EwRlJXVWxMYjFwSmVtb3dSRUZSWTBSUlowRkZialoxT0dSTWRqaG5ibEJIUmtWdlFXVmxVbGhhT1hJeFVWVnhkUXAyZUhad2JrSk9TQ3RIZDJWdWRERlBNRWxwYzNsRFpVVlpSV1ZIUVU5V1kyMXhRMHhHZVhkdlJqWXlRMVZOV2tsbGVDOVlkelUyYm1aM1BUMEtMUzB0TFMxRlRrUWdVRlZDVEVsRElFdEZXUzB0TFMwdENnPT0ifV19fQ=="}],"timestampVerificationData":{"rfc3161Timestamps":[{"signedTimestamp":"MIICyzADAgEAMIICwgYJKoZIhvcNAQcCoIICszCCAq8CAQMxDTALBglghkgBZQMEAgEwgbgGCyqGSIb3DQEJEAEEoIGoBIGlMIGiAgEBBgkrBgEEAYO/MAIwMTANBglghkgBZQMEAgEFAAQgZdRIKJbonjYUAbCUQIOss9fOUYoktSv+2rc+z7znGvACFQDriorRl8j9O+3JK/OjpquzpU1zGBgPMjAyNTEwMjkxNjI4NDRaMAMCAQGgMqQwMC4xFTATBgNVBAoTDHNpZ3N0b3JlLmRldjEVMBMGA1UEAxMMc2lnc3RvcmUtdHNhoAAxggHcMIIB2AIBATBRMDkxFTATBgNVBAoTDHNpZ3N0b3JlLmRldjEgMB4GA1UEAxMXc2lnc3RvcmUtdHNhLXNlbGZzaWduZWQCFDoTVC8MkGHuvMFDL8uKjosqI4sMMAsGCWCGSAFlAwQCAaCB/DAaBgkqhkiG9w0BCQMxDQYLKoZIhvcNAQkQAQQwHAYJKoZIhvcNAQkFMQ8XDTI1MTAyOTE2Mjg0NFowLwYJKoZIhvcNAQkEMSIEINnkHFRoKZ+7PyM0POOnBaOuZj+9sShGB6EurglTHXJGMIGOBgsqhkiG9w0BCRACLzF/MH0wezB5BCCF+Se8B6tiysO0Q1bBDvyBssaIP9p6uebYcNnROs0FtzBVMD2kOzA5MRUwEwYDVQQKEwxzaWdzdG9yZS5kZXYxIDAeBgNVBAMTF3NpZ3N0b3JlLXRzYS1zZWxmc2lnbmVkAhQ6E1QvDJBh7rzBQy/Lio6LKiOLDDAKBggqhkjOPQQDAgRoMGYCMQDzNDN1YFGsfl6M6UfPndtJ84F5cJZb4NOSvpAiV4arS59kUnfdIZrzdon6Vcv9Yx8CMQCfqggeu1pBmeay+m+5n6ORzJVC6xcOD/cMjx3tXrbYMrOHfy3OgtUh+gYJv3lCgdo="}]}},"dsseEnvelope":{"payload":"eyJfdHlwZSI6Imh0dHBzOi8vaW4tdG90by5pby9TdGF0ZW1lbnQvdjEiLCJzdWJqZWN0IjpbeyJkaWdlc3QiOnsic2hhMjU2IjoiZDIxN2ZiMmQ4MWQ0MzE0ZDQ4YWRlNDFhZDNjYWQxMzMyNDg2MWQyOTZlNGU3ODZlNjQ5OGYzMTIzOTUxNzA5YyJ9fV0sInByZWRpY2F0ZVR5cGUiOiJodHRwczovL3NpZ3N0b3JlLmRldi9jb3NpZ24vc2lnbi92MSJ9","payloadType":"application/vnd.in-toto+json","signatures":[{"sig":"MEYCIQDL1rb3RWt6h74FJGpdSbWUb5hpA4RVyefxOhfmMKLJ8gIhANIPKoUgHaCx7ofPhMzkcfcLrzlr+4UsxBFQwHx6gwpS"}]}}

commit 4db0b4020a887a333dd7c951d2b7a772fcbc3cb0

pkg: signature: Check predicate type to ensure this comes from signature.
Cosign Bundle wraps a DSSE envelope containing an in-toto statement.
This commit adds a check on the predicate type, contained in the in-toto
statement, to ensure this was originated by a cosign sign command.

Fixes: 01cf64de8fbe ("pkg: signature: Add support for cosign bundle format to Verifier.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/signature/verifier/cosign/signature-format/bundle.go
@@ -30,23 +30,41 @@ import (
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/signature/helpers"
 )
 
+const (
+	// This is used to ensure we are dealing with a signature and not
+	// an attestation. Taken from:
+	// https://github.com/sigstore/cosign/blob/d253adffe000/pkg/types/predicate.go#L19
+	cosignSignPredicateType = "https://sigstore.dev/cosign/sign/v1"
+
+	// Inside a Bundle, there is a DSSE envelope which stores the signature
+	// as an in-toto payload. Taken from:
+	// https://github.com/sigstore/cosign/blob/d253adffe000/pkg/types/payload.go#L20
+	intotoPayloadType = "application/vnd.in-toto+json"
+)
+
 type BundleFormat struct{}
 
 func (*BundleFormat) CheckPayloadImage(payloadBytes []byte, imageDigest string) error {
 	var statement spb.Statement
-	err := json.Unmarshal(payloadBytes, &statement)
+	err := protojson.Unmarshal(payloadBytes, &statement)
 	if err != nil {
 		return fmt.Errorf("unmarshalling payload: %w", err)
+	}
+
+	if statement.PredicateType != cosignSignPredicateType {
+		return fmt.Errorf("unexpected predicate type: expected %q, got %q", cosignSignPredicateType, statement.PredicateType)
 	}
 
 	if len(statement.Subject) == 0 {
 		return fmt.Errorf("payload has no subject: %v", payloadBytes)
 	}
 
-	for algorithm, hash := range statement.Subject[0].Digest {
-		digest := fmt.Sprintf("%s:%s", algorithm, hash)
-		if digest == imageDigest {
-			return nil
+	for _, subject := range statement.Subject {
+		for algorithm, hash := range subject.Digest {
+			digest := fmt.Sprintf("%s:%s", algorithm, hash)
+			if digest == imageDigest {
+				return nil
+			}
 		}
 	}
 
@@ -102,6 +120,11 @@ func (*BundleFormat) LoadSignatureAndPayload(ctx context.Context, imageStore ora
 	envelope := bundle.GetDsseEnvelope()
 	if envelope == nil {
 		return nil, nil, nil, errors.New("DSSE envelope not found in bundle")
+	}
+
+	payloadType := envelope.GetPayloadType()
+	if payloadType != intotoPayloadType {
+		return nil, nil, nil, fmt.Errorf("unexpected payload type: expected %q, got %q", intotoPayloadType, payloadType)
 	}
 
 	signatures := envelope.GetSignatures()