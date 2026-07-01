inspektor-gadget/inspektor-gadget

commit 86ea94b7d2db2bc781dc95d930614e036f7dd37e

include: add CO-RE helpers for inode ctime and syscall access
Signed-off-by: Dor Serero <dor.serero@gmail.com>

include/gadget/core_fixes.bpf.h
@@ -6,6 +6,7 @@
 
 //#include "vmlinux.h"
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
 
 /**
  * kernel commit 9fdc4273b8da ("libbpf: Fix up verifier log for unguarded
@@ -250,6 +251,106 @@ gadget_get_tcp_retransmit_skb_skaddr(void *ctx)
 			skaddr);
 	return BPF_CORE_READ((struct trace_event_raw_tcp_event_sk_skb *)ctx,
 			     skaddr);
+}
+
+/**
+ * The inode struct has undergone changes to its ctime field across kernel
+ * versions. These CO-RE flavor structs and the gadget_inode_get_ctime()
+ * helper provide portable access to the inode ctime.
+ *
+ * Kernel < 6.6:    struct timespec64 i_ctime
+ * Kernel 6.6-6.10: struct timespec64 __i_ctime
+ * Kernel >= 6.11:  time64_t i_ctime_sec + u32 i_ctime_nsec
+ *
+ * See also gadget_inode_get_mtime() in gadget/user_stack_map.h for the
+ * equivalent mtime helper.
+ * 
+ * Based on get_ctime_nanosec_timespec from:
+ * https://github.com/aquasecurity/tracee/blob/main/pkg/ebpf/c/common/filesystem.h#L61
+ */
+// kernel >= v6.6 inode i_ctime field change
+struct inode___older_v66 {
+	struct timespec64 i_ctime;
+};
+
+// kernel >= v6.11 inode i_ctime field change
+struct inode___older_v611 {
+	struct timespec64 __i_ctime;
+};
+
+// Based on get_ctime_nanosec_timespec from:
+// https://github.com/aquasecurity/tracee/blob/main/pkg/ebpf/c/common/filesystem.h#L61
+static __always_inline u64
+gadget_get_ctime_nanosec_from_inode(struct inode *inode)
+{
+	time64_t sec;
+	long nsec;
+
+	// Kernel >= 6.11
+	if (bpf_core_field_exists(inode->i_ctime_sec) &&
+	    bpf_core_field_exists(inode->i_ctime_nsec)) {
+		sec = BPF_CORE_READ(inode, i_ctime_sec);
+		nsec = BPF_CORE_READ(inode, i_ctime_nsec);
+	}
+	// Kernel 6.6 - 6.10
+	else if (bpf_core_field_exists(
+			 ((struct inode___older_v611 *)inode)->__i_ctime)) {
+		struct inode___older_v611 *old_inode_v611 = (void *)inode;
+		sec = BPF_CORE_READ(old_inode_v611, __i_ctime.tv_sec);
+		nsec = BPF_CORE_READ(old_inode_v611, __i_ctime.tv_nsec);
+	}
+	// Kernel < 6.6
+	else {
+		struct inode___older_v66 *old_inode_v66 = (void *)inode;
+		sec = BPF_CORE_READ(old_inode_v66, i_ctime.tv_sec);
+		nsec = BPF_CORE_READ(old_inode_v66, i_ctime.tv_nsec);
+	}
+
+	if (sec < 0)
+		return 0;
+
+	return (u64)sec * 1000000000ULL + (u32)nsec;
+}
+
+/**
+ * Helpers for accessing syscall information from pt_regs.
+ * These use compile-time architecture detection since pt_regs layouts
+ * differ across architectures and CO-RE flavor structs cannot bridge
+ * architectural differences (the base type's BTF only contains
+ * fields for the compilation target).
+ *
+ * x86_64: orig_ax holds the syscall number, di holds the first argument.
+ * arm64:  syscallno holds the syscall number, regs[0] holds the first argument.
+ */
+static __always_inline long gadget_get_syscall_nr(struct pt_regs *regs)
+{
+#if defined(__TARGET_ARCH_x86)
+	return BPF_CORE_READ(regs, orig_ax);
+#elif defined(__TARGET_ARCH_arm64)
+	return (long)BPF_CORE_READ(regs, syscallno);
+#else
+	return -1;
+#endif
+}
+
+static __always_inline unsigned long
+gadget_get_syscall_arg(struct pt_regs *regs, unsigned int arg_id)
+{
+	switch (arg_id) {
+	case 1:
+		return PT_REGS_PARM1_CORE_SYSCALL(regs);
+	case 2:
+		return PT_REGS_PARM2_CORE_SYSCALL(regs);
+	case 3:
+		return PT_REGS_PARM3_CORE_SYSCALL(regs);
+	case 4:
+		return PT_REGS_PARM4_CORE_SYSCALL(regs);
+	case 5:
+		return PT_REGS_PARM5_CORE_SYSCALL(regs);
+	case 6:
+		return PT_REGS_PARM6_CORE_SYSCALL(regs);
+	}
+	return 0;
 }
 
 #endif /* __CORE_FIXES_BPF_H */

commit 4ee226c3650e981f598a9d17782de70645887017

include: mask I_CTIME_QUERIED flag when reading inode ctime
Fixes: 86ea94b7d2db ("include: add CO-RE helpers for inode ctime and syscall access")

Signed-off-by: Dor Serero <dor.serero@gmail.com>

include/gadget/core_fixes.bpf.h
@@ -309,7 +309,12 @@ gadget_get_ctime_nanosec_from_inode(struct inode *inode)
 	if (sec < 0)
 		return 0;
 
-	return (u64)sec * 1000000000ULL + (u32)nsec;
+	// On kernels with multigrain timestamps (>= v6.13), the top bit of
+	// i_ctime_nsec holds the I_CTIME_QUERIED flag (BIT(31)), which the kernel
+	// masks out in inode_get_ctime_nsec(). Mask it here too so the value matches
+	// stat(); harmless on older kernels where nsec < 1e9 keeps the bit clear.
+	// https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=4e40eff0b5737c0de39e1ae5812509efbc0b986e
+	return (u64)sec * 1000000000ULL + ((u32)nsec & ~(1U << 31));
 }
 
 /**