inspektor-gadget/inspektor-gadget

commit 7ee5e7a90bbc29707ad6bf10e08f7cc3a03f8314

pkg/uprobetracer: support USDT trace points
Signed-off-by: Tianyi Liu <i.pear@outlook.com>
Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/uprobetracer/usdt.go
@@ -1,0 +1,161 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package uprobetracer
+
+import (
+	"debug/elf"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// For details regarding the data format of USDT notes, please refer to:
+// https://sourceware.org/systemtap/wiki/UserSpaceProbeImplementation
+const (
+	sdtNoteSectionName = ".note.stapsdt"
+	sdtBaseSectionName = ".stapsdt.base"
+)
+
+type noteHeader struct {
+	NameSize uint32
+	DescSize uint32
+	Type     uint32
+}
+
+type usdtAttachInfo struct {
+	attachAddress    uint64
+	semaphoreAddress uint64
+}
+
+func vaddr2ElfOffset(f *elf.File, addr uint64) (uint64, error) {
+	for _, prog := range f.Progs {
+		if prog.Vaddr <= addr && addr < (prog.Vaddr+prog.Memsz) {
+			return addr - prog.Vaddr + prog.Off, nil
+		}
+	}
+	return 0, fmt.Errorf("malformed elf file: elf prog containing addr %x not found", addr)
+}
+
+func alignUp[T int | int32 | int64 | uint | uint32 | uint64](n T, align T) T {
+	return (n + align - 1) / align * align
+}
+
+func getUsdtInfo(filepath string, attachSymbol string) (*usdtAttachInfo, error) {
+	parts := strings.Split(attachSymbol, ":")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid USDT section name: %q", attachSymbol)
+	}
+	providerName := parts[0]
+	probeName := parts[1]
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("opening file %q: %w", filepath, err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("stating file %q: %w", filepath, err)
+	}
+	if !fileInfo.Mode().IsRegular() {
+		return nil, fmt.Errorf("ELF file %q is not regular", filepath)
+	}
+
+	elfReader, err := elf.NewFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("reading elf file %q: %w", filepath, err)
+	}
+	defer elfReader.Close()
+
+	noteSection := elfReader.Section(sdtNoteSectionName)
+	if noteSection == nil {
+		return nil, errors.New("USDT note section does not exist")
+	}
+	if noteSection.Type != elf.SHT_NOTE {
+		return nil, fmt.Errorf("section %q is not a note", sdtNoteSectionName)
+	}
+	notesReader := noteSection.Open()
+
+	baseSection := elfReader.Section(sdtBaseSectionName)
+	if baseSection == nil {
+		return nil, errors.New("USDT base section does not exist")
+	}
+	if baseSection.Type != elf.SHT_PROGBITS {
+		return nil, fmt.Errorf("%q is not a program defined section", sdtBaseSectionName)
+	}
+
+	wordSize := 4
+	if elfReader.Class == elf.ELFCLASS64 {
+		wordSize = 8
+	}
+
+	// walk through USDT notes, and match with providerName and probeName
+	// For details of the structure of ELF notes, please refer to
+	// https://man7.org/linux/man-pages/man5/elf.5.html, the `Notes (Nhdr)` section
+	for {
+		var header noteHeader
+		err = binary.Read(notesReader, elfReader.ByteOrder, &header)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, fmt.Errorf("reading USDT note header: %w", err)
+		}
+
+		name := make([]byte, alignUp(uint64(header.NameSize), 4))
+		err = binary.Read(notesReader, elfReader.ByteOrder, &name)
+		if err != nil {
+			return nil, fmt.Errorf("reading USDT note name: %w", err)
+		}
+
+		desc := make([]byte, alignUp(uint64(header.DescSize), 4))
+		err = binary.Read(notesReader, elfReader.ByteOrder, &desc)
+		if err != nil {
+			return nil, fmt.Errorf("reading USDT note desc: %w", err)
+		}
+
+		if string(name) != "stapsdt\x00" || header.Type != 3 {
+			continue
+		}
+
+		elfLocation := elfReader.ByteOrder.Uint64(desc[:wordSize])
+		elfBase := elfReader.ByteOrder.Uint64(desc[wordSize : 2*wordSize])
+		elfSemaphore := elfReader.ByteOrder.Uint64(desc[2*wordSize : 3*wordSize])
+
+		diff := baseSection.Addr - elfBase
+		location, err := vaddr2ElfOffset(elfReader, elfLocation+diff)
+		if err != nil {
+			return nil, err
+		}
+
+		if elfSemaphore != 0 {
+			elfSemaphore, err = vaddr2ElfOffset(elfReader, elfSemaphore+diff)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		provider := readStringFromBytes(desc, uint32(3*wordSize))
+		probe := readStringFromBytes(desc, uint32(3*wordSize+len(provider)+1))
+		if provider == providerName && probe == probeName {
+			return &usdtAttachInfo{location, elfSemaphore}, nil
+		}
+	}
+	return nil, errors.New("no matching USDT metadata")
+}

pkg/operators/ebpf/attach.go
@@ -45,13 +45,16 @@ func (i *ebpfInstance) attachProgram(gadgetCtx operators.GadgetContext, p *ebpf.
 			i.logger.Debugf("Attaching kretprobe %q to %q", p.Name, p.AttachTo)
 			return link.Kretprobe(p.AttachTo, prog, nil)
 		case strings.HasPrefix(p.SectionName, "uprobe/") ||
-			strings.HasPrefix(p.SectionName, "uretprobe/"):
+			strings.HasPrefix(p.SectionName, "uretprobe/") ||
+			strings.HasPrefix(p.SectionName, "usdt/"):
 			uprobeTracer := i.uprobeTracers[p.Name]
 			switch strings.Split(p.SectionName, "/")[0] {
 			case "uprobe":
 				return nil, uprobeTracer.AttachProg(p.Name, uprobetracer.ProgUprobe, p.AttachTo, prog)
 			case "uretprobe":
 				return nil, uprobeTracer.AttachProg(p.Name, uprobetracer.ProgUretprobe, p.AttachTo, prog)
+			case "usdt":
+				return nil, uprobeTracer.AttachProg(p.Name, uprobetracer.ProgUSDT, p.AttachTo, prog)
 			}
 		}
 		return nil, fmt.Errorf("unsupported section name %q for program %q", p.SectionName, p.Name)

pkg/operators/ebpf/ebpf.go
@@ -376,7 +376,8 @@ func (i *ebpfInstance) Prepare(gadgetCtx operators.GadgetContext) error {
 		switch p.Type {
 		case ebpf.Kprobe:
 			if strings.HasPrefix(p.SectionName, "uprobe/") ||
-				strings.HasPrefix(p.SectionName, "uretprobe/") {
+				strings.HasPrefix(p.SectionName, "uretprobe/") ||
+				strings.HasPrefix(p.SectionName, "usdt/") {
 				uprobeTracer, err := uprobetracer.NewTracer[api.GadgetData](gadgetCtx.Logger())
 				if err != nil {
 					i.Close()

pkg/uprobetracer/bytes.go
@@ -1,3 +1,17 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package uprobetracer
 
 import (

pkg/uprobetracer/tracer.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package uprobetracer handles how uprobe/uretprobe programs are attached
+// Package uprobetracer handles how uprobe/uretprobe/USDT programs are attached
 // to containers. It has two running modes: `pending` mode and `running` mode.
 //
 // Before `AttachProg` is called, uprobetracer runs in `pending` mode, only
@@ -55,6 +55,7 @@ type ProgType uint32
 const (
 	ProgUprobe ProgType = iota
 	ProgUretprobe
+	ProgUSDT
 )
 
 type inodeUUID struct {
@@ -118,7 +119,7 @@ func NewTracer[Event any](logger logger.Logger) (*Tracer[Event], error) {
 
 // AttachProg loads the ebpf program, and try attaching if there are pending containers
 func (t *Tracer[Event]) AttachProg(progName string, progType ProgType, attachTo string, prog *ebpf.Program) error {
-	if progType != ProgUprobe && progType != ProgUretprobe {
+	if progType != ProgUprobe && progType != ProgUretprobe && progType != ProgUSDT {
 		return fmt.Errorf("unsupported uprobe prog type: %q", progType)
 	}
 
@@ -129,12 +130,15 @@ func (t *Tracer[Event]) AttachProg(progName string, progType ProgType, attachTo 
 		return errors.New("loading uprobe program twice")
 	}
 
-	parts := strings.Split(attachTo, ":")
+	parts := strings.SplitN(attachTo, ":", 2)
 	if len(parts) < 2 {
 		return fmt.Errorf("invalid section name %q", attachTo)
 	}
 	if !filepath.IsAbs(parts[0]) && strings.Contains(parts[0], "/") {
 		return fmt.Errorf("section name must be either an absolute path or a library name: %q", parts[0])
+	}
+	if progType == ProgUSDT && len(strings.Split(parts[1], ":")) != 2 {
+		return fmt.Errorf("invalid USDT section name: %q", attachTo)
 	}
 
 	t.mu.Lock()
@@ -180,7 +184,7 @@ func (t *Tracer[Event]) searchForLibrary(containerPid uint32) ([]string, error) 
 	for _, targetPath := range targetPaths {
 		securedTargetPath, err := securejoin.SecureJoin(filepath.Join(host.HostProcFs, fmt.Sprint(containerPid), "root"), targetPath)
 		if err != nil {
-			t.logger.Debugf("path %q in ld cache is not available: %q", filePath, err.Error())
+			t.logger.Debugf("path %q in ld cache is not available: %s", filePath, err.Error())
 			continue
 		}
 		securedTargetPaths = append(securedTargetPaths, securedTargetPath)
@@ -200,6 +204,16 @@ func (t *Tracer[Event]) attachUprobe(file *os.File) (link.Link, error) {
 		return ex.Uprobe(t.attachSymbol, t.prog, nil)
 	case ProgUretprobe:
 		return ex.Uretprobe(t.attachSymbol, t.prog, nil)
+	case ProgUSDT:
+		attachInfo, err := getUsdtInfo(attachPath, t.attachSymbol)
+		if err != nil {
+			return nil, fmt.Errorf("reading USDT metadata: %w", err)
+		}
+		return ex.Uprobe(t.attachSymbol, t.prog,
+			&link.UprobeOptions{
+				Address:      attachInfo.attachAddress,
+				RefCtrOffset: attachInfo.semaphoreAddress,
+			})
 	default:
 		return nil, fmt.Errorf("attaching to inode: unsupported prog type: %q", t.progType)
 	}
@@ -210,7 +224,7 @@ func (t *Tracer[Event]) attach(containerPid uint32) {
 	var attachedUUIDs []inodeUUID
 	attachFilePaths, err := t.searchForLibrary(containerPid)
 	if err != nil {
-		t.logger.Debugf("attaching to container %d: %q", containerPid, err.Error())
+		t.logger.Debugf("attaching to container %d: %s", containerPid, err.Error())
 	}
 
 	if len(attachFilePaths) == 0 {
@@ -220,22 +234,25 @@ func (t *Tracer[Event]) attach(containerPid uint32) {
 	for _, filePath := range attachFilePaths {
 		file, err := os.OpenFile(filePath, unix.O_PATH, 0)
 		if err != nil {
-			t.logger.Debugf("opening file '%q' for uprobe: %q", filePath, err.Error())
+			t.logger.Debugf("opening file '%q' for uprobe: %s", filePath, err.Error())
 			continue
 		}
 		fileUUID, err := getInodeUUID(file)
 		if err != nil {
-			t.logger.Debugf("getting inode info for '%q': %q", filePath, err.Error())
+			t.logger.Debugf("getting inode info for '%q': %s", filePath, err.Error())
 			file.Close()
 			continue
 		}
 
-		t.logger.Debugf("attaching uprobe %q to container %d: %q", t.attachSymbol, containerPid, filePath)
+		t.logger.Debugf("attaching uprobe %q to container %d: %q", t.progName, containerPid, filePath)
 		attachedUUIDs = append(attachedUUIDs, fileUUID)
 
 		inode, exists := t.inodeRefCount[fileUUID]
 		if !exists {
-			progLink, _ := t.attachUprobe(file)
+			progLink, err := t.attachUprobe(file)
+			if err != nil {
+				t.logger.Debugf("failed to attach uprobe %q: %s", t.progName, err.Error())
+			}
 			t.inodeRefCount[fileUUID] = &inodeKeeper{1, file, progLink}
 		} else {
 			inode.counter++

commit 6930992aebb9aed6f5ff00524c293aa1f089de04

uprobetracer: Fix panic parsing USDT notes of 32-bit ELF files
The stapsdt note parser derives wordSize from the ELF class, but read the
three address fields with ByteOrder.Uint64() regardless. On an ELFCLASS32
file this evaluates to Uint64(desc[:4]): binary.LittleEndian.Uint64 starts
by evaluating b[7], and Go bounds-checks that against the slice length (4)
rather than the capacity, so the parser panics with "index out of range [7]
with length 4".

The desc length guard added in 3f39e27a6a35 does not help, since it is
sized in wordSize units: for a 32-bit file the threshold is 3*4 = 12, so
any note clearing the guard still triggers the over-read. A length guard
sized in wordSize units cannot protect reads performed at a fixed 8-byte
width.

As a result the whole ELFCLASS32 USDT path was unusable: every 32-bit
binary carrying a valid stapsdt note crashed the parser. Because the ELF
may come from an untrusted container and is parsed on a container-attach
goroutine that has no panic recovery, the panic also terminated the
privileged process, dropping monitoring on that node.

Read each address field at the parser's actual word size, and wrap
getUsdtInfo() in defer/recover so a parser panic on attacker-supplied ELF
can never take down the process, as originally intended by 3f39e27a6a35.

Fixes: 7ee5e7a90bbc ("pkg/uprobetracer: support USDT trace points")
Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>
Signed-off-by: Alban Crequy <albancrequy@microsoft.com>

pkg/uprobetracer/usdt.go
@@ -68,7 +68,22 @@ func alignUp[T int | int32 | int64 | uint | uint32 | uint64](n T, align T) T {
 	return (n + align - 1) / align * align
 }
 
-func getUsdtInfo(filepath string, attachSymbol string) (*usdtAttachInfo, error) {
+// getUsdtInfo parses the USDT notes of an ELF file that may come from an
+// untrusted container. Any panic escaping the parser is turned into an error
+// so that malformed input cannot terminate the privileged process: the parser
+// runs on a container-attach goroutine that has no panic recovery of its own.
+func getUsdtInfo(filepath string, attachSymbol string) (info *usdtAttachInfo, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			info = nil
+			err = fmt.Errorf("panic parsing USDT notes of %q: %v", filepath, r)
+		}
+	}()
+
+	return parseUsdtNotes(filepath, attachSymbol)
+}
+
+func parseUsdtNotes(filepath string, attachSymbol string) (*usdtAttachInfo, error) {
 	parts := strings.Split(attachSymbol, ":")
 	if len(parts) != 2 {
 		return nil, fmt.Errorf("invalid USDT section name: %q", attachSymbol)
@@ -116,6 +131,16 @@ func getUsdtInfo(filepath string, attachSymbol string) (*usdtAttachInfo, error) 
 	wordSize := 4
 	if elfReader.Class == elf.ELFCLASS64 {
 		wordSize = 8
+	}
+
+	// Address fields are wordSize bytes wide, so they must be read at that
+	// width. Reading them with Uint64 on an ELFCLASS32 file would read past
+	// the end of the desc slice and panic.
+	readAddr := func(b []byte) uint64 {
+		if wordSize == 8 {
+			return elfReader.ByteOrder.Uint64(b)
+		}
+		return uint64(elfReader.ByteOrder.Uint32(b))
 	}
 
 	// Minimum desc size for a stapsdt note: 3 address fields.
@@ -170,9 +195,9 @@ func getUsdtInfo(filepath string, attachSymbol string) (*usdtAttachInfo, error) 
 			return nil, fmt.Errorf("malformed stapsdt note: desc too short (%d bytes, need %d)", len(desc), minDescSize)
 		}
 
-		elfLocation := elfReader.ByteOrder.Uint64(desc[:wordSize])
-		elfBase := elfReader.ByteOrder.Uint64(desc[wordSize : 2*wordSize])
-		elfSemaphore := elfReader.ByteOrder.Uint64(desc[2*wordSize : 3*wordSize])
+		elfLocation := readAddr(desc[:wordSize])
+		elfBase := readAddr(desc[wordSize : 2*wordSize])
+		elfSemaphore := readAddr(desc[2*wordSize : 3*wordSize])
 
 		diff := baseSection.Addr - elfBase
 		location, err := vaddr2ElfOffset(elfReader.File, elfLocation+diff)

pkg/uprobetracer/usdt_test.go
@@ -1,0 +1,261 @@
+// Copyright 2026 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package uprobetracer
+
+import (
+	"debug/elf"
+	"encoding/binary"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testStapsdtBaseAddr = 0x1000
+	testLoadSegmentSize = 0x10000
+)
+
+// buildUsdtELF builds a minimal little-endian ELF containing a single
+// PT_LOAD segment mapped at vaddr 0, a ".note.stapsdt" section holding one
+// stapsdt note with the given desc, and a ".stapsdt.base" section at
+// testStapsdtBaseAddr. It is used to exercise the USDT note parser with
+// inputs that a real toolchain would not produce.
+func buildUsdtELF(class elf.Class, desc []byte) []byte {
+	le := binary.LittleEndian
+	is64 := class == elf.ELFCLASS64
+
+	note := make([]byte, 12)
+	le.PutUint32(note[0:], 8)                 // NameSize, len("stapsdt\x00")
+	le.PutUint32(note[4:], uint32(len(desc))) // DescSize
+	le.PutUint32(note[8:], 3)                 // Type, NT_STAPSDT
+	note = append(note, []byte("stapsdt\x00")...)
+	note = append(note, desc...)
+
+	shstr := []byte{0}
+	nameOff := func(s string) uint32 {
+		off := uint32(len(shstr))
+		shstr = append(shstr, []byte(s)...)
+		shstr = append(shstr, 0)
+		return off
+	}
+	offNote := nameOff(sdtNoteSectionName)
+	offBase := nameOff(sdtBaseSectionName)
+	offShstr := nameOff(".shstrtab")
+
+	var ehSize, phEntSize, shEntSize uint32
+	if is64 {
+		ehSize, phEntSize, shEntSize = 64, 56, 64
+	} else {
+		ehSize, phEntSize, shEntSize = 52, 32, 40
+	}
+	phOff := ehSize
+	noteOff := phOff + phEntSize
+	baseOff := noteOff + uint32(len(note))
+	shstrOff := baseOff
+	shOff := shstrOff + uint32(len(shstr))
+
+	buf := make([]byte, shOff+4*shEntSize)
+	copy(buf, []byte{0x7f, 'E', 'L', 'F'})
+	buf[4] = byte(class)
+	buf[5] = byte(elf.ELFDATA2LSB)
+	buf[6] = byte(elf.EV_CURRENT)
+
+	le.PutUint16(buf[16:], uint16(elf.ET_EXEC))
+	le.PutUint32(buf[20:], uint32(elf.EV_CURRENT))
+	if is64 {
+		le.PutUint16(buf[18:], uint16(elf.EM_X86_64))
+		le.PutUint64(buf[32:], uint64(phOff))
+		le.PutUint64(buf[40:], uint64(shOff))
+		le.PutUint16(buf[52:], uint16(ehSize))
+		le.PutUint16(buf[54:], uint16(phEntSize))
+		le.PutUint16(buf[56:], 1)
+		le.PutUint16(buf[58:], uint16(shEntSize))
+		le.PutUint16(buf[60:], 4)
+		le.PutUint16(buf[62:], 3)
+
+		p := buf[phOff:]
+		le.PutUint32(p[0:], uint32(elf.PT_LOAD))
+		le.PutUint32(p[4:], uint32(elf.PF_R|elf.PF_X))
+		le.PutUint64(p[8:], 0)
+		le.PutUint64(p[16:], 0)
+		le.PutUint64(p[32:], testLoadSegmentSize)
+		le.PutUint64(p[40:], testLoadSegmentSize)
+	} else {
+		le.PutUint16(buf[18:], uint16(elf.EM_386))
+		le.PutUint32(buf[28:], phOff)
+		le.PutUint32(buf[32:], shOff)
+		le.PutUint16(buf[40:], uint16(ehSize))
+		le.PutUint16(buf[42:], uint16(phEntSize))
+		le.PutUint16(buf[44:], 1)
+		le.PutUint16(buf[46:], uint16(shEntSize))
+		le.PutUint16(buf[48:], 4)
+		le.PutUint16(buf[50:], 3)
+
+		p := buf[phOff:]
+		le.PutUint32(p[0:], uint32(elf.PT_LOAD))
+		le.PutUint32(p[4:], 0)
+		le.PutUint32(p[8:], 0)
+		le.PutUint32(p[16:], testLoadSegmentSize)
+		le.PutUint32(p[20:], testLoadSegmentSize)
+		le.PutUint32(p[24:], uint32(elf.PF_R|elf.PF_X))
+	}
+
+	copy(buf[noteOff:], note)
+	copy(buf[shstrOff:], shstr)
+
+	putSection := func(idx int, name uint32, typ elf.SectionType, addr, off, size uint32) {
+		b := buf[shOff+uint32(idx)*shEntSize:]
+		le.PutUint32(b[0:], name)
+		le.PutUint32(b[4:], uint32(typ))
+		if is64 {
+			le.PutUint64(b[16:], uint64(addr))
+			le.PutUint64(b[24:], uint64(off))
+			le.PutUint64(b[32:], uint64(size))
+			le.PutUint64(b[48:], 1)
+		} else {
+			le.PutUint32(b[12:], addr)
+			le.PutUint32(b[16:], off)
+			le.PutUint32(b[20:], size)
+			le.PutUint32(b[32:], 1)
+		}
+	}
+	putSection(0, 0, elf.SHT_NULL, 0, 0, 0)
+	putSection(1, offNote, elf.SHT_NOTE, 0, noteOff, uint32(len(note)))
+	putSection(2, offBase, elf.SHT_PROGBITS, testStapsdtBaseAddr, baseOff, 0)
+	putSection(3, offShstr, elf.SHT_STRTAB, 0, shstrOff, uint32(len(shstr)))
+
+	return buf
+}
+
+// usdtDesc builds a stapsdt note desc holding the three address fields at the
+// given word size, followed by the provider and probe names.
+func usdtDesc(wordSize int, location, base, semaphore uint64, provider, probe string) []byte {
+	le := binary.LittleEndian
+	desc := make([]byte, 3*wordSize)
+	put := func(off int, v uint64) {
+		if wordSize == 8 {
+			le.PutUint64(desc[off:], v)
+		} else {
+			le.PutUint32(desc[off:], uint32(v))
+		}
+	}
+	put(0, location)
+	put(wordSize, base)
+	put(2*wordSize, semaphore)
+	desc = append(desc, []byte(provider+"\x00"+probe+"\x00")...)
+	return append(desc, make([]byte, alignUp(len(desc), 4)-len(desc))...)
+}
+
+func writeTestELF(t *testing.T, data []byte) string {
+	t.Helper()
+	path := filepath.Join(t.TempDir(), "target.elf")
+	require.NoError(t, os.WriteFile(path, data, 0o644))
+	return path
+}
+
+func TestGetUsdtInfo(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name         string
+		class        elf.Class
+		desc         []byte
+		expectErr    bool
+		expectAttach uint64
+		expectSem    uint64
+	}{
+		{
+			name:         "64bit",
+			class:        elf.ELFCLASS64,
+			desc:         usdtDesc(8, 0x2000, testStapsdtBaseAddr, 0x3000, "prov", "probe"),
+			expectAttach: 0x2000,
+			expectSem:    0x3000,
+		},
+		{
+			// A 32-bit binary is parsed with 4-byte address fields. Reading
+			// them at a fixed 8-byte width used to panic.
+			name:         "32bit",
+			class:        elf.ELFCLASS32,
+			desc:         usdtDesc(4, 0x2000, testStapsdtBaseAddr, 0x3000, "prov", "probe"),
+			expectAttach: 0x2000,
+			expectSem:    0x3000,
+		},
+		{
+			name:         "32bit without semaphore",
+			class:        elf.ELFCLASS32,
+			desc:         usdtDesc(4, 0x2000, testStapsdtBaseAddr, 0, "prov", "probe"),
+			expectAttach: 0x2000,
+			expectSem:    0,
+		},
+		{
+			// Regression test: the desc length guard is 3*wordSize, so a
+			// 12-byte desc passes it on a 32-bit file. Reading the address
+			// fields with Uint64 then read past the end of the slice and
+			// panicked, killing the privileged process.
+			name:      "32bit desc holding only the address fields",
+			class:     elf.ELFCLASS32,
+			desc:      make([]byte, 12),
+			expectErr: true,
+		},
+		{
+			name:      "64bit desc too short",
+			class:     elf.ELFCLASS64,
+			desc:      make([]byte, 4),
+			expectErr: true,
+		},
+		{
+			name:      "32bit desc too short",
+			class:     elf.ELFCLASS32,
+			desc:      make([]byte, 4),
+			expectErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			path := writeTestELF(t, buildUsdtELF(test.class, test.desc))
+
+			var info *usdtAttachInfo
+			var err error
+			require.NotPanics(t, func() {
+				info, err = getUsdtInfo(path, "prov:probe")
+			})
+
+			if test.expectErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, test.expectAttach, info.attachAddress)
+			require.Equal(t, test.expectSem, info.semaphoreAddress)
+		})
+	}
+}
+
+func TestGetUsdtInfoRecoversPanic(t *testing.T) {
+	t.Parallel()
+
+	// A truncated ELF header makes safeelf.NewFile fail, but the parser must
+	// never propagate a panic to its caller whatever the input is.
+	path := writeTestELF(t, []byte{0x7f, 'E', 'L', 'F'})
+	require.NotPanics(t, func() {
+		_, err := getUsdtInfo(path, "prov:probe")
+		require.Error(t, err)
+	})
+}