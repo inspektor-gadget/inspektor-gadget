inspektor-gadget/inspektor-gadget

commit 7ee5e7a90bbc29707ad6bf10e08f7cc3a03f8314

pkg/uprobetracer: support USDT trace points
Signed-off-by: Tianyi Liu <i.pear@outlook.com>
Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

pkg/uprobetracer/usdt.go
@@ -1,0 +1,161 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package uprobetracer
+
+import (
+	"debug/elf"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// For details regarding the data format of USDT notes, please refer to:
+// https://sourceware.org/systemtap/wiki/UserSpaceProbeImplementation
+const (
+	sdtNoteSectionName = ".note.stapsdt"
+	sdtBaseSectionName = ".stapsdt.base"
+)
+
+type noteHeader struct {
+	NameSize uint32
+	DescSize uint32
+	Type     uint32
+}
+
+type usdtAttachInfo struct {
+	attachAddress    uint64
+	semaphoreAddress uint64
+}
+
+func vaddr2ElfOffset(f *elf.File, addr uint64) (uint64, error) {
+	for _, prog := range f.Progs {
+		if prog.Vaddr <= addr && addr < (prog.Vaddr+prog.Memsz) {
+			return addr - prog.Vaddr + prog.Off, nil
+		}
+	}
+	return 0, fmt.Errorf("malformed elf file: elf prog containing addr %x not found", addr)
+}
+
+func alignUp[T int | int32 | int64 | uint | uint32 | uint64](n T, align T) T {
+	return (n + align - 1) / align * align
+}
+
+func getUsdtInfo(filepath string, attachSymbol string) (*usdtAttachInfo, error) {
+	parts := strings.Split(attachSymbol, ":")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid USDT section name: %q", attachSymbol)
+	}
+	providerName := parts[0]
+	probeName := parts[1]
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("opening file %q: %w", filepath, err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("stating file %q: %w", filepath, err)
+	}
+	if !fileInfo.Mode().IsRegular() {
+		return nil, fmt.Errorf("ELF file %q is not regular", filepath)
+	}
+
+	elfReader, err := elf.NewFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("reading elf file %q: %w", filepath, err)
+	}
+	defer elfReader.Close()
+
+	noteSection := elfReader.Section(sdtNoteSectionName)
+	if noteSection == nil {
+		return nil, errors.New("USDT note section does not exist")
+	}
+	if noteSection.Type != elf.SHT_NOTE {
+		return nil, fmt.Errorf("section %q is not a note", sdtNoteSectionName)
+	}
+	notesReader := noteSection.Open()
+
+	baseSection := elfReader.Section(sdtBaseSectionName)
+	if baseSection == nil {
+		return nil, errors.New("USDT base section does not exist")
+	}
+	if baseSection.Type != elf.SHT_PROGBITS {
+		return nil, fmt.Errorf("%q is not a program defined section", sdtBaseSectionName)
+	}
+
+	wordSize := 4
+	if elfReader.Class == elf.ELFCLASS64 {
+		wordSize = 8
+	}
+
+	// walk through USDT notes, and match with providerName and probeName
+	// For details of the structure of ELF notes, please refer to
+	// https://man7.org/linux/man-pages/man5/elf.5.html, the `Notes (Nhdr)` section
+	for {
+		var header noteHeader
+		err = binary.Read(notesReader, elfReader.ByteOrder, &header)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, fmt.Errorf("reading USDT note header: %w", err)
+		}
+
+		name := make([]byte, alignUp(uint64(header.NameSize), 4))
+		err = binary.Read(notesReader, elfReader.ByteOrder, &name)
+		if err != nil {
+			return nil, fmt.Errorf("reading USDT note name: %w", err)
+		}
+
+		desc := make([]byte, alignUp(uint64(header.DescSize), 4))
+		err = binary.Read(notesReader, elfReader.ByteOrder, &desc)
+		if err != nil {
+			return nil, fmt.Errorf("reading USDT note desc: %w", err)
+		}
+
+		if string(name) != "stapsdt\x00" || header.Type != 3 {
+			continue
+		}
+
+		elfLocation := elfReader.ByteOrder.Uint64(desc[:wordSize])
+		elfBase := elfReader.ByteOrder.Uint64(desc[wordSize : 2*wordSize])
+		elfSemaphore := elfReader.ByteOrder.Uint64(desc[2*wordSize : 3*wordSize])
+
+		diff := baseSection.Addr - elfBase
+		location, err := vaddr2ElfOffset(elfReader, elfLocation+diff)
+		if err != nil {
+			return nil, err
+		}
+
+		if elfSemaphore != 0 {
+			elfSemaphore, err = vaddr2ElfOffset(elfReader, elfSemaphore+diff)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		provider := readStringFromBytes(desc, uint32(3*wordSize))
+		probe := readStringFromBytes(desc, uint32(3*wordSize+len(provider)+1))
+		if provider == providerName && probe == probeName {
+			return &usdtAttachInfo{location, elfSemaphore}, nil
+		}
+	}
+	return nil, errors.New("no matching USDT metadata")
+}

pkg/operators/ebpf/attach.go
@@ -45,13 +45,16 @@ func (i *ebpfInstance) attachProgram(gadgetCtx operators.GadgetContext, p *ebpf.
 			i.logger.Debugf("Attaching kretprobe %q to %q", p.Name, p.AttachTo)
 			return link.Kretprobe(p.AttachTo, prog, nil)
 		case strings.HasPrefix(p.SectionName, "uprobe/") ||
-			strings.HasPrefix(p.SectionName, "uretprobe/"):
+			strings.HasPrefix(p.SectionName, "uretprobe/") ||
+			strings.HasPrefix(p.SectionName, "usdt/"):
 			uprobeTracer := i.uprobeTracers[p.Name]
 			switch strings.Split(p.SectionName, "/")[0] {
 			case "uprobe":
 				return nil, uprobeTracer.AttachProg(p.Name, uprobetracer.ProgUprobe, p.AttachTo, prog)
 			case "uretprobe":
 				return nil, uprobeTracer.AttachProg(p.Name, uprobetracer.ProgUretprobe, p.AttachTo, prog)
+			case "usdt":
+				return nil, uprobeTracer.AttachProg(p.Name, uprobetracer.ProgUSDT, p.AttachTo, prog)
 			}
 		}
 		return nil, fmt.Errorf("unsupported section name %q for program %q", p.SectionName, p.Name)

pkg/operators/ebpf/ebpf.go
@@ -376,7 +376,8 @@ func (i *ebpfInstance) Prepare(gadgetCtx operators.GadgetContext) error {
 		switch p.Type {
 		case ebpf.Kprobe:
 			if strings.HasPrefix(p.SectionName, "uprobe/") ||
-				strings.HasPrefix(p.SectionName, "uretprobe/") {
+				strings.HasPrefix(p.SectionName, "uretprobe/") ||
+				strings.HasPrefix(p.SectionName, "usdt/") {
 				uprobeTracer, err := uprobetracer.NewTracer[api.GadgetData](gadgetCtx.Logger())
 				if err != nil {
 					i.Close()

pkg/uprobetracer/bytes.go
@@ -1,3 +1,17 @@
+// Copyright 2024 The Inspektor Gadget authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package uprobetracer
 
 import (

pkg/uprobetracer/tracer.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package uprobetracer handles how uprobe/uretprobe programs are attached
+// Package uprobetracer handles how uprobe/uretprobe/USDT programs are attached
 // to containers. It has two running modes: `pending` mode and `running` mode.
 //
 // Before `AttachProg` is called, uprobetracer runs in `pending` mode, only
@@ -55,6 +55,7 @@ type ProgType uint32
 const (
 	ProgUprobe ProgType = iota
 	ProgUretprobe
+	ProgUSDT
 )
 
 type inodeUUID struct {
@@ -118,7 +119,7 @@ func NewTracer[Event any](logger logger.Logger) (*Tracer[Event], error) {
 
 // AttachProg loads the ebpf program, and try attaching if there are pending containers
 func (t *Tracer[Event]) AttachProg(progName string, progType ProgType, attachTo string, prog *ebpf.Program) error {
-	if progType != ProgUprobe && progType != ProgUretprobe {
+	if progType != ProgUprobe && progType != ProgUretprobe && progType != ProgUSDT {
 		return fmt.Errorf("unsupported uprobe prog type: %q", progType)
 	}
 
@@ -129,12 +130,15 @@ func (t *Tracer[Event]) AttachProg(progName string, progType ProgType, attachTo 
 		return errors.New("loading uprobe program twice")
 	}
 
-	parts := strings.Split(attachTo, ":")
+	parts := strings.SplitN(attachTo, ":", 2)
 	if len(parts) < 2 {
 		return fmt.Errorf("invalid section name %q", attachTo)
 	}
 	if !filepath.IsAbs(parts[0]) && strings.Contains(parts[0], "/") {
 		return fmt.Errorf("section name must be either an absolute path or a library name: %q", parts[0])
+	}
+	if progType == ProgUSDT && len(strings.Split(parts[1], ":")) != 2 {
+		return fmt.Errorf("invalid USDT section name: %q", attachTo)
 	}
 
 	t.mu.Lock()
@@ -180,7 +184,7 @@ func (t *Tracer[Event]) searchForLibrary(containerPid uint32) ([]string, error) 
 	for _, targetPath := range targetPaths {
 		securedTargetPath, err := securejoin.SecureJoin(filepath.Join(host.HostProcFs, fmt.Sprint(containerPid), "root"), targetPath)
 		if err != nil {
-			t.logger.Debugf("path %q in ld cache is not available: %q", filePath, err.Error())
+			t.logger.Debugf("path %q in ld cache is not available: %s", filePath, err.Error())
 			continue
 		}
 		securedTargetPaths = append(securedTargetPaths, securedTargetPath)
@@ -200,6 +204,16 @@ func (t *Tracer[Event]) attachUprobe(file *os.File) (link.Link, error) {
 		return ex.Uprobe(t.attachSymbol, t.prog, nil)
 	case ProgUretprobe:
 		return ex.Uretprobe(t.attachSymbol, t.prog, nil)
+	case ProgUSDT:
+		attachInfo, err := getUsdtInfo(attachPath, t.attachSymbol)
+		if err != nil {
+			return nil, fmt.Errorf("reading USDT metadata: %w", err)
+		}
+		return ex.Uprobe(t.attachSymbol, t.prog,
+			&link.UprobeOptions{
+				Address:      attachInfo.attachAddress,
+				RefCtrOffset: attachInfo.semaphoreAddress,
+			})
 	default:
 		return nil, fmt.Errorf("attaching to inode: unsupported prog type: %q", t.progType)
 	}
@@ -210,7 +224,7 @@ func (t *Tracer[Event]) attach(containerPid uint32) {
 	var attachedUUIDs []inodeUUID
 	attachFilePaths, err := t.searchForLibrary(containerPid)
 	if err != nil {
-		t.logger.Debugf("attaching to container %d: %q", containerPid, err.Error())
+		t.logger.Debugf("attaching to container %d: %s", containerPid, err.Error())
 	}
 
 	if len(attachFilePaths) == 0 {
@@ -220,22 +234,25 @@ func (t *Tracer[Event]) attach(containerPid uint32) {
 	for _, filePath := range attachFilePaths {
 		file, err := os.OpenFile(filePath, unix.O_PATH, 0)
 		if err != nil {
-			t.logger.Debugf("opening file '%q' for uprobe: %q", filePath, err.Error())
+			t.logger.Debugf("opening file '%q' for uprobe: %s", filePath, err.Error())
 			continue
 		}
 		fileUUID, err := getInodeUUID(file)
 		if err != nil {
-			t.logger.Debugf("getting inode info for '%q': %q", filePath, err.Error())
+			t.logger.Debugf("getting inode info for '%q': %s", filePath, err.Error())
 			file.Close()
 			continue
 		}
 
-		t.logger.Debugf("attaching uprobe %q to container %d: %q", t.attachSymbol, containerPid, filePath)
+		t.logger.Debugf("attaching uprobe %q to container %d: %q", t.progName, containerPid, filePath)
 		attachedUUIDs = append(attachedUUIDs, fileUUID)
 
 		inode, exists := t.inodeRefCount[fileUUID]
 		if !exists {
-			progLink, _ := t.attachUprobe(file)
+			progLink, err := t.attachUprobe(file)
+			if err != nil {
+				t.logger.Debugf("failed to attach uprobe %q: %s", t.progName, err.Error())
+			}
 			t.inodeRefCount[fileUUID] = &inodeKeeper{1, file, progLink}
 		} else {
 			inode.counter++

commit d1853cbcf6bba0017935b184174a0c97dd18f91a

uprobetracer: Bound bytes read from the USDT note section
The .note.stapsdt section may carry SHF_COMPRESSED, in which case
Section.Open() transparently decompresses it, so a small file on disk can
expand to an arbitrary amount of data.

The existing caps do not bound this: maxNoteFieldSize (1 MiB) and
maxNoteCount (10,000) only bound each note individually, and their product
still allows around 10 GB of notes to be read and allocated. Parsing such a
file drove the process to 11.9 GB RSS and got it OOM-killed. Since the ELF
comes from an untrusted container and is parsed in a privileged process,
that terminates gadget monitoring on the node, and the resulting global
memory pressure can take down unrelated processes too. A failed allocation
is a fatal runtime error, so the recover() in getUsdtInfo cannot contain it.

Reject compressed note sections, as pkg/utils/safeelf already does for
symbol and string tables. Nothing legitimate is lost: the section holds a
few KiB of probe descriptors and no toolchain compresses it.

Rejecting is not sufficient on its own though, so also bound the bytes
actually read. For an uncompressed section Open() returns a reader spanning
the rest of the file rather than just the section, and the size declared in
the section header is no use as a bound either: it is attacker-controlled,
and debug/elf does not enforce it when reading through Open(), it only uses
it to seek. Section.Data() does read exactly that many bytes, which is why
the callers in resolveGoSymbol() are already bounded, but the note section
is parsed as a stream and cannot use it.

Fixes: 7ee5e7a90bbc ("pkg/uprobetracer: support USDT trace points")
Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>
Signed-off-by: Alban Crequy <albancrequy@microsoft.com>

pkg/uprobetracer/usdt.go
@@ -42,6 +42,13 @@ const (
 	// of service from a crafted section with millions of tiny valid notes.
 	// 10,000 is far more than any legitimate binary would have.
 	maxNoteCount = 10000
+
+	// maxNoteSectionSize limits the total number of bytes consumed from the
+	// note section. maxNoteFieldSize and maxNoteCount only bound each note
+	// individually, so on their own they still allow 10,000 * 1 MiB of notes.
+	// 16 MiB is far beyond any legitimate .note.stapsdt section, which is
+	// typically a few KiB.
+	maxNoteSectionSize = 16 * 1024 * 1024
 )
 
 type noteHeader struct {
@@ -118,7 +125,17 @@ func parseUsdtNotes(filepath string, attachSymbol string) (*usdtAttachInfo, erro
 	if noteSection.Type != elf.SHT_NOTE {
 		return nil, fmt.Errorf("section %q is not a note", sdtNoteSectionName)
 	}
-	notesReader := noteSection.Open()
+	// Reject compressed note sections, as pkg/utils/safeelf does for symbol
+	// and string tables. Section.Open() would transparently decompress them,
+	// letting a small file expand to an arbitrary amount of data. No toolchain
+	// compresses this section: it holds a few KiB of probe descriptors.
+	if noteSection.Flags&elf.SHF_COMPRESSED != 0 {
+		return nil, fmt.Errorf("compressed %q section not supported", sdtNoteSectionName)
+	}
+	// Bound the bytes actually read. The size declared in the section header
+	// is attacker-controlled and cannot be used for this: debug/elf does not
+	// enforce it when reading through Open(), it only uses it to seek.
+	notesReader := io.LimitReader(noteSection.Open(), maxNoteSectionSize)
 
 	baseSection := elfReader.Section(sdtBaseSectionName)
 	if baseSection == nil {